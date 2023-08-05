3ba4a55db7a9779adfd930af64e8c39695fa9779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:22 +0000

    Add package dtwc/0.0.1/dtwc-0.0.1.tar.gz

---
+++ packages/dtwc/dtwc-0.0.1.tar.gz/dtwc-0.0.1/dwc/sources/desktoppr.py
@@ -0,0 +1,33 @@
+import datetime
+import random
+
+import requests
+
+from dwc.utils import save_image, set_wallpaper_permanent
+
+date = datetime.date.today()
+
+url = 'https://api.desktoppr.co/1/wallpapers/random'
+
+
+def getphotourl():
+    source = requests.get(url)
+    if source.status_code == 200:
+        source_code = source.json()
+        photo_url = source_code['response']['image']['url']
+        return photo_url
+
+
+def picpath_desktoppr(saveDir, SHOW_DEBUG):
+    photo_url = getphotourl()
+    desktoppr_path = saveDir + 'Desktoppr' + str(date) + str(
+                random.randint(1, 100000)) + '.jpg'
+    if SHOW_DEBUG:
+        print("Download from: ", photo_url)
+    savelink = save_image(photo_url, desktoppr_path, SHOW_DEBUG)
+    return savelink
+
+
+def change_wp(wp_desktoppr, saveDir, SHOW_DEBUG):
+    savelink = picpath_desktoppr(saveDir, SHOW_DEBUG)
+    set_wallpaper_permanent(savelink, SHOW_DEBUG)
---
+++ packages/dtwc/dtwc-0.0.1.tar.gz/dtwc-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+requirement_list = [r.strip()
+                    for r in open('requirements.txt', 'r').readlines() if r]
+
+if __name__ == '__main__':
+    setup(
+        name='dtwc',
+        version='0.0.1',
+        install_requires=requirement_list,
+        description='Change your desktop wallpaper daily!',
+        long_description=open('README.md').read(),
+        entry_points={
+            'console_scripts': [
+                'dwc = dwc.main:main'
+            ],
+        },
+        python_requires='>=3.5',
+        author='ayan-b',
+        author_email='ayanbanerjee7777@gmail.com',
+        url='https://github.com/ayan-b/Desktop-Wallpaper-Changer',
+        license="MIT",
+        packages=find_packages(exclude=('tests',)),
+    )
