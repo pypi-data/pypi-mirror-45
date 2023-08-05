e013f6b294be31f1ba3d1269834c0b60e7f1b771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:06 +0000

    Add package directory-to-sql/0.1.2/directory_to_sql-0.1.2.tar.gz

---
+++ packages/directory-to-sql/directory_to_sql-0.1.2.tar.gz/directory_to_sql-0.1.2/README.rst
@@ -0,0 +1,19 @@
+directory_to_sql
+======================
+
+Get a SQLite database of a directory tree.
+
+.. image:: https://travis-ci.com/hydrospanner/directory_to_sql.svg?branch=master
+
+The data retrived includes:
+
+- File sizes and modification date.
+- Folder sizes and file counts.
+- Recursive (including sub-folders) folder sizes and file counts.
+
+To create the database and execute some example queries
+listing largest files and folders::
+
+  from directory_to_sql import get_db, top_10
+  conn = get_db('\\', 'db.db')
+  top_10(conn)
---
+++ packages/directory-to-sql/directory_to_sql-0.1.2.tar.gz/directory_to_sql-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+with open('README.rst', 'r') as file:
+    long_description = file.read()
+
+setup(name='directory_to_sql',
+      version='0.1.2',
+      description='Get a SQL database of file directory data.',
+      long_description=long_description,
+      url='http://github.com/hydrospanner/directory_to_sql',
+      author='John Tucker',
+      license='MIT',
+      packages=['directory_to_sql'],
+      install_requires=[
+          'pandas',
+      ],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      )
