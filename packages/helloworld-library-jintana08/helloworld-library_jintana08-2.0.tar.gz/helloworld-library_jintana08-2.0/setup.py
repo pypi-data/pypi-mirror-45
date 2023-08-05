1ea507fa9d0d5cb90d4471f3c2d269f7559e15f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:31 +0000

    Add package helloworld-library-jintana08/2.0/helloworld-library_jintana08-2.0.tar.gz

---
+++ packages/helloworld-library-jintana08/helloworld-library_jintana08-2.0.tar.gz/helloworld-library_jintana08-2.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="helloworld-library_jintana08",
+    version='2.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/jintana08/demo-helloworld-library',
+    author='Somkiat',
+    author_email='absuwan08@hotmail.com',
+)
