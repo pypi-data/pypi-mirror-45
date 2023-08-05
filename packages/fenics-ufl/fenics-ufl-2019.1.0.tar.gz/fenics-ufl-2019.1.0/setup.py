148c51cd0701eaeaa4455e71a1637b716c6078ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:29 +0000

    Add package fenics-ufl/2019.1.0/fenics-ufl-2019.1.0.tar.gz

---
+++ packages/fenics-ufl/fenics-ufl-2019.1.0.tar.gz/fenics-ufl-2019.1.0/setup.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import sys
+
+module_name = "ufl"
+
+if sys.version_info < (3, 5):
+    print("Python 3.5 or higher required, please upgrade.")
+    sys.exit(1)
+
+version = "2019.1.0"
+
+url = "https://bitbucket.org/fenics-project/{}/".format(module_name)
+tarball = None
+if 'dev' not in version:
+    tarball = url + "downloads/fenics-{}-{}.tar.gz".format(module_name, version)
+
+CLASSIFIERS = """\
+Development Status :: 5 - Production/Stable
+Intended Audience :: Developers
+Intended Audience :: Science/Research
+License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)
+Operating System :: POSIX
+Operating System :: POSIX :: Linux
+Operating System :: MacOS :: MacOS X
+Operating System :: Microsoft :: Windows
+Programming Language :: Python
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Topic :: Scientific/Engineering :: Mathematics
+Topic :: Software Development :: Libraries :: Python Modules
+"""
+
+setup(
+    name="fenics-ufl",
+    version=version,
+    description="Unified Form Language",
+    author="Martin Sandve Alnæs, Anders Logg",
+    author_email="fenics-dev@googlegroups.com",
+    url=url,
+    download_url=tarball,
+    classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+    packages=[
+        "ufl",
+        "ufl.utils",
+        "ufl.finiteelement",
+        "ufl.core",
+        "ufl.corealg",
+        "ufl.algorithms",
+        "ufl.formatting",
+    ],
+    package_dir={"ufl": "ufl"},
+    install_requires=["numpy"])
---
+++ packages/fenics-ufl/fenics-ufl-2019.1.0.tar.gz/fenics-ufl-2019.1.0/test/test_check_arities.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env py.test
+# -*- coding: utf-8 -*-
+import pytest
+from ufl import *
+from ufl.algorithms.compute_form_data import compute_form_data
+from ufl.algorithms.check_arities import ArityMismatch
+
+
+def test_check_arities():
+    # Code from bitbucket issue #49
+    cell = tetrahedron
+    D = Mesh(cell)
+    V = FunctionSpace(D, VectorElement("P", cell, 2))
+    dv = TestFunction(V)
+    du = TrialFunction(V)
+
+    X = SpatialCoordinate(D)
+    N = FacetNormal(D)
+
+    u = Coefficient(V)
+    x = X + u
+    F = grad(x)
+    n = cofac(F) * N
+
+    M = inner(x, n) * ds
+    L = derivative(M, u, dv)
+    a = derivative(L, u, du)
+
+    fd = compute_form_data(M)
+    fd = compute_form_data(L)
+    fd = compute_form_data(a)
+
+    assert True
+
+
+def test_complex_arities():
+    cell = tetrahedron
+    D = Mesh(cell)
+    V = FunctionSpace(D, VectorElement("P", cell, 2))
+    v = TestFunction(V)
+    u = TrialFunction(V)
+
+    # Valid form.
+    F = inner(u, v) * dx
+    compute_form_data(F, complex_mode=True)
+    # Check that adjoint conjugates correctly
+    compute_form_data(adjoint(F), complex_mode=True)
+
+    with pytest.raises(ArityMismatch):
+        compute_form_data(inner(v, u) * dx, complex_mode=True)
+
+    with pytest.raises(ArityMismatch):
+        compute_form_data(inner(conj(v), u) * dx, complex_mode=True)
