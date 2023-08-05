9e94fbcf8a79c6d5fbab5150899665ae0f5f6668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:48 +0000

    Add package django-sorcery/0.9.0/django_sorcery-0.9.0-py2.py3-none-any.whl

---
+++ packages/django-sorcery/django_sorcery-0.9.0-py2.py3-none-any.whl/django_sorcery/db/session.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+sqlalchemy session related things
+"""
+from __future__ import absolute_import, print_function, unicode_literals
+
+from sqlalchemy import event, orm
+
+from . import signals
+
+
+class SignallingSession(orm.Session):
+    """
+    A custom sqlalchemy session implementation that provides signals
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(SignallingSession, self).__init__(*args, **kwargs)
+
+        def before_flush(session, flush_context, instances):
+            signals.before_flush.send(session, flush_context=flush_context, instances=instances)
+
+        event.listen(self, "before_flush", before_flush)
+
+        def after_flush(session, flush_context):
+            signals.after_flush.send(session, flush_context=flush_context)
+
+        event.listen(self, "after_flush", after_flush)
+
+    def query(self, *args, **kwargs):
+        """
+        Override to try to use the model.query_class
+        """
+        if len(args) == 1 and hasattr(args[0], "query_class") and args[0].query_class is not None:
+            return args[0].query_class(*args, session=self, **kwargs)
+
+        return super(SignallingSession, self).query(*args, **kwargs)
+
+    def commit(self):
+        """
+        Flushes pending changes and commits the current transaction. If the transaction is the main transaction,
+        triggers before and after commit signals.
+        """
+        is_main = self.transaction and (self.transaction._parent is None or not self.transaction.nested)
+
+        if is_main:
+            signals.before_commit.send(self)
+            signals.before_scoped_commit.send(self)
+
+        super(SignallingSession, self).commit()
+
+        if is_main:
+            signals.after_scoped_commit.send(self)
+            signals.after_commit.send(self)
+
+    def rollback(self):
+        super(SignallingSession, self).rollback()
+
+        if self.transaction and (self.transaction._parent is None or self.transaction.nested):
+            signals.after_scoped_rollback.send(self)
+            signals.after_rollback.send(self)
