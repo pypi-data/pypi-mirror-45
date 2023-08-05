22d90a7b17b3257123d23ded8fc8a19d1b8b481e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:05 +0000

    Add package SimpleSQLite/0.44.1/SimpleSQLite-0.44.1.tar.gz

---
+++ packages/SimpleSQLite/SimpleSQLite-0.44.1.tar.gz/SimpleSQLite-0.44.1/test/test_from_file.py
@@ -0,0 +1,42 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import print_function, unicode_literals
+
+import os
+
+import pytablereader as ptr
+import pytest
+from pytablewriter import dump_tabledata
+from simplesqlite import SimpleSQLite, SQLiteTableDataSanitizer
+
+
+class Test_SimpleSQLite_create_table_from_tabledata(object):
+    @pytest.mark.parametrize(["filename"], [["python - Wiktionary.html"]])
+    def test_smoke(self, tmpdir, filename):
+        p = tmpdir.join("tmp.db")
+        con = SimpleSQLite(str(p), "w")
+
+        test_data_file_path = os.path.join(os.path.dirname(__file__), "data", filename)
+        loader = ptr.TableFileLoader(test_data_file_path)
+
+        success_count = 0
+
+        for table_data in loader.load():
+            if table_data.is_empty():
+                continue
+
+            print(dump_tabledata(table_data))
+
+            try:
+                con.create_table_from_tabledata(SQLiteTableDataSanitizer(table_data).normalize())
+                success_count += 1
+            except ValueError as e:
+                print(e)
+
+        con.commit()
+
+        assert success_count > 0
