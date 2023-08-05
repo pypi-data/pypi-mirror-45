3fbd4314a18add3c335065483c4b086636bf1b11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:44 +0000

    Add package openqlab/0.1.5/OpenQlab-0.1.5-py2.py3-none-any.whl

---
+++ packages/openqlab/OpenQlab-0.1.5-py2.py3-none-any.whl/OpenQlab/io/importers/__init__.py
@@ -0,0 +1,35 @@
+import os.path
+import glob
+import sys
+import importlib
+from . import utils
+
+if __name__ == '__main__':
+    importer_dir = os.path.abspath(sys.argv[0])
+else:
+    importer_dir = os.path.abspath(__file__)
+
+importer_dir = os.path.dirname(importer_dir)
+
+validImporters = {}
+
+'''
+auto importers are used to automatically determine the file _type,
+they must raise utils.UnknownFileType if they don't know what to do with
+this file.
+'''
+validAutoImporters = {}
+for fn in glob.glob(importer_dir + "/*.py"):
+    importer = utils.get_file_basename(fn)
+    if importer == '__init__' or importer == 'utils':
+        continue
+    try:
+        importer = importlib.import_module('.' + importer, package='OpenQlab.io.importers')
+    except ImportError as e:
+        print(e)
+        continue
+    try:
+        validImporters.update(importer.get_importers())
+        validAutoImporters.update(importer.get_auto_importers())
+    except AttributeError as e:
+        continue
