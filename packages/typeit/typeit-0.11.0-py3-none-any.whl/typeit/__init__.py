e71ea157999e09b40eff2c5bfbfe1a4851ee03d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:30 +0000

    Add package typeit/0.11.0/typeit-0.11.0-py3-none-any.whl

---
+++ packages/typeit/typeit-0.11.0-py3-none-any.whl/typeit/__init__.py
@@ -0,0 +1,5 @@
+from .parser import type_constructor
+from .codegen import typeit
+from . import flags
+
+__all__ = ['type_constructor', 'typeit', 'flags']
---
+++ packages/typeit/typeit-0.11.0-py3-none-any.whl/typeit/definitions.py
@@ -0,0 +1,37 @@
+from typing import NamedTuple, Union, Any, Mapping, Type
+
+from pyrsistent import pmap
+from pyrsistent.typing import PMap
+
+from .flags import _Flag
+
+
+OverridesT = PMap[
+    Union[
+        # field name override
+        property,
+        # flag override
+        _Flag,
+        # new type extension
+        Type
+    ],
+    Any
+]
+
+
+OverrideT = Union[
+    # flag override
+    _Flag,
+    # new type extension
+    Type,
+    Mapping[property, str],
+]
+
+
+NO_OVERRIDES: OverridesT = pmap()
+
+
+class FieldDefinition(NamedTuple):
+    source_name: str
+    field_name: str
+    field_type: Union[Any, NamedTuple]
