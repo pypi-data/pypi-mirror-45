11ad9960010f7df00f1215bee2d3a32586111a0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:41 +0000

    Add package ocrd-tesserocr/0.2.1/ocrd_tesserocr-0.2.1.tar.gz

---
+++ packages/ocrd-tesserocr/ocrd_tesserocr-0.2.1.tar.gz/ocrd_tesserocr-0.2.1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Installs four executables:
+
+    - ocrd_tesserocr_recognize
+    - ocrd_tesserocr_segment_region
+    - ocrd_tesserocr_segment_line
+    - ocrd_tesserocr_segment_word
+"""
+import codecs
+
+from setuptools import setup, find_packages
+
+setup(
+    name='ocrd_tesserocr',
+    version='0.2.1',
+    description='Tesserocr bindings',
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    author='Konstantin Baierer',
+    author_email='unixprog@gmail.com',
+    url='https://github.com/OCR-D/ocrd_tesserocr',
+    license='Apache License 2.0',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=open('requirements.txt').read().split('\n'),
+    package_data={
+        '': ['*.json', '*.yml', '*.yaml'],
+    },
+    entry_points={
+        'console_scripts': [
+            'ocrd-tesserocr-recognize=ocrd_tesserocr.cli:ocrd_tesserocr_recognize',
+            'ocrd-tesserocr-segment-region=ocrd_tesserocr.cli:ocrd_tesserocr_segment_region',
+            'ocrd-tesserocr-segment-line=ocrd_tesserocr.cli:ocrd_tesserocr_segment_line',
+            'ocrd-tesserocr-segment-word=ocrd_tesserocr.cli:ocrd_tesserocr_segment_word',
+        ]
+    },
+)
---
+++ packages/ocrd-tesserocr/ocrd_tesserocr-0.2.1.tar.gz/ocrd_tesserocr-0.2.1/test/test_recognize.py
@@ -0,0 +1,66 @@
+import os
+import shutil
+
+from test.base import TestCase, main, assets, skip
+
+from ocrd.resolver import Resolver
+from ocrd_tesserocr.segment_word import TesserocrSegmentWord
+from ocrd_tesserocr.segment_line import TesserocrSegmentLine
+from ocrd_tesserocr.segment_region import TesserocrSegmentRegion
+from ocrd_tesserocr.recognize import TesserocrRecognize
+
+#METS_HEROLD_SMALL = assets.url_of('SBB0000F29300010000/data/mets_one_file.xml')
+METS_HEROLD_SMALL = assets.url_of('kant_aufklaerung_1784/data/mets.xml')
+
+WORKSPACE_DIR = '/tmp/pyocrd-test-recognizer'
+
+class TestTesserocrRecognize(TestCase):
+
+    def setUp(self):
+        if os.path.exists(WORKSPACE_DIR):
+            shutil.rmtree(WORKSPACE_DIR)
+        os.makedirs(WORKSPACE_DIR)
+
+    #skip("Takes too long")
+    def runTest(self):
+        resolver = Resolver()
+        workspace = resolver.workspace_from_url(METS_HEROLD_SMALL, dst_dir=WORKSPACE_DIR)
+        TesserocrSegmentRegion(
+            workspace,
+            input_file_grp="OCR-D-IMG",
+            output_file_grp="OCR-D-SEG-BLOCK"
+        ).process()
+        workspace.save_mets()
+
+        TesserocrSegmentLine(
+            workspace,
+            input_file_grp="OCR-D-SEG-BLOCK",
+            output_file_grp="OCR-D-SEG-LINE"
+        ).process()
+        workspace.save_mets()
+
+        TesserocrRecognize(
+            workspace,
+            input_file_grp="OCR-D-SEG-LINE",
+            output_file_grp="OCR-D-OCR-TESS",
+            parameter={'textequiv_level': 'line'} # add dep tesseract-ocr-script-frak: , 'model': 'Fraktur'
+        ).process()
+        workspace.save_mets()
+
+        TesserocrSegmentWord(
+            workspace,
+            input_file_grp="OCR-D-SEG-LINE",
+            output_file_grp="OCR-D-SEG-WORD"
+        ).process()
+        workspace.save_mets()
+
+        TesserocrRecognize(
+            workspace,
+            input_file_grp="OCR-D-SEG-WORD",
+            output_file_grp="OCR-D-OCR-TESS-W2C",
+            parameter={'textequiv_level': 'glyph'} # add dep tesseract-ocr-script-frak: , 'model': 'Fraktur'}
+        ).process()
+        workspace.save_mets()
+
+if __name__ == '__main__':
+    main()
