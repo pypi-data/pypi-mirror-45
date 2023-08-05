081da1cd91319054bd5607049b0fa0b8fcf9d954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:05 +0000

    Add package anesthetic/0.8.0/anesthetic-0.8.0.tar.gz

---
+++ packages/anesthetic/anesthetic-0.8.0.tar.gz/anesthetic-0.8.0/anesthetic/__init__.py
@@ -0,0 +1,18 @@
+"""Anesthetic: nested sampling post-processing.
+
+Key routines:
+
+- ``MCMCSamples.build``
+- ``MCMCSamples.read``
+- ``NestedSamples.build``
+- ``NestedSamples.read``
+
+"""
+import anesthetic.samples
+import anesthetic.plot
+
+
+MCMCSamples = anesthetic.samples.MCMCSamples
+NestedSamples = anesthetic.samples.NestedSamples
+make_2D_axes = anesthetic.plot.make_2D_axes
+make_1D_axes = anesthetic.plot.make_1D_axes
