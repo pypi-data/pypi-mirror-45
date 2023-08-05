9170dfaf7671d19675f2bff7e72ea5f171ba210c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:35 +0000

    Add package proxytest/0.3.1/proxytest-0.3.1.tar.gz

---
+++ packages/proxytest/proxytest-0.3.1.tar.gz/proxytest-0.3.1/proxytest/__init__.py
@@ -0,0 +1,2 @@
+from .proxytest import *
+from .version import __version__
---
+++ packages/proxytest/proxytest-0.3.1.tar.gz/proxytest-0.3.1/proxytest/version.py
@@ -0,0 +1,2 @@
+# Semantic Versioning 2.0.0 - see https://semver.org/
+__version__ = '0.3.1'  # 0.X.X means API not yet stabilized!
