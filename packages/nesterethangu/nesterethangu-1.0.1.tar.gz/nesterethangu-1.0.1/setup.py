c856ccecdba7694d837214329820b43675087a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:12 +0000

    Add package nesterethangu/1.0.1/nesterethangu-1.0.1.tar.gz

---
+++ packages/nesterethangu/nesterethangu-1.0.1.tar.gz/nesterethangu-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name			=	'nesterethangu',
+		version			=	'1.0.1',
+		py_modules		=	['nesterethangu'],
+		author			=	'hfpython',
+		author_email	=	'guzhuoping@163.com',
+		url				=	'http://www.headfirstlabs.com',
+		description		=	'A simple printer of nested lists',
+	)
