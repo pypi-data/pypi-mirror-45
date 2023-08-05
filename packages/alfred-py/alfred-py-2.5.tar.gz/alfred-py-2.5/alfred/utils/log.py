58f4fc17d7cd48984063ff9dcf548ff0657ea309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:19 +0000

    Add package alfred-py/2.5/alfred-py-2.5.tar.gz

---
+++ packages/alfred-py/alfred-py-2.5.tar.gz/alfred-py-2.5/alfred/utils/log.py
@@ -0,0 +1,14 @@
+# -----------------------
+#
+# Copyright Jin Fagang @2018
+# 
+# 1/31/19
+# log
+# -----------------------
+from loguru import logger
+import sys
+
+
+def init_logger():
+    logger.remove()  # Remove the pre-configured handler
+    logger.start(sys.stderr, format="<lvl>{level}</lvl> {time:MM-DD HH:mm:ss} {file}:{line} - {message}")
