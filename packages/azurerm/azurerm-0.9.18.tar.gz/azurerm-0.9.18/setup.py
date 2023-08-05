136ef6fe2f6c730cca721e2ec76e562952e4bde7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:52 +0000

    Add package azurerm/0.9.18/azurerm-0.9.18.tar.gz

---
+++ packages/azurerm/azurerm-0.9.18.tar.gz/azurerm-0.9.18/setup.py
@@ -0,0 +1,24 @@
+'''Setup file for azurerm Azure REST wrapper library'''
+from setuptools import setup
+
+try:
+    import pypandoc
+    LONG_DESCRIPTION = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    LONG_DESCRIPTION = open('README.md').read()
+
+
+setup(name='azurerm',
+      version='0.9.18',
+      description='Azure Resource Manager REST wrappers',
+      long_description=LONG_DESCRIPTION,
+      url='http://github.com/gbowerman/azurerm',
+      author='sendmarsh',
+      author_email='guybo@outlook.com',
+      license='MIT',
+      packages=['azurerm'],
+      install_requires=[
+          'adal',
+          'requests',
+      ],
+      zip_safe=False)
