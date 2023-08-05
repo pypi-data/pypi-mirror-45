5564dd8cea53e35092a694bcff406e245979ef7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:13 +0000

    Add package carball/0.6.8/carball-0.6.8.tar.gz

---
+++ packages/carball/carball-0.6.8.tar.gz/carball-0.6.8/setup.py
@@ -0,0 +1,33 @@
+import json
+
+import setuptools
+from setuptools import setup
+import os
+
+with open(os.path.join('carball', 'analysis', 'PROTOBUF_VERSION'), 'r') as f:
+    PROTOBUF_VERSION = json.loads(f.read())
+
+subversion = 8
+version_string = '0.' + str(PROTOBUF_VERSION) + '.' + str(subversion)
+
+if os.path.isfile('README.md'):
+    with open("README.md", "r") as readme_file:
+        long_description = readme_file.read()
+else:
+    long_description = ''
+
+setup(
+    name='carball',
+    version=version_string,
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=['pandas==pandas-0.24.2', 'protobuf==3.6.1', 'xlrd'],
+    url='https://github.com/SaltieRL/carball',
+    keywords=['rocket-league'],
+    license='Apache 2.0',
+    author='Matthew Mage, Harry Xie, David Turner',
+    author_email='sciguymjm@gmail.com',
+    description='Rocket League replay parsing and analysis.',
+    long_description=long_description,
+    exclude_package_data={'': ['.gitignore', '.git/*', '.git/**/*', 'replays/*']}
+)
