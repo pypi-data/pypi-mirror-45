a6e1c5c7ce3c833f4f097bb4d2b4f69c1f64a00e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:27 +0000

    Add package zaif-client/0.1.1/zaif-client-0.1.1.tar.gz

---
+++ packages/zaif-client/zaif-client-0.1.1.tar.gz/zaif-client-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+try:
+    import setuptools
+    from setuptools import setup, find_packages
+except ImportError:
+    print("Please install setuptools.")
+
+import os
+long_description = 'zaif-client is a python client (sync/async) library for zaif api.'
+if os.path.exists('README.txt'):
+    long_description = open('README.txt').read()
+
+setup(
+    name  = 'zaif-client',
+    version = '0.1.1',
+    description = 'zaif-client is a python client (sync/async) library for zaif api.',
+    long_description = long_description,
+    license = 'MIT',
+    author = '10mohi6',
+    author_email = '10.mohi.6.y@gmail.com',
+    url = 'https://github.com/10mohi6/zaif-api-python-client',
+    keywords = 'zaif',
+    packages = find_packages(),
+    install_requires = ['grequests'],
+    classifiers = [
+      'Development Status :: 5 - Production/Stable',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.7',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'License :: OSI Approved :: MIT License'
+    ]
+)
