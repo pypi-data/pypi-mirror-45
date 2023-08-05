ded58038a97014e8e4f71a0e3f114d6b89e0de5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:32 +0000

    Add package libowei/5.11.6/libowei-5.11.6.tar.gz

---
+++ packages/libowei/libowei-5.11.6.tar.gz/libowei-5.11.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "libowei",
+    version = "5.11.6",
+    keywords = "math",
+    description = "数学工程库",
+    long_description = "基础数学,统计学,物理",
+    license = "MIT Licence",
+    url="https://github.com/",
+    author = "Libowei",
+    author_email = "913911037@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["sympy"]
+)
