b46e50736e2e4662d3e8f5308fa617a167ec381f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:02 +0000

    Add package shuogg/0.1/shuogg-0.1.tar.gz

---
+++ packages/shuogg/shuogg-0.1.tar.gz/shuogg-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="shuogg",              # 包名
+    version="0.1",              # 版本信息
+    packages=['shuogg'],        # 要打包的项目文件夹
+    description="shuoGG's common utils",
+    keywords='python util',
+    author='shuoGG',
+    author_email='328893769@qq.com',
+    url='https://github.com/shuoGG1239/ShuoPyUtils',
+    zip_safe=False
+)
