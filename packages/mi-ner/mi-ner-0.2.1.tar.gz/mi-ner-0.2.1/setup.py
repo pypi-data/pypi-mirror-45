440cfc9c4eb1c3c982b44d8548385ba761f3c507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:24 +0000

    Add package mi-ner/0.2.1/mi-ner-0.2.1.tar.gz

---
+++ packages/mi-ner/mi-ner-0.2.1.tar.gz/mi-ner-0.2.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='mi-ner',
+    version='0.2.1',
+    description='A python library for named entity recognition evaluation',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Ryuya Ikeda',
+    author_email='rikeda71@gmail.com',
+    url='https://github.com/Andolab/miNER',
+    license='MIT',
+    keywords=['named entity recognition', 'nlp',
+              'natural language processing'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing',
+    ],
+    packages=['miner'],
+    test_suite='tests',
+)
