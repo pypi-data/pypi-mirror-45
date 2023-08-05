fad32b32c0f56137599f931e5aafd2993ab97a87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:22 +0000

    Add package jtyoui/19.4.22/jtyoui-19.4.22-py3-none-any.whl

---
+++ packages/jtyoui/jtyoui-19.4.22-py3-none-any.whl/jtyoui/data/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+# @Time : 2019/2/10 0010
+# @Email : jtyoui@qq.com
+# @Software : PyCharm
+from .gitee import fetch_gitee, download_gitee  # 爬取我自己Gitee上的数据
+from .han_table import Han_J_F  # 导入繁体字和简体字的数据
+from .constant import *  # 平时常见的常量
+from .province import *  # 中国省市所有名字
