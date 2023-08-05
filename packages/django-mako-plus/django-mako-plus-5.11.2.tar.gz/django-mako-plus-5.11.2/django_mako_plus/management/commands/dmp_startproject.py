7657c24b0cc74a9efb0594ff47c28bd7227fd073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:48 +0000

    Add package django-mako-plus/5.11.2/django-mako-plus-5.11.2.tar.gz

---
+++ packages/django-mako-plus/django-mako-plus-5.11.2.tar.gz/django-mako-plus-5.11.2/django_mako_plus/management/commands/dmp_startproject.py
@@ -0,0 +1,43 @@
+from django.apps import apps
+from django.core.management.commands.startproject import Command as StartProjectCommand
+from django_mako_plus.management.mixins import DMPCommandMixIn
+
+import os, os.path, platform
+
+
+NOT_SET = object()
+
+
+class Command(DMPCommandMixIn, StartProjectCommand):
+    help = (
+        "Creates a DMP project directory structure for the given project "
+        "name in the current directory or optionally in the given directory."
+    )
+    requires_system_checks = False
+
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        self.get_action_by_dest(parser, 'template').default = NOT_SET
+
+    def handle(self, *args, **options):
+        if options.get('template') is NOT_SET:
+            # set the template to a DMP app
+            options['template'] = 'http://cdn.rawgit.com/doconix/django-mako-plus/master/project_template.zip'
+            # attempt to use a local DMP install instead of the online repo as specified above
+            # this should work unless the installation type is not normal
+            template_dir = os.path.join(self.get_dmp_path(), 'project_template')
+            if os.path.exists(template_dir):
+                options['template'] = template_dir
+
+        # call the super
+        StartProjectCommand.handle(self, *args, **options)
+
+        # display a message to help the new kids
+        pyexec = 'python' if platform.system() == 'Windows' else 'python3'
+        self.message("""Project {name} created successfully!
+
+What's next?
+    1. cd {name}
+    2. {pyexec} manage.py dmp_startapp homepage
+
+""".format(name=options.get('name'), pyexec=pyexec))
