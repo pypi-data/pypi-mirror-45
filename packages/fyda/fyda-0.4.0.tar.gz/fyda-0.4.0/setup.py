ba298087cf610d0b02c77c1d4faf74a26a4d5079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:06 +0000

    Add package fyda/0.4.0/fyda-0.4.0.tar.gz

---
+++ packages/fyda/fyda-0.4.0.tar.gz/fyda-0.4.0/setup.py
@@ -0,0 +1,32 @@
+"""Setup."""
+from setuptools import setup
+
+setup(
+    name='fyda',
+    packages=['fyda'],
+    version='0.4.0',
+    license='MIT',
+    description='General data interface for Python 3',
+    author='Robert Enzmann',
+    author_email='runningwithrobb@gmail.com',
+    url='https://github.com/renzmann/fyda',
+    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+    keywords=['Python', 'Data', 'Interface', 'Data Science', 'python 3'],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'configparser',
+        'boto3'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
