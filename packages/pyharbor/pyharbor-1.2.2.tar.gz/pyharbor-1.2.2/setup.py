ef73dca0add0bfebaf03b588f8e9c384aeb4db8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:38 +0000

    Add package pyharbor/1.2.2/pyharbor-1.2.2.tar.gz

---
+++ packages/pyharbor/pyharbor-1.2.2.tar.gz/pyharbor-1.2.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+setup(
+    name = "pyharbor",
+    version = "1.2.2",
+    keywords = ("pyharbor", "harbor","python"),
+    description = "The harbor python SDK",
+    long_description = "The harbor python SDK",
+    license = "MIT Licence",
+    url = "https://github.com/zhangliu520/python-harbor.git",
+    author = "mrzl",
+    author_email = "752477168@qq.com",
+    packages = find_packages(),
+
+    platforms = "any",
+    install_requires = [],
+
+)
+
