061c7473f25eb3f1ba1f7e3e5becdd9a6c06cfc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:28 +0000

    Add package crushfs/0.4.1/crushfs-0.4.1-py3-none-any.whl

---
+++ packages/crushfs/crushfs-0.4.1-py3-none-any.whl/crushfs/__init__.py
@@ -0,0 +1,4 @@
+from .async_client import AsyncClient
+from .sync_client import SyncClient
+from .error import CrushFsFileNotFoundError, CrushFsDownloadObjectError, CrushFsUploadObjectError, CrushFsCombineObjectsError
+
