aa1c206752bf4d029ade7101ac104041b0194f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:42 +0000

    Add package havedocker/0.1.3.4/havedocker-0.1.3.4.tar.gz

---
+++ packages/havedocker/havedocker-0.1.3.4.tar.gz/havedocker-0.1.3.4/README.md
@@ -0,0 +1,37 @@
+# `havedocker ` `update--0.1.3.4` 
+
+<br>
+
+
+### `repair problems` 
+
+> 1. Will ` Agility_Docker ` exist in the BUG has been fixed
+>
+> 2. Fixed `Agility_Docker` class `__init__` , local logic
+>
+> 3. In ` Agility_Docker ` class, remove ` debug ` parameters
+>
+
+<br>
+
+
+### newly increased
+
+>`Dockerstats`: entered the development process
+
+<br>
+
+### History node version
+
+> `newly increased 'help' from 0.1.3.2 `
+>
+> `Method name change from 0.1.3.3`
+>
+> `Docker state formatted output from 0.1.3.4`
+
+<br>
+
+### Feature push for unknown version:
+
+1. Docker monitoring will be added, and `docker stats` will obtain real-time data.
+
---
+++ packages/havedocker/havedocker-0.1.3.4.tar.gz/havedocker-0.1.3.4/setup.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="havedocker",
+    version="0.1.3.4",
+    author="haitanghuadeng",
+    author_email="491609917@qq.com",
+    description="Rename, new functionality",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/haitanghuadeng",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
