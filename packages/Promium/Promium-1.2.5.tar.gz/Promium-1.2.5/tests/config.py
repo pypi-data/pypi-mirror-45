81e73502f04c8499cf127f1833becffca197815c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:10 +0000

    Add package Promium/1.2.5/Promium-1.2.5.tar.gz

---
+++ packages/Promium/Promium-1.2.5.tar.gz/Promium-1.2.5/tests/config.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+WIKI_HOST = ''
+PROJECT_HOST = 'prom.ua'
+PROJECT_PORT = ''
+GRID_HOST_PORT = '0.0.0.0:13400'
+DB = ''
+CHECK_TEST_CASE = False
+
+OPERA_BINARY_LOCATION = '/usr/bin/opera'
+
+# for windows 7
+# OPERA_BINARY_LOCATION = r'C:\Program Files\Opera\launcher.exe'
+
+# for windows 10
+# import os
+# OPERA_BINARY_LOCATION = (
+#     r'C:\Users\{}\AppData\Local\Programs\Opera\launcher.exe'.format(
+#         os.environ.get('USERNAME')
+#     )
+# )
