ba8ac9db041ff1699d22eb07da2d1adc3fbe2359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:18 +0000

    Add package bane/1.8.7/bane-1.8.7.tar.gz

---
+++ packages/bane/bane-1.8.7.tar.gz/bane-1.8.7/bane/__init__.py
@@ -0,0 +1,28 @@
+#coding: utf-8
+'''
+
+email:
+
+      trap.leader.123@gmail.com
+
+facebook:
+
+      https://www.facebook.com/ala.chamtouri.73
+
+linkedin:
+
+      https://www.linkedin.com/in/ala-bouali-bb17ab132/
+'''
+from payloads import *
+from ddos import *
+from hasher import *
+from pager import *
+from infos import *
+from extrafun import *
+from bruteforcer import *
+from vulns import *
+from iot import *
+from wp import *
+from filt import *
+from anti_dos import *
+from proxer import *
