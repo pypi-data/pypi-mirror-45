ee3240d4c7d99bc50a43a3bef97609d688692f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:36 +0000

    Add package exdir/0.4.0/exdir-0.4.0.tar.gz

---
+++ packages/exdir/exdir-0.4.0.tar.gz/exdir-0.4.0/exdir/core/exdir_file.py
@@ -0,0 +1,178 @@
+import os
+import shutil
+import pathlib
+import warnings
+
+import exdir
+from . import exdir_object as exob
+from .group import Group
+from .. import utils
+
+
+class File(Group):
+    """
+    Exdir file object.
+    A File is a special type of :class:`.Group`.
+    See :class:`.Group` for documentation of inherited functions.
+
+    To create a File, call the File constructor with the name of the File you wish to create:
+
+        >>> import exdir
+        >>> import numpy as np
+        >>> f = exdir.File("mytestfile.exdir")
+
+    The :code:`File` object :code:`f` now points to the root folder in the exdir file
+    structure.
+    You can add groups and datasets to it as follows:
+
+        >>> my_group = f.require_group("my_group")
+        >>> a = np.arange(100)
+        >>> dset = f.require_dataset("my_data", data=a)
+
+    The data is immediately written to disk.
+
+    Parameters
+    ----------
+    directory:
+        Name of the directory to be opened or created as an Exdir File.
+    mode: str, optional
+        A file mode string that defines the read/write behavior.
+        See open() for information about the different modes.
+    allow_remove: bool
+        Set to True if you want mode 'w' to remove existing trees if they
+        exist. This False by default to avoid removing entire directory
+        trees by mistake.
+    name_validation: str, function, optional
+        Set the validation mode for names.
+        Can be a function that takes a name and returns True if the name
+        is valid or one of the following built-in validation modes:
+
+        - 'strict': only allow numbers, lowercase letters, underscore (_) and dash (-)
+        - 'simple': allow numbers, lowercase letters, uppercase letters, underscore (_) and dash (-), check if any file exists with same name in any case.
+        - 'thorough': verify if name is safe on all platforms, check if any file exists with same name in any case.
+        - 'none': allows any filename
+
+        The default is 'thorough'.
+    plugins: list, optional
+        A list of instantiated plugins or modules with a plugins()
+        function that returns a list of plugins.
+
+    """
+
+    def __init__(self, directory, mode=None, allow_remove=False,
+                 name_validation=None, plugins=None, validate_name=None):
+        if validate_name is not None:
+            warnings.warn("validate_name is deprecated. Use name_validation instead.")
+            name_validation = name_validation or validate_name
+
+        directory = pathlib.Path(directory) #.resolve()
+        if directory.suffix != ".exdir":
+            directory = directory.with_suffix(directory.suffix + ".exdir")
+        mode = mode or 'a'
+        recognized_modes = ['a', 'r', 'r+', 'w', 'w-', 'x', 'a']
+        if mode not in recognized_modes:
+            raise ValueError(
+                "IO mode {} not recognized, "
+                "mode must be one of {}".format(mode, recognized_modes)
+            )
+
+        plugin_manager = exdir.plugin_interface.plugin_interface.Manager(plugins)
+
+        if mode == "r":
+            self.io_mode = self.OpenMode.READ_ONLY
+        else:
+            self.io_mode = self.OpenMode.READ_WRITE
+
+        super(File, self).__init__(
+            root_directory=directory,
+            parent_path=pathlib.PurePosixPath(""),
+            object_name="",
+            io_mode=self.io_mode,
+            name_validation=name_validation,
+            plugin_manager=plugin_manager
+        )
+
+        already_exists = directory.exists()
+        if already_exists:
+            if not exob.is_nonraw_object_directory(directory):
+                raise RuntimeError(
+                    "Path '{}' already exists, but is not a valid exdir file.".format(directory)
+                )
+
+        should_create_directory = False
+
+        if mode == "r":
+            if not already_exists:
+                raise RuntimeError("File " + str(directory) + " does not exist.")
+        elif mode == "r+":
+            if not already_exists:
+                raise RuntimeError("File " + str(directory) + " does not exist.")
+        elif mode == "w":
+            if already_exists:
+                if allow_remove:
+                    shutil.rmtree(str(directory))  # NOTE str needed for Python 3.5
+                else:
+                    raise RuntimeError(
+                        "File {} already exists. We won't delete the entire tree "
+                        "by default. Add allow_remove=True to override.".format(directory)
+                    )
+            should_create_directory = True
+        elif mode == "w-" or mode == "x":
+            if already_exists:
+                raise RuntimeError("File " + str(directory) + " already exists.")
+            should_create_directory = True
+        elif mode == "a":
+            if not already_exists:
+                should_create_directory = True
+
+        if should_create_directory:
+            self.name_validation(directory.parent, directory.name)
+            exob._create_object_directory(directory, exob._default_metadata(exob.FILE_TYPENAME))
+
+    def close(self):
+        """
+        Closes the File object.
+        Currently, this has no effect because all data is immediately written to disk.
+        """
+        # yeah right, as if we would create a real file format
+        pass
+
+    def create_group(self, name):
+        """
+        Create a group with the given name or absolute path.
+
+        See :class:`.Group` for more details.
+
+        Note
+        ----
+        Creating groups with absolute paths is only allowed on File objects and
+        not on Group objects in general.
+        """
+        path = utils.path.remove_root(name)
+
+        return super(File, self).create_group(path)
+
+    def require_group(self, name):
+        """
+        Open an existing subgroup or create one if it does not exist.
+
+        See :class:`.Group` for more details.
+
+        Note
+        ----
+        Creating groups with absolute paths is only allowed on File objects and
+        not on Group objects in general.
+        """
+        path = utils.path.remove_root(name)
+
+        return super(File, self).require_group(path)
+
+    def __getitem__(self, name):
+        path = utils.path.remove_root(name)
+        if len(path.parts) < 1:
+            return self
+        return super(File, self).__getitem__(path)
+
+    def __contains__(self, name):
+        path = utils.path.remove_root(name)
+        return super(File, self).__contains__(path)
---
+++ packages/exdir/exdir-0.4.0.tar.gz/exdir-0.4.0/exdir/core/exdir_object.py
@@ -0,0 +1,306 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from six import with_metaclass
+
+from enum import Enum
+import os
+import warnings
+import pathlib
+try:
+    import ruamel_yaml as yaml
+except ImportError:
+    import ruamel.yaml as yaml
+
+import exdir
+
+from . import validation
+# from . import exdir_object
+from .. import utils
+from .attribute import Attribute
+from .constants import *
+
+def _resolve_path(path):
+    return pathlib.Path(path).resolve()
+
+
+def _assert_valid_name(name, container):
+    """Check if name (dataset or group) is valid."""
+    container.name_validation(container.directory, name)
+
+
+def _create_object_directory(directory, metadata):
+    """
+    Create object directory and meta file if directory
+    don't already exist.
+    """
+    if directory.exists():
+        raise IOError("The directory '" + str(directory) + "' already exists")
+    valid_types = [DATASET_TYPENAME, FILE_TYPENAME, GROUP_TYPENAME]
+    typename = metadata[EXDIR_METANAME][TYPE_METANAME]
+    if typename not in valid_types:
+        raise ValueError("{typename} is not a valid typename".format(typename=typename))
+    directory.mkdir()
+    meta_filename = directory / META_FILENAME
+    with meta_filename.open("w", encoding="utf-8") as meta_file:
+        if metadata == _default_metadata(typename):
+            # if it is the default, we know how to print it fast
+            metadata_string = (''
+                '{exdir_meta}:\n'
+                '   {type_meta}: "{typename}"\n'
+                '   {version_meta}: {version}\n'
+            '').format(
+                exdir_meta=EXDIR_METANAME,
+                type_meta=TYPE_METANAME,
+                typename=typename,
+                version_meta=VERSION_METANAME,
+                version=1
+            )
+        else:
+            metadata_string = yaml.dump(metadata)
+
+        try:
+            meta_file.write(metadata_string)
+        except TypeError:
+            # NOTE workaround for Python 2.7
+            meta_file.write(metadata_string.decode('utf8'))
+
+
+def _default_metadata(typename):
+    return {
+        EXDIR_METANAME: {
+            TYPE_METANAME: typename,
+            VERSION_METANAME: 1
+        }
+    }
+
+
+def is_exdir_object(directory):
+    """
+    WARNING: Does not test if inside exdir directory,
+    only if the object can be an exdir object (i.e. a directory).
+    """
+    return directory.is_dir()
+
+
+def is_nonraw_object_directory(directory):
+    meta_filename = directory / META_FILENAME
+    if not meta_filename.exists():
+        return False
+    with meta_filename.open("r", encoding="utf-8") as meta_file:
+        meta_data = yaml.safe_load(meta_file)
+
+        if not isinstance(meta_data, dict):
+            return False
+
+        if EXDIR_METANAME not in meta_data:
+            return False
+        if TYPE_METANAME not in meta_data[EXDIR_METANAME]:
+            return False
+        valid_types = [DATASET_TYPENAME, FILE_TYPENAME, GROUP_TYPENAME]
+        if meta_data[EXDIR_METANAME][TYPE_METANAME] not in valid_types:
+            return False
+    return True
+
+
+def is_raw_object_directory(directory):
+    return is_exdir_object(directory) and not is_nonraw_object_directory(directory)
+
+
+def root_directory(path):
+    """
+    Iterates upwards until a exdir.File object is found.
+
+    returns: path to exdir.File or None if not found.
+    """
+    path = _resolve_path(path)
+    found = False
+    while not found:
+        if path.parent == path:  # parent is self
+            return None
+        valid = is_nonraw_object_directory(path)
+        if not valid:
+            path = path.parent
+            continue
+
+        meta_filename = path / META_FILENAME
+        with meta_filename.open("r", encoding="utf-8") as meta_file:
+            meta_data = yaml.safe_load(meta_file)
+        if EXDIR_METANAME not in meta_data:
+            path = path.parent
+            continue
+        exdir_meta = meta_data[EXDIR_METANAME]
+        if TYPE_METANAME not in exdir_meta:
+            path = path.parent
+            continue
+        if FILE_TYPENAME != exdir_meta[TYPE_METANAME]:
+            path = path.parent
+            continue
+        found = True
+    return path
+
+
+def is_inside_exdir(path):
+    path = _resolve_path(path)
+    return root_directory(path) is not None
+
+
+def assert_inside_exdir(path):
+    path = _resolve_path(path)
+    if not is_inside_exdir(path):
+        raise RuntimeError("Path " + str(path) + " is not inside an Exdir repository.")
+
+
+def open_object(path):
+    from . import exdir_file
+    path = _resolve_path(path)
+    assert_inside_exdir(path)
+    root_dir = root_directory(path)
+    object_name = path.relative_to(root_dir)
+    object_name = object_name.as_posix()
+    exdir_file = exdir_file.File(root_dir)
+    if object_name == ".":
+        return exdir_file
+    return exdir_file[object_name]
+
+# NOTE This is in a separate file only because of circular imports between Object and Raw otherwise
+# TODO move this back to Object once circular imports are figured out
+
+class Object(object):
+    """
+    Parent class for exdir Group and exdir dataset objects
+    """
+    class OpenMode(Enum):
+        READ_WRITE = 1
+        READ_ONLY = 2
+
+    def __init__(self, root_directory, parent_path, object_name, io_mode=None,
+                 name_validation=None, plugin_manager=None):
+        # TODO put io_mode, name_validation, plugin_types into a configuration object
+        self.root_directory = root_directory
+        self.object_name = str(object_name)  # NOTE could be path, so convert to str
+        self.parent_path = parent_path
+        self.relative_path = self.parent_path / self.object_name
+        relative_name = str(self.relative_path)
+        if relative_name == ".":
+            relative_name = ""
+        self.name = "/" + relative_name
+        self.io_mode = io_mode
+        self.plugin_manager = plugin_manager
+
+        name_validation = name_validation or validation.thorough
+
+        if isinstance(name_validation, str):
+            if name_validation == 'simple':
+                name_validation = validation.thorough
+            elif name_validation == 'thorough':
+                name_validation = validation.thorough
+            elif name_validation == 'strict':
+                name_validation = validation.strict
+            elif name_validation == 'none':
+                name_validation = validation.none
+            else:
+                raise ValueError(
+                    'IO name rule "{}" not recognized, '
+                    'name rule must be one of "strict", "simple", '
+                    '"thorough", "none"'.format(name_validation)
+                )
+
+            warnings.warn(
+                "WARNING: name_validation should be set to one of the functions in "
+                "the exdir.validation module. "
+                "Defining naming rule by string is no longer supported."
+            )
+
+        self.name_validation = name_validation
+
+    @property
+    def directory(self):
+        return self.root_directory / self.relative_path
+
+    @property
+    def attrs(self):
+        return Attribute(
+            self,
+            mode=Attribute._Mode.ATTRIBUTES,
+            io_mode=self.io_mode,
+            plugin_manager=self.plugin_manager
+        )
+
+    @attrs.setter
+    def attrs(self, value):
+        self.attrs._set_data(value)
+
+    @property
+    def meta(self):
+        return Attribute(
+            self,
+            mode=Attribute._Mode.METADATA,
+            io_mode=self.io_mode,
+            plugin_manager=self.plugin_manager
+        )
+
+    @property
+    def attributes_filename(self):
+        return self.directory / ATTRIBUTES_FILENAME
+
+    @property
+    def meta_filename(self):
+        return self.directory / META_FILENAME
+
+    def create_raw(self, name):
+        from .raw import Raw
+        _assert_valid_name(name, self)
+        directory_name = self.directory / name
+        if directory_name.exists():
+            raise FileExistsError("'{}' already exists in '{}'".format(name, self))
+        directory_name.mkdir()
+        return Raw(
+            root_directory=self.root_directory,
+            parent_path=self.relative_path,
+            object_name=name,
+            io_mode=self.io_mode
+        )
+
+    def require_raw(self, name):
+        from .raw import Raw
+        directory_name = self.directory / name
+        if directory_name.exists():
+            if is_nonraw_object_directory(directory_name):
+                raise FileExistsError(
+                    "Directory '{}' already exists, but is not raw.".format(directory_name)
+                )
+            return Raw(
+                root_directory=self.root_directory,
+                parent_path=self.relative_path,
+                object_name=name,
+                io_mode=self.io_mode
+            )
+
+        return self.create_raw(name)
+
+    @property
+    def parent(self):
+        from .group import Group
+        if len(self.parent_path.parts) < 1:
+            return None
+        parent_name = self.parent_path.name
+        parent_parent_path = self.parent_path.parent
+        return Group(
+            root_directory=self.root_directory,
+            parent_path=parent_parent_path,
+            object_name=parent_name,
+            io_mode=self.io_mode,
+            name_validation=self.name_validation,
+            plugin_manager=self.plugin_manager
+        )
+
+    def __eq__(self, other):
+        if not isinstance(other, Object):
+            return False
+        return (
+            self.relative_path == other.relative_path and
+            self.root_directory == other.root_directory
+        )
+
+    def _repr_html_(self):
+        return exdir.utils.display.html_tree(self)
