8fefb422ef67e94253b8ca563490dad70a2efd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:19 +0000

    Add package radar-client/0.1.9/radar-client-0.1.9.tar.gz

---
+++ packages/radar-client/radar-client-0.1.9.tar.gz/radar-client-0.1.9/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['radar_client']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['radar-server>=0.1.5,<0.2.0', 'requests>=2.21,<3.0']
+
+setup_kwargs = {
+    'name': 'radar-client',
+    'version': '0.1.9',
+    'description': '',
+    'long_description': None,
+    'author': 'Jared Lunde',
+    'author_email': 'jared.lunde@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
