f018f7d7c0d7f220cd9948dc4233e0f755f2b1d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:27 +0000

    Add package segregation/1.0.3/segregation-1.0.3-py3-none-any.whl

---
+++ packages/segregation/segregation-1.0.3-py3-none-any.whl/segregation/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "1.0.3"
+
+"""
+:mod:`segregation` --- Spatial/Non-Spatial Segregation Analysis
+=================================================
+
+"""
+
+#from segregation.non_spatial_indexes import *
+#from segregation.spatial_indexes import *
+
+# Version 1.0.1 was the refactorization into non_spatial_indexes.py and spatial_indexes.py
