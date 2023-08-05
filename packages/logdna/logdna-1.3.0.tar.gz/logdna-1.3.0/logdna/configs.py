2354fb36e78b079fad89506cf276220901141bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:36 +0000

    Add package logdna/1.3.0/logdna-1.3.0.tar.gz

---
+++ packages/logdna/logdna-1.3.0.tar.gz/logdna-1.3.0/logdna/configs.py
@@ -0,0 +1,12 @@
+defaults = {
+    'CONTENT_TYPE': 'application/json; charset=UTF-8',
+    'DEFAULT_REQUEST_TIMEOUT': 30000,
+    'MS_IN_A_DAY': 86400000,
+    'MAX_REQUEST_TIMEOUT': 30,
+    'MAX_LINE_LENGTH': 32000,
+    'MAX_INPUT_LENGTH': 32,
+    'FLUSH_INTERVAL': 5,
+    'FLUSH_NOW': 1,
+    'FLUSH_BYTE_LIMIT': 1000000,
+    'LOGDNA_URL': 'https://logs.logdna.com/logs/ingest'
+}
---
+++ packages/logdna/logdna-1.3.0.tar.gz/logdna-1.3.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+setuptools.setup(
+  name = 'logdna',
+  packages = ['logdna'],
+  version = '1.3.0',
+  description = 'A python package for sending logs to LogDNA',
+  author = 'Answerbook Inc.',
+  author_email = 'help@logdna.com',
+  url = 'https://github.com/logdna/python',
+  download_url = 'https://github.com/logdna/python/tarball/1.2.7',
+  keywords = ['logdna', 'logging', 'logs', 'python', 'logdna.com', 'logger'],
+  install_requires=[
+    'requests',
+  ],
+  classifiers = [],
+)
