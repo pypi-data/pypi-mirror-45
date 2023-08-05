da24666427cd5031e043297653c011e846438a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:51 +0000

    Add package picomc/0.1.6/picomc-0.1.6.tar.gz

---
+++ packages/picomc/picomc-0.1.6.tar.gz/picomc-0.1.6/picomc/downloader.py
@@ -0,0 +1,73 @@
+import os
+import shutil
+import subprocess
+import urllib
+
+from picomc.logging import logger
+
+
+def check_aria2():
+    try:
+        subprocess.run(
+            "aria2c --version".split(),
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        return True
+    except FileNotFoundError:
+        return False
+
+
+def downloader_aria2(q, d):
+    p = subprocess.Popen(
+        "/usr/bin/aria2c -i - -j8 --dir".split() + [d],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    L = []
+    for url, outs in q:
+        L.append("{}\n out={}".format(url, outs[0]))
+    out, err = p.communicate("\n".join(L).encode())
+    if p.returncode:
+        print(out.decode(), err.decode())
+        raise RuntimeError("Failed to download files.")
+    else:
+        for url, outs in q:
+            src = os.path.join(d, outs[0])
+            for o in outs[1:]:
+                dst = os.path.join(d, o)
+                os.makedirs(os.path.dirname(dst), exist_ok=True)
+                shutil.copy(src, dst)
+
+
+def downloader_urllib(q, d):
+    # Dumb and slow.
+    for url, outs in q:
+        aouts = list(os.path.join(d, o) for o in outs)
+        for o in aouts:
+            os.makedirs(os.path.dirname(o), exist_ok=True)
+        pout, *eouts = aouts
+        urllib.request.urlretrieve(url, pout)
+        for o in eouts:
+            shutil.copy(pout, o)
+
+
+class DownloadQueue:
+
+    def __init__(self):
+        self.q = []
+
+    def add(self, url, *filename):
+        self.q.append((url, filename))
+
+    def download(self, d):
+        if not self.q:
+            return
+        if check_aria2():
+            logger.info("Using aria2 downloader.")
+            downloader = downloader_aria2
+        else:
+            logger.info("Using urllib downloader.")
+            downloader = downloader_urllib
+        downloader(self.q, d)
---
+++ packages/picomc/picomc-0.1.6.tar.gz/picomc-0.1.6/picomc/main.py
@@ -0,0 +1,58 @@
+from contextlib import ExitStack
+
+import click
+import picomc.logging
+from picomc.account import AccountManager, register_account_cli
+from picomc.config import register_config_cli
+from picomc.globals import APP_ROOT, _ctx_ptr, ctx
+from picomc.instances import register_instance_cli
+from picomc.logging import logger
+from picomc.utils import ConfigLoader, check_directories, write_profiles_dummy
+from picomc.version import VersionManager, register_version_cli
+
+__version__ = "0.1.6"
+
+
+@click.group()
+@click.option("--debug/--no-debug", default=False)
+@click.option("-r", "--root", help="Application data directory.", default=APP_ROOT)
+@click.version_option(version=__version__, prog_name="picomc")
+@click.pass_obj
+def picomc_cli(es, debug, root):
+    """picomc is a minimal CLI Minecraft launcher."""
+    picomc.logging.initialize(debug)
+    picomc.globals.APP_ROOT = root
+    check_directories()
+
+    write_profiles_dummy()
+
+    logger.debug("Using application directory: {}".format(picomc.globals.APP_ROOT))
+
+    am = es.enter_context(AccountManager())
+    ctx.am = am
+    default_config = {
+        "java.path": "java",
+        "java.memory.min": "128M",
+        "java.memory.max": "1G",
+        "java.jvmargs": "",
+    }
+    gconf = es.enter_context(ConfigLoader("config.json", defaults=default_config))
+    ctx.gconf = gconf
+    ctx.vm = VersionManager()
+
+
+def main():
+
+    class Context:
+        pass
+
+    c = Context()
+    _ctx_ptr.set(c)
+    with ExitStack() as estack:
+        picomc_cli(obj=estack)
+
+
+register_account_cli(picomc_cli)
+register_version_cli(picomc_cli)
+register_instance_cli(picomc_cli)
+register_config_cli(picomc_cli)
