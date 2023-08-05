9b54cb16f8af697eb4d41f5feaaf02ad86a3451d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:29 +0000

    Add package nb2hugo/0.1.4/nb2hugo-0.1.4.tar.gz

---
+++ packages/nb2hugo/nb2hugo-0.1.4.tar.gz/nb2hugo-0.1.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='nb2hugo',
+      python_requires='>=3.6',
+      version='0.1.4',
+      description='A Jupyter notebook to Hugo markdown converter',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      keywords='jupyter notebook hugo converter',
+      url='http://github.com/vlunot/nb2hugo',
+      author='Vincent Lunot',
+      author_email='vlunot@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'nbconvert',
+          'nbformat',
+          'traitlets',
+          'argparse',
+          'pytest',
+      ],
+      scripts=['bin/nb2hugo'],
+      include_package_data=True,
+      zip_safe=False)
