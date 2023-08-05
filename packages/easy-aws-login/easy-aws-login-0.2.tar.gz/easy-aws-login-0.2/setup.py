fc18068ea825177d358022b29570308f4c3c1583
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:21 +0000

    Add package easy-aws-login/0.2/easy-aws-login-0.2.tar.gz

---
+++ packages/easy-aws-login/easy-aws-login-0.2.tar.gz/easy-aws-login-0.2/setup.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = 'easy-aws-login'
+DESCRIPTION = 'The easiest way to log in to your AWS console.'
+URL = 'https://github.com/jeshan/easy-aws-login'
+EMAIL = 'j@jeshan.co'
+AUTHOR = 'Jeshan Babooa'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = os.environ.get('CODE_VERSION')
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    'boto3', 'requests'
+]
+
+# What packages are optional?
+EXTRAS = {
+    'boto3 code assistance': ['botostubs'],
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    entry_points={"console_scripts": ["easy-aws-login = easy_aws_login.index:main"]},
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='BSD-2-Clause',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
