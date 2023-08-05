b0ce8e3830285d604f2a50d6f5eb810764048dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:49 +0000

    Add package theagao/2019.4.19/theagao-2019.4.19-py2.7.egg

---
+++ packages/theagao/theagao-2019.4.19-py2.7.egg/theagao/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+# Author: Yongwei
+# @Time: 19-4-19 下午4:18
+
+from __future__ import absolute_import
+
+from .to_extract_feature import *
+from .to_plot import *
+
+__version__ = '1.0.0'
+__licence__ = ''
