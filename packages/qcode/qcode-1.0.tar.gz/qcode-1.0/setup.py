47b2e7e2e7612fffbd6792492c63d98dafee8609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:09 +0000

    Add package qcode/1.0/qcode-1.0.tar.gz

---
+++ packages/qcode/qcode-1.0.tar.gz/qcode-1.0/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+
+setup(
+    name='qcode',
+    version='1.0',
+    description=(
+        '<乱字符提取地区编码>'
+    ),
+    author='<loo>',
+    author_email='<1213788433@qq.com>',
+    maintainer='<loo>',
+    maintainer_email='<1213788433@qq.com',
+    platforms=["all"],
+    url='https://github.com/libinhai-loo/find_code.git'
+)
