6e406acacee81bc1a2adfe6de8d0436758e954dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:15 +0000

    Add package iPat/0.0/iPat-0.0.tar.gz

---
+++ packages/iPat/iPat-0.0.tar.gz/iPat-0.0/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+setuptools.setup(name='iPat',
+      version='0.0',
+      description='Python version of iPat',
+      url='https://github.com/Zhiwu-Zhang-Lab/James_Git',
+      author='James Chen',
+      author_email='chun-peng.chen@wsu.edu',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      install_requires=['numpy>=1.11',
+                        'matplotlib>=1.5'])
