e5f7363938f0061cfac12cdeaaa7cfc975a6d4a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:42 +0000

    Add package ecasb2share/0.0.1b15/ecasb2share-0.0.1b15.tar.gz

---
+++ packages/ecasb2share/ecasb2share-0.0.1b15.tar.gz/ecasb2share-0.0.1b15/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Setup file for ecas_b2share.
+    Use setup.cfg to configure your project.
+
+
+"""
+import sys
+
+from pkg_resources import require, VersionConflict
+from setuptools import setup, find_packages
+
+try:
+    require('setuptools>=38.3')
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
+
+long_description = (
+    open('README.rst').read() + '\n' + open('AUTHORS.rst').read() + '\n' + open('CHANGELOG.rst').read()
+)
+version = __import__('ecasb2share').__version__
+description = 'ecasb2share provides a python library to interact with B2SHARE REST API.'
+reqs = [line.strip() for line in open('requirements.txt')]
+
+setup(name='ecasb2share',
+      version=version,
+      description=description,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Bug Tracking',
+          ],
+      keywords='ecas rest python eosc-hub b2share',
+      author='Sofiane Bendoukha',
+      url='https://github.com/SofianeB/ECAS-B2SHARE',
+      license="mit",
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=reqs,
+
+      )
+
