21ea76c45ce4cb290afd90afc374d93d4f50027a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:58 +0000

    Add package press/0.5.8/press-0.5.8.tar.gz

---
+++ packages/press/press-0.5.8.tar.gz/press-0.5.8/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '0.5.8'
+
+setup(
+    name='press',
+    version=version,
+    description="An OS image installer that supports custome partitioning, lvm, and software raid",
+    long_description='',
+    classifiers=[],
+    # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Jared Rodriguez',
+    author_email='jared.rodriguez@rackspace.com',
+    url='http://github.com/jr0d/press',
+    license='GPLv2',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'pyudev',
+        'requests',
+        'pyyaml',
+        'jinja2',
+        'ipaddress',
+        'six',
+        'python-size'
+    ],
+    entry_points={
+        'console_scripts': [
+            'press = press.entry:main'
+        ]
+    }
+)
