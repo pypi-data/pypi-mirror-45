cdcafe8a5d596fc3a88bebdfb766fbca38664d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:30 +0000

    Add package pyclustering/0.9.0/pyclustering-0.9.0.tar.gz

---
+++ packages/pyclustering/pyclustering-0.9.0.tar.gz/pyclustering-0.9.0/ccore/src/cluster/elbow.hpp
@@ -0,0 +1,124 @@
+/**
+*
+* @authors Andrei Novikov (pyclustering@yandex.ru)
+* @date 2014-2019
+* @copyright GNU Public License
+*
+* GNU_PUBLIC_LICENSE
+*   pyclustering is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   pyclustering is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#pragma once
+
+
+#include "cluster/elbow_data.hpp"
+#include "cluster/kmeans.hpp"
+#include "cluster/kmeans_plus_plus.hpp"
+
+#include "utils/metric.hpp"
+
+#include "definitions.hpp"
+
+
+using namespace ccore::utils::metric;
+
+
+namespace ccore {
+
+namespace clst {
+
+
+template <class TypeInitializer = kmeans_plus_plus>
+class elbow {
+private:
+    std::size_t   m_kmin      = 0;
+    std::size_t   m_kmax      = 0;
+
+    std::vector<double> m_elbow = { };
+
+    elbow_data    * m_result  = nullptr;      /* temporary pointer to output result   */
+
+public:
+    elbow(void) = default;
+
+    elbow(const std::size_t p_kmin, const std::size_t p_kmax) :
+        m_kmin(p_kmin), m_kmax(p_kmax)
+    { verify(); }
+
+    elbow(const elbow & p_other) = default;
+
+    elbow(elbow && p_other) = default;
+
+    ~elbow(void) = default;
+
+public:
+    void process(const dataset & p_data, elbow_data & p_result) {
+        if (p_data.size() < m_kmax) {
+            throw std::invalid_argument("K max value '" + std::to_string(m_kmax) 
+              + "' is greater than amount of data points '" + std::to_string(p_data.size()) + "'.");
+        }
+
+        m_result = &p_result;
+        m_elbow.clear();
+
+        for (std::size_t i = m_kmin; i < m_kmax; i++) {
+            dataset initial_centers;
+            TypeInitializer(i).initialize(p_data, initial_centers);
+
+            kmeans_data result;
+            kmeans instance(initial_centers, 0.0001);
+            instance.process(p_data, result);
+
+            m_result->get_wce().push_back(result.wce());
+        }
+
+        calculate_elbows();
+        m_result->set_amount(find_optimal_kvalue());
+    }
+
+private:
+    void verify(void) {
+        if (m_kmax < 3 + m_kmin) {
+            throw std::invalid_argument("Amount of K '" + std::to_string(m_kmax - m_kmin) + "' is too small for analysis.");
+        }
+    }
+
+    void calculate_elbows(void) {
+        const double x0 = 0.0;
+        const double y0 = m_result->get_wce().front();
+
+        const double x1 = (double) m_result->get_wce().size();
+        const double y1 = m_result->get_wce().back();
+
+        for (std::size_t index_elbow = 1; index_elbow < m_result->get_wce().size() - 1; index_elbow++) {
+            const double x = (double) index_elbow;
+            const double y = m_result->get_wce().at(index_elbow);
+
+            const double segment = std::abs((y0 - y1) * x + (x1 - x0) * y + (x0 * y1 - x1 * y0));
+            const double norm = euclidean_distance(point({ x0, y0 }), point({ x1, y1 }));
+            m_elbow.push_back(segment / norm);
+        }
+    }
+
+    std::size_t find_optimal_kvalue(void) {
+        auto optimal_elbow_iter = std::max_element(m_elbow.cbegin(), m_elbow.cend());
+        return std::distance(m_elbow.cbegin(), optimal_elbow_iter) + 1 + m_kmin;
+    }
+};
+
+
+}
+
+}
---
+++ packages/pyclustering/pyclustering-0.9.0.tar.gz/pyclustering-0.9.0/pyclustering/cluster/tests/unit/ut_kmedoids.py
@@ -0,0 +1,233 @@
+"""!
+
+@brief Unit-tests for K-Medoids algorithm.
+
+@authors Andrei Novikov (pyclustering@yandex.ru)
+@date 2014-2019
+@copyright GNU Public License
+
+@cond GNU_PUBLIC_LICENSE
+    PyClustering is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    PyClustering is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+@endcond
+
+"""
+
+
+import unittest
+
+# Generate images without having a window appear.
+import matplotlib
+matplotlib.use('Agg')
+
+from pyclustering.cluster.tests.kmedoids_templates import KmedoidsTestTemplates
+
+from pyclustering.samples.definitions import SIMPLE_SAMPLES
+
+from pyclustering.utils import read_sample
+from pyclustering.utils.metric import type_metric, distance_metric
+
+
+class KmedoidsUnitTest(unittest.TestCase):
+    def testClusterAllocationSampleSimple1(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], False)
+
+    def testClusterAllocationSampleSimple1DistanceMatrix(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], False, data_type='distance_matrix')
+
+    def testClusterAllocationSampleSimple1DistanceMatrixNumpy(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], False, data_type='distance_matrix', input_type='numpy')
+
+    def testClusterAllocationSampleSimple1Euclidean(self):
+        metric = distance_metric(type_metric.EUCLIDEAN)
+        KmedoidsTestTemplates.templateLengthProcessWithMetric(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], metric, False)
+
+    def testClusterAllocationSampleSimple1EuclideanDistanceMatrix(self):
+        metric = distance_metric(type_metric.EUCLIDEAN)
+        KmedoidsTestTemplates.templateLengthProcessWithMetric(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], metric, False, data_type='distance_matrix')
+
+    def testClusterAllocationSampleSimple1SquareEuclidean(self):
+        metric = distance_metric(type_metric.EUCLIDEAN_SQUARE)
+        KmedoidsTestTemplates.templateLengthProcessWithMetric(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], metric, False)
+
+    def testClusterAllocationSampleSimple1SquareEuclideanDistanceMatrix(self):
+        metric = distance_metric(type_metric.EUCLIDEAN_SQUARE)
+        KmedoidsTestTemplates.templateLengthProcessWithMetric(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], metric, False, data_type='distance_matrix')
+
+    def testClusterAllocationSampleSimple1Manhattan(self):
+        metric = distance_metric(type_metric.MANHATTAN)
+        KmedoidsTestTemplates.templateLengthProcessWithMetric(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], metric, False)
+
+    def testClusterAllocationSampleSimple1ManhattanDistanceMatrix(self):
+        metric = distance_metric(type_metric.MANHATTAN)
+        KmedoidsTestTemplates.templateLengthProcessWithMetric(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], metric, False, data_type='distance_matrix')
+
+    def testClusterAllocationSampleSimple1Chebyshev(self):
+        metric = distance_metric(type_metric.CHEBYSHEV)
+        KmedoidsTestTemplates.templateLengthProcessWithMetric(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], metric, False)
+
+    def testClusterAllocationSampleSimple1ChebyshevDistanceMatrix(self):
+        metric = distance_metric(type_metric.CHEBYSHEV)
+        KmedoidsTestTemplates.templateLengthProcessWithMetric(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], metric, False, data_type='distance_matrix')
+
+    def testClusterAllocationSampleSimple1Minkowski(self):
+        metric = distance_metric(type_metric.MINKOWSKI, degree=2.0)
+        KmedoidsTestTemplates.templateLengthProcessWithMetric(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], metric, False)
+
+    def testClusterAllocationSampleSimple1MinkowskiDistanceMatrix(self):
+        metric = distance_metric(type_metric.MINKOWSKI, degree=2.0)
+        KmedoidsTestTemplates.templateLengthProcessWithMetric(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], metric, False, data_type='distance_matrix')
+
+    def testClusterAllocationSampleSimple1UserDefined(self):
+        metric = distance_metric(type_metric.USER_DEFINED, func=distance_metric(type_metric.EUCLIDEAN))
+        KmedoidsTestTemplates.templateLengthProcessWithMetric(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], metric, False)
+
+    def testClusterAllocationSampleSimple1UserDefinedDistanceMatrix(self):
+        metric = distance_metric(type_metric.USER_DEFINED, func=distance_metric(type_metric.EUCLIDEAN))
+        KmedoidsTestTemplates.templateLengthProcessWithMetric(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], metric, False, data_type='distance_matrix')
+
+    def testClusterOneAllocationSampleSimple1(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [5], [10], False)
+
+    def testClusterOneAllocationSampleSimple1DistanceMatrix(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [5], [10], False, data_type='distance_matrix')
+
+    def testClusterAllocationSampleSimple1WithMedoidsInitializer(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, None, [5, 5], False, initialize_medoids=2)
+
+    def testClusterAllocationSampleSimple2(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE2, [3, 12, 20], [10, 5, 8], False)
+
+    def testClusterAllocationSampleSimple2DistanceMatrix(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE2, [3, 12, 20], [10, 5, 8], False, data_type='distance_matrix')
+
+    def testClusterOneAllocationSampleSimple2(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE2, [10], [23], False)
+
+    def testClusterOneAllocationSampleSimple2DistanceMatrix(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE2, [10], [23], False, data_type='distance_matrix')
+
+    def testClusterAllocationSampleSimple2WithMedoidsInitializer(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE2, None, [10, 5, 8], False, initialize_medoids=3)
+
+    def testClusterAllocationSampleSimple3(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE3, [4, 12, 25, 37], [10, 10, 10, 30], False)
+
+    def testClusterAllocationSampleSimple3DistanceMatrix(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE3, [4, 12, 25, 37], [10, 10, 10, 30], False, data_type='distance_matrix')
+
+    def testClusterOneAllocationSampleSimple3(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE3, [30], [60], False)
+
+    def testClusterAllocationSampleSimple3WithMedoidsInitializer(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE3, None, [10, 10, 10, 30], False, initialize_medoids=4)
+
+    def testClusterAllocationSampleSimple5(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE5, [4, 18, 34, 55], [15, 15, 15, 15], False)
+
+    def testClusterAllocationSampleSimple5DistanceMatrix(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE5, [4, 18, 34, 55], [15, 15, 15, 15], False, data_type='distance_matrix')
+
+    def testClusterOneAllocationSampleSimple5(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE5, [35], [60], False)
+
+    def testClusterTheSameData1(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE9, [2, 20], [10, 20], False)
+
+    def testClusterTheSameData1DistanceMatrix(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE9, [2, 20], [10, 20], False, data_type='distance_matrix')
+
+    def testClusterTheSameData2(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE12, [2, 7, 12], [5, 5, 5], False)
+
+    def testClusterTheSameData2DistanceMatrix(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE12, [2, 7, 12], [5, 5, 5], False, data_type='distance_matrix')
+
+
+    def testClusterAllocationOneDimensionData(self):
+        KmedoidsTestTemplates.templateClusterAllocationOneDimensionData(False)
+
+
+    def testClusterAllocationTheSameObjectsOneInitialMedoid(self):
+        KmedoidsTestTemplates.templateClusterAllocationTheSameObjects(20, 1, False)
+
+    def testClusterAllocationTheSameObjectsTwoInitialMedoids(self):
+        KmedoidsTestTemplates.templateClusterAllocationTheSameObjects(15, 2, False)
+
+    def testClusterAllocationTheSameObjectsThreeInitialMedoids(self):
+        KmedoidsTestTemplates.templateClusterAllocationTheSameObjects(25, 3, False)
+
+
+    def testAllocatedRequestedClustersSampleSimple01(self):
+        sample = read_sample(SIMPLE_SAMPLES.SAMPLE_SIMPLE1)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 1, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 2, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 3, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 4, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 5, None, False)
+
+    def testAllocatedRequestedClustersSampleSimple02(self):
+        sample = read_sample(SIMPLE_SAMPLES.SAMPLE_SIMPLE2)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 1, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 2, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 3, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 4, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 5, None, False)
+
+    def testAllocatedRequestedClustersSampleSimple03(self):
+        sample = read_sample(SIMPLE_SAMPLES.SAMPLE_SIMPLE3)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 2, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 5, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 8, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 10, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 15, None, False)
+
+    def testAllocatedRequestedClustersSampleSimple04(self):
+        sample = read_sample(SIMPLE_SAMPLES.SAMPLE_SIMPLE4)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 10, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 25, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 40, None, False)
+
+    def testAllocatedRequestedClustersWithTheSamePoints(self):
+        # Bug issue #366 - Kmedoids returns incorrect number of clusters.
+        sample = [ [0.0, 0.0], [0.1, 0.1], [0.0, 0.0], [0.1, 0.2] ]
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 4, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 3, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 2, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 1, None, False)
+
+
+    def testAllocatedRequestedClustersWithTotallyTheSamePoints(self):
+        # Bug issue #366 - Kmedoids returns incorrect number of clusters.
+        sample = [ [0.0, 0.0], [0.0, 0.0], [0.0, 0.0], [0.0, 0.0] ]
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 4, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 3, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 2, None, False)
+        KmedoidsTestTemplates.templateAllocateRequestedClusterAmount(sample, 1, None, False)
+
+
+    def testItermax0(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [], False, itermax=0)
+
+    def testItermax1(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], False, itermax=1)
+
+    def testItermax10Simple01(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE1, [2, 9], [5, 5], False, itermax=10)
+
+    def testItermax10Simple02(self):
+        KmedoidsTestTemplates.templateLengthProcessData(SIMPLE_SAMPLES.SAMPLE_SIMPLE2, [3, 12, 20], [10, 5, 8], False, itermax=10)
+
+
+if __name__ == "__main__":
+    unittest.main()
