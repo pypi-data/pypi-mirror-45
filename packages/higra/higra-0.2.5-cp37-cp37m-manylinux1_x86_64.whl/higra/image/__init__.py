b30218194c011f501921eba0ea4e98a9cb2279f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:15:02 +0000

    Add package higra/0.2.5/higra-0.2.5-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/higra/higra-0.2.5-cp37-cp37m-manylinux1_x86_64.whl/higra/image/__init__.py
@@ -0,0 +1,13 @@
+############################################################################
+# Copyright ESIEE Paris (2018)                                             #
+#                                                                          #
+# Contributor(s) : Benjamin Perret                                         #
+#                                                                          #
+# Distributed under the terms of the CECILL-B License.                     #
+#                                                                          #
+# The full license is in the file LICENSE, distributed with this software. #
+############################################################################
+
+from .graph_image import *
+from .image_utils import *
+from .hierarchy_mean_pb import *
