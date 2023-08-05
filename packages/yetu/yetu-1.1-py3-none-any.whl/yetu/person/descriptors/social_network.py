af8c6d33c7078a6ea55b0483c1150ec3b5af9435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:14 +0000

    Add package yetu/1.1/yetu-1.1-py3-none-any.whl

---
+++ packages/yetu/yetu-1.1-py3-none-any.whl/yetu/person/descriptors/social_network.py
@@ -0,0 +1,25 @@
+__author__ = 'David Johnnes'
+__email__ = "david.johnnes@gmail.com"
+
+
+__var = """
+
+
+        "gmail_account", "gmail_accounts",
+        "facebook_account", "facebook_accounts",
+        "twitter_account", "twitter_accounts",
+        "linkedin_account", "linkedin_accounts",
+        "jabber_account", "jabber_accounts",
+        "skype_account", "skype_accounts",
+        "hotmail_account", "hotmail_accounts",
+        "flickr_account", "flickr_accounts",
+        "myspace_account", "myspace_accounts",
+        "sina_weibo_account", "sina_weibo_accounts",
+        "AIM_account", "AIM_accounts",
+        "yahoo_account", "yahoo_account",
+        "ICQ_account", "ICQ_accounts",
+        "QQ_account", "QQ_accounts",
+        "Gadu-Gadu_account", "Gadu-Gadu_accounts",
+        "private_blog_url", "private_blog_urls",
+        "private_homepage_url", "private_homepage_urls",
+"""
