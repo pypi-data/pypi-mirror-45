1adede569c614212f5711ea7d2d5c785341bf652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:45 +0000

    Add package jaraco.mongodb/10.1.3/jaraco.mongodb-10.1.3.tar.gz

---
+++ packages/jaraco.mongodb/jaraco.mongodb-10.1.3.tar.gz/jaraco.mongodb-10.1.3/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+collect_ignore = [
+	'jaraco/mongodb/pmxbot.py',
+	# disable move-gridfs check, as it causes output capturing
+	# to be disabled. See pytest-dev/pytest#3752.
+	'jaraco/mongodb/move-gridfs.py',
+]
+
+
+@pytest.fixture(scope='function')
+def database(request, mongodb_instance):
+	"""
+	Return a clean MongoDB database suitable for testing.
+	"""
+	db_name = request.node.name.replace('.', '_')
+	database = mongodb_instance.get_connection()[db_name]
+	yield database
+	database.client.drop_database(db_name)
