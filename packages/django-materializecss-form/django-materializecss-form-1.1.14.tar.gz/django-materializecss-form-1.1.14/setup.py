f97f1c6ad8a12ffd579c48f1a2d6b674fd7c1371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:18 +0000

    Add package django-materializecss-form/1.1.14/django-materializecss-form-1.1.14.tar.gz

---
+++ packages/django-materializecss-form/django-materializecss-form-1.1.14.tar.gz/django-materializecss-form-1.1.14/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+import materializecssform
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+
+    name='django-materializecss-form',
+
+    version=materializecssform.__version__,
+
+    packages=find_packages(),
+
+    author="Kal Walkden",
+
+    author_email="kal@walkden.us",
+
+    description="A simple Django form template tag to work with Materializecss",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    include_package_data=True,
+
+    url='https://github.com/kalwalkden/django-materializecss-form',
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+    ],
+
+    license="MIT",
+
+    zip_safe=False
+)
