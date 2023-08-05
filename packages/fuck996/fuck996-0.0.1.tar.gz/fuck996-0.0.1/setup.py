0bc0d6940bade58fe131647eeadc322efaac50b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:30 +0000

    Add package fuck996/0.0.1/fuck996-0.0.1.tar.gz

---
+++ packages/fuck996/fuck996-0.0.1.tar.gz/fuck996-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'fuck996',
+    version = '0.0.1',
+    keywords='fuck 996',
+    description = 'a library for 996',
+    license = 'Anti996 License',
+    url = '',
+    author = '068089dy@gmail.com',
+    author_email = '',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
