f6c68ed62ffa7e2792534244d66772f5e20d3e4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:42 +0000

    Add package frctbaapi/0.0.2/frctbaapi-0.0.2.tar.gz

---
+++ packages/frctbaapi/frctbaapi-0.0.2.tar.gz/frctbaapi-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="frctbaapi",
+    version="0.0.2",
+    author="Kyle Preiksa",
+    author_email="preiksa.kyle@gmail.com",
+    description="A package to interface with The Blue Alliance API. Placeholder.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kpreiksa/frctbaapi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
