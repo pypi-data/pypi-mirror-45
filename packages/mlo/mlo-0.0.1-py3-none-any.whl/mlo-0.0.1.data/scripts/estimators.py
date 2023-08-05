4c5b0a2263a3661677ef5370b94a07bc7c971cc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:33 +0000

    Add package mlo/0.0.1/mlo-0.0.1-py3-none-any.whl

---
+++ packages/mlo/mlo-0.0.1-py3-none-any.whl/mlo-0.0.1.data/scripts/estimators.py
@@ -0,0 +1,211 @@
+#!python
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 17 16:42:03 2018
+
+@author: Amine Laghaout
+"""
+
+from sklearn.base import BaseEstimator, ClassifierMixin
+
+class Estimator:
+    
+    def __init__(
+            self,
+            default_args=None,
+            **kwargs):
+
+        from utilities import args_to_attributes
+
+        args_to_attributes(self, default_args, **kwargs)
+
+        self.verify()    
+
+    def verify(self):
+        
+        pass
+
+    def build(self):
+        
+        pass
+    
+class MLP(Estimator):
+
+    def __init__(
+            self,
+            default_args=dict(
+                name='mutli-layer perceptron',
+                ),
+            **kwargs):
+
+        from utilities import parse_args
+
+        kwargs = parse_args(default_args, kwargs)
+
+        super().__init__(**kwargs)
+
+    def build(self, architecture):
+        
+        from keras.optimizers import SGD
+        from keras.models import Sequential
+        from keras.layers import Dense, Dropout
+
+        self.architecture = architecture
+
+        self.model = Sequential()
+
+        # Build the neural network layer by layer
+        for index, layer in self.architecture.iterrows():
+
+            # Input layer
+            if index == 0:
+
+                self.model.add(Dense(int(layer.num_nodes),
+                                     activation=layer.activation,
+                                     input_dim=self.input_dim))
+
+                self.model.add(Dropout(layer.dropout))
+
+            # Output layer
+            elif index == len(self.architecture) - 1:
+
+                self.model.add(Dense(self.output_dim,
+                                     activation=layer.activation))
+
+            # Hidden layers
+            else:
+
+                self.model.add(Dense(int(layer.num_nodes),
+                                     activation=layer.activation))
+
+                self.model.add(Dropout(layer.dropout))
+
+        # TODO: Generalize this to any kind of optimizer
+        try:
+            optimizer = SGD(**self.optimizer)
+        except BaseException:
+            from keras.optimizers import Adam
+            try:
+                optimizer = eval(self.optimizer)
+            except BaseException:
+                optimizer = self.optimizer
+
+        self.model.compile(loss=self.loss_function,
+                           optimizer=optimizer,
+                           metrics=self.metrics)
+
+        return self.model
+    
+    
+class RNN(Estimator):
+
+    def __init__(
+            self,
+            default_args=dict(
+                name='recurrent neural network',
+                ),
+            **kwargs):
+
+        from utilities import parse_args
+
+        kwargs = parse_args(default_args, kwargs)
+
+        super().__init__(**kwargs)
+        
+    def build(self):
+
+        # LSTM with dropout for sequence classification in the IMDB dataset
+        from keras.models import Sequential
+        from keras.layers import Dense, LSTM
+
+        self.model = Sequential()
+
+        # With embedding
+        try:
+
+            from keras.layers.embeddings import Embedding
+
+            # Input (embedding) layer
+            self.model.add(Embedding(self.input_dim,
+                                     self.embed_dim,
+                                     input_length=self.max_seq_len))
+
+            # Recurrent layer
+            self.model.add(
+                LSTM(
+                    self.num_nodes,
+                    dropout=self.dropout,
+                    recurrent_dropout=self.recurrent_dropout))
+
+        # Without embedding
+        # https://machinelearningmastery.com/reshape-input-data-long-short-term-memory-networks-keras/
+        except BaseException:
+
+            # Recurrent layer
+            self.model.add(
+                LSTM(
+                    self.num_nodes,
+                    input_shape=(
+                        self.max_seq_len,
+                        self.input_dim),
+                    dropout=self.dropout,
+                    recurrent_dropout=self.recurrent_dropout))
+
+        # Output layer
+        self.model.add(Dense(self.output_dim,
+                             activation=self.activation))
+
+        self.model.compile(loss=self.loss_function,
+                           optimizer=self.optimizer,
+                           metrics=self.metrics)
+        
+        return self.model
+
+
+class TemplateClassifier(BaseEstimator, ClassifierMixin):
+
+    def __init__(self, arg_1=1000, arg_2=5):
+
+        from inspect import getargvalues, currentframe
+
+        args, _, _, values = getargvalues(currentframe())
+        values.pop('self')
+
+        for arg, val in values.items():
+            setattr(self, arg, val)
+
+    def fit(self, X, y=None):
+
+        from sklearn.utils.multiclass import unique_labels
+        from sklearn.utils.validation import check_X_y
+
+        # Check that X and y have correct shape
+        X, y = check_X_y(X, y)
+        # Store the classes seen during fit
+        self.classes_ = unique_labels(y)
+
+        self.X_ = X
+        self.y_ = y
+        # Return the classifier
+        return self
+
+    def predict(self, X):
+
+        from numpy import argmin
+        from sklearn.metrics import euclidean_distances
+        from sklearn.utils.validation import check_array, check_is_fitted
+
+        # Check is fit had been called
+        check_is_fitted(self, ['X_', 'y_'])
+
+        # Input validation
+        X = check_array(X)
+
+        closest = argmin(euclidean_distances(X, self.X_), axis=1)
+        return self.y_[closest]
+
+#    def score(self):
+#
+#        # TODO
+#
+#        pass
---
+++ packages/mlo/mlo-0.0.1-py3-none-any.whl/mlo-0.0.1.data/scripts/visualizers.py
@@ -0,0 +1,233 @@
+#!python
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  6 16:14:06 2019
+
+@author: Amine Laghaout
+"""
+
+class Visualizer:
+
+    def __init__(
+            self, 
+            default_args=dict(save_as=None), 
+            **kwargs):
+
+        from utilities import args_to_attributes
+
+        args_to_attributes(self, default_args, **kwargs)
+        
+class Plot2D(Visualizer):
+
+    from numpy import logspace
+
+    def __init__(
+            self,
+            default_args=dict(
+                linewidth=5, show=True, marker=None, legend=None, xlabel='', 
+                ylabel='', title='', fontsize=16, smooth=None, save_as=None, 
+                axis_range=None, grid=True, log=(False, False),  
+                markersize=None, xticks=None, drawstyle=None, 
+                fill_between=False,
+                ),
+            **kwargs):
+
+        import matplotlib.pyplot as plt
+        from utilities import parse_args
+
+        kwargs = parse_args(default_args, kwargs)
+
+        super().__init__(**kwargs)  
+        
+        x = self.x
+        y = self.y
+        
+        if not self.show:
+            plt.ioff()
+
+        plt.figure()
+
+        # Font
+        #plt.rc('font', family = fontFamily)
+
+        # Plot several curves or...
+        if isinstance(y, tuple):
+
+            if legend in ['', None]:
+                legend = [''] * len(y)
+
+            for y_element_index, y_element in enumerate(y):
+
+                plt.plot(x, y_element, label=legend[y_element_index],
+                         marker=marker, markersize=markersize,
+                         drawstyle=drawstyle, linewidth=linewidth)
+
+        # ... a single curve?
+        else:
+
+            plt.plot(x, y, label=legend, marker=marker, linewidth=linewidth,
+                     markersize=markersize, drawstyle=drawstyle)
+
+            # Superimpose a smoothed line?
+            if smooth is not None:
+
+                yhat = sp.savgol_filter(y, smooth[0], smooth[1])
+                plt.plot(x, yhat, color='red')
+
+            if fill_between:
+                plt.fill_between(x, 0, y, step='mid')
+
+        # Use logarithmic scale?
+        if log[0]:
+            plt.xscale('log')  # , nonposy = 'clip'
+        if log[1]:
+            plt.yscale('log')  # , nonposy = 'clip'
+
+        # Labels, legend, and title
+        plt.xlabel(r'%s' % xlabel, fontsize=fontsize)
+        plt.ylabel(r'%s' % ylabel, fontsize=fontsize)
+        plt.title(r'%s' % title, fontsize=fontsize)
+        if legend is not None and legend:
+            plt.legend(loc='upper left', bbox_to_anchor=(1, 1),
+                       fontsize=fontsize)
+
+        # Abcissa labels
+        if xticks is not None:
+            if not isinstance(xticks, tuple):
+                xticks = (xticks, 'horizontal')
+            plt.xticks(x, xticks[0], rotation=xticks[1])
+
+        # Axis
+        if axis_range is None and not isinstance(y, tuple):
+
+            axis_range = (min(x), max(x), min(y), max(y))
+
+        elif axis_range is None and isinstance(y, tuple):
+
+            min_y = np.inf
+            max_y = -np.inf
+
+            for y_element in y:
+
+                # print('>>> y_element:', y_element, 'y:', y)  # TODO remove
+
+                if min(y_element) < min_y:
+                    min_y = min(y_element)
+                if max(y_element) > max_y:
+                    max_y = max(y_element)
+
+            axis_range = (min(x), max(x), min_y, max_y)
+
+        plt.axis(axis_range)
+
+        # Grid
+        if grid is not False:
+            if grid is True:
+                plt.grid()
+            else:
+                plt.grid(**grid)
+
+        if save_as is not None:
+            plt.savefig(save_as, bbox_inches='tight')
+
+        if self.show:
+            plt.show()
+
+        plt.clf()
+            
+def plotTimeSeries(
+        x, y_dict, fontsize=16, markersize=3, xlabel='', ylabel='',
+        loc='upper left', bbox_to_anchor=(1, 1), title=None, linewidth=3,
+        xtick_frequency=10, rotation=45, save_as=None, adjust_xticks=True,
+        log=(False, False), legend=True):
+
+    """
+    Plot the data stored in the dictionary ``y_dict`` versus ``x``.
+    """
+
+    import matplotlib.pyplot as plt
+    from numpy import arange
+    
+    plt.figure()
+
+    for y_key in y_dict.keys():
+
+        plt.plot(x, y_dict[y_key], label=y_key, marker='o',
+                 markersize=markersize, linewidth=linewidth)
+        plt.xlabel(r'%s' % xlabel, fontsize=fontsize)
+        plt.ylabel(r'%s' % ylabel, fontsize=fontsize)
+        plt.setp(plt.xticks()[1], rotation=rotation)
+#        ax = plt.gca() 
+
+        # Use logarithmic scale?
+        if log[0]:
+            plt.xscale('log')  # , nonposy = 'clip'
+        if log[1]:
+            plt.yscale('log')  # , nonposy = 'clip'
+
+#        if len(x) > xtick_frequency and adjust_xticks:
+#            ticks_indices = arange(0, len(x), int(len(x)/xtick_frequency))
+#            plt.xticks(ticks_indices)
+#            try:
+#                ax.set_xticklabels(x[ticks_indices])
+#            except:
+#                pass
+
+        if legend:
+            plt.legend(loc=loc, bbox_to_anchor=bbox_to_anchor, fontsize=fontsize)
+
+        if title is not None:
+            plt.title(r'%s' % title, fontsize=fontsize)
+
+    plt.grid()
+
+    if save_as is not None:
+        plt.savefig(save_as, bbox_inches='tight')
+    
+    plt.show()
+
+    
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=None, save_as=None):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+
+    import itertools
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    plt.figure()
+
+    if cmap is None:
+        cmap = plt.cm.Blues
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+    if save_as is not None:
+        plt.savefig(save_as, bbox_inches='tight')
+
+    plt.show()
+    plt.clf()
