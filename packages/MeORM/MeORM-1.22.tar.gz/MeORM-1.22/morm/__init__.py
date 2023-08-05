f2ef5bde7a9bd1e01b2ebcf4c4f85d326b81ced2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:31 +0000

    Add package MeORM/1.22/MeORM-1.22.tar.gz

---
+++ packages/MeORM/MeORM-1.22.tar.gz/MeORM-1.22/morm/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-04-15 09:12:25
+# @Author  : Blackstone
+# @to      :
+
+
+from .Lib.dbhelper import DBHelper
+from .Lib.decorator import bind_sql,insert,update,delete,select
+from .Lib._gen import Gen
+
+
