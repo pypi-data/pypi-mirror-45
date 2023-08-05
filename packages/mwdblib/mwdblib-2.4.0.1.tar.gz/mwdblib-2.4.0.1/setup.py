2ed8c1f8be4a2ee806a123cf01711012171f95e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:26 +0000

    Add package mwdblib/2.4.0.1/mwdblib-2.4.0.1.tar.gz

---
+++ packages/mwdblib/mwdblib-2.4.0.1.tar.gz/mwdblib-2.4.0.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name="mwdblib",
+      version="2.4.0.1",
+      description="malwaredb API bindings for Python",
+      author="psrok1",
+      package_dir={'mwdblib': 'src'},
+      packages=['mwdblib'],
+      url="https://github.com/CERT-Polska/mwdblib",
+      install_requires=open("requirements.txt").read().splitlines(),
+      classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+      ])
