f21551a69a8ab54b0a23b733cfe22c06db69cd8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:15 +0000

    Add package ResponsiveVoice/0.2.1/ResponsiveVoice-0.2.1.tar.gz

---
+++ packages/ResponsiveVoice/ResponsiveVoice-0.2.1.tar.gz/ResponsiveVoice-0.2.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ResponsiveVoice',
+    version='0.2.1',
+    packages=['responsive_voice'],
+    url='https://github.com/JarbasAl/py_responsivevoice',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='unofficial python wrapper for responsive voice'
+)
