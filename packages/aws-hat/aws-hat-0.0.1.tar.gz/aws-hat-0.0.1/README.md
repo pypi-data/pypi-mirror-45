836a1dc09a063c9e8302bdc1da819a66385a0551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:51 +0000

    Add package aws-hat/0.0.1/aws-hat-0.0.1.tar.gz

---
+++ packages/aws-hat/aws-hat-0.0.1.tar.gz/aws-hat-0.0.1/README.md
@@ -0,0 +1,3 @@
+# aws-hat
+
+AWS CLI for putting on a different hat (assume role)
---
+++ packages/aws-hat/aws-hat-0.0.1.tar.gz/aws-hat-0.0.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+
+'''setup.py: setuptools control.'''
+
+
+import re
+import setuptools
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'readme.md'), 'r') as f:
+    long_description = f.read()
+
+version = "0.0.1"
+
+setuptools.setup(
+    name='aws-hat',
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts': ['hat = aws_hat.main:default']
+    },
+    version=version,
+    description='Assume a role',
+    long_description=long_description,
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=['boto3', 'requests', 'configparser', 'click'],
+    author='Martijn van Dongen',
+    author_email='martijnvandongen@binx.io',
+    url='https://github.com/binxio/aws-hat',
+)
