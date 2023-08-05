dcf106737e825e69a992d63e0c6deefc51e84ba5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:59 +0000

    Add package pupy/2.9.0/pupy-2.9.0-py2.py3-none-any.whl

---
+++ packages/pupy/pupy-2.9.0-py2.py3-none-any.whl/pupy/ui.pyi
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# ~ Jesse K. Rubin ~ Pretty Useful Python
+
+from typing import Any
+
+def yesno(question: Any, default: bool = ..., tries: int = ...): ...
+def term_table(strings: Any, row_wise: bool = ..., filler: str = ...): ...
