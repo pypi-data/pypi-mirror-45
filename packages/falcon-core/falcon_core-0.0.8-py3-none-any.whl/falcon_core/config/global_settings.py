302ce650da04a1437b64ce9509f86a054b028867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:55 +0000

    Add package falcon-core/0.0.8/falcon_core-0.0.8-py3-none-any.whl

---
+++ packages/falcon-core/falcon_core-0.0.8-py3-none-any.whl/falcon_core/config/global_settings.py
@@ -0,0 +1,21 @@
+BASE_DIR = None
+
+SECRET_KEY = ''
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+MIDDLEWARE = []
+
+INSTALLED_APPS = [
+    'falcon_core',
+]
+
+DATABASES = {}
+
+ROUTER_CONVERTERS = {}
+
+ROUTES = ''
+
+DEPLOY = {}
