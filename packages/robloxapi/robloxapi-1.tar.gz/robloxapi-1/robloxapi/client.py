3fc2bccee8273c9ff50a63dc6854adef55799439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:40 +0000

    Add package robloxapi/1/robloxapi-1.tar.gz

---
+++ packages/robloxapi/robloxapi-1.tar.gz/robloxapi-1/robloxapi/client.py
@@ -0,0 +1,26 @@
+import requests
+from .src.User import User
+def client(cookie=str()):
+    global functions
+    if cookie:
+        cookies = {
+            '.ROBLOSECURITY': cookie
+        }
+        r = requests.get('https://www.roblox.com/game/GetCurrentUser.ashx', cookies=cookies)
+        if r is 'null':
+            print('Rython: Failed to login. Using Rython without login')
+            functions = lambda: None
+            functions.User = User()
+            return functions
+        else:   
+            functions = lambda: None
+            functions.User = User(cookie, r)
+            return functions
+    else:
+        functions = lambda: None
+        functions.User = User()
+        return functions
+
+        #Check cookie
+   
+
