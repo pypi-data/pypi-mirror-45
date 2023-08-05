88783c8c29fea99257e86e63ebc13097874dbeb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:48 +0000

    Add package havedocker/0.1.3.5/havedocker-0.1.3.5.tar.gz

---
+++ packages/havedocker/havedocker-0.1.3.5.tar.gz/havedocker-0.1.3.5/setup.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+
+
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="havedocker",
+    version="0.1.3.5",
+    author="haitanghuadeng",
+    author_email="491609917@qq.com",
+    description="New havedocker, already has the basic docker instruction",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/haitanghuadeng",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
