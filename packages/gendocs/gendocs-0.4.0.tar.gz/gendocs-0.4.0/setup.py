e71073f9481f467ca6f5d22d5c2c4535b8f17905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:25 +0000

    Add package gendocs/0.4.0/gendocs-0.4.0.tar.gz

---
+++ packages/gendocs/gendocs-0.4.0.tar.gz/gendocs-0.4.0/setup.py
@@ -0,0 +1,31 @@
+"""gendocs: automatically build the documentation pages to document a given Python package for Sphinx
+"""
+
+import setuptools
+
+__version__ = '0.4.0'
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="gendocs",
+    version=__version__,
+    author="Bane Sullivan",
+    author_email="banesullivan@gmail.com",
+    description="Automatic documentation pages generation",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/banesullivan/gendocs",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'Sphinx>=1.7',
+        'properties>=0.4.0',
+    ],
+    classifiers=(
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        'Natural Language :: English',
+    ),
+)
