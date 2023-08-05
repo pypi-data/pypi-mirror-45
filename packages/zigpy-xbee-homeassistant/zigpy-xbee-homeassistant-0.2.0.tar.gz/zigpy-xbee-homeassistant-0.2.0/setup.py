46677da3aa13483ee10333e399d230d07f569c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:25 +0000

    Add package zigpy-xbee-homeassistant/0.2.0/zigpy-xbee-homeassistant-0.2.0.tar.gz

---
+++ packages/zigpy-xbee-homeassistant/zigpy-xbee-homeassistant-0.2.0.tar.gz/zigpy-xbee-homeassistant-0.2.0/setup.py
@@ -0,0 +1,23 @@
+"""Setup module for zigbpy-xbee"""
+
+from setuptools import find_packages, setup
+
+import zigpy_xbee.const as xbee_const
+
+setup(
+    name="zigpy-xbee-homeassistant",
+    version=xbee_const.__version__,
+    description="A library which communicates with XBee radios for zigpy",
+    url="http://github.com/zigpy/zigpy-xbee",
+    author="Russell Cloran",
+    author_email="rcloran@gmail.com",
+    license="GPL-3.0",
+    packages=find_packages(exclude=['*.tests']),
+    install_requires=[
+        'pyserial-asyncio',
+        'zigpy-homeassistant',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
