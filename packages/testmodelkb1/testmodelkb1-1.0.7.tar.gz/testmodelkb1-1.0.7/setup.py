9b2de1ea1ed96808ca9140f1197bd1718d1a5950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:07 +0000

    Add package testmodelkb1/1.0.7/testmodelkb1-1.0.7.tar.gz

---
+++ packages/testmodelkb1/testmodelkb1-1.0.7.tar.gz/testmodelkb1-1.0.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+files = ["static/css/*", "rawdata/*", "templates/*", "static/js/*", "static/scss/*", "static/vendor/bootstrap/*",
+         "static/vendor/chart.js/*", "static/vendor/datatables/*", "static/vendor/fontawesome-free/*", "static/vendor/jquery/*"
+         ,"static/vendor/jquery-easing/*"]
+
+setuptools.setup(
+    name="testmodelkb1",
+    version="1.0.7",
+    author="Sirisha Rella",
+    author_email="srnk2@mail.umkc.edu",
+    description="ModelKB",
+    long_description="ModelKB",
+    long_description_content_type="text/markdown",
+    url="https://github.com/SirishaRella/Xsequential_package",
+    package_data={'Xsequential': files},
+    packages=['Xsequential'],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+)
