7891b13f5c5654ae3c5a298fe041008fe7afcf23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:32 +0000

    Add package django-dynamic-model-validation/0.1.11/django-dynamic-model-validation-0.1.11.tar.gz

---
+++ packages/django-dynamic-model-validation/django-dynamic-model-validation-0.1.11.tar.gz/django-dynamic-model-validation-0.1.11/manage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+from future.utils import raise_from
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_dynamic_model_validation.settings")
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise_from(
+            ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            ), exc)
+    execute_from_command_line(sys.argv)
