2e14cf24982ecbb5e6abfee65d9cbc46bc38194b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:18 +0000

    Add package bob.devtools/1.0.1/bob.devtools-1.0.1.zip

---
+++ packages/bob.devtools/bob.devtools-1.0.1.zip/bob.devtools-1.0.1/doc/macos-ci-install/update-ci.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Update CI installation
+echo "Updating homebrew..."
+brew=/usr/local/bin/brew
+${brew} update
+${brew} upgrade
+${brew} cleanup
+
+
+# Updates PIP packages installed
+function pipupdate() {
+  echo "Updating ${1} packages..."
+  [ ! -x "${1}" ] && return
+  ${1} list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 ${1} install -U;
+}
+
+pipupdate /usr/local/bin/pip3
+pipupdate /usr/local/bin/pip2
