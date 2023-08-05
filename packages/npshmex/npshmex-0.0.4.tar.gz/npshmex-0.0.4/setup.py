28af1d65fcc852198d468213b01bc5e3a6377e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:35 +0000

    Add package npshmex/0.0.4/npshmex-0.0.4.tar.gz

---
+++ packages/npshmex/npshmex-0.0.4.tar.gz/npshmex-0.0.4/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+readme = open('README.md').read()
+history = open('HISTORY.md').read().replace('.. :changelog:', '')
+requirements = open('requirements.txt').read().splitlines()
+
+setuptools.setup(name='npshmex',
+                 author='Jelle Aalbers',
+                 version='0.0.4',
+                 description=('ProcessPoolExecutor that passes numpy'
+                              'arrays through shared memory'),
+                 py_modules=['multihist'],
+                 long_description=readme + '\n\n' + history,
+                 install_requires=['SharedArray'],
+                 setup_requires=['pytest-runner'],
+                 tests_require=requirements + ['pytest'],
+                 zip_safe=False)
