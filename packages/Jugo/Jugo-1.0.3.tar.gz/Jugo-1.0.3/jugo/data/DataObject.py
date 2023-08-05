24b8302644bc6b204009db50f549f452e8ab5fa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:31 +0000

    Add package Jugo/1.0.3/Jugo-1.0.3.tar.gz

---
+++ packages/Jugo/Jugo-1.0.3.tar.gz/Jugo-1.0.3/jugo/data/DataObject.py
@@ -0,0 +1,26 @@
+import jugo.utils.connection as db
+
+class DataObject():
+    def __init__(self, table_name):
+        self.__tablename__ = table_name
+
+    def save(self, update_fields=None):
+        fields = self.__dict__
+        update_query = ''
+
+        for param in fields:
+            if fields[param].__class__.__base__.__name__ == 'DataType' or param == 'id' or param == '__tablename__' or (update_fields and param not in update_fields):
+                continue
+
+            value = fields[f"{param}__datatype__"].pre_handling(fields[param])
+
+            update_query += f'`{param}` = {value if value is not None else "NULL"}, '
+
+
+        db.cursor.execute(f'UPDATE `{self.__tablename__}` SET {update_query[:-2]} WHERE `id` = {fields["id"]}')
+
+    def delete(self):
+        fields = self.__dict__
+        db.cursor.execute(f'DELETE FROM `{self.__tablename__}` WHERE `id` = {fields["id"]}')
+
+            
