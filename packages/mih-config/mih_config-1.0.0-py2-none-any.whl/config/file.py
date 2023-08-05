155cf9a40c7d5c308cd7be15a3711dd9f04802d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:05 +0000

    Add package mih-config/1.0.0/mih_config-1.0.0-py2-none-any.whl

---
+++ packages/mih-config/mih_config-1.0.0-py2-none-any.whl/config/file.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+from constant.constant import CONSTANT
+
+
+class FILE(CONSTANT):
+
+    FILE = 'config.cfg'
---
+++ packages/mih-config/mih_config-1.0.0-py2-none-any.whl/config/sql.py
@@ -0,0 +1,13 @@
+# encoding: utf-8
+
+from constant.constant import CONSTANT
+
+
+class SQL(CONSTANT):
+
+    MYSQL          = 'mysql'
+    MYSQL_HOST     = 'mysql_host'
+    MYSQL_PORT     = 'mysql_port'
+    MYSQL_DB       = 'mysql_db'
+    MYSQL_USER     = 'mysql_user'
+    MYSQL_PASSWORD = 'mysql_password'
