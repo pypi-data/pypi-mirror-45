40687764c2987e0f2d75fbf43137e40e166f2815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:25 +0000

    Add package dott/1.0.1/dott-1.0.1-py2.py3-none-any.whl

---
+++ packages/dott/dott-1.0.1-py2.py3-none-any.whl/dott/__version__.py
@@ -0,0 +1,5 @@
+__version__ = "1.0.1"
+__description__ = "A simple scheduling package."
+__url__ = "https://github.com/alexmacniven/dott"
+__email__ = "macniven.ap@gmail.com"
+__author__ = 'Alex Macniven'
