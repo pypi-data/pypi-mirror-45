d1bb43c3f73df0395711b61c5672a41baca8e14a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:37 +0000

    Add package gbvision/0.0.1/gbvision-0.0.1.tar.gz

---
+++ packages/gbvision/gbvision-0.0.1.tar.gz/gbvision-0.0.1/gbvision/__init__.py
@@ -0,0 +1,10 @@
+from .utils import *
+from .constants import *
+from .exceptions import *
+from .finders import *
+from .gui import *
+from .net import *
+from .models import *
+from .tools import *
+
+cv_config()
