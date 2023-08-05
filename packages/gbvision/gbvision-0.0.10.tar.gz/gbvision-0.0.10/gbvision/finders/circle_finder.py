0e63cc031917b0fc5fae6629df2efa0174992164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:15 +0000

    Add package gbvision/0.0.10/gbvision-0.0.10.tar.gz

---
+++ packages/gbvision/gbvision-0.0.10.tar.gz/gbvision-0.0.10/gbvision/finders/circle_finder.py
@@ -0,0 +1,25 @@
+from gbvision.constants.math import SQRT_PI
+from gbvision.models.contours import find_contours, filter_contours, contours_to_circles_sorted
+from gbvision.models.shapes import filter_inner_circles
+from .object_finder import ObjectFinder
+
+
+class CircleFinder(ObjectFinder):
+    """
+    finds a circular shaped object, like a ball or a disk
+    """
+    def __init__(self, threshold_func, game_object, contour_min_area=3.0):
+        """
+        initializes the finder
+        :param contour_min_area: the minimal area of a contour, used in filter_contours
+        """
+        ObjectFinder.__init__(self, threshold_func, game_object)
+        self._full_pipeline = (threshold_func +
+                               find_contours +
+                               filter_contours(min_area=contour_min_area) +
+                               contours_to_circles_sorted +
+                               filter_inner_circles)
+
+    def __call__(self, frame, camera):
+        circles = self._full_pipeline(frame)
+        return map(lambda circ: self.game_object.location3d_by_params(camera, SQRT_PI * circ[1], circ[0]), circles)
