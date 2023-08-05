134366dcae40681d32b94eecd220dfbbc711414a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:26 +0000

    Add package pyfunctionbases/1.2/pyfunctionbases-1.2.tar.gz

---
+++ packages/pyfunctionbases/pyfunctionbases-1.2.tar.gz/pyfunctionbases-1.2/setup.py
@@ -0,0 +1,15 @@
+import os.path
+from setuptools import setup
+
+long_description = open('./README.md').read()
+
+setup(name='pyfunctionbases',
+      version="1.2",
+      description='A Python module to compute multidimensional arrays of evaluated functions.',
+      long_description=long_description,
+      url='https://github.com/NiMlr/PyFunctionBases',
+      author='Nils Mueller',
+      author_email='nils.mueller@ini.rub.de',
+      license='MIT',
+      packages=['pyfunctionbases'],
+      setup_requires=['setuptools_scm >= 1.7.0'])
