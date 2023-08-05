3ca0de13df19132fbb1037c35c4f59678fa80ead
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:30 +0000

    Add package lmf/2.0.4/lmf-2.0.4.zip

---
+++ packages/lmf/lmf-2.0.4.zip/lmf-2.0.4/lmf/bigdata.py
@@ -0,0 +1,39 @@
+import pandas as pd 
+from sqlalchemy import create_engine,types
+from sqlalchemy.dialects.postgresql import TEXT 
+import pymssql
+import cx_Oracle
+import psycopg2 
+import MySQLdb
+import datetime
+import re
+import sqlite3
+import os 
+
+
+#postgresql 查询结果输出csv文件
+def pg2csv(sql,conp,path,chunksize,f=None,**krg):
+    #sql="select * from hefei.gg limit 100"
+    #conp=["postgres",'since2015','192.168.4.175','anhui','hefei']
+    #path="d:\\test.csv"
+    con=create_engine("postgresql://%s:%s@%s/%s"%(conp[0],conp[1],conp[2],conp[3]),encoding='utf-8',execution_options=dict(stream_results=True))
+    dfs=pd.read_sql(sql,con,chunksize=chunksize)
+    count=1
+    for df in dfs:
+        if df is not None:
+            df=f(df)
+        if count==1:
+            df.to_csv(path,index=False,**krg)
+        else:
+            krg['header']=False
+            df.to_csv(path,mode='a+',index=False,**krg)
+        count+=1
+
+# sql="select * from hefei.gg limit 100"
+# conp=["postgres",'since2015','192.168.4.175','anhui','hefei']
+# path="d:\\test.csv"
+
+# def f1(df):
+#     df['name']='xx'
+#     return df 
+# pg2csv(sql,conp,path,f1,sep='\001')
