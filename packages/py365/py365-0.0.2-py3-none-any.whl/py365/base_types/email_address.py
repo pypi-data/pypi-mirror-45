d0ae9ce2f1c13582ff35c1877da6b9b190e104d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:49 +0000

    Add package py365/0.0.2/py365-0.0.2-py3-none-any.whl

---
+++ packages/py365/py365-0.0.2-py3-none-any.whl/py365/base_types/email_address.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+# https://docs.microsoft.com/en-us/graph/api/resources/emailaddress
+@dataclass
+class EmailAddress:
+    adress: str
+    name: str
+
+    def __repr__(self):
+        repr = {
+            "address": adress,
+            "name": name
+        }
+        return repr
