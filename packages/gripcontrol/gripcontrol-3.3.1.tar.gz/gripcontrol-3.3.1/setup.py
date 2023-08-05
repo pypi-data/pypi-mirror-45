b7e480b7f332744a512bd4588fb7053a27d3cb3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:32 +0000

    Add package gripcontrol/3.3.1/gripcontrol-3.3.1.tar.gz

---
+++ packages/gripcontrol/gripcontrol-3.3.1.tar.gz/gripcontrol-3.3.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+name='gripcontrol',
+version='3.3.1',
+description='GRIP library',
+author='Justin Karneges',
+author_email='justin@fanout.io',
+url='https://github.com/fanout/pygripcontrol',
+license='MIT',
+package_dir={'gripcontrol': 'src'},
+packages=['gripcontrol'],
+install_requires=['PyJWT>=1.5,<2', 'pubcontrol>=2.4.1,<3', 'six>=1.10.0,<2'],
+classifiers=[
+	'Topic :: Utilities',
+	'License :: OSI Approved :: MIT License'
+]
+)
