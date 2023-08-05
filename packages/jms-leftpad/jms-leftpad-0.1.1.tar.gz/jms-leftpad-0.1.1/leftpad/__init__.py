2c1709e6babd2573aa98e680c840743cf8d128a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:18 +0000

    Add package jms-leftpad/0.1.1/jms-leftpad-0.1.1.tar.gz

---
+++ packages/jms-leftpad/jms-leftpad-0.1.1.tar.gz/jms-leftpad-0.1.1/leftpad/__init__.py
@@ -0,0 +1,5 @@
+
+from .func import leftpad
+
+
+__version__ = '0.1.2'
---
+++ packages/jms-leftpad/jms-leftpad-0.1.1.tar.gz/jms-leftpad-0.1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    author='jms7446',
+    author_email='jms7446@gmail.com',
+    url='https://github.com/jms7446/pyleftpad',
+)
