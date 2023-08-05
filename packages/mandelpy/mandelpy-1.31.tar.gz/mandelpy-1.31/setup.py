27f8e7fb7fc98245556e4cebc95a84f9c315d882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:05 +0000

    Add package mandelpy/1.31/mandelpy-1.31.tar.gz

---
+++ packages/mandelpy/mandelpy-1.31.tar.gz/mandelpy-1.31/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+setup(
+  name = 'mandelpy',         
+  packages = ['mandelpy'],   
+  version = '1.31',      
+  license='MIT',        
+  description = 'Mandelbrot library for Python',   
+  author = 'Matteo Magri',                   
+  author_email = 'magri.matteo@outlook.com',      
+  url = 'https://github.com/MatteoMagri/Mandelpy',   
+  download_url = 'https://github.com/MatteoMagri/Mandelpy/archive/v1.31.tar.gz',
+  keywords = ['Python', 'Mandelbrot', 'PIL', 'ART', 'Graphics'],   
+  install_requires=[            
+          'Pillow',
+          'numpy',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+
+    'Intended Audience :: Education',     
+    'Topic :: Artistic Software',
+
+    'License :: OSI Approved :: MIT License',   
+
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
