146dd4ec3ab1f6d3c6fcf0e3c6ef766ba4266f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:39 +0000

    Add package TDMYSA/0.1/TDMYSA-0.1.tar.gz

---
+++ packages/TDMYSA/TDMYSA-0.1.tar.gz/TDMYSA-0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+long_description=""" TDMYSA """
+	
+	
+setup(name='TDMYSA',
+      version='0.1',
+      description='scores',
+	  long_description=long_description,
+	  url='https://shahabks.github.io/Speech-Rater/',
+      author='Shahab Sabahi',
+      author_email='shahab.sabahi@gmail.com',
+      license='MIT',
+      classifiers=[
+		'Intended Audience :: Developers',
+		'Intended Audience :: Science/Research',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3.7',
+		],
+	  keywords='acoustic and signal processing',
+	  install_requires=[
+		'numpy>=1.15.2',
+		'praat-parselmouth>=0.3.2',
+		'pandas>=0.23.4',
+		'scipy>=1.1.0',
+		'SpeechRecognition>=3.8.1',
+		'langdetect>=1.0.7',
+		'nltk>=3.4',
+		'scikit-learn>=0.20.1',
+		'textatistic>=0.0.1',
+		'textblob>=0.15.3',
+		],
+	  packages=['TDMYSA'],
+      zip_safe=False) 
