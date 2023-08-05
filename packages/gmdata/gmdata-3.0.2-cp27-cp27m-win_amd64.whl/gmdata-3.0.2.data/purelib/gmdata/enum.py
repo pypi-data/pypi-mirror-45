2d8efc10295390ab0480df2c6a7eb77e2692cf75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:26 +0000

    Add package gmdata/3.0.2/gmdata-3.0.2-cp27-cp27m-win_amd64.whl

---
+++ packages/gmdata/gmdata-3.0.2-cp27-cp27m-win_amd64.whl/gmdata-3.0.2.data/purelib/gmdata/enum.py
@@ -0,0 +1,13 @@
+# coding=utf8
+from __future__ import print_function, absolute_import, unicode_literals
+
+ADJUST_NONE = 0
+ADJUST_PREV = 1
+ADJUST_POST = 2
+
+SEC_TYPE_STOCK = 1
+SEC_TYPE_FUND = 2
+SEC_TYPE_INDEX = 3
+SEC_TYPE_FUTURE = 4
+SEC_TYPE_OPTION = 5
+SEC_TYPE_CONFUTURE = 10
