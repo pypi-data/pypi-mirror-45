35da473070f868847c3a60dc58823ae783c32af3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:35 +0000

    Add package PyStratum-MySQL/0.10.15/PyStratum-MySQL-0.10.15.tar.gz

---
+++ packages/PyStratum-MySQL/PyStratum-MySQL-0.10.15.tar.gz/PyStratum-MySQL-0.10.15/pystratum_mysql/wrapper/MySqlMultiWrapper.py
@@ -0,0 +1,17 @@
+"""
+PyStratum
+"""
+from pystratum.wrapper.LogWrapper import LogWrapper
+from pystratum_mysql.wrapper.MySqlWrapper import MySqlWrapper
+
+
+class MySqlMultiWrapper(MySqlWrapper, LogWrapper):
+    """
+    Wrapper method generator for stored procedures with designation type multi.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def _write_result_handler(self, routine):
+        self._write_line('return StaticDataLayer.execute_sp_multi({0!s})'.format(self._generate_command(routine)))
+
+# ----------------------------------------------------------------------------------------------------------------------
