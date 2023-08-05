56fee2a2ccefbf1b818b393799c67ace7cc8dbaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:29 +0000

    Add package sphinxcontrib-viewsource/0.1.3/sphinxcontrib-viewsource-0.1.3.tar.gz

---
+++ packages/sphinxcontrib-viewsource/sphinxcontrib-viewsource-0.1.3.tar.gz/sphinxcontrib-viewsource-0.1.3/README.rst
@@ -0,0 +1,11 @@
+########################
+sphinxcontrib-viewsource
+########################
+
+sphinxcontrib-viewsource is a Sphinx extension that enhances the ``literalinclude`` directive, adding a caption automatically with a link pointing to the source file.
+
+The strategy to resolve the link can be coded in the configuration file, enabling you to point to internal files or external links as needed.
+
+Use it to add an **Edit on GitHub** button to your code source files, or to **show the complete code** when the literalinclude directive renders a subset of the file.
+
+`Read the docs to add it into your project <https://sphinxcontrib-viewsource.readthedocs.io/en/latest/>`_.
---
+++ packages/sphinxcontrib-viewsource/sphinxcontrib-viewsource-0.1.3.tar.gz/sphinxcontrib-viewsource-0.1.3/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='sphinxcontrib-viewsource',
+    version='0.1.3',
+    url='https://github.com/dgarcia360/sphinxcontrib-viewsource',
+    # download_url='http://pypi.python.org/pypi/sphinxcontrib-viewsource',
+    license='MIT',
+    author='David Garcia',
+    author_email='dgarcia360@outlook.com',
+    description='Add an "Edit on GitHub" button to your code examples.',
+    long_description="",
+    zip_safe=True,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Documentation',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    platforms='any',
+    include_package_data=True,
+    install_requires=['Sphinx>=1.1', 'requests'],
+    packages=find_packages(),
+    namespace_packages=['sphinxcontrib']
+)
