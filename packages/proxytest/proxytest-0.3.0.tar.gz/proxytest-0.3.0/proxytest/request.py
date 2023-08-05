5731455a9836181a199f2e0773a7478f3867fc84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:35 +0000

    Add package proxytest/0.3.0/proxytest-0.3.0.tar.gz

---
+++ packages/proxytest/proxytest-0.3.0.tar.gz/proxytest-0.3.0/proxytest/request.py
@@ -0,0 +1,61 @@
+"""
+Info-carrying objects shared between proxytest and backends.
+"""
+
+import time
+from typing import Any, Callable
+
+
+class SessionConfig(object):
+    def __init__(self, timeout: float = None, max_workers: int = None):
+        self.timeout = timeout
+        self.max_workers = max_workers
+
+
+class RequestInfo(object):
+    @property
+    def succeeded(self):
+        assert self.finished, 'succeeded called before finished! started={!r}, finished={!r}'.format(self.started, self.finished)
+        return self.error is None
+
+    def __init__(self, url: str, proxy_url: str = None, user_agent: str = None, name: str = None,
+                 start_callback: Callable[['RequestInfo'], Any] = None, end_callback: Callable[['RequestInfo'], Any] = None):
+        """
+        :param url: The URL to fetch.
+        :param proxy_url: The proxy to use while fetching.
+        :param user_agent: The user agent to add to the headers.
+        :param name: A unique name to distinguish this requests from others in logs.
+        :param start_callback: Callback to call when the request starts. Will be passed one positional argument: this object.
+        :param end_callback: Callback to call when the request ends. Will be passed one positional argument: this object.
+        """
+        if not url:
+            raise ValueError('URL is required!')
+        self.url = url
+        self.proxy_url = proxy_url
+        self.name = name or ''
+        self.start_callback = start_callback
+        self.end_callback = end_callback
+        self.headers = {}
+        if user_agent:
+            self.headers['User-Agent'] = user_agent
+        # variables that change during processing
+        self.started = None
+        """ time.monotonic() at start """
+        self.finished = None
+        """ time.monotonic() at end """
+        self.error = None
+        """ Error string """
+        self.result = None
+        """ Result string """
+
+    def set_started(self):
+        self.started = time.monotonic()
+        if self.start_callback:
+            self.start_callback(self)
+
+    def set_finished(self, error: Exception = None, result: str = None):
+        self.finished = time.monotonic()
+        self.error = None if not error else (str(error) or repr(error))
+        self.result = result
+        if self.end_callback:
+            self.end_callback(self)
---
+++ packages/proxytest/proxytest-0.3.0.tar.gz/proxytest-0.3.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import os
+import sys
+import warnings
+
+from setuptools import setup, find_packages
+import proxytest
+
+if sys.version_info < (3, 5):
+    warnings.warn('Python 3.5 or above is recommended!')
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), "r") as fh:
+    long_description = fh.read()
+
+install_requires = [
+    'aiohttp>=3.5.4,<4.0; python_version>"3.5.3"',
+    'requests>=2.21.0,<3.0; python_version<"3.5.3"',
+    'typing>=3.6.6,<4.0; python_version<"3.5.3"'
+]
+
+setup(
+    name='proxytest',
+    version=proxytest.__version__,
+    description='A simple script to test if one or more HTTP proxies are working by fetching a webpage.',
+    long_description=long_description,
+    classifiers=[
+        "Environment :: Console",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords='proxy test command multiple proxies',
+    author='Oleg Pryadko',
+    author_email='oleg@olegpryadko.com',
+    maintainer='Oleg Pryadko',
+    maintainer_email='oleg@olegpryadko.com',
+    url='https://github.com/yoleg/proxytest',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'proxytest = proxytest.proxytest:main',
+        ]
+    },
+    install_requires=install_requires,
+)
