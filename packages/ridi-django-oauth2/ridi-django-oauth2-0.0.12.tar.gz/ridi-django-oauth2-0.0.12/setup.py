1ea29118bfb65707bb35b2804b79a0ee69e2ddcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:04 +0000

    Add package ridi-django-oauth2/0.0.12/ridi-django-oauth2-0.0.12.tar.gz

---
+++ packages/ridi-django-oauth2/ridi-django-oauth2-0.0.12.tar.gz/ridi-django-oauth2-0.0.12/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+version = '0.0.12'
+
+with open('requirements/base.txt') as f:
+    install_requires = [line for line in f if line and not line.startswith('-')]
+
+
+setup(
+    name='ridi-django-oauth2',
+    version=version,
+    url='https://github.com/ridi/django-oauth2',
+    license='MIT',
+    description='RIDI Django OAuth2 Library',
+    keywords=['oauth2', 'oauth2-resource', 'ridi', 'ridibooks'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Django :: 2.0',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    install_requires=install_requires,
+    include_package_data=True,
+)
