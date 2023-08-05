57aa22f4fb8f535b245a4689a171c8afe894bd52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:08 +0000

    Add package aitools-aimfeld/0.3.5/aitools-aimfeld-0.3.5.tar.gz

---
+++ packages/aitools-aimfeld/aitools-aimfeld-0.3.5.tar.gz/aitools-aimfeld-0.3.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='aitools-aimfeld',
+    version='0.3.5',
+    description='Python tools for datascience and AI',
+    url='https://github.com/aimfeld/aitools',
+    author='Adrian Imfeld',
+    author_email='aimfeld@aimfeld.ch',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=False
+)
+
+# Update package
+# python setup.py sdist bdist_wheel
+# python -m twine upload dist/*
