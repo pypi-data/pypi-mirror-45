828d5cb1f9fec37c8a3ad271ff9fe9bd83cd65bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:07 +0000

    Add package havedocker/0.1.3.7/havedocker-0.1.3.7.tar.gz

---
+++ packages/havedocker/havedocker-0.1.3.7.tar.gz/havedocker-0.1.3.7/havedocker/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+
+from .Docker import Agility_Docker
+from .DockerError import *
+
+name = "havedocker"
+version = '0.1.3.7'
+author = 'haitanghuadeng'
+project = 'https://github.com/haitanghuadeng/havedocker'
+description = "Additional version based on 0.1.3.6 version"
+
