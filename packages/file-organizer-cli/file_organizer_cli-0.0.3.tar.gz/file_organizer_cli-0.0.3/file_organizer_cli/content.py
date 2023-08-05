c64d3115018ade83893169f3f1d9a2e52f166d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:37 +0000

    Add package file-organizer-cli/0.0.3/file_organizer_cli-0.0.3.tar.gz

---
+++ packages/file-organizer-cli/file_organizer_cli-0.0.3.tar.gz/file_organizer_cli-0.0.3/file_organizer_cli/content.py
@@ -0,0 +1,158 @@
+import typing
+import re
+
+
+class DjangoContent:
+    """
+    Content storage for django model entity
+    """
+    model = None
+    pk = None
+    fields = None
+
+    def __init__(self, model: str = None, pk: int = None, fields: typing.List = list):
+        self.model = model
+        self.pk = pk
+        self.fields = fields
+
+    def __str__(self):
+        return '<DjangoContent model="{}" pk="{}">'.format(
+            self.model if self.model else 'None',
+            self.pk if self.pk else 'None'
+        )
+
+    def __dict__(self):
+        """
+        Serialization method
+        :return:
+        """
+        return {
+            'model': self.model or None,
+            'pk': self.pk or None,
+            'fields': self.fields or {}
+        }
+
+
+class DjangoContentList:
+    """
+    Content Manager for django model objs
+    """
+    objects = []
+    objects_dict = {}
+    fields_set = set()
+    __iterator = 0
+
+    def as_dict(self):
+        """
+        :return: content as dict
+        """
+        return self.objects_dict
+
+    def as_list(self):
+        """
+        :return: content as list
+        """
+        return self.objects
+
+    @property
+    def length(self):
+        """
+        :return: num elem
+        """
+        return len(self.objects) - 1
+
+    def exclude(self, app: str = None, model: str = None, pk: int = -1):
+        """
+        content exclusion filter
+        :param app: app label
+        :param model: model label
+        :param pk: entities primary key
+        :return: filtered list
+        """
+        if pk == -1:
+            if model:
+                new_list = [i for i in self.objects if i.model != model]
+                new_dict = {i: v for i, v in self.objects_dict.items() if i != model}
+                self.objects_dict = {}
+                self.objects = []
+                self.objects = new_list
+                self.objects_dict = new_dict.copy()
+                return self
+            elif app:
+                new_list = [i for i in self.objects if i.model.find(app) == -1]
+                new_dict = {i: v for i, v in self.objects_dict.items() if i.find(app) == -1}
+                self.objects_dict = {}
+                self.objects = []
+                self.objects = new_list
+                self.objects_dict = new_dict.copy()
+                return self
+        else:
+            regex = re.findall(r'pk="(?P<pk>[0-9]*)"', pk)
+            if regex:
+                for real_pk in regex:
+                    new_list = [i for i in self.objects if i.pk != int(real_pk)]
+                    new_dict = {i: v for i, v in self.objects_dict.items() for obj in v if obj.pk != int(real_pk)}
+                    self.objects_dict = {}
+                    self.objects = []
+                    self.objects = new_list
+                    self.objects_dict = new_dict.copy()
+                return self
+
+    def filter(self, app: str = None, model: str = None, pk: int = -1):
+        """
+        content filter
+        :param app: app label
+        :param model: model label
+        :param pk: entities primary key
+        :return: filtered list
+        """
+        if pk == -1:
+            new_list = [i for i in self.objects if i.model == model]
+            new_dict = {i: v for i, v in self.objects_dict.items() if i == model}
+            self.objects_dict = {}
+            self.objects = []
+            self.objects = new_list
+            self.objects_dict = new_dict.copy()
+            del new_dict, new_list
+
+    def append(self, obj) -> None:
+        """
+        :param obj:
+        :return:
+        """
+        self.objects.append(obj)
+        try:
+            self.objects_dict[obj.model].append(obj)
+        except KeyError:
+            self.objects_dict[obj.model] = [obj]
+
+        self.fields_set.add(field for field in obj.fields.keys())
+
+    def __str__(self):
+        return '<DjangoContentList count={} models={} fields={}>'.format(
+            self.length,
+            self.objects_dict.keys(),
+            self.fields_set
+        )
+
+    def __len__(self):
+        return self.length
+
+    def __iter__(self):
+        """
+
+        :return:
+        """
+        self.__iterator = 0
+        return self
+
+    def __next__(self):
+        """
+
+        :return:
+        """
+        if self.__iterator < self.length:
+            self.__iterator += 1
+            return self.objects[self.__iterator]
+        else:
+            raise StopIteration
---
+++ packages/file-organizer-cli/file_organizer_cli-0.0.3.tar.gz/file_organizer_cli-0.0.3/setup.py
@@ -0,0 +1,43 @@
+import os
+from distutils.core import setup
+
+with open('README', 'r') as fh:
+    long_description = fh.read()
+
+if os.environ.get('CI_COMMIT_TAG'):
+    version = os.environ['CI_COMMIT_TAG']
+else:
+    version = os.environ.get('CI_JOB_ID', os.environ.get('CI_COMMIT_SHORT_SHA', '0.0.3'))
+
+setup(
+    name='file_organizer_cli',
+    packages=['file_organizer_cli'],
+    version=version,
+    license='gpl-3.0',
+    description='interactive file extension manager cli',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Philipp Zettl',
+    author_email='philipp.zettl@time-dev.de',
+    url='https://gitlab.com/philsupertramp/file-organizer-cli',
+    download_url='https://gitlab.com/philsupertramp/file-organizer-cli/-/archive/master/file-organizer-cli-master.tar.gz',
+    install_requires=[
+        'PyInquirer',
+    ],
+    requires=[
+        'PyInquirer',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
