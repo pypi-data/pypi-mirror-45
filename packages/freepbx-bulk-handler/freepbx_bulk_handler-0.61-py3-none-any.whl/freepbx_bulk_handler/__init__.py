17a832da7f86035f7c70fcf5542741e37574a26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:26 +0000

    Add package freepbx-bulk-handler/0.61/freepbx_bulk_handler-0.61-py3-none-any.whl

---
+++ packages/freepbx-bulk-handler/freepbx_bulk_handler-0.61-py3-none-any.whl/freepbx_bulk_handler/__init__.py
@@ -0,0 +1,16 @@
+"""
+FreePBX Bulk Handler uses CSV files for importation.
+This package is designed to assist in formatting the CSV file.
+"""
+
+from .Extension import *
+
+
+def __authorship__():
+    """
+***********************************************************************************************************************
+This python module was created by Dax Mickelson along with LOTs of help from my friend and colleague Dmitry Figol.
+***********************************************************************************************************************
+    """
+
+__authorship__()
