080875e81e84864b8764ae2d0a7b0da9e0488e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:12 +0000

    Add package setupmeta-builder/0.0.2/setupmeta_builder-0.0.2-py3-none-any.whl

---
+++ packages/setupmeta-builder/setupmeta_builder-0.0.2-py3-none-any.whl/setupmeta_builder/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019~2999 - Cologler <skyoflw@gmail.com>
+# ----------
+#
+# ----------
+
+from .core import SetupAttrContext, SetupMetaBuilder
+
+def get_setup_attrs() -> dict:
+    '''
+    get the auto generated attrs dict.
+    '''
+    ctx = SetupAttrContext()
+    SetupMetaBuilder().fill_ctx(ctx)
+    return ctx.setup_attrs
+
+def setup_it(**attrs):
+    '''
+    just enjoy the auto setup!
+    '''
+    setup_attrs = get_setup_attrs()
+    setup_attrs.update(attrs)
+    from setuptools import setup
+    setup(**setup_attrs)
