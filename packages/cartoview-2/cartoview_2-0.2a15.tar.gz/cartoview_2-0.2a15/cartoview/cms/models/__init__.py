4acf1897b87fe92d648787991d48a4aecb8072f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:34 +0000

    Add package cartoview-2/0.2a15/cartoview_2-0.2a15.tar.gz

---
+++ packages/cartoview-2/cartoview_2-0.2a15.tar.gz/cartoview_2-0.2a15/cartoview/cms/models/__init__.py
@@ -0,0 +1,9 @@
+from .HomePage import HomePage
+from .map_catalog.MapCatalogPage import MapCatalogPage
+from .map_catalog.MapCatalogKeywordIndex import MapCatalogKeywordIndex
+from .map_catalog.MapCatalogSearchIndex import MapCatalogSearchIndex
+
+from .menu.MenuItem import MenuItem
+from .menu.MenuLink import MenuLink
+
+from .settings.SiteSettings import SiteSettings
