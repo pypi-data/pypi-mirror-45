d92441cc749cb792fe19ed77c8c92a9bbe6060f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:28 +0000

    Add package helloworld-library-friendryu/1.0/helloworld-library_friendryu-1.0.tar.gz

---
+++ packages/helloworld-library-friendryu/helloworld-library_friendryu-1.0.tar.gz/helloworld-library_friendryu-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="helloworld-library_friendryu",
+    version='1.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/friendryu/demo-helloworld-library',
+    author='Friendryu',
+    author_email='friendryu.bb@gmail.com',
+)
