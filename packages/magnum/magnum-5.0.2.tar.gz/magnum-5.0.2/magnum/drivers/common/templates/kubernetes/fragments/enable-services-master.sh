f9cc05bab2fb4d66ab6fa4620948cad9a2f5328d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:32 +0000

    Add package magnum/5.0.2/magnum-5.0.2.tar.gz

---
+++ packages/magnum/magnum-5.0.2.tar.gz/magnum-5.0.2/magnum/drivers/common/templates/kubernetes/fragments/enable-services-master.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# make sure we pick up any modified unit files
+systemctl daemon-reload
+
+echo "starting services"
+for service in etcd docker kube-apiserver kube-controller-manager kube-scheduler kubelet kube-proxy; do
+    echo "activating service $service"
+    systemctl enable $service
+    systemctl --no-block start $service
+done
