60b3ed8a02a7ffb90c0ac45d282dc3688ba920a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:55 +0000

    Add package mask-predictor/0.0.7/mask-predictor-0.0.7.tar.gz

---
+++ packages/mask-predictor/mask-predictor-0.0.7.tar.gz/mask-predictor-0.0.7/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mask-predictor",
+version="0.0.7",
+    author="Tan Kim Yong",
+    author_email="kimyong95@gmail.com",
+    description="A wrapper of BERT to predict the covered word in a sentence",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "torch",
+        "spacy",
+        "numpy",
+        "sklearn",
+        "pytorch-pretrained-bert",
+    ],
+)
