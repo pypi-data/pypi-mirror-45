20ad8e669a16e64c12e3b0611a729bf8a7f75669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:15 +0000

    Add package fake-bpy-module-2.78/20190423/fake-bpy-module-2.78-20190423.tar.gz

---
+++ packages/fake-bpy-module-2.78/fake-bpy-module-2.78-20190423.tar.gz/fake-bpy-module-2.78-20190423/bpy/__init__.py
@@ -0,0 +1,12 @@
+from .context import Context as context
+
+from . import types
+from . import ops
+from . import utils
+from . import app
+from . import context
+from . import props
+from . import path
+
+data = None  # type:  bpy.types.BlendData
+'''Access to Blenders internal data '''
