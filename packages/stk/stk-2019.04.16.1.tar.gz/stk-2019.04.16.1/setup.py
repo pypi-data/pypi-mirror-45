fdc076d2867b0c498024b7c95cc9ce59f5d90bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:55 +0000

    Add package stk/2019.04.16.1/stk-2019.04.16.1.tar.gz

---
+++ packages/stk/stk-2019.04.16.1.tar.gz/stk-2019.04.16.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(name='stk',
+      author='Lukas Turcani',
+      author_email='lukasturcani93@gmail.com',
+      url='https://www.github.com/lukasturcani/stk',
+      version='2019.04.16.1',
+      packages=['stk',
+                'stk.utilities',
+                'stk.molecular',
+                'stk.molecular.topologies',
+                'stk.molecular.topologies.cage',
+                'stk.optimization'],
+      install_requires=['networkx',
+                        'scipy',
+                        'matplotlib',
+                        'scikit-learn',
+                        'psutil',
+                        'pywindowx',
+                        'pandas',
+                        'seaborn'],
+      python_requires='>=3.6')
