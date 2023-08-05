fdd09cca375a0633a51872c51cc37b21a683fb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:30 +0000

    Add package MeORM/1.12/MeORM-1.12.tar.gz

---
+++ packages/MeORM/MeORM-1.12.tar.gz/MeORM-1.12/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+#导入setup函数
+ 
+setup(
+	  name="MeORM", 
+	  version="1.12",
+	  description="orm框架",
+	  author="blackstone",
+	  author_email="971406187@qq.com",
+	  url="https://github.com/Blackstone1204",
+	  #py_modules=['morm']
+	  packages=find_packages()
+	  )
