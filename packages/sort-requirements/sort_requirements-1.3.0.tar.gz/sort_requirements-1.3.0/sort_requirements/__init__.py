092b960415e28e394e5a4e7f23adc2bb1dba4879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:50 +0000

    Add package sort-requirements/1.3.0/sort_requirements-1.3.0.tar.gz

---
+++ packages/sort-requirements/sort_requirements-1.3.0.tar.gz/sort_requirements-1.3.0/sort_requirements/__init__.py
@@ -0,0 +1,21 @@
+import re
+
+
+VERSION = (1, 3, 0)
+DEPS_RE = r"((?:#[^\n]+?\n)*)([^\n]+?)([=!~>]=)([^\\\n]+)((?:\\\n[^\\\n]+)*)"
+
+
+__version__ = ".".join(str(v) for v in VERSION)
+
+
+def sort_requirements(requirements):
+    matches = re.findall(DEPS_RE, requirements)
+    data = re.sub(DEPS_RE, "{}", requirements)
+    matches = sorted(matches, key=lambda d: d[1].lower())
+
+    deps = []
+    for m in matches:
+        deps.append("{}{}{}{}{}".format(*m))
+
+    data = data.format(*deps)
+    return data
