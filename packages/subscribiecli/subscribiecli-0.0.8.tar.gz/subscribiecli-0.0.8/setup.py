09a692210ce801521e8b69d848b31265824e2253
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:15 +0000

    Add package subscribiecli/0.0.8/subscribiecli-0.0.8.tar.gz

---
+++ packages/subscribiecli/subscribiecli-0.0.8.tar.gz/subscribiecli-0.0.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+setuptools.setup(
+    name='subscribiecli',
+    version="0.0.8",
+    author="Karma Computing",
+    author_email="subscribie@karmacomputing.co.uk",
+    desciption="cli utility for Subscribie",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ),
+    python_requires=">=3",
+    install_requires=[
+        'click',
+        'gitpython',
+        'pyOpenSSL',
+    ],
+    entry_points='''
+        [console_scripts]
+        subscribie=subscribie_cli:cli
+    ''',
+)
