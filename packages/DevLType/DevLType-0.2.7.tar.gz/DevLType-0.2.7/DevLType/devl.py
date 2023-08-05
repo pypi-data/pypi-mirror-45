d5ff800183202c82af88bdb8fa126d2711182e27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:29 +0000

    Add package DevLType/0.2.7/DevLType-0.2.7.tar.gz

---
+++ packages/DevLType/DevLType-0.2.7.tar.gz/DevLType-0.2.7/DevLType/devl.py
@@ -0,0 +1,12 @@
+from . import OrderedDict,dumps
+
+class DevLTypes(object):
+
+	def default(self):
+		L = {}
+		for key, value in self.__dict__.items():
+			if self.__dict__[key] is not None and isinstance(value,(int,str,list,dict)):
+				L[key] = value
+			else:
+				L[key] = value.__class__.__name__
+		return '%s(%s)' % (self.__class__.__name__,dumps(L, indent=4, sort_keys=True))
