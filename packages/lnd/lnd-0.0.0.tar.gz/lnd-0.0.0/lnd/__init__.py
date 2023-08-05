ccbbd3aac5c6cade0f5a706521cee5effcbdbe69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:09 +0000

    Add package lnd/0.0.0/lnd-0.0.0.tar.gz

---
+++ packages/lnd/lnd-0.0.0.tar.gz/lnd-0.0.0/lnd/__init__.py
@@ -0,0 +1,2 @@
+def foo():
+    print('will be back')
---
+++ packages/lnd/lnd-0.0.0.tar.gz/lnd-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='lnd',
+      version='0.0.0',
+      description='The lnd gRPC client',
+      url='http://github.com/',
+      author='yyforyongyu',
+      author_email='yy2452@columbia.edu',
+      license='MIT',
+      packages=['lnd'],
+      zip_safe=False)
