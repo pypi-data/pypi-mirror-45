c02076099c36a35f0a1108bb56b62f20e5156a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:23 +0000

    Add package autohandshake/1.0.2/autohandshake-1.0.2.tar.gz

---
+++ packages/autohandshake/autohandshake-1.0.2.tar.gz/autohandshake-1.0.2/autohandshake/__init__.py
@@ -0,0 +1,18 @@
+from autohandshake.src import (
+    # main classes
+    HandshakeSession,
+    HandshakeBrowser,
+
+    # pages
+    AccessRequestPage,
+    AppointmentCalendarPage,
+    InsightsPage,
+    AppointmentTypesListPage,
+    AppointmentTypePage,
+    MajorSettingsPage,
+    FileType,
+    RequestStatus
+)
+
+
+name = "autohandshake"
