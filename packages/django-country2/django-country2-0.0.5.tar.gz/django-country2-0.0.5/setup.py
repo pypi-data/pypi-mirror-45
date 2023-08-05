06269297e1992d49de6533c9c73febd9987d92e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package django-country2/0.0.5/django-country2-0.0.5.tar.gz

---
+++ packages/django-country2/django-country2-0.0.5.tar.gz/django-country2-0.0.5/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(HERE, 'README.md')).read()
+
+
+setup(
+    name='django-country2',
+    version='0.0.5',
+    url='https://github.com/Develatio/django-country2',
+    author='Develatio Technologies S.L.',
+    author_email='contacto@develat.io',
+    description=('Provides Django middleware that detects '
+                 'a country the request came from.'),
+    long_description=README,
+    packages=['django_country2'],
+    include_package_data=True,
+    scripts=[],
+    extras_require={
+        "pygeoip": ["pygeoip>=0.3.1"],
+        "django-countries": ["django-countries>=5.3.3"],
+    },
+    zip_safe=False,
+)
