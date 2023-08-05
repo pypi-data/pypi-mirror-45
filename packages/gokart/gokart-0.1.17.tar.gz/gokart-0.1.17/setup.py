a1807b1ce5a6dd63603aef7269f5eff2fbc2e08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:29 +0000

    Add package gokart/0.1.17/gokart-0.1.17.tar.gz

---
+++ packages/gokart/gokart-0.1.17.tar.gz/gokart-0.1.17/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+readme_note = """\
+.. note::
+
+   For the latest source, discussion, etc, please visit the
+   `GitHub repository <https://github.com/m3dev/gokart>`_\n\n
+"""
+
+with open('README.md') as f:
+    long_description = readme_note + f.read()
+
+install_requires = [
+    'luigi',
+    'python-dateutil==2.7.5',
+    'boto3',
+    'slackclient',
+    'pandas',
+    'numpy',
+    'tqdm',
+]
+
+setup(
+    name='gokart',
+    version='0.1.17',
+    description='A wrapper of luigi. This make it easy to define tasks.',
+    long_description=long_description,
+    author='M3, inc.',
+    url='https://github.com/m3dev/gokart',
+    license='MIT License',
+    packages=find_packages(),
+    install_requires=install_requires,
+    tests_require=['moto==1.3.6'],
+    test_suite='test',
+    classifiers=['Programming Language :: Python :: 3.6'],
+)
