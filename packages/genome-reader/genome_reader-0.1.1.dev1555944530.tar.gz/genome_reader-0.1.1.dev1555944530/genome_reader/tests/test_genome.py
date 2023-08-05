01c8131a1ef459a99d9e76241616bc69ff3f8ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:06 +0000

    Add package genome-reader/0.1.1.dev1555944530/genome_reader-0.1.1.dev1555944530.tar.gz

---
+++ packages/genome-reader/genome_reader-0.1.1.dev1555944530.tar.gz/genome_reader-0.1.1.dev1555944530/genome_reader/tests/test_genome.py
@@ -0,0 +1,26 @@
+import os
+
+from genome_reader import load
+
+
+def test_load():
+    test_dir = os.path.dirname(os.path.realpath(__file__))
+    genome = load(os.path.join(str(test_dir), 'test.txt'))
+    assert len(genome) == 10
+    assert repr(genome) == '<Genome: SNPs=10, name=\'test.txt\'>'
+    assert 'rs75333668' in genome
+    assert [i for i in genome]
+    assert len(genome.chromosome(1)) == 10
+    assert len(genome.chromosome(2)) == 0
+
+
+def test_snp(snp):
+    assert repr(snp) == '<SNP: chromosome=\'1\' position=762320 genotype=<Genotype: \'CC\'>>'
+    assert snp.chromosome == "1"
+    assert snp.position == 762320
+
+
+def test_genotype(genotype):
+    assert genotype == "CC"
+    assert repr(genotype) == '<Genotype: \'CC\'>'
+    assert str(genotype) == 'CC'
