80234dd04d5253921e85fdf0af96c9e50a012e29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:44 +0000

    Add package dipr/1.0.dev7/dipr-1.0.dev7.tar.gz

---
+++ packages/dipr/dipr-1.0.dev7.tar.gz/dipr-1.0.dev7/dipr/Utilities/Version.py
@@ -0,0 +1,8 @@
+
+DIPR_VERSION = "1.0.dev7"
+DIPR_RELEASE_DATE = "04-17-2019"
+DIPR_URL = "https://dipr.dev"
+
+
+def get_version_string():
+    return "Version: " + DIPR_VERSION + " (" + DIPR_RELEASE_DATE + ")"
