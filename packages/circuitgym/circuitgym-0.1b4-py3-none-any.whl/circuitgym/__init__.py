3dbf4639a94c50b6866ed5b4a484ae53c0b3aa09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:50 +0000

    Add package circuitgym/0.1b4/circuitgym-0.1b4-py3-none-any.whl

---
+++ packages/circuitgym/circuitgym-0.1b4-py3-none-any.whl/circuitgym/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2018 Barry Muldrey
+#
+# This file is part of circuitgym.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+__version__ = "0.1b4"  # this version is used as the definitive version for distribution
+__author__ = "Barry Muldrey"
+__copyright__ = "Copyright 2018"
+__license__ = "GNU Affero GPL"
+__maintainer__ = "Barry Muldrey"
+__email__ = "barry@muldrey.net"
+__status__ = "Alpha"
+__credits__ = []
+
+from circuitgym.envs import *
+from circuitgym.spaces import *
+from circuitgym.experience import *
