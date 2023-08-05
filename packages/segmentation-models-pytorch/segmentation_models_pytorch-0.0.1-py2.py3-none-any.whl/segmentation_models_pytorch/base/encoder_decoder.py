03db40c65105f753975ed934a043d043c9a2f22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:12 +0000

    Add package segmentation-models-pytorch/0.0.1/segmentation_models_pytorch-0.0.1-py2.py3-none-any.whl

---
+++ packages/segmentation-models-pytorch/segmentation_models_pytorch-0.0.1-py2.py3-none-any.whl/segmentation_models_pytorch/base/encoder_decoder.py
@@ -0,0 +1,35 @@
+import torch
+import torch.nn as nn
+from .model import Model
+
+
+class EncoderDecoder(Model):
+
+    def __init__(self, encoder, decoder, activation):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        
+        if callable(activation):
+            self.activation = activation
+        elif activation == 'softmax':
+            self.activation = nn.Softmax(dim=1)
+        elif activation == 'sigmoid':
+            self.activation = nn.Sigmoid()
+        else:
+            raise ValueError('Activation should be "sigmoid" or "softmax"')
+            
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+    def predict(self, x):
+        if self.training:
+            self.eval()
+
+        with torch.no_grad():
+            x = self.forward(x)
+            x = self.activation(x)
+
+        return x
