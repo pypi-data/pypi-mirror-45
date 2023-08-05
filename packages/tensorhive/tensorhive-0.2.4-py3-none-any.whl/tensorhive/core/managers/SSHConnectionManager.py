d98286be60b99e7d37a0b172c9dbf08ff6cfb1f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:53 +0000

    Add package tensorhive/0.2.4/tensorhive-0.2.4-py3-none-any.whl

---
+++ packages/tensorhive/tensorhive-0.2.4-py3-none-any.whl/tensorhive/core/managers/SSHConnectionManager.py
@@ -0,0 +1,116 @@
+
+from tensorhive.config import SSH
+from pssh.clients.native import ParallelSSHClient
+from paramiko.rsakey import RSAKey
+from typing import Dict
+from tensorhive.core.utils import ssh
+from pathlib import PosixPath
+import logging
+log = logging.getLogger(__name__)
+
+
+class SSHConnectionManager():
+    '''Responsible for configuring, establishing and holding shell sessions'''
+    _connection_group = None
+    _connection_container = {}  # type: Dict
+
+    def __init__(self, config: Dict):
+        self._connection_group = self.new_parallel_ssh_client(config)
+        self.ssh_key = self.init_ssh_key(PosixPath(SSH.KEY_FILE).expanduser())
+
+    def init_ssh_key(self, path: PosixPath):
+        if path.exists():
+            key = RSAKey.from_private_key_file(str(path))
+            log.info('Using existing SSH key in {}'.format(path))
+        else:
+            key = ssh.generate_cert(path)
+            log.info('Generated SSH key in {}'.format(path))
+        return key
+
+    @classmethod
+    def new_parallel_ssh_client(cls, config) -> ParallelSSHClient:
+        hostnames = config.keys()
+        if SSH.PROXY:
+            return ParallelSSHClient(
+                hosts=hostnames,
+                host_config=config,
+                proxy_host=SSH.PROXY['proxy_host'],
+                proxy_user=SSH.PROXY['proxy_user'],
+                proxy_port=SSH.PROXY['proxy_port']
+                # Ignore timeout and num_retires for proxy
+            )
+
+        return ParallelSSHClient(
+            hosts=hostnames,
+            host_config=config,
+            timeout=SSH.TIMEOUT,
+            num_retries=SSH.NUM_RETRIES)
+
+    def add_host(self, host_config: Dict):
+        '''
+        Appends a host (as hostname + config) directly into parallel ssh client instance.
+
+        Expected dict structure:
+        host_config = {'<some_hostname>': {'user': '<some_username>'}}
+        '''
+        hostname = [*host_config][0]
+        self.connections.hosts = [*self.connections.hosts, hostname]
+        self.connections.host_config = {
+            **self.connections.host_config, **host_config
+        }
+
+    def single_connection(self, hostname: str):
+        config = {hostname: SSH.AVAILABLE_NODES[hostname]}
+
+        if not self._connection_container.get(hostname):
+            # Create and store in cache
+            self._connection_container[hostname] = self.new_parallel_ssh_client(config)
+
+        # Return cached object
+        return self._connection_container[hostname]
+
+    @property
+    def connections(self):
+        return self._connection_group
+
+    @staticmethod
+    def test_all_connections(config):
+        '''
+        It checks if all of the defined hosts are accessible via SSH.
+        Typically runs on each TensorHive startup.
+        You can turn it off (INI config -> [ssh] -> test_on_startup = off
+        '''
+        log.info('[⚙] Testing SSH connections...')
+
+        # 1. Establish connection
+        connections = SSHConnectionManager.new_parallel_ssh_client(config)
+
+        # 2. Execute and gather output
+        command = 'uname'
+        message_template = '[{icon}] {host:20} {msg}'
+        output = connections.run_command(command, stop_on_errors=False)
+        connections.join(output)
+
+        # 3. Log appropriate messages based on command's result
+        num_failed = 0
+        for host, host_output in output.items():
+            if host_output.exception is None and host_output.exit_code == 0:
+                log.info(message_template.format(
+                    icon='✔',
+                    host=host,
+                    msg='OK'))
+            else:
+                num_failed += 1
+                error_message = 'FAILED (exit code: {}, exception: {})'.format(
+                    host_output.exit_code,
+                    host_output.exception.__class__.__name__)
+                log.critical(message_template.format(
+                    icon='✘',
+                    host=host,
+                    msg=error_message))
+
+        # 4. Show simple summary of failed connections
+        if num_failed > 0:
+            log.critical('Summary: {failed}/{all} failed to connect.'.format(
+                failed=num_failed,
+                all=len(output)))
---
+++ packages/tensorhive/tensorhive-0.2.4-py3-none-any.whl/tensorhive/core/managers/TensorHiveManager.py
@@ -0,0 +1,86 @@
+from threading import Thread
+from tensorhive.core.utils.Singleton import Singleton
+from tensorhive.core.managers.InfrastructureManager import InfrastructureManager
+from tensorhive.core.managers.SSHConnectionManager import SSHConnectionManager
+from tensorhive.core.managers.ServiceManager import ServiceManager
+from tensorhive.core.services.Service import Service
+from typing import List, Dict
+from tensorhive.core.utils.decorators.override import override
+from tensorhive.config import SSH, MONITORING_SERVICE, PROTECTION_SERVICE, USAGE_LOGGING_SERVICE
+from tensorhive.api.APIServer import APIServer
+from tensorhive.core.utils.StoppableThread import StoppableThread
+from tensorhive.core.utils.exceptions import ConfigurationException
+from tensorhive.core.monitors.Monitor import Monitor
+from tensorhive.core.monitors.GPUMonitoringBehaviour import GPUMonitoringBehaviour
+from tensorhive.core.services.MonitoringService import MonitoringService
+from tensorhive.core.services.ProtectionService import ProtectionService
+from tensorhive.core.services.UsageLoggingService import UsageLoggingService
+from tensorhive.core.violation_handlers.ProtectionHandler import ProtectionHandler
+from tensorhive.core.violation_handlers.MessageSendingBehaviour import MessageSendingBehaviour
+from tensorhive.core.violation_handlers.EmailSendingBehaviour import EmailSendingBehaviour
+import logging
+log = logging.getLogger(__name__)
+
+
+class TensorHiveManager(metaclass=Singleton):
+    """Heart of the whole engine"""
+
+    def __init__(self):
+        super().__init__()
+        self.infrastructure_manager = InfrastructureManager()
+
+        if not SSH.AVAILABLE_NODES:
+            log.error('[!] Empty ssh configuration. Please check {}'.format(SSH.HOSTS_CONFIG_FILE))
+            raise ConfigurationException
+
+        if SSH.TEST_ON_STARTUP:
+            SSHConnectionManager.test_all_connections(config=SSH.AVAILABLE_NODES)
+        self.connection_manager = SSHConnectionManager(config=SSH.AVAILABLE_NODES)
+        self.service_manager = None
+
+    @staticmethod
+    def instantiate_services_from_config() -> List[Service]:
+        '''Creates preconfigured instances of services based on config'''
+        services = []  # type: List[Service]
+        if MONITORING_SERVICE.ENABLED:
+            monitors = []
+            if MONITORING_SERVICE.ENABLE_GPU_MONITOR:
+                gpu_monitor = Monitor(GPUMonitoringBehaviour())
+                monitors.append(gpu_monitor)
+            # TODO Add more monitors here
+            monitoring_service = MonitoringService(
+                monitors=monitors,
+                interval=MONITORING_SERVICE.UPDATE_INTERVAL
+            )
+            services.append(monitoring_service)
+        if PROTECTION_SERVICE.ENABLED:
+            violation_handlers = []
+            if PROTECTION_SERVICE.NOTIFY_ON_PTY:
+                message_sending_handler = ProtectionHandler(behaviour=MessageSendingBehaviour())
+                violation_handlers.append(message_sending_handler)
+            if PROTECTION_SERVICE.NOTIFY_VIA_EMAIL:
+                email_sending_handler = ProtectionHandler(behaviour=EmailSendingBehaviour())
+                violation_handlers.append(email_sending_handler)
+            protection_service = ProtectionService(
+                handlers=violation_handlers,
+                interval=PROTECTION_SERVICE.UPDATE_INTERVAL
+            )
+            services.append(protection_service)
+        if USAGE_LOGGING_SERVICE.ENABLED:
+            usage_logging_service = UsageLoggingService(interval=USAGE_LOGGING_SERVICE.UPDATE_INTERVAL)
+            services.append(usage_logging_service)
+        return services
+
+    def configure_services_from_config(self):
+        services = self.instantiate_services_from_config()
+        self.service_manager = ServiceManager(services=services,
+                                              infrastructure_manager=self.infrastructure_manager,
+                                              connection_manager=self.connection_manager)
+
+    def init(self):
+        log.info('[⚙] Initializing services...'.format(self.__class__.__name__))
+        self.service_manager.start_all_services()
+
+    def shutdown(self):
+        log.info('[⚙] Shutting down all services...')
+        self.service_manager.shutdown_all_services()
