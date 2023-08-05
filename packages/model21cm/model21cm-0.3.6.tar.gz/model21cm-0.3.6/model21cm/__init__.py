a327d081c61fe098f0a612e61e4448bd44d0caad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:01 +0000

    Add package model21cm/0.3.6/model21cm-0.3.6.tar.gz

---
+++ packages/model21cm/model21cm-0.3.6.tar.gz/model21cm-0.3.6/model21cm/__init__.py
@@ -0,0 +1,6 @@
+from .io import Data21Cm, get_data_file_path, load_data
+from .hello import hello_world
+from .model import likelihood
+
+name = "model21cm"
+example = Data21Cm('figure1_plotdata.csv')
