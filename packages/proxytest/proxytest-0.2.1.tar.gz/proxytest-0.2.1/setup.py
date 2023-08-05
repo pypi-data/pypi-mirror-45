cdd578eda697ff5a52fab01e29b9fd097912ea61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:32 +0000

    Add package proxytest/0.2.1/proxytest-0.2.1.tar.gz

---
+++ packages/proxytest/proxytest-0.2.1.tar.gz/proxytest-0.2.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+import os
+import sys
+import warnings
+
+from setuptools import setup, find_packages
+import proxytest
+
+if sys.version_info < (3, 5):
+    warnings.warn('Python 3.5 or above is recommended!')
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='proxytest',
+    version=proxytest.__version__,
+    description='A simple script to test if one or more HTTP proxies are working by fetching a webpage.',
+    long_description=long_description,
+    classifiers=[
+        "Environment :: Console",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords='proxy test command multiple proxies',
+    author='Oleg Pryadko',
+    author_email='oleg@olegpryadko.com',
+    maintainer='Oleg Pryadko',
+    maintainer_email='oleg@olegpryadko.com',
+    url='https://github.com/yoleg/proxytest',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'proxytest = proxytest.proxytest:main',
+        ]
+    },
+    install_requires=[
+        ('aiohttp' if sys.version_info > (3, 4) else 'requests'),
+    ],
+)
