f12f2ff2864d74b372f92585a2a1cceeffd6909b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:57 +0000

    Add package spoffy/0.1.0/spoffy-0.1.0.tar.gz

---
+++ packages/spoffy/spoffy-0.1.0.tar.gz/spoffy-0.1.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['spoffy', 'spoffy.client', 'spoffy.io', 'spoffy.models', 'spoffy.modules']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['attrs>=19,<20', 'cattrs>=0.9,<0.10', 'urlobject>=2,<3']
+
+setup_kwargs = {
+    'name': 'spoffy',
+    'version': '0.1.0',
+    'description': 'Spotify API client with async and sync support',
+    'long_description': None,
+    'author': 'Steinthor Palsson',
+    'author_email': 'steini90@gmail.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
