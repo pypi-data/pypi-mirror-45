4abd25a7a6b60a3a1aa6bacdacc9561cebb5157a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:46 +0000

    Add package DevLType/0.3.1/DevLType-0.3.1.tar.gz

---
+++ packages/DevLType/DevLType-0.3.1.tar.gz/DevLType-0.3.1/DevLType/devl.py
@@ -0,0 +1,26 @@
+from . import OrderedDict,dumps
+
+class DevLTypes(object):
+
+	def to_dict(obj):
+		if isinstance(obj, (list, tuple, set)):
+			return type(obj)(to_dict(x) for x in obj if x is not None)
+		elif isinstance(obj, dict):
+			return type(obj)((to_dict(k), to_dict(v)) for k, v in obj.items() if k is not None and v is not None)
+		else:
+			return obj
+
+	def default(self):
+		return dumps(self, indent=4, default=to_json, ensure_ascii=False)
+
+	def to_json(self):
+		try:
+			content = {i: getattr(self.__dict__, i) for i in self.__dict__}
+			return to_dict(
+				OrderedDict(
+					[("_", "DevLGram." + self.__class__.__name__)]
+					+ [i for i in content.items()]
+					)
+				)
+		except AttributeError:
+			return repr(self),'error'
