af480496140482b6e1ebed0749247ab0097144bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:07 +0000

    Add package MeORM/1.16/MeORM-1.16.tar.gz

---
+++ packages/MeORM/MeORM-1.16.tar.gz/MeORM-1.16/morm/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-04-15 09:12:25
+# @Author  : Blackstone
+# @to      :
+
+
+from .Lib  import dbhelper,decorator,_gen
+from Lib.dbhelper import DBHelper
+
+
