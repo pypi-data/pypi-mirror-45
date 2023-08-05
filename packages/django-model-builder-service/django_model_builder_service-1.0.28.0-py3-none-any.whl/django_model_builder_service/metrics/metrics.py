717f417be30982f75f42585f81c00f93e0bc2877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:12 +0000

    Add package django-model-builder-service/1.0.28.0/django_model_builder_service-1.0.28.0-py3-none-any.whl

---
+++ packages/django-model-builder-service/django_model_builder_service-1.0.28.0-py3-none-any.whl/django_model_builder_service/metrics/metrics.py
@@ -0,0 +1,25 @@
+COUNTERS = 'counters'
+SHOW_MODEL = 'show_model'
+GET_MODEL = 'get_model'
+GAUGES = 'gauges'
+BUILD_TIME = 'build_time'
+ACCURACY = 'accuracy'
+ERROR_RATE = 'error_rate'
+TRAIN_SET_SIZE = 'train_set_size'
+TEST_SET_SIZE = 'test_set_size'
+DATASET_SIZE = 'dataset_size'
+
+metrics_dict = {
+    COUNTERS: {
+        SHOW_MODEL: None,
+        GET_MODEL: None
+    },
+    GAUGES: {
+        BUILD_TIME: None,
+        ACCURACY: None,
+        ERROR_RATE: None,
+        TRAIN_SET_SIZE: None,
+        TEST_SET_SIZE: None,
+        DATASET_SIZE: None
+    }
+}
