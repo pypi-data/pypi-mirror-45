eb8259ea747bce4618b1709f2c12e47f5938ff09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:32 +0000

    Add package mautrix/0.4.0.dev28/mautrix-0.4.0.dev28-py3-none-any.whl

---
+++ packages/mautrix/mautrix-0.4.0.dev28-py3-none-any.whl/mautrix/client/api/types/event/base.py
@@ -0,0 +1,92 @@
+# Copyright (c) 2018 Tulir Asokan
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from attr import dataclass
+import attr
+
+from ..primitive import RoomID, UserID, EventID
+from ..util import SerializableEnum, Obj
+
+STATE_EVENTS = ("m.room.aliases", "m.room.canonical_alias", "m.room.create", "m.room.join_rules",
+                "m.room.member", "m.room.power_levels", "m.room.name", "m.room.topic",
+                "m.room.avatar", "m.room.pinned_events")
+
+MESSAGE_EVENTS = ("m.room.redaction", "m.room.message", "m.sticker")
+
+EPHEMERAL_EVENTS = ("m.receipt", "m.typing", "m.presence")
+
+ACCOUNT_DATA_EVENTS = ("m.direct", "m.push_rules", "m.tag", "m.ignored_user_list")
+
+
+class EventType(SerializableEnum):
+    """An event type."""
+    ROOM_ALIASES = "m.room.aliases"
+    ROOM_CANONICAL_ALIAS = "m.room.canonical_alias"
+    ROOM_CREATE = "m.room.create"
+    ROOM_JOIN_RULES = "m.room.join_rules"
+    ROOM_MEMBER = "m.room.member"
+    ROOM_POWER_LEVELS = "m.room.power_levels"
+    ROOM_NAME = "m.room.name"
+    ROOM_TOPIC = "m.room.topic"
+    ROOM_AVATAR = "m.room.avatar"
+    ROOM_PINNED_EVENTS = "m.room.pinned_events"
+
+    ROOM_REDACTION = "m.room.redaction"
+    ROOM_MESSAGE = "m.room.message"
+    STICKER = "m.sticker"
+
+    RECEIPT = "m.receipt"
+    TYPING = "m.typing"
+    PRESENCE = "m.presence"
+
+    DIRECT = "m.direct"
+    PUSH_RULES = "m.push_rules"
+    TAG = "m.tag"
+    IGNORED_USER_LIST = "m.ignored_user_list"
+
+    ALL = "__ALL__"
+
+    @property
+    def is_message(self) -> bool:
+        """Whether or not the event is a message event."""
+        return self.value in EPHEMERAL_EVENTS
+
+    @property
+    def is_state(self) -> bool:
+        """Whether or not the event is a state event."""
+        return self.value in STATE_EVENTS
+
+    @property
+    def is_ephemeral(self) -> bool:
+        """Whether or not the event is ephemeral."""
+        return self.value in EPHEMERAL_EVENTS
+
+    @property
+    def is_account_data(self) -> bool:
+        """Whether or not the event is an account data event."""
+        return self.value in ACCOUNT_DATA_EVENTS
+
+
+@dataclass
+class BaseUnsigned:
+    """Base unsigned information."""
+    age: int = None
+
+
+@dataclass
+class BaseEvent:
+    """Base event class. The only things an event **must** have are content and event type."""
+    content: Obj
+    type: EventType
+
+
+@dataclass
+class BaseRoomEvent(BaseEvent):
+    """Base room event class. Room events must have a room ID, event ID, sender and timestamp in
+    addition to the content and type in the base event."""
+    room_id: RoomID
+    event_id: EventID
+    sender: UserID
+    timestamp: int = attr.ib(metadata={"json": "origin_server_ts"})
---
+++ packages/mautrix/mautrix-0.4.0.dev28-py3-none-any.whl/mautrix/types.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2018 Tulir Asokan
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from .api.http import JSON, Method, APIPath
+
+from .client.api.types import (
+    UserID, EventID, RoomID, RoomAlias, FilterID, ContentURI, SyncToken,
+
+    Filter, EventFilter, RoomFilter, RoomEventFilter,
+
+    EventType, RedactionEvent, RedactionEventContent,
+    MessageEvent, MessageEventContent, MessageUnsigned, MediaMessageEventContent,
+    LocationMessageEventContent, LocationInfo, InReplyTo, MessageType, Format, MediaInfo, FileInfo,
+    AudioInfo, VideoInfo, ImageInfo, ThumbnailInfo, TextMessageEventContent, MatchedPassiveCommand,
+    BaseMessageEventContent, MatchedCommand, RelatesTo, PowerLevelStateEventContent, Membership,
+    MemberStateEventContent, StateEvent, AliasesStateEventContent, CanonicalAliasStateEventContent,
+    StrippedStateEvent, RoomNameStateEventContent, RoomTopicStateEventContent, StateUnsigned,
+    RoomAvatarStateEventContent, StateEventContent, AccountDataEvent, AccountDataEventContent,
+    RoomTagInfo, RoomTagAccountDataEventContent, Event, EventContent, BaseFileInfo,
+
+    User, Member, UserSearchResults,
+
+    MediaRepoConfig, MXOpenGraph, OpenGraphVideo, OpenGraphImage, OpenGraphAudio,
+
+    SerializerError, Serializable, SerializableEnum, SerializableAttrs, serializer, deserializer)
