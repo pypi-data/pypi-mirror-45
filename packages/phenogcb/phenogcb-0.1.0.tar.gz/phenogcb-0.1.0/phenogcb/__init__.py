91f9d459e79dc1aaee2ed4c7d622a496fad62297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:06 +0000

    Add package phenogcb/0.1.0/phenogcb-0.1.0.tar.gz

---
+++ packages/phenogcb/phenogcb-0.1.0.tar.gz/phenogcb-0.1.0/phenogcb/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+from IPython.display import display, Math
+import sympy as sp
+sp.init_printing(use_latex=True)
+from ipywidgets import *
+import thermo as th
+import fluids as fl
+from fluids.units import *
+import ht as ht
---
+++ packages/phenogcb/phenogcb-0.1.0.tar.gz/phenogcb-0.1.0/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="phenogcb",
+    version="0.1.0",
+    author="Pierre Proulx",
+    author_email="pierre.proulx@usherbrooke.ca",
+    description="Pheno GCB",
+    long_description="Pheno GCB",
+    long_description_content_type="text/markdown",
+    url="https://catalyseur.ca",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
