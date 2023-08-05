374298bd0909f345d3c8b45bbb5298dd7f460bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:22 +0000

    Add package py-zap/1.2.2/py_zap-1.2.2.tar.gz

---
+++ packages/py-zap/py_zap-1.2.2.tar.gz/py_zap-1.2.2/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='py_zap',
+    version='1.2.2',
+    description='Python scraper for accessing ratings from tvbythenumbers.zap2it.com',
+    long_description=readme(),
+    author='sharibarboza',
+    author_email='barbozashari@gmail.com',
+    url='https://github.com/sharibarboza/py_zap',
+    download_url='https://github.com/sharibarboza/py_zap/archive/1.0.0.tar.gz',
+    keywords=['zap2it', 'ratings', 'tv'],
+    license='MIT License',
+    packages=['py_zap'],
+    install_requires=[
+        'beautifulsoup4',
+        'requests>=2.9.1'
+    ]
+)
