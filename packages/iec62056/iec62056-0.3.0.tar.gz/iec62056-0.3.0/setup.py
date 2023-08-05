79d39e663433672106b2b51e4a744724c655b920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:50 +0000

    Add package iec62056/0.3.0/iec62056-0.3.0.tar.gz

---
+++ packages/iec62056/iec62056-0.3.0.tar.gz/iec62056-0.3.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+long_description = open('README.md', 'r').read()
+
+setup(
+	author='Jorik Jonker',
+	author_email='jorik@kippendief.biz',
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+		'Topic :: Software Development :: Libraries',
+	],
+	description='Library to parse IEC62056/IEC1107/DSMR telegrams',
+	include_package_data=True,
+	install_requires=[
+		'lark-parser==0.7.0',
+	],
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	name='iec62056',
+	packages=find_packages(),
+	url='https://github.com/jonkerj/iec62056',
+	version='0.3.0',
+)
