d845f25dad69fba54c1343965b05a62a079081fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:05 +0000

    Add package pydarts/1.1.3/pydarts-1.1.3.tar.gz

---
+++ packages/pydarts/pydarts-1.1.3.tar.gz/pydarts-1.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, Extension
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    ext_modules = [
+        Extension('pydarts', sources=['pydarts.cpp'])
+    ]
+else:
+    ext_modules = cythonize(['pydarts.pyx'])
+
+setup(
+    name="pydarts",
+    version="1.1.3",
+    description="Python binding of Darts",
+    author="JX PRESS Corp.",
+    author_email="info@jxpress.net",
+    url="https://github.com/jxpress/pydarts",
+    include_package_data=True,
+    ext_modules=ext_modules,
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3"
+    ]
+)
