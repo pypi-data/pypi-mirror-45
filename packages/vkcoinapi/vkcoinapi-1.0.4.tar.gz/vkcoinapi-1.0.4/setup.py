9cf5f17465e1ad8d17de482eba6e0764eed6f578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:45 +0000

    Add package vkcoinapi/1.0.4/vkcoinapi-1.0.4.tar.gz

---
+++ packages/vkcoinapi/vkcoinapi-1.0.4.tar.gz/vkcoinapi-1.0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'vkcoinapi',
+    version = '1.0.4',
+    packages = find_packages(),
+    url = 'https://github.com/bixnel/vkcoinapi',
+    license = 'MIT',
+)
