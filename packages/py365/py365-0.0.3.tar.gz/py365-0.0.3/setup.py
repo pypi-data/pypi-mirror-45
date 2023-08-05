362c5d7619d2dc934152fdc64d6c8d11cd2dec0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:49 +0000

    Add package py365/0.0.3/py365-0.0.3.tar.gz

---
+++ packages/py365/py365-0.0.3.tar.gz/py365-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+VERSION = '0.0.3'
+
+CLASSIFIERS = [
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Operating System :: OS Independent',
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py365",
+    version=VERSION,
+    author="Dudi Levy",
+    author_email="dudil@users.noreply.github.com",
+    description="Python Library for App Development over MS Open Graph ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dudil/py365",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    classifiers=CLASSIFIERS,
+)
