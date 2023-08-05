feeabb33a609b8ebe68016ed57ef0a36d09ec9b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:59 +0000

    Add package nester-benny/1.1.0/nester_benny-1.1.0.tar.gz

---
+++ packages/nester-benny/nester_benny-1.1.0.tar.gz/nester_benny-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+       name         ='nester_benny',
+       version      ='1.1.0',
+       py_modules   =['nester_benny'],
+       author       ='benny',
+       author_email ='benny08118@gmail.com',
+       url          ='',
+       description  ='A simple printer of nested lists',
+
+	)
