f301b684c6dac4aee1f8690ae6dd362632fb82d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:34 +0000

    Add package pspring-aws/0.0.17/pspring-aws-0.0.17.tar.gz

---
+++ packages/pspring-aws/pspring-aws-0.0.17.tar.gz/pspring-aws-0.0.17/pspringaws/dynamodb.py
@@ -0,0 +1,48 @@
+import boto3
+
+from .defaultvars import *
+
+import time
+
+class DynamoDBTable():
+    def __init__(self,*args,**kargs):
+        self.tableName = kargs.get("tableName")
+        self.primaryKey = kargs.get("primaryKey")
+        self.sortKey = kargs.get("sortKey")
+        self.ttlcolumnname = kargs.get("ttlColumnName","ttl")
+        self.ttl = int(kargs.get("ttl",defaultTtl))
+        dynamodb = boto3.resource("dynamodb",region_name=region)
+        self.table = dynamodb.Table(self.tableName)
+
+    def __call__(self,classObj):
+        def put(selfObj,data):
+            data[self.ttlcolumnname] = int(time.time())+self.ttl
+            self.table.put_item(Item=data)
+
+        def get(selfObj,primaryKey,**kargs):
+            key = {}
+            key[self.primaryKey] = primaryKey
+            sortKey = kargs.get(self.sortKey,None)
+            columns = kargs.get("columns",None)
+            column = kargs.get("column",None)
+            if self.sortKey != None and sortKey != None:
+                key.update({
+                    self.sortKey : sortKey
+                })
+
+            response = self.table.get_item(Key=key)
+            responseData = {}
+            item = response.get("Item")
+            if item != None:
+                if columns != None:
+                    for column in columns:
+                        responseData.update({
+                            column : item.get(column)
+                        })
+                    return responseData
+                if column != None:
+                    return item.get(column)
+            return None
+        classObj.put = put
+        classObj.get = get
+        return classObj
---
+++ packages/pspring-aws/pspring-aws-0.0.17.tar.gz/pspring-aws-0.0.17/pspringaws/pspringaws.py
@@ -0,0 +1,5 @@
+from .secretsmngr import SecretsManager
+from .lambdaexceptions import *
+from .lambdahandler import *
+from .defaultvars import *
+from .dynamodb import *
