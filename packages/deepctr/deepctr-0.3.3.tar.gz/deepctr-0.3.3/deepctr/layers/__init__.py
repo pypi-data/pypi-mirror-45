3bff7ea9aed26d5a9dbca113d8a7ca8cc607dec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:29 +0000

    Add package deepctr/0.3.3/deepctr-0.3.3.tar.gz

---
+++ packages/deepctr/deepctr-0.3.3.tar.gz/deepctr-0.3.3/deepctr/layers/__init__.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+
+from .activation import Dice
+from .core import MLP, LocalActivationUnit, PredictionLayer
+from .interaction import (CIN, FM, AFMLayer, BiInteractionPooling, CrossNet,
+                          InnerProductLayer, InteractingLayer,
+                          OutterProductLayer)
+from .normalization import LayerNormalization
+from .sequence import (AttentionSequencePoolingLayer, BiasEncoding, BiLSTM,
+                       KMaxPooling, Position_Embedding, SequencePoolingLayer,
+                       Transformer)
+from .utils import NoMask
+
+custom_objects = {'tf': tf,
+                  'InnerProductLayer': InnerProductLayer,
+                  'OutterProductLayer': OutterProductLayer,
+                  'MLP': MLP,
+                  'PredictionLayer': PredictionLayer,
+                  'FM': FM,
+                  'AFMLayer': AFMLayer,
+                  'CrossNet': CrossNet,
+                  'BiInteractionPooling': BiInteractionPooling,
+                  'LocalActivationUnit': LocalActivationUnit,
+                  'Dice': Dice,
+                  'SequencePoolingLayer': SequencePoolingLayer,
+                  'AttentionSequencePoolingLayer': AttentionSequencePoolingLayer,
+                  'CIN': CIN,
+                  'InteractingLayer': InteractingLayer,
+                  'LayerNormalization': LayerNormalization,
+                  'BiLSTM': BiLSTM,
+                  'Transformer': Transformer,
+                  'NoMask': NoMask,
+                  'BiasEncoding': BiasEncoding,
+                  'KMaxPooling': KMaxPooling}
