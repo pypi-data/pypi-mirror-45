5571b4f59d85cc739fe628560b0526ee23bf51ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:49 +0000

    Add package aglink/1.45/aglink-1.45.tar.gz

---
+++ packages/aglink/aglink-1.45.tar.gz/aglink-1.45/aglink/__init__.py
@@ -0,0 +1,9 @@
+import imp
+version = imp.load_source('version', "../__version__.py")
+__version__ 	= version
+__test__    	= '0.6'
+__build__		= '2.7'
+__platform__	= 'all'
+__email__		= "licface@yahoo.com"
+
+from .agl import autogeneratelink
