478a69410c3ee2e720387df3df9c6643584fd0bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:54 +0000

    Add package havedocker/0.1.3.6/havedocker-0.1.3.6.tar.gz

---
+++ packages/havedocker/havedocker-0.1.3.6.tar.gz/havedocker-0.1.3.6/havedocker/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+
+from .Docker import Agility_Docker
+from .DockerError import *
+
+name = "havedocker"
+version = '0.1.3.5'
+author = 'haitanghuadeng'
+project = 'https://github.com/haitanghuadeng/havedocker'
+description = "Improved foundation version 0.1.3.6"
+
