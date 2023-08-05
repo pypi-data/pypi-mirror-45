1e46ffd2dfa69f91acecd0cce09b5be45dd976ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:22 +0000

    Add package hackingtools/0.9.82/hackingtools-0.9.82-py3-none-any.whl

---
+++ packages/hackingtools/hackingtools-0.9.82-py3-none-any.whl/hackingtools/modules/web/des/ht_des.py
@@ -0,0 +1,10 @@
+from hackingtools.core import Logger
+
+class StartModule():
+
+	def __init__(self):
+		Logger.printMessage(message='ht_des loaded', debug_module=True)
+		pass
+
+	def hola(self, nombre, edad):
+		pass
