0afd3c366a9a229dabfd016bf26aacefc6666dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:35 +0000

    Add package mlflow/0.9.1/mlflow-0.9.1.tar.gz

---
+++ packages/mlflow/mlflow-0.9.1.tar.gz/mlflow-0.9.1/mlflow/cli.py
@@ -0,0 +1,268 @@
+from __future__ import print_function
+
+import json
+import os
+import sys
+import logging
+
+import click
+from click import UsageError
+
+import mlflow.azureml.cli
+import mlflow.projects as projects
+import mlflow.data
+import mlflow.experiments
+import mlflow.pyfunc.cli
+import mlflow.rfunc.cli
+import mlflow.sagemaker.cli
+import mlflow.runs
+
+from mlflow.tracking.utils import _is_local_uri
+from mlflow.utils.logging_utils import eprint
+from mlflow.utils.process import ShellCommandException
+from mlflow.utils import cli_args
+from mlflow.server import _run_server
+from mlflow.store import DEFAULT_LOCAL_FILE_AND_ARTIFACT_PATH
+from mlflow import tracking
+import mlflow.store.cli
+
+
+_logger = logging.getLogger(__name__)
+
+
+@click.group()
+@click.version_option()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("uri")
+@click.option("--entry-point", "-e", metavar="NAME", default="main",
+              help="Entry point within project. [default: main]. If the entry point is not found, "
+                   "attempts to run the project file with the specified name as a script, "
+                   "using 'python' to run .py files and the default shell (specified by "
+                   "environment variable $SHELL) to run .sh files")
+@click.option("--version", "-v", metavar="VERSION",
+              help="Version of the project to run, as a Git commit reference for Git projects.")
+@click.option("--param-list", "-P", metavar="NAME=VALUE", multiple=True,
+              help="A parameter for the run, of the form -P name=value. Provided parameters that "
+                   "are not in the list of parameters for an entry point will be passed to the "
+                   "corresponding entry point as command-line arguments in the form `--name value`")
+@click.option("--experiment-name", envvar=tracking._EXPERIMENT_NAME_ENV_VAR,
+              help="Name of the experiment under which to launch the run. If not "
+                   "specified, 'experiment-id' option will be used to launch run.")
+@click.option("--experiment-id", envvar=tracking._EXPERIMENT_ID_ENV_VAR, type=click.STRING,
+              help="ID of the experiment under which to launch the run.")
+# TODO: Add tracking server argument once we have it working.
+@click.option("--mode", "-m", metavar="MODE",
+              help="Execution mode to use for run. Supported values: 'local' (runs project "
+                   "locally) and 'databricks' (runs project on a Databricks cluster). "
+                   "Defaults to 'local'. If running against Databricks, will run against a "
+                   "Databricks workspace determined as follows: if a Databricks tracking URI "
+                   "of the form 'databricks://profile' has been set (e.g. by setting "
+                   "the MLFLOW_TRACKING_URI environment variable), will run against the "
+                   "workspace specified by <profile>. Otherwise, runs against the workspace "
+                   "specified by the default Databricks CLI profile. See "
+                   "https://github.com/databricks/databricks-cli for more info on configuring a "
+                   "Databricks CLI profile.")
+@click.option("--cluster-spec", "-c", metavar="FILE",
+              help="Path to JSON file (must end in '.json') or JSON string describing the cluster "
+                   "to use when launching a run on Databricks. See "
+                   "https://docs.databricks.com/api/latest/jobs.html#jobsclusterspecnewcluster for "
+                   "more info. Note that MLflow runs are currently launched against a new cluster.")
+@click.option("--git-username", metavar="USERNAME", envvar="MLFLOW_GIT_USERNAME",
+              help="Username for HTTP(S) Git authentication.")
+@click.option("--git-password", metavar="PASSWORD", envvar="MLFLOW_GIT_PASSWORD",
+              help="Password for HTTP(S) Git authentication.")
+@cli_args.NO_CONDA
+@click.option("--storage-dir", envvar="MLFLOW_TMP_DIR",
+              help="Only valid when `mode` is local."
+                   "MLflow downloads artifacts from distributed URIs passed to parameters of "
+                   "type 'path' to subdirectories of storage_dir.")
+@click.option("--run-id", metavar="RUN_ID",
+              help="If specified, the given run ID will be used instead of creating a new run. "
+                   "Note: this argument is used internally by the MLflow project APIs "
+                   "and should not be specified.")
+def run(uri, entry_point, version, param_list, experiment_name, experiment_id, mode, cluster_spec,
+        git_username, git_password, no_conda, storage_dir, run_id):
+    """
+    Run an MLflow project from the given URI.
+
+    For local runs, the run will block until it completes.
+    Otherwise, the project will run asynchronously.
+
+    If running locally (the default), the URI can be either a Git repository URI or a local path.
+    If running on Databricks, the URI must be a Git repository.
+
+    By default, Git projects run in a new working directory with the given parameters, while
+    local projects run from the project's root directory.
+    """
+    if experiment_id is not None and experiment_name is not None:
+        eprint("Specify only one of 'experiment-name' or 'experiment-id' options.")
+        sys.exit(1)
+
+    param_dict = {}
+    for s in param_list:
+        index = s.find("=")
+        if index == -1:
+            eprint("Invalid format for -P parameter: '%s'. Use -P name=value." % s)
+            sys.exit(1)
+        name = s[:index]
+        value = s[index + 1:]
+        if name in param_dict:
+            eprint("Repeated parameter: '%s'" % name)
+            sys.exit(1)
+        param_dict[name] = value
+    cluster_spec_arg = cluster_spec
+    if cluster_spec is not None and os.path.splitext(cluster_spec)[-1] != ".json":
+        try:
+            cluster_spec_arg = json.loads(cluster_spec)
+        except ValueError as e:
+            eprint("Invalid cluster spec JSON. Parse error: %s" % e)
+            raise
+    try:
+        projects.run(
+            uri,
+            entry_point,
+            version,
+            experiment_name=experiment_name,
+            experiment_id=experiment_id,
+            parameters=param_dict,
+            mode=mode,
+            cluster_spec=cluster_spec_arg,
+            git_username=git_username,
+            git_password=git_password,
+            use_conda=(not no_conda),
+            storage_dir=storage_dir,
+            block=mode == "local" or mode is None,
+            run_id=run_id,
+        )
+    except projects.ExecutionException as e:
+        _logger.error("=== %s ===", e)
+        sys.exit(1)
+
+
+@cli.command()
+@click.option("--backend-store-uri", "--file-store", metavar="PATH",
+              default=DEFAULT_LOCAL_FILE_AND_ARTIFACT_PATH,
+              help="URI or path for backend store implementation. Acceptable backend store "
+                   "are SQLAlchemy compatible implementation or local storage. "
+                   "Example 'sqlite:///path/to/file.db'. "
+                   "By default file backed store will be used. (default: ./mlruns).")
+@click.option("--default-artifact-root", metavar="URI", default=None,
+              help="Path to local directory to store artifacts, for new experiments. "
+                   "Note that this flag does not impact already-created experiments. "
+                   "Default: " + DEFAULT_LOCAL_FILE_AND_ARTIFACT_PATH)
+@click.option("--host", "-h", metavar="HOST", default="127.0.0.1",
+              help="The network address to listen on (default: 127.0.0.1). "
+                   "Use 0.0.0.0 to bind to all addresses if you want to access the UI from "
+                   "other machines.")
+@click.option("--port", "-p", default=5000,
+              help="The port to listen on (default: 5000).")
+@click.option("--gunicorn-opts", default=None,
+              help="Additional command line options forwarded to gunicorn processes.")
+def ui(backend_store_uri, default_artifact_root, host, port, gunicorn_opts):
+    """
+    Launch the MLflow tracking UI.
+
+    The UI will be visible at http://localhost:5000 by default.
+    """
+    # Ensure that both backend_store_uri and default_artifact_uri are set correctly.
+    if not backend_store_uri:
+        backend_store_uri = DEFAULT_LOCAL_FILE_AND_ARTIFACT_PATH
+
+    if not default_artifact_root:
+        if _is_local_uri(backend_store_uri):
+            default_artifact_root = backend_store_uri
+        else:
+            default_artifact_root = DEFAULT_LOCAL_FILE_AND_ARTIFACT_PATH
+
+    # TODO: We eventually want to disable the write path in this version of the server.
+    try:
+        _run_server(backend_store_uri, default_artifact_root, host, port, 1, None, gunicorn_opts)
+    except ShellCommandException:
+        eprint("Running the mlflow server failed. Please see the logs above for details.")
+        sys.exit(1)
+
+
+def _validate_static_prefix(ctx, param, value):  # pylint: disable=unused-argument
+    """
+    Validate that the static_prefix option starts with a "/" and does not end in a "/".
+    Conforms to the callback interface of click documented at
+    http://click.pocoo.org/5/options/#callbacks-for-validation.
+    """
+    if value is not None:
+        if not value.startswith("/"):
+            raise UsageError("--static-prefix must begin with a '/'.")
+        if value.endswith("/"):
+            raise UsageError("--static-prefix should not end with a '/'.")
+    return value
+
+
+@cli.command()
+@click.option("--backend-store-uri", "--file-store", metavar="PATH",
+              default=DEFAULT_LOCAL_FILE_AND_ARTIFACT_PATH,
+              help="URI or path for backend store implementation. Acceptable backend store "
+                   "are SQLAlchemy compatible implementation or local storage. Supports "
+                   "various SQLAlchemy compatible database like SQLite, MySQL, PostgreSQL. As an "
+                   "example MySQL backed store can be configured using connection string. "
+                   "'mysql://<user_name>:<password>@<host>:<port>/<database_name>' "
+                   "By default file based backed store will be used. (default: ./mlruns).")
+@click.option("--default-artifact-root", metavar="URI", default=None,
+              help="Local or S3 URI to store artifacts, for new experiments. "
+                   "Note that this flag does not impact already-created experiments. "
+                   "Default: Within file store")
+@click.option("--host", "-h", metavar="HOST", default="127.0.0.1",
+              help="The network address to listen on (default: 127.0.0.1). "
+                   "Use 0.0.0.0 to bind to all addresses if you want to access the tracking "
+                   "server from other machines.")
+@click.option("--port", "-p", default=5000,
+              help="The port to listen on (default: 5000).")
+@click.option("--workers", "-w", default=4,
+              help="Number of gunicorn worker processes to handle requests (default: 4).")
+@click.option("--static-prefix", default=None, callback=_validate_static_prefix,
+              help="A prefix which will be prepended to the path of all static paths.")
+@click.option("--gunicorn-opts", default=None,
+              help="Additional command line options forwarded to gunicorn processes.")
+def server(backend_store_uri, default_artifact_root, host, port,
+           workers, static_prefix, gunicorn_opts):
+    """
+    Run the MLflow tracking server.
+
+    The server which listen on http://localhost:5000 by default, and only accept connections from
+    the local machine. To let the server accept connections from other machines, you will need to
+    pass --host 0.0.0.0 to listen on all network interfaces (or a specific interface address).
+    """
+
+    # Ensure that both backend_store_uri and default_artifact_uri are set correctly.
+    if not backend_store_uri:
+        backend_store_uri = DEFAULT_LOCAL_FILE_AND_ARTIFACT_PATH
+
+    if not default_artifact_root:
+        if _is_local_uri(backend_store_uri):
+            default_artifact_root = backend_store_uri
+        else:
+            eprint("Option 'default-artifact-root' is required, when backend store is not "
+                   "local file based.")
+            sys.exit(1)
+
+    try:
+        _run_server(backend_store_uri, default_artifact_root, host, port, workers, static_prefix,
+                    gunicorn_opts)
+    except ShellCommandException:
+        eprint("Running the mlflow server failed. Please see the logs above for details.")
+        sys.exit(1)
+
+
+cli.add_command(mlflow.data.download)
+cli.add_command(mlflow.pyfunc.cli.commands)
+cli.add_command(mlflow.rfunc.cli.commands)
+cli.add_command(mlflow.sagemaker.cli.commands)
+cli.add_command(mlflow.experiments.commands)
+cli.add_command(mlflow.store.cli.commands)
+cli.add_command(mlflow.azureml.cli.commands)
+cli.add_command(mlflow.runs.commands)
+
+if __name__ == '__main__':
+    cli()
---
+++ packages/mlflow/mlflow-0.9.1.tar.gz/mlflow-0.9.1/mlflow/tracking/client.py
@@ -0,0 +1,303 @@
+"""
+Internal package providing a Python CRUD interface to MLflow experiments and runs.
+This is a lower level API than the :py:mod:`mlflow.tracking.fluent` module, and is
+exposed in the :py:mod:`mlflow.tracking` module.
+"""
+
+import os
+import time
+from six import iteritems
+
+from mlflow.tracking import utils
+from mlflow.utils.search_utils import SearchFilter
+from mlflow.utils.validation import _validate_param_name, _validate_tag_name, _validate_run_id, \
+    _validate_experiment_name, _validate_metric
+from mlflow.entities import Param, Metric, RunStatus, RunTag, ViewType, SourceType
+from mlflow.store.artifact_repository_registry import get_artifact_repository
+from mlflow.utils.mlflow_tags import MLFLOW_SOURCE_NAME, MLFLOW_SOURCE_TYPE, MLFLOW_PARENT_RUN_ID, \
+    MLFLOW_GIT_COMMIT, MLFLOW_PROJECT_ENTRY_POINT
+
+_DEFAULT_USER_ID = "unknown"
+
+
+class MlflowClient(object):
+    """Client of an MLflow Tracking Server that creates and manages experiments and runs.
+    """
+
+    def __init__(self, tracking_uri=None):
+        """
+        :param tracking_uri: Address of local or remote tracking server. If not provided, defaults
+                             to the service set by ``mlflow.tracking.set_tracking_uri``. See
+                             `Where Runs Get Recorded <../tracking.html#where-runs-get-recorded>`_
+                             for more info.
+        """
+        self.tracking_uri = tracking_uri or utils.get_tracking_uri()
+        self.store = utils._get_store(self.tracking_uri)
+
+    def get_run(self, run_id):
+        """
+        Fetch the run from backend store. The resulting :py:class:`Run <mlflow.entities.Run>`
+        contains a collection of run metadata - :py:class:`RunInfo <mlflow.entities.RunInfo>`,
+        as well as a collection of run parameters, tags, and metrics -
+        :py:class`RunData <mlflow.entities.RunData>`. In the case where multiple metrics with the
+        same key are logged for the run, the :py:class:`RunData <mlflow.entities.RunData>` contains
+        the value at the latest timestamp for each metric. If there are multiple values with the
+        latest timestamp for a given metric, the maximum of these values is returned.
+
+        :param run_uuid: Unique identifier for the run.
+
+        :return: A single :py:class:`mlflow.entities.Run` object, if the run exists. Otherwise,
+                 raises an exception.
+        """
+        _validate_run_id(run_id)
+        return self.store.get_run(run_id)
+
+    def create_run(self, experiment_id, user_id=None, run_name=None, start_time=None,
+                   parent_run_id=None, tags=None):
+        """
+        Create a :py:class:`mlflow.entities.Run` object that can be associated with
+        metrics, parameters, artifacts, etc.
+        Unlike :py:func:`mlflow.projects.run`, creates objects but does not run code.
+        Unlike :py:func:`mlflow.start_run`, does not change the "active run" used by
+        :py:func:`mlflow.log_param`.
+
+        :param user_id: If not provided, use the current user as a default.
+        :param start_time: If not provided, use the current timestamp.
+        :param parent_run_id Optional parent run ID - takes precedence over parent run ID included
+                             in the `tags` argument.
+        :param tags: A dictionary of key-value pairs that are converted into
+                     :py:class:`mlflow.entities.RunTag` objects.
+        :return: :py:class:`mlflow.entities.Run` that was created.
+        """
+
+        tags = tags if tags else {}
+
+        # Extract run attributes from tags
+        # This logic is temporary; by the 1.0 release, this information will only be stored in tags
+        # and will not be available as attributes of the run
+        final_parent_run_id =\
+            tags.get(MLFLOW_PARENT_RUN_ID) if parent_run_id is None else parent_run_id
+        source_name = tags.get(MLFLOW_SOURCE_NAME, "Python Application")
+        source_version = tags.get(MLFLOW_GIT_COMMIT)
+        entry_point_name = tags.get(MLFLOW_PROJECT_ENTRY_POINT)
+
+        source_type_string = tags.get(MLFLOW_SOURCE_TYPE)
+        if source_type_string is None:
+            source_type = SourceType.LOCAL
+        else:
+            source_type = SourceType.from_string(source_type_string)
+
+        return self.store.create_run(
+            experiment_id=experiment_id,
+            user_id=user_id if user_id is not None else _get_user_id(),
+            run_name=run_name,
+            start_time=start_time or int(time.time() * 1000),
+            tags=[RunTag(key, value) for (key, value) in iteritems(tags)],
+            # The below arguments remain set for backwards compatability:
+            parent_run_id=final_parent_run_id,
+            source_type=source_type,
+            source_name=source_name,
+            entry_point_name=entry_point_name,
+            source_version=source_version
+        )
+
+    def list_run_infos(self, experiment_id, run_view_type=ViewType.ACTIVE_ONLY):
+        """:return: List of :py:class:`mlflow.entities.RunInfo`"""
+        return self.store.list_run_infos(experiment_id, run_view_type)
+
+    def list_experiments(self):
+        """:return: List of :py:class:`mlflow.entities.Experiment`"""
+        return self.store.list_experiments()
+
+    def get_experiment(self, experiment_id):
+        """
+        :param experiment_id: The experiment ID returned from ``create_experiment``.
+        :return: :py:class:`mlflow.entities.Experiment`
+        """
+        return self.store.get_experiment(experiment_id)
+
+    def get_experiment_by_name(self, name):
+        """
+        :param name: The experiment name.
+        :return: :py:class:`mlflow.entities.Experiment`
+        """
+        return self.store.get_experiment_by_name(name)
+
+    def create_experiment(self, name, artifact_location=None):
+        """Create an experiment.
+
+        :param name: The experiment name. Must be unique.
+        :param artifact_location: The location to store run artifacts.
+                                  If not provided, the server picks an appropriate default.
+        :return: Integer ID of the created experiment.
+        """
+        _validate_experiment_name(name)
+        return self.store.create_experiment(
+            name=name,
+            artifact_location=artifact_location,
+        )
+
+    def delete_experiment(self, experiment_id):
+        """
+        Delete an experiment from the backend store.
+
+        :param experiment_id: The experiment ID returned from ``create_experiment``.
+        """
+        self.store.delete_experiment(experiment_id)
+
+    def restore_experiment(self, experiment_id):
+        """
+        Restore a deleted experiment unless permanently deleted.
+
+        :param experiment_id: The experiment ID returned from ``create_experiment``.
+        """
+        self.store.restore_experiment(experiment_id)
+
+    def rename_experiment(self, experiment_id, new_name):
+        """
+        Update an experiment's name. The new name must be unique.
+
+        :param experiment_id: The experiment ID returned from ``create_experiment``.
+        """
+        self.store.rename_experiment(experiment_id, new_name)
+
+    def log_metric(self, run_id, key, value, timestamp=None):
+        """
+        Log a metric against the run ID. If timestamp is not provided, uses
+        the current timestamp.
+        """
+        timestamp = timestamp if timestamp is not None else int(time.time())
+        _validate_metric(key, value, timestamp)
+        metric = Metric(key, value, timestamp)
+        self.store.log_metric(run_id, metric)
+
+    def log_param(self, run_id, key, value):
+        """
+        Log a parameter against the run ID. Value is converted to a string.
+        """
+        _validate_param_name(key)
+        param = Param(key, str(value))
+        self.store.log_param(run_id, param)
+
+    def set_tag(self, run_id, key, value):
+        """
+        Set a tag on the run ID. Value is converted to a string.
+        """
+        _validate_tag_name(key)
+        tag = RunTag(key, str(value))
+        self.store.set_tag(run_id, tag)
+
+    def log_batch(self, run_id, metrics, params, tags):
+        """
+        Log multiple metrics, params, and/or tags.
+
+        :param metrics: List of Metric(key, value, timestamp) instances.
+        :param params: List of Param(key, value) instances.
+        :param tags: List of RunTag(key, value) instances.
+
+        Raises an MlflowException if any errors occur.
+        :returns: None
+        """
+        for metric in metrics:
+            _validate_metric(metric.key, metric.value, metric.timestamp)
+        for param in params:
+            _validate_param_name(param.key)
+        for tag in tags:
+            _validate_tag_name(tag.key)
+        self.store.log_batch(run_id=run_id, metrics=metrics, params=params, tags=tags)
+
+    def log_artifact(self, run_id, local_path, artifact_path=None):
+        """
+        Write a local file to the remote ``artifact_uri``.
+
+        :param local_path: Path to the file to write.
+        :param artifact_path: If provided, the directory in ``artifact_uri`` to write to.
+        """
+        run = self.get_run(run_id)
+        artifact_repo = get_artifact_repository(run.info.artifact_uri, self.store)
+        artifact_repo.log_artifact(local_path, artifact_path)
+
+    def log_artifacts(self, run_id, local_dir, artifact_path=None):
+        """
+        Write a directory of files to the remote ``artifact_uri``.
+
+        :param local_dir: Path to the directory of files to write.
+        :param artifact_path: If provided, the directory in ``artifact_uri`` to write to.
+        """
+        run = self.get_run(run_id)
+        artifact_repo = get_artifact_repository(run.info.artifact_uri, self.store)
+        artifact_repo.log_artifacts(local_dir, artifact_path)
+
+    def list_artifacts(self, run_id, path=None):
+        """
+        List the artifacts for a run.
+
+        :param run_id: The run to list artifacts from.
+        :param path: The run's relative artifact path to list from. By default it is set to None
+                     or the root artifact path.
+        :return: List of :py:class:`mlflow.entities.FileInfo`
+        """
+        run = self.get_run(run_id)
+        artifact_root = run.info.artifact_uri
+        artifact_repo = get_artifact_repository(artifact_root, self.store)
+        return artifact_repo.list_artifacts(path)
+
+    def download_artifacts(self, run_id, path):
+        """
+        Download an artifact file or directory from a run to a local directory if applicable,
+        and return a local path for it.
+
+        :param run_id: The run to download artifacts from.
+        :param path: Relative source path to the desired artifact.
+        :return: Local path of desired artifact.
+        """
+        run = self.get_run(run_id)
+        artifact_root = run.info.artifact_uri
+        artifact_repo = get_artifact_repository(artifact_root, self.store)
+        return artifact_repo.download_artifacts(path)
+
+    def set_terminated(self, run_id, status=None, end_time=None):
+        """Set a run's status to terminated.
+
+        :param status: A string value of :py:class:`mlflow.entities.RunStatus`.
+                       Defaults to "FINISHED".
+        :param end_time: If not provided, defaults to the current time."""
+        end_time = end_time if end_time else int(time.time() * 1000)
+        status = status if status else RunStatus.to_string(RunStatus.FINISHED)
+        self.store.update_run_info(run_id, run_status=RunStatus.from_string(status),
+                                   end_time=end_time)
+
+    def delete_run(self, run_id):
+        """
+        Deletes a run with the given ID.
+        """
+        self.store.delete_run(run_id)
+
+    def restore_run(self, run_id):
+        """
+        Restores a deleted run with the given ID.
+        """
+        self.store.restore_run(run_id)
+
+    def search_runs(self, experiment_ids, filter_string, run_view_type=ViewType.ACTIVE_ONLY):
+        """
+        Search experiments that fit the search criteria.
+
+        :param experiment_ids: List of experiment IDs
+        :param filter_string: Filter query string.
+        :param run_view_type: one of enum values ACTIVE_ONLY, DELETED_ONLY, or ALL runs
+                              defined in :py:class:`mlflow.entities.ViewType`.
+        :return:
+        """
+        return self.store.search_runs(experiment_ids=experiment_ids,
+                                      search_filter=SearchFilter(filter_string=filter_string),
+                                      run_view_type=run_view_type)
+
+
+def _get_user_id():
+    """Get the ID of the user for the current run."""
+    try:
+        import pwd
+        return pwd.getpwuid(os.getuid())[0]
+    except ImportError:
+        return _DEFAULT_USER_ID
