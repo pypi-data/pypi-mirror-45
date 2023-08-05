942e3e9670d0fa513297b926b62204ef74040fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:05 +0000

    Add package django-react-templatetags/5.3.0/django_react_templatetags-5.3.0.tar.gz

---
+++ packages/django-react-templatetags/django_react_templatetags-5.3.0.tar.gz/django_react_templatetags-5.3.0/setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import io
+import os
+import sys
+import re
+import pip
+from setuptools import setup, find_packages
+
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+# Convert markdown to rst
+try:
+    from pypandoc import convert
+    long_description = convert("README.md", "rst")
+except:
+    long_description = ""
+
+version = ''
+with io.open('django_react_templatetags/__init__.py', 'r', encoding='utf8') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
+setup(
+    name="django_react_templatetags",
+    version=version,
+    description=("This django library allows you to add React components into your django templates."),  # NOQA
+    long_description=long_description,
+    author="Fröjd",
+    author_email="martin@marteinn.se",
+    url="https://github.com/frojd/django-react-templatetags",
+    packages=find_packages(exclude=('tests*', "django_react_templatetags_example")),
+    include_package_data=True,
+    install_requires=[
+        'Django>=1.11',
+    ],
+    extras_require={
+        'ssr': ['requests'],
+    },
+    tests_require=[
+        'Django>=1.11',
+        'requests',
+        'responses',
+    ],
+    license="MIT",
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Environment :: Web Environment',
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        'Intended Audience :: Developers',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        'Programming Language :: Python :: 2',
+        "Programming Language :: Python :: 2.7",
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'Topic :: Utilities',
+    ],
+)
