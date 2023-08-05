74b083b50c5c1f642fb897b1142d97bb7e822851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:45 +0000

    Add package nd123/0.2/nd123-0.2-py3-none-any.whl

---
+++ packages/nd123/nd123-0.2-py3-none-any.whl/nd123/mymodule.py
@@ -0,0 +1,12 @@
+def sqr(num):
+	res = num * num
+	print("square = " , res)
+
+def cube(num):
+	res = num * num * num
+	print("cube = ", res)
+
+def sqrt(num):
+	import math
+	res = math.sqrt(num)
+	print("sqrt = ", res)
