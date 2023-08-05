68bda3adcd49dfe438275b4b9967e211ff36af90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:23 +0000

    Add package ml-legit/19.4.2795/ml_legit-19.4.2795-py2.py3-none-any.whl

---
+++ packages/ml-legit/ml_legit-19.4.2795-py2.py3-none-any.whl/missinglink/legit/object_store/null_storage.py
@@ -0,0 +1,10 @@
+# -*- coding: utf8 -*-
+from ..dulwich.object_store import BaseObjectStore
+
+
+class NullObjectStore(BaseObjectStore):
+    def __iter__(self):
+        pass
+
+    def get_raw(self, metadata):
+        raise KeyError()
