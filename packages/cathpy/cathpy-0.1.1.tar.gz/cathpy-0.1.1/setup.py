956afa1b3f723af8c08a444ae222760d56f16cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:42 +0000

    Add package cathpy/0.1.1/cathpy-0.1.1.tar.gz

---
+++ packages/cathpy/cathpy-0.1.1.tar.gz/cathpy-0.1.1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+import cathpy
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cathpy",
+    version=cathpy.__version__,
+    author="Ian Sillitoe",
+    author_email="i.sillitoe@ucl.ac.uk",
+    description="CathPy - Python Bioinformatics Toolkit for CATH (Protein Classification).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/UCL/cathpy",
+    packages=['cathpy'],
+    test_suite="tests",
+    scripts=[
+        'scripts/cath-funfhmmer-api',
+        'scripts/cath-align-scorecons',
+        'scripts/cath-align-summary',
+        'scripts/cath-sc-merge-alignment',
+    ],
+    install_requires=[
+        'requests',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
