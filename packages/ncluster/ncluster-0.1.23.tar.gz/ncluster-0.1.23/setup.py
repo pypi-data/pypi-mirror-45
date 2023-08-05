b6af1d3129530cd44767117d197354192e5c5aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:24 +0000

    Add package ncluster/0.1.23/ncluster-0.1.23.tar.gz

---
+++ packages/ncluster/ncluster-0.1.23.tar.gz/ncluster-0.1.23/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(scripts=['tools/aws_create_resources.py',
+               'tools/aws_delete_resources.py',
+               'tools/nsync',
+               'tools/ncluster'
+])
