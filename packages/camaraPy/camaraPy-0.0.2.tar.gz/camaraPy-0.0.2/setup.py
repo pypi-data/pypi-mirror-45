5e4840d7f80b1cd5ae977649ede7ce7a61b4e198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:08 +0000

    Add package camaraPy/0.0.2/camaraPy-0.0.2.tar.gz

---
+++ packages/camaraPy/camaraPy-0.0.2.tar.gz/camaraPy-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="camaraPy",
+	version="0.0.2",
+	author="Rodrigo Menegat Schuinski",
+	author_email="rodrigoschuinski@gmail.com",
+	description="A wrapper for the Brazilian House of Representatives public data API",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/RodrigoMenegat/camaraPy",
+	packages=setuptools.find_packages(),
+	install_requires = [
+		"requests>=2.21.0",
+		"xmltodict>=0.12.0",
+],
+	classifiers = [
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: GNU General Public License (GPL)",
+		"Operating System :: Unix",
+	]
+)
