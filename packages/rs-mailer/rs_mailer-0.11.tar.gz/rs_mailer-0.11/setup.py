3c214343146c00ce9c4bfad6b98518c3a4023cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:02 +0000

    Add package rs-mailer/0.11/rs_mailer-0.11.tar.gz

---
+++ packages/rs-mailer/rs_mailer-0.11.tar.gz/rs_mailer-0.11/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rs_mailer',
+      version='0.11',
+      description=' A really simple Python package for sending emails through SMTP servers.',
+      url='https://github.com/ronaldseoh/rs_mailer',
+      author='Ronald Seoh',
+      author_email='ronaldseoh@icloud.com',
+      license='BSD-3',
+      packages=['rs_mailer'],
+      zip_safe=False)
