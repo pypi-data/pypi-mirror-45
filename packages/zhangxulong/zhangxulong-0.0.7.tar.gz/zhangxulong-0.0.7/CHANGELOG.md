7838ec2d2b7b42cc0aefc64477e122cf1bd35f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:48 +0000

    Add package zhangxulong/0.0.7/zhangxulong-0.0.7.tar.gz

---
+++ packages/zhangxulong/zhangxulong-0.0.7.tar.gz/zhangxulong-0.0.7/CHANGELOG.md
@@ -0,0 +1,3 @@
+* Ver: 0.0.7 wechat bot with tuling
+* Ver: 0.0.5 baseZhang review codes
+* Ver: 0.0.2 python 3.0 and init 
---
+++ packages/zhangxulong/zhangxulong-0.0.7.tar.gz/zhangxulong-0.0.7/zhangxulong/__init__.py
@@ -0,0 +1,5 @@
+from .plot_utils import *
+from .classification_evaluate import *
+from .basic_wav_io import *
+from .extract_mfcc import *
+from .wechat_bot import *
