45fcbcc15ee1597cbc0fa98181d3459d57665042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:37 +0000

    Add package apiron/2.5.0/apiron-2.5.0.tar.gz

---
+++ packages/apiron/apiron-2.5.0.tar.gz/apiron-2.5.0/apiron/service/__init__.py
@@ -0,0 +1,9 @@
+from apiron.service.base import Service, ServiceBase
+from apiron.service.discoverable import DiscoverableService
+
+
+__all__ = [
+    'Service',
+    'ServiceBase',
+    'DiscoverableService',
+]
