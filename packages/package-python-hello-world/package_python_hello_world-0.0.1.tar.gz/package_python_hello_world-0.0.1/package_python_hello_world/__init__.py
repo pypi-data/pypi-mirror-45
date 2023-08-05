d51553b864a55daa7ccb4d44a85264898e655010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:43 +0000

    Add package package-python-hello-world/0.0.1/package_python_hello_world-0.0.1.tar.gz

---
+++ packages/package-python-hello-world/package_python_hello_world-0.0.1.tar.gz/package_python_hello_world-0.0.1/package_python_hello_world/__init__.py
@@ -0,0 +1,2 @@
+def say_hello(name='anonymous'):
+    print(f'Hello {name}')
---
+++ packages/package-python-hello-world/package_python_hello_world-0.0.1.tar.gz/package_python_hello_world-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='package_python_hello_world',
+      version='0.0.1',
+      author='flaviogf',
+      author_email='flavio@smn.com.br',
+      description='package python hello world',
+      packages=['package_python_hello_world'])
