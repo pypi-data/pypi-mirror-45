8ee627dff6a6af2f8cbf5c39919e9d2dc5995ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:57 +0000

    Add package tgcli/0.1.2/tgcli-0.1.2.tar.gz

---
+++ packages/tgcli/tgcli-0.1.2.tar.gz/tgcli-0.1.2/tgcli/cli.py
@@ -0,0 +1,111 @@
+import io
+import platform
+
+import sys
+import colorful
+import click
+import yaspin
+import yaspin.spinners
+
+import tgcli.request.bot
+
+
+IS_DARWIN = platform.system().lower() == "darwin"
+
+
+MESSAGE_FORMATS = {"html": "HTML", "markdown": "Markdown"}
+MEDIA_TYPES = [str(v.value) for v in list(tgcli.request.bot.MediaType)]
+
+
+@click.group()
+@click.option(
+    "--secure/--no-secure",
+    default=(not IS_DARWIN),
+    help='Whether to validate HTTPS requests. Default is "secure" except OSX. '
+    'You have to update built-in OpenSSL and manually pass "secure" each '
+    "time in OSX.",
+)
+@click.pass_context
+def cli(ctx, secure: bool):
+    ctx.obj = dict()
+    ctx.obj["secure"] = secure
+
+
+@cli.group()
+@click.option(
+    "-t",
+    "--token",
+    envvar="TELEGRAM_BOT_TOKEN",
+    required=True,
+    help="Token of bot. Can be provided via TELEGRAM_BOT_TOKEN environment variable.",
+)
+@click.pass_context
+def bot(ctx, token):
+    ctx.obj["token"] = token
+
+
+@bot.command()
+@click.option(
+    "--format",
+    default="markdown",
+    type=click.Choice(MESSAGE_FORMATS.keys()),
+    help='Format of the message. Default is "markdown".',
+)
+@click.option("-f", "--file", type=click.File("rb"), help="File to send.")
+@click.option(
+    "--as",
+    "as_",
+    default="document",
+    type=click.Choice(MEDIA_TYPES),
+    help='Send the file as as type. Default is "document".',
+)
+@click.option(
+    "-r", "--receiver", required=True, help="Receiver of the message."
+)
+@click.argument("message", required=True)
+@click.pass_context
+def send(
+    ctx, format: str, file: io.BytesIO, as_: str, receiver: str, message: str
+):
+    session = tgcli.request.bot.BotSession(ctx.obj["token"])
+    session.verify = ctx.obj["secure"]
+
+    if file:
+        request = tgcli.request.bot.SendFileRequest(
+            session,
+            receiver,
+            file,
+            message,
+            tgcli.request.bot.MediaType(as_),
+            MESSAGE_FORMATS[format],
+        )
+        file.close()
+    else:
+        request = tgcli.request.bot.SendMessageRequest(
+            session, receiver, message, MESSAGE_FORMATS[format]
+        )
+
+    with yaspin.yaspin(yaspin.spinners.Spinners.clock) as spinner:
+        spinner.text = "Sending message..."
+        response = session.send(request)
+
+        if response.ok:
+            spinner.text = "Message was sent successfully."
+            spinner.ok("✔️ ")
+        else:
+            data = response.json()
+
+            code = data.get("error_code", "Unknown")
+            description = data.get("description", "No description found.")
+            spinner.write(
+                "{c.bold_red}Error Code:{c.reset} {}".format(code, c=colorful)
+            )
+            spinner.write(
+                "{c.bold_red}Error Details:{c.reset} {}".format(
+                    description, c=colorful
+                )
+            )
+
+            spinner.text = "Failed sending message."
+            spinner.fail("❌")
+            sys.exit(1)
