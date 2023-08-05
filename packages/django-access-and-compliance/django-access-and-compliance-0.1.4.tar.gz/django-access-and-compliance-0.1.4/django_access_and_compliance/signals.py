d127266f261c27aa9d87d53dfd7a79493327535e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:36 +0000

    Add package django-access-and-compliance/0.1.4/django-access-and-compliance-0.1.4.tar.gz

---
+++ packages/django-access-and-compliance/django-access-and-compliance-0.1.4.tar.gz/django-access-and-compliance-0.1.4/django_access_and_compliance/signals.py
@@ -0,0 +1,27 @@
+import requests
+import logging
+
+from django.conf import settings
+from django.contrib.auth.models import Group
+
+from .config import access_and_compliance_group_name
+
+logger = logging.getLogger(__name__)
+
+
+def ensure_compliant(sender, request, user, **kwargs):
+    payload = {'uniqname': user.username}
+    response = requests.get(settings.ACCESS_AND_COMPLIANCE_VALIDATION_URL, params=payload)
+    response.raise_for_status()
+    group = Group.objects.get_or_create(name=access_and_compliance_group_name)
+
+    if _is_compliant(response):
+        group.user_set.add(user)
+        logger.debug(f'{user} has attested to the data access and compliance policy')
+    else:
+        group.user_set.remove(user)
+        logger.debug(f'{user} has not attested to data compliance policy')
+
+
+def _is_compliant(response):
+    return response.text in settings.ACCESS_AND_COMPLIANCE_TRUTHY_VALUES
