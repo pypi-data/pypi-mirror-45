99cd5646258be5be742ce92580d58a7a8737c027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:10 +0000

    Add package bitwarden-simple-cli/1.2/bitwarden%20simple%20cli-1.2.tar.gz

---
+++ packages/bitwarden-simple-cli/bitwarden%20simple%20cli-1.2.tar.gz/bitwarden simple cli-1.2/bitwarden_simple_cli/services/ContainerService_test.py
@@ -0,0 +1,21 @@
+import pytest
+from bitwarden_simple_cli.services.ContainerService import ContainerService
+from bitwarden_simple_cli.services.StorageService import StorageService
+from bitwarden_simple_cli.services.SecureStorageService import SecureStorageService
+from bitwarden_simple_cli.services.CryptoService import CryptoService
+from bitwarden_simple_cli.tests.fixtures_common import bw_session
+
+
+@pytest.mark.usefixtures("bw_session")
+def test_container_service():
+    storage_service = StorageService()
+    crypto_service = CryptoService(storage_service)
+    secure_storage_service = SecureStorageService(storage_service, crypto_service)
+    container_service = ContainerService()
+    container_service.add_service(crypto_service)
+    container_service.add_service(secure_storage_service)
+    container_service2 = ContainerService()
+    assert container_service.get_crypto_service() == crypto_service
+    assert container_service.get_secure_storage_service() == secure_storage_service
+    assert container_service2.get_crypto_service() == crypto_service
+    assert container_service2.get_secure_storage_service() == secure_storage_service
