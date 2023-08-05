b05f0379014e7a16108f27a2ff85412d7e20c55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:28 +0000

    Add package django-migration-testcase/0.0.14/django-migration-testcase-0.0.14.tar.gz

---
+++ packages/django-migration-testcase/django-migration-testcase-0.0.14.tar.gz/django-migration-testcase-0.0.14/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='django-migration-testcase',
+    version='0.0.14',
+    author='Andrew Plummer',
+    author_email='plummer574@gmail.com',
+    description='For testing migrations in Django',
+    url='https://github.com/plumdog/django_migration_testcase',
+    packages=find_packages(),
+    install_requires=['Django>=1.4'])
