b93a5ceba52128ce7d0743a9fd970564850f7f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:08 +0000

    Add package qmix/1.0.4/QMix-1.0.4.tar.gz

---
+++ packages/qmix/QMix-1.0.4.tar.gz/QMix-1.0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "QMix",
+    version = "1.0.4",
+    author = "John Garrett",
+    author_email = "garrettj403@gmail.com",
+    description = ("Simulate SIS mixer operation"),
+    license = "GPL v3",
+    keywords = "SIS mixers, radio astronomy, superconducting detectors, terahertz instrumentation, Python",
+    url = "https://garrettj403.github.io/QMix/",
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'numba',
+        'numpy',
+        'scipy'
+    ],
+    long_description="QMix is a software package for simulating quasiparticle tunneling currents in Superconductor/Insulator/Superconductor (SIS) junctions. These junctions are commonly used for heterodyne mixing at millimeter and submillimeter wavelengths. QMix can be used to simulate their performance, investigate their experimental results and optimize their operation.",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Astronomy",
+    ],
+)
---
+++ packages/qmix/QMix-1.0.4.tar.gz/QMix-1.0.4/tests/test_exp_data.py
@@ -0,0 +1,72 @@
+"""Test the module that analyzes experimental data (qmix.exp.exp_data).
+
+Note: This module does not affect the QMix simulations. This module is used
+to analyze experimental data. This module is much easier to test by running it
+on experimental data and plotting the results.
+
+"""
+
+import numpy as np
+import pytest
+
+import qmix.exp.exp_data as qe
+
+
+@pytest.mark.filterwarnings("ignore::FutureWarning")
+def test_importing_exp_data(directory='tests/exp-data/'):
+    """Try importing experimental data and then compare the results to values
+    that I have calculated by hand."""
+
+    ### Import DC data (no LO pumping) ###
+
+    # Import by passing file name
+    dciv = qe.RawData0(directory+'dciv-data.csv', 
+                       directory+'dcif-data.csv',
+                       analyze=False)
+
+    # Import by passing Numpy array
+    params = dict(skip_header=1, delimiter=',')
+    dciv_data = np.genfromtxt(directory+'dciv-data.csv', **params)
+    dcif_data = np.genfromtxt(directory+'dcif-data.csv', **params)
+    dciv = qe.RawData0(dciv_data, dcif_data)
+
+    # Check some of the attributes
+    # Note: I calculated these by hand
+    assert np.abs(dciv.vgap - 2.72e-3) < 0.1e-3
+    assert np.abs(dciv.rn - 13.4) < 0.2
+    assert np.abs(dciv.offset[0] - 0.1e-3) < 0.01e-3
+    assert np.abs(dciv.offset[1] - 9.68e-6) < 0.1e-6
+
+    ### Import data at 230 GHz ###
+
+    # Import by passing file name
+    pump = qe.RawData(directory+'f230_0_iv.csv', dciv,
+                      directory+'f230_0_hot.csv',
+                      directory+'f230_0_cold.csv', analyze=False)
+
+    # Import by passing Numpy array
+    csv = dict(delimiter=',', usecols=(0,1), skip_header=1)
+    ivdata = np.genfromtxt(directory+'f230_0_iv.csv', **csv)
+    hotdata = np.genfromtxt(directory+'f230_0_hot.csv', **csv)
+    colddata = np.genfromtxt(directory+'f230_0_cold.csv', **csv)
+    pump = qe.RawData(ivdata, dciv, hotdata, colddata, freq=230.2, best_pt="Min Tn")
+    assert pump.freq == 230.2, "Wrong frequency."
+    # Try importing without specifying the frequency
+    with pytest.raises(ValueError):
+        pump = qe.RawData(ivdata, dciv, hotdata, colddata)
+
+    # Check some of the attributes
+    # Note: I calculated these by hand
+    assert 35. < pump.tn_best < 40., "Wrong noise temperature."
+    assert -1.2 < pump.g_db < -1.0, "Wrong conversion gain."
+
+    # Check automatic frequency determination
+    pump = qe.RawData(directory+'f230_0_iv.csv', dciv, analyze_iv=False)
+    assert pump.freq == 230.0, "Wrong frequency."
+
+    # Try importing data incorrectly
+    data = [1, 2, 3]
+    with pytest.raises(ValueError):
+        dciv = qe.RawData0(data)
+    with pytest.raises(ValueError):
+        pump = qe.RawData(data, dciv)
