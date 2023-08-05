d8d8008926f26e9f54600a146dedbda3f13527bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:45 +0000

    Add package vidtext/1.1/vidtext-1.1.tar.gz

---
+++ packages/vidtext/vidtext-1.1.tar.gz/vidtext-1.1/setup.py
@@ -0,0 +1,38 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+setup(
+    name="vidtext",
+    version="1.1",
+    url="https://github.com/anubhavshakya/vidtext",
+    license='MIT',
+
+    author="Anubhav Shakya",
+    author_email="inbox.anubhavshakya@gmail.com",
+
+    description="This Project or Package Is Made for to Convert the Text to Video",
+    long_description=read("README.md"),
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=["pillow","google_images_download","rake_nltk","opencv-python"],
+
+    classifiers=[
+    'Development Status :: 4 - Beta', 
+    'Intended Audience :: System Administrators', 
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3'
+    ],
+)
---
+++ packages/vidtext/vidtext-1.1.tar.gz/vidtext-1.1/vidtext/textTovideo.py
@@ -0,0 +1,39 @@
+from .image_utils import ImageText
+from PIL import Image
+import random
+import string
+import cv2,glob,tempfile,shutil
+import os
+from .directory import TempDir
+from .text import Text
+from .imagesource import getImage
+def TextToVideo(sourceData,vs=0):
+	i = 'A'
+	color = 'white'
+	font = "calibri.ttf"
+	backcolor = ['#210f05','#444441','#16082d','#3d091f','#420000','#012824','#332401','#250130','#011d30']
+	li = Text(sourceData)
+	secure_random = random.SystemRandom()
+	dir_path = TempDir()
+	img_array = []
+	for text in range(0,len(li)):
+		img = ImageText((800, 600), background=secure_random.choice(backcolor))
+		img.write_text_box((50, 50), li[text], box_width=700, font_filename=font,font_size=30, color=color, place='justify')
+		img.save(dir_path+i+'.png')
+		first_Image = Image.open(dir_path+i+'.png')
+		second_Image = Image.open(getImage(li[text]))
+		final_check = second_Image.resize((700,300),resample=0)
+		first_Image.paste(final_check, (50, 282))
+		first_Image.save(dir_path+i+'.png')
+		filename = dir_path+i+'.png'
+		videoimg = cv2.imread(filename)
+		height,width,layers = videoimg.shape
+		size = (width,height)
+		img_array.append(videoimg)
+		i = ''.join(random.choices(string.ascii_uppercase + string.digits))
+	out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), vs, size)
+	for i in range(len(img_array)):
+	    out.write(img_array[i])
+	out.release()
+	shutil.rmtree(dir_path, ignore_errors=True)
+	shutil.rmtree('downloads', ignore_errors=True)
