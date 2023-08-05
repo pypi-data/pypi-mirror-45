739ec01f8be9238e052d10775fac97c79624c88e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:57 +0000

    Add package httpcore/0.2.0/httpcore-0.2.0.tar.gz

---
+++ packages/httpcore/httpcore-0.2.0.tar.gz/httpcore-0.2.0/httpcore/__init__.py
@@ -0,0 +1,16 @@
+from .config import PoolLimits, SSLConfig, TimeoutConfig
+from .connections import Connection
+from .datastructures import URL, Origin, Request, Response
+from .exceptions import (
+    BadResponse,
+    ConnectTimeout,
+    PoolTimeout,
+    ReadTimeout,
+    ResponseClosed,
+    StreamConsumed,
+    Timeout,
+)
+from .pool import ConnectionPool
+from .sync import SyncClient, SyncConnectionPool
+
+__version__ = "0.2.0"
