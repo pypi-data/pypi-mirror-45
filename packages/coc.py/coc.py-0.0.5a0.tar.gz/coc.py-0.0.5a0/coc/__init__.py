a4e089b3674f56bef75c80836c1a660632bb4b11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:23 +0000

    Add package coc.py/0.0.5a0/coc.py-0.0.5a0.tar.gz

---
+++ packages/coc.py/coc.py-0.0.5a0.tar.gz/coc.py-0.0.5a0/coc/__init__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.0.5a'
+
+from .client import Client
+from .dataclasses import (
+    Clan,
+    BasicClan,
+    SearchClan,
+    WarClan,
+    Player,
+    BasicPlayer,
+    WarMember,
+    SearchPlayer,
+    BaseWar,
+    WarLog,
+    CurrentWar,
+    Achievement,
+    Troop,
+    Hero,
+    Spell,
+    WarAttack,
+    Location,
+    League,
+    LeagueRankedPlayer,
+    Season,
+    LegendStatistics,
+    Badge,
+    Timestamp,
+    LeaguePlayer,
+    LeagueClan,
+    LeagueGroup,
+    LeagueWar,
+    LeagueWarLogEntry
+)
+from .errors import (
+    ClashOfClansException,
+    HTTPException,
+    NotFound,
+    InvalidArgument,
+    InvalidToken,
+    Forbidden,
+    Maitenance
+)
+from .http import HTTPClient
