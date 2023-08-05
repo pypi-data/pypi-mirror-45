9b5acb113e14561d3bcb1ec9a7251cebed0c6a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:15 +0000

    Add package pyps4-homeassistant/0.7.0/pyps4_homeassistant-0.7.0-py3-none-any.whl

---
+++ packages/pyps4-homeassistant/pyps4_homeassistant-0.7.0-py3-none-any.whl/pyps4_homeassistant/errors.py
@@ -0,0 +1,21 @@
+"""Exceptions Definitions."""
+
+
+class CredentialTimeout(Exception):
+    """Recieved no credentials or timed out."""
+
+
+class NotReady(Exception):
+    """PS4 no connection."""
+
+
+class LoginFailed(Exception):
+    """PS4 Failed Login."""
+
+
+class UnknownButton(Exception):
+    """Button not valid."""
+
+
+class UnknownDDPResponse(Exception):
+    """DDP Response is Unknown."""
