ea7b116df833f4d68e0c687823a1008930bf9f4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:26 +0000

    Add package pyoploverz/0.0.3/pyoploverz-0.0.3-py3-none-any.whl

---
+++ packages/pyoploverz/pyoploverz-0.0.3-py3-none-any.whl/pyoploverz/__init__.py
@@ -0,0 +1,22 @@
+from .base import Response
+from .style import LastedOp, LastedResponse, SearchOp, SearchResponse,StreamInfo,StreamOp,StreamDownload
+from .util import LastedOpLoverz, SearchOpLoverz, StreamOpLoverz, Request
+from .api import OpzApi
+__author__ = "Dyseo"
+__version__ ="0.0.3"
+__all__ = [
+        "__version__",
+	"OpzApi",
+	"Response",
+	"LastedResponse",
+	"LastedOp",
+	"SearchOp",
+	"SearchResponse",
+	"StreamOp",
+	"StreamInfo",
+	"StreamDownload",
+	"LastedOpLoverz",
+	"SearchOpLoverz",
+	"StreamOpLoverz",
+	"Request"
+	]
