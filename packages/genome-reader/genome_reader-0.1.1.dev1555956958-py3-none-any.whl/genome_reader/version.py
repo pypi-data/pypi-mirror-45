8f5ba9d131c486b3caff0dc9c949da93f919dc7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:13 +0000

    Add package genome-reader/0.1.1.dev1555956958/genome_reader-0.1.1.dev1555956958-py3-none-any.whl

---
+++ packages/genome-reader/genome_reader-0.1.1.dev1555956958-py3-none-any.whl/genome_reader/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.1.1'
+version = '0.1.1'
+full_version = '0.1.1.dev1555956958'
+dev_version = '1555956958'
+release = False
+if not release:
+    version = full_version
