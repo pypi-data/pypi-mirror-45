da2d70adc060fc07ecf36bd3b5d75e4a71c13a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:47 +0000

    Add package stackify-python-apm/1.1.2/stackify-python-apm-1.1.2.tar.gz

---
+++ packages/stackify-python-apm/stackify-python-apm-1.1.2.tar.gz/stackify-python-apm-1.1.2/tests/instrumentation/fixtures/module_test.py
@@ -0,0 +1,23 @@
+class CustomClassOne(object):
+    def custom_method_one(self):
+        return True
+
+
+class CustomClassTwo(object):
+    def custom_method_two(self):
+        return True
+
+
+class CustomClassThree(object):
+    def custom_method_three(self):
+        return True
+
+
+class CustomClassFour(object):
+    def custom_method_four(self):
+        return True
+
+
+class CustomClassFive(object):
+    def custom_method_five(self):
+        return True
