7ddf8fc0ee4df99020a4e9c360043509228d6004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:59 +0000

    Add package simple-gen/2019.4.22/simple-gen-2019.4.22.tar.gz

---
+++ packages/simple-gen/simple-gen-2019.4.22.tar.gz/simple-gen-2019.4.22/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='simple-gen',
+    version='2019.4.22',
+    description='gen files',
+    author='buglan',
+    author_email='1831353087@qq.com',
+    scripts=['gen.py']
+)
