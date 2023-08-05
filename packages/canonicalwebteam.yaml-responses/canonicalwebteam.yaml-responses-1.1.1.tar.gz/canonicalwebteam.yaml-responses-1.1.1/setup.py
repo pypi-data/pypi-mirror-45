7da4de22e068b95a623abd7f76a2ab68c7820667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:25 +0000

    Add package canonicalwebteam.yaml-responses/1.1.1/canonicalwebteam.yaml-responses-1.1.1.tar.gz

---
+++ packages/canonicalwebteam.yaml-responses/canonicalwebteam.yaml-responses-1.1.1.tar.gz/canonicalwebteam.yaml-responses-1.1.1/setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name="canonicalwebteam.yaml-responses",
+    version="1.1.1",
+    author="Canonical Webteam",
+    url="https://github.com/canonical-webteam/yaml-responses",
+    packages=["canonicalwebteam.yaml_responses"],
+    description=(
+        "Functions to read from yaml files to provide"
+        "generic responses to URLs in Django and Flask"
+    ),
+    install_requires=["pyyaml", "yamlloader"],
+    extras_require={"django": ["Django"], "flask": ["flask"]},
+    tests_require=["Django", "flask", "pyyaml", "yamlloader"],
+    test_suite="tests",
+)
