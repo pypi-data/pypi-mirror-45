3a0be5d8c1b49a0dae14b4672b5d6bc6cc51a59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:29 +0000

    Add package python-ipaymu/0.1.3.1/python-ipaymu-0.1.3.1.tar.gz

---
+++ packages/python-ipaymu/python-ipaymu-0.1.3.1.tar.gz/python-ipaymu-0.1.3.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+setuptools.setup(
+     name='python-ipaymu',  
+     version='0.1.3.1',
+     author="Bayu Wardani",
+     author_email="bayuwardani51@gmail.com",
+     description="Integrasi iPaymu",
+     long_description="Memudahkan ada bagi pengguna python untuk integrasi dengan iPaymu",
+     long_description_content_type="text/markdown",
+     url="https://github.com/BayuWardani/python-ipaymu.git",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Development Status :: 4 - Beta",
+         "Framework :: Django :: 2.2",
+         "Framework :: Django :: 1.11",
+         "Framework :: Flask",
+         "Natural Language :: Indonesian",
+         "Operating System :: Microsoft :: Windows",
+         "Operating System :: Unix","Operating System :: MacOS",
+         "Programming Language :: Python",
+         "Programming Language :: Python :: 3 :: Only"
+     ],
+ )
