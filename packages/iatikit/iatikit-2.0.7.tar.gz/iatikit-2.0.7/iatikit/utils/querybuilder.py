f641ef5fdefbb94475cd36d93c044902d20c0601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:47 +0000

    Add package iatikit/2.0.7/iatikit-2.0.7.tar.gz

---
+++ packages/iatikit/iatikit-2.0.7.tar.gz/iatikit-2.0.7/iatikit/utils/querybuilder.py
@@ -0,0 +1,24 @@
+class XPathQueryBuilder(object):
+    def __init__(self, schema, prefix='', count=False):
+        self._schema = schema
+        self._count = count
+        self._prefix = prefix
+
+    def where(self, **kwargs):
+        query_str = self._prefix
+
+        exprs = []
+        for shortcut, value in kwargs.items():
+            if '__' in shortcut:
+                shortcut, operator = shortcut.split('__')
+            else:
+                operator = 'eq'
+            expr = self.filter(shortcut, operator, value)
+            exprs.append(expr)
+        query_str += ''.join(['[{}]'.format(x) for x in exprs])
+        if self._count:
+            query_str = 'count({})'.format(query_str)
+        return query_str
+
+    def filter(self, shortcut, operator, value):
+        return getattr(self._schema, shortcut)().where(operator, value)
