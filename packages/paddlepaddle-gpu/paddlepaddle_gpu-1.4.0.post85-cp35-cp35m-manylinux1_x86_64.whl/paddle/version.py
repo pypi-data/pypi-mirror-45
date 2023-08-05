30c2a6ce44205b5e8d5765ef706280da6315feb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:10 +0000

    Add package paddlepaddle-gpu/1.4.0.post85/paddlepaddle_gpu-1.4.0.post85-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/paddlepaddle-gpu/paddlepaddle_gpu-1.4.0.post85-cp35-cp35m-manylinux1_x86_64.whl/paddle/version.py
@@ -0,0 +1,23 @@
+# THIS FILE IS GENERATED FROM PADDLEPADDLE SETUP.PY
+#
+full_version    = '1.4.0'
+major           = '1'
+minor           = '4'
+patch           = '0'
+rc              = '0'
+istaged         = False
+commit          = '2ff867f88628e9cb8b76eaf79045eca0f52e5b85'
+with_mkl        = 'ON'
+
+def show():
+    if istaged:
+        print('full_version:', full_version)
+        print('major:', major)
+        print('minor:', minor)
+        print('patch:', patch)
+        print('rc:', rc)
+    else:
+        print('commit:', commit)
+
+def mkl():
+    return with_mkl
