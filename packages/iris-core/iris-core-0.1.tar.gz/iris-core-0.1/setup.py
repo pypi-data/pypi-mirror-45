c9c5ccacda98d7f4718fc850521ef7e5e5c4e9e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:20 +0000

    Add package iris-core/0.1/iris-core-0.1.tar.gz

---
+++ packages/iris-core/iris-core-0.1.tar.gz/iris-core-0.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='iris-core',
+      version='0.1',
+      description='Iris message bus',
+      author="John Root",
+      author_email="john.root@digirati.com",
+      url='https://github.com/digirati-co-uk/iris4py',
+      packages=['iris',],
+      license='MIT',
+      install_requires=[
+          'boto3'
+      ],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          "Programming Language :: Python :: 3",
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ]
+      )
