e9e53d78f7eccc31d4f1c7cb0734441358580793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:44 +0000

    Add package drizzle/1.13/drizzle-1.13.tar.gz

---
+++ packages/drizzle/drizzle-1.13.tar.gz/drizzle-1.13/drizzle/__init__.py
@@ -0,0 +1,9 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+
+"""
+A package for combining dithered images into a single image
+"""
+
+from __future__ import absolute_import, division, unicode_literals, print_function
+
+from ._astropy_init import *
