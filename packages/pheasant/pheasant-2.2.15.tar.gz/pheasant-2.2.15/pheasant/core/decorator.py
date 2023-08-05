c2ceebe0eeed3b51a53e7fb8530f0bd2f3839458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:00 +0000

    Add package pheasant/2.2.15/pheasant-2.2.15.tar.gz

---
+++ packages/pheasant/pheasant-2.2.15.tar.gz/pheasant-2.2.15/pheasant/core/decorator.py
@@ -0,0 +1,89 @@
+import datetime
+import functools
+import re
+from dataclasses import field
+from typing import Callable, Dict, Iterable, Union
+
+from pheasant.core.base import Base
+from pheasant.renderers.jupyter.client import execution_report
+from pheasant.utils.time import format_timedelta_human
+
+SURROUND_TAG = re.compile(
+    r"^([^<]*)<(?P<tag>(span|div))(.*)</(?P=tag)>([^>]*)$", re.DOTALL
+)
+
+
+class Decorator(Base):
+    decorates: Dict[str, Callable[..., None]] = field(default_factory=dict)
+    class_names: Dict[str, str] = field(default_factory=dict)
+
+    def register(self, renderers: Iterable, decorate: Union[Callable, str]):
+        if isinstance(decorate, str):
+            decorate = getattr(self, decorate)
+        for renderer in renderers:
+            if renderer.parser:
+                renderer.parser.decorator = self  # type: ignore
+            for render_name, render in renderer.renders.items():
+                self.decorates[render_name] = decorate  # type: ignore
+                self.class_names[render_name] = self.class_name(render_name)
+
+    def class_name(self, render_name):
+        return self.name + "-" + render_name.split("__")[-1].replace("_", "-")
+
+    def decorate(self, cell):
+        if cell.match is None and None in self.decorates:
+            self.decorates[None](cell)
+        elif cell.match is not None and cell.render_name in self.decorates:
+            self.decorates[cell.render_name](cell)
+
+    def surround(self, cell):
+        if cell.output:
+            class_name = self.class_names[cell.render_name]
+            cell.output = surround(cell.output, class_name)
+
+
+def surround(source, class_name):
+    """Surround a html source with a tag accoring to the most out-side tag."""
+    replace = r'\1<\2 class="{class_name}"><\2\4</\2></\2>\5'
+    replace = replace.format(class_name=class_name)
+    return SURROUND_TAG.sub(replace, source)
+
+
+def comment(name):
+    def deco(render):
+        @functools.wraps(render)
+        def render_(self, context, splitter, parser):
+            if context[name].startswith("#"):
+                yield context["_source"].replace(context[name], context[name][1:])
+            else:
+                yield from render(self, context, splitter, parser)
+
+        return render_
+
+    return deco
+
+
+def monitor(format=True):
+    def deco(func):
+        @functools.wraps(func)
+        def func_(*args, **kwargs):
+            start_kernel = execution_report["life"]
+            start_func = datetime.datetime.now()
+            output = func(*args, **kwargs)
+            end_func = datetime.datetime.now()
+            end_kernel = execution_report["life"]
+
+            timedelta = end_func - start_func
+            if format:
+                timedelta = format_timedelta_human(timedelta)
+            func_.func_time = timedelta
+
+            timedelta = end_kernel - start_kernel
+            if format:
+                timedelta = format_timedelta_human(timedelta)
+            func_.kernel_time = timedelta
+            return output
+
+        return func_
+
+    return deco
---
+++ packages/pheasant/pheasant-2.2.15.tar.gz/pheasant-2.2.15/pheasant/utils/progress.py
@@ -0,0 +1,140 @@
+import datetime
+import itertools
+import sys
+import threading
+import time
+
+import colorama
+import cursor
+from termcolor import colored
+
+from pheasant.utils.time import format_timedelta_human
+
+colorama.init()
+
+
+class ProgressBar:
+    def __init__(
+        self, total: int = 0, multi: int = 0, spinner: bool = True, init: str = ""
+    ):
+        self.total = total
+        self.multi = multi
+        self.step = 1
+        self.spinner = spinner
+        self.init = init
+        self.count = 0
+        self.bar_length = 30
+        self.zfill = 3
+        self.write = sys.stdout.write
+        self.flush = sys.stdout.flush
+        self.bar = ""
+        self.show = False
+        self.result = ""
+
+    def spin(self, done):
+        start = datetime.datetime.now()
+        limit = datetime.timedelta(seconds=1)
+        status = ""
+        for char in itertools.cycle("|/-\\"):
+            dt = datetime.datetime.now() - start
+            if dt > limit:
+                status = " " + char + " " + format_timedelta_human(dt)
+                self.write(status)
+                self.flush()
+                self.write("\x08" * len(status))
+            if done.wait(0.3):
+                break
+        self.write(" " * len(status) + "\x08" * len(status))
+
+    def supervisor(self, func):
+        done = threading.Event()
+        spinner = threading.Thread(target=self.spin, args=(done,))
+        spinner.start()
+        result = func()
+        done.set()
+        spinner.join()
+        return result
+
+    def update(self, result=None, finish=False):
+        count = str(self.count).zfill(self.zfill)
+        total = str(self.total).zfill(self.zfill)
+        prefix = f"[{count}/{total}]"
+        prefix = colored(prefix, "cyan")
+
+        if self.multi:
+            step = str(self.step).zfill(self.zfill)
+            multi = str(self.multi).zfill(self.zfill)
+            prefix = f"({step}/{multi}) " + prefix
+
+        if self.count == self.total:
+            color = "green"
+            bar = colored("[" + "=" * self.bar_length + "=]", color)
+        else:
+            color = "green" if finish else "yellow"
+            current = int(self.bar_length * min(self.count / self.total, 1))
+            bar = (
+                colored("[", "green")
+                + colored("=" * current, "green")
+                + colored(">", "green", attrs=["bold"])
+                + colored("-" * (self.bar_length - current), color)
+                + colored("]", color)
+            )
+
+        bar = " ".join([prefix, bar])
+        if result:
+            bar = " ".join([bar, colored(result, color)])
+            self.result = result
+        back = "\x08" * len(self.bar)
+        self.write(back + bar)
+        diff = len(self.bar) - len(bar)
+        if diff > 0:
+            self.write(" " * diff + "\x08" * diff)
+        self.bar = bar
+        self.show = True
+
+    def progress(self, func, format=None, init=None, count=None):
+        if self.count == 0:
+            cursor.hide()
+            self.update(init or self.init)
+
+        if isinstance(func, str):
+            result = func
+        elif self.spinner:
+            result = self.supervisor(func)
+        else:
+            result = func()
+
+        self.count = count or self.count + 1
+        self.update(result if not format else format(result))
+
+        return result
+
+    def finish(self, count=None, reset=True):
+        if count:
+            self.count = count
+        if self.show:
+            self.update(self.result, finish=True)
+            self.write("\n")
+            self.flush()
+            self.show = False
+            cursor.show()
+        if reset:
+            self.count = 0
+        if self.multi:
+            self.step += 1
+
+
+def main():
+    def run():
+        time.sleep(1.5)
+        return "hello"
+
+    bar = ProgressBar(3, multi=3)
+    for k in range(3):
+        for k in range(bar.total):
+            bar.progress(run, init="start..........")
+        bar.finish()
+
+
+if __name__ == "__main__":
+    main()
