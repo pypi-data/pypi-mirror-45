f70ec71642e1f2a5ea2d9e9c5e402a0656078e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:31 +0000

    Add package wrapt-timeout-decorator/1.2.8/wrapt_timeout_decorator-1.2.8-py3-none-any.whl

---
+++ packages/wrapt-timeout-decorator/wrapt_timeout_decorator-1.2.8-py3-none-any.whl/wrapt_timeout_decorator/wrap_helper.py
@@ -0,0 +1,159 @@
+import dill
+import logging
+import platform
+import signal
+import sys
+import threading
+from typing import Any, Dict
+
+logging.basicConfig()
+logger = logging.getLogger('pickle_analyzer')
+
+if sys.version_info < (3, 3):
+    TimeoutError = AssertionError  # there is no TimeoutError below Python 3.3
+
+
+class WrapHelper(object):
+    def __init__(self, dec_timeout=None, use_signals=True, timeout_exception=None,
+                 exception_message=None, dec_allow_eval=False, dec_hard_timeout=False):
+        self.dec_timeout = dec_timeout
+        self.use_signals = use_signals
+        self.timeout_exception = timeout_exception
+        self.exception_message = exception_message
+        self.dec_allow_eval = dec_allow_eval
+        self.dec_hard_timeout = dec_hard_timeout
+        self.old_alarm_handler = None
+
+    def get_kwargs(self, kwargs):
+        self.dec_allow_eval = kwargs.pop('dec_allow_eval', self.dec_allow_eval)
+        self.dec_timeout = kwargs.pop('dec_timeout', self.dec_timeout)
+        self.use_signals = kwargs.pop('use_signals', self.use_signals)
+        self.dec_hard_timeout = kwargs.pop('dec_hard_timeout', self.dec_hard_timeout)
+
+    @property
+    def should_eval(self):
+        if self.dec_allow_eval and isinstance(self.dec_timeout, str):
+            return True
+        else:
+            return False
+
+    def format_exception_message(self, wrapped):
+        function_name = wrapped.__name__ or '(unknown name)'
+        if not self.exception_message:
+            self.exception_message = 'Function {function_name} timed out after {dec_timeout} seconds'
+        self.exception_message = self.exception_message.format(function_name=function_name, dec_timeout=self.dec_timeout)
+
+    def new_alarm_handler(self, signum, frame):
+        raise_exception(self.timeout_exception, self.exception_message)
+
+    def save_old_and_set_new_alarm_handler(self):
+        self.old_alarm_handler = signal.signal(signal.SIGALRM, self.new_alarm_handler)
+        signal.setitimer(signal.ITIMER_REAL, self.dec_timeout)
+
+    def restore_old_alarm_handler(self):
+        signal.setitimer(signal.ITIMER_REAL, 0)
+        signal.signal(signal.SIGALRM, self.old_alarm_handler)
+
+    def set_signals_to_false_if_not_possible(self):
+        if is_system_windows() or not is_in_main_thread():
+            self.use_signals = False
+
+
+def detect_unpickable_objects_and_reraise(object_to_pickle):
+    # sometimes the detection detects unpickable objects but actually
+    # they can be pickled - so we just try to start the thread and report
+    # the unpickable objects if that fails
+    dict_result = detect_unpickable_objects(object_to_pickle, dill_trace=False, log_warning=False)
+    s_err = 'can not pickle {on}, bad items: {bi}, bad objects: {bo}, bad types {bt}'.format(on=dict_result['object_name'],
+                                                                                             bi=dict_result['bad_items'],
+                                                                                             bo=dict_result['bad_objects'],
+                                                                                             bt=dict_result['bad_types'])
+    raise dill.PicklingError(s_err)
+
+
+def detect_unpickable_objects(object_to_pickle, dill_trace=True, log_warning=True):
+    # type: (Any, bool, bool) -> Dict
+    if log_warning:
+        logger.warning('always remember that the "object_to_pickle" should not be defined within the main context')
+    dict_result = dict()
+    dict_result['object_name'] = ''
+    dict_result['bad_items'] = list()
+    dict_result['bad_objects'] = list()
+    dict_result['bad_types'] = list()
+
+    safe_status_of_dill_trace = dill.detect.trace
+    # noinspection PyBroadException
+    try:
+        if dill_trace:
+            dill.detect.trace = True
+        pickled_object = dill.dumps(object_to_pickle)
+        dill.loads(pickled_object)
+    except Exception:
+        dict_result['object_name'] = object_to_pickle.__name__ or 'object'
+        dict_result['bad_objects'] = get_bad_pickling_objects(object_to_pickle)
+        dict_result['bad_types'] = get_bad_pickling_types(object_to_pickle)
+    finally:
+        dill.detect.trace = safe_status_of_dill_trace
+        return dict_result
+
+
+def get_bad_pickling_types(object_to_pickle):
+    bad_types = list()
+    # noinspection PyBroadException
+    try:
+        bad_types = dill.detect.badtypes(object_to_pickle)
+    except Exception:
+        bad_types = [sys.exc_info()[1]]
+    finally:
+        return bad_types
+
+
+def get_bad_pickling_objects(object_to_pickle):
+    bad_objects = list()
+    # noinspection PyBroadException
+    try:
+        bad_objects = dill.detect.badobjects(object_to_pickle)
+    except Exception:
+        bad_objects = [sys.exc_info()[1]]
+    finally:
+        return bad_objects
+
+
+def raise_exception(exception, exception_message):
+    """ This function checks if a exception message is given.
+    If there is no exception message, the default behaviour is maintained.
+    If there is an exception message, the message is passed to the exception.
+    """
+    if not exception:
+        exception = TimeoutError
+    raise exception(exception_message)
+
+
+def is_in_main_thread():
+    if sys.version_info < (3, 4):
+        return is_in_main_thread_pre_python_3_4()
+    else:
+        return is_in_main_thread_from_python_3_4_up()
+
+
+def is_in_main_thread_pre_python_3_4():
+    # old python versions below 3.4
+    if isinstance(threading.current_thread(), threading._MainThread):
+        return True
+    else:
+        return False
+
+
+def is_in_main_thread_from_python_3_4_up():
+    # python versions from 3.4 up
+    if threading.current_thread() == threading.main_thread():
+        return True
+    else:
+        return False
+
+
+def is_system_windows():
+    if platform.system().lower().startswith('win'):
+        return True
+    else:
+        return False
---
+++ packages/wrapt-timeout-decorator/wrapt_timeout_decorator-1.2.8-py3-none-any.whl/wrapt_timeout_decorator/wrapt_timeout_decorator.py
@@ -0,0 +1,132 @@
+"""
+Timeout decorator.
+    :copyright: (c) 2017 by Robert Nowotny
+    :license: MIT, see LICENSE for more details.
+"""
+
+from dill import PicklingError
+from typing import Union, Callable
+import wrapt
+from .wrap_helper import WrapHelper, detect_unpickable_objects_and_reraise
+from.wrap_function_multiprocess import Timeout
+
+
+def timeout(dec_timeout=None, use_signals=True, timeout_exception=None, exception_message=None,
+            dec_allow_eval=False, dec_hard_timeout=False):
+
+    # type: (Union[None, float, str], bool, Exception, str, bool, bool) -> Callable
+
+    """Add a timeout parameter to a function and return it.
+
+    ToDo :   Traceback information when use_signals=False (see https://pypi.python.org/pypi/tblib)
+             connect the Logger of the Subprocess to the main logger when use_signals=False
+             makes life easier on Windows
+
+    Windows remark : dont use the decorator on classes in the main.py because of Windows multiprocessing limitations
+                     read the README
+
+    Usage:
+
+    @timeout(3)
+    def foo():
+        pass
+
+    Overriding the timeout:
+
+    foo(dec_timeout=5)
+
+    Usage without decorating a function :
+
+    def test_method(a,b,c):
+        pass
+
+    timeout(3)(test_method)(1,2,c=3)
+
+    Usage with eval (beware, security hazard, no user input values here):
+        read : https://nedbatchelder.com/blog/201206/eval_really_is_dangerous.html before usage !
+
+    def class ClassTest4(object):
+        def __init__(self,x):
+            self.x=x
+
+        @timeout('instance.x', dec_allow_eval=True)
+        def test_method(self):
+            print('swallow')
+
+        @timeout(1)
+        def foo3(self):
+            print('parrot')
+
+    # or override via kwarg :
+    my_foo = ClassTest4(3)
+    my_foo.test_method(dec_timeout='instance.x * 2.5 +1')
+    my_foo.foo3(dec_timeout='instance.x * 2.5 +1', dec_allow_eval=True)
+
+    :param dec_timeout: *       optional time limit in seconds or fractions of a second. If None is passed,
+                                no seconds is applied. This adds some flexibility to the usage: you can disable timing
+                                out depending on the settings. dec_timeout will always be overridden by a
+                                kwarg passed to the wrapped function, class or class method.
+    :param use_signals:         flag indicating whether signals should be used or the multiprocessing
+                                when using multiprocessing, timeout granularity is limited to 10ths of a second.
+    :param timeout_exception:   the Exception to be raised when timeout occurs, default = TimeoutException
+    :param exception_message:   the Message for the Exception. Default: 'Function {f} timed out after {s} seconds.
+    :param dec_allow_eval: *    allows a string in parameter dec_timeout what will be evaluated. Beware this can
+                                be a security issue. This is very powerful, but is also very dangerous if you
+                                accept strings to evaluate from untrusted input.
+                                read: https://nedbatchelder.com/blog/201206/eval_really_is_dangerous.html
+
+                                If enabled, the parameter of the function dec_timeout, or the parameter passed
+                                by kwarg dec_timeout will be evaluated if its type is string. You can access :
+                                wrapped (the function object and all their exposed objects)
+                                instance    Example: 'instance.x' - see example above or doku
+                                args        Example: 'args[0]' - the timeout is the first argument in args
+                                kwargs      Example: 'kwargs["max_time"] * 2'
+
+    :param dec_hard_timeout:    only considered when use_signals = True (Windows)
+                                if dec_hard_timeout = True, the decorator will timeout after dec_timeout after the
+                                decorated function is called by the main program.
+                                If You set up a small timeout value like 0.1 seconds, in windows that function might
+                                actually never run - because setting up the process will already take longer
+                                then 0.1 seconds - that means the decorated function will ALWAYS time out (and never run).
+
+                                if dec_hard_timeout = False, the decorator will timeout after the process is allowed to
+                                run for dec_timeout seconds, that means the time to set up the new process is not considered.
+                                If You set up a small timeout value like 0.1 seconds, in windows that function might now
+                                take something like 0.6 seconds to timeout - 0.5 seconds to set up the process, and
+                                allowing the function in the process to run for 0.1 seconds.
+                                Since You can not know how long the spawn() will take under Windows, this is the default setting.
+
+    * all parameters starting with dec_ can be overridden via kwargs passed to the wrapped function.
+
+    :raises:                    TimeoutError if time limit is reached
+    :returns:                   the Result of the wrapped function
+
+    It is illegal to pass anything other than a function as the first parameter.
+    The function is wrapped and returned to the caller.
+    """
+
+    @wrapt.decorator
+    def wrapper(wrapped, instance, args, kwargs):
+        wrap_helper = WrapHelper(dec_timeout, use_signals, timeout_exception, exception_message, dec_allow_eval, dec_hard_timeout)
+        wrap_helper.get_kwargs(kwargs)
+        wrap_helper.set_signals_to_false_if_not_possible()
+        if wrap_helper.should_eval:
+            wrap_helper.dec_timeout = eval(str(wrap_helper.dec_timeout))
+        wrap_helper.format_exception_message(wrapped)
+        if not wrap_helper.dec_timeout:
+            return wrapped(*args, **kwargs)
+        else:
+            if wrap_helper.use_signals:
+                try:
+                    wrap_helper.save_old_and_set_new_alarm_handler()
+                    return wrapped(*args, **kwargs)
+                finally:
+                    wrap_helper.restore_old_alarm_handler()
+            else:
+                try:
+                    timeout_wrapper = Timeout(wrapped, wrap_helper.timeout_exception,
+                                              wrap_helper.exception_message, wrap_helper.dec_timeout, wrap_helper.dec_hard_timeout)
+                    return timeout_wrapper(*args, **kwargs)
+                except PicklingError:
+                    detect_unpickable_objects_and_reraise(wrapped)
+    return wrapper
