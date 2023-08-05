dd616b1d943ca01f9d033beda70c68bbeeba55fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:48 +0000

    Add package languageflow/1.1.11rc0/languageflow-1.1.11rc0.tar.gz

---
+++ packages/languageflow/languageflow-1.1.11rc0.tar.gz/languageflow-1.1.11rc0/languageflow/datasets.py
@@ -0,0 +1,65 @@
+REPO = {
+    "VNESES": {
+        "cache_dir": "datasets/LTA",
+        "type": "Plaintext",
+        "license": "Open",
+        "year": "2017",
+        "filepath": "VNESEScorpus.txt"
+    },
+    "VNTQ_SMALL": {
+        "cache_dir": "datasets/LTA",
+        "type": "Plaintext",
+        "license": "Open",
+        "year": "2017",
+        "filepath": "VNTQcorpus-small.txt"
+    },
+    "VNTQ_BIG": {
+        "cache_dir": "datasets/LTA",
+        "type": "Plaintext",
+        "license": "Open",
+        "year": "2017",
+        "filepath": "VNTQcorpus-big.txt"
+    },
+    "VNTC": {
+        "cache_dir": "datasets/VNTC",
+        "type": "Categorized",
+        "license": "Open",
+        "year": "2007",
+        "filepath": ""
+    },
+    "VLSP2013-WTK": {
+        "cache_dir": "datasets/VLSP2013-WTK",
+        "type": "Tagged",
+        "license": "Close",
+        "year": "2013",
+        "filepath": ""
+    },
+    "VLSP2013-POS": {
+        "cache_dir": "datasets/VLSP2013-POS",
+        "type": "Tagged",
+        "license": "Close",
+        "year": "2013",
+        "filepath": ""
+    },
+    "VTB-CHUNK": {
+        "cache_dir": "datasets/VTB-CHUNK",
+        "type": "Tagged",
+        "license": "Close",
+        "year": "2017",
+        "filepath": ""
+    },
+    "VLSP2016-NER": {
+        "cache_dir": "datasets/VLSP2016-NER",
+        "type": "Tagged",
+        "license": "Close",
+        "year": "2016",
+        "filepath": ""
+    },
+    "VLSP2018-NER": {
+        "cache_dir": "datasets/VLSP2018-NER",
+        "type": "Tagged",
+        "license": "Close",
+        "year": "2018",
+        "filepath": ""
+    }
+}
