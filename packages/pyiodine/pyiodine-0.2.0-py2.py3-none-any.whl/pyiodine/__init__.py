2b4e9c2285be26b1ac5258e7dd75c7336204251b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:52 +0000

    Add package pyiodine/0.2.0/pyiodine-0.2.0-py2.py3-none-any.whl

---
+++ packages/pyiodine/pyiodine-0.2.0-py2.py3-none-any.whl/pyiodine/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for AHR kinetic model."""
+
+__author__ = """Dileep Kishore"""
+__email__ = "dkishore@bu.edu"
+__version__ = "0.2.0"
+
+from .main import AHR_MODEL
