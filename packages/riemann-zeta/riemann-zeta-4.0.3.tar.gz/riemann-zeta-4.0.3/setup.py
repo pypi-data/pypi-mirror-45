561a9f66a23bd436577d023aaede417d66232627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:52 +0000

    Add package riemann-zeta/4.0.3/riemann-zeta-4.0.3.tar.gz

---
+++ packages/riemann-zeta/riemann-zeta-4.0.3.tar.gz/riemann-zeta-4.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='riemann-zeta',
+    version='4.0.3',
+    author='James Prestwich',
+    license='LGPL',
+    packages=find_packages(),
+    package_data={'zeta': ['py.typed']},
+    package_dir={'zeta': 'zeta'},
+    install_requires=[
+        'connectrum',
+        'riemann-tx==1.1.6',
+        'ecdsa',
+        'pycryptodomex'],
+    tests_require=[
+        'tox',
+        'mypy',
+        'flake8',
+        'pytest',
+        'pytest-cov'],
+    python_requires='>=3.6'
+)
