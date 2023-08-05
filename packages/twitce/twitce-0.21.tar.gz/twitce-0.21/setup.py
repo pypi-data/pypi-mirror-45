37b678d1f6ac2021bd7eb5931b171389b0e465a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:02 +0000

    Add package twitce/0.21/twitce-0.21.tar.gz

---
+++ packages/twitce/twitce-0.21.tar.gz/twitce-0.21/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+  name = 'twitce',         
+  packages = ['twitce'],   
+  version = '0.21',      
+  license='MIT',        
+  description = 'Gather userinformation from twitter',   
+  author = 'Wouter Molhoek',                   
+  author_email = 'woutermolhoek@hotmail.com',      
+  url = 'https://github.com/WouterMolhoek/twitse',   
+  download_url = 'https://github.com/WouterMolhoek/twitse/archive/0,1.tar.gz',   
+  keywords = ['Twitter', 'Python', 'BeautifilSoup'],   
+  install_requires=[            
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
