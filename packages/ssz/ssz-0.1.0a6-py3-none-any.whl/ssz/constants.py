bcb7736aa0b45a44f0d617aa467659ef7b79622d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:56 +0000

    Add package ssz/0.1.0a6/ssz-0.1.0a6-py3-none-any.whl

---
+++ packages/ssz/ssz-0.1.0a6-py3-none-any.whl/ssz/constants.py
@@ -0,0 +1,11 @@
+from eth_typing import (
+    Hash32,
+)
+
+CHUNK_SIZE = 32  # named BYTES_PER_CHUNK in the spec
+EMPTY_CHUNK = Hash32(b"\x00" * CHUNK_SIZE)
+
+SIZE_PREFIX_SIZE = 4  # named BYTES_PER_LENGTH_PREFIX in the spec
+MAX_CONTENT_SIZE = 2 ** (SIZE_PREFIX_SIZE * 8) - 1
+
+SIGNATURE_FIELD_NAME = "signature"
---
+++ packages/ssz/ssz-0.1.0a6-py3-none-any.whl/ssz/sedes/byte_vector.py
@@ -0,0 +1,59 @@
+from typing import (
+    Union,
+)
+
+from ssz.exceptions import (
+    SerializationError,
+)
+from ssz.sedes.base import (
+    CompositeSedes,
+)
+from ssz.utils import (
+    merkleize,
+    pack_bytes,
+)
+
+BytesOrByteArray = Union[bytes, bytearray]
+
+
+class ByteVector(CompositeSedes[BytesOrByteArray, bytes]):
+    def __init__(self, size: int) -> None:
+        self.size = size
+
+    #
+    # Size
+    #
+    is_static_sized = True
+
+    def get_static_size(self):
+        return self.size
+
+    #
+    # Serialization
+    #
+    def serialize_content(self, value: BytesOrByteArray) -> bytes:
+        if len(value) != self.size:
+            raise SerializationError(
+                f"Cannot serialize {len(value)} bytes as bytes{self.size}"
+            )
+
+        return value
+
+    #
+    # Deserialization
+    #
+    def deserialize_content(self, content: bytes) -> bytes:
+        return content
+
+    #
+    # Tree hashing
+    #
+    def hash_tree_root(self, value: bytes) -> bytes:
+        serialized_value = self.serialize(value)
+        return merkleize(pack_bytes(serialized_value))
+
+
+bytes4 = ByteVector(4)
+bytes32 = ByteVector(32)
+bytes48 = ByteVector(48)
+bytes96 = ByteVector(96)
