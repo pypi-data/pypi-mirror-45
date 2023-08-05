3815e202dc0ba6a60d9a9dd22c6a9200a2cb3341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:14 +0000

    Add package dodoo/2.0.0rc8/dodoo-2.0.0rc8-py2.py3-none-any.whl

---
+++ packages/dodoo/dodoo-2.0.0rc8-py2.py3-none-any.whl/dodoo/__init__.py
@@ -0,0 +1,9 @@
+# Copyright 2018 ACSONE SA/NV (<http://acsone.eu>)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from .commands import CommandWithOdooEnv  # noqa
+from .env import OdooEnvironment  # noqa
+from .env import odoo  # noqa
+from .env import odoo_bin  # noqa
+from .cli import main  # noqa
+from . import options  # noqa
