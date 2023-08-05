239e4d9c861344d60b2a730b2eba8dfd578ed95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:56 +0000

    Add package magicinvoke/2.2.3/magicinvoke-2.2.3.tar.gz

---
+++ packages/magicinvoke/magicinvoke-2.2.3.tar.gz/magicinvoke-2.2.3/sites/magic_docs/examples/bugtesting/tasks.py
@@ -0,0 +1,23 @@
+from invoke import task, Collection
+from magicinvoke import magictask
+
+ns = Collection()
+
+@ns.magictask
+def mine(ctx, output_path=None):
+    print(ctx)
+    print(output_path + '')
+    pass
+
+@task(mine)
+def mytask(ctx):
+    pass
+
+@ns.task(no_ctx=True)
+def noctxtask():
+    print("hi")
+    return
+
+# Used to test error msgs
+ns.configure({"tasks": {"mine": {"output_path": lambda ctx: "/dev/null"}}})
+#ns.configure({"tasks": {"mine": lambda ctx: 2+{"output_path": lambda ctx: "/dev/null"}}})
