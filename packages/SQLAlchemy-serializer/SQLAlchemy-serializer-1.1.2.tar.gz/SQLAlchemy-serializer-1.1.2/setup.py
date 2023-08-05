58a81c89ed50bd1c5c4dca8000aac97bd3d96038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:07 +0000

    Add package SQLAlchemy-serializer/1.1.2/SQLAlchemy-serializer-1.1.2.tar.gz

---
+++ packages/SQLAlchemy-serializer/SQLAlchemy-serializer-1.1.2.tar.gz/SQLAlchemy-serializer-1.1.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='SQLAlchemy-serializer',
+    version='1.1.2',
+    description='Mixin for SQLAlchemy-models serialization without pain',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Y-Bro',
+    url='https://github.com/n0nSmoker/SQLAlchemy-serializer.git',
+    keywords=['sqlalchemy', 'serialize', 'to_dict', 'JSON'],
+    packages=['sqlalchemy_serializer', 'sqlalchemy_serializer.lib'],
+    install_requires=[
+        'SQLAlchemy',
+    ]
+)
+
+
+
