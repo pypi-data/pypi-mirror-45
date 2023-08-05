75f2cb6ab8589ea0065a0420a23cf165fede46e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:05 +0000

    Add package selinux/0.1.3a1/selinux-0.1.3a1.tar.gz

---
+++ packages/selinux/selinux-0.1.3a1.tar.gz/selinux-0.1.3a1/selinux/__init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""That is shim pure-python module that detects and loads the original selinux
+package from outside the current virtualenv, avoiding a very common error
+inside virtualenvs: ModuleNotFoundError: No module named 'selinux'
+"""
+
+__author__ = """Sorin Sbarnea"""
+__email__ = 'sorin.sbarnea@gmail.com'
+__version__ = '0.1.0'
+
+import imp
+import os
+import platform
+import sys
+import warnings
+
+
+if platform.system() == 'Linux':
+
+    if platform.architecture()[0] == '64bit':
+        arch = 'lib64'
+    else:
+        arch = 'lib'
+
+    location = '/usr/%s/python%s/site-packages/selinux' % \
+        (arch, ".".join(platform.python_version_tuple()[:2]))
+
+    if not os.path.isdir(location):
+        warnings.warn(
+            "Failed to detect selinux python bindings at %s" % location)
+    else:
+        # py27 load_source works with folders but puy36 needs a file
+        sys.modules['selinux'] = \
+            imp.load_source('selinux', "%s/__init__.py" % location)
