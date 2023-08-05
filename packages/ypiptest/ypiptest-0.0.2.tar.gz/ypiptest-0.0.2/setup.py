8892869eb886f986ee933a9c5fbe4315ee5225cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:14 +0000

    Add package ypiptest/0.0.2/ypiptest-0.0.2.tar.gz

---
+++ packages/ypiptest/ypiptest-0.0.2.tar.gz/ypiptest-0.0.2/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name="ypiptest",
+    version="0.0.2",
+    author="gowthambalusamy",
+    author_email="xxxxxxxxxxxxx@gmail.com",
+    description="A small example package",
+    url="https://github.com/pypa/example-project",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
