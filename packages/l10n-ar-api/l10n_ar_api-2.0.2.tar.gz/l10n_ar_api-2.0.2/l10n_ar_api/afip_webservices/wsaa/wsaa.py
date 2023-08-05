32db75a9ca346b733bb2f5ff9716a86355e2480e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:47 +0000

    Add package l10n-ar-api/2.0.2/l10n_ar_api-2.0.2.tar.gz

---
+++ packages/l10n-ar-api/l10n_ar_api-2.0.2.tar.gz/l10n_ar_api-2.0.2/l10n_ar_api/afip_webservices/wsaa/wsaa.py
@@ -0,0 +1,26 @@
+from zeep import Client
+from l10n_ar_api.afip_webservices import config
+
+
+class Wsaa(object):
+
+    def __init__(self, homologation=True, url=None):
+        
+        if not url:
+            self.url = config.authorization_urls.get('homologation') if homologation\
+                else config.authorization_urls.get('production')
+        else:
+            self.url = url
+
+    def login(self, tra):
+        """
+        :param tra: TRA que se usara para el logeo
+        :return: XML con el login autorizado
+        """
+        
+        try:
+            login_fault = Client(self.url).service.loginCms(tra)
+        except Exception as e:
+            raise Exception("Error al autenticarse\n{}".format(e))
+        
+        return login_fault
