291ab8ccf4dabece65f459383a577266023b2d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:44 +0000

    Add package xyscript/0.0.2.1/xyscript-0.0.2.1.tar.gz

---
+++ packages/xyscript/xyscript-0.0.2.1.tar.gz/xyscript-0.0.2.1/xyscript/config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+#-*- encoding:utf-8 -*-
+# from __future__ import print_function
+
+class Config:
+    def get(self,project):
+        print(project)
+        # pass
+    
