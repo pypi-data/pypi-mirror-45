48fee69abe29a6ad6be88b672aab064a136ff3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:26 +0000

    Add package conformity/1.21.0/conformity-1.21.0.tar.gz

---
+++ packages/conformity/conformity-1.21.0.tar.gz/conformity-1.21.0/conformity/fields/meta.py
@@ -0,0 +1,214 @@
+from __future__ import (
+    absolute_import,
+    unicode_literals,
+)
+
+import attr
+import six
+
+from conformity.error import (
+    ERROR_CODE_UNKNOWN,
+    Error,
+)
+from conformity.fields.basic import Base
+from conformity.utils import strip_none
+
+
+@attr.s
+class Nullable(Base):
+    """
+    Accepts the field type passed as the first positional argument or a value of null/None. Introspection is a
+    dictionary with "type" set to "nullable" and key "nullable" set to the introspection of the first positional
+    argument.
+    """
+
+    introspect_type = 'nullable'
+    field = attr.ib()
+
+    def errors(self, value):
+        if value is None:
+            return []
+
+        return self.field.errors(value)
+
+    def introspect(self):
+        return {
+            'type': self.introspect_type,
+            'nullable': self.field.introspect(),
+        }
+
+
+@attr.s
+class Polymorph(Base):
+    """
+    A field which has one of a set of possible contents based on a field
+    within it (which must be accessible via dictionary lookups)
+    """
+
+    introspect_type = 'polymorph'
+    switch_field = attr.ib()
+    contents_map = attr.ib()
+    description = attr.ib(default=None)
+
+    def errors(self, value):
+        # Get switch field value
+        bits = self.switch_field.split('.')
+        switch_value = value
+        for bit in bits:
+            switch_value = switch_value[bit]
+        # Get field
+        if switch_value not in self.contents_map:
+            if '__default__' in self.contents_map:
+                switch_value = '__default__'
+            else:
+                return [
+                    Error('Invalid switch value {}'.format(switch_value), code=ERROR_CODE_UNKNOWN),
+                ]
+        field = self.contents_map[switch_value]
+        # Run field errors
+        return field.errors(value)
+
+    def introspect(self):
+        return strip_none({
+            'type': self.introspect_type,
+            'description': self.description,
+            'switch_field': self.switch_field,
+            'contents_map': {
+                key: value.introspect()
+                for key, value in self.contents_map.items()
+            },
+        })
+
+
+@attr.s
+class ObjectInstance(Base):
+    """
+    Accepts only instances of a given class or type
+    """
+
+    introspect_type = 'object_instance'
+    valid_type = attr.ib()
+    description = attr.ib(default=None)
+
+    def errors(self, value):
+        if not isinstance(value, self.valid_type):
+            return [
+                Error('Not an instance of %s' % self.valid_type.__name__),
+            ]
+        else:
+            return []
+
+    def introspect(self):
+        return strip_none({
+            'type': self.introspect_type,
+            'description': self.description,
+            # Unfortunately, this is the one sort of thing we can't represent
+            # super well. Maybe add some dotted path stuff in here.
+            'valid_type': repr(self.valid_type),
+        })
+
+
+class Any(Base):
+    """
+    Accepts any one of the types passed as positional arguments.
+    Intended to be used for constants but could be used with others.
+    """
+
+    introspect_type = 'any'
+    description = None
+
+    def __init__(self, *args, **kwargs):
+        self.options = args
+        # We can't put a keyword argument after *args in Python 2, so we need this
+        if 'description' in kwargs:
+            self.description = kwargs['description']
+            del kwargs['description']
+        if kwargs:
+            raise TypeError('Unknown keyword arguments: %s' % ', '.join(kwargs.keys()))
+
+    def errors(self, value):
+        result = []
+        for option in self.options:
+            sub_errors = option.errors(value)
+            # If there's no errors from a sub-field, then it's all OK!
+            if not sub_errors:
+                return []
+            # Otherwise, add the errors to the overall results
+            result.extend(sub_errors)
+        return result
+
+    def introspect(self):
+        return strip_none({
+            'type': self.introspect_type,
+            'description': self.description,
+            'options': [option.introspect() for option in self.options],
+        })
+
+
+class All(Base):
+    """
+    The value must pass all of the types passed as positional arguments.
+    Intended to be used for adding extra validation.
+    """
+
+    introspect_type = 'all'
+    description = None
+
+    def __init__(self, *args, **kwargs):
+        self.requirements = args
+        # We can't put a keyword argument after *args in Python 2, so we need this
+        if 'description' in kwargs:
+            self.description = kwargs['description']
+            del kwargs['description']
+        if kwargs:
+            raise TypeError('Unknown keyword arguments: %s' % ', '.join(kwargs.keys()))
+
+    def errors(self, value):
+        result = []
+        for requirement in self.requirements:
+            result.extend(requirement.errors(value) or [])
+        return result
+
+    def introspect(self):
+        return strip_none({
+            'type': self.introspect_type,
+            'description': self.description,
+            'requirements': [requirement.introspect() for requirement in self.requirements],
+        })
+
+
+@attr.s
+class BooleanValidator(Base):
+    """
+    Uses a boolean callable (probably lambda) passed in to validate the value
+    based on if it returns True (valid) or False (invalid).
+    """
+
+    introspect_type = 'boolean_validator'
+    validator = attr.ib()
+    validator_description = attr.ib(validator=attr.validators.instance_of(six.text_type))
+    error = attr.ib(validator=attr.validators.instance_of(six.text_type))
+    description = attr.ib(default=None)
+
+    def errors(self, value):
+        # Run the validator, but catch any errors and return them as an error
+        # as this is maybe in an All next to a type-checker.
+        try:
+            ok = self.validator(value)
+        except Exception:
+            return [
+                Error('Validator encountered an error (invalid type?)'),
+            ]
+        if ok:
+            return []
+        else:
+            return [
+                Error(self.error),
+            ]
+
+    def introspect(self):
+        return strip_none({
+            'type': self.introspect_type,
+            'description': self.description,
+            'validator': self.validator_description,
+        })
---
+++ packages/conformity/conformity-1.21.0.tar.gz/conformity-1.21.0/conformity/validator.py
@@ -0,0 +1,88 @@
+from __future__ import (
+    absolute_import,
+    unicode_literals,
+)
+
+from functools import (
+    partial,
+    wraps,
+)
+import types
+
+
+__all__ = (
+    'PositionalError',
+    'ValidationError',
+    'validate',
+    'validate_call',
+    'validate_method',
+)
+
+
+class ValidationError(ValueError):
+    """
+    Error raised when a value fails to validate.
+    """
+    pass
+
+
+class PositionalError(TypeError):
+    """
+    Error raised when you pass positional arguments into a validated function.
+    """
+    pass
+
+
+def validate(schema, value, noun='value'):
+    """
+    Checks the value against the schema, and raises ValidationError if validation
+    fails.
+    """
+    errors = schema.errors(value)
+    if errors:
+        error_details = ''
+        for error in errors:
+            if error.pointer:
+                error_details += '  - %s: %s\n' % (error.pointer, error.message)
+            else:
+                error_details += '  - %s\n' % error.message
+        raise ValidationError('Invalid %s:\n%s' % (noun, error_details))
+
+
+def validate_call(kwargs, returns, is_method=False):
+    """
+    Decorator which runs validation on a callable's arguments and its return
+    value. Pass a schema for the kwargs and for the return value. Positional
+    arguments are not supported.
+    """
+    def decorator(func):
+        @wraps(func)
+        def inner(*passed_args, **passed_kwargs):
+            # Enforce no positional args
+            # first argument of instance method and class method is always positonal so we need
+            # to make expception for them. Static methods are still validated according to standard rules
+            # this check happens before methods are bound, so instance method is still a regular function
+            max_allowed_passed_args_len = 0
+            if is_method and type(func) in (types.FunctionType, classmethod):
+                max_allowed_passed_args_len = 1
+
+            if len(passed_args) > max_allowed_passed_args_len:
+                raise PositionalError('You cannot call this with positional arguments.')
+            # Validate keyword arguments
+            validate(kwargs, passed_kwargs, 'keyword arguments')
+            # Call callable
+            return_value = func(*passed_args, **passed_kwargs)
+            # Validate return value
+            validate(returns, return_value, 'return value')
+            return return_value
+        inner.__wrapped__ = func
+        # caveat: checking for f.__validated__ will only work if @validate_call
+        # is not masked by other decorators except for @classmethod or @staticmethod
+        inner.__validated__ = True
+        return inner
+    return decorator
+
+
+# use @validate_method for methods. If it's a class or static method,
+# @classdecorator/@staticmethod should be outmost, while @validate_method second outmost
+validate_method = partial(validate_call, is_method=True)
