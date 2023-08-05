25b43fc61c3750bff92df24c91d32fa0f357edec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:14 +0000

    Add package bw2speedups/2.3/bw2speedups-2.3.tar.gz

---
+++ packages/bw2speedups/bw2speedups-2.3.tar.gz/bw2speedups-2.3/bw2speedups/__init__.py
@@ -0,0 +1,5 @@
+__version__ = (2, 3)
+
+from ._indexer import indexer
+from ._find_first import find_first
+from ._consolidate import consolidate
---
+++ packages/bw2speedups/bw2speedups-2.3.tar.gz/bw2speedups-2.3/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, Extension
+# from Cython.Distutils import build_ext
+import numpy as np
+
+SRC_DIR = "bw2speedups"
+
+kwargs = {
+    "libraries": [],
+    "include_dirs": [np.get_include()]
+}
+
+extension_1 = Extension(SRC_DIR + "._indexer",
+                        # [SRC_DIR + "/_indexer.pyx"],
+                        [SRC_DIR + "/_indexer.c"],
+                        **kwargs)
+
+extension_2 = Extension(SRC_DIR + "._find_first",
+                        [SRC_DIR + "/_find_first.c"],
+                        **kwargs)
+
+extension_3 = Extension(SRC_DIR + "._consolidate",
+                        [SRC_DIR + "/_consolidate.c"],
+                        **kwargs)
+
+
+if __name__ == "__main__":
+    setup(
+        # cmdclass = {'build_ext': build_ext},
+        # install_requires=['numpy', 'cython'],
+        author="Chris Mutel",
+        author_email="cmutel@gmail.com",
+        url="https://bitbucket.org/cmutel/brightway2-speedups/",
+        description="""Cython functions to speed up Brightway2 LCA calculations""",
+        long_description=open('README.md').read(),
+        ext_modules = [extension_1, extension_2, extension_3],
+        install_requires=['numpy'],
+        name = 'bw2speedups',
+        packages=[SRC_DIR],
+        version="2.3",
+        zip_safe=False,
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+        ],
+    )
