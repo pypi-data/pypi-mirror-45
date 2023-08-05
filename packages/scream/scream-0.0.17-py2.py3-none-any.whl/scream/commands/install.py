c1740c804c2d62bb495ac79560a47fcbfe2d9012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:42 +0000

    Add package scream/0.0.17/scream-0.0.17-py2.py3-none-any.whl

---
+++ packages/scream/scream-0.0.17-py2.py3-none-any.whl/scream/commands/install.py
@@ -0,0 +1,91 @@
+import logging
+import os
+import subprocess
+
+from scream.detect_changed_packages import get_changed_packages
+from ..package import Package
+
+
+class NoMatchingDistributionException(Exception):
+    pass
+
+
+class PackageInstallationException(Exception):
+    pass
+
+
+def install(package):
+    """
+    Installs individual packages and their dependents then caches them as wheels in the `wheelhouse` dir.
+
+    Args:
+        package (str): A local package name. Ex: `company_examplea`.
+
+    """
+    changed_packages = get_changed_packages()
+
+    package = Package(package_name=package)
+    logging.info("Installing package: `{}`...".format(package.package_name))
+
+    for dependent_package in package.local_dependencies:
+        logging.info("Installing dependency: `{}`...".format(dependent_package.package_name))
+        _install_package(dependent_package, changed_packages)
+
+    # finally install the package that was originally requested.
+    _install_package(package)
+
+    logging.info("Installation complete.")
+
+
+def _install_package(package, changed_packages=None):
+    """
+    Tries to install `package` from the wheelhouse cache.
+    If it doesnt exist:
+        1. Create the wheel, so future packages can skip the work.
+        2. Install from the wheelhouse.
+
+    Args:
+        package (str): a package name to install
+        changed_packages (list[Package])
+    """
+    if changed_packages is None:
+        changed_packages = []
+
+    wheelhouse_dir = os.path.join(os.path.dirname(package.package_dir), "wheelhouse")
+
+    create_wheel_cmd = ["pip", "wheel", "-f", wheelhouse_dir, "-w", wheelhouse_dir, package.package_dir]
+    install_cmd = ["pip", "install", "-f", wheelhouse_dir]
+
+    for other_deps in package.other_dependencies:
+        run(install_cmd + [other_deps])
+    try:
+        # Since we might have changed multiple packages in this commit
+        # Re-build wheels for all changed package dependencies.
+        if package.package_name in changed_packages:
+            logging.info("Package {} has changed, rebuilding wheel.".format(package.package_name))
+            run(create_wheel_cmd)
+
+        run(install_cmd + [package.package_name])
+    except NoMatchingDistributionException:
+        run(create_wheel_cmd)
+        run(install_cmd + [package.package_name])
+
+
+def run(cmd):
+    """A helper to run and catch installation errors.
+    Args:
+        cmd (list): a set of commands to run in the shell.
+
+    Returns:
+        Executes the `cmd`
+    Raises:
+        NoMatchingDistributionException
+    """
+    try:
+        r = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+        logging.info(r.decode("utf-8"))
+    except subprocess.CalledProcessError as err:
+        err_str = err.output.decode("utf-8")
+        if "No matching distribution found" in err_str:
+            raise NoMatchingDistributionException(err_str)
+        raise PackageInstallationException(err_str)
---
+++ packages/scream/scream-0.0.17-py2.py3-none-any.whl/scream/files/__init__.py
@@ -0,0 +1,8 @@
+from .docs import Docs
+from .git import GitIgnore
+from .module import ModuleExample, TestExample
+from .readme import PackageReadme, MonorepoReadme
+from .scream import Scream
+from .setup import SetupCfg, SetupPy
+from .tox import Tox
+from .util import File
