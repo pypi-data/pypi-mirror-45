9a1c3a22fece8159e1bd824bcd1fe6ceb70c8cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:33 +0000

    Add package mezzanine-cartridge-api/1.5.16/mezzanine_cartridge_api-1.5.16.tar.gz

---
+++ packages/mezzanine-cartridge-api/mezzanine_cartridge_api-1.5.16.tar.gz/mezzanine_cartridge_api-1.5.16/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open('readme.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'mezzanine_cartridge_api',
+    version = '1.5.16',
+    author = 'Jack van Zyl',
+    author_email = 'jackvanzyl@icloud.com',
+    description = 'A REST Web API for Mezzanine CMS with the Cartridge e-commerce extension',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/jackvz/mezzanine-cartridge-api',
+    packages = setuptools.find_packages(),
+    data_files=[('readme', ['readme.md']), 
+        ('templates', ['mezzanine_cartridge_api/templates/swagger-ui-all-schemes.html']),
+        ('static', [
+            'mezzanine_cartridge_api/static/swagger-ui-bundle-http.js',
+            'mezzanine_cartridge_api/static/swagger-ui-bundle-https.js',
+            'mezzanine_cartridge_api/static/swagger-ui-standalone-preset.js',
+        ])
+    ],
+    include_package_data=True,
+    install_requires=[
+        'Django==1.11.20',
+        'Mezzanine==4.3.1',
+        'django-custom-settings==0.1.4',
+        'django-oauth-toolkit==1.1.1',
+        'django-cors-middleware==1.3.1',
+        'djangorestframework==3.9.2',
+        'django-rest-framework-braces==0.3.4',
+        'django-enumfields==1.0.0',
+        'djangorestframework-api-key==0.3.1',
+        'drf_yasg==1.15.0'
+    ],
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
