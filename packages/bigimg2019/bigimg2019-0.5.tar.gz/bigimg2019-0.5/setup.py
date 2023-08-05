9d06f69512bf1969184143cbbeecc7c661263186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:51 +0000

    Add package bigimg2019/0.5/bigimg2019-0.5.tar.gz

---
+++ packages/bigimg2019/bigimg2019-0.5.tar.gz/bigimg2019-0.5/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name='bigimg2019',  
+        version='0.5',
+        scripts=['bigimg2019'] ,
+        author="Thomas Nilsson",
+        author_email="tnni@dtu.dk",
+        description="A compilation of functions for Big Imaging 2019",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/thomasnilsson/big-imaging-project",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        install_requires=[
+          'numpy',
+          'matplotlib',
+          'scikit-image',
+          'opencv-python'
+      ],
+)
