d7fef1ff1fbd468669c1858bb74d01982a435fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:54 +0000

    Add package Promium/1.2.6/Promium-1.2.6.tar.gz

---
+++ packages/Promium/Promium-1.2.6.tar.gz/Promium-1.2.6/promium/__init__.py
@@ -0,0 +1,25 @@
+
+from promium.core.base import Page, Block, Element
+from promium.core.elements.checkbox import Checkbox
+from promium.core.elements.radio_button import RadioButton
+from promium.core.elements.upload_button import UploadButton
+from promium.core.elements.link import Link
+from promium.core.elements.input_field import InputField
+from promium.core.elements.drop_downs import DropDown
+from promium.core.elements.textarea_field import TextareaField
+from promium.core.elements.multiselect_listbox import MultiSelectListBox
+
+
+__all__ = (
+    'Page',
+    'Block',
+    'Element',
+    'Checkbox',
+    'RadioButton',
+    'UploadButton',
+    'InputField',
+    'TextareaField',
+    'Link',
+    'DropDown',
+    'MultiSelectListBox',
+)
