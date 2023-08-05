b901e741bc577bc79908e5df7f5e2993137e7bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:09 +0000

    Add package PySnooper/0.0.7/PySnooper-0.0.7.tar.gz

---
+++ packages/PySnooper/PySnooper-0.0.7.tar.gz/PySnooper-0.0.7/setup.py
@@ -0,0 +1,33 @@
+# Copyright 2019 Ram Rachum.
+# This program is distributed under the MIT license.
+
+import setuptools
+
+
+packages = [package for package in setuptools.find_packages()
+            if package != 'tests']
+
+setuptools.setup(
+    name='PySnooper',
+    version='0.0.7',
+    author='Ram Rachum',
+    author_email='ram@rachum.com',
+    description="A poor man's debugger for Python.",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/cool-RR/PySnooper',
+    packages=packages,
+    install_requires=open('requirements.txt', 'r').read().split('\n'),
+    tests_require=open('test_requirements.txt', 'r').read().split('\n'),
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+
+)
