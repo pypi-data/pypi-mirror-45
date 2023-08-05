0405c45304a97d27ac24eebc84642b1b9370f225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:12 +0000

    Add package ibbifunniest/0.1.1/ibbifunniest-0.1.1.tar.gz

---
+++ packages/ibbifunniest/ibbifunniest-0.1.1.tar.gz/ibbifunniest-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='ibbifunniest',
+      version='0.1.1',
+      description='The funniest joke in the world',
+      url='',
+      author='Ihor Bilous',
+      author_email='ihorkobilous@gmail.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
