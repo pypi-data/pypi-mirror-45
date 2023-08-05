f72733cfb6822569b646207abe2ba525eb40e5a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:05 +0000

    Add package divvy/0.3/divvy-0.3.tar.gz

---
+++ packages/divvy/divvy-0.3.tar.gz/divvy-0.3/README.md
@@ -0,0 +1,5 @@
+# <img src="docs/img/divvy_logo.svg" alt="divvy logo" height="70"/>
+
+[![Build Status](https://travis-ci.org/pepkit/divvy.svg?branch=master)](https://travis-ci.org/pepkit/divvy) [![PEP compatible](http://pepkit.github.io/img/PEP-compatible-green.svg)](http://pepkit.github.io)
+
+`divvy` is a python package for reading portable computing environment configuration files, which we call *divvy configuration files*. Complete documentation and API for the `divvy` python package is at [code.databio.org/divvy](http://code.databio.org/divvy).
---
+++ packages/divvy/divvy-0.3.tar.gz/divvy-0.3/divvy/const.py
@@ -0,0 +1,26 @@
+""" Package constants """
+
+__author__ = "Vince Reuter"
+__email__ = "vreuter@virginia.edu"
+
+
+# Compute-related
+COMPUTE_SETTINGS_VARNAME = ["DIVCFG", "PEPENV"]
+DEFAULT_COMPUTE_RESOURCES_NAME = "default"
+COMPUTE_CONSTANTS = ["COMPUTE_SETTINGS_VARNAME",
+                     "DEFAULT_COMPUTE_RESOURCES_NAME"]
+
+# Project-related
+DERIVATIONS_DECLARATION = "derived_attributes"
+IMPLICATIONS_DECLARATION = "implied_attributes"
+SAMPLE_INDEPENDENT_PROJECT_SECTIONS = ["metadata", DERIVATIONS_DECLARATION,
+                                       IMPLICATIONS_DECLARATION, "trackhubs"]
+PROJECT_CONSTANTS = ["IMPLICATIONS_DECLARATION",
+                     "SAMPLE_INDEPENDENT_PROJECT_SECTIONS"]
+
+# Other
+GENERIC_PROTOCOL_KEY = "*"
+OTHER_CONSTANTS = ["GENERIC_PROTOCOL_KEY"]
+
+
+__all__ = COMPUTE_CONSTANTS + PROJECT_CONSTANTS + OTHER_CONSTANTS
