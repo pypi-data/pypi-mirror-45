c8892583370505b47929f60456bbce58dfc1764b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:29 +0000

    Add package distob/0.3.3/distob-0.3.3.tar.gz

---
+++ packages/distob/distob-0.3.3.tar.gz/distob-0.3.3/distob/tests/test_distob.py
@@ -0,0 +1,11 @@
+"""An empty file where tests should be
+"""
+
+import pytest
+import distob
+
+#TODO Use pytest fixtures to create an ipyparallel local cluster for tests
+#     (check how the ipyparallel test suite does it)
+
+def test_engines():
+    pass
