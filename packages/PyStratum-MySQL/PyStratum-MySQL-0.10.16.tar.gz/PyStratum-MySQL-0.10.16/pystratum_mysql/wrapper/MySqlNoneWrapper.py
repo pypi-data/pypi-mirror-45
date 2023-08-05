beca5e84bec85bcecee711b0018e65bc9363cb40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:36 +0000

    Add package PyStratum-MySQL/0.10.16/PyStratum-MySQL-0.10.16.tar.gz

---
+++ packages/PyStratum-MySQL/PyStratum-MySQL-0.10.16.tar.gz/PyStratum-MySQL-0.10.16/pystratum_mysql/wrapper/MySqlNoneWrapper.py
@@ -0,0 +1,17 @@
+"""
+PyStratum
+"""
+from pystratum.wrapper.NoneWrapper import NoneWrapper
+from pystratum_mysql.wrapper.MySqlWrapper import MySqlWrapper
+
+
+class MySqlNoneWrapper(MySqlWrapper, NoneWrapper):
+    """
+    Wrapper method generator for stored procedures without any result set.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def _write_result_handler(self, routine):
+        self._write_line('return StaticDataLayer.execute_sp_none({0!s})'.format(self._generate_command(routine)))
+
+# ----------------------------------------------------------------------------------------------------------------------
---
+++ packages/PyStratum-MySQL/PyStratum-MySQL-0.10.16.tar.gz/PyStratum-MySQL-0.10.16/pystratum_mysql/wrapper/MySqlSingleton1Wrapper.py
@@ -0,0 +1,18 @@
+"""
+PyStratum
+"""
+from pystratum.wrapper.Singleton1Wrapper import Singleton1Wrapper
+from pystratum_mysql.wrapper.MySqlWrapper import MySqlWrapper
+
+
+class MySqlSingleton1Wrapper(MySqlWrapper, Singleton1Wrapper):
+    """
+    Wrapper method generator for stored procedures that are selecting 1 row with one column only.
+    """
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def _write_result_handler(self, routine):
+        self._write_line(
+            'return StaticDataLayer.execute_sp_singleton1({0!s})'.format(str(self._generate_command(routine))))
+
+# ----------------------------------------------------------------------------------------------------------------------
