dfe31a56f1812676a41665ce49fe6818d43b2482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:14 +0000

    Add package diffino/0.1.1/diffino-0.1.1.tar.gz

---
+++ packages/diffino/diffino-0.1.1.tar.gz/diffino-0.1.1/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup
+
+VERSION = "0.1.1"
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="diffino",
+    version=VERSION,
+    packages=["diffino"],
+    include_package_data=True,
+    install_requires=required,
+    entry_points={'console_scripts': ['diffino = diffino.cli:main']},
+    author="BriteCore",
+    description="Diffing tools for comparing datasets in CSV, XLSX and other formats",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    keywords="diffing comparing csv excel json"
+)
