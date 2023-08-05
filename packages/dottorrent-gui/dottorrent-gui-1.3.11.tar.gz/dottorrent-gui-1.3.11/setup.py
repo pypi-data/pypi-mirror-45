6936f7f24c91bb6830d786556d7b5c3f3ef5df63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:16 +0000

    Add package dottorrent-gui/1.3.11/dottorrent-gui-1.3.11.tar.gz

---
+++ packages/dottorrent-gui/dottorrent-gui-1.3.11.tar.gz/dottorrent-gui-1.3.11/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open('dottorrentGUI/version.py') as f:
+	exec(f.read())
+
+setup(
+    name="dottorrent-gui",
+    version=__version__,
+    packages=find_packages(),
+    entry_points={
+        'gui_scripts': [
+            'dottorrent-gui = dottorrentGUI.gui:main'
+        ]
+    },
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=['dottorrent>=1.9.2', 'humanfriendly', 'PyQt5>=5.7'],
+
+    # metadata for upload to PyPI
+    author="Kevin Zhang",
+    author_email="kevin@kevinzhang.me",
+    description="An advanced GUI torrent file creator with batch functionality, powered by PyQt and dottorrent",
+    long_description=open('README.rst').read(),
+    keywords="bittorrent torrent",
+    url="https://github.com/kz26/dottorrent-gui",   # project home page, if any
+
+    # could also include long_description, download_url, classifiers, etc.
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)
