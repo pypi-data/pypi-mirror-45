d072bdfccdc4e1af32c8b446c5dce973987de893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:52 +0000

    Add package version-dem/0.0.4/version_dem-0.0.4.tar.gz

---
+++ packages/version-dem/version_dem-0.0.4.tar.gz/version_dem-0.0.4/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='version_dem',
+    version='0.0.4',
+    author='avneesh gupta',
+    author_email='avneesh.gupta@paytm.com',
+    description="Just demo for checking how is version work",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/paytm/paytm-pg-python-sdk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
