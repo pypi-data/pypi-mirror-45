1da450e817f6df04e3516be22133c37e638f235d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:27 +0000

    Add package sukanyaw-helloworld-library/1.0/sukanyaw-helloworld-library-1.0.tar.gz

---
+++ packages/sukanyaw-helloworld-library/sukanyaw-helloworld-library-1.0.tar.gz/sukanyaw-helloworld-library-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="sukanyaw-helloworld-library",
+    version='1.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/sukanyaw/demo-helloworld-library',
+    author='Sukanya',
+    author_email='sukanya.wh@gmail.com',
+)
