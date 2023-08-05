968404b8a05e74375188f3450bc1c334dfc330f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:41 +0000

    Add package mautrix/0.4.0.dev30/mautrix-0.4.0.dev30-py3-none-any.whl

---
+++ packages/mautrix/mautrix-0.4.0.dev30-py3-none-any.whl/mautrix/client/api/types/__init__.py
@@ -0,0 +1,33 @@
+# Copyright (c) 2018 Tulir Asokan
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from .primitive import UserID, EventID, RoomID, RoomAlias, FilterID, ContentURI, SyncToken
+from .filter import Filter, EventFilter, RoomFilter, RoomEventFilter
+from .event import (EventType,
+
+                    RedactionEvent, RedactionEventContent,
+
+                    MessageEvent, MessageEventContent, MessageUnsigned, MediaMessageEventContent,
+                    LocationMessageEventContent, LocationInfo, InReplyTo, MessageType, Format,
+                    MediaInfo, FileInfo, AudioInfo, VideoInfo, ImageInfo, ThumbnailInfo,
+                    TextMessageEventContent, BaseMessageEventContent, MatchedCommand, RelatesTo,
+                    MatchedPassiveCommand, BaseFileInfo,
+
+                    PowerLevelStateEventContent, Membership, MemberStateEventContent, StateEvent,
+                    AliasesStateEventContent, CanonicalAliasStateEventContent, StrippedStateEvent,
+                    RoomNameStateEventContent, RoomTopicStateEventContent, StateUnsigned,
+                    RoomAvatarStateEventContent, StateEventContent,
+
+                    AccountDataEvent, AccountDataEventContent, RoomTagInfo,
+                    RoomTagAccountDataEventContent,
+
+                    Event, EventContent)
+from .misc import (RoomCreatePreset, RoomDirectoryVisibility, PaginationDirection, RoomAliasInfo,
+                   RoomDirectoryResponse, DirectoryPaginationToken, PaginatedMessages, Presence,
+                   PresenceState)
+from .users import User, Member, UserSearchResults
+from .media import MediaRepoConfig, MXOpenGraph, OpenGraphVideo, OpenGraphImage, OpenGraphAudio
+from .util import (SerializerError, Serializable, SerializableEnum, SerializableAttrs,
+                   serializer, deserializer)
