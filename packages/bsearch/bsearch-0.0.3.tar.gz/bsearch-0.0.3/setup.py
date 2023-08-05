a0e95c7f851826edef4e7d3eaf02c568d8d8152a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:30 +0000

    Add package bsearch/0.0.3/bsearch-0.0.3.tar.gz

---
+++ packages/bsearch/bsearch-0.0.3.tar.gz/bsearch-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="bsearch",
+    version="0.0.3",
+    description="Binary search - bisect_left and bisect_right with more flexible comparison",
+    author='Dominic Slee',
+    author_email='domslee1@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=[
+        'bsearch',
+    ],
+    package_dir={'': 'bsearch'},
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    url='https://github.com/domsleee/bsearch',
+    license='BSD'
+)
