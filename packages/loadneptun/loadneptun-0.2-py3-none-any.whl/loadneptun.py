77526b8665831494d7c43d2d6c55a3b9344f7ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:53 +0000

    Add package loadneptun/0.2/loadneptun-0.2-py3-none-any.whl

---
+++ packages/loadneptun/loadneptun-0.2-py3-none-any.whl/loadneptun.py
@@ -0,0 +1,21 @@
+def loadneptun(versionNumber):  
+    import pandas as pd 
+    
+    df = pd.read_csv(f"https://github.com/bradib0y/neptun_db/raw/master/neptun_db_v{versionNumber}.csv", sep="\t", encoding="utf-8")
+    
+    df.set_index(['Key', 'FélévSorszám'], inplace=True)
+
+    df['KépzésStátusz'] = df['KépzésStátusz'].astype('category')
+    df['FélévesStátusz'] = df['FélévesStátusz'].astype('category')
+    df['Pénzügyi státusz'] = df['Pénzügyi státusz'].astype('category')
+    df['Képzési szint'] = df['Képzési szint'].astype('category')
+    df['Modulkód'] = df['Modulkód'].astype('category')
+    df['Tagozat'] = df['Tagozat'].astype('category')
+    df['Nem'] = df['Nem'].astype('category')
+    df['Irányítószám'] = df['Irányítószám'].astype('category')
+
+    return df
+
+if __name__ == "__main__":
+    import sys
+    loadneptun(int(sys.argv[1]))
