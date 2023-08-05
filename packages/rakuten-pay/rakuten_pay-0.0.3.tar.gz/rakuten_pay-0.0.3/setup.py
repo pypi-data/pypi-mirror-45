cfb354c6a27adec38ea33be5d9ee5b449a8a7bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:07 +0000

    Add package rakuten-pay/0.0.3/rakuten_pay-0.0.3.tar.gz

---
+++ packages/rakuten-pay/rakuten_pay-0.0.3.tar.gz/rakuten_pay-0.0.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='rakuten_pay',
+    version='0.0.3',
+    url='https://github.com/ivcmartello/rakuten_pay',
+    license='MIT License',
+    author='Ivan Carlos Martello',
+    author_email='ivcmartello@gmail.com',
+    keywords='rakuten pay payment pagamento ecommerce',
+    description=u'Rackuten Pay Package',
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+)
