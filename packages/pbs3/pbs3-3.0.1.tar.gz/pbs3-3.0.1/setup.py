23b36bc443bed45a935010f9198986b039c1d943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:53 +0000

    Add package pbs3/3.0.1/pbs3-3.0.1.tar.gz

---
+++ packages/pbs3/pbs3-3.0.1.tar.gz/pbs3-3.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name             = 'pbs3',
+        version          = '3.0.1',
+        description      = 'pbs3 is a python package for launching external processes easily.',
+        long_description = open('README.md').read(),
+        license          = 'MIT',
+        url              = 'http://github.com/xapple/pbs3/',
+        author           = 'Lucas Sinclair',
+        author_email     = 'lucas.sinclair@me.com',
+        py_modules       = ["pbs3"],
+    )
