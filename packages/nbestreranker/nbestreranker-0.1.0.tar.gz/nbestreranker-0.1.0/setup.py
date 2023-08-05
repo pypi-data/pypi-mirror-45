d5aa6a9a3a52b087a8bcb4d6a9db2638c73c7060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:02 +0000

    Add package nbestreranker/0.1.0/nbestreranker-0.1.0.tar.gz

---
+++ packages/nbestreranker/nbestreranker-0.1.0.tar.gz/nbestreranker-0.1.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='nbestreranker',
+    version="0.1.0",
+    keywords=("pip", "nbest", "ranker"),
+    description="nbest reranker nlp",
+    long_description="nbest reranker",
+    license="MIT Licence",
+    url="https://github.com/GodsLeft",
+    author="GodsLeft",
+    author_email="1456466573@qq.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["numpy", "kenlm", "configreader", "imp", "argparse"]
+)
