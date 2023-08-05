f3f7297bc90fc0ed1576698a9a96d719944b19cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:40 +0000

    Add package Terry_toolkit/0.0.1.4/Terry-toolkit-0.0.1.4.tar.gz

---
+++ packages/Terry_toolkit/Terry-toolkit-0.0.1.4.tar.gz/Terry-toolkit-0.0.1.4/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+setup(
+    name='Terry-toolkit',
+    version='0.0.1.4',
+    description='Terry toolkit',
+    author='Terry Chan',
+    author_email='napoler2008@gmail.com',
+    url='https://terry-toolkit.terrychan.org/zh/master/',
+
+    # packages=find_packages(),
+    packages=['Terry_toolkit'])
+    #install_requires=['jieba'])
+
+# python3 setup.py sdist
+# #python3 setup.py install
+# python3 setup.py sdist upload
