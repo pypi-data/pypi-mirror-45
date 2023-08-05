fc52e238835a049c87587f293b28f3037ec2b658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:39 +0000

    Add package gnuhealth-client/3.4.4/gnuhealth-client-3.4.4.tar.gz

---
+++ packages/gnuhealth-client/gnuhealth-client-3.4.4.tar.gz/gnuhealth-client-3.4.4/tryton/gui/window/view_form/screen/__init__.py
@@ -0,0 +1,3 @@
+# This file is part of the GNU Health GTK Client.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+from screen import *
---
+++ packages/gnuhealth-client/gnuhealth-client-3.4.4.tar.gz/gnuhealth-client-3.4.4/tryton/gui/window/view_form/view/list_gtk/__init__.py
@@ -0,0 +1,2 @@
+# This file is part of the GNU Health GTK Client.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
