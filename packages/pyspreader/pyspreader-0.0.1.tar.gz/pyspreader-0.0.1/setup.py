8900fcef1628021c0af96cad4884bdd77b1ebb04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:05 +0000

    Add package pyspreader/0.0.1/pyspreader-0.0.1.tar.gz

---
+++ packages/pyspreader/pyspreader-0.0.1.tar.gz/pyspreader-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyspreader",
+    version="0.0.1",
+    author="Luke Croteau",
+    author_email="luke.j.croteau@gmail.com",
+    description="Python Reference client for the Spreader distributed work system",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LukeCroteau/pyspreader_client_package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
