cc96af44d23c60066f8746688d785000e833be35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:59 +0000

    Add package whopy/0.1/whopy-0.1.tar.gz

---
+++ packages/whopy/whopy-0.1.tar.gz/whopy-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='whopy',
+      version='0.1',
+      description='Module for retrieving and parsing the WHOIS data for a domain. Supports most domains.',
+      author='Andrew Shamah',
+      author_email='amshamah@gmail.com',
+      url='https://github.com/amshamah419/whopy',
+      packages=['whopy'],
+      package_dir={"whopy":"whopy"},
+      package_data={"whopy": ["*.dat", "*.json"]},
+      install_requires=['argparse'],
+      provides=['whopy'],
+      license="MIT"
+     )
