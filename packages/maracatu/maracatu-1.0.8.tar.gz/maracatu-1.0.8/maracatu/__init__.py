4a61e3147daf5aac93bdc7d39165db6d69568eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:53 +0000

    Add package maracatu/1.0.8/maracatu-1.0.8.tar.gz

---
+++ packages/maracatu/maracatu-1.0.8.tar.gz/maracatu-1.0.8/maracatu/__init__.py
@@ -0,0 +1,6 @@
+
+from maracatu.src.base.scatter import Scatter
+from maracatu.src.base.curves import Curves
+from maracatu.src.base.error_bar import ErrorBar
+from maracatu.src.base.plot_base import PlotBase
+from maracatu.src.base.parameters import Parameter
