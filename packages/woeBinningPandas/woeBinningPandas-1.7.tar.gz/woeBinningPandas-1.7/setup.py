63bd8ed31b21923d5c7ff605c9d559336eced441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:59 +0000

    Add package woeBinningPandas/1.7/woeBinningPandas-1.7.tar.gz

---
+++ packages/woeBinningPandas/woeBinningPandas-1.7.tar.gz/woeBinningPandas-1.7/setup.py
@@ -0,0 +1,26 @@
+import os
+from setuptools import setup
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+ 
+setup(
+    name='woeBinningPandas',
+    version='1.7',
+    packages=['woeBinning'],
+    include_package_data=True,
+    license='MIT',  # example license
+    description='My package from github repo.',
+    long_description=README,
+    url='https://github.com/V1ad98/woeBinningPandas.git',
+    author='V1ad98',
+    author_email='47775603+V1ad98@users.noreply.github.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
