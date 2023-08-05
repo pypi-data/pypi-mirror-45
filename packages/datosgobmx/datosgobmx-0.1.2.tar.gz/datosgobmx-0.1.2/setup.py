ff3917b86306b3a7fce3b88545413d31ed4ab1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:21 +0000

    Add package datosgobmx/0.1.2/datosgobmx-0.1.2.tar.gz

---
+++ packages/datosgobmx/datosgobmx-0.1.2.tar.gz/datosgobmx-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'datosgobmx',
+  packages = ['datosgobmx'], # this must be the same as the name above
+  version = '0.1.2',
+  description = 'Connect to api.datos.gob.mx',
+  author = 'Leo Castaneda',
+  author_email = 'leonel.castaneda@datos.mx',
+  url = 'https://github.com/mxabierto/api_python_client',
+  download_url = 'https://github.com/mxabierto/api_python_client/archive/0.1.2.tar.gz',
+  keywords = ['ckan', 'dgm', 'open data'],
+  classifiers = []
+)
