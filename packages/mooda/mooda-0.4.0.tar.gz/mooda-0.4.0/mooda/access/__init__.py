34b83c878fce49d3819e4277be70f359e936e7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:08 +0000

    Add package mooda/0.4.0/mooda-0.4.0.tar.gz

---
+++ packages/mooda/mooda-0.4.0.tar.gz/mooda-0.4.0/mooda/access/__init__.py
@@ -0,0 +1,8 @@
+""" Write here the imports"""
+
+from mooda.access.egim import EGIM
+from mooda.access.pangaea import Pangaea
+from mooda.access.licor import Licor
+from mooda.access.hobo import Hobo
+from mooda.access.seaandsun import SeaAndSun
+from mooda.access.seabird import SeaBird
