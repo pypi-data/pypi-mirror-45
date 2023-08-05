52d7b597b72cd4264b14d179630f051bd8405fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:52 +0000

    Add package version-dem/0.0.3/version_dem-0.0.3-py3-none-any.whl

---
+++ packages/version-dem/version_dem-0.0.3-py3-none-any.whl/version_demo/akg.py
@@ -0,0 +1,18 @@
+class Util:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    @staticmethod
+    def hello_world():
+        print("hello world")
+
+    def sum(self):
+        return self.a + self.b
+
+    def sum_extended(self):
+        return self.a * self.a + self.b
+
+    def sum_extended_version2(self):
+        return self.a * self.a + self.b * self.b
