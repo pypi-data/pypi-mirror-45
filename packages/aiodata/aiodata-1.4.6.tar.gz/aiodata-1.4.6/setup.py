4ad2ebbd3f88d2c64b0be6ea5565525412ea66db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:28 +0000

    Add package aiodata/1.4.6/aiodata-1.4.6.tar.gz

---
+++ packages/aiodata/aiodata-1.4.6.tar.gz/aiodata-1.4.6/setup.py
@@ -0,0 +1,48 @@
+import setuptools
+
+with open('README.md') as file:
+
+    readme = file.read()
+
+name = 'aiodata'
+
+module = __import__(name)
+
+version = module.__version__
+
+author = 'Exahilosys'
+
+url = f'https://github.com/{author}/{name}'
+
+download_url = f'{url}/archive/v{version}.tar.gz'
+
+setuptools.setup(
+    name = name,
+    version = version,
+    author = author,
+    author_email = 'exahilosys@gmail.com',
+    url = url,
+    download_url = download_url,
+    packages = setuptools.find_packages(),
+    license = 'MIT',
+    description = 'HTTP database access.',
+    long_description = readme,
+    long_description_content_type = 'text/markdown',
+    include_package_data = True,
+    install_requires = [
+        'aiohttp',
+        'yarl',
+        'aiocogs',
+        'peewee'
+    ],
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ]
+)
