1348d2ae9f9a00deb3e533d0ec3c4e038a021655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:28 +0000

    Add package django-bom/1.69/django-bom-1.69.tar.gz

---
+++ packages/django-bom/django-bom-1.69.tar.gz/django-bom-1.69/bom/local_settings.py
@@ -0,0 +1,15 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'supersecretkey'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+BOM_CONFIG = {
+    'octopart_api_key': '46b1a32d',
+}
+
+# google GoogleOAuth
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '449904336434-mlab3bm7v0fvvocbb8qifg19vjr66fun.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'uwEEKQEPeoGIIXM9OWuNlARB'
---
+++ packages/django-bom/django-bom-1.69.tar.gz/django-bom-1.69/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-bom',
+    version='1.69',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='A simple Django app to manage a bill of materials.',
+    long_description=README,
+    url='https://www.indabom.com/',
+    author='Mike Kasparian',
+    author_email='mpkasp@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'python-social-auth',
+        'social-auth-app-django',
+        'google-api-python-client',
+        'django-materializecss-form',
+    ],
+)
