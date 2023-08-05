1ad015b53800516ef85db9e033187dbe2042ad4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:36 +0000

    Add package oriole-api/5.0.0/oriole_api-5.0.0-py3-none-any.whl

---
+++ packages/oriole-api/oriole_api-5.0.0-py3-none-any.whl/oriole_api/__init__.py
@@ -0,0 +1,10 @@
+"""Code for webapi.
+
+1. Add sso for pyramid.
+2. Add json for response.
+3. Add microservice for webapi.
+"""
+
+from oriole_api.main import check_factory
+from oriole_api.main import run as includeme
+from oriole_api.json import error_response
