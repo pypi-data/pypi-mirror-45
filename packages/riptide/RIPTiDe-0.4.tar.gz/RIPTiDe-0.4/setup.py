910db74c300430a8f199e3333fca03649081ef1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:47 +0000

    Add package riptide/0.4/RIPTiDe-0.4.tar.gz

---
+++ packages/riptide/RIPTiDe-0.4.tar.gz/RIPTiDe-0.4/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='RIPTiDe',
+	version='0.4', 
+	description='Reaction Inclusion by Parsimonious usage and Transcript Distribution',
+	author='Matt Jenior',
+	author_email='mattjenior@gmail.com',
+	url='https://github.com/mjenior/riptide',
+	packages=['riptide'],
+    license='MIT License',
+    long_description=open('README.txt').read()
+	)
