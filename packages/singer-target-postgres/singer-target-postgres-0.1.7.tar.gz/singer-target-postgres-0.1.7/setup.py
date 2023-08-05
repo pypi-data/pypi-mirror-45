630c0d6dc4090b40d164912116899193ed85ae6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:41 +0000

    Add package singer-target-postgres/0.1.7/singer-target-postgres-0.1.7.tar.gz

---
+++ packages/singer-target-postgres/singer-target-postgres-0.1.7.tar.gz/singer-target-postgres-0.1.7/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from os import path
+
+from setuptools import setup, find_packages
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='singer-target-postgres',
+    url='https://github.com/datamill-co/target-postgres',
+    author='datamill',
+    version="0.1.7",
+    description='Singer.io target for loading data into postgres',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    py_modules=['target_postgres'],
+    install_requires=[
+        'arrow==0.13.0',
+        'jsonschema==2.6.0',
+        'psycopg2==2.7.7',
+        'psycopg2-binary==2.7.7',
+        'singer-python==5.4.1'
+    ],
+    setup_requires=[
+        "pytest-runner"
+    ],
+    tests_require=[
+        "chance==0.110",
+        "Faker==0.9.2",
+        "pytest==3.9.3"
+    ],
+    entry_points='''
+      [console_scripts]
+      target-postgres=target_postgres:cli
+    ''',
+    packages=find_packages()
+)
