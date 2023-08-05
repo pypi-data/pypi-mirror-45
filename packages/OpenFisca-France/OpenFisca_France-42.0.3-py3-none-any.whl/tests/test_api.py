1f0a5dae35e67c44b6716d119e967ab05d4b4f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:08 +0000

    Add package OpenFisca-France/42.0.3/OpenFisca_France-42.0.3-py3-none-any.whl

---
+++ packages/OpenFisca-France/OpenFisca_France-42.0.3-py3-none-any.whl/tests/test_api.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+from unittest import TestCase
+
+
+class TestNewApi(TestCase):
+
+    def setUp(self):
+        self.process = subprocess.Popen(['openfisca', 'serve', '--country-package', 'openfisca_france'])
+
+    def tearDown(self):
+        self.process.terminate()
+
+    def test_response(self):
+        try:
+            subprocess.check_call(['wget', '--quiet', '--retry-connrefused', '--waitretry=1', '--tries=10', 'http://localhost:5000/parameters', '--output-document=/dev/null'])
+        except subprocess.CalledProcessError:
+            raise subprocess.CalledProcessError("Could not reach OpenFisca Web API at localhost:5000 after 10s")
+        except OSError:
+            try:
+                subprocess.check_call(['wget', '--version'])
+            except OSError:
+                raise OSError("Check if 'wget'(https://www.gnu.org/software/wget/) is installed on your computer")
