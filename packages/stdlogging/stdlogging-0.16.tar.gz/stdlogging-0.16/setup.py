dc4e43d6eae64ab3841d46f9f40f1dbf0919cfb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:03 +0000

    Add package stdlogging/0.16/stdlogging-0.16.tar.gz

---
+++ packages/stdlogging/stdlogging-0.16.tar.gz/stdlogging-0.16/setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2018 fx-kirin
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
+setup(
+    name='stdlogging',
+    version='0.16',
+    description='Capture stdout and stderr output by logging',
+    long_description='',
+    author='fx-kirin',
+    author_email='ono.kirin@gmail.com',
+    url='https://github.com/fx-kirin/py-stdlogging',
+    download_url='',
+    packages=['']
+)
