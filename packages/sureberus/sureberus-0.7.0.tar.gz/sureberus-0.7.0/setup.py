7bfa9bd99c6d6d1e40228e8bc0ec9ea1b9d9e37d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:13 +0000

    Add package sureberus/0.7.0/sureberus-0.7.0.tar.gz

---
+++ packages/sureberus/sureberus-0.7.0.tar.gz/sureberus-0.7.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import setuptools
+
+setuptools.setup(
+    name="sureberus",
+    version="0.7.0",
+    description="Cerberus alternative",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="http://github.com/radix/sureberus/",
+    author="Christopher Armstrong",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["sureberus"],
+    install_requires=["six", "attrs"],
+)
