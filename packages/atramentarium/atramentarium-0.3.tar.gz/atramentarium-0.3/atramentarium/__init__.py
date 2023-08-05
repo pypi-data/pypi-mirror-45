98cedd93a0b91bc92b3e31dddfdfa60fbd4a61c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:01 +0000

    Add package atramentarium/0.3/atramentarium-0.3.tar.gz

---
+++ packages/atramentarium/atramentarium-0.3.tar.gz/atramentarium-0.3/atramentarium/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from atramentarium.atramentarium import parse_command_list
+from atramentarium.atramentarium import prompt
+from atramentarium.atramentarium import CommandCompleter
