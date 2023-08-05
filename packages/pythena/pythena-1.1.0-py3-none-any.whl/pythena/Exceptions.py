c90947dc9b6c75aa422aafa7e82528932dad8f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:57 +0000

    Add package pythena/1.1.0/pythena-1.1.0-py3-none-any.whl

---
+++ packages/pythena/pythena-1.1.0-py3-none-any.whl/pythena/Exceptions.py
@@ -0,0 +1,26 @@
+class QueryExecutionTimeoutException(Exception):
+    pass
+
+
+class InvalidS3PathException(Exception):
+    pass
+
+
+class QueryExecutionFailedException(Exception):
+    pass
+
+
+class QueryUnknownStatusException(Exception):
+    pass
+
+
+class NoRegionFoundError(Exception):
+    pass
+
+
+class DatabaseNotFound(Exception):
+    pass
+
+
+class QueryNotSupported(Exception):
+    pass
