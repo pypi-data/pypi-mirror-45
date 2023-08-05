b3df8fa905970866f86673453559a25be3c2028f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:07 +0000

    Add package sqlitebiter/0.27.3/sqlitebiter-0.27.3.tar.gz

---
+++ packages/sqlitebiter/sqlitebiter-0.27.3.tar.gz/sqlitebiter-0.27.3/sqlitebiter/_const.py
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import, unicode_literals
+
+
+PROGRAM_NAME = "sqlitebiter"
+MAX_VERBOSITY_LEVEL = 2
+
+IPYNB_FORMAT_NAME_LIST = ["ipynb"]
+TABLE_NOT_FOUND_MSG_FORMAT = "convertible table not found in {}"
