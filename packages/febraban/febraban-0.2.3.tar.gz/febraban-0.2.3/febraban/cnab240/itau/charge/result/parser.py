dea8a39d49f67cb335bc197ac3105fbb2ac30123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:34 +0000

    Add package febraban/0.2.3/febraban-0.2.3.tar.gz

---
+++ packages/febraban/febraban-0.2.3.tar.gz/febraban-0.2.3/febraban/cnab240/itau/charge/result/parser.py
@@ -0,0 +1,82 @@
+from .occurrences import occurrences
+
+
+class SlipResponseStatus:
+
+    registered = "registered"
+    paid = "paid"
+    canceled = "canceled"
+    overdue = "overdue"
+    failed = "failed"
+    unknown = "unknown"
+
+
+class SlipResponse:
+
+    def __init__(self, identifier=None, occurrences=None, content=None, amountInCents=None, fine=None):
+        self.identifier = identifier
+        self.occurrences = occurrences
+        self.amountInCents = amountInCents
+        self.fine = fine
+        self.content = content or []
+
+    def occurrencesText(self):
+        return [occurrences[occurrenceId] for occurrenceId in self.occurrences]
+
+    def occurrencesTextAtIndex(self, index):
+        occurrenceId = self.occurrences[index]
+        return occurrences[occurrenceId]
+
+    def status(self):
+        if "02" in self.occurrences:
+            return SlipResponseStatus.registered
+        if "03" in self.occurrences:
+            return SlipResponseStatus.failed
+        if "06" in self.occurrences:
+            return SlipResponseStatus.paid
+        if "08" in self.occurrences:
+            return SlipResponseStatus.paid
+        if "09" in self.occurrences:
+            return SlipResponseStatus.canceled
+        return SlipResponseStatus.unknown
+
+    def contentText(self, breakLine="\n"):
+        return breakLine.join(self.content)
+
+
+class SlipParser:
+
+    @classmethod
+    def parseFile(cls, file):
+        lines = file.readlines()
+        return cls._parseLines(lines)
+
+    @classmethod
+    def parseText(cls, text):
+        lines = text.splitlines()[:-1]
+        return cls._parseLines(lines)
+
+    @classmethod
+    def _parseLines(cls, lines):
+        result = []
+        for line in lines:
+            if line[7] == "1":
+                currentResponse = SlipResponse()
+            elif line[7] == "3":
+                currentResponse.content.append(line)
+            if line[13] == "T":
+                currentResponse.amountInCents = int(line[81:96])
+                currentResponse.occurrences = [line[15:17]]
+                currentResponse.identifier = line[105:130].strip()
+            elif line[13] == "U":
+                currentResponse.fine = int(line[17:32])
+                result.append(currentResponse)
+                currentResponse = SlipResponse()
+            elif line[13] == "P":
+                currentResponse.amountInCents = int(line[85:100])
+                currentResponse.occurrences = [line[15:17]]
+                currentResponse.identifier = line[195:220].strip()
+            elif line[13] == "Q":
+                result.append(currentResponse)
+                currentResponse = SlipResponse()
+        return result
