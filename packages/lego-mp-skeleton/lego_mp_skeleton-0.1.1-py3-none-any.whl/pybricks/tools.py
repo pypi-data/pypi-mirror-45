68c6d184f39f986256c3038b0c04a0f3c26dbec0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:03 +0000

    Add package lego-mp-skeleton/0.1.1/lego_mp_skeleton-0.1.1-py3-none-any.whl

---
+++ packages/lego-mp-skeleton/lego_mp_skeleton-0.1.1-py3-none-any.whl/pybricks/tools.py
@@ -0,0 +1,21 @@
+from sys import stdout
+
+def print(value, sep='', end='\n', file=stdout, flush=False):
+    return
+
+def wait(time):
+    return
+
+class StopWatch():
+
+    def time(self):
+        return
+
+    def pause(self):
+        return
+
+    def resume(self):
+        return
+
+    def reset(self):
+        return
