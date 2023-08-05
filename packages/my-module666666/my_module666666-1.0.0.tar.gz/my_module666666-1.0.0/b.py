33b63a5999d7370a78fa9b75e6ffa40c79717746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:43 +0000

    Add package my-module666666/1.0.0/my_module666666-1.0.0.tar.gz

---
+++ packages/my-module666666/my_module666666-1.0.0.tar.gz/my_module666666-1.0.0/b.py
@@ -0,0 +1,8 @@
+#--coding:utf-8--
+#创建于：2019/4/17 15:44
+
+# def func_b():
+#     print('我是{}的func_b模块'.format(__name__))
+
+
+print(1234456)
---
+++ packages/my-module666666/my_module666666-1.0.0.tar.gz/my_module666666-1.0.0/setup.py
@@ -0,0 +1,14 @@
+#--coding:utf-8--
+#创建于：2019/4/18 17:08
+
+from distutils.core import setup
+setup (
+name = 'my_module666666',
+version='1.0.0',
+author = 'ben',
+author_email='62680109@qq.com',
+
+url = 'http://www.xxxxx.com',
+description='测试',
+py_modules = ['b'],
+)
