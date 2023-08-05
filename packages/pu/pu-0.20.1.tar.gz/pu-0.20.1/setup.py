0d99e465fbeba541223bd9c56284c8e743f359bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:29 +0000

    Add package pu/0.20.1/pu-0.20.1.tar.gz

---
+++ packages/pu/pu-0.20.1.tar.gz/pu-0.20.1/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import os.path
+import sys
+
+from setuptools import setup
+
+import pu
+
+classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+
+def read(fname):
+    fname = os.path.join(os.path.dirname(__file__), fname)
+    if sys.version > '3.0':
+        content = open(fname, encoding='utf-8').read()
+    else:
+        content = open(fname).read().decode('utf-8')
+    return content.strip()
+
+
+def read_files(*fnames):
+    return '\r\n\r\n\r\n'.join(map(read, fnames))
+
+
+setup(
+    name='pu',
+    version=pu.version,
+    packages=[
+        'pu',
+        'pu.aio',
+        'pu.aio.protocols',
+        'pu.pattern',
+        'pu.misc',
+    ],
+    description='Python utils',
+    long_description=read_files('README.md', 'CHANGELOG.md'),
+    long_description_content_type='text/markdown',
+    license='GNU Library or Lesser General Public License (LGPL)',
+    author='yuxin',
+    author_email='ycyuxin@gmail.com',
+    url='https://github.com/huyx/pu',
+    keywords=['python', 'util'],
+    classifiers=classifiers,
+)
