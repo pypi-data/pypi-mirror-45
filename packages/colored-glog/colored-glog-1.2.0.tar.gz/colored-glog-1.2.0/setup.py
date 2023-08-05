f203fff23ee48db5cbaa34319cf9a3dfad5beb24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:22 +0000

    Add package colored-glog/1.2.0/colored-glog-1.2.0.tar.gz

---
+++ packages/colored-glog/colored-glog-1.2.0.tar.gz/colored-glog-1.2.0/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+import setuptools
+
+VERSION = '1.2.0'
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+setuptools.setup(
+    name='colored-glog',
+    author='Xiangquan Xiao',
+    author_email='xiaoxiangquan@gmail.com',
+    url='https://github.com/xiaoxq/colored-glog',
+    install_requires=[
+        'absl-py',
+        'termcolor',
+    ],
+    description='Colored Google-style logging wrapper for Python.',
+    long_description=README,
+    py_modules=['colored_glog'],
+    license='BSD',
+    test_suite='tests',
+    version=VERSION,
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: System :: Logging',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    platforms='any',
+)
