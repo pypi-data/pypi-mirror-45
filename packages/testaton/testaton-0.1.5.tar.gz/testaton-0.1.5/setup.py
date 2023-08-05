58affbdffa5f7632b33501486999180b75191307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:11 +0000

    Add package testaton/0.1.5/testaton-0.1.5.tar.gz

---
+++ packages/testaton/testaton-0.1.5.tar.gz/testaton-0.1.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+readme = open('README.md').read().strip()
+
+setup(
+    name='testaton',
+    version='0.1.5',
+    license='MIT',
+    author='Michael Farrugia',
+    author_email='mike.farrugia@gmail.com',
+    url='https://github.com/mikelupu/testaton',
+    description='A command line tool to allow the testing of datasets',
+    long_description=readme,
+    packages=find_packages(),
+    install_requires=[
+        # put packages here
+        'six',
+        'findspark',
+        'pandas',
+        'sqlalchemy',
+        'dtest-framework',
+        'pyhamcrest'
+    ],
+    test_suite='tests',
+    entry_points={'console_scripts': ['testaton = testaton.cli:main']}
+)
