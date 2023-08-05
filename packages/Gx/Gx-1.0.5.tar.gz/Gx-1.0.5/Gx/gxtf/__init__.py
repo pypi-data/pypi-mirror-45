e0d2c171ba4d4c886935ffa333e8661983b3e763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:40 +0000

    Add package Gx/1.0.5/Gx-1.0.5.tar.gz

---
+++ packages/Gx/Gx-1.0.5.tar.gz/Gx-1.0.5/Gx/gxtf/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Author: Gx
+# @Date:   2019-04-15 15:19:08
+# @Last Modified by:   Administrator
+# @Last Modified time: 2019-04-18 08:39:10
+'''
+ultilize the tensorflow library to deal with the problem in the question
+contains:
+	1. ty.py, which contain the main func, like BN、fully-Connected、CNN etc... to be updateds
+	2. ...to be updated
+'''
---
+++ packages/Gx/Gx-1.0.5.tar.gz/Gx-1.0.5/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @Author: Gx
+# @Date:   2019-04-15 15:10:25
+# @Last Modified by:   Administrator
+# @Last Modified time: 2019-04-18 09:23:17
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+'Mylife code'
+setup(
+    name='Gx',
+    version='1.0.5',
+    description=(
+        'A Scavenger `s code in his whole life'
+    ),
+    long_description=('A Scavenger in his hometown\n 大风起兮云飞扬\n威加海内兮归故乡\n安得猛士兮守四方 '),
+    author='WRQ',
+    author_email='wrq2149678246@qq.com',
+    maintainer='wrq,',
+    maintainer_email='wrq2149678246@qq.com',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
