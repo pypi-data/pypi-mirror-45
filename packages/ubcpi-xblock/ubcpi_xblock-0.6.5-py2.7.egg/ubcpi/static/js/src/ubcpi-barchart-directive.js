4c1ee74a74cc216a5ec3abd667eeebac30ff3d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:10 +0000

    Add package ubcpi-xblock/0.6.5/ubcpi_xblock-0.6.5-py2.7.egg

---
+++ packages/ubcpi-xblock/ubcpi_xblock-0.6.5-py2.7.egg/ubcpi/static/js/src/ubcpi-barchart-directive.js
@@ -0,0 +1,39 @@
+angular.module('UBCPI').
+    directive('piBarchart', ['gettext', function(gettext){
+        return {
+            restrict: 'E',
+            scope: {
+                options: '=',
+                stats: '=',
+                correct: '=',
+                answer: '=',
+                role: '='
+            },
+            // no overwrite template
+            replace: false,
+            link: function(scope, element) {
+                // watch the stats as it could be async populated
+                scope.$watch('stats', function(stats) {
+                    if(!stats) {
+                        return;
+                    }
+
+                    var data = [];
+                    for (var i = 0; i < scope.options.length; i++) {
+                        data.push({
+                            frequency: i in stats ? stats[i] : 0,
+                            label: 'Option ' + (i + 1) + (scope.correct == i ? ' (correct)' : ''),
+                            class: 'ubcpibar' +  (scope.correct == i ? ' correct-answer' : '')
+                        });
+                    }
+
+                    // generate the chart
+                    var chartLayout = d3.custom.barChart(scope, gettext);
+
+                    d3.select(element[0])
+                        .datum(data)
+                        .call(chartLayout)
+                }, true)
+            }
+        }
+    }]);
