43f6282ce2b04a9893f89a933cca95181c4af19b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:13 +0000

    Add package flask-io/1.14.2/flask-io-1.14.2.tar.gz

---
+++ packages/flask-io/flask-io-1.14.2.tar.gz/flask-io-1.14.2/README.rst
@@ -0,0 +1,27 @@
+=================================
+Flask-IO
+=================================
+Flask-IO is a library for parsing Flask request arguments into parameters and for serialization of complex objects into Flask response.
+
+Installation
+===============
+You can install Flask-IO via Python Package Index (PyPi_),::
+
+    $ pip install flask-io
+
+Documentation
+===============
+Full documentation is available at https://flask-io.readthedocs.org/.
+
+Feedback
+===============
+Please use the Issues_ for feature requests and troubleshooting usage.
+
+.. |Version| image:: https://badge.fury.io/py/flask-io.svg?
+   :target: http://badge.fury.io/py/flask-io
+
+.. _example: https://github.com/viniciuschiele/flask-io/tree/master/example
+
+.. _PyPi: https://pypi.python.org/pypi/flask-io
+
+.. _Issues: https://github.com/viniciuschiele/flask-io/issues
---
+++ packages/flask-io/flask-io-1.14.2.tar.gz/flask-io-1.14.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='flask-io',
+    version='1.14.2',
+    packages=['flask_io'],
+    url='https://github.com/viniciuschiele/flask-io',
+    license='MIT',
+    author='Vinicius Chiele',
+    author_email='vinicius.chiele@gmail.com',
+    description='Flask-IO is a library for parsing Flask request arguments into parameters and for serialization of complex objects into Flask response.',
+    keywords=['flask', 'rest', 'parse', 'encode', 'decode', 'request', 'json', 'marshmallow'],
+    install_requires=['flask>=0.10.1', 'python-dateutil>=2.4.2', 'marshmallow>=2.9.1,<3'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ]
+)
