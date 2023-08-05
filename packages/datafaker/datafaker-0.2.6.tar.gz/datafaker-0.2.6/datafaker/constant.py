2049abc7207ce83083d1754ef759762f94a0f291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:09 +0000

    Add package datafaker/0.2.6/datafaker-0.2.6.tar.gz

---
+++ packages/datafaker/datafaker-0.2.6.tar.gz/datafaker-0.2.6/datafaker/constant.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+__version__ = '0.2.6'
+
+# 每次批量插入数据条数
+RDB_BATCH_SIZE = 10
+HBASE_BATCH_SIZE = 10
+ES_BATCH_SIZE = 10
+
+# 产生流数据间隔时间(秒)
+DEFAULT_INTERVAL = 1
+
+# 输出数据格式
+TEXT_FORMAT = 'text'
+JSON_FORMAT = 'json'
+DEFAULT_FORMAT = TEXT_FORMAT
+
+# 语言类型
+DEFAULT_LOCALE = 'zh_CN'
+
+# ENUM类型，从文件中读取枚举值
+ENUM_FILE = 'file://'
+
+# 判断哪些需要加上引号
+STR_TYPES = ['date', 'time', 'datetime', 'char', 'varchar', 'tinyblob',
+             'tinytext', 'text', 'mediumtext', 'longtext', 'string']
+
+
+INT_TYPES = ['tinyint', 'smallint', 'mediumint', 'int', 'integer', 'bigint', ]
+
+FLOAT_TYPES = ['float', 'double', 'decimal', ]
+
+
+
---
+++ packages/datafaker/datafaker-0.2.6.tar.gz/datafaker-0.2.6/datafaker/utils.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import datetime
+import functools
+import os
+import json
+import re
+import time
+
+from datafaker.compat import safe_decode, safe_encode
+from datafaker.constant import RDB_BATCH_SIZE, STR_TYPES, INT_TYPES, FLOAT_TYPES
+from datafaker.drivers import load_sqlalchemy
+from datafaker.exceptions import FileNotFoundError
+from datafaker.reg import reg_keyword, reg_cmd, reg_args, reg_integer, reg_int, reg_all_int
+
+
+def save2file(items, outfile):
+    """
+    将数据保存到文件
+    :param items:
+    :param outfile:
+    :param spliter:
+    :return:
+    """
+    with open(outfile, 'w') as fp:
+        fp.writelines(items)
+
+
+def save2db(items, table, schema, connect):
+    """
+    保存数据到mysql, hive
+    :param items: 保存的数据，list
+    :param table: 表名
+    :param schema: 表shema
+    :param connect: 数据库连接信息
+    :return:
+    """
+    session = load_sqlalchemy(connect)
+
+    names = [column['name'] for column in schema]
+    ctypes = [column['ctype'] for column in schema]
+
+    # 构造数据格式，字符串需要加上单引号
+    formats = ["'%s'" if ctype in STR_TYPES else "%s" for ctype in ctypes]
+    names_format = "(" + ",".join(formats) + ")"
+    batches = [items[i:i + RDB_BATCH_SIZE] for i in range(0, len(items), RDB_BATCH_SIZE)]
+    column_names = ','.join(names)
+    i = 0
+    for batch in batches:
+        batch_value = []
+        for row in batch:
+            batch_value.append(names_format % tuple(row))
+        sql = u"insert into {table} ({column_names}) values {values}".format(
+            table=table, column_names=column_names, values=u','.join([safe_decode(item) for item in batch_value]))
+        session.execute(sql)
+        i += RDB_BATCH_SIZE
+        print('insert %d records' % i)
+    session.commit()
+
+
+def json_item(column_names, item):
+    map = dict(zip(column_names, item))
+    return json.dumps(map)
+
+
+def count_time(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        ret = func(*args, **kwargs)
+        now = time.time()
+        timeused = float((now - start))
+        print('time used: %.3f s' % timeused)
+        return ret
+    return wrapper
+
+
+def read_file_lines(filepath):
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(filepath)
+    with open(filepath) as fp:
+        lines = fp.read().splitlines()
+        # start with # is comment line, and filter empty line
+        lines = [line for line in lines if line and not line.startswith("#") and line.strip()]
+    return lines
+
+
+def diffdate(date1, date2):
+    """
+    #计算两个日期相差天数，自定义函数名，和两个日期的变量名。
+    :param date1:
+    :param date2:
+    :return:
+    """
+    date1 = datetime.datetime.strptime(date1, '%Y-%m-%d')
+    date2 = datetime.datetime.strptime(date2, '%Y-%m-%d')
+
+    return (date2-date1).days
+
+
+def process_op_args(arg, lst_name):
+    """
+    解析op标记的参数
+    将c12*c2+c22 解析成 columns[12]*columns[2]+columns[22]
+    
+    :param arg: 
+    :param lst_name: 
+    :return: 
+    """
+    digits = sorted(reg_all_int(arg), reverse=True)
+
+    for digit in digits:
+         arg = arg.replace('c%d' % digit, 'c[%d]' % digit)
+    arg = arg.replace('c', lst_name)
+
+    return arg
