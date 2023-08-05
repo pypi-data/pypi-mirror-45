8c7654af60a07eeccfb4202425738f3513e9f489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:23 +0000

    Add package awacs/0.9.0/awacs-0.9.0.tar.gz

---
+++ packages/awacs/awacs-0.9.0.tar.gz/awacs-0.9.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='awacs',
+    version='0.9.0',
+    description="AWS Access Policy Language creation library",
+    author="Mark Peek",
+    author_email="mark@peek.org",
+    url="https://github.com/cloudtools/awacs",
+    license="New BSD license",
+    packages=find_packages(),
+    test_suite="tests",
+    use_2to3=True,
+    requires=['slimit'],
+)
