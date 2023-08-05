f73ba4faa111149195ceda461ea0835ca68229a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:58 +0000

    Add package kerncraft/0.8.0/kerncraft-0.8.0.tar.gz

---
+++ packages/kerncraft/kerncraft-0.8.0.tar.gz/kerncraft-0.8.0/kerncraft/headers/kerncraft.h
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+inline void* aligned_malloc(size_t size, size_t align) {
+    // Based on http://stackoverflow.com/q/16376942
+    void *result;
+    #if defined(_MSC_VER)
+        result = _aligned_malloc(size, align);
+    #elif defined(__INTEL_COMPILER)
+         result = _mm_malloc(size, align);
+    #else
+         if(posix_memalign(&result, align, size)) result = 0;
+    #endif
+    return result;
+}
+
+void swap(void* a, void* b) {
+    void* t = a;
+    a = b;
+    b = t;
+}
