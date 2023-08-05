968d641a46f6449a95506fef6c84065175cec4a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:21 +0000

    Add package zappa-secrets-manager/0.5.0/zappa_secrets_manager-0.5.0.tar.gz

---
+++ packages/zappa-secrets-manager/zappa_secrets_manager-0.5.0.tar.gz/zappa_secrets_manager-0.5.0/setup.py
@@ -0,0 +1,22 @@
+import os
+
+from setuptools import setup
+
+about = {}
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'zappa_secrets_manager', '__version__.py'), 'r') as f:
+    exec(f.read(), about)
+
+setup(name='zappa_secrets_manager',
+      version=about['__version__'],
+      description='Enables seamless zappa integration with AWS Secrets '
+                  'Manager while still allowing local development via '
+                  'environment variables',
+      author='AgileTek Engineering',
+      author_email='',
+      license='MIT',
+      packages=['zappa_secrets_manager', ],
+      install_requires=['python-dotenv', 'boto3',],
+      include_package_data=True,
+      zip_safe=False)
