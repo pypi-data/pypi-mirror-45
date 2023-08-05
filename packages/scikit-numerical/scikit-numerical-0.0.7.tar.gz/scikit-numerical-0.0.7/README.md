4a1ac6eb3b89a2f3327fa6877f54cd9115b0120e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:30 +0000

    Add package scikit-numerical/0.0.7/scikit-numerical-0.0.7.tar.gz

---
+++ packages/scikit-numerical/scikit-numerical-0.0.7.tar.gz/scikit-numerical-0.0.7/README.md
@@ -0,0 +1,23 @@
+## Tools for numerical math calculations
+
+[![Build Status](https://travis-ci.com/Bellator95/scikit-numerical.svg?branch=master)](https://travis-ci.com/Bellator95/scikit-numerical)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/e599de8c6de048ef8351811388c63632)](https://www.codacy.com/app/maksym.shpakovych/numerical?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=Bellator95/scikit-numerical&amp;utm_campaign=Badge_Grade)
+[![Codacy Badge](https://api.codacy.com/project/badge/Coverage/e599de8c6de048ef8351811388c63632)](https://www.codacy.com/app/maksym.shpakovych/numerical?utm_source=github.com&utm_medium=referral&utm_content=Bellator95/scikit-numerical&utm_campaign=Badge_Coverage)
+
+This repository contains tools for math numerical computation such as numerical integration and interpolation. The current implementation contains:
+
+- numerical integration using Gauss formula
+
+```python
+import numpy as np
+from numerical.integration import gauss
+from numerical.area import bound
+from numerical.area import grid
+
+def f(x):
+    return np.power(x[0], 2)
+
+bd = bound.LineBoundary1D(0, 1)
+gd = grid.UniformGrid(bd, step=[0.02])
+gauss.integrate(f, gd)
+```
---
+++ packages/scikit-numerical/scikit-numerical-0.0.7.tar.gz/scikit-numerical-0.0.7/numerical/area/grid.py
@@ -0,0 +1,77 @@
+import numpy as np
+from abc import ABCMeta, abstractmethod
+
+
+class _BaseGrid(metaclass=ABCMeta):
+    """ Base class for grid kinds. """
+    @abstractmethod
+    def _build_nodes(self):
+        """ Builds grid on predefined boundary. """
+
+    @abstractmethod
+    def __repr__(self):
+        """ Defines str representation of grid object. """
+
+
+class _GridIterable(metaclass=ABCMeta):
+    """ The class represents iterator interface for grid. """
+
+    def __init__(self, data: list):
+        self._data = data
+        self.dim = len(data)
+
+    def __iter__(self):
+        self._grid_id = -1
+        return self
+
+    def __next__(self):
+        if self._grid_id == self.dim - 1:
+            raise StopIteration
+        self._grid_id += 1
+        return self._data[self._grid_id]
+
+    def __getitem__(self, item):
+        return self._data[item]
+
+    def __repr__(self):
+        return "<[" + ", ".join([grd.__repr__() for grd in self._data]) + "]>"
+
+
+class _UniformGrid(_BaseGrid):
+    """ Class defines uniform grid object.
+
+    'Uniform' means that distance between nearest nodes is the same
+    for each node and defines as 'step'.
+    """
+    def __init__(self, bound, step):
+        self._bound = bound
+        self._step = step
+
+        self._build_nodes()
+        self._build_integration_meta()
+
+    def _build_nodes(self):
+        self._nodes = np.arange(self._bound.start, self._bound.end, self._step)
+        self._nodes = np.append(self._nodes, self._bound.end)
+
+    def _build_integration_meta(self):
+        self.sum = ((self._nodes[1:] + self._nodes[:-1]) / 2.0).reshape(-1, 1)
+        self.diff = (self._nodes[1:] - self._nodes[:-1]) / 2.0
+        self.diff_t = self.diff.reshape(-1, 1)
+        self.nodes_count = len(self._nodes)
+
+    def get_nodes(self):
+        return self._nodes
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__}: " \
+            f"bound={str(self._bound)}, " \
+            f"step={str(self._step)}, " \
+            f"nodes_count={len(self._nodes)}>"
+
+
+class UniformGrid(_GridIterable):
+    def __init__(self, bound: "area boundary", step: list):
+        super().__init__(
+            [_UniformGrid(bd, st) for bd, st in zip(bound, step)]
+        )
