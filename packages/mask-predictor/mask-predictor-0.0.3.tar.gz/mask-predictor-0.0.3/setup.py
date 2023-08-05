d8a8d74cd8e43aa97c4b961aea2ff76de63d71c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:16 +0000

    Add package mask-predictor/0.0.3/mask-predictor-0.0.3.tar.gz

---
+++ packages/mask-predictor/mask-predictor-0.0.3.tar.gz/mask-predictor-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mask-predictor",
+    version="0.0.3",
+    author="Tan Kim Yong",
+    author_email="kimyong95@gmail.com",
+    description="A wrapper of BERT to predict the covered word in a sentence",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "torch",
+        "spacy",
+        "numpy",
+        "sklearn",
+        "pytorch-pretrained-bert",
+    ],
+    dependency_links=[
+        "https://github.com/explosion/spacy-models/releases/download/en_core_web_lg-2.1.0/en_core_web_lg-2.1.0.tar.gz"
+    ],
+)
