945a095c62f637f4c267161c9968d5d46a02edb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:10 +0000

    Add package rdfunniest/0.1/rdfunniest-0.1.tar.gz

---
+++ packages/rdfunniest/rdfunniest-0.1.tar.gz/rdfunniest-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rdfunniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='',
+      author='Flying Circus',
+      author_email='rdmytriv@icloud.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
