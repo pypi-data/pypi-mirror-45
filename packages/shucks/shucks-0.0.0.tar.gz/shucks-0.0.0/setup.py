496ef8d7243d1f1422b70ec07a5780ac7cda0e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:12 +0000

    Add package shucks/0.0.0/shucks-0.0.0.tar.gz

---
+++ packages/shucks/shucks-0.0.0.tar.gz/shucks-0.0.0/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open('README.md') as file:
+
+    readme = file.read()
+
+name = 'shucks'
+
+module = __import__(name)
+
+version = module.__version__
+
+author = 'Exahilosys'
+
+url = f'https://github.com/{author}/{name}'
+
+setuptools.setup(
+    name = name,
+    version = version,
+    author = author,
+    author_email = 'exahilosys@gmail.com',
+    url = url,
+    packages = setuptools.find_packages(),
+    license = 'MIT',
+    description = 'Mini schema validator.',
+    long_description = readme,
+    long_description_content_type = 'text/markdown',
+    include_package_data = True,
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ]
+)
