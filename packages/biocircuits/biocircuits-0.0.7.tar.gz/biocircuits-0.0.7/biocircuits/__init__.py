5291a7e88b1c4ab9955167dcedc80ac87426db99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:49 +0000

    Add package biocircuits/0.0.7/biocircuits-0.0.7.tar.gz

---
+++ packages/biocircuits/biocircuits-0.0.7.tar.gz/biocircuits-0.0.7/biocircuits/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for biocircuits"""
+
+__all__ = ['viz', 'utils', 'reg', 'dynsys']
+
+from .viz import *
+from .utils import *
+from .reg import *
+from .dynsys import *
+
+__author__ = """Justin Bois"""
+__email__ = 'bois@caltech.edu'
+__version__ = '0.0.7'
