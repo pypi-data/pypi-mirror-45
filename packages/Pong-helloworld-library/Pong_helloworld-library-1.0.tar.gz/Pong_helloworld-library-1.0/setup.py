3118c524a8826e29c05d048d82ba8ff3398f6716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:27 +0000

    Add package Pong-helloworld-library/1.0/Pong_helloworld-library-1.0.tar.gz

---
+++ packages/Pong-helloworld-library/Pong_helloworld-library-1.0.tar.gz/Pong_helloworld-library-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Pong_helloworld-library",
+    version='1.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/soemphong/demo-helloworld-library',
+    author='Soemphong',
+    author_email='soemphong.j@gmail.com',
+)
