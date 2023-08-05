fe67050cddc2488e48c36d12f0a156e1f662953d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:51 +0000

    Add package auto-red-test/0.2.1/auto-red-test-0.2.1.tar.gz

---
+++ packages/auto-red-test/auto-red-test-0.2.1.tar.gz/auto-red-test-0.2.1/auto_red_test/dict_replace.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import collections
+LIST_COUNT = 2
+
+
+def replace_dict(from_dict, to_dict):
+    _replace_dict(None, from_dict, to_dict)
+
+
+def replace_param(attr_name, attr_type, from_dict, to_dict):
+    replace_func = _REPLACE_FUNC[attr_type]
+    return replace_func(attr_name, from_dict, to_dict)
+
+
+def _replace_item(attr_name, from_dict, to_dict):
+    to_dict[attr_name] = from_dict[attr_name]
+
+
+def _replace_list(attr_name, from_dict, to_dict):
+    from_list = from_dict[attr_name]
+    to_list = to_dict[attr_name]
+
+    from_length = len(from_list)
+    if from_length == 0:
+        to_dict[attr_name] = from_dict[attr_name]
+        return
+
+    from_type = __get_value_type(from_list[0])
+    to_type = __get_value_type(to_list[0])
+    if from_type == to_type:
+        if from_type == 'dict':
+
+            if from_length < LIST_COUNT:
+                del to_list[-1]
+            elif from_length > LIST_COUNT:
+                for i in range(from_length - LIST_COUNT):
+                    to_list.append(to_list[-1])
+
+            for i in range(from_length):
+                replace_param(None, from_type, from_list[i], to_list[i])
+        else:
+            to_dict[attr_name] = from_dict[attr_name]
+
+
+def _replace_dict(attr_name, from_dict, to_dict):
+    if attr_name:
+        inner_from_dict = from_dict[attr_name]
+        inner_to_dict = to_dict[attr_name]
+
+        first_key = inner_to_dict.keys()[0]
+        if first_key.endswith('***') or not (
+           type(first_key) == unicode or type(first_key) == str):
+            to_dict[attr_name] = from_dict[attr_name]
+            return
+    else:
+        inner_from_dict = from_dict
+        inner_to_dict = to_dict
+
+    for key, value in inner_to_dict.items():
+        if key in inner_from_dict.keys():
+            from_type = __get_value_type(inner_from_dict[key])
+            to_type = __get_value_type(value)
+            if from_type == to_type:
+                replace_param(key, from_type, inner_from_dict, inner_to_dict)
+            elif inner_from_dict[key] is None:  # 弥补None导致类型不符的特殊情况
+                inner_to_dict[key] = inner_from_dict[key]
+
+
+_REPLACE_FUNC = {
+    'default': _replace_item,
+    'list': _replace_list,
+    'dict': _replace_dict,
+}
+
+
+def __get_value_type(value):
+    if type(value) == list or type(value) == set:
+        return 'list'
+    elif type(value) == dict or type(value) == collections.OrderedDict:
+        return 'dict'
+    else:
+        return 'default'
