93ab4dd634bb297070a78ba77decb7dec6a728e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:42 +0000

    Add package torsiondrive/0.9.6/torsiondrive-0.9.6.tar.gz

---
+++ packages/torsiondrive/torsiondrive-0.9.6.tar.gz/torsiondrive-0.9.6/setup.py
@@ -0,0 +1,27 @@
+"""
+torsiondrive
+Dihedral scanner with wavefront propagation
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+DOCLINES = __doc__.split("\n")
+
+setup(
+    name='torsiondrive',
+    author='Yudong Qiu, Lee-Ping Wang',
+    description=DOCLINES[0],
+    long_description="\n".join(DOCLINES[2:]),
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(),
+    entry_points={'console_scripts': [
+        'torsiondrive-launch = torsiondrive.launch:main',
+        'torsiondrive-api = torsiondrive.td_api:main',
+    ]},
+    url='https://github.com/lpwgroup/torsiondrive',
+    install_requires=[
+        'numpy>=1.11',
+        'geometric>=0.9.5'
+    ]
+)
