08fa917f1a0d06dbc8dc4b90c55a70dc47d31529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:27 +0000

    Add package Noon-helloworld-library/1.0/Noon-helloworld-library-1.0.tar.gz

---
+++ packages/Noon-helloworld-library/Noon-helloworld-library-1.0.tar.gz/Noon-helloworld-library-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Noon-helloworld-library",
+    version='1.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/ekkawuts/demo-helloworld-library',
+    author='Ekkawut',
+    author_email='ekkawut.saeng@hotmail.com',
+)
