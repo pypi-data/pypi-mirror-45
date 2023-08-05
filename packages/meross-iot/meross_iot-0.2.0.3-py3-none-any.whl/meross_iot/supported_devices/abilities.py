2684a6501834dfd329d3341cfd0403a778065af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:38 +0000

    Add package meross-iot/0.2.0.3/meross_iot-0.2.0.3-py3-none-any.whl

---
+++ packages/meross-iot/meross_iot-0.2.0.3-py3-none-any.whl/meross_iot/supported_devices/abilities.py
@@ -0,0 +1,8 @@
+# Abilities
+TOGGLE = 'Appliance.Control.Toggle'
+TOGGLEX = 'Appliance.Control.ToggleX'
+TRIGGER = 'Appliance.Control.Trigger'
+TRIGGERX = 'Appliance.Control.TriggerX'
+ELECTRICITY = 'Appliance.Control.Electricity'
+CONSUMPTIONX = 'Appliance.Control.ConsumptionX'
+ONLINE = 'Appliance.System.Online'
