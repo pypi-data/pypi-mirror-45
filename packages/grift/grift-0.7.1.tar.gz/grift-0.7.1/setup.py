972ef8de8387858568be2a156e95d4a744528ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:22 +0000

    Add package grift/0.7.1/grift-0.7.1.tar.gz

---
+++ packages/grift/grift-0.7.1.tar.gz/grift-0.7.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+package_name = 'grift'
+
+setup(name=package_name,
+      version='0.7.1',
+      description='A clean approach to app configuration',
+      keywords='app config configuration schema python',
+      maintainer_email='grift-maintainer@kensho.com',
+      url='https://github.com/kensho-technologies/grift',
+      packages=find_packages(),
+      package_data={package_name + '.tests': ['*.json']},
+      install_requires=[
+          'schematics==1.1.1',
+          'requests>=2.13.0',
+          'six>=1.11.0,<2',
+      ],
+      )
