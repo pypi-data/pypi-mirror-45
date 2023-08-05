af0224fd8e9933db2dbc07102df7e110f07e12ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:53 +0000

    Add package btcbox-client/0.1.1/btcbox-client-0.1.1.tar.gz

---
+++ packages/btcbox-client/btcbox-client-0.1.1.tar.gz/btcbox-client-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+try:
+    import setuptools
+    from setuptools import setup, find_packages
+except ImportError:
+    print("Please install setuptools.")
+
+import os
+long_description = 'btcbox-client is a python client (sync/async) library for btcbox api.'
+if os.path.exists('README.txt'):
+    long_description = open('README.txt').read()
+
+setup(
+    name  = 'btcbox-client',
+    version = '0.1.1',
+    description = 'btcbox-client is a python client (sync/async) library for btcbox api.',
+    long_description = long_description,
+    license = 'MIT',
+    author = '10mohi6',
+    author_email = '10.mohi.6.y@gmail.com',
+    url = 'https://github.com/10mohi6/btcbox-api-python-client',
+    keywords = 'btcbox',
+    packages = find_packages(),
+    install_requires = ['grequests'],
+    classifiers = [
+      'Development Status :: 5 - Production/Stable',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.7',
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'License :: OSI Approved :: MIT License'
+    ]
+)
