9c53d5ff117154e049f2d75cb23c18dae23b2715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:04 +0000

    Add package squad/0.63/squad-0.63.tar.gz

---
+++ packages/squad/squad-0.63.tar.gz/squad-0.63/squad/frontend/static/squad/build.js
@@ -0,0 +1,52 @@
+import {AnnotationController} from './controllers/annotation.js'
+import {BuildSettingsController} from './controllers/buildSettings.js'
+import {FilterController} from './controllers/filter.js'
+import {ResubmitController} from './controllers/resubmit.js'
+import {Config as appConfig} from './config.js'
+
+var app = angular.module('Build', []);
+
+app.value('build', window.build);
+
+appConfig(app, ['httpProvider']);
+
+app.controller(
+    'AnnotationController',
+    [
+        '$scope',
+        '$http',
+        '$httpParamSerializerJQLike',
+        AnnotationController
+    ]
+)
+
+app.controller(
+    'BuildSettingsController',
+    [
+        '$scope',
+        '$http',
+        'build',
+        BuildSettingsController
+    ]
+)
+
+app.controller(
+    'FilterController',
+    [
+        '$scope',
+        '$attrs',
+        '$location',
+        FilterController
+    ]
+)
+
+app.controller(
+    'ResubmitController',
+    [
+        '$scope',
+        '$http',
+        '$location',
+        '$timeout',
+        ResubmitController
+    ]
+);
