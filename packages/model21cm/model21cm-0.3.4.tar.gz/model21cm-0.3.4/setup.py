e822ad313b0911f66a090e9382b6655190b06889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:39 +0000

    Add package model21cm/0.3.4/model21cm-0.3.4.tar.gz

---
+++ packages/model21cm/model21cm-0.3.4.tar.gz/model21cm-0.3.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='model21cm',
+      version='0.3.4',
+      description='Infer parameters of a 21cm cosmology model.',
+      url='http://github.com/phys201/model21cm',
+      author='I. Davenport, N. DePorzio',
+      author_email='iandavenport@g.harvard.edu, nicholasdeporzio@g.harvard.edu',
+      license='GNU GPL-3.0',
+      packages=['model21cm'],
+      install_requires = ['numpy','pandas','matplotlib'],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'], 
+      include_package_data=True)
+
+#Note: PyPi doesn't recognized 'os' or 'matplotlib.pyplot' dependencies
