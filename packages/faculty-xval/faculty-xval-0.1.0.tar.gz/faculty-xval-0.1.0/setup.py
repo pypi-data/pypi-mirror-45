0599c233437f2d51b010577b3ef178e965ae21dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:11 +0000

    Add package faculty-xval/0.1.0/faculty-xval-0.1.0.tar.gz

---
+++ packages/faculty-xval/faculty-xval-0.1.0.tar.gz/faculty-xval-0.1.0/setup.py
@@ -0,0 +1,38 @@
+from os import path
+from setuptools import setup
+
+# Read the contents of the README file.
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+# Remove logo as not supported on PyPI.
+long_description = long_description.replace("![img|small](img/logo.png)", "")
+
+setup(
+    name="faculty-xval",
+    version="0.1.0",
+    description="Cross validation of machine-learning models on Faculty platform.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/facultyai/faculty-xval",
+    author="Faculty",
+    author_email="info@faculty.ai",
+    install_requires=[
+        "click",
+        "faculty",
+        "keras>=2.2.4",
+        "numpy",
+        "scikit_learn",
+        "tensorflow",
+    ],
+    packages=["faculty_xval", "faculty_xval.bin"],
+    entry_points={
+        "console_scripts": [
+            "faculty_xval_jobs_xval"
+            + " = "
+            + "faculty_xval.bin.jobs_cross_validation_executor:main"
+        ]
+    },
+    zip_safe=False,
+)
