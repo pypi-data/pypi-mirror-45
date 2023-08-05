a45a39ca62bd4824a697e2c2495a9905543b5816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:46 +0000

    Add package awsutils-s3/0.2.1/awsutils-s3-0.2.1.tar.gz

---
+++ packages/awsutils-s3/awsutils-s3-0.2.1.tar.gz/awsutils-s3-0.2.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+
+name = 'awsutils-s3'
+
+
+def get_version(version_file='_version.py'):
+    """Retrieve the package version from a version file in the package root."""
+    filename = os.path.join(os.path.dirname(__file__), 'awsutils', 's3', version_file)
+    with open(filename, 'rb') as fp:
+        return fp.read().decode('utf8').split('=')[1].strip(" \n'")
+
+
+setup(
+    name=name,
+    version=get_version(),
+    packages=find_packages(),
+    namespace_packages=['awsutils'],
+    install_requires=[
+        'awscli',
+        'dirutility>=0.6.1',
+        'tldextract',
+        'validators'
+    ],
+    url='https://github.com/mrstephenneal/awsutils-s3',
+    license='MIT',
+    author='Stephen Neal',
+    author_email='stephen@stephenneal.net',
+    description='AWS Command Line Interface wrapper for S3 services.'
+)
