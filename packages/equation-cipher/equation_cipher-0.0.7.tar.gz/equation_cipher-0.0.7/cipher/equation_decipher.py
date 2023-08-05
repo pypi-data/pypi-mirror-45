c26aac531e969ef692c7a9fdbdad5daa07ed84a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:40 +0000

    Add package equation-cipher/0.0.7/equation_cipher-0.0.7.tar.gz

---
+++ packages/equation-cipher/equation_cipher-0.0.7.tar.gz/equation_cipher-0.0.7/cipher/equation_decipher.py
@@ -0,0 +1,34 @@
+"""
+Contains the methods to decipher the equation cipher method.
+"""
+import base64
+import datetime
+import sys
+sys.path.append('..')
+
+from cipher.equation_cipher import Encrypter
+
+
+class Decrypter(Encrypter):
+    """
+    Class to decrypt the equation cipher encryption.
+    Note: This decryption can not be done complete decryption but partial
+    decryption can be done and string passed can be verified at some stage of
+    encryption.
+    """
+
+    def match_decrypt(self, target: str, hash_str: bytes) -> bool:
+        """
+        Checks if the passed hash_str is representation of passed
+        target string or not.
+        :param target: <data type: str>
+        :param hash_str: <data type: str(bytes)>
+        :return: <data type: bool>
+        """
+        decrypted_hash = base64.b64decode(hash_str).decode('utf-8')
+        equation = decrypted_hash.split('@')[0]
+        date_obj = datetime.datetime.strptime(decrypted_hash.split('@')[1],
+                                              '%m/%d/%Y,%H:%M:%S')
+        en_str = self._form_equation(target, date_obj)
+
+        return True if equation == en_str else False
