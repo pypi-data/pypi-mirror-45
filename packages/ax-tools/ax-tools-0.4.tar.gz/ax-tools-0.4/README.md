df8c1bc8926833a91cd255b3d5c1ebc4631ebdcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:38 +0000

    Add package ax-tools/0.4/ax-tools-0.4.tar.gz

---
+++ packages/ax-tools/ax-tools-0.4.tar.gz/ax-tools-0.4/README.md
@@ -0,0 +1,6 @@
+# AX-Tools Module
+The AX-Tools Module provides utilities for use with Axyl. These are licensed under the GNU GPLv3 so are free to use and modify.
+
+[My Reddit](https://www.reddit.com/user/Omega0x013)
+
+[My StackOverflow](https://stackoverflow.com/users/8357167/omega0x013?tab=profile)
---
+++ packages/ax-tools/ax-tools-0.4.tar.gz/ax-tools-0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='ax-tools',
+      version='0.4',
+      description='Tools for Axyl AX System',
+      long_description=open('README.md','r').read(),
+      long_description_content_type='text/markdown',
+      url='http://github.com/Omega0x013/ax-tools',
+      author='Alexander Bielby',
+      author_email='xanderbielby@gmail.com',
+      license='GNU GPLv3',
+      packages=['axm'],
+      zip_safe=False)
