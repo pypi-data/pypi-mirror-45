1176e774f1b2f98fc9b8e8d8df520ee5cd0429d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:24 +0000

    Add package meross-iot/0.2.1.1/meross_iot-0.2.1.1.tar.gz

---
+++ packages/meross-iot/meross_iot-0.2.1.1.tar.gz/meross_iot-0.2.1.1/meross_iot/supported_devices/abilities.py
@@ -0,0 +1,9 @@
+# Abilities
+TOGGLE = 'Appliance.Control.Toggle'
+TOGGLEX = 'Appliance.Control.ToggleX'
+TRIGGER = 'Appliance.Control.Trigger'
+TRIGGERX = 'Appliance.Control.TriggerX'
+ELECTRICITY = 'Appliance.Control.Electricity'
+CONSUMPTIONX = 'Appliance.Control.ConsumptionX'
+ONLINE = 'Appliance.System.Online'
+WIFI_LIST = 'Appliance.Config.WifiList'
