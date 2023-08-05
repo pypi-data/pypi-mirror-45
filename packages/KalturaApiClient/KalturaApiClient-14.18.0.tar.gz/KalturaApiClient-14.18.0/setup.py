a1d50960ad43d35e44311e397e235e8192e2cff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:57 +0000

    Add package KalturaApiClient/14.18.0/KalturaApiClient-14.18.0.tar.gz

---
+++ packages/KalturaApiClient/KalturaApiClient-14.18.0.tar.gz/KalturaApiClient-14.18.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='KalturaApiClient',
+    version='14.18.0',
+    url='https://github.com/kaltura/KalturaGeneratedAPIClientsPython',
+    packages=['KalturaClient', 'KalturaClient.Plugins'],
+    install_requires=['requests>=2.4.2', 'requests-toolbelt', 'six'],
+    license='AGPLv3+',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='Kaltura API client',
+    description='A Python module for accessing the Kaltura API.',
+    long_description=open('README.txt').read(),
+)
