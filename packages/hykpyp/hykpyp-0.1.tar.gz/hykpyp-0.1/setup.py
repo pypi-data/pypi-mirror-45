7b2c9cbe2c824b7d81cd75fc490319f440a443b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:18 +0000

    Add package hykpyp/0.1/hykpyp-0.1.tar.gz

---
+++ packages/hykpyp/hykpyp-0.1.tar.gz/hykpyp-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='hykpyp',
+      version='0.1',
+      description='this is demo',
+      url='https://github.com/hykfft/hykpyp',
+      author='hyk',
+      author_email='hykfft@gmail.com',
+      license='MIT',
+      packages=[],
+      zip_safe=False)
