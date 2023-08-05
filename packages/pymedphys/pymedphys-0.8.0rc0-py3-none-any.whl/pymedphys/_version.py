940f0c8ec55cf8e3eba56845931f5dae81fed619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:44 +0000

    Add package pymedphys/0.8.0rc0/pymedphys-0.8.0rc0-py3-none-any.whl

---
+++ packages/pymedphys/pymedphys-0.8.0rc0-py3-none-any.whl/pymedphys/_version.py
@@ -0,0 +1,17 @@
+"""Extracts version number information from gui/package.json."""
+
+# pylint: disable=C0103
+
+import os
+import json
+
+try:
+    HERE = os.path.dirname(__file__)
+except NameError:
+    HERE = os.path.join('src', 'pymedphys')
+
+with open(os.path.join(HERE, 'gui', 'package.json')) as file:
+    data = json.load(file)
+
+version_info = data['version'].replace('.', ' ').replace('-', ' ').split(' ')
+__version__ = '.'.join(map(str, version_info[:3])) + ''.join(version_info[3:])
