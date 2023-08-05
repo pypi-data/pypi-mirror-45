434238d8ffabc5d7df20705a6b15fc5edf2bad39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:12 +0000

    Add package hail/0.2.13/hail-0.2.13.tar.gz

---
+++ packages/hail/hail-0.2.13.tar.gz/hail-0.2.13/hail/plot/__init__.py
@@ -0,0 +1,12 @@
+from .plots import output_notebook, show, histogram, cumulative_histogram, histogram2d, scatter, qq, manhattan, pdf, cdf
+
+__all__ = ['output_notebook',
+           'show',
+           'histogram',
+           'cumulative_histogram',
+           'scatter',
+           'histogram2d',
+           'qq',
+           'manhattan',
+           'pdf',
+           'cdf']
