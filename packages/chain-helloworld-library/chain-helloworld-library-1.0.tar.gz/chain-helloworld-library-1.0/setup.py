714f94c0b36b96f91aa7ce4cbf1f1fedbadda337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:28 +0000

    Add package chain-helloworld-library/1.0/chain-helloworld-library-1.0.tar.gz

---
+++ packages/chain-helloworld-library/chain-helloworld-library-1.0.tar.gz/chain-helloworld-library-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="chain-helloworld-library",
+    version='1.0',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/kachain2019/demo-helloworld-library',
+    author='kachain',
+    author_email='kachain2019.a@gmail.com',
+)
