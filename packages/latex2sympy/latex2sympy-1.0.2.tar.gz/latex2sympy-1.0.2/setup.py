e0c8334daf3571b4e9df7d2e19b1d80d82ae8a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:52 +0000

    Add package latex2sympy/1.0.2/latex2sympy-1.0.2.tar.gz

---
+++ packages/latex2sympy/latex2sympy-1.0.2.tar.gz/latex2sympy-1.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="latex2sympy",
+    version="1.0.2",
+    author="Volker Weißmann",
+    author_email="volker.weissmann@gmx.de",
+    description="Convert latex code to sympy code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/volkerweissmann/latex2sympy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
