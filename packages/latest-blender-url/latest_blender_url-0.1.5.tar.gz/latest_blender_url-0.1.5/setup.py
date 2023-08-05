1dbbb4b549f6bd80e0bc5ac3c91fd1ad9fec3c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:13 +0000

    Add package latest-blender-url/0.1.5/latest_blender_url-0.1.5.tar.gz

---
+++ packages/latest-blender-url/latest_blender_url-0.1.5.tar.gz/latest_blender_url-0.1.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="latest_blender_url",
+    version="0.1.5",
+    author="Justas Birgiolas",
+    author_email="justas@asu.edu",
+    description="A python package that retrieves the latest Blender download URL",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/justasb/latest_blender_url",
+    packages=setuptools.find_packages(),
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'lxml',
+        'beautifulsoup4',
+    ]
+)
