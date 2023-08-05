f51db32c934685284031d3f553b722008a218a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:36 +0000

    Add package cc-pypackage/0.2.1/cc-pypackage-0.2.1.tar.gz

---
+++ packages/cc-pypackage/cc-pypackage-0.2.1.tar.gz/cc-pypackage-0.2.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by Roberto Preste
+import setuptools
+from distutils.core import setup
+
+setup(  # pragma: no cover
+    name="cc-pypackage",
+    packages=[],
+    version='0.2.1',
+    description="My custom Cookiecutter template for a Python package",
+    author="Roberto Preste",
+    license="BSD",
+    author_email="roberto.preste@gmail.com",
+    url="https://github.com/robertopreste/cc-pypackage",
+    keywords=["cookiecutter", "template", "package", ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development",
+    ],
+)
