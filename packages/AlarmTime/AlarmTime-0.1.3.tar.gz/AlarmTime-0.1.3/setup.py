b865e8d472185571475ebc2fa1a9ca6abbca132c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:18 +0000

    Add package AlarmTime/0.1.3/AlarmTime-0.1.3.tar.gz

---
+++ packages/AlarmTime/AlarmTime-0.1.3.tar.gz/AlarmTime-0.1.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="AlarmTime",
+    version="0.1.3",
+    author="Md. Shohanur Rahman",
+    author_email="dshohan112@gmail.com",
+    url = 'https://github.com/shohan98/alarm_time_claculate',
+    description="Detect alarm time from natural sentence and calculate time difference from current time to target time.",
+    long_description='For calculating time difference from current time to Target time and can detect a time from our natural language',
+    long_description_content_type="text/markdown",
+    license = 'MIT',
+    install_requires = ['datetime','python-dateutil','regex'],
+    packages=setuptools.find_packages(),
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
