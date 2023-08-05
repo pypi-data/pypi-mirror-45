4b7b0a72f9d21bef3cfa642463162a6974f27f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:45 +0000

    Add package fuse-3ds/1.4/fuse-3ds-1.4.tar.gz

---
+++ packages/fuse-3ds/fuse-3ds-1.4.tar.gz/fuse-3ds-1.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='fuse-3ds',
+    version='1.4',
+    url='https://github.com/ihaveamac/ninfs',
+    author='ihaveamac',
+    description='Mirror package for ninfs',
+    long_description='This is a mirror package for ninfs, the new name for fuse-3ds.',
+    packages=[],
+    install_requires=['ninfs>=1.4'],
+)
