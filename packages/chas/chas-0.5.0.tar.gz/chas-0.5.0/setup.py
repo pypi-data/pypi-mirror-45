89913d26b5bd37d8b9381f743313fdced541d964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:08 +0000

    Add package chas/0.5.0/chas-0.5.0.tar.gz

---
+++ packages/chas/chas-0.5.0.tar.gz/chas-0.5.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chas",
+    version="0.5.0",
+    author="Lukas Cerny",
+    author_email="lukas.cerny@exponea.com",
+    description="Framework for creating and running cron jobs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["chas"],
+    include_package_data=True,
+    entry_points = {
+        "console_scripts": ["chas=chas.command_line:main"],
+    },
+    install_requires=["flask", "prometheus-flask-exporter", "prometheus_client"],
+    url="https://github.com/lukasotocerny/chas",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+    ]
+)
