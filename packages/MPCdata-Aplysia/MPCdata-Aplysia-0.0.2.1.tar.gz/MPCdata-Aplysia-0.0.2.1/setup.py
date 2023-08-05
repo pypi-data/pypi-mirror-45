f8c4a5c0636d5a44219d7cdda8a0ec1451b3fdde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:00 +0000

    Add package MPCdata-Aplysia/0.0.2.1/MPCdata-Aplysia-0.0.2.1.tar.gz

---
+++ packages/MPCdata-Aplysia/MPCdata-Aplysia-0.0.2.1.tar.gz/MPCdata-Aplysia-0.0.2.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MPCdata-Aplysia",
+    version="0.0.2.1",
+    author="Matthew Perkins",
+    author_email="matthew.perkins@mssm.edu",
+    description="Package to read Med Associates Files MedPCIV",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/matthewperkins/MPCdata",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
