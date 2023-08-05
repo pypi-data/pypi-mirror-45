e3043ead30ead5ce28014fe605ad0e2840ef1a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:57 +0000

    Add package baizhan-math/1.0/baizhan-math-1.0.tar.gz

---
+++ packages/baizhan-math/baizhan-math-1.0.tar.gz/baizhan-math-1.0/baizhan-math/demo.py
@@ -0,0 +1,5 @@
+def sum(*a):
+    count = 0
+    for i in a:
+        count += i
+    return count  
---
+++ packages/baizhan-math/baizhan-math-1.0.tar.gz/baizhan-math-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='baizhan-math',  # 对外我们模块的名字
+    version='1.0', # 版本号
+    description='这是第一个对外发布的模块，测试哦',  #描述
+    author='gaoqi', # 作者
+    py_modules=['baizhan-math.demo'] # 要发布的模块
+)
