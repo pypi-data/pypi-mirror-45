74a4664e7735d59f8ae9b60ebd711170bb3d7df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:15 +0000

    Add package c2cgeoportal-geoportal/2.4.post20190417/c2cgeoportal_geoportal-2.4.post20190417.tar.gz

---
+++ packages/c2cgeoportal-geoportal/c2cgeoportal_geoportal-2.4.post20190417.tar.gz/c2cgeoportal_geoportal-2.4.post20190417/c2cgeoportal_geoportal/scaffolds/update/CONST_create_template/geoportal/+package+_geoportal/static-ngeo/api/index.js
@@ -0,0 +1,10 @@
+import Map from 'api/Map.js';
+import config from 'api/constants.js';
+
+config.themesUrl = '{FULL_ENTRY_POINT}themes?version=2&background=background';
+
+const lib = {
+  Map
+};
+
+export default lib;
