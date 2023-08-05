f553d6144b05dc227ee10f4a5f91089c5e5fc7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:00 +0000

    Add package cgstatistical/0.0.5/cgstatistical-0.0.5.tar.gz

---
+++ packages/cgstatistical/cgstatistical-0.0.5.tar.gz/cgstatistical-0.0.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='cgstatistical',  
+     version='0.0.5',
+     author="Jackson Prado Lima, Dimmy Magalhaes",
+     author_email="dimmyk@gmail.com",
+     description="CBio-Gres Statistical Tests",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/dimmykarson/CGStatistic",
+     packages=setuptools.find_packages(),
+     install_requires=[
+          'seaborn', 'pandas', 'rpy2', 'matplotlib', 'numpy'
+          ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
