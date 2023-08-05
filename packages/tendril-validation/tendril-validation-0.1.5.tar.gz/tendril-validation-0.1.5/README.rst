2d0da611d7837f041694ac4dfa68d57abf364588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:31 +0000

    Add package tendril-validation/0.1.5/tendril-validation-0.1.5.tar.gz

---
+++ packages/tendril-validation/tendril-validation-0.1.5.tar.gz/tendril-validation-0.1.5/README.rst
@@ -0,0 +1,15 @@
+=====================================
+Validation Infrastructure for Tendril
+=====================================
+
+.. image:: https://img.shields.io/travis/chintal/tendril-validation.svg
+        :target: https://travis-ci.org/chintal/tendril-validation
+
+.. image:: https://img.shields.io/pypi/v/tendril-validation.svg
+        :target: https://pypi.python.org/pypi/tendril-validation
+
+* License: AGPLv3
+* Documentation: http://tendril-validation.readthedocs.org
+
+
+Validation Infrastructure for Tendril
---
+++ packages/tendril-validation/tendril-validation-0.1.5.tar.gz/tendril-validation-0.1.5/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+except ImportError:
+    from distutils.core import setup
+    find_packages = None
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'six',
+    'colorama',
+    'tendril-utils-terminal>=0.1.1',
+    'tendril-utils-core>=0.1.13',
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='tendril-validation',
+    version='0.1.5',
+    description="Validation Infrastructure for Tendril",
+    long_description=readme,
+    author="Chintalagiri Shashank",
+    author_email='shashank@chintal.in',
+    url='https://github.com/chintal/tendril-validation',
+    packages=find_packages(),
+    install_requires=requirements,
+    license="AGPLv3",
+    zip_safe=False,
+    keywords='tendril',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ],
+    # test_suite='tests',
+    # tests_require=test_requirements
+)
