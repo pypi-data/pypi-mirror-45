84c7026114fda0aba1d4d99539382333a028cdf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:01 +0000

    Add package aon-helloworld-library/1.2/aon-helloworld-library-1.2.tar.gz

---
+++ packages/aon-helloworld-library/aon-helloworld-library-1.2.tar.gz/aon-helloworld-library-1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="aon-helloworld-library",
+    version='1.2',
+    package_dir={'' : 'src'},
+    packages=['HelloWorld'],
+    url='https://github.com/Soonthorn6996/demo-helloworld-library',
+    author='Soonthorn',
+    author_email='soonthorn6996@gmail.com',
+)
