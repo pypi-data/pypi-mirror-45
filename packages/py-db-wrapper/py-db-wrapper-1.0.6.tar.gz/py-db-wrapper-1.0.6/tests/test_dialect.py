12622a03583552efe5dcd2493a55b96a9b588cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:16 +0000

    Add package py-db-wrapper/1.0.6/py-db-wrapper-1.0.6.tar.gz

---
+++ packages/py-db-wrapper/py-db-wrapper-1.0.6.tar.gz/py-db-wrapper-1.0.6/tests/test_dialect.py
@@ -0,0 +1,18 @@
+import unittest
+from py_db_wrapper import dialect
+
+class TestDialectMethods(unittest.TestCase):
+
+    # def test_add_ticks_and_brackets(self):
+    #     d = dialect.Dialect()
+    #     self.assertEqual('a', d.add_ticks_and_brackets('a'))
+    #     d.brackets = True
+    #     d.ticks = False
+    #     self.assertEqual('[a]', d.add_ticks_and_brackets('a'))
+    #     d.brackets = False
+    #     d.ticks = True
+    #     self.assertEqual('`a`', d.add_ticks_and_brackets('a'))
+    #     d.brackets = True
+    #     d.ticks = True
+    #     self.assertEqual('[`a`]', d.add_ticks_and_brackets('a'))
+    pass
