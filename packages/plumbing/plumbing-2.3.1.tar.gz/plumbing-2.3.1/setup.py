8efc53c902b70b591bdbfbccf3f56d19eca6c631
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:01 +0000

    Add package plumbing/2.3.1/plumbing-2.3.1.tar.gz

---
+++ packages/plumbing/plumbing-2.3.1.tar.gz/plumbing-2.3.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'plumbing',
+    version          = '2.3.1',
+    description      = 'Helps with plumbing-type programing in python.',
+    license          = 'MIT',
+    url              = 'http://github.com/xapple/plumbing/',
+    author           = 'Lucas Sinclair',
+    author_email     = 'lucas.sinclair@me.com',
+    packages         = find_packages(),
+    install_requires = ['six', 'autopaths', 'sh', 'biopython', 'matplotlib', 'brewer2mpl', 'decorator',
+                        'shell_command', 'pandas', 'tqdm', 'brewer2mpl', 'boto3', 'sshconf'],
+    long_description = open('README.md').read(),
+)
