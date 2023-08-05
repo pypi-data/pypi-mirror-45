ac7c2e1904adde9f5f7bdb8e96023c3ce28b12ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:10 +0000

    Add package Flask-Performance/0.0.4/Flask-Performance-0.0.4.tar.gz

---
+++ packages/Flask-Performance/Flask-Performance-0.0.4.tar.gz/Flask-Performance-0.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Flask-Performance",
+    version="0.0.4",
+    author="Spacebox(盒子空间) Team",
+    author_email="spacebox@spacebox.fun",
+    description="A Flask extension to monitoring every REAL api request and collect it's performance in REAL-time.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SpaceBoxer/flask-performance",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'flask',
+        'requests'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
