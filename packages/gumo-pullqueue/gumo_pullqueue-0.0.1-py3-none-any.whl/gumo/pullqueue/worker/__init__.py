2df549b59ca07b53b9627aad6e664f3667f40e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:06 +0000

    Add package gumo-pullqueue/0.0.1/gumo_pullqueue-0.0.1-py3-none-any.whl

---
+++ packages/gumo-pullqueue/gumo_pullqueue-0.0.1-py3-none-any.whl/gumo/pullqueue/worker/__init__.py
@@ -0,0 +1,7 @@
+from gumo.pullqueue.worker._configuration import configure
+from gumo.pullqueue.worker.domain.configuration import PullQueueWorkerConfiguration
+
+__all__ = [
+    configure.__name__,
+    PullQueueWorkerConfiguration.__name__,
+]
