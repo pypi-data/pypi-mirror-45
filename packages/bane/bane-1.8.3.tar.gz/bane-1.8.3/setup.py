1691de88fa6f97777bbdaf3e0e4ddb1d1b9a24cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:20 +0000

    Add package bane/1.8.3/bane-1.8.3.tar.gz

---
+++ packages/bane/bane-1.8.3.tar.gz/bane-1.8.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+req=["requests","PySocks","bs4","pexpect","paramiko","mysql-connector","scapy","stem"]
+
+setuptools.setup(
+    name="bane",
+    version="1.8.3",
+    author="AlaBouali",
+    author_email="trap.leader.123@gmail.com",
+    description="cyber security library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlaBouali/bane",
+    python_requires=">=2.7",
+    install_requires=req,
+    packages=["bane"],
+    license="MIT License",
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License ",
+        "Operating System :: Unix",
+    ],
+)
