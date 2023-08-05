30872802d74126632733525996ff898cda486861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:54 +0000

    Add package rtsp-curl/0.1/rtsp_curl-0.1.tar.gz

---
+++ packages/rtsp-curl/rtsp_curl-0.1.tar.gz/rtsp_curl-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='rtsp_curl',
+      version='0.1',
+      description='What the module does',
+      url='https://github.com/madyel/rtsp_curl',
+      author='MaDyEl',
+      author_email='email@domain.net',
+      license='MIT',
+      packages=['package'],
+      install_requires=['scanf>=1.5.2',
+                        'pycurl==7.43.0.2'])
