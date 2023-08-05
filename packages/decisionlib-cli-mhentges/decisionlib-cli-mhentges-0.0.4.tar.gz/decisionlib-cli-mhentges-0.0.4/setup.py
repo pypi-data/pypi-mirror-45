09da16cd32e8b6a07023e5e9378f5d65068b1a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:19 +0000

    Add package decisionlib-cli-mhentges/0.0.4/decisionlib-cli-mhentges-0.0.4.tar.gz

---
+++ packages/decisionlib-cli-mhentges/decisionlib-cli-mhentges-0.0.4.tar.gz/decisionlib-cli-mhentges-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    requirements = [line.rstrip('\n') for line in f if not line.startswith('#')]
+
+setuptools.setup(
+    name='decisionlib-cli-mhentges',
+    version='0.0.4',
+    author='Mozilla Release Engineering',
+    author_email='release+python@mozilla.com',
+    description='Taskcluster task-builder',
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    include_package_data=True,
+    python_requires='~=2.7',
+    entry_points={
+        'console_scripts': [
+            'decisionlib-cli = decisionlib_cli:main',
+        ],
+    },
+)
