a0367d90817a3f8508bb97d0edbbba56468d3007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:30 +0000

    Add package qucode/2.4/qucode-2.4.tar.gz

---
+++ packages/qucode/qucode-2.4.tar.gz/qucode-2.4/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+import setuptools
+setup(
+    name='qucode',
+    version='2.4',
+    description=(
+        '<乱字符提取地区编码>'
+    ),
+    author='<loo>',
+    author_email='<1213788433@qq.com>',
+    maintainer='<loo>',
+    maintainer_email='<1213788433@qq.com',
+    platforms=["all"],
+    packages=['qucode'],
+    url='https://github.com/libinhai-loo/find_code.git'
+)
