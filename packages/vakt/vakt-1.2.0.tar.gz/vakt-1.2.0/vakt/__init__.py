00c4ffc250851701b078ad0b73a74fe7195fb75f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:34 +0000

    Add package vakt/1.2.0/vakt-1.2.0.tar.gz

---
+++ packages/vakt/vakt-1.2.0.tar.gz/vakt-1.2.0/vakt/__init__.py
@@ -0,0 +1,42 @@
+"""
+Vakt is an SDK for access policies.
+"""
+
+import logging
+
+###########################
+#    Public API Imports   #
+###########################
+
+from .version import version_info, __version__
+
+from .policy import Policy
+
+from .guard import (
+    Inquiry,
+    Guard,
+)
+
+from .effects import (
+    ALLOW_ACCESS,
+    DENY_ACCESS,
+)
+
+from .checker import (
+    RegexChecker,
+    StringFuzzyChecker,
+    StringExactChecker,
+    RulesChecker,
+)
+
+from . import rules
+
+from .storage.memory import MemoryStorage
+from .storage.mongo import MongoStorage
+
+
+################
+#  Setting up  #
+################
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
