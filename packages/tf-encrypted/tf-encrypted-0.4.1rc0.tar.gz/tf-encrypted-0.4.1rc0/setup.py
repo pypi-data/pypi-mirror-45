92d8893e4f9704d33f68a424d049ee8f68604e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:44 +0000

    Add package tf-encrypted/0.4.1rc0/tf-encrypted-0.4.1rc0.tar.gz

---
+++ packages/tf-encrypted/tf-encrypted-0.4.1rc0.tar.gz/tf-encrypted-0.4.1rc0/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tf-encrypted",
+    version="0.4.1-rc0",
+    packages=setuptools.find_packages(),
+    package_data={'tf_encrypted': [
+        'operations/secure_random/*.so',
+    ]},
+    python_requires=">=3.5",
+    install_requires=[
+        "tensorflow>=1.12.0,<2",
+        "numpy>=1.14.0"
+    ],
+    extra_requires={
+        "tf": ["tensorflow>=1.12.0,<2"]
+    },
+    license="Apache License 2.0",
+    url="https://github.com/mortendahl/tf-encrypted",
+    description="Layer on top of TensorFlow for doing machine learning on encrypted data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Morten Dahl",
+    author_email="mortendahlcs@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 2 - Pre-Alpha",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Security :: Cryptography"
+    ]
+)
