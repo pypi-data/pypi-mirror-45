4c702d9014bd5f7730695a4fb11a986cdedbe880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:14 +0000

    Add package hashchain/0.8.4/hashchain-0.8.4.tar.gz

---
+++ packages/hashchain/hashchain-0.8.4.tar.gz/hashchain-0.8.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='hashchain',
+    version='0.8.4',
+    description='Helper to certify database entries, using hashchains and blockchains',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/svandegar/hashchain',
+    author='Seraphin Vandegar',
+    author_email='svandegar@hotmail.com',
+    license='MIT',
+    packages=['hashchain','hashchain.ethereum'],
+    zip_safe=False,
+    download_url='',
+    install_requires=[
+        'web3',
+        'pysha3'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5'
+    ]
+)
