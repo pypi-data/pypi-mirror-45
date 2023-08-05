63b806bd3c81c4c23fb0c439317929b022132caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:05 +0000

    Add package mattes-allegro-pl/0.6.1/mattes-allegro-pl-0.6.1.tar.gz

---
+++ packages/mattes-allegro-pl/mattes-allegro-pl-0.6.1.tar.gz/mattes-allegro-pl-0.6.1/src/allegro_pl/__init__.py
@@ -0,0 +1,6 @@
+from .allegro import Allegro
+from .oauth import AllegroAuth, AuthorizationCodeAuth, ClientCodeStore, ClientCredentialsAuth, TokenError, TokenStore, \
+    URL_AUTHORIZE, URL_TOKEN
+
+__name__ = 'mattes-allegro-pl'
+__version__ = '0.6.1'
