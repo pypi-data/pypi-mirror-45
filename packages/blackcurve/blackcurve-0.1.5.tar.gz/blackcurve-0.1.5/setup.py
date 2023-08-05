b0a43fb985031ad23ec731ff983e90a3de2f35e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:06 +0000

    Add package blackcurve/0.1.5/blackcurve-0.1.5.tar.gz

---
+++ packages/blackcurve/blackcurve-0.1.5.tar.gz/blackcurve-0.1.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="blackcurve",
+    version="0.1.5",
+    author="BlackCurve LTD.",
+    author_email="george.rowberry@blackcurve.com",
+    description="An easy to use python interface for BlackCurve's API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/blackcurve/BlackCurve-API",
+    packages=setuptools.find_packages(exclude=['tests']),
+    classifiers=(
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
