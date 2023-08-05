6889c1a0086816a522512c5f32c2fcbd6d4c4abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:11 +0000

    Add package django-channels-graphql-ws/0.1.18/django-channels-graphql-ws-0.1.18.tar.gz

---
+++ packages/django-channels-graphql-ws/django-channels-graphql-ws-0.1.18.tar.gz/django-channels-graphql-ws-0.1.18/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['channels_graphql_ws']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp',
+ 'asgiref',
+ 'channels',
+ 'django',
+ 'graphene',
+ 'graphene_django',
+ 'graphql-core',
+ 'msgpack',
+ 'rx']
+
+setup_kwargs = {
+    'name': 'django-channels-graphql-ws',
+    'version': '0.1.18',
+    'description': 'Django Channels based WebSocket GraphQL server with Graphene-like subscriptions',
+    'long_description': None,
+    'author': 'Alexander A. Prokhorov',
+    'author_email': 'alexander.prokhorov@datadvance.net',
+    'url': 'https://github.com/datadvance/DjangoChannelsGraphqlWs',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.0,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
