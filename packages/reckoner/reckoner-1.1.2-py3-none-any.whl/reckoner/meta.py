f906f33330055fdd13661e1a4c16786f7601103e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:45 +0000

    Add package reckoner/1.1.2/reckoner-1.1.2-py3-none-any.whl

---
+++ packages/reckoner/reckoner-1.1.2-py3-none-any.whl/reckoner/meta.py
@@ -0,0 +1,27 @@
+# -- coding: utf-8 --
+
+# pylint: skip-file
+
+# Copyright 2019 ReactiveOps Inc
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from pkg_resources import get_distribution, DistributionNotFound
+import re
+
+__version_modifier__ = re.compile(r'^([0-9]+\.[0-9]+\.[0-9]+)\.(.*)$')
+__distribution_name__ = 'reckoner'
+try:
+    __version__ = re.sub(__version_modifier__, r'\g<1>-\g<2>', get_distribution(__distribution_name__).version)
+except DistributionNotFound:
+    pass
+__author__ = 'ReactiveOps, Inc.'
