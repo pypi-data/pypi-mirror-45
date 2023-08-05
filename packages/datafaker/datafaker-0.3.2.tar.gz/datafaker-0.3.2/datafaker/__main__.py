5b3c19f4803ce2992f151b0142d8e03cc256b4a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:31 +0000

    Add package datafaker/0.3.2/datafaker-0.3.2.tar.gz

---
+++ packages/datafaker/datafaker-0.3.2.tar.gz/datafaker-0.3.2/datafaker/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import sys
+from datafaker.cli import main
+
+if __name__ == "__main__":
+
+    # cmd = 'datafaker file . hello.txt 10 --meta data/student.txt --withheader --outprint --batch 2'
+    # cmd = 'datafaker file . hello.txt 10 --meta data/student.txt --withheader --batch 2'
+    # cmd = 'datafaker mysql mysql+mysqldb://root:root@localhost:3600/test student 10 --batch 3'
+    # sys.argv = cmd.strip().split(' ')
+    sys.exit(main())
+
