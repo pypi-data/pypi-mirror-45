7c321cae183b01c1277c4f2e086b647f535d6df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:48 +0000

    Add package bigimg2019/0.3/bigimg2019-0.3.tar.gz

---
+++ packages/bigimg2019/bigimg2019-0.3.tar.gz/bigimg2019-0.3/bigimg/__init__.py
@@ -0,0 +1,10 @@
+from bigimg.load_data import *
+from bigimg.fourier import *
+from bigimg.lowest_dims import *
+from bigimg.patient import *
+from bigimg.plot_data import *
+from bigimg.preprocess import *
+from bigimg.segmentation import *
+
+if __name__ == "__main__":
+    your example code goes here
