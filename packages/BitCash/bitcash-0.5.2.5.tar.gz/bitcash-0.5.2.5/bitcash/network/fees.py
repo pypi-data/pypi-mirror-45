92dfe8371e06bbb1039d9d19dcde975cd7650649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:55 +0000

    Add package BitCash/0.5.2.5/bitcash-0.5.2.5.tar.gz

---
+++ packages/BitCash/bitcash-0.5.2.5.tar.gz/bitcash-0.5.2.5/bitcash/network/fees.py
@@ -0,0 +1,37 @@
+# import requests
+# from requests.exceptions import ConnectionError, HTTPError, Timeout
+
+# Ideally, fast, medium, slow would correlate with actually blocks out.
+# Fast, really shoot for getting into the next block no matter what.
+# Medium should get in within the next couple blocks, 90% certainty.
+# Slow, in a few hours, 90% certainty.
+# The default should be medium so you can go up/down from there.
+DEFAULT_FEE_FAST = 8
+DEFAULT_FEE_MEDIUM = 4
+DEFAULT_FEE_SLOW = 2
+
+# FIXME: Need to add in a fees API. Issue #1
+# URL = 'https://bitcoincashfees.earn.com/api/v1/fees/recommended'
+
+FEE_SPEED_FAST = 'fast'
+FEE_SPEED_MEDIUM = 'medium'
+FEE_SPEED_SLOW = 'slow'
+
+
+# FIXME: Not sure if this is better, bools are better, or creating its
+# own type is better.
+def get_fee(speed=FEE_SPEED_MEDIUM):
+    """Gets the recommended satoshi per byte fee.
+
+    :param speed: One of: 'fast', 'medium', 'slow'.
+    :type speed: ``string``
+    :rtype: ``int``
+    """
+    if speed == FEE_SPEED_FAST:
+        return DEFAULT_FEE_FAST
+    elif speed == FEE_SPEED_MEDIUM:
+        return DEFAULT_FEE_MEDIUM
+    elif speed == FEE_SPEED_SLOW:
+        return DEFAULT_FEE_SLOW
+    else:
+        raise ValueError('Invalid speed argument.')
---
+++ packages/BitCash/bitcash-0.5.2.5.tar.gz/bitcash-0.5.2.5/setup.py
@@ -0,0 +1,56 @@
+from setuptools import find_packages, setup
+
+with open('bitcash/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('= ')[1].strip("'")
+            break
+
+setup(
+    name='bitcash',
+    version=version,
+    description='Bitcoin Cash made easier.',
+    long_description=open('README.rst', 'r').read(),
+    author='Teran McKinney',
+    author_email='sega01@go-beyond.org',
+    maintainer='Teran McKinney',
+    maintainer_email='sega01@go-beyond.org',
+    url='https://github.com/sporestack/bitcash',
+    download_url='https://github.com/sporestack/bitcash/tarball/{}'.format(version),
+    license='MIT',
+
+    keywords=[
+        'bitcoincash',
+        'cryptocurrency',
+        'payments',
+        'tools',
+        'wallet',
+    ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+
+    install_requires=['coincurve>=4.3.0', 'requests', 'cashaddress==1.0.4'],
+    extras_require={
+        'cli': ('appdirs', 'click', 'privy', 'tinydb'),
+        'cache': ('lmdb', ),
+    },
+    tests_require=['pytest'],
+
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': (
+            'bitcash = bitcash.cli:bitcash',
+        ),
+    },
+)
