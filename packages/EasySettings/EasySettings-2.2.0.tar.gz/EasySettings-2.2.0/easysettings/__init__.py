6fa73cc136d7e46d60f6271e284b80253644368a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:19 +0000

    Add package EasySettings/2.2.0/EasySettings-2.2.0.tar.gz

---
+++ packages/EasySettings/EasySettings-2.2.0.tar.gz/EasySettings-2.2.0/easysettings/__init__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+  EasySettings
+  An easy interface for setting and retrieving application settings using
+  pickle or json.
+
+Created on Jan 16, 2013
+
+@author: Christopher Welborn
+'''
+
+from .easy_settings import (  # noqa
+    EasySettings,
+    __version__,
+    version,
+    esError,
+    esGetError,
+    esSetError,
+    esCompareError,
+    esSaveError,
+    esValueError,
+)
+from .json_settings import (
+    JSONMixin,
+    JSONSettings,
+    load_json_settings,
+)
+
+__all__ = [
+    'EasySettings',
+    'JSONMixin',
+    'JSONSettings',
+    'esCompareError',
+    'esError',
+    'esGetError',
+    'esSaveError',
+    'esSetError',
+    'esValueError',
+    'load_json_settings',
+    'version',
+]
