2d12f3c7aaf33492798c77b88fa4fdfd3cf5332a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:08 +0000

    Add package advoservice-integration/1.1.0/advoservice_integration-1.1.0-py3-none-any.whl

---
+++ packages/advoservice-integration/advoservice_integration-1.1.0-py3-none-any.whl/advoservice_integration/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from advoservice_integration.api.default_api import DefaultApi
+from advoservice_integration.api.purchase_api import PurchaseApi
