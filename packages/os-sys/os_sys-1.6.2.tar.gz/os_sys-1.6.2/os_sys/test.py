d681ed25ec0b9de0a04b70a371fcfc045afe02ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:40 +0000

    Add package os-sys/1.6.2/os_sys-1.6.2.tar.gz

---
+++ packages/os-sys/os_sys-1.6.2.tar.gz/os_sys-1.6.2/os_sys/test.py
@@ -0,0 +1,10 @@
+from devserver import build
+import devserver
+
+def index(request):
+    return build.HTTPResponse('''post: %s <br> get: %s<br>python: <?py
+print("hello")
+print(''.join(str(request.GET)))
+py?>
+<?php echo('  php test');?>''' % (str(request.POST), str(request.GET)))
+devserver.build.config(index=index)
