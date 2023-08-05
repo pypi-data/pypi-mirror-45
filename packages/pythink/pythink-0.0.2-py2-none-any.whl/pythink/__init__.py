3d56218458487e7484a91988b3cd2c2f29c09658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:26 +0000

    Add package pythink/0.0.2/pythink-0.0.2-py2-none-any.whl

---
+++ packages/pythink/pythink-0.0.2-py2-none-any.whl/pythink/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# @Date    : 2019-04-13
+# @Author  : Peng Shiyu
+
+from __future__ import unicode_literals, print_function
+
+from .think_database import ThinkDatabase
+from .think_table import ThinkTable
+from .think_model import ThinkModel
+
---
+++ packages/pythink/pythink-0.0.2-py2-none-any.whl/pythink/logger.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# @Date    : 2019-04-14
+# @Author  : Peng Shiyu
+
+import logging
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
