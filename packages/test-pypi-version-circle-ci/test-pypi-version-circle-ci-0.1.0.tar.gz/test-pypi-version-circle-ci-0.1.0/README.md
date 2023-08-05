67d3c6a483ee4724a85bde7bf2399216ebed3f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:31 +0000

    Add package test-pypi-version-circle-ci/0.1.0/test-pypi-version-circle-ci-0.1.0.tar.gz

---
+++ packages/test-pypi-version-circle-ci/test-pypi-version-circle-ci-0.1.0.tar.gz/test-pypi-version-circle-ci-0.1.0/README.md
@@ -0,0 +1,3 @@
+# test-pypi-version-travis-ci
+
+The example of the PyPi version checking on the Travis-CI.
---
+++ packages/test-pypi-version-circle-ci/test-pypi-version-circle-ci-0.1.0.tar.gz/test-pypi-version-circle-ci-0.1.0/setup.py
@@ -0,0 +1,10 @@
+"""
+Setup the package.
+"""
+from setuptools import find_packages, setup
+
+setup(
+    version='0.1.0',
+    name='test-pypi-version-circle-ci',
+    packages=find_packages(),
+)
