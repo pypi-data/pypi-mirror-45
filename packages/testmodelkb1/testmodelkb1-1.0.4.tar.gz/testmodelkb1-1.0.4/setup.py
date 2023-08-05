97d67e97defd5c54bd13f407280feb0ade0931b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:37 +0000

    Add package testmodelkb1/1.0.4/testmodelkb1-1.0.4.tar.gz

---
+++ packages/testmodelkb1/testmodelkb1-1.0.4.tar.gz/testmodelkb1-1.0.4/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testmodelkb1",
+    version="1.0.4",
+    author="Sirisha Rella",
+    author_email="srnk2@mail.umkc.edu",
+    description="ModelKB",
+    long_description="ModelKB",
+    long_description_content_type="text/markdown",
+    url="https://github.com/SirishaRella/Xsequential_package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+)
