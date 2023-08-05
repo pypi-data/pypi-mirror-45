0407fe6285bc10503f5c936434e4928ab349312e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:14:15 +0000

    Add package tortoise-orm/0.11.9/tortoise-orm-0.11.9.tar.gz

---
+++ packages/tortoise-orm/tortoise-orm-0.11.9.tar.gz/tortoise-orm-0.11.9/tortoise/backends/sqlite/executor.py
@@ -0,0 +1,35 @@
+from decimal import Decimal
+from typing import List
+
+from pypika import Parameter, Table
+
+from tortoise import fields
+from tortoise.backends.base.executor import BaseExecutor
+
+
+def to_db_bool(self, value, instance):
+    if value is None:
+        return None
+    return int(bool(value))
+
+
+def to_db_decimal(self, value, instance):
+    if value is None:
+        return None
+    if self.decimal_places == 0:
+        quant = "1"
+    else:
+        quant = "1.{}".format("0" * self.decimal_places)
+    return str(Decimal(value).quantize(Decimal(quant)).normalize())
+
+
+class SqliteExecutor(BaseExecutor):
+    TO_DB_OVERRIDE = {fields.BooleanField: to_db_bool, fields.DecimalField: to_db_decimal}
+    EXPLAIN_PREFIX = "EXPLAIN QUERY PLAN"
+
+    def _prepare_insert_statement(self, columns: List[str]) -> str:
+        return str(
+            self.db.query_class.into(Table(self.model._meta.table))
+            .columns(*columns)
+            .insert(*[Parameter("?") for _ in range(len(columns))])
+        )
