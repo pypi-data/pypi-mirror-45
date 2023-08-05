342675944dfe1ec37ba9f6223b18af12a1a62790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:05 +0000

    Add package cancerscope/0.28/cancerscope-0.28.tar.gz

---
+++ packages/cancerscope/cancerscope-0.28.tar.gz/cancerscope-0.28/cancerscope/__init__.py
@@ -0,0 +1,25 @@
+""" 
+cancerscope provides cancer-type predictions for RNA-Seq samples  
+"""
+import os, sys
+from tests import *
+
+### Global opts
+from config import REF_LABELCOUNTS,SCOPEMODELS_DATADIR, SCOPEMODELS_LIST, SCOPEMODELS_FILELIST_DIR, REF_DISEASECODES, getmodelsdict
+from cancerscope.utils import *
+
+### Data sources
+from cancerscope.get_models import getmodel, findmodel, downloadmodel
+from cancerscope.get_models import *
+
+### Dependent data
+from cancerscope.scope_plots import *
+from cancerscope.scope_ensemble import scopemodel, scope
+
+if sys.version_info < (2, 7):
+	raise ImportError("Cancerscope module requires Python 2.7 or higher")
+
+__version__ = '0.28'
+
+
+
