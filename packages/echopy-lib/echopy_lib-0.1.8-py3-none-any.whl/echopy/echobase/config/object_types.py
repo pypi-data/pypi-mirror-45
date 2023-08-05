3a00949b92a2d77723b42e864ec8a6c021dd7f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:35 +0000

    Add package echopy-lib/0.1.8/echopy_lib-0.1.8-py3-none-any.whl

---
+++ packages/echopy-lib/echopy_lib-0.1.8-py3-none-any.whl/echopy/echobase/config/object_types.py
@@ -0,0 +1,21 @@
+#: object_types
+class ObjectTypes:
+    def __init__(self):
+        self.null = 0
+        self.base = 1
+        self.account = 2
+        self.asset = 3
+        self.force_settlement = 4
+        self.committee_member = 5
+        self.limit_order = 6
+        self.call_order = 7
+        self.custom = 8
+        self.proposal = 9
+        self.operation_history = 10
+        self.withdraw_permission = 11
+        self.vesting_balance = 12
+        self.balance = 13
+        self.contract = 14
+        self.contract_result = 15
+        self.block_result = 16
+        self.sidechain_transfer = 17
