dc7724981b7b28c4ea0c717a172c4fc2290ce463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:06 +0000

    Add package pytest-blame/0.0.1/pytest-blame-0.0.1.tar.gz

---
+++ packages/pytest-blame/pytest-blame-0.0.1.tar.gz/pytest-blame-0.0.1/setup.py
@@ -0,0 +1,35 @@
+""" This file is used for deployment. """
+import io
+import os
+from setuptools import setup, find_packages
+
+install_requires = ["pygithub", "pytest>=4.4.0", "gitpython", "requests"]
+
+
+def read(filename):
+    """ This function is reads in the file with the file path """
+    filepath = os.path.join(os.path.dirname(__file__), filename)
+    with io.open(filepath, mode="r", encoding="utf-8") as f:
+        return f.read()
+
+
+setup(
+    name="pytest-blame",
+    version="0.0.1",
+    description="A pytest plugin helps developers to debug by providing useful commits history.",
+    long_description=read("README.md"),
+    author="Lancaster Wu, Spencer Huang, Carson Quigley, Patrick Palad, Paul Livingston",
+    author_email="wuj@allegheny.edu, huangs@allegheny.edu, quigleyc@allegheny.edu, "
+    + "paladp@allegheny.edu, livingstonp@allegheny.edu",
+    url="https://github.com/inTestiGator/pytest-blame",
+    license="GNU",
+    platforms="any",
+    packages=find_packages(),
+    install_requires=install_requires,
+    py_modules=["pytest-blame"],
+    entry_points={
+        'pytest11': [
+            'blame = pytest_blame',
+        ],
+    },
+)
