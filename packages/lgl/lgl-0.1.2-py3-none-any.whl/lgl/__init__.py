c14dcd8376a7522d8d0eebd0be5399b99380daad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:24 +0000

    Add package lgl/0.1.2/lgl-0.1.2-py3-none-any.whl

---
+++ packages/lgl/lgl-0.1.2-py3-none-any.whl/lgl/__init__.py
@@ -0,0 +1,51 @@
+import sys
+import os
+import pathlib
+import lgl.run
+import lgl.init
+import lgl.traverse
+import lgl.get_unresolve
+
+import importmagic
+
+def project_home():
+    cwd = os.getcwd()
+    while not os.path.exists(cwd+os.sep+"setup.py"):
+        cwd = pathlib.Path(os.getcwd()).resolve().parents[0]
+    if os.path.exists(cwd+os.sep+"setup.py"):
+        return cwd
+
+def filelist(path):
+    if os.sep==path[len(path)-1]:
+        return traverse.traverse(path)
+    else:
+        return [path]
+
+def list_unresolved(path):
+    return get_unresolve.list_unresolved(path)
+
+def add_import(files):
+    path_ = sys.path + [os.getcwd()]
+    index = importmagic.SymbolIndex()
+    index.build_index(paths=path_)
+    result=[]
+    for file in files:
+        filelist_ = filelist(file)
+        if(isinstance(filelist_,list)):
+            result=result+filelist_
+    return get_unresolve.add_import(result,index)
+
+def install_module(basefiles):
+    result=[]
+    for file in basefiles:
+        filelist_ = filelist(file)
+        if(isinstance(filelist_,list)):
+            for file in filelist_:
+                result=result+list_unresolved(file)
+    return get_unresolve.install_module(result)
+
+def run_():
+    run.cmd()
+
+def init_(args):
+    init.cmd(args)
---
+++ packages/lgl/lgl-0.1.2-py3-none-any.whl/lgl/get_unresolve.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import subprocess
+import toml
+import pkgutil
+
+import importmagic
+
+def list_unresolved (path):
+    with open(path) as f:
+        python_source = f.read()
+    scope = importmagic.Scope.from_source(python_source)
+    unresolved, unreferenced = scope.find_unresolved_and_unreferenced_symbols()
+    return list(unresolved)
+
+def add_import (paths,index):
+    path_ = sys.path + [os.getcwd()]
+    for path in paths:
+        with open(path) as f:
+            python_source = f.read()
+        scope = importmagic.Scope.from_source(python_source)
+        unresolved, unreferenced = scope.find_unresolved_and_unreferenced_symbols()
+        python_source = importmagic.update_imports(python_source, index, unresolved, unreferenced)
+        with open(path, 'w') as f:
+            f.write(python_source)
+
+def get_proper_module_name(name):
+    file=os.path.expanduser("~")+os.path.sep+".lgl"+os.path.sep+"pydic"+os.path.sep+"info.toml"
+    with open(file) as f:
+        toml_src = f.read()
+    parsed_toml = toml.loads(toml_src)
+    return parsed_toml['dict'].get(name) or name
+
+def filter_modules(mods):
+    for m in pkgutil.iter_modules():
+        if m.name in mods:
+            mods.remove(m.name)
+    for m in mods:
+        if m in sys.builtin_module_names:
+            mods.remove(m)
+    return mods
+
+def install_module(modules):
+    mlist = list(dict.fromkeys([get_proper_module_name(x.split(".")[0]) for x in modules]))
+    mlist = filter_modules(mlist)
+    if mlist:
+        subprocess.run(["conda","install","-y"]+mlist)
