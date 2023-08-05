b0d9b4d5e71a735a3ed0124d75e103a83f46c87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:10 +0000

    Add package unassigner/0.0.3/unassigner-0.0.3.tar.gz

---
+++ packages/unassigner/unassigner-0.0.3.tar.gz/unassigner-0.0.3/unassigner/command.py
@@ -0,0 +1,115 @@
+import argparse
+import os
+
+from unassigner.algorithm import (
+    UnassignAligner, FileAligner, ThresholdAlgorithm,
+)
+from unassigner.parse import parse_fasta, parse_species_names
+from unassigner.prepare_strain_data import download_type_strain_data
+
+def main(argv=None):
+    p = argparse.ArgumentParser()
+    p.add_argument("query_fasta", type=argparse.FileType("r"),
+        help="Query sequences FASTA file")
+    p.add_argument("--output_dir",
+        help=(
+            "Output directory (default: basename of query sequences FASTA "
+            "file, plus '_unassigned')"))
+    p.add_argument("--type_strain_fasta", default="unassigner_species.fasta",
+        help=(
+            "Type strain sequences FASTA file (default: %(default)s). "
+            "If the default file is not found, sequences are downloaded "
+            "and re-formatted automatically."))
+    p.add_argument("--num_cpus", type=int,
+        help=(
+            "Number of CPUs to use during sequence aligment (default: "
+            "use all the CPUs)"))
+    p.add_argument("--verbose", action="store_true",
+        help= "Activate verbose mode.")
+    args = p.parse_args(argv)
+
+    if args.verbose is True:
+        logging.basicConfig(level=logging.INFO)
+
+    query_seqs = list(parse_fasta(args.query_fasta, trim_desc=True))
+
+    if args.output_dir is None:
+        output_dir = os.path.splitext(args.query_fasta.name)[0] + "_unassigned"
+    else:
+        output_dir = args.output_dir
+
+    # Download type strain files if needed
+    type_strain_fp_is_default = (
+        args.type_strain_fasta == p.get_default("type_strain_fasta"))
+    type_strain_fp_is_missing = not os.path.exists(args.type_strain_fasta)
+    if type_strain_fp_is_default and type_strain_fp_is_missing:
+        download_type_strain_data()
+
+    with open(args.type_strain_fasta) as f:
+        species_names = dict(parse_species_names(f))
+
+    writer = OutputWriter(output_dir, species_names)
+
+    alignment_query_fp = writer.output_fp("unassigner_query.fasta")
+    alignment_output_fp = writer.output_fp("unassigner_query_hits.txt")
+    if os.path.exists(alignment_output_fp):
+        a = FileAligner(args.type_strain_fasta, alignment_output_fp)
+    else:
+        a = UnassignAligner(args.type_strain_fasta)
+        a.species_input_fp = alignment_query_fp
+        a.species_output_fp = alignment_output_fp
+        a.num_cpus = args.num_cpus
+
+    algorithm = ThresholdAlgorithm(a)
+    for query_id, query_results in algorithm.unassign(query_seqs):
+        writer.write_results(query_id, query_results)
+
+
+class OutputWriter:
+    standard_keys = [
+        "typestrain_id", "region_mismatches", "region_positions",
+        "probability_incompatible"]
+
+    def __init__(self, output_dir, species_names):
+        self.species_names = species_names
+
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+        self.output_dir = output_dir
+
+        standard_fp = self.output_fp("unassigner_output.tsv")
+        self.standard_file = open(standard_fp, "w")
+        standard_header = ["query_id", "species"] + self.standard_keys
+        self._write_tsv_line(self.standard_file, standard_header)
+
+        algorithm_fp = self.output_fp("algorithm_output.tsv")
+        self.algorithm_file = open(algorithm_fp, "w")
+        self.algorithm_keys = None
+
+    def output_fp(self, filename):
+        return os.path.join(self.output_dir, filename)
+
+    @staticmethod
+    def _write_tsv_line(fileobj, vals):
+        vals = [str(val) for val in vals]
+        line = "\t".join(vals)
+        fileobj.write(line)
+        fileobj.write("\n")
+
+    def write_results(self, query_id, results):
+        for result in results:
+            species_name = self.species_names.get(result["typestrain_id"], "NA")
+
+            # Set custom algorithm keys based on the first result
+            if self.algorithm_keys is None:
+                self.algorithm_keys = list(result.keys())
+                algorithm_header = ["query_id", "species_name"] + self.algorithm_keys
+                self._write_tsv_line(self.algorithm_file, algorithm_header)
+
+            standard_vals = [query_id, species_name] + \
+                [result[k] for k in self.standard_keys]
+            self._write_tsv_line(self.standard_file, standard_vals)
+
+            algorithm_vals = [query_id, species_name] + \
+                [result[k] for k in self.algorithm_keys]
+            self._write_tsv_line(self.algorithm_file, algorithm_vals)
---
+++ packages/unassigner/unassigner-0.0.3.tar.gz/unassigner-0.0.3/unassigner/download.py
@@ -0,0 +1,119 @@
+import collections
+import os
+import subprocess
+import tarfile
+
+from unassigner.parse import parse_fasta, parse_greengenes_accessions
+
+LTP_METADATA_COLS = [
+    "accession",
+    "start",
+    "stop",
+    "unknown",
+    "fullname_ltp",
+    "type_ltp",
+    "hi_tax_ltp",
+    "riskgroup_ltp",
+    "url_lpsn_ltp",
+    "tax_ltp",
+    "rel_ltp",
+    "NJ_support_pk4_ltp"
+    ]
+LTP_METADATA_URL = \
+    "https://www.arb-silva.de/fileadmin/silva_databases/living_tree/LTP_release_128/LTPs128_SSU/LTPs128_SSU.csv"
+LTP_SEQS_URL = \
+    "https://www.arb-silva.de/fileadmin/silva_databases/living_tree/LTP_release_128/LTPs128_SSU/LTPs128_SSU_unaligned.fasta"
+GG_SEQS_URL = \
+    "ftp://greengenes.microbio.me/greengenes_release/gg_13_5/gg_13_5.fasta.gz"
+GG_ACCESSIONS_URL = \
+    "ftp://greengenes.microbio.me/greengenes_release/gg_13_5/gg_13_5_accessions.txt.gz"
+SPECIES_FASTA_FP = "unassigner_species.fasta"
+REFSEQS_FASTA_FP = "refseqs.fasta"
+GG_DUPLICATE_FP = "gg_duplicate_ids.txt"
+
+def clean(db_dir):
+    fps = [
+        url_fp(LTP_METADATA_URL),
+        url_fp(LTP_SEQS_URL),
+        SPECIES_FASTA_FP,
+        gunzip_fp(url_fp(GG_SEQS_URL)),
+        gunzip_fp(url_fp(GG_ACCESSIONS_URL)),
+        REFSEQS_FASTA_FP,
+        GG_DUPLICATE_FP
+        ]
+    for fp in fps:
+        fp_full = os.path.join(db_dir, fp)
+        if os.path.exists(fp_full):
+            os.remove(fp_full)
+
+
+def url_fp(url):
+    return url.split('/')[-1]
+
+
+def gunzip_fp(fp):
+    return fp[:-3]
+
+
+def get_url(url, fp):
+    if os.path.exists(fp):
+        os.remove(fp)
+    subprocess.check_call(["wget", "-O", fp, url])
+    return fp
+
+
+def process_ltp_seqs(input_fp, output_fp=SPECIES_FASTA_FP):
+    if os.path.isdir(output_fp):
+        output_fp = os.path.join(output_fp, SPECIES_FASTA_FP)
+    # Re-format FASTA file
+    with open(input_fp) as f_in:
+        seqs = parse_fasta(f_in)
+        with open(output_fp, "w") as f_out:
+            for desc, seq in seqs:
+                vals = desc.split("\t")
+                accession = vals[0]
+                species_name = vals[5]
+                f_out.write(
+                    ">{0}\t{1}\n{2}\n".format(accession, species_name, seq))
+    return output_fp
+
+
+def process_greengenes_seqs(seqs_fp, accessions_fp, output_fp=REFSEQS_FASTA_FP):
+    duplicates_fp = GG_DUPLICATE_FP
+    if os.path.isdir(output_fp):
+        duplicates_fp = os.path.join(output_fp, duplicates_fp)
+        output_fp = os.path.join(output_fp, REFSEQS_FASTA_FP)
+
+    # Extract table of accessions
+    if accessions_fp.endswith(".gz"):
+        subprocess.check_call(["gunzip", "-f", accessions_fp])
+        accessions_fp = gunzip_fp(accessions_fp)
+
+    # Load accessions
+    gg_accessions = {}
+    with open(accessions_fp) as f:
+        for ggid, src, acc in parse_greengenes_accessions(f):
+            gg_accessions[ggid] = (acc, src)
+
+    # Extract FASTA file
+    if seqs_fp.endswith(".gz"):
+        subprocess.check_call(["gunzip", "-f", seqs_fp])
+        seqs_fp = gunzip_fp(seqs_fp)
+
+    # Remove duplicate reference seqs
+    uniq_seqs = collections.defaultdict(list)
+    with open(seqs_fp) as f:
+        for ggid, seq in parse_fasta(f):
+            uniq_seqs[seq].append(ggid)
+
+    with open(duplicates_fp, "w") as dups:
+        with open(output_fp, "w") as f:
+            for seq, ggids in uniq_seqs.items():
+                ggid = ggids[0]
+                if len(ggids) > 1:
+                    dups.write(" ".join(ggids))
+                # Re-label seqs with accession numbers
+                acc, src = gg_accessions[ggid]
+                f.write(">%s %s %s\n%s\n" % (acc, src, ggid, seq))
+
+    return output_fp
