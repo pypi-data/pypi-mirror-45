f6afed41fd668b8640bf62d3a387de669003531e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:08 +0000

    Add package dpd-static-support/0.0.3/dpd_static_support-0.0.3-py3-none-any.whl

---
+++ packages/dpd-static-support/dpd_static_support-0.0.3-py3-none-any.whl/dpd_static_support/mappings.py
@@ -0,0 +1,15 @@
+# Direct substitution mappings
+
+substitutions = [
+
+    ('https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css','static/dpd_static_support/css'),
+    ('https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js','static/dpd_static_support/js'),
+    ("https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd", 'static/dpd_static_support/js'),
+    ("https://code.jquery.com/jquery-3.3.1.min.js", "static/dpd_static_support/js/jquery-3.3.1.min.js"),
+
+    ('integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"', ''),
+    ('integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"', ''),
+    ('integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"', ''),
+    ('integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT"', ''),
+
+]
