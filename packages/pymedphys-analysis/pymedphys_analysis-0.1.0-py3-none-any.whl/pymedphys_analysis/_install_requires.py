a962da37e70857163d61b16ddd1ee93077ac04f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:35 +0000

    Add package pymedphys-analysis/0.1.0/pymedphys_analysis-0.1.0-py3-none-any.whl

---
+++ packages/pymedphys-analysis/pymedphys_analysis-0.1.0-py3-none-any.whl/pymedphys_analysis/_install_requires.py
@@ -0,0 +1,10 @@
+install_requires = [
+    'psutil',
+    'pymedphys_coordsandscales',
+    'pymedphys_utilities',
+    'Pillow',
+    'scipy',
+    'numpy < 1.16, >= 1.12',
+    'pandas',
+    'matplotlib'
+]
