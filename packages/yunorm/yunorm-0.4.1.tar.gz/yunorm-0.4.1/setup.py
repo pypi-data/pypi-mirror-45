149c037ac56360e720a0a6bbf9df1a04ba1738bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:26 +0000

    Add package yunorm/0.4.1/yunorm-0.4.1.tar.gz

---
+++ packages/yunorm/yunorm-0.4.1.tar.gz/yunorm-0.4.1/setup.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+import os
+from setuptools import setup
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+setup(
+    name="yunorm",
+    version="0.4.1",
+    packages=get_packages('yunorm'),
+    author="yunsonbai",
+    author_email='1572120849@qq.com',
+    url="https://github.com/yunsonbai/yunorm",
+    description="A small ORM but It's very practical",
+    long_description="Usage document: https://github.com/yunsonbai/yunorm",
+    license="Apache",
+    install_requires=["mysqlclient==1.3.14", "mysql-connector-python==8.0.15"],
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        "License :: OSI Approved :: Apache Software License",
+        'Operating System :: OS Independent',
+    ]
+)
