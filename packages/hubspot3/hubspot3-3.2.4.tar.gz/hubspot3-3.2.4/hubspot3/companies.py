0b104d5380d883f02b0d584e4d6c821244d24638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:31 +0000

    Add package hubspot3/3.2.4/hubspot3-3.2.4.tar.gz

---
+++ packages/hubspot3/hubspot3-3.2.4.tar.gz/hubspot3-3.2.4/hubspot3/companies.py
@@ -0,0 +1,160 @@
+"""
+hubspot companies api
+"""
+from hubspot3 import logging_helper
+from hubspot3.base import BaseClient
+from hubspot3.utils import prettify
+
+
+COMPANIES_API_VERSION = "2"
+
+
+class CompaniesClient(BaseClient):
+    """
+    The hubspot3 Companies client uses the _make_request method to call the API
+    for data.  It returns a python object translated from the json return
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(CompaniesClient, self).__init__(*args, **kwargs)
+        self.log = logging_helper.get_log("hubspot3.companies")
+
+    def _get_path(self, subpath):
+        return "companies/v{}/{}".format(
+            self.options.get("version") or COMPANIES_API_VERSION, subpath
+        )
+
+    def create(self, data=None, **options):
+        """create a new company"""
+        data = data or {}
+        return self._call("companies/", data=data, method="POST", **options)
+
+    def update(self, company_id, data=None, **options):
+        """update the given company with data"""
+        data = data or {}
+        return self._call(
+            "companies/{}".format(company_id), data=data, method="PUT", **options
+        )
+
+    def delete(self, company_id, **options):
+        """delete a company"""
+        return self._call("companies/{}".format(company_id), method="DELETE", **options)
+
+    def get(self, company_id, **options):
+        """get a single company by it's ID"""
+        return self._call("companies/{}".format(company_id), method="GET", **options)
+
+    def search_domain(self, domain, limit=1, extra_properties=None, **options):
+        """searches for companies by domain name. limit is max'd at 100"""
+        # default properties to fetch
+        properties = [
+            "domain",
+            "createdate",
+            "name",
+            "hs_lastmodifieddate",
+            "hubspot_owner_id",
+        ]
+
+        # append extras if they exist
+        if extra_properties:
+            if isinstance(extra_properties, list):
+                properties += extra_properties
+            if isinstance(extra_properties, str):
+                properties.append(extra_properties)
+
+        return self._call(
+            "domains/{}/companies".format(domain),
+            method="POST",
+            data={"limit": limit, "requestOptions": {"properties": properties}},
+            **options,
+        )
+
+    def get_all(self, extra_properties=None, **options):
+        """get all companies"""
+        finished = False
+        output = []
+        offset = 0
+        querylimit = 250  # Max value according to docs
+
+        # default properties to fetch
+        properties = [
+            "name",
+            "description",
+            "address",
+            "address2",
+            "city",
+            "state",
+            "story",
+            "hubspot_owner_id",
+        ]
+
+        # append extras if they exist
+        if extra_properties:
+            if isinstance(extra_properties, list):
+                properties += extra_properties
+            if isinstance(extra_properties, str):
+                properties.append(extra_properties)
+
+        while not finished:
+            batch = self._call(
+                "companies/paged",
+                method="GET",
+                doseq=True,
+                params={
+                    "limit": querylimit,
+                    "offset": offset,
+                    "properties": properties,
+                },
+                **options,
+            )
+            output.extend(
+                [
+                    prettify(company, id_key="companyId")
+                    for company in batch["companies"]
+                    if not company["isDeleted"]
+                ]
+            )
+            finished = not batch["has-more"]
+            offset = batch["offset"]
+
+        return output
+
+    def _get_recent(self, recency_type, **options):
+        """
+        Returns either list of recently modified companies or recently created companies,
+        depending on recency_type passed in. Both API endpoints take identical parameters
+        and return identical formats, they differ only in the URLs
+        (companies/recent/created or companies/recent/modified)
+        @see: https://developers.hubspot.com/docs/methods/companies/get_companies_modified
+        @see: https://developers.hubspot.com/docs/methods/companies/get_companies_created
+        """
+        finished = False
+        output = []
+        offset = 0
+        querylimit = 250  # Max value according to docs
+
+        while not finished:
+            batch = self._call(
+                "companies/recent/%s" % recency_type,
+                method="GET",
+                doseq=True,
+                params={"count": querylimit, "offset": offset},
+                **options,
+            )
+            output.extend(
+                [
+                    prettify(company, id_key="companyId")
+                    for company in batch["results"]
+                    if not company["isDeleted"]
+                ]
+            )
+            finished = not batch["hasMore"]
+            offset = batch["offset"]
+
+        return output
+
+    def get_recently_modified(self, **options):
+        return self._get_recent("modified", **options)
+
+    def get_recently_created(self, **options):
+        return self._get_recent("created", **options)
---
+++ packages/hubspot3/hubspot3-3.2.4.tar.gz/hubspot3-3.2.4/hubspot3/engagements.py
@@ -0,0 +1,78 @@
+"""
+hubspot engagements api
+"""
+from hubspot3 import logging_helper
+from hubspot3.base import BaseClient
+
+
+ENGAGEMENTS_API_VERSION = "1"
+
+
+class EngagementsClient(BaseClient):
+    """
+    The hubspot3 Engagements client uses the _make_request method to call the API
+    for data.  It returns a python object translated from the json return
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(EngagementsClient, self).__init__(*args, **kwargs)
+        self.log = logging_helper.get_log("hubspot3.engagements")
+
+    def _get_path(self, subpath):
+        return "engagements/v{}/{}".format(
+            self.options.get("version") or ENGAGEMENTS_API_VERSION, subpath
+        )
+
+    def get(self, engagement_id, **options):
+        """Get a HubSpot engagement."""
+        return self._call(
+            "engagements/{}".format(engagement_id), method="GET", **options
+        )
+
+    def get_associated(self, object_type, object_id, **options):
+        finished = False
+        output = []
+        querylimit = 100  # Max value according to docs
+        offset = 0
+        while not finished:
+            print(offset)
+            batch = self._call(
+                "engagements/associated/{}/{}/paged".format(object_type, object_id),
+                method="GET",
+                params={"limit": querylimit, "offset": offset},
+                **options
+            )
+            print(len(batch["results"]))
+            output.extend(batch["results"])
+            finished = not batch["hasMore"]
+            offset = batch["offset"]
+
+        return output
+
+    def create(self, data=None, **options):
+        data = data or {}
+        return self._call("engagements", data=data, method="POST", **options)
+
+    def update(self, key, data=None, **options):
+        data = data or {}
+        return self._call(
+            "engagements/{}".format(key), data=data, method="PUT", **options
+        )
+
+    def get_all(self, **options):
+        finished = False
+        output = []
+        querylimit = 250  # Max value according to docs
+        offset = 0
+        while not finished:
+            batch = self._call(
+                "engagements/paged",
+                method="GET",
+                params={"limit": querylimit, "offset": offset},
+                **options
+            )
+            output.extend(batch["results"])
+            finished = not batch["hasMore"]
+            offset = batch["offset"]
+
+        return output
