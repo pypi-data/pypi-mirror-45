bcef9c80dc26282dd9a20d1c9a5af874b7e25d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:01 +0000

    Add package miniworldmaker/0.9.27/miniworldmaker-0.9.27-py3.6.egg

---
+++ packages/miniworldmaker/miniworldmaker-0.9.27-py3.6.egg/miniworldmaker/__init__.py
@@ -0,0 +1,43 @@
+import miniworldmaker.tokens.token
+import miniworldmaker.tokens.actor
+from miniworldmaker.tokens import token
+import miniworldmaker.containers.container as container
+import miniworldmaker.windows.miniworldwindow as window
+import miniworldmaker.tools.db_manager as db_manager
+from miniworldmaker import boards
+from miniworldmaker.tokens.token import Token
+from miniworldmaker.tokens.actor import Actor
+from miniworldmaker.boards.tiled_board import TiledBoard
+from miniworldmaker.boards.pixel_board import PixelBoard
+from miniworldmaker.boards.board import Board
+from miniworldmaker.containers.toolbar import Toolbar
+from miniworldmaker.containers.event_console import EventConsole
+from miniworldmaker.containers.select_actor_toolbar import SelectActorToolbar
+from miniworldmaker.containers.console import Console
+from miniworldmaker.containers.color_console import ColorConsole
+from miniworldmaker.containers.actionbar import ActionBar
+from miniworldmaker.containers.toolbar_widgets import *
+from miniworldmaker.containers.token_toolbar import TokenToolbar
+from miniworldmaker.boards.board_position import BoardPosition
+
+__all__ = ['Token',
+           'Actor',
+           'TiledBoard',
+           'SelectActorToolbar',
+           'PixelBoard',
+           'Toolbar',
+           'Board',
+           'ToolbarLabel',
+           'ToolbarButton',
+           'SaveButton',
+           'LoadButton',
+           'Console',
+           'EventConsole',
+           'ActionBar',
+           'TokenToolbar',
+           'ColorConsole',
+           'BoardPosition',
+           'CounterLabel',
+           'TimeLabel',
+           'FPSLabel',
+           ]
---
+++ packages/miniworldmaker/miniworldmaker-0.9.27-py3.6.egg/miniworldmaker/tokens/costume.py
@@ -0,0 +1,50 @@
+from tools import appearance
+import pygame
+
+
+class Costume(appearance.Appearance):
+
+    def __init__(self, token):
+        super().__init__()
+        self.parent = token
+        self.size = self.parent.size
+        self.is_upscaled = True
+        self.is_rotatable = True
+        self.image_actions.append("info_overlay")
+        self.enabled_image_actions["info_overlay"] =  False
+        self.image_handlers["info_overlay"] = self.info_overlay
+
+    def update(self):
+        if self.parent.board.frame % self.animation_speed == 0:
+            self.next_sprite()
+
+    def set_costume(self, index):
+        self._image_index = index
+
+    def show_info_overlay(self, color=(255, 255, 255, 255)):
+        """
+        Shows info overlay (Rectangle around the token and Direction marker)
+        Args:
+            color: Color of info_overlay
+        """
+        self.parent.dirty = 1
+        self.color = color
+        self.enabled_image_actions["info_overlay"] = True
+        self.call_action("info_overlay")
+
+    def hide_info_overlay(self):
+        self.parent.dirty = 1
+        self.enabled_image_actions["info_overlay"] = False
+        self.call_action("info_overlay")
+
+
+    def info_overlay(self, image):
+        pygame.draw.rect(image, self.color,
+                         (0, 0, image.get_rect().width, image.get_rect().height), 10)
+        # draw direction marker on image
+        rect = image.get_rect()
+        center = rect.center
+        x = rect.right
+        y = rect.centery
+        pygame.draw.line(image, self.color, (center[0], center[1]), (x, y))
+        return image
