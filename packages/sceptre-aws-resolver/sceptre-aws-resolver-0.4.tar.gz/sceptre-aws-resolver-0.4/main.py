0534d52d1f46a0b53d19e1db09cf977e6d5f5f47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:26 +0000

    Add package sceptre-aws-resolver/0.4/sceptre-aws-resolver-0.4.tar.gz

---
+++ packages/sceptre-aws-resolver/sceptre-aws-resolver-0.4.tar.gz/sceptre-aws-resolver-0.4/main.py
@@ -0,0 +1,37 @@
+import logging
+from ast import literal_eval
+
+import jmespath
+from sceptre.resolvers import Resolver
+
+
+class Aws(Resolver):
+    """
+    Docs: https://github.com/jeshan/sceptre-aws-resolver
+    """
+
+    def __init__(self, *args, **kwargs):
+        self.logger = logging.getLogger(__name__)
+        super(Aws, self).__init__(*args, **kwargs)
+
+    def resolve(self):
+        # self.argument, self.stack
+        arg_list = self.argument.split('::')
+        service = arg_list[0]
+        operation = arg_list[1]
+        expression = None
+        region = None
+        parsed_args = {}
+        if len(arg_list) > 2 and arg_list[2]:
+            args = arg_list[2]
+            parsed_args = literal_eval("{" + args + "}")
+        if len(arg_list) > 3 and arg_list[3]:
+            expression = arg_list[3]
+        if len(arg_list) > 4 and arg_list[4]:
+            region = arg_list[4]
+        # noinspection PyStatementEffect
+        self.stack.template  # just to initialise the connection manager
+        result = self.stack.connection_manager.call(service, operation, parsed_args, region=region)
+        if expression:
+            result = jmespath.search(expression, result)
+        return str(result)
