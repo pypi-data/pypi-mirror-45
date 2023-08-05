f9147cd9d4c6b58ace70d7bc1266662a8a6a79f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:54 +0000

    Add package BayanaaTest/1.0.0/BayanaaTest-1.0.0-py3-none-any.whl

---
+++ packages/BayanaaTest/BayanaaTest-1.0.0-py3-none-any.whl/homework/sanamsargui.py
@@ -0,0 +1,12 @@
+import random
+
+print('sanamsargui', __name__)
+
+def sanamsargui_ner(*ners):
+    return random.choice(ners)
+
+def sanamsargui_too(a, b):
+    return random.randint(a, b)
+
+def sanamsargui_butarkhai():
+    return random.random()
