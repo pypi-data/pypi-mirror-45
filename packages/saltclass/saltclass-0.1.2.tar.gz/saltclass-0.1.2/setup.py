ea3bd2f9a417fdc0ca83a9745798f478e3d52c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:16:21 +0000

    Add package saltclass/0.1.2/saltclass-0.1.2.tar.gz

---
+++ packages/saltclass/saltclass-0.1.2.tar.gz/saltclass-0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='saltclass',
+      version='0.1.2',
+      description='Short and Long Text Classifier using clustering-based enrichment',
+      long_description=readme(),
+      keywords='short text classification',
+      url='https://github.com/bagheria/saltclass',
+      author='Ayoub Bagheri',
+      author_email='ayoub.bagheri@gmail.com',
+      license='MIT',
+      packages=['saltclass'],
+      include_package_data=True,
+      zip_safe=False)
