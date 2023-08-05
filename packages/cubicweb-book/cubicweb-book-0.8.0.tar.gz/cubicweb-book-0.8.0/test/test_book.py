3f3c5e53811b937878e4f3f4b0cc6268d17024fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:24 +0000

    Add package cubicweb-book/0.8.0/cubicweb-book-0.8.0.tar.gz

---
+++ packages/cubicweb-book/cubicweb-book-0.8.0.tar.gz/cubicweb-book-0.8.0/test/test_book.py
@@ -0,0 +1,32 @@
+"""template automatic tests"""
+
+from cubicweb.devtools.testlib import AutomaticWebTest
+from cubicweb.devtools.fill import ValueGenerator
+
+import random
+
+
+def random_numbers(size):
+    return u''.join(random.choice('0123456789') for i in range(size))
+
+
+class MyValueGenerator(ValueGenerator):
+    def generate_Book_isbn10(self, entity, index):
+        return random_numbers(10)
+
+    def generate_Book_isbn13(self, entity, index):
+        return random_numbers(13)
+
+
+class AutomaticWebTest(AutomaticWebTest):
+
+    def to_test_etypes(self):
+        return set(('Book', 'Collection', 'Editor'))
+
+    def list_startup_views(self):
+        return ()
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
