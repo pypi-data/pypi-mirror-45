37951765f0589c1f3f8103a7acdbe8e60cc514ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:29 +0000

    Add package PysparkGateway/0.0.5/PysparkGateway-0.0.5.tar.gz

---
+++ packages/PysparkGateway/PysparkGateway-0.0.5.tar.gz/PysparkGateway-0.0.5/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import os
+from distutils.core import setup
+
+with open('README.rst') as fp:
+    readme = fp.read()
+
+with open('requirements.txt') as fp:
+    requires = fp.read().split('\n')
+
+setup(
+    name='PysparkGateway',
+    version='0.0.5',
+    packages=[
+        'pyspark_gateway',
+        'pyspark_gateway.patch_files'],
+    license='Apache 2.0',
+    description='Connect Pyspark to remote clusters',
+    long_description=readme,
+    install_requires=requires,
+    extras_require={
+        'dev': [
+            'pandas',
+            'psutil'
+        ]
+    },
+    python_requires='>=2.7',
+    scripts=['bin/pyspark-gateway'],
+    url='https://github.com/abronte/PysparkGateway',
+    author='Adam Bronte',
+    author_email='adam@bronte.me',
+)
