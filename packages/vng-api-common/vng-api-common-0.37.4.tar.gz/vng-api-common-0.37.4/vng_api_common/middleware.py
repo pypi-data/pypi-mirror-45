5094b5b5c04a7176cadd811e43b60f5f0c66724a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:16 +0000

    Add package vng-api-common/0.37.4/vng-api-common-0.37.4.tar.gz

---
+++ packages/vng-api-common/vng-api-common-0.37.4.tar.gz/vng-api-common-0.37.4/vng_api_common/middleware.py
@@ -0,0 +1,144 @@
+# https://pyjwt.readthedocs.io/en/latest/usage.html#reading-headers-without-validation
+# -> we can put the organization/service in the headers itself
+import logging
+from typing import Union
+
+from django.conf import settings
+from django.utils.functional import cached_property
+from django.utils.translation import ugettext as _
+
+import jwt
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
+
+from .constants import VERSION_HEADER
+from .models import JWTSecret
+from .scopes import Scope
+
+logger = logging.getLogger(__name__)
+
+EMPTY_PAYLOAD = {
+    'scopes': [],
+    'zaaktypes': [],
+}
+
+
+class JWTPayload:
+
+    def __init__(self, encoded: str=None):
+        self.encoded = encoded
+
+    def __repr__(self):
+        return "<%s: payload=%r>" % (self.__class__.__name__, self.payload)
+
+    def __getitem__(self, key):
+        return self.payload[key]
+
+    def get(self, attr, *args, **kwargs):
+        return self.payload.get(attr, *args, **kwargs)
+
+    @cached_property
+    def payload(self) -> dict:
+        if self.encoded is None:
+            return EMPTY_PAYLOAD
+
+        try:
+            header = jwt.get_unverified_header(self.encoded)
+        except jwt.DecodeError:
+            logger.info("Invalid JWT encountered")
+            raise PermissionDenied(
+                _('JWT could not be decoded. Possibly you made a copy-paste mistake.'),
+                code='jwt-decode-error'
+            )
+
+        try:
+            jwt_secret = JWTSecret.objects.get(identifier=header['client_identifier'])
+        except JWTSecret.DoesNotExist:
+            raise PermissionDenied(
+                'Client identifier bestaat niet',
+                code='invalid-client-identifier'
+            )
+        except KeyError:
+            raise PermissionDenied(
+                'Client identifier is niet aanwezig in JWT',
+                code='missing-client-identifier'
+            )
+        else:
+            key = jwt_secret.secret
+
+        # the jwt package does verification against tampering (TODO: unit test)
+        try:
+            payload = jwt.decode(self.encoded, key, algorithms='HS256')
+        except jwt.InvalidSignatureError as exc:
+            logger.exception("Invalid signature - possible payload tampering?")
+            raise PermissionDenied(
+                'Client credentials zijn niet geldig',
+                code='invalid-jwt-signature'
+            )
+
+        res = payload.get('zds', EMPTY_PAYLOAD)
+        res['client_id'] = header['client_identifier']
+
+        return res
+
+    def has_scopes(self, scopes: Union[Scope, None]) -> bool:
+        """
+        Check whether all of the expected scopes are present or not.
+        """
+        if scopes is None:
+            # TODO: should block instead of allow it - we'll gradually introduce this
+            return True
+
+        scopes_provided = self.payload.get('scopes', [])
+        logger.debug("Scopes provided are: %s", scopes)
+        # simple form - needs a more complex setup if a scope 'bundles'
+        # other scopes
+        return scopes.is_contained_in(scopes_provided)
+
+
+class AuthMiddleware:
+
+    header = 'HTTP_AUTHORIZATION'
+    auth_type = 'Bearer'
+
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        self.extract_jwt_payload(request)
+        return self.get_response(request) if self.get_response else None
+
+    def extract_jwt_payload(self, request):
+        authorization = request.META.get(self.header, '')
+        prefix = f"{self.auth_type} "
+        if authorization.startswith(prefix):
+            # grab the actual token
+            encoded = authorization[len(prefix):]
+        else:
+            encoded = None
+
+        request.jwt_payload = JWTPayload(encoded)
+
+
+class APIVersionHeaderMiddleware:
+    """
+    Include a header specifying the API-version
+    """
+
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if self.get_response is None:
+            return None
+
+        response = self.get_response(request)
+
+        # not an API response, exit early
+        if not isinstance(response, Response):
+            return response
+
+        # set the header
+        response[VERSION_HEADER] = settings.API_VERSION
+
+        return response
---
+++ packages/vng-api-common/vng-api-common-0.37.4.tar.gz/vng-api-common-0.37.4/vng_api_common/models.py
@@ -0,0 +1,92 @@
+from urllib.parse import urlsplit, urlunsplit
+
+from django.db import models
+from django.db.models.functions import Length
+from django.utils.translation import ugettext_lazy as _
+
+from rest_framework.reverse import reverse
+from zds_client import ClientAuth
+
+
+class APIMixin:
+    """
+    Determine the absolute URL of a resource in the API.
+
+    Model mixin that reverses the URL-path in the API based on the
+    ``uuid``-field of a model instance.
+    """
+
+    def get_absolute_api_url(self, request=None, **kwargs) -> str:
+        """
+        Build the absolute URL of the object in the API.
+        """
+        # build the URL of the informatieobject
+        resource_name = self._meta.model_name
+
+        reverse_kwargs = {'uuid': self.uuid}
+        reverse_kwargs.update(**kwargs)
+
+        url = reverse(
+            f'{resource_name}-detail',
+            kwargs=reverse_kwargs, request=request
+        )
+        return url
+
+
+class JWTSecret(models.Model):
+    """
+    Store credentials of clients that want to access our API.
+
+    Only clients that are known can access the API (if so configured).
+    """
+    identifier = models.CharField(_("identifier"), max_length=50, unique=True)
+    secret = models.CharField(_("secret"), max_length=255)
+
+    def __str__(self):
+        return self.identifier
+
+
+class APICredential(models.Model):
+    """
+    Store credentials for external APIs.
+
+    When we need to authenticate against a remote API, we need to know which
+    client ID and secret to use to sign the JWT.
+    """
+    api_root = models.URLField(_("api root"), unique=True)
+    client_id = models.CharField(_("client id"), max_length=255)
+    secret = models.CharField(_("secret"), max_length=255)
+
+    class Meta:
+        verbose_name = _("API credential")
+        verbose_name_plural = _("API credentials")
+
+    def __str__(self):
+        return self.api_root
+
+    @classmethod
+    def get_auth(cls, url: str, **kwargs) -> ClientAuth:
+        split_url = urlsplit(url)
+        scheme_and_domain = urlunsplit(split_url[:2] + ('', '', ''))
+
+        candidates = (
+            cls.objects
+            .filter(api_root__startswith=scheme_and_domain)
+            .annotate(api_root_length=Length('api_root'))
+            .order_by('-api_root_length')
+        )
+
+        # select the one matching
+        for candidate in candidates.iterator():
+            if url.startswith(candidate.api_root):
+                credentials = candidate
+                break
+        else:
+            return None
+
+        auth = ClientAuth(
+            client_id=credentials.client_id,
+            secret=credentials.secret,
+            **kwargs
+        )
+        return auth
