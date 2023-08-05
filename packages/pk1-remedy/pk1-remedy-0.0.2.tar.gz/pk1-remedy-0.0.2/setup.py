6199e32574eab48a8320e02dde52b35e0c793db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:30 +0000

    Add package pk1-remedy/0.0.2/pk1-remedy-0.0.2.tar.gz

---
+++ packages/pk1-remedy/pk1-remedy-0.0.2.tar.gz/pk1-remedy-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "pk1-remedy",
+    version = "0.0.2",
+    keywords = ("pip", "packone"),
+    description = "Scripts to remedy vm images (of packone)",
+    long_description = open('README.rst').read(),
+    license = "Apache-2.0 Licence",
+    url = "https://github.com/cas-bigdatalab/packone_remedy",
+    author = "Excel Wang",
+    author_email = "wanghj@cnic.com",
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["requests"],
+    entry_points = {
+        'console_scripts': [
+            'ambari-host-clone = pk1.remedy.ambari.host_clone:main'
+        ]
+    }
+)
