6abb43d2fcec5ddd5ec6fa1ab7a2120b8c299cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:01 +0000

    Add package intelligencer/0.1/intelligencer-0.1.tar.gz

---
+++ packages/intelligencer/intelligencer-0.1.tar.gz/intelligencer-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='intelligencer',
+      version='0.1',
+      description='Web and Data Automation Hub',
+      url='http://github.com/jgissend10/intelligencer',
+      author='James Szalay',
+      author_email='jamestszalay@gmail.com',
+      license='MIT',
+      packages=['intelligencer'],
+      zip_safe=False)
