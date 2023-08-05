5497f37751b828d262ac5f29ae2e8d0ee32d7dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:00 +0000

    Add package cnx-common/0.0.0/cnx_common-0.0.0-py2.py3-none-any.whl

---
+++ packages/cnx-common/cnx_common-0.0.0-py2.py3-none-any.whl/cnxcommon/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright (C) 2019 Rice University.
+
+This software is subject to the provisions of the
+GNU AFFERO GENERAL PUBLIC LICENSE Version 3.0 (AGPL).
+See LICENSE.txt for details.
+"""
