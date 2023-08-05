ea86c5626ffc9bf9c19b0b770fbdeab210fe465e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:45 +0000

    Add package doom/0.2.1/doom-0.2.1.tar.gz

---
+++ packages/doom/doom-0.2.1.tar.gz/doom-0.2.1/README.md
@@ -0,0 +1,8 @@
+doom collects well-tested utility code that I find myself repeating in other
+projects.
+
+doom is *not* well-scoped.
+
+doom is actively used and maintained (as of 2019-04-17).
+
+doom tries to avoid inter-module dependencies.
---
+++ packages/doom/doom-0.2.1.tar.gz/doom-0.2.1/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+from setuptools import setup, find_packages
+
+import versioneer
+cmdclass = versioneer.get_cmdclass()
+
+
+# Note this will fail if an appropriate MANIFEST.in file is not present.
+with open('requirements.txt') as requirements_f:
+    REQUIREMENTS = requirements_f.readlines()
+
+REQUIREMENTS = [x for x in REQUIREMENTS if not x.startswith('#')]
+
+py2only = [x.split(';')[0] for x in REQUIREMENTS
+           if re.search('python_version.*2', x)]
+REQUIREMENTS = [x for x in REQUIREMENTS
+                if not re.search('python_version.*2', x)]
+
+
+with open('README.md', 'rb') as fd:
+    LONG_DESCRIPTION = fd.read().decode('utf-8')
+
+
+setup(name='doom',
+      version=versioneer.get_version(),
+      packages=find_packages(include=['doom', 'doom.*']),
+      description='',
+      long_description=LONG_DESCRIPTION,
+      url='https://github.com/jbrockmendel/doom',
+      license='MIT',
+      cmdclass=cmdclass,
+
+      author='Brock Mendel',
+      author_email='jbrockmendel@gmail.com',
+
+      py_modules=['compat', 'fslib', 'lning', 'utils'],
+      install_requires=REQUIREMENTS,
+      extras_require={':python_version == "2.7"': py2only},
+
+      entry_points={'console_scripts': []},
+
+      test_suite="tests",
+      keywords=[],
+
+      classifiers=[])
