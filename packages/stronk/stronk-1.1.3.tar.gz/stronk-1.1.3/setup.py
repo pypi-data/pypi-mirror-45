c2d929a955909c268774a7ede45042e3fbe6f306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:26 +0000

    Add package stronk/1.1.3/stronk-1.1.3.tar.gz

---
+++ packages/stronk/stronk-1.1.3.tar.gz/stronk-1.1.3/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='stronk',
+    version='1.1.3',
+    description='Strong key generator',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/brianrthompson/stronk',
+    author='Brian Thompson',
+    author_email='brianrt23@gmail.com',
+    license='MIT',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6"
+    ],
+    packages=find_packages(exclude=("test",)),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "stronk=stronk.__main__:main",
+        ]
+    },
+)
