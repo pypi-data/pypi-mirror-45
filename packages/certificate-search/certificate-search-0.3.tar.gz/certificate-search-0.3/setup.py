ee055f9af31fd4f989289b9f023f084b0f8a5c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:39 +0000

    Add package certificate-search/0.3/certificate-search-0.3.tar.gz

---
+++ packages/certificate-search/certificate-search-0.3.tar.gz/certificate-search-0.3/setup.py
@@ -0,0 +1,28 @@
+'''
+Standard Python setup script.
+'''
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='certificate-search',
+    version='0.3',
+    description='Certificate Transparency search',
+    url='https://github.com/huydhn/crt-search',
+    author='Huy Do',
+    author_email='huydhn@gmail.com',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['requests', 'cryptography'],
+    tests_require=['unittest2', 'coverage', 'nose', 'pytest-pep8', 'pytest-cov', 'codecov'],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
