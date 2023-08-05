826a2eb95fa3f9b1597cb40782df58f845921ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:21 +0000

    Add package PySnooper/0.0.19/PySnooper-0.0.19.tar.gz

---
+++ packages/PySnooper/PySnooper-0.0.19.tar.gz/PySnooper-0.0.19/pysnooper/__init__.py
@@ -0,0 +1,13 @@
+# Copyright 2019 Ram Rachum and collaborators.
+# This program is distributed under the MIT license.
+
+from .pysnooper import snoop
+import collections
+
+__VersionInfo = collections.namedtuple('VersionInfo',
+                                       ('major', 'minor', 'micro'))
+
+__version_info__ = __VersionInfo(0, 0, 19)
+__version__ = '.'.join(map(str, __version_info__))
+
+del collections, __VersionInfo # Avoid polluting the namespace
