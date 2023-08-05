4d994681107c8db29399220b8cb48781144b811b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:23 +0000

    Add package imap-tools/0.5.1/imap_tools-0.5.1-py3-none-any.whl

---
+++ packages/imap-tools/imap_tools-0.5.1-py3-none-any.whl/tests/test_2_message.py
@@ -0,0 +1,27 @@
+import unittest
+from tests.utils import MailboxTestCase
+
+
+class MessageTest(MailboxTestCase):
+    def test_attributes(self):
+        for mailbox in self.mailbox_set.values():
+            mailbox.folder.set(mailbox.folder_test_base)
+            for message in mailbox.fetch():
+                self.assertIs(type(message.id), str)
+                self.assertIn(type(message.uid), [str, type(None)])
+                self.assertIs(type(message.subject), str)
+                self.assertIs(type(message.from_), str)
+                self.assertIs(type(message.from_values), dict)
+                self.assertIs(type(message.to), list)
+                self.assertIs(type(message.to_values), list)
+                self.assertIs(type(message.date), str)
+                self.assertIn(type(message.text), [str, type(None)])
+                self.assertIn(type(message.html), [str, type(None)])
+                self.assertIs(type(message.flags), list)
+                for filename, payload in message.attachments:
+                    self.assertIs(type(filename), str)
+                    self.assertIs(type(payload), bytes)
+
+
+if __name__ == "__main__":
+    unittest.main()
