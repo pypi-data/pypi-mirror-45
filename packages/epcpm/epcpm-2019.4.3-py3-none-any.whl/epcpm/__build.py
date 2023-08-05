a516a5a582ef24e076eaa457174e64568ad4470b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:51 +0000

    Add package epcpm/2019.4.3/epcpm-2019.4.3-py3-none-any.whl

---
+++ packages/epcpm/epcpm-2019.4.3-py3-none-any.whl/epcpm/__build.py
@@ -0,0 +1,9 @@
+# This file has been generated
+
+
+build_system = 'AppVeyor'
+build_id = '23894285'
+build_number = '412'
+build_version = '1.0.412'
+job_id = 'hmiqf20omo3ditm5'
+job_url = 'https://ci.appveyor.com/project/KyleAltendorf/pm/build/job/hmiqf20omo3ditm5'
