bf82fd04d9b4c08c6ffaec21ba3487db2298a26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:09 +0000

    Add package yumemi/1.2/yumemi-1.2.tar.gz

---
+++ packages/yumemi/yumemi-1.2.tar.gz/yumemi-1.2/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+from os import path
+import re
+
+
+version = None
+with open(path.join('yumemi', '__init__.py')) as f:
+    version_cre = re.compile(
+        r'^__version__\s+=\s+[\'"](?P<version>\d+\.\d+.*)[\'"]$'
+    )
+    for line in f:
+        match = version_cre.match(line)
+        if match:
+            version = match.group('version')
+            break
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+
+setup(
+    name='yumemi',
+    version=version,
+    description='AniDB library and simple CLI client.',
+    long_description=long_description,
+    author='Filip Pobořil',
+    author_email='tsuki@fpob.cz',
+    url='https://github.com/fpob/yumemi',
+    download_url='https://github.com/fpob/yumemi/archive/v{}.tar.gz'.format(version),
+    license='MIT',
+    keywords=['AniDB'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities',
+    ],
+    packages=['yumemi'],
+    install_requires=['click', 'python-dateutil'],
+    extras_require={
+        'encrypt': ['pycrypto'],
+    },
+    entry_points={
+        'console_scripts': [
+            'yumemi=yumemi.cli:main'
+        ],
+    },
+)
