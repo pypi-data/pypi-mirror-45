19f11c3bca7f496d9644c3022365ffff37dcbdc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:43 +0000

    Add package meltano/0.21.0/meltano-0.21.0.tar.gz

---
+++ packages/meltano/meltano-0.21.0.tar.gz/meltano-0.21.0/src/meltano/api/app.py
@@ -0,0 +1,124 @@
+import datetime
+import logging
+import logging.handlers
+import os
+from flask import Flask, request, render_template
+from flask import jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import login_required
+from flask_login import current_user
+from flask_cors import CORS
+from jinja2.exceptions import TemplateNotFound
+from importlib import reload
+
+from .external_connector import ExternalConnector
+from .workers import MeltanoBackgroundCompiler, UIAvailableWorker
+from . import config as default_config
+
+from meltano.core.project import Project
+from meltano.core.compiler.project_compiler import ProjectCompiler
+
+
+connector = ExternalConnector()
+logger = logging.getLogger(__name__)
+available_worker = UIAvailableWorker("http://localhost:5000")
+
+
+def create_app(config={}):
+    project = Project.find()
+
+    app = Flask(__name__)
+    app.config.from_object(reload(default_config))
+    app.config.update(**config)
+
+    if not app.config["SQLALCHEMY_DATABASE_URI"]:
+        app.config[
+            "SQLALCHEMY_DATABASE_URI"
+        ] = f"sqlite:///{project.root.joinpath('meltano.db')}"
+
+    # Initial compilation
+    compiler = ProjectCompiler(project)
+    try:
+        compiler.compile()
+    except Exception as e:
+        pass
+
+    # Logging
+    file_handler = logging.handlers.RotatingFileHandler(
+        app.config["LOG_PATH"], maxBytes=2000, backupCount=10
+    )
+    stdout_handler = logging.StreamHandler()
+
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
+    logger.addHandler(stdout_handler)
+
+    now = str(datetime.datetime.utcnow().strftime("%b %d %Y %I:%M:%S:%f"))
+    logger.warning(f"Meltano UI started at: {now}")
+
+    # Extensions
+    security_options = {}
+
+    from .models import db
+    from .mail import mail
+    from .security import security, users, setup_security
+    from .security.oauth import setup_oauth
+
+    db.init_app(app)
+    mail.init_app(app)
+    setup_security(app, project)
+    setup_oauth(app)
+    CORS(app, origins="*")
+
+    from .controllers.root import root
+    from .controllers.dashboards import dashboardsBP
+    from .controllers.reports import reportsBP
+    from .controllers.repos import reposBP
+    from .controllers.settings import settingsBP
+    from .controllers.sql import sqlBP
+
+    app.register_blueprint(root)
+    app.register_blueprint(dashboardsBP)
+    app.register_blueprint(reportsBP)
+    app.register_blueprint(reposBP)
+    app.register_blueprint(settingsBP)
+    app.register_blueprint(sqlBP)
+
+    if app.config["PROFILE"]:
+        from .profiler import init
+
+        init(app)
+
+    @app.after_request
+    def after_request(res):
+        request_message = f"[{request.url}]"
+
+        if request.method != "OPTIONS":
+            request_message += f" as {current_user}"
+
+        logger.info(request_message)
+        return res
+
+    return app
+
+
+def start(project, **kwargs):
+    """Start Meltano UI as a single-threaded web server."""
+
+    compiler_worker = MeltanoBackgroundCompiler(project)
+    compiler_worker.start()
+    available_worker.start()
+
+    try:
+        app_config = kwargs.pop("app_config", {})
+        app = create_app(app_config)
+        from .security.identity import create_dev_user
+
+        with app.app_context():
+            # TODO: alembic migration
+            create_dev_user()
+
+        app.run(**kwargs)
+    finally:
+        compiler_worker.stop()
+        available_worker.stop()
---
+++ packages/meltano/meltano-0.21.0.tar.gz/meltano-0.21.0/src/meltano/cli/select.py
@@ -0,0 +1,124 @@
+import os
+import click
+import logging
+import fnmatch
+import json
+
+from . import cli
+from .params import project
+from meltano.core.config_service import ConfigService
+from meltano.core.plugin import Plugin, PluginType
+from meltano.core.plugin.error import PluginExecutionError
+from meltano.core.plugin_invoker import PluginInvoker
+from meltano.core.plugin.singer.catalog import (
+    visit,
+    parse_select_pattern,
+    ListSelectedExecutor,
+)
+from meltano.core.tracking import GoogleAnalyticsTracker
+
+
+@cli.command()
+@project
+@click.argument("extractor")
+@click.argument("entities_filter", default="*")
+@click.argument("attributes_filter", default="*")
+@click.option("--list", is_flag=True)
+@click.option("--all", is_flag=True)
+@click.option("--exclude", is_flag=True)
+def select(project, extractor, entities_filter, attributes_filter, **flags):
+    try:
+        if flags["list"]:
+            show(
+                project,
+                extractor,
+                entities_filter,
+                attributes_filter,
+                show_all=flags["all"],
+            )
+        else:
+            add(
+                project,
+                extractor,
+                entities_filter,
+                attributes_filter,
+                exclude=flags["exclude"],
+            )
+
+        tracker = GoogleAnalyticsTracker(project)
+        tracker.track_meltano_select(
+            extractor=extractor,
+            entities_filter=entities_filter,
+            attributes_filter=attributes_filter,
+            flags=flags,
+        )
+    except PluginExecutionError as e:
+        raise click.ClickException(
+            f"Cannot list the selected properties: "
+            "there was a problem running the tap with `--discover`. "
+            "Make sure the tap supports `--discover` and run "
+            "`meltano invoke {extractor} --discover` to make "
+            "sure it runs correctly."
+        ) from e
+    except Exception as e:
+        raise click.ClickException(str(e)) from e
+
+
+def add(project, extractor, entities_filter, attributes_filter, exclude=False):
+    exclude = "!" if exclude else ""
+    config = ConfigService(project)
+    pattern = f"{exclude}{entities_filter}.{attributes_filter}"
+
+    with project.meltano_update() as meltano:
+        extractor = config.get_plugin(extractor, plugin_type=PluginType.EXTRACTORS)
+        extractor.add_select_filter(pattern)
+
+        idx = next(i for i, it in enumerate(config.get_extractors()) if it == extractor)
+        meltano["plugins"]["extractors"][idx] = extractor.canonical()
+
+
+def show(project, extractor, entities_filter, attributes_filter, show_all=False):
+    config = ConfigService(project)
+    extractor = config.get_plugin(extractor, plugin_type=PluginType.EXTRACTORS)
+    invoker = PluginInvoker(project, extractor)
+    pattern = f"{entities_filter}.{attributes_filter}"
+
+    list_all = ListSelectedExecutor()
+    try:
+        if not invoker.files["catalog"].exists():
+            logging.info("Catalog not found, trying to run the tap with --discover.")
+            extractor.run_discovery(invoker)
+        else:
+            extractor.apply_select(invoker)
+
+        with invoker.files["catalog"].open() as catalog:
+            schema = json.load(catalog)
+            visit(schema, list_all)
+    except FileNotFoundError as e:
+        logging.error(
+            "Cannot find catalog: make sure the tap runs correctly with --discover; `meltano invoke TAP --discover`"
+        )
+        raise e
+
+    # report
+    click.secho("Enabled patterns:")
+    for select in map(parse_select_pattern, extractor.select):
+        click.secho(
+            f"\t{select.property_pattern}", fg="red" if select.negated else "white"
+        )
+    else:
+        click.echo()
+
+    click.secho("Selected properties:")
+    color = lambda selected: "white" if selected else "red"
+    for stream, prop in (
+        (stream, prop)
+        for stream in list_all.streams
+        for prop in list_all.properties[stream.key]
+    ):
+        if show_all:
+            click.secho(f"\t{stream.key}", fg=color(stream.selected), nl=False)
+            click.echo(".", nl=False)
+            click.secho(prop.key, fg=color(stream.selected and prop.selected))
+        elif stream.selected and prop.selected:
+            click.echo(f"\t{stream.key}.{prop.key}")
