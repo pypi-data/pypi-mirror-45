07e6127c5b5c9c8a1a309928584da7bf528abcc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package yyqq188-mydemo/0.0.6/yyqq188_mydemo-0.0.6.tar.gz

---
+++ packages/yyqq188-mydemo/yyqq188_mydemo-0.0.6.tar.gz/yyqq188_mydemo-0.0.6/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='yyqq188_mydemo',
+    version='0.0.6',
+    description='Python pypi demo',
+    author='Tyrone Zhao',
+    author_email='tyrone-zhao@qq.com',
+    packages=['testPyPi','testPyPi.test1','testPyPi.test2']
+)
---
+++ packages/yyqq188-mydemo/yyqq188_mydemo-0.0.6.tar.gz/yyqq188_mydemo-0.0.6/testPyPi/__init__.py
@@ -0,0 +1,2 @@
+from testPyPi.test1 import test1
+from testPyPi.test2 import test2
