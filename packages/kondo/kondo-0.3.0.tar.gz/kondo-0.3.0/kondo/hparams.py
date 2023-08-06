106cbc7bd9d0db40a429ce7c1f198b185fc6660c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:29 +0000

    Add package kondo/0.3.0/kondo-0.3.0.tar.gz

---
+++ packages/kondo/kondo-0.3.0.tar.gz/kondo-0.3.0/kondo/hparams.py
@@ -0,0 +1,59 @@
+import os
+import time
+import inspect
+from ruamel import yaml
+from typing import Generator, Tuple
+
+from .param_types import ParamType
+
+
+class HParams:
+  def __init__(self, exp_class):
+    self.exp_class = exp_class
+    self._hparams = self.prep(exp_class)
+
+  @property
+  def hparams(self) -> dict:
+    return self._hparams
+
+  @staticmethod
+  def prep(cls) -> dict:
+    attribs = {}
+
+    for sup_c in type.mro(cls)[::-1]:
+      argspec = inspect.getfullargspec(getattr(sup_c, '__init__'))
+      argsdict = dict(dict(zip(argspec.args[1:], argspec.defaults or [])))
+      attribs = {**attribs, **argsdict}
+    
+    return attribs
+
+  def sample(self) -> Tuple[dict, str]:
+    for trial in self.trials():
+      return trial
+
+  def trials(self) -> Generator[Tuple[dict, str], None, None]:
+    for group, num, spec in self.exp_class.spec_list():
+      rvs = {
+        k: v.sample(size=num).tolist() if isinstance(v, ParamType) else v
+        for k, v in spec.items()
+      }
+
+      for t in range(num):
+        t_rvs = {k: v[t] if isinstance(v, list) else v
+                for k, v in rvs.items()}
+
+        yield {**self._hparams, **t_rvs}, group
+
+  def save_trials(self, trials_dir: str):
+    trials_dir = os.path.abspath(trials_dir)
+    os.makedirs(trials_dir, exist_ok=True)
+    for trial, group in self.trials():
+      name = '{}-{}-{}'.format(self.exp_class.__name__, group, time.time())
+      t_dir = os.path.join(trials_dir, name)
+      
+      os.makedirs(t_dir, exist_ok=True)
+      with open(os.path.join(t_dir, 'trial.yaml'), 'w') as f:
+        trial['name'] = name
+        trial['log_dir'] = t_dir
+        trial['group'] = group
+        yaml.safe_dump(trial, stream=f, default_flow_style=False)
