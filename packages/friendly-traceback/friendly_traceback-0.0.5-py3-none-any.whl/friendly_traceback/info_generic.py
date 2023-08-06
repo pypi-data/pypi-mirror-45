b22a9319aa425337d1e5a3a309d63ae67998cc46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:05 +0000

    Add package friendly-traceback/0.0.5/friendly_traceback-0.0.5-py3-none-any.whl

---
+++ packages/friendly-traceback/friendly_traceback-0.0.5-py3-none-any.whl/friendly_traceback/info_generic.py
@@ -0,0 +1,162 @@
+"""info_generic.py
+
+Generic information about Python exceptions.
+"""
+import os
+from .my_gettext import current_lang
+from . import utils
+
+
+def arithmetic_error(*args):
+    _ = current_lang.lang
+    return _(
+        "    ArithmeticError is the base class for those built-in exceptions\n"
+        "    that are raised for various arithmetic errors.\n"
+        "    It is unusual that you are seeing this exception;\n"
+        "    normally, a more specific exception should have been raised.\n"
+    )
+
+
+def import_error(*args):
+    _ = current_lang.lang
+    return _(
+        "    This exception indicates that a certain object could not\n"
+        "    be imported from a module or package. Most often, this is\n"
+        "    because the name of the object is not spelled correctly.\n"
+    )
+
+
+def indentation_error(etype, value):
+    _ = current_lang.lang
+    filename = value.filename
+    linenumber = value.lineno
+    offset = value.offset
+    source, _ignore = utils.get_partial_source(filename, linenumber, offset)
+    filename = os.path.basename(filename)
+    info = _(
+        "    Python could not parse the file '{filename}'\n"
+        "    beyond the location indicated below by --> and ^.\n"
+        "\n"
+        "{source}\n"
+    ).format(filename=filename, source=source)
+    return (
+        _(
+            "    An IndentationError occurs when a given line of code is\n"
+            "    not indented (aligned vertically with other lines) as expected.\n"
+        )
+        + info
+    )
+
+
+def index_error(*args):
+    _ = current_lang.lang
+    return _(
+        "    An IndexError occurs when you are try to get an item from a list,\n"
+        "    a tuple, or a similar object (sequence), by using an index which\n"
+        "    does not exists; typically, this is because the index you give\n"
+        "    is greater than the length of the sequence.\n"
+        "    Reminder: the first item of a sequence is at index 0.\n"
+    )
+
+
+def key_error(*args):
+    _ = current_lang.lang
+    return _(
+        "    A KeyError is raised when a value is not found as a\n"
+        "    key in a Python dict.\n"
+    )
+
+
+def lookup_error(*args):
+    _ = current_lang.lang
+    return _(
+        "    LookupError is the base class for the exceptions that are raised\n"
+        "    when a key or index used on a mapping or sequence is invalid.\n"
+        "    It can also be raised directly by codecs.lookup().\n"
+    )
+
+
+def module_not_found_error(*args):
+    _ = current_lang.lang
+    return _(
+        "    A ModuleNotFoundError exception indicates that you\n"
+        "    are trying to import a module that cannot be found by Python.\n"
+        "    This could be because you misspelled the name of the module\n"
+        "    or because it is not installed on your computer.\n"
+    )
+
+
+def name_error(*args):
+    _ = current_lang.lang
+    return _(
+        "    A NameError exception indicates that a variable or\n"
+        "    function name is not known to Python.\n"
+        "    Most often, this is because there is a spelling mistake.\n"
+        "    However, sometimes it is because the name is used\n"
+        "    before being defined or given a value.\n"
+    )
+
+
+def syntax_error(*args):
+    _ = current_lang.lang
+    return _("    A SyntaxError occurs when Python cannot understand your code.\n")
+
+
+def tab_error(*args):
+    _ = current_lang.lang
+    return _(
+        "    A TabError indicates that you have used both spaces\n"
+        "    and tab characters to indent your code.\n"
+        "    This is not allowed in Python.\n"
+        "    Indenting your code means to have block of codes aligned vertically\n"
+        "    by inserting either spaces or tab characters at the beginning of lines.\n"
+        "    Python's recommendation is to always use spaces to indent your code.\n"
+    )
+
+
+def unbound_local_error(*args):
+    _ = current_lang.lang
+    return _(
+        "    In Python, variables that are used inside a function are known as \n"
+        "    local variables. Before they are used, they must be assigned a value.\n"
+        "    A variable that is used before it is assigned a value is assumed to\n"
+        "    be defined outside that function; it is known as a 'global'\n"
+        "    (or sometimes 'nonlocal') variable. You cannot assign a value to such\n"
+        "    a global variable inside a function without first indicating to\n"
+        "    Python that this is a global variable, otherwise you will see\n"
+        "    an UnboundLocalError.\n"
+    )
+
+
+def unknown(*args):
+    _ = current_lang.lang
+    return _("    No information is known about this exception.\n")
+
+
+def zero_division_error(*args):
+    _ = current_lang.lang
+    return _(
+        "    A ZeroDivisionError occurs when you are attempting to divide\n"
+        "    a value by zero:\n"
+        "        result = my_variable / 0\n"
+        "    It can also happen if you calculate the remainder of a division\n"
+        "    using the modulo operator '%'\n"
+        "        result = my_variable % 0\n"
+    )
+
+
+generic = {
+    "ArithmeticError": arithmetic_error,
+    "ImportError": import_error,
+    "IndentationError": indentation_error,
+    "IndexError": index_error,
+    "KeyError": key_error,
+    "LookupError": lookup_error,
+    "ModuleNotFoundError": module_not_found_error,
+    "NameError": name_error,
+    "SyntaxError": syntax_error,
+    "TabError": tab_error,
+    "UnboundLocalError": unbound_local_error,
+    "Unknown": unknown,
+    "ZeroDivisionError": zero_division_error,
+}
---
+++ packages/friendly-traceback/friendly_traceback-0.0.5-py3-none-any.whl/friendly_traceback/info_traceback.py
@@ -0,0 +1,216 @@
+"""info_traceback.py
+
+First version - needs to be documented.
+"""
+import inspect
+import os
+import runpy
+
+from . import info_generic
+from . import info_specific
+from . import utils
+from .my_gettext import current_lang
+from .info_variables import get_var_info
+
+CONTEXT = utils.CONTEXT
+CONSOLE_SOURCE = utils.CONSOLE_SOURCE
+
+# ====================
+# The following is an example of a formatted traceback, with each
+# part identified by a number enclosed by brackets
+# corresponding to a comment in the function get_traceback_info
+
+# [1]
+# Python exception:
+#     UnboundLocalError: local variable 'a' referenced before assignment
+
+# [2]
+# In Python, variables that are used inside a function are known as
+# local variables. Before they are used, they must be assigned a value.
+# A variable that is used before it is assigned a value is assumed to
+# be defined outside that function; it is known as a 'global'
+# (or sometimes 'nonlocal') variable. You cannot assign a value to such
+# a global variable inside a function without first indicating to
+# Python that this is a global variable, otherwise you will see
+# an UnboundLocalError.
+
+# [3]
+# Likely cause:
+#     The variable that appears to cause the problem is 'a'.
+#     Try inserting the statement
+#         global a
+#     as the first line inside your function.
+#
+# [4]
+# Execution stopped on line 14 of file 'C:\Users...\test_unbound_local_error.py'.
+
+# [5]
+#    12:
+#    13:     try:
+# -->14:         inner()
+#    15:     except Exception:
+
+# [6]
+# inner: <function test_unbound_local_error.<loca... >
+
+# [7]
+# Exception raised on line 11 of file 'C:\Users\...\test_unbound_local_error.py'.
+
+# [8]
+#     9:     def inner():
+#    10:         b = 2
+# -->11:         a = a + b
+#    12:
+
+# [9]
+# b: 2
+
+
+def get_traceback_info(etype, value, tb, running_script=False):
+    """ Gathers the basic information related to a traceback and
+    returns the result in a dict.
+    """
+    info = {}
+    # Note: the numbered comments refer to the example above
+    info["header"] = get_header(etype.__name__, value)  # [1]
+    info["generic"] = get_generic_explanation(etype.__name__, etype, value)  # [2]
+
+    if issubclass(etype, SyntaxError) and value.filename == "<string>":
+        cause = cannot_analyze_string()
+    else:
+        cause = get_likely_cause(etype, value)
+
+    if cause is not None:
+        info["cause"] = cause  # [3]
+
+    if issubclass(etype, SyntaxError):
+        return info
+
+    # Get all calls made
+    records = inspect.getinnerframes(tb, CONTEXT)
+
+    # Last call made
+    if running_script:
+        # Do not show traceback from our own code
+        excluded_files = excluded_file_names()
+        for record in records[:-1]:
+            _frame, filename, linenumber, _func, lines, index = record
+            if filename in excluded_files:
+                continue
+            break
+    else:
+        _frame, filename, linenumber, _func, lines, index = records[0]
+
+    info["last_call header"] = last_call_header(linenumber, filename)  # [4]
+    source_info = get_partial_source(filename, linenumber, lines, index)
+    info["last_call source"] = source_info["source"]  # [5]
+
+    if "line" in source_info and source_info["line"] is not None:
+        result = get_var_info(source_info["line"], _frame)
+        if result:
+            info["last_call variables"] = result  # [6]
+
+    # Origin of the exception
+    if len(records) > 1:
+        _frame, filename, linenumber, _func, lines, index = records[-1]
+        # [7] below
+        info["exception_raised header"] = exception_raised_header(linenumber, filename)
+        source_info = get_partial_source(filename, linenumber, lines, index)
+        info["exception_raised source"] = source_info["source"]  # [8]
+
+        if "line" in source_info and source_info["line"] is not None:
+            result = get_var_info(source_info["line"], _frame)
+            if result:
+                info["exception_raised variables"] = result  # [9]
+
+    return info
+
+
+def cannot_analyze_string():
+    _ = current_lang.lang
+    return _(
+        "        Unfortunately, no additional information is available:\n"
+        "        the content of file '<string>' is not accessible.\n"
+    )
+
+
+def excluded_file_names():
+    """In many places, by default we exclude the files from this project,
+       as well as runpy from the standard Python library, in order to
+       restrict tracebacks to code written by the users.
+    """
+    excluded = [runpy.__file__]
+    dirname = os.path.dirname(__file__)
+    for file in os.listdir(os.path.dirname(__file__)):
+        excluded.append(os.path.join(dirname, file))
+    return excluded
+
+
+def get_header(name, value):
+    """Provides the header for a standard Python exception"""
+    _ = current_lang.lang
+    # fmt: off
+    return _(
+        "    Python exception: \n"
+        "        {name}: {value}\n"
+    ).format(name=name, value=value)
+    # fmt: on
+
+
+def get_generic_explanation(name, etype, value):
+    """Provides a generic explanation about a particular exception.
+    """
+    if name in info_generic.generic:
+        explanation = info_generic.generic[name](etype, value)
+    else:
+        explanation = info_generic.generic["Unknown"]()
+    return explanation
+
+
+def get_likely_cause(etype, value):
+    """Gets the likely cause of a given exception based on some information
+       specific to a given exception.
+    """
+    _ = current_lang.lang
+    if etype.__name__ in info_specific.get_cause:
+        cause = info_specific.get_cause[etype.__name__](etype, value)
+        if cause is not None:
+            if issubclass(etype, SyntaxError):
+                return cause
+            else:
+                return _("    Likely cause:\n{cause}").format(cause=cause)
+    return None
+
+
+def get_partial_source(filename, linenumber, lines, index):
+    """Gets the part of the source where an exception occurred,
+       formatted in a pre-determined way, as well as the content
+       of the specific line where the exception occurred.
+    """
+    if filename == "<string>":
+        source = cannot_analyze_string()
+        line = None
+    elif filename in CONSOLE_SOURCE:
+        _filename, source = CONSOLE_SOURCE[filename]
+        source, line = utils.get_partial_source(filename, linenumber, None)
+    elif filename and os.path.abspath(filename):
+        filename = os.path.basename(filename)
+        source, line = utils.highlight_source(linenumber, index, lines)
+    elif not filename:
+        raise FileNotFoundError("Cannot find %s" % filename)
+
+    return {"source": source, "line": line}
+
+
+def last_call_header(linenumber, filename):
+    _ = current_lang.lang
+    return _(
+        "    Execution stopped on line {linenumber} of file '{filename}'.\n"
+    ).format(linenumber=linenumber, filename=filename)
+
+
+def exception_raised_header(linenumber, filename):
+    _ = current_lang.lang
+    return _(
+        "    Exception raised on line {linenumber} of file '{filename}'.\n"
+    ).format(linenumber=linenumber, filename=filename)
