5cde9d559d90e993fa1556e30f195198f4f0fb1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:41 +0000

    Add package djangohttpbench/0.0.2/djangohttpbench-0.0.2.tar.gz

---
+++ packages/djangohttpbench/djangohttpbench-0.0.2.tar.gz/djangohttpbench-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name='djangohttpbench',
+    version='0.0.2',
+    packages=find_packages(),
+    description='HTTP load testing tools for Django. Like an apache bench '
+                'but requests are sent from authenticated users.',
+    long_description=README,
+    author='Masashi Shibata',
+    author_email='contact@c-bata.link',
+    url='https://github.com/c-bata/django-httpbench',
+    license='MIT',
+    install_requires=[
+        'Django',
+        'requests',
+    ]
+)
