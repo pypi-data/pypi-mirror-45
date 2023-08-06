f61b2686e048a95b5126ee5267e3257be84f99a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:16 +0000

    Add package dataset-manager/0.0.11/dataset_manager-0.0.11.tar.gz

---
+++ packages/dataset-manager/dataset_manager-0.0.11.tar.gz/dataset_manager-0.0.11/setup.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name="dataset_manager",
+    version="0.0.11",
+    description="Manage and automatize datasets for data science projects.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Diogo Munaro Vieira",
+    author_email="diogo.mvieira@gmail.com",
+    license='Apache 2',
+    install_requires=[
+        "PyYAML>=3.13",
+        "pandas>=0.19.2",
+        "requests>=2.21.0"
+    ],
+    packages=find_packages(),
+)
