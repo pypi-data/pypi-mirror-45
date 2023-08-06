f8fa189a631a509692a1dda5fd9a1f8e8d4bbcdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:40 +0000

    Add package kitsune/1.1.2/kitsune-1.1.2.tar.gz

---
+++ packages/kitsune/kitsune-1.1.2.tar.gz/kitsune-1.1.2/kitsune/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Kitsune - K-mer analysis toolkit.
+
+.. moduleauthor:: Natapol Pornputtapong <natapol.p@chula.ac.th>
+
+"""
+
+__author__ = 'Natapol Pornputtapong'
+
+from .version import version as __version__
+
+name = "kitsune"
