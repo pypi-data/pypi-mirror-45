89ca55c2806e96558e7f09f8489ba7078fd6dfd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:51 +0000

    Add package ipython-cosmos/0.1.3/ipython-cosmos-0.1.3.tar.gz

---
+++ packages/ipython-cosmos/ipython-cosmos-0.1.3.tar.gz/ipython-cosmos-0.1.3/cosmos_sql/__init__.py
@@ -0,0 +1,36 @@
+from IPython.core.magic import Magics, magics_class, cell_magic, line_magic, needs_local_scope
+import azure.cosmos.cosmos_client as cosmos_client
+
+@magics_class
+class CosmosMagics(Magics):
+    @cell_magic
+    @magic_arguments.magic_arguments()
+    @magic_arguments.argument('--database', '-d',
+      help='specifies database name'
+    )
+    @magic_arguments.argument('--container', '-c',
+      help='specifies container name'
+    )
+    @magic_arguments.argument('--host',
+      help='specifies host endpoint'
+    )
+    @magic_arguments.argument('--masterKey',
+      help='specifies masterKey'
+    )
+    def sql(self, line='', cell=None):
+        args = magic_arguments.parse_argstring(self.sql, line)
+        DATABASE_ID = args.database
+        COLLECTION_ID = args.container
+
+        host = args.host
+        key = args.masterKey
+
+        print("host is " + host)
+        CosmosClient = cosmos_client.CosmosClient(host, {'masterKey': key})
+
+        database_link = 'dbs/' + DATABASE_ID
+        earthquakes = database_link + '/colls/' + COLLECTION_ID
+        query = {"query": cell}
+        items = list(CosmosClient.QueryItems(earthquakes, query, {'enableCrossPartitionQuery': True}))
+        df = pd.DataFrame.from_records(items)
+        return df
