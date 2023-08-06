cac00bbb7784f99af202d3249d99dff7e57d0a17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:33 +0000

    Add package nester-slupiani/1.1.0/nester-slupiani-1.1.0.tar.gz

---
+++ packages/nester-slupiani/nester-slupiani-1.1.0.tar.gz/nester-slupiani-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	= 'nester-slupiani',
+	version	= '1.1.0', 
+	py_modules = ['nester-lupiani'],
+	author	= 'hfpython', 
+	author_email = 'hypython@headfirstlabs.com',
+	url	= 'http://www.headfirstlabs.com', 
+	description = 'A simple printer of nested lists',
+)
