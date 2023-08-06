3a73b2ede2695903646c83b3615c78af852ad2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:16 +0000

    Add package ocspy/0.0.2/ocspy-0.0.2.tar.gz

---
+++ packages/ocspy/ocspy-0.0.2.tar.gz/ocspy-0.0.2/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "ocspy",
+    version = "0.0.2",
+    keywords = ("pip", "optical communication","coherent optical", "ocspy", "ocspy"),
+    description = "simulate optical communication",
+    long_description = "time and path tool",
+    license = "MIT Licence",
+
+    url = "",
+    author = "nigulasikaochuan",
+    author_email = "nigulasikaochuan@sjtu.edu.cn",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
