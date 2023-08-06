09331e83f1dda448ac664f2c44035db18274803d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:45 +0000

    Add package meshoid/0.12.8/meshoid-0.12.8.tar.gz

---
+++ packages/meshoid/meshoid-0.12.8.tar.gz/meshoid-0.12.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='meshoid',
+      version='0.12.8',
+      description='Package for analysis of meshless simulation data',
+#      url='http://github.com/omgspace/Meshoid',
+      author='Mike Grudić',
+      author_email='mgrudich@caltech.edu',
+      license='MIT',
+      packages=['Meshoid'],
+      zip_safe=False)
