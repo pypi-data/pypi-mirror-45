110e898e8769174da3a6628c284ae3576fb67a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:22 +0000

    Add package qiskit-jku-provider/1.0.1/qiskit_jku_provider-1.0.1-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/qiskit-jku-provider/qiskit_jku_provider-1.0.1-cp35-cp35m-manylinux1_x86_64.whl/qiskit_jku_provider-1.0.1.data/purelib/qiskit_jku_provider/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+"""Local JKU Backend."""
+
+from .qasm_simulator_jku import QasmSimulator
+from .jkuprovider import JKUProvider
