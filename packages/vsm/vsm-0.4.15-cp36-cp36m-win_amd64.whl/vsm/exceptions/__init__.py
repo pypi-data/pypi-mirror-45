7fcdab7d6368c520f67b56da8b31389904b246b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:56 +0000

    Add package vsm/0.4.15/vsm-0.4.15-cp36-cp36m-win_amd64.whl

---
+++ packages/vsm/vsm-0.4.15-cp36-cp36m-win_amd64.whl/vsm/exceptions/__init__.py
@@ -0,0 +1,9 @@
+"""
+[General documentation about the :mod:`exceptions` submodule]
+"""
+from __future__ import absolute_import
+
+from . import compatibility
+from .compatibility import *
+
+__all__ = compatibility.__all__
