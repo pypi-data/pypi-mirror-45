9658a2176baa4910c47fce4c26f0071e3e9a85e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:23 +0000

    Add package eip712-structs/0.1.6/eip712_structs-0.1.6-py3-none-any.whl

---
+++ packages/eip712-structs/eip712_structs-0.1.6-py3-none-any.whl/eip712_structs/__init__.py
@@ -0,0 +1,6 @@
+from eip712_structs.domain_separator import make_domain
+from eip712_structs.struct import EIP712Struct, struct_from_message, struct_to_message
+from eip712_structs.types import Address, Array, Boolean, Bytes, Int, String, Uint
+
+name = 'eip712-structs'
+version = '0.1.6'
