26c20f03b4745cc2c756593c032d2c37f193af07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:04 +0000

    Add package atomicdataMB/1.0.0/atomicdataMB-1.0.0-py3-none-any.whl

---
+++ packages/atomicdataMB/atomicdataMB-1.0.0-py3-none-any.whl/atomicdataMB/configure_database.py
@@ -0,0 +1,17 @@
+import os
+import psycopg2
+from atomicdataMB import make_gvalue_table, make_photo_table
+
+configfile = os.path.join(os.environ['HOME'], '.nexoclom')
+config = {}
+if os.path.isfile(configfile):
+    for line in open(configfile, 'r').readlines():
+        key, value = line.split('=')
+        config[key.strip()] = value.strip()
+
+database = config['database']
+
+con = psycopg2.connect(database=database)
+con.autocommit = True
+make_gvalue_table(con)
+make_photo_table(con)
