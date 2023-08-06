226b51a713a3b3967f55d1db07a5c09a142f9455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:21 +0000

    Add package easytello/0.0.4/easytello-0.0.4-py3-none-any.whl

---
+++ packages/easytello/easytello-0.0.4-py3-none-any.whl/easytello/stats.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+class Stats:
+    def __init__(self, command: str, id: int):
+        self.command = command
+        self.response = None
+        self.id = id
+
+        self.start_time = datetime.now()
+        self.end_time = None
+        self.duration = None
+
+    def add_response(self, response: str):
+        self.response = response
+        # Calculating total time taken to execute command
+        self.end_time = datetime.now()
+        self.duration = (self.end_time-self.start_time).total_seconds()
+
+    def got_response(self):
+        if self.response is None:
+            return False
+        else:
+            return True
+
+    def get_response(self):
+        return self.response
+
