01afc37aaf0345bc7038d739da4e12e2a2715b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:55 +0000

    Add package skiply/0.1.2/skiply-0.1.2.tar.gz

---
+++ packages/skiply/skiply-0.1.2.tar.gz/skiply-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setup(
+	name='skiply',
+	version='0.1.2',
+	description='Skiply Library',
+	url='https://github.com/skiplyfrance/skiply.git',
+	author='Caroline Bouchat',
+	author_email='caroline@skiply.fr',
+	install_requires=['flask-sqlalchemy'],
+	zip_safe=False)
