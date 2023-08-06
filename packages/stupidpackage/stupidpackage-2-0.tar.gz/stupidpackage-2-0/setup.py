7c82e960e983e8807ce4ba5d0cb8c28c1ded8972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:31 +0000

    Add package stupidpackage/2-0/stupidpackage-2-0.tar.gz

---
+++ packages/stupidpackage/stupidpackage-2-0.tar.gz/stupidpackage-2-0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+# Try to convert markdown README to rst format for PyPI.
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='stupidpackage',
+    version='2,0',
+    description='This is a sample server Petstore server.  You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, you can use the api key `special-key` to test the authorization filters.',
+    long_description=long_description,
+    author='APIMatic SDK Generator',
+    author_email='support@apimatic.io',
+    url='https://apimatic.io',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.9.1, <3.0',
+        'jsonpickle>=0.7.1, <1.0',
+        'cachecontrol>=0.11.7, <1.0',
+        'python-dateutil>=2.5.3, <3.0'
+    ]
+)
---
+++ packages/stupidpackage/stupidpackage-2-0.tar.gz/stupidpackage-2-0/stupidpackage/models/__init__.py
@@ -0,0 +1,11 @@
+__all__ = [
+    'order',
+    'user',
+    'category',
+    'tag',
+    'pet',
+    'api_response',
+    'status_enum',
+    'status_6_enum',
+    'o_auth_scope_enum',
+]
