0cfb13bb54f61a46c0b27ef0aaa074a30e9c6135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:59 +0000

    Add package simulating-pkg-taking-10mins-to-install/0.0.1/simulating-pkg-taking-10mins-to-install-0.0.1.tar.gz

---
+++ packages/simulating-pkg-taking-10mins-to-install/simulating-pkg-taking-10mins-to-install-0.0.1.tar.gz/simulating-pkg-taking-10mins-to-install-0.0.1/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+from setuptools import setup
+import time
+
+
+class CustomInstallCommand(install):
+    def run(self):
+        print("Installing pkg... Will take about 600 seconds")
+        time.sleep(600)
+        install.run(self)
+
+
+class CustomDevelopCommand(develop):
+    def run(self):
+        develop.run(self)
+
+
+class CustomEggInfoCommand(egg_info):
+    def run(self):
+        egg_info.run(self)
+
+setup(
+    name="simulating-pkg-taking-10mins-to-install",
+    version="0.0.1",
+    author="yb",
+    author_email="ykim828@hotmail.com",
+    description="A small example package",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    cmdclass={
+        'install': CustomInstallCommand,
+        'develop': CustomDevelopCommand,
+        'egg_info': CustomEggInfoCommand,
+    },
+)
