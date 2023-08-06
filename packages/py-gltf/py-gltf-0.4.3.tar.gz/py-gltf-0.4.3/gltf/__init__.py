64b27b1a1f33522bf300e5031d8833897dad9405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:24 +0000

    Add package py-gltf/0.4.3/py-gltf-0.4.3.tar.gz

---
+++ packages/py-gltf/py-gltf-0.4.3.tar.gz/py-gltf-0.4.3/gltf/__init__.py
@@ -0,0 +1,7 @@
+from .gltf import GLTF, Scene
+from .animations import Animation, Skin
+from .buffers import Buffer, BufferView, Vec2Array, Vec3Array, Vec4Array, ScalarArray, Mat4Array
+from .materials import Material, Texture, Sampler, Image
+from .nodes import Node, Mesh, Primitive
+
+VERSION = '0.4.3'
