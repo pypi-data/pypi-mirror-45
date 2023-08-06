c9cbe6cf4bbfbec2021c957d32b79c5ceecf759d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:10 +0000

    Add package lch-etl/1.0.1/lch_etl-1.0.1.zip

---
+++ packages/lch-etl/lch_etl-1.0.1.zip/lch_etl-1.0.1/setup.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="lch_etl",
+
+    version="1.0.1",
+
+    author="lichanghua",
+    author_email="1371214116@qq.com",
+    description="数据库etl包",
+    long_description=open("README.txt", encoding="utf8").read(),
+
+    url="https://github.com/",
+
+    packages=find_packages(),
+
+    # package_data={
+    #     "lch_etl": [""]
+    #
+    # },
+
+    install_requires=[
+        "mysqlclient","sqlalchemy","sqlite3",'pandas','psycopg2','pymssql','cx_Oracle',''
+    ],
+
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5"
+    ],
+)
