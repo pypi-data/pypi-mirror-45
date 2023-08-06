e3b86919188d95ce443ba7b41e104a2fafa98c8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:23 +0000

    Add package rephunt/0.1.2/rephunt-0.1.2.tar.gz

---
+++ packages/rephunt/rephunt-0.1.2.tar.gz/rephunt-0.1.2/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+version = '0.1.2'
+
+with open('README.md') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='rephunt',
+    version=version,
+    author='stefanitsky',
+    author_email='stefanitsky.mozdor@google.com',
+    description='Reputation enhancer for Stack Exchange',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Stefanitsky/rephunt',
+    packages=['rephunt'],
+    keywords='stackexchange stackoverflow reputation enhance raise up',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Information Technology',
+        'Topic :: Communications',
+        'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+        'Topic :: Utilities'
+    ],
+    project_urls={
+        'Documentation': 'https://github.com/Stefanitsky/rephunt/wiki',
+        'Say Thanks!': 'https://saythanks.io/to/Stefanitsky',
+        'Source': 'https://github.com/Stefanitsky/rephunt',
+        'Tracker': 'https://github.com/Stefanitsky/rephunt/issues',
+    },
+)
