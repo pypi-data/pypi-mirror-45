4b165dcb8baae85c59dd3a3d8937858cbc7cc4ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:03 +0000

    Add package factorfunc/0.3.0/factorfunc-0.3.0.tar.gz

---
+++ packages/factorfunc/factorfunc-0.3.0.tar.gz/factorfunc-0.3.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = "factorfunc",
+    version = "0.3.0",
+    license = "MIT Licence",
+    py_modules=['factorfunc'],
+    author = "Leon Tian",
+    author_email = "iamleon@yeah.net",
+    decription = 'factorfunc',
+    
+)
