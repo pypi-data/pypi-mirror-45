394b7eb4a7763e94ae80a1bf50a604cf43b413cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:43 +0000

    Add package cglacet-smartquadtree/1.1.2/cglacet-smartquadtree-1.1.2.tar.gz

---
+++ packages/cglacet-smartquadtree/cglacet-smartquadtree-1.1.2.tar.gz/cglacet-smartquadtree-1.1.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+
+extensions = [
+    Extension("smartquadtree",
+              ["smartquadtree.pyx", "quadtree.cpp", "neighbour.cpp"],
+              extra_compile_args=["-std=c++11"],
+              language="c++")
+]
+
+def get_long_description():
+    import codecs
+    with codecs.open('tutorial.rst', encoding='utf-8') as f:
+        readme = f.read()
+    return readme
+
+setup(
+    name="cglacet-smartquadtree",
+    version="1.1.2",
+    author="Xavier Olive",
+    author_email="xavier@xoolive.org",
+    description="Implementation of quadtrees for moving objects",
+    long_description=get_long_description(),
+    license="MIT",
+    url="https://github.com/cglacet/quadtree",
+    ext_modules=cythonize(extensions),
+    package_data={'': ['tutorial.rst']},
+    include_package_data=True,
+)
+
+# Producing long description
+# ipython nbconvert tutorial.ipynb --to rst
+# Then manually edit paths to images to point to github
+
+# Windows compilation (mingw)
+# edit smartquadtree.cpp
+# add #include <cmath> at the top of the file before #include "pyconfig.h"
