3b2d1afa889ba05e340c7b90acf33e0e06e5ce8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:17 +0000

    Add package jasonpi/0.13.0/jasonpi-0.13.0.tar.gz

---
+++ packages/jasonpi/jasonpi-0.13.0.tar.gz/jasonpi-0.13.0/jasonpi/normalizers.py
@@ -0,0 +1,31 @@
+import datetime
+
+
+def google_profile(data):
+    print(data)
+    profile = {
+        'email': data['emailAddresses'][0]['value'],
+        'first_name': data['names'][0]['givenName'],
+        'last_name': data['names'][0]['familyName'],
+        'gender': data['genders'][0]['value'] if 'genders' in data else '',
+    }
+    if 'birthdays' in data and len(data['birthdays']) > 0:
+        bd = data['birthdays'][0]['date']
+        profile['birthday'] = datetime.date(bd.get('year', 1900), bd.get('month', 1), bd.get('day', 1))
+    return profile
+
+
+def facebook_profile(data):
+    profile = data.copy()
+    profile['uid'] = profile['id']
+    del profile['id']
+    print(profile)
+    try:
+        profile['birthday'] = \
+            datetime.datetime.strptime(data['birthday'], "%m/%d/%Y").date()
+    except (ValueError, KeyError):
+        pass
+    if 'picture' in data:
+        if type(data['picture']) == dict:
+            profile['picture'] = data['picture']['data']['url']
+    return profile
---
+++ packages/jasonpi/jasonpi-0.13.0.tar.gz/jasonpi-0.13.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+setup(
+    name='jasonpi',
+    version='0.13.0',
+    description='Authentication package to use with '
+                'django-rest-framework & json-api',
+    url='http://github.com/gobadiah/jasonpi',
+    author='Michaël Journo',
+    author_email='gobadiah@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'djangorestframework',
+        'django',
+        'boto3',
+        'pyjwt',
+        'httplib2',
+        'oauth2client',
+        'google-api-python-client',
+        'facebook-sdk',
+        'inflect',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    zip_safe=True,
+)
