61682d052486ec70c21cdf690981de6abb2aa35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:39 +0000

    Add package nazobase/0.1.17/nazobase-0.1.17.tar.gz

---
+++ packages/nazobase/nazobase-0.1.17.tar.gz/nazobase-0.1.17/setup.py
@@ -0,0 +1,28 @@
+#############################################
+# File Name: setup.py
+# Author: WEN
+# Created Time:  2019-05-04 03:54:00
+#############################################
+
+from setuptools import setup
+
+setup(
+    name            ='nazobase',
+    version         ='0.1.17',
+    py_modules      =['nazobase'],
+    author          = 'WEN',
+    license         = "LGPLv3",
+    description     = "NAZOrip's basement",
+    # install_requires=[
+    #     '',
+    # ],
+    entry_points='''
+        [console_scripts]
+        nazobase=nazobase:nazobase
+    ''',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
