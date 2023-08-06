02c73294ff6492fda907d0208fdb36d833b64f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:29:24 +0000

    Add package akashIsAwesome/0.0.1/akashIsAwesome-0.0.1.tar.gz

---
+++ packages/akashIsAwesome/akashIsAwesome-0.0.1.tar.gz/akashIsAwesome-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='akashIsAwesome',
+    packages=['akashIsAwesome'],
+    description='This is a test library',
+    version='0.0.1',
+    url='',
+    author='Akash',
+    author_email='akash5gupta5@gmail.com',
+    keywords=['pip','akash','awesome']
+    )
