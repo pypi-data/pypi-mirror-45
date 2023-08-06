2e96a4fc4606386556d1bce479007bbdb2d40a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:30 +0000

    Add package matyimodule/0.1/matyimodule-0.1-py3-none-any.whl

---
+++ packages/matyimodule/matyimodule-0.1-py3-none-any.whl/matyimodule/matyimodule.py
@@ -0,0 +1,8 @@
+# from InstagramAPI import InstagramAPI
+from markdown import markdown
+def sb(value):
+    #a = InstagramAPI('matyi','oner')
+    '''
+    Kiírja a kijelződre hogy seggbefasz.
+    '''
+    return markdown(u'Matyi vagyok *cső*')
