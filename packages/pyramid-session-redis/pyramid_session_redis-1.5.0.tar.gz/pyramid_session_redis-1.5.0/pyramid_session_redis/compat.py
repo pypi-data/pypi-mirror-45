5960ed5818933f0d519fa4ef96e0bf3a6046ed03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package pyramid-session-redis/1.5.0/pyramid_session_redis-1.5.0.tar.gz

---
+++ packages/pyramid-session-redis/pyramid_session_redis-1.5.0.tar.gz/pyramid_session_redis-1.5.0/pyramid_session_redis/compat.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+Compatability module for various pythons and environments.
+"""
+import six
+from six.moves import cPickle as pickle
+
+try:
+    # python3.6 secrets module
+    from secrets import token_urlsafe, token_hex
+except ImportError:  # pragma: no cover
+    import os
+    import base64
+    import binascii
+
+    def token_bytes(nbytes=32):
+        return os.urandom(nbytes)
+
+    def token_urlsafe(nbytes=32):
+        token = base64.urlsafe_b64encode(token_bytes(nbytes)).rstrip(b'=')
+        return token.decode('ascii') if six.PY3 else token
+
+    def token_hex(nbytes=32):
+        token = binascii.hexlify(token_bytes(nbytes))
+        return token.decode('ascii') if six.PY3 else token
