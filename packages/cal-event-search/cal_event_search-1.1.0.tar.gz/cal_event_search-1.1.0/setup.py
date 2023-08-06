3da0cf1c3fd30d4ab3a4cf88b764a6a6144f69e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:02 +0000

    Add package cal-event-search/1.1.0/cal_event_search-1.1.0.tar.gz

---
+++ packages/cal-event-search/cal_event_search-1.1.0.tar.gz/cal_event_search-1.1.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cal_event_search",
+    version="1.1.0",
+    author="Pol Monroig",
+    author_email="polmonroig@gmail.com",
+    description="Calendar Event Search",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/polmonroig/calendar_api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)
