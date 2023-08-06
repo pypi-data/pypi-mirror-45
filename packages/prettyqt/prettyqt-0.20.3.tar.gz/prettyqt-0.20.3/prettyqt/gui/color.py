cb6f5fdc27cbfca1cf813aff1cb704c81900a84d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:42 +0000

    Add package prettyqt/0.20.3/prettyqt-0.20.3.tar.gz

---
+++ packages/prettyqt/prettyqt-0.20.3.tar.gz/prettyqt-0.20.3/prettyqt/gui/color.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Philipp Temminghoff
+"""
+
+from qtpy import QtGui
+
+
+class Color(QtGui.QColor):
+
+    def set_color(self, color):
+        if isinstance(color, str):
+            self.setNamedColor(color)
+        else:
+            self.setRgb(*color)
+
+    @classmethod
+    def from_text(cls, text):
+        """Create a QColor from specified string"""
+        color = cls()
+        text = str(text)
+        if text.startswith("#") and len(text) == 7:
+            correct = "#0123456789abcdef"
+            for char in text:
+                if char.lower() not in correct:
+                    return color
+        elif text not in list(cls.colorNames()):
+            return color
+        color.setNamedColor(text)
+        return color
---
+++ packages/prettyqt/prettyqt-0.20.3.tar.gz/prettyqt-0.20.3/tests/test_gui.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `prettyqt` package."""
+
+from prettyqt import gui, core
+from qtpy import QtCore
+
+
+def test_brush():
+    gui.Brush()
+
+
+def test_color():
+    color = gui.Color()
+    color.set_color("gray")
+
+
+def test_icon():
+    icon = gui.Icon()
+    icon.for_color("black")
+
+
+def test_keysequence():
+    assert(gui.KeySequence.to_shortcut_str(0x41, QtCore.Qt.ShiftModifier) == "Shift+A")
+
+
+def test_standarditemmodel():
+    model = gui.StandardItemModel()
+    model.add_item("test")
+
+
+def test_painter():
+    painter = gui.Painter()
+    painter.use_antialiasing()
+    painter.set_pen("none")
+    painter.fill_rect(core.Rect(), "transparent")
+    painter.set_color("black")
+    painter.set_composition_mode("source_at_top")
+
+
+def test_pdfwriter():
+    writer = gui.PdfWriter("test")
+    writer.setup(core.RectF())
+
+
+def test_pen():
+    gui.Pen()
+
+
+def test_picture():
+    gui.Picture()
+
+
+def test_pixmap():
+    gui.Pixmap()
+
+
+def test_polygonf():
+    gui.PolygonF()
+
+
+def test_regexpvalidator():
+    val = gui.RegExpValidator()
+    val.set_regex("[0-9]")
+
+
+def test_standarditem():
+    gui.StandardItem()
+
+
+def test_syntaxhighlighter():
+    gui.SyntaxHighlighter(None)
+
+
+def test_textcharformat():
+    fmt = gui.TextCharFormat()
+    fmt.set_font_weight("bold")
+    fmt.set_foreground_color("yellow")
