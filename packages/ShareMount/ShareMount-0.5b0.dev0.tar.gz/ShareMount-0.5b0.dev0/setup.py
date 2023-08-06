9d9d03249491f4095bb93e3a80abaf57220bdc06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:37 +0000

    Add package ShareMount/0.5b0.dev0/ShareMount-0.5b0.dev0.tar.gz

---
+++ packages/ShareMount/ShareMount-0.5b0.dev0.tar.gz/ShareMount-0.5b0.dev0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ShareMount',
+    version='0.5b.0dev',
+    author='Darryl lane',
+    author_email='DarrylLane101@gmail.com',
+    packages=['ShareMount'],
+    include_package_data=True,
+    license='LICENSE',
+    description='''
+    Mount shares on Linux and OSX.
+    ''',
+    long_description=open('README.md').read(),
+    
+    scripts=['ShareMount/ShareMount.py']
+)
+
