53f2182cdf354d7cd4dc252cb6d5f90fd4970e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:50 +0000

    Add package dyaiapi/0.1/dyaiapi-0.1.tar.gz

---
+++ packages/dyaiapi/dyaiapi-0.1.tar.gz/dyaiapi-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='dyaiapi',
+      version='0.1',
+      description='dyai API Exchange Form',
+      url='http://github.com/jxhno1/dyai-api',
+      author='zhuc',
+      author_email='xxxxxxxx@163.com',
+      license='MIT',
+      packages=['dyaiapi'],
+      zip_safe=False)
