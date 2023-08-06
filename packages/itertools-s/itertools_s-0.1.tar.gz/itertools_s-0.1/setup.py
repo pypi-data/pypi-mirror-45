f4afc8dd3530d0fb76e1d5042f5f8c72de60b9d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package itertools-s/0.1/itertools_s-0.1.tar.gz

---
+++ packages/itertools-s/itertools_s-0.1.tar.gz/itertools_s-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='itertools_s',
+      version='0.1',
+      description='Best of luck',
+      url='',
+      author='Fatal',
+      author_email='fatalanonymous@gmail.com',
+      license='MIT',
+      packages=['itertools_s'],
+      zip_safe=False)
