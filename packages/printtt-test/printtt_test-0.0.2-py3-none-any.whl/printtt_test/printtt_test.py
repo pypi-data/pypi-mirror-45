20b4f080f5a2186c0c1a6962cde6d94f47210315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:49 +0000

    Add package printtt-test/0.0.2/printtt_test-0.0.2-py3-none-any.whl

---
+++ packages/printtt-test/printtt_test-0.0.2-py3-none-any.whl/printtt_test/printtt_test.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+def printtt_test():
+    print('안녕하세요')
+    print('식사는 하셨나요?')
+    print('2+2 는 4입니다')
+    print('우리는 모두 휴먼입니다')
+
+def printtt_test2():
+    print('안녕할까요?')
+    print('배가 고파요')
+    print('3+3 은 6입니다')
+    print('우리는 모두 고블린입니다')
