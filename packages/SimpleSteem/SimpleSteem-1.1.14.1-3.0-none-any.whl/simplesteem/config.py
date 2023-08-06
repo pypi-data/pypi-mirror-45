372f76bf75dd7bed5feb0ed27c29768a069b8132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:54 +0000

    Add package SimpleSteem/1.1.14.1/SimpleSteem-1.1.14.1-3.0-none-any.whl

---
+++ packages/SimpleSteem/SimpleSteem-1.1.14.1-3.0-none-any.whl/simplesteem/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+screenmode = "verbose"
+permissions = "login,offline,vote"
+logpath = ""
+client_id = ""
+callback_url = ""
+mainaccount = "artturtle"
+keys = ['5KN2rkFoR1s6hNdA5VbQqh5rd3Atvnf46apRhhnCTup25quQusv','5JSckknbPMJ1if6yY2f1j8ei5ssbdnXecm29qQDxL5XZbxWtJgA']
+nodes = ["https://rpc.curiesteem.com","https://rpc.buildteam.io","https://steemd.minnowsupportproject.org","https://gtg.steem.house:8090","https://rpc.steemliberator.com","https://api.steemitdev.com/","https://api.steemit.com/","https://api.steemitstage.com"]
+client_secret = ""
