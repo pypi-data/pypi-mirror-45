9179b40d17d571a9ef2135178bd81fb638227948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:15 +0000

    Add package sghmcmc/0.1.7/sghmcmc-0.1.7-py3-none-any.whl

---
+++ packages/sghmcmc/sghmcmc-0.1.7-py3-none-any.whl/sghmcmc/__init__.py
@@ -0,0 +1,6 @@
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
+
+from .sghmcmc import hmc 
+from .sghmc_jit import sghmc_jit
