d2cb8d5bd3ffaeb0fd7b7f98a6d94efcb97948ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:05 +0000

    Add package dockercomposeHelp/0.1.2/dockercomposeHelp-0.1.2.tar.gz

---
+++ packages/dockercomposeHelp/dockercomposeHelp-0.1.2.tar.gz/dockercomposeHelp-0.1.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dockercomposeHelp",
+    version="0.1.2",
+    author="Ariel Lima",
+    author_email="ariel.lima@gordon.edu",
+    description="A Python wrapper for generating a compose file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArielLima/dockercomposeHelp",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'pyyaml',
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
