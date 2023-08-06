f41e1ed52dcdac5185bcbd3e26f6860320f320c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:20 +0000

    Add package pydiagnostics/0.3.0/pydiagnostics-0.3.0.tar.gz

---
+++ packages/pydiagnostics/pydiagnostics-0.3.0.tar.gz/pydiagnostics-0.3.0/setup.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = ""
+
+    def run_tests(self):
+        import shlex
+
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["pytz>=2018", "numpy>=1.16.0", "matplotlib>=3.0.0"]
+
+# upload to pypi:
+#   python setup.py sdist bdist_wheel
+#   python -m twine upload dist/*
+
+setup(
+    name="pydiagnostics",
+    version="0.3.0",
+    author="Timo Lesterhuis",
+    author_email="timolesterhuis@gmail.com",
+    description="A toolbox to analyse diagnostic data!",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tim00w/diagnostics/",
+    packages=find_packages(),
+    install_requires=requirements,
+    tests_require=["pytest", "pytest-cov", "pytest-mpl"],
+    cmdclass={"pytest": PyTest},
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
