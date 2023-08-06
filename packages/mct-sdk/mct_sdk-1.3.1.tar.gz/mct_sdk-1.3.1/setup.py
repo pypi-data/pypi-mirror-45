65d4e807acff4c2fd02fdd3a59cf6929f4373353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:54 +0000

    Add package mct-sdk/1.3.1/mct_sdk-1.3.1.tar.gz

---
+++ packages/mct-sdk/mct_sdk-1.3.1.tar.gz/mct_sdk-1.3.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='mct_sdk',
+    version='1.3.1',
+    author="IO-HOST DEV",
+    author_email="admin@1-0.su",
+    url="https://github.com/Underlor/mct_sdk",
+    description="MCT python SKD",
+    install_requires=['requests', ],
+    packages=setuptools.find_packages(),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description="GIT: https://github.com/Underlor/mct_sdk",
+)
