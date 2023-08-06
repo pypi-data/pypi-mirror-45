3975f069771dd2a0c2a39fb5e7f3e6f668b7eeda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:13 +0000

    Add package wrapt-timeout-decorator/1.2.9/wrapt_timeout_decorator-1.2.9-py3-none-any.whl

---
+++ packages/wrapt-timeout-decorator/wrapt_timeout_decorator-1.2.9-py3-none-any.whl/wrapt_timeout_decorator/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from .wrapt_timeout_decorator import timeout
+from .wrap_helper import detect_unpickable_objects
+
+__version__ = '1.2.9'
+__title__ = 'wrapt_timeout_decorator'
+__name__ = 'wrapt_timeout_decorator'
