4b95218e354e714c0ebce6f1d609910ee2c581f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:34 +0000

    Add package rchitect/0.3.1/rchitect-0.3.1-cp37-cp37m-win32.whl

---
+++ packages/rchitect/rchitect-0.3.1-cp37-cp37m-win32.whl/rchitect/reticulate.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+import os
+import sys
+from .interface import rcall, rcopy, set_hook, package_event
+
+
+def run_or_set_hooks():
+    def hooks():
+        os.environ["RETICULATE_PYTHON"] = sys.executable
+        os.environ["RETICULATE_REMAP_OUTPUT_STREAMS"] = "0"
+
+        rcall(
+            ("base", "source"),
+            os.path.join(os.path.dirname(__file__), "R", "reticulate.R"),
+            rcall(("base", "new.env")))
+
+    if "reticulate" in rcopy(rcall(("base", "loadedNamespaces"))):
+        hooks()
+    else:
+        set_hook(package_event("reticulate", "onLoad"), lambda x, y: hooks())
