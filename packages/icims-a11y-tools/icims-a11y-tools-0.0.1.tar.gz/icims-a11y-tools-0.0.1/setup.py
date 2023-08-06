26fa9c425c806b18b7bea6ade741955b86fb76ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:40 +0000

    Add package icims-a11y-tools/0.0.1/icims-a11y-tools-0.0.1.tar.gz

---
+++ packages/icims-a11y-tools/icims-a11y-tools-0.0.1.tar.gz/icims-a11y-tools-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="icims-a11y-tools",
+    version="0.0.1",
+    author="chrisding",
+    description="The icims a11y scanner package",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
