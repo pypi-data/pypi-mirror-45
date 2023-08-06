ab610f3e47d0b367d3fee7ba40baf9070a90bdf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:56 +0000

    Add package sghmcmc/0.0.7/sghmcmc-0.0.7.tar.gz

---
+++ packages/sghmcmc/sghmcmc-0.0.7.tar.gz/sghmcmc-0.0.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sghmcmc",
+    version="0.0.7",                        # Update this for every new version
+    author="Xiaoyu Jiang",
+    author_email="xj35@duke.edu",
+    description="long description",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[                      # Add project dependencies here
+        "pandas>=0.20.0"                    # example: pandas version 0.20 or greater                          
+    ],                                             
+    url="https://github.com/jiangxiaoyuww/sghmc-",  
+    packages=setuptools.find_packages(),
+    classifiers=(                                 # Classifiers help people find your 
+        "Programming Language :: Python :: 3",    # projects. See all possible classifiers 
+        "License :: OSI Approved :: MIT License", # in https://pypi.org/classifiers/
+        "Operating System :: OS Independent",   
+    ),
+)
+
