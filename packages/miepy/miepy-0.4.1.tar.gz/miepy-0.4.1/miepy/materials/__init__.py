797e408b39af5dc002f31332c3895d1c65878aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:56 +0000

    Add package miepy/0.4.1/miepy-0.4.1.tar.gz

---
+++ packages/miepy/miepy-0.4.1.tar.gz/miepy-0.4.1/miepy/materials/__init__.py
@@ -0,0 +1,7 @@
+from . import load
+from . import predefined
+from . import create
+
+from .load import load_material, plot_material_by_author, material_info_by_author
+from .material_struct import material_struct
+from .predefined import *
