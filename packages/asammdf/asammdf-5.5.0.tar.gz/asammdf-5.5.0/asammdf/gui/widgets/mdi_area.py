f0e458091bbb037be83f5c09b6c4c92cce1e9304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:55 +0000

    Add package asammdf/5.5.0/asammdf-5.5.0.tar.gz

---
+++ packages/asammdf/asammdf-5.5.0.tar.gz/asammdf-5.5.0/asammdf/gui/widgets/mdi_area.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
+
+
+class MdiAreaWidget(QtWidgets.QMdiArea):
+
+    add_window_request = QtCore.pyqtSignal(list)
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+        self.setAcceptDrops(True)
+        self.show()
+
+    def dragEnterEvent(self, e):
+        e.accept()
+        super().dragEnterEvent(e)
+
+    def dropEvent(self, e):
+        if e.source() is self:
+            super().dropEvent(e)
+        else:
+            data = e.mimeData()
+            if data.hasFormat('application/x-qabstractitemmodeldatalist'):
+                if data.hasFormat('text/plain'):
+                    names = [
+                        name.strip('"\'')
+                        for name in data.text().strip('[]').split(', ')
+                    ]
+                else:
+                    model = QtGui.QStandardItemModel()
+                    model.dropMimeData(data, QtCore.Qt.CopyAction, 0,0, QtCore.QModelIndex())
+
+                    names = [
+                        model.item(row, 0).text()
+                        for row in range(model.rowCount())
+                    ]
+                ret, ok = QtWidgets.QInputDialog.getItem(
+                    None,
+                    "Select window type",
+                    "Type:",
+                    ["Plot", "Numeric"],
+                    0,
+                    False,
+                )
+                if ok:
+                    self.add_window_request.emit([ret, names])
