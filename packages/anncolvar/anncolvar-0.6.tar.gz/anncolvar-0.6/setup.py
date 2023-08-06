2e0aaa14a564ba787d039556829dbdf211223946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:59 +0000

    Add package anncolvar/0.6/anncolvar-0.6.tar.gz

---
+++ packages/anncolvar/anncolvar-0.6.tar.gz/anncolvar-0.6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+def readme():
+  with open('README.rst') as f:
+    return f.read()
+
+setup(name='anncolvar',
+      version='0.6',
+      description='Coding collective variables by artificial neural networks',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Chemistry',
+      ],
+      keywords='artificial neural networks molecular dynamics simulation',
+      url='https://github.com/spiwokv/anncolvar',
+      author='Vojtech Spiwok, ',
+      author_email='spiwokv@vscht.cz',
+      license='MIT',
+      packages=['anncolvar'],
+      scripts=['bin/anncolvar'],
+      install_requires=[
+          'numpy',
+          'cython',
+          'mdtraj',
+          'keras',
+          'argparse',
+          'datetime',
+          'codecov',
+      ],
+      include_package_data=True,
+      zip_safe=False)
+
