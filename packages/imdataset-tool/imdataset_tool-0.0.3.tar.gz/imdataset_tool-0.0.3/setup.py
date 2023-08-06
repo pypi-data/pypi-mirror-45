9f99d71a611b738c831eea9f3a104bfa8c5faa98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:09 +0000

    Add package imdataset-tool/0.0.3/imdataset_tool-0.0.3.tar.gz

---
+++ packages/imdataset-tool/imdataset_tool-0.0.3.tar.gz/imdataset_tool-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "imdataset_tool",
+    version = "0.0.3",
+    packages=['imdataset-tool'],
+    author = "kathy.chen",
+    author_email = "v_xchen_v@pku.edu.cn",
+    description = "a tool helps programmer to manage dataset of cv project"
+)
