8595b374d724c38229790ad2256bebf8169c7c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:47 +0000

    Add package DerPyBooru-Nullforce/0.9.0/DerPyBooru_Nullforce-0.9.0.tar.gz

---
+++ packages/DerPyBooru-Nullforce/DerPyBooru_Nullforce-0.9.0.tar.gz/DerPyBooru_Nullforce-0.9.0/derpibooru/__init__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+"""
+Derpibooru API bindings
+~~~~~~~~~~~~~~~~~~~~~~~
+
+Python bindings for Derpibooru's API
+
+Typical usage:
+
+>>> from derpibooru import Search, sort
+>>> for image in Search().sort_by(sort.SCORE):
+...   print(image.url)
+
+Full API Documentation is found at <https://derpibooru.org/pages/api>.
+
+Library documentation is found at <https://github.com/joshua-stone/DerPyBooru>.
+
+"""
+
+__title__ = "DerPyBooru"
+__version__ = "0.8.0"
+__author__ = "Joshua Stone"
+__license__ = "Simplified BSD Licence"
+__copyright__ = "Copyright (c) 2014-2018, Joshua Stone, Nullforce"
+
+from .search import Search
+from .query import query
+from .sort import sort
+from .user import user
+
+__all__ = [
+  "Search",
+  "query",
+  "sort",
+  "user"
+]
