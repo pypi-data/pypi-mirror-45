760cc8b33e085d522c4a9c121628ab17ecd0d875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:37 +0000

    Add package phylo-treetime/0.5.6/phylo_treetime-0.5.6-py3-none-any.whl

---
+++ packages/phylo-treetime/phylo_treetime-0.5.6-py3-none-any.whl/treetime/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import print_function, division, absolute_import
+from .treeanc import TreeAnc
+from .treetime import TreeTime, plot_vs_years
+from .clock_tree import ClockTree
+from .treetime import ttconf as treetime_conf
+from .gtr import GTR
+from .merger_models import Coalescent
+from .treeregression import TreeRegression
+version="0.5.6"
