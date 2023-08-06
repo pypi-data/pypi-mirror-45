605320a4c2e0ac6c2161785ed2373252e97e2f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:34 +0000

    Add package apybiomart/0.2.6/apybiomart-0.2.6.tar.gz

---
+++ packages/apybiomart/apybiomart-0.2.6.tar.gz/apybiomart-0.2.6/apybiomart/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Created by Roberto Preste
+from apybiomart.apybiomart import list_marts, list_datasets, \
+    list_attributes, list_filters, query, aquery
+
+__author__ = """Roberto Preste"""
+__email__ = 'robertopreste@gmail.com'
+__version__ = '0.2.6'
