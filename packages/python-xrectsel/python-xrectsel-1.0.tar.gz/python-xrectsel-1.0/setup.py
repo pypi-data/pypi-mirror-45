9c668f05cd0df9aed7d6adf272f9fab00b2276fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:18 +0000

    Add package python-xrectsel/1.0/python-xrectsel-1.0.tar.gz

---
+++ packages/python-xrectsel/python-xrectsel-1.0.tar.gz/python-xrectsel-1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    author="Nikhil Dhandre",
+    author_email="nik.digitronik@live.com",
+    classifiers=[
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities",
+        "Topic :: Software Development :: User Interfaces",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    description="Geometry of a rectangular screen region",
+    entry_points={"console_scripts": ["xrectsel= xrectsel.console:cli"]},
+    install_requires=["python-xlib"],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    setup_requires=["setuptools_scm"],
+    python_requires=">=3.4",
+    keywords="xrectsel, python-xrectsel",
+    name="python-xrectsel",
+    packages=find_packages(include=["python-xrectsel"]),
+    url="https://github.com/digitronik/python-xrectsel",
+    version="1.0",
+    license="GPLv3",
+    zip_safe=False,
+)
