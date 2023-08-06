f41d1af172cbccefd0e07d30a3c4e13a93688a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:03 +0000

    Add package finance-dl/1.3.2/finance-dl-1.3.2.zip

---
+++ packages/finance-dl/finance-dl-1.3.2.zip/finance-dl-1.3.2/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='finance-dl',
+    description='Tools for scraping personal financial data.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='1.3.2',
+    url='https://github.com/jbms/finance-dl',
+    author='Jeremy Maitin-Shepard',
+    author_email="jeremy@jeremyms.com",
+    license='GPLv2',
+    packages=["finance_dl"],
+    entry_points={
+        'console_scripts': [
+            'finance-dl = finance_dl.cli:main',
+            'finance-dl-chromedriver-wrapper = finance_dl.chromedriver_wrapper:main',
+        ],
+    },
+    python_requires='>=3.5',
+    install_requires=[
+        'bs4',
+        'mintapi>=1.31',
+        'ofxclient',
+        'selenium',
+        'ipython',
+        'selenium-requests',
+        'chromedriver-binary',
+        'beancount>=2.1.2',
+        'atomicwrites>=1.3.0',
+        'jsonschema',
+    ],
+)
