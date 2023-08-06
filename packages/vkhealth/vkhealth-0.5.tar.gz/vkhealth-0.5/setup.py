25562f68018f2d88beef68ef09770691aa911b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:01 +0000

    Add package vkhealth/0.5/vkhealth-0.5.tar.gz

---
+++ packages/vkhealth/vkhealth-0.5.tar.gz/vkhealth-0.5/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import setuptools
+
+def read (filename):
+  file = open(filename)
+  text = file.read()
+  file.close()
+  return text
+
+setup(
+  name = 'vkhealth',
+  version = '0.5',
+  description = 'Module that shows the current health of the VKontakte platform',
+  long_description=read('README.rst'),
+  packages=setuptools.find_packages(),
+  author = 'Daniil Chizhevskij',
+  author_email = 'daniilchizhevskij@gmail.com',
+  url = 'https://github.com/DaniilChizhevskii/vkhealth',
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Operating System :: OS Independent',
+  ],
+)
