13ff13936b3384ae111c6bf753d3ee8bd5dc4ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:42 +0000

    Add package reem/0.0.10/reem-0.0.10.tar.gz

---
+++ packages/reem/reem-0.0.10.tar.gz/reem-0.0.10/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='reem',
+    version='v0.0.10',
+    packages=['reem'],
+    url='https://www.github.com/tn74/reem',
+    license='Apache 2.0',
+    author='Trishul Nagenalli',
+    author_email='trishul.nagenalli@duke.edu',
+    description='Redis Extendable Efficient Middleware'
+)
