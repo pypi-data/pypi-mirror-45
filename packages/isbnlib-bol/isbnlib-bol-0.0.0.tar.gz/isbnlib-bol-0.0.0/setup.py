e76673a466e3604622f92905a3d31bed04454005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:39 +0000

    Add package isbnlib-bol/0.0.0/isbnlib-bol-0.0.0.tar.gz

---
+++ packages/isbnlib-bol/isbnlib-bol-0.0.0.tar.gz/isbnlib-bol-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="isbnlib-bol",
+      description="ISBN lookup for Bol.com",
+      packages=['isbnlib_bol/'],
+      author="Stan Janssen",
+      author_email="pypi@finetuned.nl",
+      url="https://git.finetuned.nl/stan/isbnlib-bol",
+      entry_points = {
+        'isbnlib.metadata': ['bol=isbnlib_bol:query']
+      },
+      install_requires=['requests', 'bs4', 'isbnlib'])
