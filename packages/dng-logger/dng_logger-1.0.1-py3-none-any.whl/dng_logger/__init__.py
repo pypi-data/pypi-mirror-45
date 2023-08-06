a45121a0dab1bf6cfd615d72318162b1fc659fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:21 +0000

    Add package dng-logger/1.0.1/dng_logger-1.0.1-py3-none-any.whl

---
+++ packages/dng-logger/dng_logger-1.0.1-py3-none-any.whl/dng_logger/__init__.py
@@ -0,0 +1,8 @@
+name = 'dng_logger'
+version = '1.0.1'
+
+from .logger import log
+
+print('[WARNING] dng_logger library has been deprecated.')
+print('          Run `pip install hedgepig-logger` and switch')
+print('          to using `import hedgepig_logger` instead.')
