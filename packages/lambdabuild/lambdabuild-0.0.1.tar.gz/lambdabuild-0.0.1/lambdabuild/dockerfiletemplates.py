2d1eee3580750ffbb31860220e73581f091eb184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:47 +0000

    Add package lambdabuild/0.0.1/lambdabuild-0.0.1.tar.gz

---
+++ packages/lambdabuild/lambdabuild-0.0.1.tar.gz/lambdabuild-0.0.1/lambdabuild/dockerfiletemplates.py
@@ -0,0 +1,26 @@
+BUILD_LAYER = '''
+FROM %(base_image)s
+
+RUN mkdir /lambdabuild
+WORKDIR /lambdabuild
+RUN pip%(three)s install -t /lambdabuild %(requirements)s
+RUN rm -fr boto boto3 botocore
+COPY clean_lambda_directory_before_zip_python%(two_or_three)s.py /
+RUN python%(three)s /clean_lambda_directory_before_zip_python%(two_or_three)s.py . --sourceless
+RUN find -name '*.so*' | xargs -I @ strip @
+RUN tar -cf /layer.tar *
+CMD /bin/bash
+'''
+
+BUILD_ARTIFACT = '''
+FROM %(base_image)s
+
+RUN mkdir /lambdabuild
+WORKDIR /lambdabuild
+COPY clean_lambda_directory_before_zip_python%(two_or_three)s.py /
+COPY sourcecode/ /lambdabuild/
+RUN python%(three)s /clean_lambda_directory_before_zip_python%(two_or_three)s.py . %(entry_points)s
+RUN find -name '*.so*' | xargs -I @ strip @
+RUN tar -cf /artifact.tar *
+CMD /bin/bash
+'''
