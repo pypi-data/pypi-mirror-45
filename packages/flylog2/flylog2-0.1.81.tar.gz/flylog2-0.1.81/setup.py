c15158baf3c1b935f0ad6ed84e9c462cb76d2d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:27 +0000

    Add package flylog2/0.1.81/flylog2-0.1.81.tar.gz

---
+++ packages/flylog2/flylog2-0.1.81.tar.gz/flylog2-0.1.81/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name='flylog2',
+      version='0.1.81',
+      description='make log fly to mail or other',
+      author='grey',
+      author_email='greyeee@gmail.com',
+      url='https://github.com/dantezhu/flylog',
+      packages=find_packages(),
+      install_requires=['requests', 'telegram==0.0.1', 'python-telegram-bot==11.1.0'],
+)
