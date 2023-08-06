a57074654d6fe49c077ea930541f4c263036d3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:29 +0000

    Add package ipython-cosmos-extension/0.1.2/ipython-cosmos-extension-0.1.2.tar.gz

---
+++ packages/ipython-cosmos-extension/ipython-cosmos-extension-0.1.2.tar.gz/ipython-cosmos-extension-0.1.2/cosmos_sql/__init__.py
@@ -0,0 +1,85 @@
+import azure.cosmos.documents as documents
+import azure.cosmos.cosmos_client as cosmos_client
+import azure.cosmos.errors as errors
+from IPython.core import magic_arguments
+from IPython.core.magic import line_magic, cell_magic, line_cell_magic, Magics, magics_class
+import pandas as pd
+import logging
+
+CosmosClient = None
+database = None
+container = None
+
+def load_ipython_extension(ipython):
+    global database, container, CosmosClient
+    import os
+    host = os.environ["COSMOS_ENDPOINT"]
+    masterkey = os.environ["COSMOS_MASTERKEY"]
+    print(host)
+
+    if host is None or masterkey is None:
+        logging.error("Cosmos endpoint credentials is not set.")
+        print("cosmos endpoint credentials is not set")
+        raise
+
+    CosmosClient = cosmos_client.CosmosClient(host, {'masterKey': masterkey})
+    ipython.register_magics(CosmosMagics)
+
+def unload_ipython_extension(ipython):
+    CosmosClient = None
+    pass
+
+@magics_class
+class CosmosMagics(Magics):
+    @cell_magic
+    @magic_arguments.magic_arguments()
+    @magic_arguments.argument('--database', '-d',
+      help='specifies database name'
+    )
+    @magic_arguments.argument('--container', '-c',
+      help='specifies container name'
+    )
+    @magic_arguments.argument('--asJson',
+      help='specifies the output format'
+    )
+    def sql(self, line='', cell=None):
+        global database, container, CosmosClient
+
+        args = magic_arguments.parse_argstring(self.sql, line)
+
+        if args.database:
+            database_id = args.database
+        else:
+            database_id = database
+        if args.container:
+            container_id = args.container
+        else:
+            container_id = container
+
+        if database_id is None:
+            raise Exception('database is not specified')
+
+        if container_id is None:
+            raise Exception('container is not specified')
+
+        database_link = 'dbs/' + database_id
+        earthquakes = database_link + '/colls/' + container_id
+        query = {"query": cell}
+        items = list(CosmosClient.QueryItems(earthquakes, query, {'enableCrossPartitionQuery': True}))
+
+        if bool(args.asJson):
+            return items
+        else:
+            return pd.DataFrame.from_records(items)
+
+    @line_magic("database")
+    def set_database(self, line, cell="", local_ns=None):
+        print("Using database " + line)
+        global database, container, CosmosClient
+        database = line
+
+    @line_magic("container")
+    def set_container(self, line, cell="", local_ns=None):
+        print("Using container " + line)
+        global database, container, CosmosClient
+        container = line
