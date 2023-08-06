125a3436177fe37064c3e0eaf6afb264017133f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:45 +0000

    Add package understreck/0.3.0/understreck-0.3.0.tar.gz

---
+++ packages/understreck/understreck-0.3.0.tar.gz/understreck-0.3.0/understreck/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Understreck."""
+
+__author__ = """Carl-Fredrik Arvidson"""
+__email__ = "carl-fredrik@arvidson.io"
+__version__ = "0.3.0"
+
+from .nested_get import nested_get, get
+from .chunks import *
+from .filter import filter
