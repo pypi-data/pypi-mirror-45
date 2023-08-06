a099f48c871d36d77c1a4ba55e9720286a9971f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:19 +0000

    Add package django-andablog/3.0.0/django-andablog-3.0.0.tar.gz

---
+++ packages/django-andablog/django-andablog-3.0.0.tar.gz/django-andablog-3.0.0/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+requirements = [
+    'six',
+    'Django>=1.11,<2.1',
+    'django-model-utils>=3.0,<4.0',
+    'django-markupfield>=1.5,<2',
+    'django-taggit>=0.22.2,<1.0.0',
+    'pillow>=4.0.0,<5.0.0',
+]
+
+test_requirements = [
+    # None, these go into the test_requirements file
+]
+
+setup(
+    name='django-andablog',
+    version='3.0.0',
+    description='A blog app that is only intended to be embedded within an existing Django site.',
+    long_description=readme + '\n\n' + history,
+    author='Ivan VenOsdel',
+    author_email='ivan@wimpyanalytics.com',
+    url='https://github.com/WimpyAnalytics/django-andablog',
+    download_url='https://github.com/wimpyanalytics/django-andablog/tarball/3.0.0',
+    packages=[
+        'andablog',
+    ],
+    package_dir={'andablog':
+                 'andablog'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords=['django-andablog', 'blog', 'django', 'app', 'reusable app', 'andablog'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+)
