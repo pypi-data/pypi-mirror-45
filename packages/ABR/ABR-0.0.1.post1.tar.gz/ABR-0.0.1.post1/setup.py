fd245e6377febe9085fc2ccde52b471516a1c314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:01 +0000

    Add package ABR/0.0.1.post1/ABR-0.0.1.post1.tar.gz

---
+++ packages/ABR/ABR-0.0.1.post1.tar.gz/ABR-0.0.1.post1/setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+    readme = f.read()
+
+packages = [
+    'abr',
+]
+
+package_data = {
+    'abr': ['*.enaml', 'data/*'],
+}
+
+requires = [
+    'atom',
+    'enaml',
+    'matplotlib',
+    'numpy',
+    'pandas',
+    'pyqt5',
+    'scipy',
+]
+
+classifiers = [
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: BSD License',
+]
+
+setup(
+    name='ABR',
+    version='0.0.1.post1',
+    author='Brad Buran',
+    author_email='bburan@alum.mit.edu',
+    description='ABR wave analyzer',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/bburan/abr',
+    packages=find_packages(),
+    package_data=package_data,
+    install_requires=requires,
+    classifiers=classifiers,
+    entry_points={
+        'console_scripts': [
+            'abr = abr.app.cmd_launcher:main',
+            'abr_gui = abr.app.cmd_main:main',
+            'abr_loop = abr.app.cmd_main_loop:main',
+        ]
+    },
+)
