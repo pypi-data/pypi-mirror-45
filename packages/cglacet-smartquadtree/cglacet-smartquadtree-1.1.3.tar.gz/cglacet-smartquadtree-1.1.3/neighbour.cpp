9214d4ee2767334a6ab561bb0f95328a369424d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:48 +0000

    Add package cglacet-smartquadtree/1.1.3/cglacet-smartquadtree-1.1.3.tar.gz

---
+++ packages/cglacet-smartquadtree/cglacet-smartquadtree-1.1.3.tar.gz/cglacet-smartquadtree-1.1.3/neighbour.cpp
@@ -0,0 +1,16 @@
+/*
+ * Interface for a smart version of quadtrees specialised for tracking moving
+ * objects. When iterating over elements in the quadtree, a simple flag
+ * (boolean) indicates whether the element might have moved to a neighbouring
+ * subdivision.
+ *
+ * Xavier Olive, 28 nov. 2014
+ */
+
+#include "neighbour.h"
+
+unsigned int Neighbour::maxlevel = 0;
+unsigned int Neighbour::_x = 0;
+unsigned int Neighbour::_y = 0;
+unsigned int Neighbour::directions[8] = {1, 3, 2, 0, 0, 0, 0, 0};
+
