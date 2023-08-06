8627249ad0d28664f53075a1fc54149a2f4f6bb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:52 +0000

    Add package funniest_ltp/0.1/funniest_ltp-0.1.tar.gz

---
+++ packages/funniest_ltp/funniest_ltp-0.1.tar.gz/funniest_ltp-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest_ltp',
+      version='0.1',
+      description='blablabla',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='LTP',
+      packages=['funniest_ltp'],
+      zip_safe=False)
