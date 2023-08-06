bc394b9e4fe231601b8cf57db4244a32261c36c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:17 +0000

    Add package jupyter-syncthing-proxy/1.0.dev0/jupyter-syncthing-proxy-1.0.dev0.tar.gz

---
+++ packages/jupyter-syncthing-proxy/jupyter-syncthing-proxy-1.0.dev0.tar.gz/jupyter-syncthing-proxy-1.0.dev0/jupyter_syncthing_proxy/__init__.py
@@ -0,0 +1,17 @@
+"""
+Return config on servers to start for syncthing
+
+See https://jupyter-server-proxy.readthedocs.io/en/latest/server-process.html
+for more information.
+"""
+import os
+
+def setup_syncthing():
+    return {
+        'command': ['syncthing'],
+        'environment': {},
+        'launcher_entry': {
+            'title': 'syncthing',
+            'icon_url': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', 'syncthing.svg')
+        }
+    }
---
+++ packages/jupyter-syncthing-proxy/jupyter-syncthing-proxy-1.0.dev0.tar.gz/jupyter-syncthing-proxy-1.0.dev0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="jupyter-syncthing-proxy",
+    version='1.0dev',
+    url="https://github.com/jupyterhub/jupyter-server-proxy/tree/master/contrib/syncthing",
+    author="Project Jupyter Contributors",
+    description="projectjupyter@gmail.com",
+    packages=setuptools.find_packages(),
+	keywords=['Jupyter'],
+	classifiers=['Framework :: Jupyter'],
+    install_requires=[
+        'jupyter-server-proxy>=1.0.2'
+    ],
+    entry_points={
+        'jupyter_serverproxy_servers': [
+            'syncthing = jupyter_syncthing_proxy:setup_syncthing',
+        ]
+    },
+    package_data={
+        'jupyter_syncthing_proxy': ['icons/*'],
+    },
+)
