938c92c28d9d2b70ea4966e690e5d1d4193f5ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:06 +0000

    Add package ltp_eval/0.3/ltp_eval-0.3.tar.gz

---
+++ packages/ltp_eval/ltp_eval-0.3.tar.gz/ltp_eval-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ltp_eval',
+      version='0.3',
+      description='Query your results',
+      url='https://gitlab.com/famorim/ltp_eval.git',
+      author='LTP',
+      author_email='franciscoamorim@ltplabs.com',
+      license='LTP',
+      packages=['ltp_eval'],
+      zip_safe=False)
