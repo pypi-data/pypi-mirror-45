296c63ec0273814e2b81ae458efc821eefbe8ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:23 +0000

    Add package blanc-basic-assets/0.2.3/blanc-basic-assets-0.2.3.tar.gz

---
+++ packages/blanc-basic-assets/blanc-basic-assets-0.2.3.tar.gz/blanc-basic-assets-0.2.3/blanc_basic_assets/assets/listeners.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import pre_save, post_delete
+from .models import Image, File
+
+
+def asset_file_change(sender, instance, **kwargs):
+    # Must be saved already
+    if instance.pk is not None:
+        old_obj = sender.objects.get(pk=instance.pk)
+
+        # Delete the old file if the file names don't match
+        if old_obj.file.name != instance.file.name:
+            storage = old_obj.file.storage
+            storage.delete(name=old_obj.file.name)
+
+
+def asset_file_delete(instance, **kwargs):
+    # Remove the file along with it
+    instance.file.delete(save=False)
+
+
+pre_save.connect(asset_file_change, sender=Image)
+pre_save.connect(asset_file_change, sender=File)
+post_delete.connect(asset_file_delete, sender=Image)
+post_delete.connect(asset_file_delete, sender=File)
