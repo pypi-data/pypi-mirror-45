6861263f9d189da792641d57bd66ca578dd87826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:56 +0000

    Add package minimaldb/1.2/minimaldb-1.2.tar.gz

---
+++ packages/minimaldb/minimaldb-1.2.tar.gz/minimaldb-1.2/README.md
@@ -0,0 +1,30 @@
+# Minimaldb
+
+Minimaldb is a very simple document oriented database.
+
+## Supported Python Versions
+Minimaldb is tested with Python 3.7
+
+## Installation
+From PyPi:
+```
+$ pip install minimaldb
+```
+
+## Example Code
+```python
+from minimaldb import MinimalDB
+
+db = MinimalDB("database_name.db")
+
+# Setting a value
+db.set("key_name", value)
+
+# Getting a value
+db.get("key_name")
+
+# Deleting a value
+db.delete("key_name")
+
+# Updating a value
+db.update("key_name", value)
---
+++ packages/minimaldb/minimaldb-1.2.tar.gz/minimaldb-1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+with open("README.md", "r") as stream:
+    long_description = stream.read()
+
+setuptools.setup(
+    name='minimaldb',
+    version='1.2',
+    description='Minimalistic document oriented database.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/ZAZPRO/minimaldb',
+    author='Daniil Mira',
+    author_email='daniilmira@gmail.com',
+    packages=setuptools.find_packages(),
+    classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+    ],
+)
