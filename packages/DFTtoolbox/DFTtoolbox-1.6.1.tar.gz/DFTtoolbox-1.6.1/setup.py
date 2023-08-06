3442dbe79e0406dba03580efd091ccd4cbaa1400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:56 +0000

    Add package DFTtoolbox/1.6.1/DFTtoolbox-1.6.1.tar.gz

---
+++ packages/DFTtoolbox/DFTtoolbox-1.6.1.tar.gz/DFTtoolbox-1.6.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+version='1.6.1'
+setup(
+    name = 'DFTtoolbox',
+    version = version,
+    packages = ['DFTtoolbox'],
+    description = 'A toolbox to initialize or postpocess several DFT codes',
+    scripts = [],
+    license='MIT',
+    author = 'pipidog',
+    author_email = 'pipidog@gmail.com',
+    url = 'https://github.com/pipidog/DFTtoolbox',
+    download_url = 'https://github.com/pipidog/DFTtoolbox/archive/v'+version+'.tar.gz',
+    keywords = ['density-functional-theory','qantum-espresso','elk','abinit'],
+    classifiers = ['Topic :: Scientific/Engineering :: Physics'],
+    install_requires=['numpy','matplotlib']
+)
