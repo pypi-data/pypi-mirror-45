3f045d15128c6ed107b366f70d945e4d717a36da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:59 +0000

    Add package lmfhawq/1.1.8/lmfhawq-1.1.8.zip

---
+++ packages/lmfhawq/lmfhawq-1.1.8.zip/lmfhawq-1.1.8/lmfhawq/hdb.py
@@ -0,0 +1,18 @@
+import lmfhawq.zl as src 
+
+import lmfhawq.zldb as v 
+
+src_add_quyu=src.add_quyu_fast 
+
+
+src_drop_quyu=src.drop_quyu_fast
+
+
+v_add_quyu=v.add_quyu 
+
+v_drop_quyu=v.drop_quyu 
+
+
+
+
+
