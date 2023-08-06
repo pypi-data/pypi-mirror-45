f1ffd27923ab8ae28a3ba94ed31b98f8a5e14277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:10 +0000

    Add package screen19/0.204/screen19-0.204.tar.gz

---
+++ packages/screen19/screen19-0.204.tar.gz/screen19-0.204/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["procrunner", "typing"]
+test_requirements = ["mock>=2.0", "pytest>=3.1"]
+
+setup(
+    author="Diamond Light Source",
+    author_email="scientificsoftware@diamond.ac.uk",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+    ],
+    description="Screening program for small-molecule single-crystal X-ray diffraction data",
+    entry_points={
+        "console_scripts": [
+            "i19.screen = screen19.screen:main",
+            "screen19 = screen19.screen:main",
+            "i19.stability_fft = screen19.stability_fft:main",
+            "i19.sync = screen19.sync:main",
+            "i19.minimum_flux = screen19.minimum_flux:run",
+        ],
+        "libtbx.dispatcher.script": [
+            "i19.screen = i19.screen",
+            "screen19 = screen19",
+            "i19.stability_fft = i19.stability_fft",
+            "i19.sync = i19.sync",
+            "i19.minimum_flux = i19.minimum_flux",
+        ],
+    },
+    install_requires=requirements,
+    license="BSD license",
+    long_description=readme,
+    include_package_data=True,
+    name="screen19",
+    packages=find_packages(),
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/xia2/screen19",
+    version="0.204",
+    zip_safe=False,
+)
