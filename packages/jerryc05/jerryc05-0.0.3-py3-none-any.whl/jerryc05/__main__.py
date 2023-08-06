d5318dc5bfa983d04ed4a32088e2bc7e7dbcd2d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:36 +0000

    Add package jerryc05/0.0.3/jerryc05-0.0.3-py3-none-any.whl

---
+++ packages/jerryc05/jerryc05-0.0.3-py3-none-any.whl/jerryc05/__main__.py
@@ -0,0 +1,16 @@
+def main():
+    from sys import argv
+    argv: list
+    if len(argv) == 1:
+        argv.append('12306')
+    from jerryc05.my_12306.__main__ import main as main_12306
+    # todo
+
+    {
+        '12306': lambda: main_12306(argv[1:]),
+    }.get(argv[1] if type(argv[1]) == str else f'{argv[1]}',
+          lambda: print(f'Argument {argv[1]} invalid'))()
+
+
+if __name__ == "__main__":
+    main()
