81e4b337db78a40099863bd5960e362637b8cf66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:30 +0000

    Add package ragnar/0.1.1/ragnar-0.1.1.tar.gz

---
+++ packages/ragnar/ragnar-0.1.1.tar.gz/ragnar-0.1.1/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = []
+
+setup_requirements = []
+
+test_requirements = []
+
+setup(
+    author="vikilab",
+    author_email='vikilab.library@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Text Processing',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="lightweight Extract-Transform-Load (ETL) framework for Python 3+",
+    entry_points={
+        'console_scripts': [
+            'ragnar=ragnar.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='ragnar',
+    name='ragnar',
+    packages=find_packages(include=['ragnar']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/vikilab/ragnar',
+    version='0.1.1',
+    zip_safe=False,
+)
