24ece5904727431d406d99b0595ca188dac6d5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:18:02 +0000

    Add package fiddler-client/0.2.1/fiddler-client-0.2.1.tar.gz

---
+++ packages/fiddler-client/fiddler-client-0.2.1.tar.gz/fiddler-client-0.2.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('Readme.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='fiddler-client',
+    version='0.2.1',
+    author='Fiddler Labs',
+    description='Python client for Fiddler Service',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://fiddler.ai',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
