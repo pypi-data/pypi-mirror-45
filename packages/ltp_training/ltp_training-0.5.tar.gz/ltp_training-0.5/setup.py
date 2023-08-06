d1cb96a515b6596e033650388deb489491a6c8ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:50 +0000

    Add package ltp_training/0.5/ltp_training-0.5.tar.gz

---
+++ packages/ltp_training/ltp_training-0.5.tar.gz/ltp_training-0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ltp_training',
+      version='0.5',
+      description='Query your results',
+      url='https://gitlab.com/famorim/ltp_eval.git',
+      author='LTP',
+      author_email='franciscoamorim@ltplabs.com',
+      license='LTP',
+      packages=['ltp_training'],
+      zip_safe=False)
