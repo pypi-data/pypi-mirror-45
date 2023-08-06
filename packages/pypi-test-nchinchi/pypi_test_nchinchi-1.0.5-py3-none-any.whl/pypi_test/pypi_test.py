d55ed947b388f869fef8a88a47e41875b9f46e77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:42 +0000

    Add package pypi-test-nchinchi/1.0.5/pypi_test_nchinchi-1.0.5-py3-none-any.whl

---
+++ packages/pypi-test-nchinchi/pypi_test_nchinchi-1.0.5-py3-none-any.whl/pypi_test/pypi_test.py
@@ -0,0 +1,18 @@
+class PyPITest:
+
+    @staticmethod
+    def plus(a: int, b: int) -> int:
+        """
+        Practice PyPI project.
+
+        Parameters
+        ----------
+        a: a added number
+        b: a add number
+
+        Returns
+        -------
+        result: a + b
+        """
+        result = a + b
+        return result
