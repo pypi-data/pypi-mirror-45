434182ed894ceeaf7549cb6e878161280b8a8f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:52 +0000

    Add package AgricolAI/0.0.19/AgricolAI-0.0.19.tar.gz

---
+++ packages/AgricolAI/AgricolAI-0.0.19.tar.gz/AgricolAI-0.0.19/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(name='AgricolAI',
+      version='0.0.19',
+      description='AI Module for Agricultural application',
+      url='https://github.com/Zhiwu-Zhang-Lab/James_Git',
+      author='James Chen',
+      author_email='chun-peng.chen@wsu.edu',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      install_requires=['numpy',
+                        'pandas>=0.19.2',
+                        'sklearn',
+                        'scipy',
+                        'matplotlib',
+                        'spectral'])
