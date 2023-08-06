4346156b8f3822756df596cbc58ce06faa65d883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:06 +0000

    Add package ket/0.0.4/ket-0.0.4.tar.gz

---
+++ packages/ket/ket-0.0.4.tar.gz/ket-0.0.4/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='ket',
+    version='0.0.4',
+    description='Bitbucket workflows in your terminal',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Andrii Yurchuk',
+    author_email='ay@mntw.re',
+    license='MIT',
+    url='https://github.com/Ch00k/ket',
+    install_requires=[
+        'click==7.0',
+        'furl==2.0.0',
+        'gitpython==2.1.11',
+        'requests==2.21.0',
+        'tabulate==0.8.3'],
+    packages=find_packages(),
+    entry_points='''
+        [console_scripts]
+        ket=ket.cli:ket''',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Version Control',
+        'Topic :: Software Development :: Version Control :: Git'])
