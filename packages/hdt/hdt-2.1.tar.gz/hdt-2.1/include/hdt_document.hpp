7e3d97e928f726e87fe55770e64b4f9a602ea9f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:11 +0000

    Add package hdt/2.1/hdt-2.1.tar.gz

---
+++ packages/hdt/hdt-2.1.tar.gz/hdt-2.1/include/hdt_document.hpp
@@ -0,0 +1,153 @@
+/**
+ * hdt_document.hpp
+ * Author: Thomas MINIER - MIT License 2017-2019
+ */
+
+#ifndef PYHDT_DOCUMENT_HPP
+#define PYHDT_DOCUMENT_HPP
+
+#include "HDT.hpp"
+#include "QueryProcessor.hpp"
+#include "pyhdt_types.hpp"
+#include "triple_iterator.hpp"
+#include "tripleid_iterator.hpp"
+#include "join_iterator.hpp"
+#include <list>
+#include <string>
+#include <vector>
+
+// The result of a search for a triple pattern in a HDT document:
+// a tuple (matching RDF triples, nb of matching RDF triples)
+typedef std::tuple<TripleIterator *, size_t> search_results;
+
+// Same as seach_results, but for an iterator over triple ids
+typedef std::tuple<TripleIDIterator *, size_t> search_results_ids;
+
+/*!
+ * HDTDocument is the main entry to manage an hdt document
+ * \author Thomas Minier
+ */
+class HDTDocument {
+private:
+  std::string hdt_file;
+  hdt::HDT *hdt;
+  hdt::QueryProcessor *processor;
+  HDTDocument(std::string file);
+
+public:
+  /*!
+   * Destructor
+   */
+  ~HDTDocument();
+
+  /*!
+   * Get the path to the HDT file currently loaded
+   * @return The path to the HDT file currently loaded
+   */
+  std::string getFilePath();
+
+  /*!
+   * Implementation for Python function "__repr__"
+   * @return A string representation of the object
+   */
+  std::string python_repr();
+
+  /*!
+   * Get the total number of triples in the HDT document
+   * @return The total number of triples in the HDT document
+   */
+  unsigned int getNbTriples();
+
+  /*!
+   * Get the number of distinct subjects in the HDT document
+   * @return The number of distinct subjects in the HDT document
+   */
+  unsigned int getNbSubjects();
+
+  /*!
+   * Get the number of distinct predicates in the HDT document
+   * @return The number of distinct predicates in the HDT document
+   */
+  unsigned int getNbPredicates();
+
+  /*!
+   * Get the number of distinct objects in the HDT document
+   * @return The number of distinct objects in the HDT document
+   */
+  unsigned int getNbObjects();
+
+  /*!
+   * Get the number of shared subjects-objects in the HDT document
+   * @return The number of shared subjects-objects in the HDT document
+   */
+  unsigned int getNbShared();
+
+  /*!
+   * Static factory method used to create a new HDT Document
+   * @param file - Path to the HDT file
+   */
+  static HDTDocument create(std::string file) { return HDTDocument(file); }
+
+  /*!
+   * Convert a TripleID to a string RDF triple
+   * @param  subject   - Triple's subject
+   * @param  predicate - Triple's predicate
+   * @param  object    - Triple's object
+   * @return The associated RDF triple
+   */
+  triple convertTripleID(unsigned int subject, unsigned int predicate,
+                     unsigned int object);
+
+  /**
+   * Convert an Object Identifier into the equivalent an RDF term
+   * @param  id  - Object Identifier
+   * @param  pos - Identifier position (subject, predicate or object)
+   * @return The an RDF term equivalent to the Object Identifier
+   */
+  string convertID(unsigned int id, IdentifierPosition pos);
+
+  /**
+   * Convert an RDF term into the associated an Object Identifier.
+   * @param  term  - RDF Term in string format
+   * @param  pos - Identifier position (subject, predicate or object)
+   * @return The Object Identifier associated with the RDF term
+   */
+  unsigned int convertTerm(std::string term, IdentifierPosition pos);
+
+  /*!
+   * Search all matching triples for a triple pattern, whith an optional limit and offset.
+   * Returns a tuple<TripleIterator*, cardinality>
+   * @param subject   - Triple pattern's subject
+   * @param predicate - Triple pattern's predicate
+   * @param object    - Triple pattern's object
+   * @param limit     - (Optional) Maximum number of matching triples to read
+   * @param offset    - (Optional) Number of matching triples to skip
+   * @return A tuple (TripleIterator*, cardinality)
+   */
+  search_results search(std::string subject, std::string predicate,
+                        std::string object, unsigned int limit = 0,
+                        unsigned int offset = 0);
+
+  /*!
+   * Same as HDTDocument#search, but search for TripleIDs instead.
+   * Returns a tuple<TripleIDIterator*, cardinality>
+   * @param subject   - Triple pattern's subject identifier
+   * @param predicate - Triple pattern's predicate identifier
+   * @param object    - Triple pattern's object identifier
+   * @param limit     - (Optional) Maximum number of matching triples to read
+   * @param offset    - (Optional) Number of matching triples to skip
+   * @return A tuple (TripleIDIterator*, cardinality)
+   */
+  search_results_ids searchIDs(unsigned int subject, unsigned int predicate,
+                               unsigned int object, unsigned int limit = 0,
+                               unsigned int offset = 0);
+
+  /**
+   * Evaluate a join between a set of triple patterns using a JoinIterator.
+   * @param  patterns - Set of triple patterns
+   * @return A JoinIterator* used to evaluated the join.
+   */
+  JoinIterator * searchJoin(std::vector<triple> patterns);
+};
+
+#endif /* PYHDT_DOCUMENT_HPP */
---
+++ packages/hdt/hdt-2.1.tar.gz/hdt-2.1/src/hdt.cpp
@@ -0,0 +1,114 @@
+/**
+ * hdt.cpp
+ * Author: Thomas MINIER - MIT License 2017-2019
+ */
+
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include "docstrings.hpp"
+#include "hdt_document.hpp"
+#include "triple_iterator.hpp"
+#include "tripleid_iterator.hpp"
+#include "join_iterator.hpp"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(hdt, m) {
+  m.doc() = MODULE_DOC;
+
+  py::enum_<IdentifierPosition>(m, "IdentifierPosition", IDENTIFIER_POSITION_DOC)
+    .value("Subject", IdentifierPosition::Subject)
+    .value("Predicate", IdentifierPosition::Predicate)
+    .value("Object", IdentifierPosition::Object)
+    .export_values();
+
+  py::class_<TripleIterator>(m, "TripleIterator", TRIPLE_ITERATOR_CLASS_DOC)
+      .def("next", &TripleIterator::next, TRIPLE_ITERATOR_NEXT_DOC)
+      .def("__next__", &TripleIterator::next, TRIPLE_ITERATOR_NEXT_DOC)
+      .def("peek", &TripleIterator::peek, TRIPLE_ITERATOR_PEEK_DOC)
+      .def("has_next", &TripleIterator::hasNext, TRIPLE_ITERATOR_HASNEXT_DOC)
+      .def("size_hint", &TripleIterator::sizeHint, TRIPLE_ITERATOR_SIZE_DOC)
+      .def("__len__", &TripleIterator::sizeHint,
+           TRIPLE_ITERATOR_SIZE_DOC)
+      .def("__iter__", &TripleIterator::python_iter)
+      .def_property_readonly("subject", &TripleIterator::getSubject,
+                             TRIPLE_ITERATOR_GETSUBJECT_DOC)
+      .def_property_readonly("predicate", &TripleIterator::getPredicate,
+                             TRIPLE_ITERATOR_GETPREDICATE_DOC)
+      .def_property_readonly("object", &TripleIterator::getObject,
+                             TRIPLE_ITERATOR_GETOBJECT_DOC)
+      .def_property_readonly("limit", &TripleIterator::getLimit,
+                             TRIPLE_ITERATOR_GETLIMIT_DOC)
+      .def_property_readonly("offset", &TripleIterator::getOffset,
+                             TRIPLE_ITERATOR_GETOFFSET_DOC)
+      .def_property_readonly("nb_reads", &TripleIterator::getNbResultsRead,
+                    TRIPLE_ITERATOR_NBREADS_DOC)
+      .def("__repr__", &TripleIterator::python_repr);
+
+  py::class_<TripleIDIterator>(m, "TripleIDIterator", TRIPLE_ID_ITERATOR_CLASS_DOC)
+      .def("next", &TripleIDIterator::next, TRIPLE_ITERATOR_NEXT_DOC)
+      .def("__next__", &TripleIDIterator::next, TRIPLE_ITERATOR_NEXT_DOC)
+      .def("peek", &TripleIDIterator::peek, TRIPLE_ITERATOR_PEEK_DOC)
+      .def("has_next", &TripleIDIterator::hasNext, TRIPLE_ITERATOR_HASNEXT_DOC)
+      .def("size_hint", &TripleIDIterator::sizeHint, TRIPLE_ITERATOR_SIZE_DOC)
+      .def("__len__", &TripleIDIterator::sizeHint, TRIPLE_ITERATOR_SIZE_DOC)
+      .def("__iter__", &TripleIDIterator::python_iter)
+      .def_property_readonly("subject", &TripleIDIterator::getSubject,
+                             TRIPLE_ITERATOR_GETSUBJECT_DOC)
+      .def_property_readonly("predicate", &TripleIDIterator::getPredicate,
+                             TRIPLE_ITERATOR_GETPREDICATE_DOC)
+      .def_property_readonly("object", &TripleIDIterator::getObject,
+                             TRIPLE_ITERATOR_GETOBJECT_DOC)
+      .def_property_readonly("limit", &TripleIDIterator::getLimit,
+                             TRIPLE_ITERATOR_GETLIMIT_DOC)
+      .def_property_readonly("offset", &TripleIDIterator::getOffset,
+                             TRIPLE_ITERATOR_GETOFFSET_DOC)
+      .def_property_readonly("nb_reads", &TripleIDIterator::getNbResultsRead,
+                    TRIPLE_ITERATOR_NBREADS_DOC)
+      .def("__repr__", &TripleIDIterator::python_repr);
+
+  py::class_<JoinIterator>(m, "JoinIterator", JOIN_ITERATOR_CLASS_DOC)
+    .def("next", &JoinIterator::next, JOIN_ITERATOR_NEXT_DOC)
+    .def("has_next", &JoinIterator::hasNext, JOIN_ITERATOR_HAS_NEXT_DOC)
+    .def("cardinality", &JoinIterator::estimatedCardinality, JOIN_ITERATOR_SIZE_DOC)
+    .def("reset", &JoinIterator::reset, JOIN_ITERATOR_RESET_DOC)
+    .def("__len__", &JoinIterator::estimatedCardinality, JOIN_ITERATOR_SIZE_DOC)
+    .def("__next__", &JoinIterator::next, JOIN_ITERATOR_NEXT_DOC)
+    .def("__iter__", &JoinIterator::python_iter)
+    .def("__repr__", &JoinIterator::python_repr);
+
+  py::class_<HDTDocument>(m, "HDTDocument", HDT_DOCUMENT_CLASS_DOC)
+      .def(py::init(&HDTDocument::create))
+      .def_property_readonly("file_path", &HDTDocument::getFilePath,
+                             HDT_DOCUMENT_GETFILEPATH_DOC)
+      .def_property_readonly("total_triples", &HDTDocument::getNbTriples,
+                             HDT_DOCUMENT_GETNBTRIPLES_DOC)
+      .def_property_readonly("nb_subjects", &HDTDocument::getNbSubjects,
+                             HDT_DOCUMENT_GETNBSUBJECTS_DOC)
+      .def_property_readonly("nb_predicates", &HDTDocument::getNbPredicates,
+                             HDT_DOCUMENT_GETNBPREDICATES_DOC)
+      .def_property_readonly("nb_objects", &HDTDocument::getNbObjects,
+                             HDT_DOCUMENT_GETNBOBJECTS_DOC)
+      .def_property_readonly("nb_shared", &HDTDocument::getNbShared,
+                             HDT_DOCUMENT_GETNBSHARED_DOC)
+      .def("search_triples", &HDTDocument::search,
+           HDT_DOCUMENT_SEARCH_TRIPLES_DOC, py::arg("subject"),
+           py::arg("predicate"), py::arg("object"), py::arg("limit") = 0,
+           py::arg("offset") = 0)
+      .def("search_join", &HDTDocument::searchJoin, HDT_DOCUMENT_SEARCH_JOIN_DOC, py::arg("patterns"))
+     .def("search_triples_ids", &HDTDocument::searchIDs,
+          HDT_DOCUMENT_SEARCH_TRIPLES_IDS_DOC, py::arg("subject"),
+          py::arg("predicate"), py::arg("object"), py::arg("limit") = 0,
+          py::arg("offset") = 0)
+      .def("convert_tripleid", &HDTDocument::convertTripleID,
+           HDT_DOCUMENT_TRIPLES_IDS_TO_STRING_DOC,
+           py::arg("subject"), py::arg("predicate"), py::arg("object"))
+      .def("convert_id", &HDTDocument::convertID, HDT_DOCUMENT_CONVERT_ID_DOC,
+           py::arg("id"), py::arg("position"))
+     .def("convert_term", &HDTDocument::convertTerm, HDT_DOCUMENT_CONVERT_TERM_DOC,
+          py::arg("term"), py::arg("position"))
+      .def("__len__", &HDTDocument::getNbTriples, HDT_DOCUMENT_GETNBTRIPLES_DOC)
+      .def("__repr__", &HDTDocument::python_repr);
+
+}
