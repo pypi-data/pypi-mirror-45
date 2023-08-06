052e35141f57c05af844c11ebe18dc9ea23f8992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:40 +0000

    Add package alphashape/0.1.7/alphashape-0.1.7.tar.gz

---
+++ packages/alphashape/alphashape-0.1.7.tar.gz/alphashape-0.1.7/alphashape/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for Alpha Shape Toolbox."""
+
+__author__ = """Kenneth E. Bellock"""
+__email__ = 'ken@bellock.net'
+
+from .alphashape import alphashape
+from ._version import __version__  # noqa: F401
+__all__ = ['alphashape']
