a15f4ff563e9816d20de9fe092dc4d53effb1058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:38 +0000

    Add package tdameritrade-client/0.3.2/tdameritrade-client-0.3.2.tar.gz

---
+++ packages/tdameritrade-client/tdameritrade-client-0.3.2.tar.gz/tdameritrade-client-0.3.2/tdameritrade_client/__init__.py
@@ -0,0 +1,9 @@
+from tdameritrade_client import auth
+from tdameritrade_client import client
+from tdameritrade_client import utils
+
+__version__ = '0.3.2'
+__doc__ = \
+    """
+    python client for the TDA API
+    """
---
+++ packages/tdameritrade-client/tdameritrade-client-0.3.2.tar.gz/tdameritrade-client-0.3.2/tdameritrade_client/utils/urls.py
@@ -0,0 +1,30 @@
+import os
+import platform
+from environs import Env
+
+env = Env()
+
+# Package urls
+PACKAGE_BASE = os.path.dirname(os.path.dirname(__file__))
+if platform.system() == 'Windows':
+    DEFAULT_TOKEN_DIR = os.path.join(env('HOMEDRIVE'), env('HOMEPATH'),
+                                      '.tda_certs')
+elif platform.system() == 'Linux' or platform.system() == 'Darwin':
+    DEFAULT_TOKEN_DIR = os.path.join(env('HOME'), '.tda_certs')
+
+else:
+    raise NotImplementedError(f'Your OS ({platform.system()}) is not supported by this package.')
+
+# TDAmeritrade base urls
+AUTH_URL = 'https://auth.tdameritrade.com/auth'
+BASE_URL = 'https://api.tdameritrade.com'
+
+# Resource URLs
+TOKEN_URL = '/v1/oauth2/token'
+ACCOUNT_URL = '/v1/accounts/'
+INSTRUMENTS_URL = '/v1/instruments/'
+QUOTES_URL = '/v1/marketdata/quotes'
+
+# Params
+AUTH_PARAMS = '?response_type=code&redirect_uri='
+APIKEY_PARAM = '?apikey='
