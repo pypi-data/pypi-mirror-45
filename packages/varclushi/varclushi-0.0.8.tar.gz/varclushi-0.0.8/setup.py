bc04fc69611d5096ac6b07e344cca98f07789d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:25 +0000

    Add package varclushi/0.0.8/varclushi-0.0.8.tar.gz

---
+++ packages/varclushi/varclushi-0.0.8.tar.gz/varclushi-0.0.8/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="varclushi",
+    version="0.0.8",
+    author="Xuan Jing",
+    author_email="xuanjing@hotmail.com",
+    description="A package for variable clustering",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jingtt/varclushi",
+    packages=setuptools.find_packages(),
+	install_requires=[
+		"pandas",
+		"numpy",
+		"factor-analyzer==0.2.3",
+	],
+    classifiers=[
+		"Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+		"Operating System :: Microsoft :: Windows",
+		"Operating System :: POSIX :: Linux",
+		"Operating System :: Unix",
+    ],
+)
