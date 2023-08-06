79b30e39ae66aee7fbaa3012441e2cc37ba526e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:11 +0000

    Add package PyUpdater-SCP-Plugin/4.0/PyUpdater-SCP-Plugin-4.0.tar.gz

---
+++ packages/PyUpdater-SCP-Plugin/PyUpdater-SCP-Plugin-4.0.tar.gz/PyUpdater-SCP-Plugin-4.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+import versioneer
+
+setup(
+    name='PyUpdater-SCP-Plugin',
+    version=versioneer.get_version(),
+    description='SCP plugin for PyUpdater',
+    author='JMSwag',
+    author_email='johnymoswag@gmail.com',
+    url='https://github.com/JMSwag/PyUpdater-SCP-Plugin',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Developers',
+        'Environment :: Console'
+                 ],
+    platforms=['Any'],
+    install_requires=[
+        'paramiko',
+        'scp',
+        ],
+    py_modules=['scp_uploader', '_version'],
+    include_package_data=True,
+    entry_points={
+        'pyupdater.plugins.upload': [
+            'scp = scp_uploader:SCPUploader',
+        ],
+    },
+    zip_safe=False,
+    cmdclass=versioneer.get_cmdclass(),
+)
