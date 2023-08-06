66b4dd58f55b4cc99a8edfd1a9df2bd7a66ad706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:06 +0000

    Add package django-deploy-templates/0.1.1/django_deploy_templates-0.1.1-py3-none-any.whl

---
+++ packages/django-deploy-templates/django_deploy_templates-0.1.1-py3-none-any.whl/django_deploy_templates/management/commands/generate_config.py
@@ -0,0 +1,30 @@
+import os
+from jinja2 import Environment, FileSystemLoader
+import glob
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    help = 'Generating config files with deploy templates'
+
+    def handle(self, **options):
+
+        if not settings.ALLOWED_HOSTS:
+            return 'Ничего не сделали, заполни ALLOWED_HOSTS в настройках'
+
+        jinja_env = Environment(
+            loader=FileSystemLoader(settings.DEPLOY_TEMPLATES_DIR),
+        )
+
+        for filepath in glob.glob(os.path.join(settings.DEPLOY_TEMPLATES_DIR, '*.jinja2'), recursive=True):
+            template_name = filepath.replace(f"{settings.DEPLOY_TEMPLATES_DIR}{os.sep}", '')
+            template = jinja_env.get_template(template_name)
+            config_filename = os.path.splitext(template_name)[0]
+
+            generated_config = template.render({'settings': settings})
+
+            # Creating folder
+            os.makedirs(os.path.join(settings.DEPLOY_CONF_DIR), exist_ok=True)
+            with open(os.path.join(settings.DEPLOY_CONF_DIR, config_filename), 'w', encoding='utf8') as f:
+                f.write(generated_config)
