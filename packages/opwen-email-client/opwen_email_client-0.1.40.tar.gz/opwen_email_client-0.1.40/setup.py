20d55b71061bd277d4311367f6fe802157f3f5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:11 +0000

    Add package opwen-email-client/0.1.40/opwen_email_client-0.1.40.tar.gz

---
+++ packages/opwen-email-client/opwen_email_client-0.1.40.tar.gz/opwen_email_client-0.1.40/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt', encoding='utf-8') as fobj:
+    install_requires = [line.strip() for line in fobj]
+
+with open('README.rst', encoding='utf-8') as fobj:
+    long_description = fobj.read()
+
+with open('version.txt', encoding='utf-8') as fobj:
+    version = fobj.read().strip()
+
+packages = find_packages(exclude=['tests*'])
+scripts = ['manage.py']
+
+setup(
+    name='opwen_email_client',
+    version=version,
+    author='Clemens Wolff',
+    author_email='clemens.wolff+pypi@gmail.com',
+    packages=packages,
+    url='https://github.com/ascoderu/opwen-webapp',
+    license='Apache Software License',
+    description='Email client for the Lokole project: https://ascoderu.ca',
+    long_description=long_description,
+    include_package_data=True,
+    scripts=scripts,
+    install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications :: Email',
+    ])
