e0add3cf608319d9ec20c86a44e1507070ac952a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:23 +0000

    Add package alibi/0.1.0/alibi-0.1.0-py3-none-any.whl

---
+++ packages/alibi/alibi-0.1.0-py3-none-any.whl/alibi/explainers/__init__.py
@@ -0,0 +1,13 @@
+"""
+The 'alibi.explainers' module includes feature importance, counterfactual and anchor-based explainers.
+"""
+
+from .anchor_tabular import AnchorTabular
+from .anchor_text import AnchorText
+from .anchor_image import AnchorImage
+from .cem import CEM
+
+__all__ = ["AnchorTabular",
+           "AnchorText",
+           "AnchorImage",
+           "CEM"]
