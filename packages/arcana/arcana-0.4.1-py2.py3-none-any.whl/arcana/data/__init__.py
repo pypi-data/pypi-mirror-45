440a0cd39d9228fb23fd038b85b83d0f72b2e391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:27 +0000

    Add package arcana/0.4.1/arcana-0.4.1-py2.py3-none-any.whl

---
+++ packages/arcana/arcana-0.4.1-py2.py3-none-any.whl/arcana/data/__init__.py
@@ -0,0 +1,8 @@
+from .item import Fileset, Field
+from .collection import FilesetCollection, FieldCollection
+from .spec import (
+    FilesetSpec, FieldSpec, FilesetInputSpec, FieldInputSpec, BaseSpec,
+    BaseInputSpec)
+from .base import BaseField, BaseFileset, BaseData
+from .input import FilesetInput, FieldInput, BaseInput
+from .file_format import FileFormat, Converter, IdentityConverter
---
+++ packages/arcana/arcana-0.4.1-py2.py3-none-any.whl/arcana/processor/__init__.py
@@ -0,0 +1,4 @@
+from .base import DEFAULT_PROV_CHECK, DEFAULT_PROV_IGNORE
+from .linear import SingleProc
+from .slurm import SlurmProc
+from .multiproc import MultiProc
