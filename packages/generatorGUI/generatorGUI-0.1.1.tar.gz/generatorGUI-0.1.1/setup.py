c4b8346dab518ff2fc2c254e6c9a30036f242719
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:37 +0000

    Add package generatorGUI/0.1.1/generatorGUI-0.1.1.tar.gz

---
+++ packages/generatorGUI/generatorGUI-0.1.1.tar.gz/generatorGUI-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.MD", "r") as fh:
+    long_des = fh.read()
+
+setup(
+    name="generatorGUI",
+    version="0.1.1",
+    author="Ao Liu",
+    author_email="ao@aoliu.tech",
+    description="Edit parameters for the Astra generator program "
+                "and run generator using a GUI.",
+    license="MIT",
+    keywords=["generator", "astra", "GUI", "PyQt5"],
+    url="https://pypi.python.org/pypi/generatorGUI",
+    packages=find_packages(),
+    python_requires='>=3.0',
+    long_description=long_des,
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=["numpy>=1.16", "easyexception", "pandas>=0.24", "PyQt5"],
+    setup_requires=["numpy>=1.16", "easyexception", "pandas>=0.24", "PyQt5"]
+)
