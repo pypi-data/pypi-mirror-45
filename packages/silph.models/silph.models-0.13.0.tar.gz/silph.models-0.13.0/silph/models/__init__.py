dc913df85fbd2af92e69e36165d57654aa015e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:06 +0000

    Add package silph.models/0.13.0/silph.models-0.13.0.tar.gz

---
+++ packages/silph.models/silph.models-0.13.0.tar.gz/silph.models-0.13.0/silph/models/__init__.py
@@ -0,0 +1,51 @@
+
+from .communities import (
+    Community,
+    CommunityAdmin,
+    DiscordMeta,
+    TelegramMeta,
+)
+
+from .events import (
+    Event,
+    EventCommunities,
+    EventStaff,
+    EventCheckin,
+)
+
+from .maps import (
+    Pip,
+)
+
+from .meetings import (
+    Meeting,
+    TravelersMet,
+)
+
+from .nests import (
+    Nest,
+    NestVerification,
+    NestAdvice,
+    NestMigration,
+)
+
+from .news import (
+    RedditSubmission,
+)
+
+from .pokemon import (
+    Pkmn,
+    Pokemon,
+    PokemonType,
+)
+
+from .users import (
+    ApiUser,
+    Avatar,
+    Badge,
+    TravelersCard,
+    User,
+    UserLogin,
+    UnlockedAvatar,
+    UserBadge,
+)
---
+++ packages/silph.models/silph.models-0.13.0.tar.gz/silph.models-0.13.0/silph/models/base.py
@@ -0,0 +1,5 @@
+
+from asyncqlio import table_base
+
+__all__ = ['Table']
+Table = table_base()
