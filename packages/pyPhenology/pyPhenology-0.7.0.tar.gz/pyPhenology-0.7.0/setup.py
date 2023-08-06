d29f7878d81a89d03c2ea36b56950da8fb238274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:15 +0000

    Add package pyPhenology/0.7.0/pyPhenology-0.7.0.tar.gz

---
+++ packages/pyPhenology/pyPhenology-0.7.0.tar.gz/pyPhenology-0.7.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='pyPhenology',
+      version='0.7.0',
+      description='Plant phenology models in python',
+      url='https://github.com/sdtaylor/pyPhenology',
+      author='Shawn Taylor',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
