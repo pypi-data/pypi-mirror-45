1fdaacf11ab820a975a03af0058060d55c3e34ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:51 +0000

    Add package notetools/0.0.1/notetools-0.0.1-py3-none-any.whl

---
+++ packages/notetools/notetools-0.0.1-py3-none-any.whl/notetools/__init__.py
@@ -0,0 +1,4 @@
+"""Initialize module"""
+
+__author__ = 'johntd54'
+__version__ = '0.0.1'
---
+++ packages/notetools/notetools-0.0.1-py3-none-any.whl/notetools/colab/constants.py
@@ -0,0 +1,4 @@
+"""Hold constant variables"""
+
+PUBLIC_BASE_DIR = '/usr/local/share/jupyter/nbextensions'
+PUBLIC_BASE_URL = 'nbextensions'
