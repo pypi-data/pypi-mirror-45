5c758f6737416c7a09ceeb481ca155b70c90dcbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:08 +0000

    Add package cf-lines-overlap/0.0.2/cf_lines_overlap-0.0.2.tar.gz

---
+++ packages/cf-lines-overlap/cf_lines_overlap-0.0.2.tar.gz/cf_lines_overlap-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='cf_lines_overlap',
+    version='0.0.2',
+    packages=['overlap'],
+    url='https://github.com/carlos-ferras/code-challenge/overlap',
+    license='MIT',
+    author='Carlos Ferras',
+    author_email='c4rlos.ferra5@gmail.com',
+    description='Check if 2 lines overlap',
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown"
+)
