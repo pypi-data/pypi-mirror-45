8554b7c881d2b0a508bf984a7a3d4cb3f35ba208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:38 +0000

    Add package hex2/2.2.0/hex2-2.2.0.tar.gz

---
+++ packages/hex2/hex2-2.2.0.tar.gz/hex2-2.2.0/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Hexdump Utility
+===============
+
+A command line hexdump utility.
+
+See the module's `Github homepage <https://github.com/yafeile/hex>`_
+for details.
+
+"""
+
+import os
+import re
+import io
+
+from setuptools import setup
+
+
+filepath = os.path.join(os.path.dirname(__file__), 'hex.py')
+with io.open(filepath, encoding='utf-8') as metafile:
+    regex = r'''^__([a-z]+)__ = ["'](.*)["']'''
+    meta = dict(re.findall(regex, metafile.read(), flags=re.MULTILINE))
+
+
+setup(
+    name = 'hex2',
+    version = meta['version'],
+    py_modules = ['hex'],
+    entry_points = {
+        'console_scripts': [
+            'hex2 = hex:main',
+        ],
+    },
+    author = 'yafeile',
+    author_email="yafeile@sohu.com",
+    url = 'https://github.com/yafeile/hex',
+    license = 'Public Domain',
+    description = (
+        'Hexdump command line utility.'
+    ),
+    long_description = __doc__,
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 2',
+        'Operating System :: OS Independent',
+        'License :: Public Domain',
+        'Topic :: Utilities',
+    ],
+)
