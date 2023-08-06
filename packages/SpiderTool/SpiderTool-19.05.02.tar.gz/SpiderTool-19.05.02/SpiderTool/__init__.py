d3ddfe049dd7d164ff0ecae0a6acb07512689bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:20 +0000

    Add package SpiderTool/19.05.02/SpiderTool-19.05.02.tar.gz

---
+++ packages/SpiderTool/SpiderTool-19.05.02.tar.gz/SpiderTool-19.05.02/SpiderTool/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+"""
+File:   __init__.py
+Author: Lijiacai(1050518702@qq.com)
+Description:
+    this package provide BROWSER and REQUEST
+    if you wish print your logging to you stdout
+        please add a logging filename or use import loggingtool
+"""
