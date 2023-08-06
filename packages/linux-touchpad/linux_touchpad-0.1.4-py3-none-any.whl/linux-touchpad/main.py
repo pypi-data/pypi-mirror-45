38c010fdd540dbe3b4df6b61664db44a084571f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:22 +0000

    Add package linux-touchpad/0.1.4/linux_touchpad-0.1.4-py3-none-any.whl

---
+++ packages/linux-touchpad/linux_touchpad-0.1.4-py3-none-any.whl/linux-touchpad/main.py
@@ -0,0 +1,51 @@
+import os
+import asyncio as aio
+import signal
+import argparse
+from operator import itemgetter
+
+from .lock import Lock, LockExistsError
+from .touchpad import SIGTOGGLE, watch_devices
+from .process import handler
+
+
+def start():
+    try:
+        with Lock():
+            signal.signal(signal.SIGTERM, handler)
+            signal.signal(SIGTOGGLE, handler)
+
+            aio.run(watch_devices())
+
+    except LockExistsError:
+        pass
+
+
+def signal_toggle():
+    pid = Lock.getpid()
+    os.kill(pid, SIGTOGGLE)
+
+
+def signal_kill():
+    pid = Lock.getpid()
+    os.kill(pid, signal.SIGTERM)
+
+
+def main():
+    choices = {
+        'start': start,
+        'toggle': signal_toggle,
+        'kill': signal_kill
+    }
+    parser = argparse.ArgumentParser(
+        prog="linux-touchpad",
+        description="Auto disable touchpad when mouse is detected."
+    )
+    parser.add_argument('command', choices=choices, type=itemgetter)
+    args = parser.parse_args()
+    command = args.command(choices)
+    command()
+
+
+if __name__ == '__main__':
+    main()
