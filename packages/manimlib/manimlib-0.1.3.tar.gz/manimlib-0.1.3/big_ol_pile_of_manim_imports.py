97dd07aa006251cc4697726b267b36a6c962cd3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:56 +0000

    Add package manimlib/0.1.3/manimlib-0.1.3.tar.gz

---
+++ packages/manimlib/manimlib-0.1.3.tar.gz/manimlib-0.1.3/big_ol_pile_of_manim_imports.py
@@ -0,0 +1,37 @@
+"""
+I won't pretend like this is best practice, by in creating animations for a video,
+it can be very nice to simply have all of the Mobjects, Animations, Scenes, etc.
+of manim available without having to worry about what namespace they come from.
+
+Rather than having a large pile of "from <module> import *" at the top of every such
+script, the intent of this file is to make it so that one can just include
+"from big_ol_pile_of_manim_imports import *".  The effects of adding more modules
+or refactoring the library on current or older scene scripts should be entirely
+addressible by changing this file.
+
+Note: One should NOT import from this file for main library code, it is meant only
+as a convenience for scripts creating scenes for videos.
+"""
+
+from manimlib.shortcuts import *
+
+from manimlib.for_3b1b_videos.common_scenes import *
+from manimlib.for_3b1b_videos.pi_creature import *
+from manimlib.for_3b1b_videos.pi_creature_animations import *
+from manimlib.for_3b1b_videos.pi_creature_scene import *
+
+# Non manim libraries that are also nice to have without thinking
+
+import inspect
+import itertools as it
+import numpy as np
+import operator as op
+import os
+import random
+import re
+import string
+import sys
+import math
+
+from PIL import Image
+from colour import Color
