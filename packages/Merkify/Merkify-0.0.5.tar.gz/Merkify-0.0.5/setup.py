cfb92826f0bac9baee90981e76492d80f1f89042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:47 +0000

    Add package Merkify/0.0.5/Merkify-0.0.5.tar.gz

---
+++ packages/Merkify/Merkify-0.0.5.tar.gz/Merkify-0.0.5/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+  name = 'Merkify',
+  version = '0.0.5',
+  license='MIT',
+  description = 'TYPE YOUR DESCRIPTION HERE',
+  author = 'Bani Singh',
+  author_email = 'banihns@gmail.com',
+  url = 'https://github.com/user/reponame',
+  download_url = 'https://github.com/banisingh/Merkify/archive/0.0.5.tar.gz',
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],
+  packages = setuptools.find_packages(),
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
