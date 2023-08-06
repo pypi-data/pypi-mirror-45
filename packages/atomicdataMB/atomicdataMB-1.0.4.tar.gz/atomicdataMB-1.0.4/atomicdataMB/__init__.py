0158165fd4cc79723eb619352b67e0f127345662
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:38 +0000

    Add package atomicdataMB/1.0.4/atomicdataMB-1.0.4.tar.gz

---
+++ packages/atomicdataMB/atomicdataMB-1.0.4.tar.gz/atomicdataMB-1.0.4/atomicdataMB/__init__.py
@@ -0,0 +1,10 @@
+from .g_values import make_gvalue_table, RadPresConst, gValue
+from .make_photo_table import make_photo_table
+from .configure_database import configure_database
+from .atomicmass import atomicmass
+
+
+name = 'atomicdataMB'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
+__version__ = '1.0.4'
