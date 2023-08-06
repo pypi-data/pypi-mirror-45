413307e2183d788c52c0238aec235fcecd309e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:08 +0000

    Add package scikit-gaze/0.0.7/scikit-gaze-0.0.7.tar.gz

---
+++ packages/scikit-gaze/scikit-gaze-0.0.7.tar.gz/scikit-gaze-0.0.7/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scikit-gaze",
+    version="0.0.7",
+    author="Zajic Michal",
+    author_email="zajicmichal6@gmail.com",
+    description="Library for scanpath comparison",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zuhi6/scikit-gaze/",
+    packages=["skgaze","skgaze.core","skgaze.EMDAT","skgaze.common_scanpath","skgaze.pattern_search","skgaze.RQA","skgaze.similarity_algorithms","skgaze.RQA.CustomPythonModules"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+)
