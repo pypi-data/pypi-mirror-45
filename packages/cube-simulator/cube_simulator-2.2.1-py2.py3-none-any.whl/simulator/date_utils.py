94dd40acdb13d73200c5e91f124cb85a002cd116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:54 +0000

    Add package cube-simulator/2.2.1/cube_simulator-2.2.1-py2.py3-none-any.whl

---
+++ packages/cube-simulator/cube_simulator-2.2.1-py2.py3-none-any.whl/simulator/date_utils.py
@@ -0,0 +1,25 @@
+__author__ = 'SHASHANK'
+
+from datetime import datetime
+from dateutil import rrule
+from .constants import SCHEME_HOLIDAYS, HOLIDAYS
+
+
+class DateUtils:
+
+    def __init__(self):
+        rs = rrule.rruleset()
+        [rs.exdate(datetime.strptime(HOLIDAY, "%Y-%m-%d")) for HOLIDAY in HOLIDAYS]
+
+        r = rrule.rrule(rrule.YEARLY, count=366, byweekday=[rrule.MO, rrule.TU, rrule.WE, rrule.TH, rrule.FR], dtstart=datetime.strptime("2019-01-01", "%Y-%m-%d"))
+        rs.rrule(r)
+
+        self.working_day_list = list(rs._iter())
+        self.mf_scheme_non_working_day_list = {key: [datetime.strptime(val, "%Y-%m-%d") for val in vals] for (key, vals) in SCHEME_HOLIDAYS}
+
+    def get_next_date(self, date, days=1):
+        ind = min(self.working_day_list, key=lambda x: (date.date() - x.date()).days if x < date else 0)
+        return self.working_day_list[self.working_day_list.index(ind) + days]
+
+    def is_scheme_holiday(self, date, scheme_id):
+        return date in self.mf_scheme_non_working_day_list.get(scheme_id, [])
