b0bdd94f6e57db930ffeabda25e33a2ac3f54a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:53 +0000

    Add package cellxgene/0.9.1/cellxgene-0.9.1.tar.gz

---
+++ packages/cellxgene/cellxgene-0.9.1.tar.gz/cellxgene-0.9.1/server/tmp.py
@@ -0,0 +1,6 @@
+import profile
+# from server.app.scanpy_engine.scanpy_engine import ScanpyEngine
+
+import scanpy as sc
+a = sc.read("/Users/charlotteweaver/Documents/Git/cellxgene/datasets-local/1mil_with_categories.h5ad")
+# ScanpyEngine("/Users/charlotteweaver/Documents/Git/cellxgene/datasets-local/1mil_with_categories.h5ad")
