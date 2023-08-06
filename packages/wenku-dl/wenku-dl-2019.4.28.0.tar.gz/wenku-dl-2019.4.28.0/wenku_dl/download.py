5c935d164e9a6eba1a321c2559dfe35a73fbd6e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:34 +0000

    Add package wenku-dl/2019.4.28.0/wenku-dl-2019.4.28.0.tar.gz

---
+++ packages/wenku-dl/wenku-dl-2019.4.28.0.tar.gz/wenku-dl-2019.4.28.0/wenku_dl/download.py
@@ -0,0 +1,30 @@
+import os
+import requests
+
+from wenku_dl.wenku import WenkuAPI
+from wenku_dl.wenku import WenkuConfig
+
+from aigpy import pathHelper
+from aigpy import pdfHelper
+
+class Downloader(object):
+    def __init__(self):
+        self.api = WenkuAPI()
+    
+    def start(self, url):
+        conf = WenkuConfig()
+        info = self.api.parse(url)
+        path = pathHelper.getDiffTmpPathName(conf.outputdir)
+        name = conf.outputdir + '/' + pathHelper.replaceLimitChar(info['title'], '-')
+        # 下载图片
+        pathHelper.mkdirs(path)
+        array = []
+        for index, item in enumerate(info['pngurls']):
+            img = requests.get(item).content
+            with open(f'{path}/{index}.png', 'wb') as fp:
+                fp.write(img)
+                array.append(f'{path}/{index}.png')
+        # 图片转pdf
+        pdfHelper.imag2pdf(array, name+'.pdf')
+        pathHelper.remove(path)
+        print(f'[下载完成]{name}')
