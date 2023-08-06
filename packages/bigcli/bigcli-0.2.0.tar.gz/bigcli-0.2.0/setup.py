705c6e8efe77267c29423945370d3e4e1f5cf34a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:42 +0000

    Add package bigcli/0.2.0/bigcli-0.2.0.tar.gz

---
+++ packages/bigcli/bigcli-0.2.0.tar.gz/bigcli-0.2.0/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+setup(
+    name='bigcli',
+    version='0.2.0',
+    description="A python framwork to write large CLIs using dependency injection",
+    long_description=readme + '\n\n' + history,
+    author="Duda Dornelles",
+    author_email='duda@thoughtworks.com',
+    url='https://github.com/dudadornelles/bigcli',
+    packages=find_packages(include=['bigcli']),
+    include_package_data=True,
+    install_requires=['pinject'],
+    license="MIT license",
+    zip_safe=False,
+    keywords='bigcli',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+    ],
+    test_suite='tests',
+    tests_require=['mock'],
+    setup_requires=[],
+)
---
+++ packages/bigcli/bigcli-0.2.0.tar.gz/bigcli-0.2.0/tests/test_bigcli.py
@@ -0,0 +1,59 @@
+from __future__ import print_function
+from mock import patch, call
+import unittest
+
+import bigcli
+
+class DomainObject(object):
+    __args__ = [
+        bigcli.arg('--password', help='the password', required=True),
+        bigcli.arg('value', help='a value')
+    ]
+
+    def __init__(self, args):
+       self.password = args.password
+       self.value = args.value
+
+
+class ExampleCommand(object):
+    __depends_on__ = [DomainObject]
+
+    def __init__(self, domain_object):
+        self.domain_object = domain_object
+
+    def __call__(self, *args, **kwargs):
+        print(self.domain_object.password)
+        print(self.domain_object.value)
+
+
+class ExampleSubcommand(object):
+    __parent__ = "parent-command"
+
+    def __call__(self, *args, **kwargs):
+        print("subcommand executed")
+
+class TestBigcli(unittest.TestCase):
+
+    def test_creates_parser_and_provides_complete_command_object(self):
+        cli = bigcli.BigCli(commands=[ExampleCommand])
+
+        parsed_args = cli.parser.parse_args(['example-command', 'a_value', '--password', 'notmypassword'])
+        self.assertEqual('a_value', parsed_args.value)
+        self.assertEqual('notmypassword', parsed_args.password)
+
+        command_object = cli._provide(ExampleCommand, parsed_args)
+
+        self.assertEqual('a_value', command_object.domain_object.value)
+        self.assertEqual('notmypassword', command_object.domain_object.password)
+
+    @patch('tests.test_bigcli.print')
+    def test_execute(self, print_mock):
+        bigcli.BigCli(commands=[ExampleCommand]).execute(['example-command', 'a_value', '--password', 'notmypassword'])
+
+        print_mock.assert_has_calls([call('notmypassword'), call('a_value')])
+
+    @patch('tests.test_bigcli.print')
+    def test_execute_subcommand(self, print_mock):
+        bigcli.BigCli(commands=[ExampleSubcommand]).execute(['parent-command', 'example-subcommand'])
+
+        print_mock.assert_called_with("subcommand executed")
