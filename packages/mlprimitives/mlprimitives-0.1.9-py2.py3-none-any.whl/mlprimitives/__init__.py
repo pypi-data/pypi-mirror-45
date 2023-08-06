84e6f2b027fd29458f12fca0bb41f9acfadc11e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:30 +0000

    Add package mlprimitives/0.1.9/mlprimitives-0.1.9-py2.py3-none-any.whl

---
+++ packages/mlprimitives/mlprimitives-0.1.9-py2.py3-none-any.whl/mlprimitives/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for MLPrimitives."""
+
+__author__ = 'MIT Data To AI Lab'
+__email__ = 'dailabmit@gmail.com'
+__version__ = '0.1.9'
+
+import os
+
+MLPRIMITIVES_JSONS_PATH = os.path.join(os.path.dirname(__file__), 'jsons')
