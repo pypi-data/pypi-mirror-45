f046ceed26b35d5ac4d481e8e107cd11de272ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:09 +0000

    Add package skale.py/0.62.0/skale.py-0.62.0.tar.gz

---
+++ packages/skale.py/skale.py-0.62.0.tar.gz/skale.py-0.62.0/skale/skaled_ports.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class SkaledPorts(Enum):
+    PROPOSAL = 0
+    CATCHUP = 1
+    HTTP_JSON = 2
+    WS_JSON = 3
+    BINARY_CONSENSUS = 4
+    ZMQ_BROADCAST = 5
+    MTA = 6
