c02d8b580b664feaf237f297eafc63de35b21df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:08 +0000

    Add package bs-ds/0.2.2/bs_ds-0.2.2.tar.gz

---
+++ packages/bs-ds/bs_ds-0.2.2.tar.gz/bs_ds-0.2.2/bs_ds/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for bs_ds."""
+
+__author__ = """James Irving, Michael Moravetz"""
+__email__ = 'james.irving.phd@outlook.com'
+__version__ = '0.2.2'
+
+from .bs_ds import *
