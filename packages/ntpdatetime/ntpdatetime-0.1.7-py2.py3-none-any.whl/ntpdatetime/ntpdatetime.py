6262e479c650759e7fa6cf853baf8db583aa3871
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:46 +0000

    Add package ntpdatetime/0.1.7/ntpdatetime-0.1.7-py2.py3-none-any.whl

---
+++ packages/ntpdatetime/ntpdatetime-0.1.7-py2.py3-none-any.whl/ntpdatetime/ntpdatetime.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from datetime import datetime
+from socket import gaierror
+
+from ntplib import NTPClient, NTPException
+
+from .config import poolservers
+
+
+class NTPDateTime(datetime):
+    """Extends the datetime module to add a custom method
+    """
+
+    @classmethod
+    def ntp_now(cls):
+        """Returns a tuple containing a datatime object based on the time
+           received from an NTP poolserver and a boolean value depending on
+           whether it was fetched from a poolserver or not
+        """
+        for poolserver in poolservers:
+            try:
+                response = NTPClient().request(
+                    poolserver,
+                    version=3,
+                    timeout=2
+                )
+                return cls.fromtimestamp(response.tx_time), True
+            except (NTPException, gaierror):
+                continue
+
+        # No poolservers supplied or no response from poolservers
+        # Just return the system time for now
+        return cls.now(), False
+
+
+ntpdatetime = NTPDateTime
+now = NTPDateTime.ntp_now
