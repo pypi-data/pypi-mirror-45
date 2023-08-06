6c993e3c0131193b2f41f2c8aba16c5627431e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:31 +0000

    Add package pypi-name-cli/1.0.3/pypi_name_cli-1.0.3.tar.gz

---
+++ packages/pypi-name-cli/pypi_name_cli-1.0.3.tar.gz/pypi_name_cli-1.0.3/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "pypi_name_cli",
+    packages = ["pypi_name_cli"],
+    entry_points = {
+        "console_scripts": ['pypi = pypi_name_cli.checkname:main']
+        },
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    version = "1.0.3",
+    description = "Check whether a package name is available on PyPI",
+    author = "Yoginth",
+    maintainer = "Yoginth",
+    author_email = "me@yoginth.com",
+    maintainer_email = "me@yoginth.com",
+    url = "https://yoginth.com",
+    keywords = "PyPI, CLI, Name Checker, Package Name",
+    license = "MIT",
+    classifiers=(
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: MIT License",
+    ),
+    project_urls={
+        'Source': 'https://gitlab.com/yoginth/pypi_name_cli',
+    },
+    install_requires=[
+        'pypi_name',
+    ],
+)
