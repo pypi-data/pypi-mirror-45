c19ca98a358968786dc0f620409bf53f7f7f31c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:57 +0000

    Add package nesterlupiani/1.3.0/nesterlupiani-1.3.0.tar.gz

---
+++ packages/nesterlupiani/nesterlupiani-1.3.0.tar.gz/nesterlupiani-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	= 'nesterlupiani',
+	version	= '1.3.0', 
+	py_modules = ['nesterlupiani'],
+	author	= 'hfpython', 
+	author_email = 'hypython@headfirstlabs.com',
+	url	= 'http://www.headfirstlabs.com', 
+	description = 'A simple printer of nested lists',
+)
