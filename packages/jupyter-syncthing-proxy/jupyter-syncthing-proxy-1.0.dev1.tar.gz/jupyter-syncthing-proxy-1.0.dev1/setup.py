4885f9dfaff99cdc7eb9567042dd0903979b3b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:17 +0000

    Add package jupyter-syncthing-proxy/1.0.dev1/jupyter-syncthing-proxy-1.0.dev1.tar.gz

---
+++ packages/jupyter-syncthing-proxy/jupyter-syncthing-proxy-1.0.dev1.tar.gz/jupyter-syncthing-proxy-1.0.dev1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="jupyter-syncthing-proxy",
+    version='1.0dev1',
+    url="https://github.com/jupyterhub/jupyter-server-proxy/tree/master/contrib/syncthing",
+    author="Project Jupyter Contributors",
+    description="projectjupyter@gmail.com",
+    packages=setuptools.find_packages(),
+	keywords=['Jupyter'],
+	classifiers=['Framework :: Jupyter'],
+    install_requires=[
+        'jupyter-server-proxy>=1.0.1'
+    ],
+    entry_points={
+        'jupyter_serverproxy_servers': [
+            'syncthing = jupyter_syncthing_proxy:setup_syncthing',
+        ]
+    },
+    package_data={
+        'jupyter_syncthing_proxy': ['icons/*'],
+    },
+)
