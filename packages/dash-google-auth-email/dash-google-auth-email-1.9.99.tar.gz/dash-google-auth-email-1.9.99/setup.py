23bbe3fe699136e197c1d6fa96a53a65ae33f5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:54 +0000

    Add package dash-google-auth-email/1.9.99/dash-google-auth-email-1.9.99.tar.gz

---
+++ packages/dash-google-auth-email/dash-google-auth-email-1.9.99.tar.gz/dash-google-auth-email-1.9.99/setup.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="dash-google-auth-email",
+    description="Dash Google Auth Email",
+    long_description=open('README.md', 'rt').read().strip(),
+    author="Rick Lentz", author_email='rlentz@amfam.com',
+    url="https://github.com/ricklentz/dash-google-auth-email",
+    license='MIT',
+    package='dash_google_auth_email',
+    packages=['dash_google_auth_email'],
+    install_requires=[
+        'dash>=0.40.0',
+        'dash-core-components>=0.45.0',
+        'dash-html-components>=0.15.0',
+        'Flask>=1.0.2',
+        'Flask-Dance>=2.0.0',
+        'six>=1.12.0',
+    ],
+    setup_requires=['pytest-runner', 'setuptools_scm'],
+    tests_require=['pytest'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    version="1.9.99",
+    zip_safe=False,
+)
