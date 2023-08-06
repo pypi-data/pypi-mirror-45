4e9f53ac63e6148d89e26c61be693f2e32a8cf04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:11 +0000

    Add package XJ-Flask-JWT-Extension/0.0.2/XJ-Flask-JWT-Extension-0.0.2.tar.gz

---
+++ packages/XJ-Flask-JWT-Extension/XJ-Flask-JWT-Extension-0.0.2.tar.gz/XJ-Flask-JWT-Extension-0.0.2/flask_jwt_extended/exceptions.py
@@ -0,0 +1,56 @@
+class JWTExtendedException(Exception):
+    """
+    Base except which all flask_jwt_extended errors extend
+    """
+    pass
+
+
+class JWTDecodeError(JWTExtendedException):
+    """
+    An error decoding a JWT
+    """
+    pass
+
+
+class JWTEncodeError(JWTExtendedException):
+    """
+    An error encoding a JWT
+    """
+    pass
+
+
+class InvalidHeaderError(JWTExtendedException):
+    """
+    An error getting header information from a request
+    """
+    pass
+
+
+class NoAuthHeaderError(JWTExtendedException):
+    """
+    An error getting header information from a request
+    """
+    pass
+
+
+class WrongTokenError(JWTExtendedException):
+    """
+    Error raised when attempting to use a refresh token to access an endpoint
+    or vice versa
+    """
+    pass
+
+
+class RevokedTokenError(JWTExtendedException):
+    """
+    Error raised when a revoked token attempt to access a protected endpoint
+    """
+    pass
+
+
+class FreshTokenRequired(JWTExtendedException):
+    """
+    Error raised when a valid, non-fresh JWT attempt to access an endpoint
+    protected by fresh_jwt_required
+    """
+    pass
