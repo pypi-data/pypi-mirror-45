66360239795bd9e04be5ef678f9545e8384b556b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:38 +0000

    Add package wenku-dl/2019.4.28.4/wenku-dl-2019.4.28.4.tar.gz

---
+++ packages/wenku-dl/wenku-dl-2019.4.28.4.tar.gz/wenku-dl-2019.4.28.4/wenku_dl/__init__.py
@@ -0,0 +1,51 @@
+
+import os
+from aigpy.cmdHelper import myinput, myinputInt
+import aigpy.pathHelper as pathHelper
+
+from wenku_dl.download import Downloader
+from wenku_dl.wenku import WenkuConfig
+
+WENKU_DL_VERSION = '2019.4.28.4'
+
+def setting(cf):
+    print('--------------设置--------------')
+    while True:
+        out = myinput('输出目录:')
+        if not os.path.exists(out):
+            if pathHelper.mkdirs(out) is False:
+                print('[错误] 输入地址不正确!')
+                continue
+        break
+    while True:
+        num = myinputInt('线程数:', 0)
+        if num == 0:
+            print('[错误] 输入数量不正确!')
+            continue
+        break
+    cf.set(out, str(num))
+
+def printMenu(cf):
+    print("====================Wenku-dl========================")
+    print("[输出目录]: " + cf.outputdir)
+    print("[线程数量]: " + cf.threadnum)
+    print("[输入0]  : 退出")
+    print("[输入1]  : 设置")
+    print("[输入url]: 下载链接文档")
+    print("===================================================")
+
+def main(argv=None):
+    cf = WenkuConfig()
+    dl = Downloader(int(cf.threadnum))
+    while True:
+        printMenu(cf)
+        url = myinput('输入:')
+        if url == '0':
+            return
+        if url == '1':
+            setting(cf)
+            dl = Downloader(int(cf.threadnum))
+            continue
+        dl.start(url)
+        
+__all__ = ['main']
---
+++ packages/wenku-dl/wenku-dl-2019.4.28.4.tar.gz/wenku-dl-2019.4.28.4/wenku_dl/wenku.py
@@ -0,0 +1,59 @@
+import os
+import re
+import json
+import requests
+from aigpy import configHelper
+
+class WenkuAPI(object):
+    def __init__(self):
+        pass
+
+    def getDocid(self, inurl):
+        try:
+            return re.findall('view/(.*).html', inurl)[0]
+        except:
+            return None
+    
+    def getDocInfo(self, inurl):
+        docid = self.getDocid(inurl)
+        url   = 'https://wenku.baidu.com/api/doc/getdocinfo?callback=json&doc_id=' + str(docid)
+        data  = requests.get(url).text
+        data  = re.match(r'.*json\((.*)\)', data).group(1)
+        data  = data.encode().decode('unicode_escape')
+        data  = json.loads(data)
+        return data
+
+    def getPngUrls(self, inurl):
+        html  = requests.get(inurl).text
+        urls  = re.search("WkInfo.htmlUrls = '(.*)'", html).group(1)
+        urls  = urls.replace(r'\/', '/')
+        urls  = urls.encode().decode('unicode_escape')
+        urls  = json.loads(urls)
+        array = []
+        for url in urls['png']:
+            array.append(url['pageLoadUrl'])
+        return array
+    
+    def parse(self, inurl):
+        docid   = self.getDocid(inurl)
+        pngurls = self.getPngUrls(inurl)
+        info    = self.getDocInfo(inurl)
+        ret = {'docid': docid, 
+               'pngurls': pngurls,
+               'title': info['docInfo']['docTitle'],
+               'creattime': info['docInfo']['createTime']}
+        return ret
+  
+class WenkuConfig(object):
+    FILE_NAME = "wenku.ini"
+    def __init__(self):
+        self.outputdir   = configHelper.GetValue("base", "outputdir", "./", self.FILE_NAME)
+        self.threadnum   = configHelper.GetValue("base", "threadnum", "3", self.FILE_NAME)
+    
+    def set(self, outputdir=None, threadnum=None):
+        if outputdir is not None:
+            configHelper.SetValue("base", "outputdir", outputdir, self.FILE_NAME)
+            self.outputdir = outputdir
+        if threadnum is not None:
+            configHelper.SetValue("base", "threadnum", threadnum, self.FILE_NAME)
+            self.threadnum = threadnum
