3b32e037a438eaa6aa8349e009b89fcd69964182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:13 +0000

    Add package tenark/0.1.9/tenark-0.1.9.tar.gz

---
+++ packages/tenark/tenark-0.1.9.tar.gz/tenark-0.1.9/tenark/__init__.py
@@ -0,0 +1,7 @@
+from .provider import Provider
+from .arranger import Arranger
+from .resolver import (
+    resolve_arranger, resolve_cataloguer,
+    resolve_provider, resolve_provisioner)
+
+__version__ = '0.1.9'
