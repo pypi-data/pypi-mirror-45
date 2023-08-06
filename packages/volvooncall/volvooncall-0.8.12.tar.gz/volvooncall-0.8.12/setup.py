73d155442bb1ada5242081babf2841b6c130da3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package volvooncall/0.8.12/volvooncall-0.8.12.tar.gz

---
+++ packages/volvooncall/volvooncall-0.8.12.tar.gz/volvooncall-0.8.12/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(
+    name="volvooncall",
+    version="0.8.12",
+    description="Communicate with VOC",
+    url="https://github.com/molobrakos/volvooncall",
+    license="",
+    author="Erik",
+    author_email="error.errorsson@gmail.com",
+    scripts=["voc"],
+    packages=["volvooncall"],
+    install_requires=list(open("requirements.txt").read().strip().split("\n")),
+    extras_require={"console": ["docopt"]},
+)
