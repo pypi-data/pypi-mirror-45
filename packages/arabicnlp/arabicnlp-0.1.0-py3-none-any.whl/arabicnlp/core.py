52dc118d1117ef31e17776a67029a5fe18632c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:11 +0000

    Add package arabicnlp/0.1.0/arabicnlp-0.1.0-py3-none-any.whl

---
+++ packages/arabicnlp/arabicnlp-0.1.0-py3-none-any.whl/arabicnlp/core.py
@@ -0,0 +1,35 @@
+import re
+from .preprocessing import ArabicStemmer
+from .models import tags as _tags
+
+available_models = (
+    ('POST', 'LSTM')
+)
+
+stemmer = ArabicStemmer()
+
+def tokens(text):
+    r = re.compile(r'\w+|[^\w\s]+', re.UNICODE | re.MULTILINE | re.DOTALL)
+    return r.findall(text)
+
+def stem(text):
+    return [stemmer.stem(token) for token in tokens(text)]
+
+
+def tags(text):
+    return _tags(text)
+    
+
+def correct(text):
+    return False
+
+
+def sentiment(text):
+    return False
+
+
+def similarity(text1, text2):
+    return False
+
+def download(name):
+    pass
