80122398e3069c86f8ff3b835d35442329e37cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:02 +0000

    Add package ml-dash/0.1.26/ml-dash-0.1.26.tar.gz

---
+++ packages/ml-dash/ml-dash-0.1.26.tar.gz/ml-dash-0.1.26/ml_dash/client-dist/precache-manifest.848c98279d0663647e92e7ee56f8dc13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7c43143f04efd87a9eaa",
+    "url": "/static/js/main.7c43143f.chunk.js"
+  },
+  {
+    "revision": "7d2ba8c545503c233271",
+    "url": "/static/js/1.7d2ba8c5.chunk.js"
+  },
+  {
+    "revision": "7c43143f04efd87a9eaa",
+    "url": "/static/css/main.4e7f7702.chunk.css"
+  },
+  {
+    "revision": "7d2ba8c545503c233271",
+    "url": "/static/css/1.340ebab9.chunk.css"
+  },
+  {
+    "revision": "92cde8b9742908b11b6888800284e396",
+    "url": "/index.html"
+  }
+];
