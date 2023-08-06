906556cd947b02c0ed366c370c82f0d6cb3aa5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:43 +0000

    Add package eventsflow/0.1.3/eventsflow-0.1.3.tar.gz

---
+++ packages/eventsflow/eventsflow-0.1.3.tar.gz/eventsflow-0.1.3/README.md
@@ -0,0 +1,10 @@
+# eventsflow
+
+[![Build Status](https://travis-ci.org/eventsflow/eventsflow.svg?branch=master)](https://travis-ci.org/eventsflow/eventsflow)
+
+Simple library and console tool for building data processing pipelines, https://eventsflow.github.io/eventsflow/
+
+## Development
+
+For local testing please be sure that [drone-cli](https://github.com/drone/drone-cli) installed
+
---
+++ packages/eventsflow/eventsflow-0.1.3.tar.gz/eventsflow-0.1.3/setup.py
@@ -0,0 +1,44 @@
+
+import os
+
+from codecs import open
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), 'r', 'utf-8') as handle:
+    readme = handle.read()
+
+
+def build_package_names(path):
+
+    packages = [path, ]
+    for root, dirs, files in os.walk(path):
+        packages.extend(['.'.join([root, d]) for d in dirs])
+    return packages
+
+
+setup(
+    name='eventsflow',
+    version='0.1.3',
+    description="Events Flow",
+    long_description=readme,
+    packages=build_package_names('eventsflow'),
+    python_requires='>=3.5',
+    install_requires=[
+        'jinja2==2.10.1',
+        'pyyaml==5.1',
+    ],
+    entry_points={
+        'console_scripts': ['eventsflow=eventsflow.cli:launch_new_instance']
+    },
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+    ]
+)
