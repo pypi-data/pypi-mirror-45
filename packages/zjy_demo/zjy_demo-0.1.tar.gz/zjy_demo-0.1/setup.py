67141eef92fcedc4bf1e038f3a308be1df50cced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:58 +0000

    Add package zjy_demo/0.1/zjy_demo-0.1.tar.gz

---
+++ packages/zjy_demo/zjy_demo-0.1.tar.gz/zjy_demo-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='zjy_demo',
+    version='0.1',
+    packages=['zjy_pack', 'zjy_pack2'],
+    description='This is my demo test',
+    author='zjy',
+    author_email='331182615@qq.com',
+    zip_safe=False,
+    license='MIT'
+)
+
