9855dc87e93e4e11e6cabc6ac76c0693bb734215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:17 +0000

    Add package renfe-cli/1.0.1/renfe-cli-1.0.1.tar.gz

---
+++ packages/renfe-cli/renfe-cli-1.0.1.tar.gz/renfe-cli-1.0.1/CHANGELOG.md
@@ -0,0 +1,18 @@
+<!--
+ Copyright 2019 gerardcastillolasheras. All rights reserved.
+ Use of this source code is governed by a BSD-style
+ license that can be found in the LICENSE file.
+-->
+
+# Changelog
+
+## v1.0.1 (2019-05-04)
+
+* Travis CI integration
+* Travis CI tests working within Windows, OSX and Linux
+* Readme update
+
+## v1.0.0 (2019-05-03)
+
+* Creation of python package.
+* Test commands.
---
+++ packages/renfe-cli/renfe-cli-1.0.1.tar.gz/renfe-cli-1.0.1/setup.py
@@ -0,0 +1,49 @@
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+def read(*parts):
+    return codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), *parts), 'r').read()
+
+long_description = read('README.md')
+
+setup(
+    name='renfe-cli',
+    version='1.0.1',
+    description='Get faster RENFE Spanish Trains timetables in your terminal',
+    long_description=long_description,
+    keywords='Get faster RENFE Spanish Trains timetables terminal',
+    author='Gerard Castillo',
+    author_email='gerardcl@gmail.com',
+    url='https://github.com/gerardcl/renfe-cli',
+    license='BSD',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    py_modules=['renfe-cli'],
+    include_package_data=True,
+    install_requires=[
+        'setuptools', 'lxml', 'numpy', 'pandas', 'python-dateutil', 'pytz', 'six'
+    ],
+    entry_points="""
+        [console_scripts]
+        renfe-cli = renfe.cli:main
+        """,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Topic :: Terminals',
+        'Topic :: Text Processing :: Markup :: HTML',
+    ],
+    tests_require=['nose'],
+    test_suite = 'nose.collector',
+)
