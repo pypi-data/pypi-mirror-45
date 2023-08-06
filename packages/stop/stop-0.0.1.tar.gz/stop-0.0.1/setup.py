d60d6734b3ffa787bcc2c92efd5a1062011ce188
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:43 +0000

    Add package stop/0.0.1/stop-0.0.1.tar.gz

---
+++ packages/stop/stop-0.0.1.tar.gz/stop-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stop",
+    version="0.0.1",
+    author="Daniel Wendon-Blixrud",
+    author_email="d@nielwb.com",
+    description="Scratch in Python, and conversion",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dantechguy/stop",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
