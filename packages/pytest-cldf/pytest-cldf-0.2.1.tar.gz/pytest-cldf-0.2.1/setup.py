f26602de49b189d3c79650651b3d6e2263d85651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:15 +0000

    Add package pytest-cldf/0.2.1/pytest-cldf-0.2.1.tar.gz

---
+++ packages/pytest-cldf/pytest-cldf-0.2.1.tar.gz/pytest-cldf-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='pytest-cldf',
+    version='0.2.1',
+    description="Easy quality control for CLDF datasets using pytest",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=['pytest_cldf'],
+    install_requires=[
+        'attrs>=18.2',  # upgrade to the new baseline within our packages.
+        'pytest>=3.6',
+        'pycldf>=1.6.1',
+    ],
+    entry_points={
+        'pytest11': [
+            'pytest_cldf = pytest_cldf.plugin',
+        ]
+    },
+    classifiers=[
+        'Framework :: Pytest',
+    ],
+)
