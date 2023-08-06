fedf8d04f6f71e9d1d72c1be4fde538d4ee367dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:07 +0000

    Add package wikitextparser/0.25.1/wikitextparser-0.25.1.tar.gz

---
+++ packages/wikitextparser/wikitextparser-0.25.1.tar.gz/wikitextparser-0.25.1/tests/test_wikilink.py
@@ -0,0 +1,146 @@
+﻿"""Test the wikilink.py module."""
+
+
+from unittest import main, TestCase
+
+from wikitextparser import WikiLink
+
+
+class TestWikiLink(TestCase):
+
+    """Test WikiLink functionalities."""
+
+    def test_basic(self):
+        wl = WikiLink('[[a]]')
+        self.assertEqual("WikiLink('[[a]]')", repr(wl))
+
+    def test_wikilink_target_text(self):
+        wl = WikiLink('[[A | faf a\n\nfads]]')
+        self.assertEqual('A ', wl.target)
+        self.assertEqual(' faf a\n\nfads', wl.text)
+
+    def test_set_target(self):
+        wl = WikiLink('[[A | B]]')
+        wl.target = ' C '
+        self.assertEqual('[[ C | B]]', wl.string)
+        del wl.target
+        self.assertEqual('[[ B]]', wl.string)
+        del wl.target
+        self.assertEqual('[[]]', wl.string)
+        wl = WikiLink('[[A]]')
+        wl.target = ' C '
+        self.assertEqual('[[ C ]]', wl.string)
+
+    def test_text_settter(self):
+        ae = self.assertEqual
+        wl = WikiLink('[[A | B]]')
+        wl.text = ' C '
+        ae('[[A | C ]]', wl.string)
+        with self.assertWarns(DeprecationWarning):
+            wl.text = None
+        ae('[[A ]]', wl.string)
+        with self.assertWarns(DeprecationWarning):
+            wl.text = None
+        ae('[[A ]]', wl.string)
+
+    def test_test_deleter(self):
+        ae = self.assertEqual
+        wl = WikiLink('[[t|x]]')
+        del wl.text
+        ae(wl.string, '[[t]]')
+        del wl.text
+        ae(wl.string, '[[t]]')
+
+    def test_set_text_when_there_is_no_text(self):
+        wl = WikiLink('[[ A ]]')
+        self.assertEqual(wl.text, None)
+        wl.text = ' C '
+        self.assertEqual('[[ A | C ]]', wl.string)
+
+    def test_dont_confuse_pipe_in_target_template_with_wl_pipe(self):
+        wl = WikiLink('[[ {{text|target}} | text ]]')
+        self.assertEqual(' {{text|target}} ', wl.target)
+        self.assertEqual(' text ', wl.text)
+
+    def test_tricks(self):
+        """Test unsupported wikilink tricks.
+
+        Currently WikiLink.text returns the piped text literally and does not
+        expand these tricks (which by the way do not always work as expected).
+        """
+        # Pipe trick
+        # Note that pipe trick does not work in ref or gallery tags (T4700),
+        # also not with links that have anchors, or edit summery links; see:
+        # https://en.wikipedia.org/wiki/Help:Pipe_trick#Where_it_doesn't_work
+        # https://en.wikipedia.org/wiki/Help:Pipe_trick
+        self.assertEqual(WikiLink('[[L|]]').text, '')
+        # Slash trick
+        # https://en.wikipedia.org/wiki/Help:Pipe_trick#Slash_trick
+        self.assertEqual(WikiLink('[[/Subpage/]]').text, None)
+        # Reverse pipe trick (depends on page title)
+        # https://en.wikipedia.org/wiki/Help:Pipe_trick#Reverse_pipe_trick
+        self.assertEqual(WikiLink('[[|t]]').text, 't')
+
+    def test_fragment_getter(self):
+        ae = self.assertEqual
+        ae(WikiLink('[[a<!--#1-->#<!--#2-->f|x]]').fragment, '<!--#2-->f')
+        ae(WikiLink('[[a<!--#1-->#<!--#2-->f]]').fragment, '<!--#2-->f')
+        ae(WikiLink('[[{{#if:||t}}#{{#if:||s}}|x]]').fragment, '{{#if:||s}}')
+        ae(WikiLink('[[{{#if:||t}}#{{#if:||s}}]]').fragment, '{{#if:||s}}')
+        ae(WikiLink('[[t|x]]').fragment, None)
+        ae(WikiLink('[[t]]').fragment, None)
+        ae(WikiLink('[[t|#]]').fragment, None)
+        ae(WikiLink('[[t#|x]]').fragment, '')
+        ae(WikiLink('[[t#]]').fragment, '')
+
+    def test_fragment_setter(self):
+        ae = self.assertEqual
+        # no frag, no pipe
+        wl = WikiLink('[[a]]')
+        wl.fragment = 'b'
+        ae(wl.string, '[[a#b]]')
+
+        # frag, no pipe
+        wl.fragment = 'c'
+        ae(wl.string, '[[a#c]]')
+
+        # frag, pipe
+        wl.text = ''  # [[a#c|]]
+        wl.fragment = 'd'
+        ae(wl.string, '[[a#d|]]')
+
+        # no frag, pipe
+        del wl.fragment
+        wl.fragment = 'e'
+        ae(wl.string, '[[a#e|]]')
+
+        # no frag after pipe
+        wl = WikiLink('[[a|#]]')
+        wl.fragment = 'f'
+        ae(wl.string, '[[a#f|#]]')
+
+    def test_fragment_deleter(self):
+        ae = self.assertEqual
+        wl = WikiLink('[[a]]')
+        del wl.fragment
+        ae(wl.string, '[[a]]')
+
+        wl = WikiLink('[[a#]]')
+        del wl.fragment
+        ae(wl.string, '[[a]]')
+
+        wl = WikiLink('[[a|]]')
+        del wl.fragment
+        ae(wl.string, '[[a|]]')
+
+        wl = WikiLink('[[a#|]]')
+        del wl.fragment
+        ae(wl.string, '[[a|]]')
+
+        wl = WikiLink('[[a|#]]')
+        del wl.fragment
+        ae(wl.string, '[[a|#]]')
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/wikitextparser/wikitextparser-0.25.1.tar.gz/wikitextparser-0.25.1/wikitextparser/_section.py
@@ -0,0 +1,76 @@
+﻿"""Define the Section class."""
+
+
+from regex import compile as regex_compile, MULTILINE
+
+from ._wikitext import WS, SubWikiText
+
+
+HEADER_MATCH = regex_compile(rb'(={1,6})[^\n]+?\1[ \t]*$', MULTILINE).match
+
+
+class Section(SubWikiText):
+
+    """Section class is used to represent page sections."""
+
+    @property
+    def level(self) -> int:
+        """Return level of this section.
+
+        Level is in range(1,7) or 0 for the lead section.
+        """
+        m = HEADER_MATCH(self._shadow)
+        if m:
+            return len(m[1])
+        return 0
+
+    @level.setter
+    def level(self, value: int) -> None:
+        """Change level of this section."""
+        old_level = self.level
+        title = self.title
+        new_equals = '=' * value
+        self[0:old_level + len(title) + old_level] =\
+            new_equals + title + new_equals
+
+    @property
+    def title(self) -> str:
+        """Return title of this section. Return '' for lead sections."""
+        level = self.level
+        if level == 0:
+            return ''
+        lf = self._shadow.find(10)
+        if lf == -1:
+            return self(0, None).rstrip(WS)[level:-level]
+        return self(0, lf).rstrip(WS)[level:-level]
+
+    @title.setter
+    def title(self, value: str) -> None:
+        """Set the new title for this section and update self.lststr."""
+        level = self.level
+        if level == 0:
+            raise RuntimeError(
+                "Can't set title for a lead section. "
+                "Try adding it to contents.")
+        title = self.title
+        self[level:level + len(title)] = value
+
+    @property
+    def contents(self) -> str:
+        """Return contents of this section."""
+        if self.level == 0:
+            return self(0, None)
+        lf = self._shadow.find(10)
+        if lf == -1:
+            return ''
+        return self(lf + 1, None)
+
+    @contents.setter
+    def contents(self, value: str) -> None:
+        """Set value as the contents of this section."""
+        level = self.level
+        if level == 0:
+            self[:] = value
+            return
+        start = level + len(self.title) + level + 1
+        self[start:start + len(self.contents)] = value
