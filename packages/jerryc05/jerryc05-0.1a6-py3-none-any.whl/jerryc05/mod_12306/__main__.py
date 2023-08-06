6375b73fe6338c1c32e83e0e4b531a7b67386f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:16 +0000

    Add package jerryc05/0.1a6/jerryc05-0.1a6-py3-none-any.whl

---
+++ packages/jerryc05/jerryc05-0.1a6-py3-none-any.whl/jerryc05/mod_12306/__main__.py
@@ -0,0 +1,29 @@
+def main(args: list = []):
+    from jerryc05.mod_12306.train_station import parse
+    keyword: str = args[0].lower() if args else ''
+    while not keyword or not 96 < ord(keyword[0]) < 123:
+        keyword = input('Keywords for train station? ').lower()
+
+    station: list = parse(keyword)
+    if not station:
+        station=[('','','','--- NO RESULT! ---','','')]
+    station.sort(key=lambda x: x[3])
+    print('+--------------------+-----+------+-------------+')
+    print('|    STATION NAME    |     |  ID  |     CHN     |')
+    print('+--------------------+-----+------+-------------+')
+    for x in station:
+        print(f'| {x[3]:18} | {x[2]:3} | {x[5]:4} | {x[1]:{11-len(x[1])+x[1].count(" ")}} |')
+        # print(
+        #     f'| {x[3]:14} | {x[2]:3} | {x[5]:4} | {x[1].replace(" ", chr(12288)):{chr(12288)}<4}  |')
+
+    print('+--------------------+-----+------+-------------+')
+
+    # from getpass import getpass
+    # print(
+    #     f'\nYour password is hidden when typing!\nPassword = {getpass("Type password here (hidden): ")}')
+
+
+if __name__ == "__main__":
+    from sys import path
+    path.insert(0, '.')
+    main(['b'])
