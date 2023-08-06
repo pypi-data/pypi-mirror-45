afd336671d4cf93388ae1ad28efcaaa1e6040f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:45 +0000

    Add package mrldb/0.0.1/mrldb-0.0.1.tar.gz

---
+++ packages/mrldb/mrldb-0.0.1.tar.gz/mrldb-0.0.1/mrldb/__init__.py
@@ -0,0 +1,5 @@
+from .db_types import *
+from .mrl_cluster import *
+##version#start##
+__version__='0.0.1'
+##version#end##
---
+++ packages/mrldb/mrldb-0.0.1.tar.gz/mrldb-0.0.1/mrldb/type_sqlite.py
@@ -0,0 +1,85 @@
+from .struct import get_struct_init
+import threading
+from queue import Queue
+class MrlDBSqlite:
+    def __init__(self, file, structure=None, autocommit=0):
+
+        self.structure=structure
+        self._config={"system":"sqlite", "file": file}
+        self.db=sqlite_dbthread(file)
+        if autocommit!=0: self.autocommit=autocommit(self.db, autocommit)
+        return
+    def insert(self, table, data):
+        def frmt(d):
+            return f"'{d}'" if isinstance(d, str) else str(d)
+        return self.db.execute(f"INSERT INTO {table} ({', '.join([frmt(x) for x in data.keys()])}) VALUES ({', '.join([frmt(x) for x in data.values()])})")
+    def update(table, data, conds=None):
+        def frmt(d):
+            return f"'{d}'" if isinstance(d, str) else str(d)
+        return self.db.execute(f"UPDATE {table} SET {', '.join([key+'='+frmt(arg) for key, arg in data.items()])}{' WHERE '+conds if conds!=None else ''}")
+    def select(table, columns, conds=None):
+        if self.structure!=None:
+            if columns=="*":columns=self.structure[table].keys()
+            return [
+            {_col:_var for _col, _var in zip(columns, record)}
+            for record in
+            self.db.execute(f"SELECT {'*' if columns=='*' else ', '.join(columns)} FROM {table}{' WHERE '+conds if conds!=None else ''}")
+            ]
+        else:
+            return self.db.execute(f"SELECT {'*' if columns=='*' else ', '.join(columns)} FROM {table}{' WHERE '+conds if conds!=None else ''}")
+    def _getinfos(self):
+        return self._config
+    def init(self):
+        [self.db.execute(command) for command in get_struct_init(self.structure)]
+        return self
+
+class sqlite_dbthread(threading.Thread):
+    def __init__(self, db):
+        threading.Thread.__init__(self)
+        self.db=db
+        self.status=True
+        self.reqs=Queue()
+        self.start()
+    def run(self):
+        import sqlite3
+        cnx = sqlite3.Connection(self.db)
+        cursor = cnx.cursor()
+        while self.status:
+            req, arg, res = self.reqs.get()
+            if req=='--close--': break
+            elif req=='--commit--': cnx.commit()
+            cursor.execute(req, arg)
+            if res:
+                for rec in cursor:
+                    res.put(rec)
+                res.put('--no more--')
+        cnx.close()
+    def execute(self, req, arg=None, res=None):
+        self.reqs.put((req, arg or tuple(), res))
+    def select(self, req, arg=None):
+        res=Queue()
+        self.execute(req, arg, res)
+        while True:
+            rec=res.get()
+            if rec=='--no more--': break
+            yield rec
+    def commit(self):
+    	sql.execute("--commit--")
+    def close(self):
+        self.execute('--close--')
+    def stop(self):
+        self.status=False
+class sqlite_autocommit(threading.Thread):
+    def __init__(self, sql, t):
+        self._stopevent = threading.Event( )
+        self.sql=sql
+        self.timing=t
+        self.start()
+    def run(self):
+        lastnbr=0
+        idn=0
+        while not self._stopevent.isSet():
+            self._stopevent.wait(self.timing)
+            self.sql.commit()
+    def stop(self):
+        self._stopevent.set( )
