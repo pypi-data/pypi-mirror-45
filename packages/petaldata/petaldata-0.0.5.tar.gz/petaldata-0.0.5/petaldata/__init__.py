080a9e3d8854981bd35321e394c43d86bf2e45d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:13 +0000

    Add package petaldata/0.0.5/petaldata-0.0.5.tar.gz

---
+++ packages/petaldata/petaldata-0.0.5.tar.gz/petaldata-0.0.5/petaldata/__init__.py
@@ -0,0 +1,19 @@
+# import numpy
+# import pandas
+
+# Configuration variables
+
+api_key = None
+api_base = "https://petaldata.app/api"
+cache_dir = None
+
+# API Resources
+
+from petaldata.resource import *
+from petaldata.resource import abstract
+from petaldata.resource import hubspot
+from petaldata.resource import stripe
+from petaldata.resource.stripe.reports import *
+
+
+from petaldata.storage import *
