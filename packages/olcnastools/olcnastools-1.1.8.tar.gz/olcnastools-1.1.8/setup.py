9480ddcaf85f80605a901bd1351056fde17ad2f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:02 +0000

    Add package olcnastools/1.1.8/olcnastools-1.1.8.tar.gz

---
+++ packages/olcnastools/olcnastools-1.1.8.tar.gz/olcnastools-1.1.8/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="olcnastools",
+    version="1.1.8",
+    packages=find_packages(),
+    author="Adam Koziol, Andrew Low, Forest Dussault",
+    author_email="adam.koziol@canada.ca",
+    url="https://github.com/OLC-Bioinformatics/OLC_NAS_Tools",  # link to the repo
+    scripts=['nastools/nastools.py'],
+    install_requires=['olctools']
+)
