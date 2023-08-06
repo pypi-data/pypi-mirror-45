c43edbd98108a6da0c8ba63c351c6737a7de034e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:08 +0000

    Add package pymangle/0.9.1/pymangle-0.9.1.tar.gz

---
+++ packages/pymangle/pymangle-0.9.1.tar.gz/pymangle-0.9.1/setup.py
@@ -0,0 +1,43 @@
+import distutils
+from distutils.core import setup, Extension, Command
+import os
+import numpy
+
+
+ext=Extension("pymangle._mangle", ["pymangle/_mangle.c",
+                                   "pymangle/mangle.c",
+                                   "pymangle/cap.c",
+                                   "pymangle/polygon.c",
+                                   "pymangle/pixel.c",
+                                   "pymangle/point.c",
+                                   "pymangle/stack.c",
+                                   "pymangle/rand.c"],
+              include_dirs=[numpy.get_include()])
+              
+
+exec(open('pymangle/version.py').read())
+
+description="Simple python code to read and work with Mangle masks."
+
+dname=os.path.dirname(__file__)
+rstname=os.path.join(dname,'README.rst')
+if os.path.exists(rstname):
+    desc_file=rstname
+else:
+    desc_file=os.path.join(dname,'README.md')
+
+with open(desc_file) as fobj:
+    long_description=fobj.read()
+
+
+
+setup(name="pymangle", 
+      packages=['pymangle'],
+      description=description,
+      long_description=long_description,
+      version=__version__,
+      license = "GPL",
+      ext_modules=[ext],
+      include_dirs=numpy.get_include())
+
+
