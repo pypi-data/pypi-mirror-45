dae44b62d31e155215fd9aa5e627e36f68de5544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:19 +0000

    Add package joysticks/0.1.3/joysticks-0.1.3.tar.gz

---
+++ packages/joysticks/joysticks-0.1.3.tar.gz/joysticks-0.1.3/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='joysticks',
+    version='0.1.3',
+    license='MIT',
+    author='Matt Donahoe',
+    author_email='matt.donahoe@gmail.com',
+    description='Parse joystick events in python',
+    packages=setuptools.find_packages(),
+)
