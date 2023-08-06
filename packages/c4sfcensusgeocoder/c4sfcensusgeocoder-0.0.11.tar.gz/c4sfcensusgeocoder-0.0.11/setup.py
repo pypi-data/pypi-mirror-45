b02a718e0b1e418673cc98457f2389158a31edde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:13 +0000

    Add package c4sfcensusgeocoder/0.0.11/c4sfcensusgeocoder-0.0.11.tar.gz

---
+++ packages/c4sfcensusgeocoder/c4sfcensusgeocoder-0.0.11.tar.gz/c4sfcensusgeocoder-0.0.11/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="c4sfcensusgeocoder",
+    version="0.0.11",
+    author="Data Science Working Group - Code for San Francisco",
+    author_email="datascience@codeforsanfrancisco.org",
+    description="Censusgeocoder",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sfbrigade/c4sf-censusgeocoder",
+    install_requires=[
+        "censusgeocode",
+        "pandas",
+        "click",
+    ],
+    py_modules=[
+        "cli",
+    ],
+    entry_points='''
+        [console_scripts]
+        c4sfcensusgeocoder=c4sfcensusgeocoder.cli:main
+    ''',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
