4c0b494bdd1573ba9997ca745564be84771e3992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:14 +0000

    Add package dissertate/0.0.9/dissertate-0.0.9.tar.gz

---
+++ packages/dissertate/dissertate-0.0.9.tar.gz/dissertate-0.0.9/dissertate/__init__.py
@@ -0,0 +1,15 @@
+__title__ = "dissertate"
+__author__ = "John Bjorn Nelson (generativist)"
+__email__ = "jbn@abreka.com"
+__description__ = "A package and CLI for writing dissertations."
+__uri__ = "https://github.com/jbn/dissertate"
+__doc__ = __description__ + " <" + __uri__ + ">"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2017 John Bjorn Nelson"
+__version__ = "0.0.9"
+
+
+def markdown_template_path():
+    import os
+    self_dir = os.path.dirname(os.path.realpath(__file__))
+    return os.path.abspath(os.path.join(self_dir, "dissertate.tpl"))
