b00e4c5ea420ac178316877da9e4658e59e9a149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:39 +0000

    Add package robotstatuschecker/1.4/robotstatuschecker-1.4.tar.gz

---
+++ packages/robotstatuschecker/robotstatuschecker-1.4.tar.gz/robotstatuschecker-1.4/README.rst
@@ -0,0 +1,201 @@
+StatusChecker
+=============
+
+.. contents::
+   :local:
+
+Introduction
+------------
+
+StatusChecker is a tool for validating that executed `Robot Framework`_
+test cases have expected statuses and log messages. It is mainly useful
+for Robot Framework test library developers who want to use Robot
+Framework to also test their libraries. StatusChecker 1.3 and newer are
+compatible both with Python 2 and Python 3.
+
+StatusChecker project is hosted at GitHub_ and downloads are at
+PyPI_.
+
+.. _Robot Framework: http://robotframework.org
+.. _GitHub: https://github.com/robotframework/statuschecker
+.. _PyPI: https://pypi.python.org/pypi/robotstatuschecker
+.. _pip: http://pip-installer.org
+
+Installation instructions
+-------------------------
+
+The easiest way to install StatusChecker is by using pip_::
+
+    pip install robotstatuschecker
+
+Alternatively you can get the code by cloning the project from
+GitHub_ or downloading the source distribution from PyPI_ and
+extracting it. After that you can install the tool with::
+
+    python setup.py install
+
+Usage
+-----
+
+From the command line::
+
+    python -m robotstatuschecker infile [outfile]
+
+Programmatically:
+
+.. sourcecode:: python
+
+    from robotstatuschecker import process_output
+
+    process_output('infile.xml', 'outfile.xml')
+
+If an output file is not given, the input file is edited in place.
+
+Defining expected test status
+-----------------------------
+
+By default, all test cases are expected to *PASS* and have no
+message. Changing the expected status to *FAIL* is done by having
+the word ``FAIL`` (in uppercase) somewhere in the test case
+documentation. The expected error message must then follow
+the ``FAIL`` marker.
+
+If a test is expected to *PASS* with a certain message, the word
+``PASS`` must be added to its documentation explicitly and the
+expected message given after that.
+
+The expected message can also be specified as a regular expression by
+prefixing it with ``REGEXP:``. The specified regular expression
+must match the error message fully. Having spaces between the status,
+the message and the possible regular expression prefix is optional.
+
+An alternative to using regular expressions is using glob patterns where
+``*`` matches anything (including newline) and ``?`` matches any single
+character. This is can be accomplished by starting the expected message
+with ``GLOB:``.
+
+Finally, it is possible to test that the message starts with something
+by prefixing the expected message with ``STARTS:``.
+
+The following examples illustrate different ways to define test
+statuses and messages:
+
+.. sourcecode:: robotframework
+
+    *** Test Cases ***
+    Simple failure
+        [Documentation]    FAIL Expected error message
+        Steps
+
+    Exclude documentation before marker
+        [Documentation]    This text is ignored FAIL Expected error message
+        Steps
+
+    Regexp example
+        [Documentation]    FAIL REGEXP: (IOError|OSError): .*
+        Steps
+
+    Glob example
+        [Documentation]    FAIL GLOB: ??Error: *
+        Steps
+
+    Start example
+        [Documentation]    FAIL STARTS: IOError:
+        Steps
+
+    Passing without message
+        Steps
+
+    Passing with message
+        [Documentation]    PASS Expected message
+        Steps
+
+Defining expected log messages
+------------------------------
+
+The expected keyword log messages can also be defined in the test case
+documentation using a syntax such as::
+
+   LOG x.y:z LEVEL Actual message
+
+The part before the colon specifies the keyword to check. For
+example, ``1`` means first keyword, ``1.2`` is the second child
+keyword of the first keyword, and so on.
+
+The part after the colon species the message. For example, ``1:2``
+means the second message of the first keyword and ``1.2:3`` is
+the third message of the second child keyword of the first keyword.
+The message index is optional and defaults to ``1``.
+
+Message level is specified before the actual message, and it can be
+any of the valid log levels in capital letters. If the level is not
+given it defaults to ``INFO``. Starting from 1.4 release also
+``ERROR`` level is supported.
+
+Possible leading and trailing whitespace is ignored both in the expected
+and in the actual log message.
+
+This syntax can be used multiple times to test multiple messages.  It
+also works together with specifying the expected error message with
+``FAIL``, but it that case ``FAIL`` and the expected error must
+be first.
+
+It is also possible to give the message as a regular expression or glob
+pattern or to give just the start of the message. This is accomplished
+by prefixing the message with ``REGEXP:``, ``GLOB:`` or ``STARTS:``,
+respectively, exactly like when `defining expected test status`_.
+
+Finally, to check that a keyword does not have a certain message, it
+is possible to use ``NONE`` in the place of the message.
+
+.. sourcecode:: robotframework
+
+    *** Test cases ***
+    Simple example
+        [Documentation]    LOG 1        Hello, world!
+        Steps
+
+    Nested keywords
+        [Documentation]    LOG 2.1      1st child of 2nd kw
+        Steps
+
+    Message index
+        [Documentation]    LOG 2:2      2nd msg of 2nd kw
+        Steps
+
+    Nested and index
+        [Documentation]    LOG 3.1:2    2nd msg of 3rd kw's 1st child
+        Steps
+
+    Log levels
+        [Documentation]    LOG 2        DEBUG Debug-level message
+        ...                LOG 1.2:3    WARN Warning
+        Steps
+
+    Multiple messages
+        [Documentation]    LOG 1        First tested message
+        ...                LOG 1.2      Second tested message
+        ...                LOG 2.2.1    DEBUG Third tested message
+        Steps
+
+    Status and log
+        [Documentation]    FAIL         Expected error message
+        ...                LOG 1.2      Expected log message
+        Steps
+
+    Regexp message
+        [Documentation]    LOG 1        REGEXP: (Hello|Hi) world!
+        Steps
+
+    Glob message
+        [Documentation]    LOG 1        GLOB: * world!
+        Steps
+
+    Start of the message
+        [Documentation]    LOG 1        STARTS: Hello w
+        Steps
+
+    No message
+        [Documentation]    LOG 1:1      Test that we have only 1 msg
+        ...                LOG 1:2      NONE
+        Steps
---
+++ packages/robotstatuschecker/robotstatuschecker-1.4.tar.gz/robotstatuschecker-1.4/robotstatuschecker.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python
+
+#  Copyright 2008-2016 Nokia Networks
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+"""Robot Framework test status checker
+
+This tool processes Robot Framework output XML files and checks that test case
+statuses and messages are as expected. Main use case is post-processing output
+files got when testing Robot Framework test libraries using Robot Framework
+itself.
+
+The project is hosted at https://github.com/robotframework/statuschecker/.
+See documentation there for the syntax how to specify expected statuses and
+log messages.
+
+Command-line usage:
+
+    python -m robotstatuschecker infile [outfile]
+
+Programmatic usage:
+
+    from robotstatuschecker import process_output
+    process_output('infile.xml', 'outfile.xml')
+
+If an output file is not given, the input file is edited in place.
+"""
+
+from __future__ import print_function
+
+from os.path import abspath
+import re
+import sys
+
+from robot.api import ExecutionResult, ResultVisitor
+from robot.utils import Matcher
+
+
+__version__ = '1.4'
+
+
+def process_output(inpath, outpath=None, verbose=True):
+    """The main programmatic entry point to status checker.
+
+    Args:
+        inpath (str): Path to Robot Framework XML output file to process.
+        outpath (str): Path where to write processed output. If not given,
+            ``inpath`` is edited in place.
+        verbose (bool): When ``True`` (default), prints both ``inpath`` and
+            ``outpath`` to console.
+
+    Returns:
+        int: Number of failed critical tests after post-processing.
+    """
+    if verbose:
+        print('Checking %s' % abspath(inpath))
+    result = StatusChecker().process_output(inpath, outpath)
+    if verbose and outpath:
+        print('Output: %s' % abspath(outpath))
+    return result.return_code
+
+
+class StatusChecker(ResultVisitor):
+
+    def process_output(self, inpath, outpath=None):
+        result = ExecutionResult(inpath)
+        result.suite.visit(self)
+        result.save(outpath)
+        return result
+
+    def visit_test(self, test):
+        expected = Expected(test.doc)
+        if TestStatusChecker(expected).check(test):
+            LogMessageChecker(expected).check(test)
+
+    def visit_keyword(self, kw):
+        pass
+
+
+class Expected(object):
+
+    def __init__(self, doc):
+        self.status = self._get_status(doc)
+        self.message = self._get_message(doc)
+        self.logs = self._get_logs(doc)
+
+    def _get_status(self, doc):
+        return 'FAIL' if 'FAIL' in doc else 'PASS'
+
+    def _get_message(self, doc):
+        if 'FAIL' not in doc and 'PASS' not in doc:
+            return ''
+        status = self._get_status(doc)
+        return doc.split(status, 1)[1].split('LOG', 1)[0].strip()
+
+    def _get_logs(self, doc):
+        return [ExpectedLog(item) for item in doc.split('LOG')[1:]]
+
+
+class ExpectedLog(object):
+
+    def __init__(self, doc):
+        index, message = doc.strip().split(' ', 1)
+        self.kw_index, self.msg_index = self._split_index(index)
+        self.level, self.message = self._split_level(message)
+
+    @property
+    def kw_index_str(self):
+        return '.'.join(str(index + 1) for index in self.kw_index)
+
+    @property
+    def msg_index_str(self):
+        return str(self.msg_index + 1)
+
+    def _split_index(self, index):
+        if ':' in index:
+            kw_index, msg_index = index.split(':')
+        else:
+            kw_index, msg_index = index, 1
+        kw_index = [int(index) - 1 for index in kw_index.split('.')]
+        msg_index = int(msg_index) - 1
+        return kw_index, msg_index
+
+    def _split_level(self, message):
+        for level in ['TRACE', 'DEBUG', 'INFO', 'WARN', 'FAIL', 'ERROR']:
+            if message.startswith(level):
+                return level, message[len(level):].strip()
+        return 'INFO', message
+
+
+class BaseChecker(object):
+
+    def _message_matches(self, actual, expected):
+        if actual == expected:
+            return True
+        if expected.startswith('REGEXP:'):
+            pattern = '^%s$' % expected.replace('REGEXP:', '', 1).strip()
+            if re.match(pattern, actual, re.DOTALL):
+                return True
+        if expected.startswith('GLOB:'):
+            pattern = expected.replace('GLOB:', '', 1).strip()
+            matcher = Matcher(pattern, caseless=False, spaceless=False)
+            if matcher.match(actual):
+                return True
+        if expected.startswith('STARTS:'):
+            start = expected.replace('STARTS:', '', 1).strip()
+            if actual.startswith(start):
+                return True
+        return False
+
+    def _assert(self, condition, test, message):
+        if not condition:
+            return self._fail(test, message)
+        return True
+
+    def _fail(self, test, message):
+        test.status = 'FAIL'
+        self._set_message(test, message)
+        return False
+
+    def _pass(self, test, message):
+        test.status = 'PASS'
+        self._set_message(test, message)
+        return True
+
+    def _set_message(self, test, message):
+        if test.message:
+            original = '\n\nOriginal message:\n%s' % test.message
+        else:
+            original = ''
+        test.message = message + original
+
+
+class TestStatusChecker(BaseChecker):
+
+    def __init__(self, expected):
+        self.status = expected.status
+        self.message = expected.message
+
+    def check(self, test):
+        if self._check_status(test):
+            return self._check_message(test)
+
+    def _check_status(self, test):
+        condition = test.status == self.status
+        message = ('Test was expected to %s but it %sED.'
+                   % (self.status, test.status))
+        return self._assert(condition, test, message)
+
+    def _check_message(self, test):
+        if not self._message_matches(test.message, self.message):
+            message = 'Wrong message.\n\nExpected:\n%s' % self.message
+            return self._fail(test, message)
+        if test.status == 'FAIL':
+            return self._pass(test, 'Test failed as expected.')
+        return True
+
+
+class LogMessageChecker(BaseChecker):
+
+    def __init__(self, expected):
+        self.logs = expected.logs
+
+    def check(self, test):
+        for expected in self.logs:
+            kw = self._get_keyword(test, expected)
+            if kw:
+                self._check_message(test, kw, expected)
+
+    def _get_keyword(self, test, expected):
+        kw = None
+        try:
+            for index in expected.kw_index:
+                kw = (kw or test).keywords[index]
+            return kw
+        except IndexError:
+            message = "No keyword with index '%s'." % expected.kw_index_str
+            self._fail(test, message)
+            return None
+
+    def _check_message(self, test, kw, expected):
+        try:
+            msg = kw.messages[expected.msg_index]
+        except IndexError:
+            condition = expected.message == 'NONE'
+            message = (
+                "Keyword '%s' (index %s) does not have message %s."
+                % (kw.name, expected.kw_index_str, expected.msg_index_str))
+            self._assert(condition, test, message)
+        else:
+            if self._check_msg_level(test, kw, msg, expected):
+                self._check_msg_message(test, kw, msg, expected)
+
+    def _check_msg_level(self, test, kw, msg, expected):
+        condition = msg.level == expected.level
+        message = ("Keyword '%s' (index %s) message %s has wrong level.\n\n"
+                   "Expected: %s\nActual: %s"
+                   % (kw.name, expected.kw_index_str, expected.msg_index_str,
+                      expected.level, msg.level))
+        return self._assert(condition, test, message)
+
+    def _check_msg_message(self, test, kw, msg, expected):
+        condition = self._message_matches(msg.message.strip(), expected.message)
+        message = ("Keyword '%s' (index %s) message %s has wrong content.\n\n"
+                   "Expected:\n%s\n\nActual:\n%s"
+                   % (kw.name, expected.kw_index_str, expected.msg_index_str,
+                      expected.message, msg.message))
+        return self._assert(condition, test, message)
+
+
+if __name__ == '__main__':
+    if '-h' in sys.argv or '--help' in sys.argv:
+        print(__doc__)
+        sys.exit(251)
+    try:
+        rc = process_output(*sys.argv[1:])
+    except TypeError:
+        print(__doc__)
+        sys.exit(252)
+    sys.exit(rc)
