7f9dbeedb9810542ea37a40ac1b66ed3bd7a2469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:40 +0000

    Add package simplabel/0.1.3/simplabel-0.1.3.tar.gz

---
+++ packages/simplabel/simplabel-0.1.3.tar.gz/simplabel-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(name='simplabel',
+      version='0.1.3',
+      description='Simple tool to manually label images in disctinct categories to build training datasets.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/hlgirard/Simplabel',
+      author='Henri-Louis Girard',
+      author_email='hl.girard@gmail.com',
+      license='GPLv3',
+      packages=find_packages(exclude=["tests.*", "tests"]),
+      install_requires=[
+          'pillow',
+      ],
+      entry_points={
+          'console_scripts': [
+              'simplabel = simplabel.simplabel:main',
+              'flow_to_directory = simplabel.flow_to_dict:main',
+          ],
+      },
+      zip_safe=False)
