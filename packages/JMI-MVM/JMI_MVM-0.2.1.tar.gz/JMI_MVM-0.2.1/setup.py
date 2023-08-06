2aa55bc56b95ee66a3d4fcbb827e748ec1bb8d10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:49 +0000

    Add package JMI-MVM/0.2.1/JMI_MVM-0.2.1.tar.gz

---
+++ packages/JMI-MVM/JMI_MVM-0.2.1.tar.gz/JMI_MVM-0.2.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="JMI_MVM",
+    version="0.2.1",
+    author="James M. Irving, Michael V. Moravetz",
+    author_email="james.irving.phd@outlook.com",
+    description="A collection of our functions and classes from bootcamp. ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jirvingphd/JMI_MVM",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=['numpy','pandas','seaborn','matplotlib','sklearn','pydotplus','scipy','xgboost'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
