a73f138d4fb48fe5fc0f30f2c9dea8b77b686ff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:05 +0000

    Add package cortexpy/0.46.4/cortexpy-0.46.4.tar.gz

---
+++ packages/cortexpy/cortexpy-0.46.4.tar.gz/cortexpy-0.46.4/src/cortexpy/constants.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class EdgeTraversalOrientation(Enum):
+    original = 0
+    reverse = 1
+
+    @classmethod
+    def other(cls, orientation):
+        if orientation == cls.original:
+            return cls.reverse
+        return cls.original
+
+
+class EngineTraversalOrientation(Enum):
+    original = 0
+    reverse = 1
+    both = 2
