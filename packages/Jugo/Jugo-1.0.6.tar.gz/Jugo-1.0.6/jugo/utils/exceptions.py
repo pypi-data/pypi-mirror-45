fafedc2e0c3323e3299f3e908310b5c57a2e85f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:48 +0000

    Add package Jugo/1.0.6/Jugo-1.0.6.tar.gz

---
+++ packages/Jugo/Jugo-1.0.6.tar.gz/Jugo-1.0.6/jugo/utils/exceptions.py
@@ -0,0 +1,23 @@
+class ItemNotInFieldset(Exception):
+    def __init__(self, field, model):
+        self.field = field
+        self.model = model
+    
+    def __str__(self):
+        return f"Unknown field {self.field} in {self.model}"
+
+class UsedReservedVariable(Exception):
+    def __init__(self, var, model):
+        self.var = var
+        self.model = model
+    
+    def __str__(self):
+        return f"Variable {self.var} in {self.model} is RESERVED, please use another one"
+
+class UsedExceptedSymbol(Exception):
+    def __init__(self, field, model):
+        self.field = field
+        self.model = model
+    
+    def __str__(self):
+        return f"Field {self.field} in {self.model} has '__', you cant use it"
