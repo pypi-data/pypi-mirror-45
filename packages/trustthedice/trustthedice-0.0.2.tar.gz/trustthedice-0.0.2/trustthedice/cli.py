cc4ce6e83797f23f89e534df0f6d6a4cc55efbad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:29 +0000

    Add package trustthedice/0.0.2/trustthedice-0.0.2.tar.gz

---
+++ packages/trustthedice/trustthedice-0.0.2.tar.gz/trustthedice-0.0.2/trustthedice/cli.py
@@ -0,0 +1,107 @@
+import random
+
+from functools import wraps
+
+import click
+
+from . import exceptions, lib
+
+
+# TODO: make this configurable?
+PROJECT_DIR = ".trustthedice"
+
+
+class ProbableOutcomeParamType(click.ParamType):
+    name = "ProbableOutcome"
+
+    def convert(self, value, param, ctx):
+        try:
+            return lib.parse_probable_outcome(value)
+        except exceptions.BaseError as e:
+            self.fail(e.title(), param, ctx)
+
+
+def handle_errors_nicely(func):
+    """This attempts to run some code, and deal (nicely) with any exception
+
+    In particular, for our own exceptions, we can print a suggestion that helps
+    """
+
+    @wraps(func)
+    def wrapped(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except click.ClickException as e:
+            # Let click handle this, it'll show the relevant erroe message and
+            # exit with a non-zero code.
+            raise
+        except:
+            click.echo("unknown error")
+            # TODO: don't just blindly raise! Deal with it better
+            raise
+
+    return wrapped
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+@click.option(
+    "--ignore-existing/--no-ignore-existing",
+    default=False,
+    help="If a project already exists, ignore it rather than throwing an error",
+)
+def init(ignore_existing):
+    project_dir = PROJECT_DIR
+    lib.initialise(project_dir, ignore_existing=ignore_existing)
+
+
+@main.group()
+def events():
+    pass
+
+
+@events.command("save")
+@click.argument("name", type=str)
+@click.option(
+    "--outcome", "-oc", "outcomes", multiple=True, type=ProbableOutcomeParamType()
+)
+@click.option("--otherwise", type=str, default="")
+@click.option("--overwrite/--no-overwrite", default=False)
+@handle_errors_nicely
+def save_random_event(name, outcomes, otherwise, overwrite):
+    project_dir = PROJECT_DIR
+
+    all_outcomes = lib.calculate_cumulative_probabilities(
+        outcomes, remainder_name=otherwise
+    )
+    random_event = lib.RandomEvent(name=name, outcomes=all_outcomes)
+    lib.save_random_event(project_dir, random_event, overwrite)
+
+
+@main.command("random")
+@click.option(
+    "--outcome", "-oc", "outcomes", multiple=True, type=ProbableOutcomeParamType()
+)
+@click.option("--otherwise", type=str, default="")
+@click.option("--from-saved", "saved_event_name", type=str, default="")
+@handle_errors_nicely
+def pick_random_outcome(outcomes, otherwise, saved_event_name):
+    if saved_event_name:
+        if outcomes or otherwise:
+            raise exceptions.CantHaveOutcomesAndSavedEventError()
+
+        project_dir = PROJECT_DIR
+        event = lib.load_random_event(project_dir, saved_event_name)
+        outcomes = event.outcomes
+    else:
+        outcomes = lib.calculate_cumulative_probabilities(
+            outcomes, remainder_name=otherwise
+        )
+    value = random.random()
+    chosen_outcome = lib.pick_outcome(value, outcomes)
+
+    click.echo(chosen_outcome.name)
---
+++ packages/trustthedice/trustthedice-0.0.2.tar.gz/trustthedice-0.0.2/trustthedice/lib.py
@@ -0,0 +1,205 @@
+from fractions import Fraction
+from os import makedirs, path
+from typing import List
+
+from attr import attrs, attrib
+
+from . import exceptions, serialise
+
+
+@attrs
+class ProbableOutcome(serialise.Serialisable):
+    """A probable outcome has a name and a probability.
+    """
+
+    name: str = attrib()
+    probability: Fraction = attrib()
+
+    def to_simple_list(self):
+        return [self.name, self.probability.numerator, self.probability.denominator]
+
+    @classmethod
+    def from_simple_list(cls, simple_list):
+        if not isinstance(simple_list, list) or len(simple_list) != 3:
+            raise exceptions.SerialisationError(
+                f"Expected a list [str, int, int] but got {simple_list}"
+            )
+        [name, num, den] = simple_list
+        return ProbableOutcome(name, Fraction(num, den))
+
+
+@attrs
+class RandomEvent(serialise.Serialisable):
+    """A random event has a name, and several probable outcomes.
+    """
+
+    name: str = attrib()
+    outcomes: List[ProbableOutcome] = attrib(factory=list)
+
+    def to_simple_list(self):
+        return [self.name, [oc.to_simple_list() for oc in self.outcomes]]
+
+    @classmethod
+    def from_simple_list(cls, simple_list):
+        if not isinstance(simple_list, list) or len(simple_list) != 2:
+            raise exceptions.SerialisationError(
+                f"Expected a list [str, list] but got {simple_list}"
+            )
+        [name, raw_outcomes] = simple_list
+        outcomes = [
+            ProbableOutcome.from_simple_list(raw_outcome)
+            for raw_outcome in raw_outcomes
+        ]
+        return RandomEvent(name, outcomes)
+
+
+def parse_probable_outcome(outcome_string):
+    """Parse a probable outcome from a string.
+
+    >>> parse_probable_outcome("Win: 1 in 10")
+    ProbableOutcome(name='Win', probability=Fraction(1, 10))
+    """
+    parts = [part.strip() for part in outcome_string.split(":")]
+    if len(parts) != 2:
+        raise exceptions.InvalidProbableOutcomeStringError()
+    name = parts[0]
+
+    number_parts = [part.strip() for part in parts[1].split("in")]
+    if len(parts) != 2:
+        raise exceptions.InvalidProbableOutcomeStringError()
+
+    try:
+        numbers = [int(part) for part in number_parts]
+    except (TypeError, ValueError) as e:
+        raise exceptions.InvalidProbableOutcomeStringError(e)
+
+    probability = Fraction(numbers[0], numbers[1])
+
+    return ProbableOutcome(name=name, probability=probability)
+
+
+def calculate_cumulative_probabilities(outcomes, remainder_name=None):
+    """Return a new set of outcomes, but with probabilities made cumulative.
+
+    I.e. if we have a list of outcomes, and each has its own individual
+    probability, return a new list of outcomes where the probabilities are:
+    [prob0, prob0 + prob1, prob0 + prob1 + prob2, ...]
+
+    This way, selecting a random value is as simple as choosing a number
+    between 0 and 1, then finding the first outcome whose (cumulative)
+    probability is greater than our value.
+
+    If a remainder_name is given, then a probable-outcome is added at the end
+    that has a probability of 1 (to catch any values beyond the final probable
+    outcome)
+    """
+    result = []
+
+    one = Fraction(1, 1)
+    current_total = Fraction(0, 1)
+    for outcome in outcomes:
+        current_total += outcome.probability
+        if current_total > one:
+            raise exceptions.TotalProbabilityMoreThanOneError()
+        result.append(ProbableOutcome(name=outcome.name, probability=current_total))
+
+    if current_total == one:
+        if remainder_name:
+            raise exceptions.RedundantRemainderError()
+    else:
+        if remainder_name:
+            result.append(ProbableOutcome(name=remainder_name, probability=one))
+        else:
+            # We have a gap between our current total and 1.0
+            # Any values within there will have no probabilities, and that's bad.
+            raise exceptions.TotalProbabilityLessThanOneError()
+
+    return result
+
+
+def pick_outcome(value, outcomes):
+    """Pick the first outcome that has a probability that is greater than the given value.
+
+    >>> o1 = ProbableOutcome(name="a", probability=Fraction(1, 3))
+    >>> o2 = ProbableOutcome(name="b", probability=Fraction(2, 3))
+    >>> o3 = ProbableOutcome(name="c", probability=Fraction(3, 3))
+    >>> outcomes = [o1, o2, o3]
+
+    Anything less than (or equal to) 1/3 will give us a...
+    >>> pick_outcome(0.001, outcomes).name
+    'a'
+    >>> pick_outcome(0.333, outcomes).name
+    'a'
+    >>> pick_outcome(Fraction(1, 3), outcomes).name
+    'a'
+
+    Between 1/3 and 2/3 gives us 'b'
+    >>> pick_outcome(0.4, outcomes).name
+    'b'
+    """
+    for outcome in outcomes:
+        if value <= outcome.probability:
+            return outcome
+    raise exceptions.CouldntPickOutcomeError()
+
+
+def initialise(project_dir, ignore_existing=None):
+    if path.exists(project_dir) and not ignore_existing:
+        raise exceptions.ProjectAlreadyExistsError(project_dir)
+
+    makedirs(project_dir, exist_ok=True)
+
+    events_filename = path.join(project_dir, "random_events")
+    if not path.exists(events_filename):
+        with open(events_filename, "w") as out:
+            out.write("")
+
+
+def _get_and_assert_filename(project_dir, relative_filename):
+    if not path.isdir(project_dir):
+        raise exceptions.ProjectNotInitialisedError()
+
+    full_filename = path.join(project_dir, relative_filename)
+    if not path.isfile(full_filename):
+        raise exceptions.ProjectCorruptedError()
+    return full_filename
+
+
+def load_random_events(project_dir):
+    filename = _get_and_assert_filename(project_dir, "random_events")
+    with open(filename, "r") as input_file:
+        return list(serialise.read_many(input_file, RandomEvent))
+
+
+def load_random_event(project_dir, desired_event_name):
+    # TODO: loading everything into memory just to get one item is ok as long
+    # as we only have a few events.
+    for event in load_random_events(project_dir):
+        if event.name == desired_event_name:
+            return event
+    raise exceptions.RandomEventDoesntExistError()
+
+
+def save_random_event(project_dir, random_event, overwrite=None):
+
+    # TODO: this is not the most efficient way of doing things, but with only
+    # a handful of events it doesn't matter that much.
+    event_with_same_name = None
+    all_other_events = []
+
+    for existing_event in load_random_events(project_dir):
+        if existing_event.name == random_event.name:
+            event_with_same_name = existing_event
+        else:
+            all_other_events.append(existing_event)
+
+    if event_with_same_name is not None and not overwrite:
+        raise exceptions.RandomEventExistsError()
+
+    filename = _get_and_assert_filename(project_dir, "random_events")
+
+    events_to_write = all_other_events + [random_event]
+
+    with open(filename, "w") as output_file:
+        for event in events_to_write:
+            serialise.write(event, output_file)
