6fd13b611b2f8c2ecc066dc5f6c9d1a3225551d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:10 +0000

    Add package turicreate/5.5/turicreate-5.5-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/turicreate/turicreate-5.5-cp27-cp27mu-manylinux1_x86_64.whl/turicreate/version_info.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2017 Apple Inc. All rights reserved.
+#
+# Use of this source code is governed by a BSD-3-clause license that can
+# be found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
+'''
+Obtain the version number of the current turicreate create.
+'''
+from __future__ import print_function as _
+from __future__ import division as _
+from __future__ import absolute_import as _
+
+# python egg version
+__version__ = '5.5'#{{VERSION_STRING}}
+version = __version__
+build_number = '67446'#{{BUILD_NUMBER}}
+git_sha = '89db46b913c91e1e5ecc0e019115ad3c88ebee28'#{{GIT_SHA}}
