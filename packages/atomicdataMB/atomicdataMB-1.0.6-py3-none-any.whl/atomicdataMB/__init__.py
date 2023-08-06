2c5b3762d5d16ad9c9defb747d24e037d42b5176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:12 +0000

    Add package atomicdataMB/1.0.6/atomicdataMB-1.0.6-py3-none-any.whl

---
+++ packages/atomicdataMB/atomicdataMB-1.0.6-py3-none-any.whl/atomicdataMB/__init__.py
@@ -0,0 +1,13 @@
+from .g_values import make_gvalue_table, RadPresConst, gValue
+from .make_photo_table import make_photo_table
+from .initialize_atomicdata import initialize_atomicdata
+from .atomicmass import atomicmass
+
+
+name = 'atomicdataMB'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
+__version__ = '1.0.6'
+
+# Verify the database is setup correctly
+initialize_atomicdata()
