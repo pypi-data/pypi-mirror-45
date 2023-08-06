afb4b35e24f2017de5365d46484db3e5fece507c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:38 +0000

    Add package pyvdr/0.1/pyvdr-0.1.tar.gz

---
+++ packages/pyvdr/pyvdr-0.1.tar.gz/pyvdr-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyvdr',
+      version='0.1',
+      description='The pyvdr joke in the world',
+      url='http://github.com/baschno/pyvdr',
+      author='Flying Circus',
+      author_email='bastian.schnorbus@googlemail.com',
+      license='MIT',
+      packages=['pyvdr'],
+      zip_safe=False)
