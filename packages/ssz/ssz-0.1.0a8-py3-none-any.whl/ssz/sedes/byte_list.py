d36fc51af3ddb0b37c608bac7d2e678bcccfdc9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:00 +0000

    Add package ssz/0.1.0a8/ssz-0.1.0a8-py3-none-any.whl

---
+++ packages/ssz/ssz-0.1.0a8-py3-none-any.whl/ssz/sedes/byte_list.py
@@ -0,0 +1,35 @@
+from typing import (
+    Union,
+)
+
+from ssz.sedes.base import (
+    CompositeSedes,
+)
+from ssz.utils import (
+    merkleize,
+    mix_in_length,
+    pack_bytes,
+)
+
+BytesOrByteArray = Union[bytes, bytearray]
+
+
+class ByteList(CompositeSedes[BytesOrByteArray, bytes]):
+    is_static_sized = False
+
+    def get_static_size(self):
+        raise ValueError("byte list has no static size")
+
+    def serialize_content(self, value: BytesOrByteArray) -> bytes:
+        return value
+
+    def deserialize_content(self, content: bytes) -> bytes:
+        return content
+
+    def hash_tree_root(self, value: bytes) -> bytes:
+        merkle_leaves = pack_bytes(value)
+        merkleized = merkleize(merkle_leaves)
+        return mix_in_length(merkleized, len(value))
+
+
+byte_list = ByteList()
