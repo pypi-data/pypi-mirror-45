c2043b5e85ae21195de98e6ca6fd6c8d6f9b8c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:07 +0000

    Add package Flask-AuthOOB/0.0.5/Flask_AuthOOB-0.0.5-py2.py3-none-any.whl

---
+++ packages/Flask-AuthOOB/Flask_AuthOOB-0.0.5-py2.py3-none-any.whl/flask_authoob/email_provider.py
@@ -0,0 +1,25 @@
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+
+class SendGridEmailProvider:
+    def __init__(self, app):
+        self.apikey = app.config.get("SENDGRID_API_KEY", None)
+        self.sender = app.config.get("EMAIL_SENDER")
+        if self.apikey is None:
+            raise Exception(
+                "Missing SENDGRID_API_KEY configuration for sendgrid email provider"
+            )
+
+    def send_mail(self, to_emails=None, subject=None, html=None):
+        message = Mail(
+            from_email=self.sender,
+            to_emails=to_emails,
+            subject=subject,
+            html_content=html,
+        )
+        try:
+            SendGridAPIClient(self.apikey).send(message)
+        except Exception as e:
+            print(e)
