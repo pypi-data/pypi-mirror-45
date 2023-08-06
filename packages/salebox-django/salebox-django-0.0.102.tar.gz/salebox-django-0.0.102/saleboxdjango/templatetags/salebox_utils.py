70dbf21615595583af85d2965126ca79b40d6abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:35 +0000

    Add package salebox-django/0.0.102/salebox-django-0.0.102.tar.gz

---
+++ packages/salebox-django/salebox-django-0.0.102.tar.gz/salebox-django-0.0.102/saleboxdjango/templatetags/salebox_utils.py
@@ -0,0 +1,23 @@
+from math import modf
+
+from django import template
+from django.contrib.humanize.templatetags.humanize import intcomma
+from django.utils.safestring import mark_safe
+
+
+register = template.Library()
+
+@register.filter(is_safe=True)
+def sb_currency(value):
+    minor, major = modf(value / 100)
+    return mark_safe('%s<span>.%s</span>' % \
+        (intcomma(int(major)), '{:.2f}'.format(minor)[2:]))
+
+@register.filter(is_safe=True)
+def sb_currency_text(value):
+    minor, major = modf(value / 100)
+    return '%s.%s' % (intcomma(int(major)), '{:.2f}'.format(minor)[2:])
+
+@register.filter(is_safe=True)
+def sb_trans(obj, field):
+    return obj.get_trans(field)
