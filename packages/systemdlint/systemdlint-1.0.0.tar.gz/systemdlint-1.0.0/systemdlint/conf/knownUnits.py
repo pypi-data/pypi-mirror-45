2d0deb30d118a8796bcda7e32f69c22e8cade472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:26 +0000

    Add package systemdlint/1.0.0/systemdlint-1.0.0.tar.gz

---
+++ packages/systemdlint/systemdlint-1.0.0.tar.gz/systemdlint-1.0.0/systemdlint/conf/knownUnits.py
@@ -0,0 +1,20 @@
+KNOWN_UNITS_EXT = [
+    ".automount",
+    ".conf",
+    ".link",
+    ".mount",
+    ".network",
+    ".path",
+    ".service",
+    ".slice",
+    ".socket",
+    ".swap",
+    ".target",
+    ".timer"
+]
+KNOWN_UNITS_MUST_HAVE_UNITSECTION = [
+    ".mount",
+    ".service",
+    ".socket",
+    ".target"
+]
