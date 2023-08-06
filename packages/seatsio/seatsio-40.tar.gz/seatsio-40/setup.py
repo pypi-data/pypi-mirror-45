d316f935f1b7d5b98241deea1f51111105282a20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:05 +0000

    Add package seatsio/40/seatsio-40.tar.gz

---
+++ packages/seatsio/seatsio-40.tar.gz/seatsio-40/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='seatsio',
+    version='v40',
+    description='The official Seats.io Python client library',
+    long_description=readme,
+    author='The seats.io dev team',
+    author_email='hello@seats.io',
+    url='https://github.com/seatsio/seatsio-python',
+    license="MIT",
+
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "munch",
+        "jsonpickle",
+        "future",
+        "six"
+    ],
+
+    test_suite='tests',
+    tests_require=['unittest2']
+)
