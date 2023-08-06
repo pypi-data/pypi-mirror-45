f003cea16b8ec049d5c85099cf8900afae9158cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:34 +0000

    Add package django-advancedsearchfilter/0.2.2/django-advancedsearchfilter-0.2.2.tar.gz

---
+++ packages/django-advancedsearchfilter/django-advancedsearchfilter-0.2.2.tar.gz/django-advancedsearchfilter-0.2.2/setup.py
@@ -0,0 +1,39 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+
+requires = [
+    'django',
+    'django-select2',
+]
+
+setup(
+    name='django-advancedsearchfilter',
+    version='0.2.2',
+    description='Advanced Search Filter for Django Contrib Admin',
+    long_description=README,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+    ],
+    author='Wildcard Corp.',
+    author_email='corporate@wildcardcorp.com',
+    url='http://github.com/wildcardcorp/django-advancedsearchfilter',
+    keywords='web django search',
+    packages=find_packages(),
+    license="LICENSE.txt",
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    tests_require=requires,
+    test_suite="",
+    entry_points="",
+)
