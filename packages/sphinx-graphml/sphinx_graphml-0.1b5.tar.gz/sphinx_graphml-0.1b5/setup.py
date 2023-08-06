cb84d08c8a5f5d934c633f1b6dd90514c9e2fe66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:26 +0000

    Add package sphinx-graphml/0.1b5/sphinx_graphml-0.1b5.tar.gz

---
+++ packages/sphinx-graphml/sphinx_graphml-0.1b5.tar.gz/sphinx_graphml-0.1b5/setup.py
@@ -0,0 +1,56 @@
+"""
+sphinx-graphml
+Graphml charts embedding tool for Sphinx
+"""
+
+from setuptools import setup, find_packages
+import os
+
+PACKAGE = 'sphinx_graphml'
+
+# credentials
+AUTHOR = 'Thomas Khyn'
+AUTHOR_EMAIL = 'thomas@ksytek.com'
+URL = 'https://bitbucket.org/tkhyn/sphinx-graphml/'
+
+# imports __version__ and __version_info__ variables
+exec(open('sphinx_graphml/version.py').read())
+
+dev_status = __version_info__[3]
+if dev_status == 'alpha' and not __version_info__[4]:
+    dev_status = 'pre'
+
+DEV_STATUS = {'pre': '2 - Pre-Alpha',
+              'alpha': '3 - Alpha',
+              'beta': '4 - Beta',
+              'rc': '4 - Beta',
+              'final': '5 - Production/Stable'}
+
+setup(
+    name=PACKAGE,
+    version=__version__,
+    description='GraphML charts embedding extension for Sphinx',
+    long_description=open(os.path.join('README.rst')).read(),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    keywords=['sphinx', 'graphml', 'yEd', 'chart'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Development Status :: %s' % DEV_STATUS[dev_status],
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Plugins',
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Documentation :: Sphinx'
+    ],
+    packages=find_packages(),
+    install_requires=(
+        'sphinx>=1.8'
+    ),
+    package_data={PACKAGE: ['static/js/*.*']},
+    include_package_data=True,
+    # static files are not available from zipped directories, so we need to set this flag to False
+    zip_safe=False
+)
---
+++ packages/sphinx-graphml/sphinx_graphml-0.1b5.tar.gz/sphinx_graphml-0.1b5/sphinx_graphml/__init__.py
@@ -0,0 +1,32 @@
+"""
+Extension setup hook
+"""
+
+import os
+
+from .version import __version__
+from .directives import GraphDirective
+from .nodes import graphml_reference, visit_graphml_reference, depart_graphml_reference
+
+
+def setup(app):
+    """
+    Registers directives, roles, etc.
+
+    :param app: the Sphinx app
+    """
+
+    app.add_directive('graphml', GraphDirective)
+    app.add_node(graphml_reference, html=(visit_graphml_reference, depart_graphml_reference))
+
+    app.add_js_file('js/GraphMLViewer.js')
+
+    # modifying raw config is the only way to add a static path from which static files can be
+    # collected, see:
+    # https://github.com/sphinx-doc/sphinx/issues/1379
+    # https://groups.google.com/forum/#!msg/sphinx-users/Z-wcktOhIAc/pGDWO0yVBQAJ
+    app.config._raw_config.setdefault('html_static_path', []).append(
+        os.path.join(os.path.dirname(__file__), 'static')
+    )
+
+    return {'version': __version__}
