808a0cbe4ae8c1684d3a54ab1e71190f69f42f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:51 +0000

    Add package Neodroid/0.0.6/Neodroid-0.0.6-py36-none-any.whl

---
+++ packages/Neodroid/Neodroid-0.0.6-py36-none-any.whl/neodroid/wrappers/formal_wrapper/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'cnheider'
+
+import numpy as np
+
+from .formal_wrapper import NeodroidFormalWrapper
+
+
+def make(environment_name, **kwargs):
+  return NeodroidFormalWrapper(environment_name=environment_name, **kwargs)
+
+
+def seed(s):
+  np.random.random(s)
