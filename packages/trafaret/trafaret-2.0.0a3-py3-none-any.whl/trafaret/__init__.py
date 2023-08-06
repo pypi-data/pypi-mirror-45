f814e3b45c0fcc3fddcf1299d10c08147d8c0912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:51 +0000

    Add package trafaret/2.0.0a3/trafaret-2.0.0a3-py3-none-any.whl

---
+++ packages/trafaret/trafaret-2.0.0a3-py3-none-any.whl/trafaret/__init__.py
@@ -0,0 +1,109 @@
+from .base import (
+    DataError,
+    Trafaret,
+    Call,
+    Or,
+    And,
+    Forward,
+
+    Any,
+    Null,
+    List,
+    Key,
+    Dict,
+    Enum,
+    Tuple,
+
+    Atom,
+    String,
+    AnyString,
+    Bytes,
+    Callable,
+    Bool,
+    Type,
+    Subclass,
+    Mapping,
+    StrBool,
+    DictKeys,
+
+    guard,
+
+    # utils
+    OnError,
+    WithRepr,
+    ensure_trafaret,
+    extract_error,
+    ignore,
+    catch,
+    catch_error,
+)
+from .numeric import (
+    Float,
+    ToFloat,
+    Int,
+    ToInt,
+    ToDecimal,
+)
+from .regexp import Regexp, RegexpRaw
+from .internet import (
+    URL,
+    Email,
+    IPv4,
+    IPv6,
+    IP,
+)
+
+__all__ = (
+    "DataError",
+    "Trafaret",
+    "Call",
+    "Or",
+    "And",
+    "Forward",
+
+    "Any",
+    "Null",
+    "List",
+    "Key",
+    "Dict",
+    "Enum",
+    "Tuple",
+
+    "Atom",
+    "String",
+    "AnyString",
+    "Bytes",
+    "Float",
+    "ToFloat",
+    "Int",
+    "ToInt",
+    "ToDecimal",
+    "Callable",
+    "Bool",
+    "Type",
+    "Subclass",
+    "Mapping",
+    "StrBool",
+    "DictKeys",
+
+    "guard",
+
+    "Regexp",
+    "RegexpRaw",
+    "URL",
+    "Email",
+    "IPv4",
+    "IPv6",
+    "IP",
+
+    "OnError",
+    "WithRepr",
+    "ensure_trafaret",
+    "extract_error",
+    "ignore",
+    "catch",
+    "catch_error",
+)
+
+
+__VERSION__ = (2, 0, '0-alpha.03')
