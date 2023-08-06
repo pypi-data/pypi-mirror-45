8cf1f5318095a5bd335adc5135bf72caa117e050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:22 +0000

    Add package solarsystemMB/1.0.0/solarsystemMB-1.0.0.tar.gz

---
+++ packages/solarsystemMB/solarsystemMB-1.0.0.tar.gz/solarsystemMB-1.0.0/solarsystemMB/__init__.py
@@ -0,0 +1,11 @@
+name = 'solarsystemMB'
+__author__ = 'Matthew Burger'
+__email__ = 'mburger@stsci.edu'
+__version__ = '1.0.0'
+
+
+from .SSObject import SSObject
+from .planet_dist import planet_dist
+from .load_kernels import load_kernels
+from .relative_position import relative_position
+from .planet_geometry import planet_geometry
