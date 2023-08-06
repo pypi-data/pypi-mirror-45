c902798d8a542165e480bcf740bfdf381261df29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:23 +0000

    Add package oceandata/0.2.1/oceandata-0.2.1.tar.gz

---
+++ packages/oceandata/oceandata-0.2.1.tar.gz/oceandata-0.2.1/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['oceandata']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.0,<8.0',
+ 'numpy>=1.16,<2.0',
+ 'pandas>=0.24.1,<0.25.0',
+ 'requests>=2.21,<3.0']
+
+setup_kwargs = {
+    'name': 'oceandata',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Bror Jonsson',
+    'author_email': 'brorfred@gmail.com',
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
