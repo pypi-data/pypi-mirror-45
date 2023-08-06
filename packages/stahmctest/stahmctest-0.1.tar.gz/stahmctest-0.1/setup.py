fe90a97dfdaca3142a3b1901e9bee0b96698be46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:52 +0000

    Add package stahmctest/0.1/stahmctest-0.1.tar.gz

---
+++ packages/stahmctest/stahmctest-0.1.tar.gz/stahmctest-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='stahmctest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='DUKE',
+      packages=['stahmctest'],
+      zip_safe=False)
