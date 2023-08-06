772a3a4035b2609f71a69f943e48db536f7b910a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:56 +0000

    Add package vsm/0.4.15/vsm-0.4.15-cp27-cp27m-win32.whl

---
+++ packages/vsm/vsm-0.4.15-cp27-cp27m-win32.whl/vsm/extensions/corpusbuilders/util.py
@@ -0,0 +1,304 @@
+from builtins import str
+from builtins import chr
+from builtins import range
+from builtins import object
+from past.builtins import basestring
+import re
+import string
+
+from chardet.universaldetector import UniversalDetector
+import numpy as np
+
+__all__ = ['strip_punc', 'rem_num', 'rehyph',
+           'apply_stoplist', 'filter_by_suffix', 'word_tokenize',
+           'sentence_tokenize', 'paragraph_tokenize', 'detect_encoding',
+           'sentence_span_tokenize', 'in_place_stoplist']
+
+
+def _unicode_range(start, stop):
+    return u''.join([chr(i) for i in range(int(start,16), int(stop,16))])
+
+PUNCTUATION_START = re.compile(r'^(['+string.punctuation+r'\u2000-\u206F\u3000-\u303F\uFF00-\uFFFF]*)')
+PUNCTUATION_END = re.compile(r'(['+string.punctuation+r'\u2000-\u206F\u3000-\u303F\uFF00-\uFFFF]*)$')
+PUNC = str(string.punctuation) + _unicode_range('2000','206F') + _unicode_range('3000', '303F') + _unicode_range('FF00', 'FFFF')
+PUNC_TABLE = {ord(c): None for c in PUNC}
+
+def strip_punc(tsent):
+    """
+    """
+    out = []
+    for word in tsent:
+        w = strip_punc_word(word)
+        if w:
+            out.append(w)
+
+    return out
+
+def strip_punc_word(word):
+    if isinstance(word, str):
+        return word.translate(PUNC_TABLE)
+    elif isinstance(word, basestring):
+        return word.translate(None, PUNC.encode('utf-8'))
+        
+
+NUMS = string.digits
+NUMS_TABLE =  {ord(c): None for c in NUMS}
+
+def rem_num(tsent):
+    """
+    """
+    #p = re.compile(r'(^\D+$)|(^\D*[1-2]\d\D*$|^\D*\d\D*$)')
+    out = []
+    for word in tsent:
+        w = rem_num_word(word)
+        if w:
+            out.append(w)
+
+    return out
+
+def rem_num_word(word):
+    if isinstance(word, str):
+        return word.translate(NUMS_TABLE)
+    elif isinstance(word, basestring):
+        return word.translate(None, NUMS.encode('utf-8'))
+
+def rehyph(sent):
+    """
+    """
+    return re.sub(r'(?P<x1>.)--(?P<x2>.)', '\g<x1> - \g<x2>', sent)
+
+
+BIG_TABLE = NUMS_TABLE.copy()
+BIG_TABLE.update(PUNC_TABLE)
+BIG_LIST = string.digits + string.punctuation
+def process_word(word):
+    if isinstance(word, str):
+        return word.translate(BIG_TABLE)
+    elif isinstance(word, basestring):
+        return word.translate(None, BIG_LIST.encode('utf-8'))
+
+def apply_stoplist(corp, nltk_stop=True, add_stop=None, freq=0, in_place=True):
+    """
+    Returns a Corpus object with stop words eliminated.
+
+    :param corp: Corpus object to apply stoplist to.
+    :type corp: Corpus
+
+    :param nltk_stop: If `True` English stopwords from nltk are included
+        in the stoplist. Default is `True`.
+    :type nltk_stop: boolean, optional
+    
+    :param add_stop: list of words to eliminate from `corp` words.
+        Default is `None`.
+    :type add_stop: List, optional
+
+    :param freq: Eliminates words that appear <= `freq` times. Default is
+        0.
+    :type freq: int
+
+    :returns: Corpus with words in the stoplist removed.
+
+    :See Also: :class:`vsm.corpus.Corpus`, :meth:`vsm.corpus.Corpus.apply_stoplist`
+    """
+    stoplist = set()
+    if nltk_stop:
+        import nltk
+        for w in nltk.corpus.stopwords.words('english'):
+            stoplist.add(w)
+    if add_stop:
+        for w in add_stop:
+            stoplist.add(w)
+
+    if not in_place:
+        return corp.apply_stoplist(stoplist=stoplist, freq=freq)
+    else:
+        corp.in_place_stoplist(stoplist=stoplist, freq=freq)
+        return corp
+
+def in_place_stoplist(corp, nltk_stop=True, add_stop=None, freq=0):
+    """
+    Returns a Corpus object with stop words eliminated.
+
+    :param corp: Corpus object to apply stoplist to.
+    :type corp: Corpus
+
+    :param nltk_stop: If `True` English stopwords from nltk are included
+        in the stoplist. Default is `True`.
+    :type nltk_stop: boolean, optional
+    
+    :param add_stop: list of words to eliminate from `corp` words.
+        Default is `None`.
+    :type add_stop: List, optional
+
+    :param freq: Eliminates words that appear <= `freq` times. Default is
+        0.
+    :type freq: int
+
+    :returns: Corpus with words in the stoplist removed.
+
+    :See Also: :class:`vsm.corpus.Corpus`, :meth:`vsm.corpus.Corpus.apply_stoplist`
+    """
+    stoplist = set()
+    if nltk_stop:
+        import nltk
+        for w in nltk.corpus.stopwords.words('english'):
+            stoplist.add(w)
+    if add_stop:
+        for w in add_stop:
+            stoplist.add(w)
+
+    corp.in_place_stoplist(stoplist=stoplist, freq=freq)
+
+
+def filter_by_suffix(l, ignore, filter_dotfiles=True):
+    """
+    Returns elements in `l` that does not end with elements in `ignore`,
+    and filters dotfiles (files that start with '.').
+
+    :param l: List of strings to filter.
+    :type l: list
+
+    :param ignore: List of suffix to be ignored or filtered out.
+    :type ignore: list
+
+    :param filter_dotfiles: Filter dotfiles.
+    :type filter_dotfiles: boolean, default True
+
+    :returns: List of elements in `l` whose suffix is not in `ignore`.
+
+    **Examples**
+
+    >>> l = ['a.txt', 'b.json', 'c.txt']
+    >>> ignore = ['.txt']
+    >>> filter_by_suffix(l, ignore)
+    ['b.json']
+    """
+    filter_list = [e for e in l if not sum([e.endswith(s) for s in ignore])]
+    if filter_dotfiles:
+        filter_list = [e for e in filter_list if not e.startswith('.')]
+    return filter_list
+
+class _tokenizer(object):
+    def tokenize(self, text):
+        return text.split()
+
+word_tokenizer = _tokenizer()
+def word_tokenize(text):
+    """Takes a string and returns a list of strings. Intended use: the
+    input string is English text and the output consists of the
+    lower-case words in this text with numbers and punctuation, except
+    for hyphens, removed.
+
+    The core work is done by NLTK's Treebank Word Tokenizer.
+    
+    :param text: Text to be tokeized.
+    :type text: string
+
+    :returns: tokens : list of strings
+    """
+    global word_tokenizer
+    if word_tokenizer is None:
+        import nltk
+        word_tokenizer = nltk.TreebankWordTokenizer()
+
+    text = rehyph(text)
+    text = process_word(text)
+    if isinstance(text, str):
+        text = text.replace(u'\x00', '')
+    elif isinstance(text, basestring):
+        pass
+    text = text.lower()
+    tokens = word_tokenizer.tokenize(text)
+
+    #process_word = lambda x: strip_punc_word(rem_num_word(word)).lower().replace(u'\x00','')
+    #tokens = [process_word(word) for word in text]
+
+    return tokens
+
+
+sent_tokenizer = None
+def sentence_tokenize(text):
+    """
+    Takes a string and returns a list of strings. Intended use: the
+    input string is English text and the output consists of the
+    sentences in this text.
+
+    This is a wrapper for NLTK's pre-trained Punkt Tokenizer.
+     
+    :param text: Text to be tokeized.
+    :type text: string
+
+    :returns: tokens : list of strings
+    """
+    global sent_tokenizer
+    if sent_tokenizer is None:
+        import nltk
+        sent_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+    return sent_tokenizer.tokenize(text)
+
+def sentence_span_tokenize(text):
+    """
+    Takes a string and returns a list of strings. Intended use: the
+    input string is English text and the output consists of the
+    sentences in this text.
+
+    This is a wrapper for NLTK's pre-trained Punkt Tokenizer.
+     
+    :param text: Text to be tokeized.
+    :type text: string
+
+    :returns: token_spans : iterator of (start,stop) tuples.
+    """
+    global sent_tokenizer
+    if sent_tokenizer is None:
+        import nltk
+        sent_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+    return sent_tokenizer.span_tokenize(text)
+
+
+
+def paragraph_tokenize(text):
+    """
+    Takes a string and returns a list of strings. Intended use: the
+    input string is English text and the output consists of the
+    paragraphs in this text. It's expected that the text marks
+    paragraphs with two consecutive line breaks.
+     
+    :param text: Text to be tokeized.
+    :type text: string
+
+    :returns: tokens : list of strings
+    """
+
+    par_break = re.compile(r'[\r\n]{2,}')
+    span_start = 0
+    for match in par_break.finditer(text):
+        span_end = match.span()[0]
+        yield text[span_start:span_end]
+        span_start = match.span()[1]
+    yield text[span_start:]
+    #return par_break.split(text)
+
+
+def detect_encoding(filename):
+    """
+    Takes a filename and attempts to detect the character encoding of the file
+    using `chardet`.
+     
+    :param filename: Name of the file to process
+    :type filename: string
+
+    :returns: encoding : string
+    """
+    detector = UniversalDetector()
+    with open(filename, 'rb') as unknown_file:
+        for line in unknown_file:
+            detector.feed(line)
+            if detector.done:
+                break
+    detector.close()
+
+    return detector.result['encoding']
+ 
---
+++ packages/vsm/vsm-0.4.15-cp27-cp27m-win32.whl/vsm/model/ldacgsseq.py
@@ -0,0 +1,290 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from builtins import range
+from builtins import object
+
+import numpy as np
+import time
+from vsm.split import split_corpus
+from vsm.corpus import align_corpora as align
+from vsm.model.ldafunctions import *
+from progressbar import ProgressBar, Percentage, Bar
+
+from vsm.model._cgs_update import cgs_update
+
+
+__all__ = [ 'LdaCgsSeq', 'LdaCgsQuerySampler' ]
+
+
+
+class LdaCgsSeq(object):
+    """
+    An implementation of LDA using collapsed Gibbs sampling.
+    """
+    def __init__(self, corpus=None, context_type=None,
+                 K=20, V=0, alpha=[], beta=[], seed=None):
+        """
+        Initialize LdaCgsSeq.
+
+        :param corpus: Source of observed data.
+        :type corpus: `Corpus`
+
+        :param context_type: Name of tokenization stored in `corpus` whose tokens
+            will be treated as documents.
+        :type context_type: string, optional
+
+        :param K: Number of topics. Default is `20`.
+        :type K: int, optional
+
+        :param alpha: Document priors. Default is a flat prior of 0.01
+            for all topics.
+        :type alpha: list, optional
+
+        :param beta: Topic priors. Default is 0.01 for all words.
+        :type beta: list, optional
+
+        :param seed: Seed for numpy's RandomState. Default is `None`.
+        :type seed: int, optional
+        """
+
+        self.context_type = context_type
+        self.K = K
+
+        if corpus:
+            self.V = corpus.words.size
+            self.indices = corpus.view_contexts(self.context_type, as_indices=True)
+            self.corpus = corpus.corpus
+            self.dtype = corpus.corpus.dtype
+        else:
+            self.V = V
+            self.indices = []
+            self.corpus = []
+            self.dtype = None
+
+        if self.K < 2 ** 8:
+            self.Ktype = np.uint8
+        elif self.K < 2 ** 16:
+            self.Ktype = np.uint16
+        else:
+            raise RuntimeError("More than 65536 topics are not supported")
+
+        self.indices = np.array(self.indices, dtype='i')
+        self.Z = np.zeros_like(self.corpus, dtype=self.Ktype)
+
+        priors = init_priors(self.V, self.K, beta, alpha)
+        self.beta, self.alpha = priors
+        self.beta = self.beta.astype(np.float32)
+        self.alpha = self.alpha.astype(np.float32)
+
+        self.word_top = (np.zeros((self.V, self.K), dtype=np.float32)
+                         + self.beta)
+        if self.V==0:
+            self.inv_top_sums = np.inf
+        else:
+            self.inv_top_sums = 1. / self.word_top.sum(0)
+        self.top_doc = (np.zeros((self.K, len(self.indices)),
+                                 dtype=np.float32) + self.alpha)
+
+        self.iteration = 0
+        self.log_probs = []
+
+        if seed is None:
+            maxint = np.iinfo(np.int32).max
+            self.seed = np.random.randint(0, maxint)
+        else:
+            self.seed = seed
+        self._mtrand_state = np.random.RandomState(self.seed).get_state()
+
+    @property
+    def Z_split(self):
+        return split_corpus(self.Z, self.indices)
+
+    @property
+    def docs(self):
+        return split_corpus(self.corpus, self.indices)
+
+
+    def _compute_word_top(self):
+        self.word_top = compute_word_top(self.docs, self.Z_split, self.K,
+                                         self.V, self.beta)
+
+
+    def _compute_top_doc(self):
+        self.top_doc = compute_top_doc(self.Z_split, self.K, self.alpha)
+
+
+    def _compute_log_prob(self, increment=False):
+        log_prob = compute_log_prob(self.docs, self.Z_split,
+                                    self.word_top, self.top_doc)
+        if increment:
+            self.log_probs.append((self.iteration, log_prob))
+            self.iteration += 1
+        else:
+            return log_prob
+    """
+    def train(self, n_iterations=100, verbose=1, **kwargs):
+        return train(self, n_iterations, verbose)
+    """
+    
+
+    @staticmethod
+    def load(filename):
+        """
+        A static method for loading a saved LdaCgsMulti model.
+
+        :param filename: Name of a saved model to be loaded.
+        :type filename: string
+
+        :returns: m : LdaCgsMulti object
+
+        :See Also: :class:`numpy.load`
+        """
+        return load_lda(filename, LdaCgsSeq)
+
+
+    def save(self, filename):
+        """
+        Saves the model in an `.npz` file.
+
+        :param filename: Name of a saved model to be loaded.
+        :type filename: string
+
+        :See Also: :class:`numpy.savez`
+        """
+        save_lda(self, filename)
+
+    def train(self, n_iterations=100, verbose=1, **kwargs):
+        """
+        Takes an optional argument, `n_iterations` and updates the model
+        `n_iterations` times.
+    
+        :param n_iterations: Number of iterations. Default is 100.
+        :type n_iterations: int, optional
+    
+        :param verbose: If 1, current number of iterations
+            are printed out to notify the user. Default is 1.
+        :type verbose: int, optional
+        
+        :param kwargs: For compatability with calls to LdaCgsMulti.
+        :type kwargs: optional
+        """
+        import cython
+        if self.dtype == np.uint16 and self.Ktype == np.uint8:
+            update = cgs_update[cython.ushort,cython.uchar]
+        elif self.dtype == np.uint16 and self.Ktype == np.uint16:
+            update = cgs_update[cython.ushort,cython.ushort]
+        elif self.dtype == np.uint32 and self.Ktype == np.uint8:
+            update = cgs_update[cython.uint,cython.uchar]
+        elif self.dtype == np.uint32 and self.Ktype == np.uint16:
+            update = cgs_update[cython.uint,cython.ushort]
+        else:
+            raise NotImplementedError(
+                "Unsupported corpus dtype ({}) and topic dtype ({}) combination".format(
+                    self.corpus.dtype, self.Ktype))
+    
+        random_state = np.random.RandomState(self.seed)
+        random_state.set_state(self._mtrand_state)
+    
+    
+        if verbose > 0:
+            print ('Begin LDA training for {0} iterations'\
+                   .format(n_iterations))
+            start = time.time()
+            t = start
+    
+        # Training loop
+        stop = self.iteration + n_iterations
+        if verbose == 1:
+            pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=n_iterations).start()
+    
+        #print("Stop ", stop)
+        for itr in range(self.iteration , stop):
+            results = update(self.iteration, self.corpus, self.word_top,
+                                 self.inv_top_sums, self.top_doc, self.Z,
+                                 self.indices, self._mtrand_state[0],
+                                 self._mtrand_state[1], self._mtrand_state[2],
+                                 self._mtrand_state[3], self._mtrand_state[4])
+    
+            lp = np.float32(results[4])
+            self.log_probs.append((self.iteration, lp))
+    
+            if verbose == 2:
+                itr_time = np.around(time.time()-t, decimals=1)
+                t = time.time()
+                if verbose > 1 or itr==stop-1:
+                    print ('\nIteration {0} complete: log_prob={1}, time={2}'
+                           .format(self.iteration, lp, itr_time))
+    
+            if verbose == 1:
+                #print("Self iteration", self.iteration)
+                pbar.update(self.iteration - (stop - n_iterations))
+    
+            self.iteration += 1
+    
+            self._mtrand_state = results[5:]
+        if verbose == 1:
+            pbar.finish();
+        if verbose > 1:
+            print('-'*60, ('\n\nWalltime per iteration: {0} seconds'
+                           .format(np.around((t-start)/n_iterations, decimals=2))))
+
+
+class LdaCgsQuerySampler(LdaCgsSeq):
+    """
+    """
+    def __init__(self, lda_obj=None, new_corpus=None,
+                 align_corpora=False, old_corpus=None,
+                 context_type=None, seed=None,
+                 alpha=[], beta=[]):
+
+        if align_corpora:
+            new_corp = align(old_corpus, new_corpus)
+
+        if lda_obj:
+            if context_type is None:
+                context_type = lda_obj.context_type
+            if not alpha:
+                alpha = lda_obj.alpha
+            if not beta:
+                beta = lda_obj.beta
+
+            kwargs = dict(corpus=new_corpus,
+                          context_type=context_type,
+                          K=lda_obj.K, V=lda_obj.V,
+                          alpha=alpha, beta=beta, seed=seed)
+        else:
+            kwargs = dict(corpus=new_corpus)
+
+
+
+        super(LdaCgsQuerySampler, self).__init__(**kwargs)
+
+        if lda_obj:
+            self.word_top[:] = lda_obj.word_top
+            self.inv_top_sums[:] = lda_obj.inv_top_sums
+
+
+
+#################################################################
+#                            Demos
+#################################################################
+
+
+def demo_LdaCgsSeq(doc_len=500, V=100000, n_docs=100,
+                   K=20, n_iterations=5,
+                   corpus_seed=None, model_seed=None):
+
+    from vsm.extensions.corpusbuilders import random_corpus
+
+    print('Words per document:', doc_len)
+    print('Words in vocabulary:', V)
+    print('Documents in corpus:', n_docs)
+    print('Number of topics:', K)
+    print('Iterations:', n_iterations)
+
+    c = random_corpus(n_docs*doc_len, V, doc_len, doc_len+1, seed=corpus_seed)
+    m = LdaCgsSeq(c, 'document', K=K, seed=model_seed)
+    m.train(n_iterations=n_iterations, verbose=2)
+
+    return m
