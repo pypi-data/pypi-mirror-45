a8144fda8534424889f8f988b74d0edf9f32742b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:08 +0000

    Add package Chronomancy/0.0.2/Chronomancy-0.0.2-py3-none-any.whl

---
+++ packages/Chronomancy/Chronomancy-0.0.2-py3-none-any.whl/chronomancy/__init__.py
@@ -0,0 +1,4 @@
+from .previous_frame import PreviousFrame
+from .arcane_recall import arcane_recall
+
+__version__ = "0.0.2"
---
+++ packages/Chronomancy/Chronomancy-0.0.2-py3-none-any.whl/chronomancy/previous_frame.py
@@ -0,0 +1,6 @@
+""" Class for handling the previous frame in the call stack. """
+
+
+class PreviousFrame(object):
+    def __init__(self):
+        self.locals = None
