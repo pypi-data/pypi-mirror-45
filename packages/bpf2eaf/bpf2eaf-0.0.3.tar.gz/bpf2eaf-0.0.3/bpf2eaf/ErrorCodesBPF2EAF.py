9fbe6b7522aa53ae9e33e1d197cb3f017ea2ae53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:44 +0000

    Add package bpf2eaf/0.0.3/bpf2eaf-0.0.3.tar.gz

---
+++ packages/bpf2eaf/bpf2eaf-0.0.3.tar.gz/bpf2eaf-0.0.3/bpf2eaf/ErrorCodesBPF2EAF.py
@@ -0,0 +1,5 @@
+# 1 = not enough parameters specified on the command line
+# 2 = MAU tier is missing from the BPF file (can not get the timing information -> abort)
+
+ERROR_CODE_NOT_ENOUGH_CMD_ARGUMENTS: int = 1
+ERROR_CODE_MAU_TIER_MISSING: int = 2
---
+++ packages/bpf2eaf/bpf2eaf-0.0.3.tar.gz/bpf2eaf-0.0.3/bpf2eaf/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["bpfFileHandling", "xml_validation", "ErrorCodesBPF2EAF"]
