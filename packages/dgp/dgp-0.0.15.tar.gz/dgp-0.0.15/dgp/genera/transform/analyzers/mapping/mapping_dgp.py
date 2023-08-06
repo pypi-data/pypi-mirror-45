a1cdd89ef0be072af1928e352293c9240c61c6e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:04 +0000

    Add package dgp/0.0.15/dgp-0.0.15.tar.gz

---
+++ packages/dgp/dgp-0.0.15.tar.gz/dgp-0.0.15/dgp/genera/transform/analyzers/mapping/mapping_dgp.py
@@ -0,0 +1,19 @@
+from .....core import BaseDataGenusProcessor
+from .map_guesser import MappingGuesserAnalyzer
+from .mandatory_fields import MandatoryFieldsAnalyzer
+
+from .....config.consts import CONFIG_MODEL_MAPPING, CONFIG_CONSTANTS
+
+
+class MappingDGP(BaseDataGenusProcessor):
+
+    def init(self):
+        self.steps = self.init_classes([
+            MappingGuesserAnalyzer,
+            MandatoryFieldsAnalyzer
+        ])
+
+    def analyze(self):
+        self.config.setdefault(CONFIG_MODEL_MAPPING, [])
+        self.config.setdefault(CONFIG_CONSTANTS, [])
+        return super().analyze()
