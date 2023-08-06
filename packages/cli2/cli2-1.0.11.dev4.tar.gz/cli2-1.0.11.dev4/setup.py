bf0d8c5b57c3b4af719c91a8d39385e7744492e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:59 +0000

    Add package cli2/1.0.11.dev4/cli2-1.0.11.dev4.tar.gz

---
+++ packages/cli2/cli2-1.0.11.dev4.tar.gz/cli2-1.0.11.dev4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name='cli2',
+    version='1.0.11.dev4',
+    setup_requires='setupmeta',
+    install_requires=['colorama'],
+    extras_require=dict(
+        test=[
+            'freezegun',
+        ],
+    ),
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    url='https://yourlabs.io/oss/cli2',
+    include_package_data=True,
+    license='MIT',
+    keywords='cli',
+    python_requires='>=3',
+)
