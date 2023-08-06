6c77c6b48f8e4cd0a0fe225c6d09b7bdb823a01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:30 +0000

    Add package stahmctestt/0.6/stahmctestt-0.6.tar.gz

---
+++ packages/stahmctestt/stahmctestt-0.6.tar.gz/stahmctestt-0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='stahmctestt',
+      version='0.6',
+      description='testversion',
+      url='https://github.com/RihuiOu95/HMC.git',
+      author='Flying Circus',
+      author_email='thuizhou@outlook.com',
+      license='DUKE',
+      packages=['stahmctestt'],
+      py_modules = ['stahmctestt.oned', 'stahmctestt.vec'],
+      zip_safe=False)
---
+++ packages/stahmctestt/stahmctestt-0.6.tar.gz/stahmctestt-0.6/stahmctestt/__init__.py
@@ -0,0 +1,3 @@
+import numpy as np
+import math
+from numba import jit, vectorize, float64, int64
