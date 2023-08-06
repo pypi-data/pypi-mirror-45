7c284724107e7cf00efb2961b53cbc3fc1d6fe62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:55 +0000

    Add package django-cronman/1.1.1/django-cronman-1.1.1.tar.gz

---
+++ packages/django-cronman/django-cronman-1.1.1.tar.gz/django-cronman-1.1.1/cronman/tests/settings.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+from __future__ import unicode_literals
+
+import os
+
+DEBUG = True
+
+SECRET_KEY = "test-secret-key-n8gm2vaj43tqzh3yvbg9x1vj9f3m9mfcq7"
+
+# SQLite:
+DATABASES = {
+    "default": {"NAME": ":memory:", "ENGINE": "django.db.backends.sqlite3"}
+}
+
+INSTALLED_APPS = (
+    "django.contrib.contenttypes",
+    "django.contrib.auth",
+    "django.contrib.admin",
+    "cronman",
+)
+
+USE_I18N = True
+
+LANGUAGE_CODE = "en"
+
+# ROOT_URLCONF = "cronman.tests.urls"
+
+_log_level = os.environ.get("TEST_CRONMAN_LOG_LEVEL", "INFO")
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "loggers": {
+        "django": {"handlers": ["console"], "level": _log_level},
+        "cronman": {"handlers": ["console"], "level": _log_level},
+    },
+}
