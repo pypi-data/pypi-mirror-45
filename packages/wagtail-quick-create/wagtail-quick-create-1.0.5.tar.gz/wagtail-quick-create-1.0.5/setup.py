3f9cb4da09c1393b9f1b763d2972d7a780abf314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:22 +0000

    Add package wagtail-quick-create/1.0.5/wagtail-quick-create-1.0.5.tar.gz

---
+++ packages/wagtail-quick-create/wagtail-quick-create-1.0.5.tar.gz/wagtail-quick-create-1.0.5/setup.py
@@ -0,0 +1,29 @@
+
+#!/usr/bin/env python
+"""
+Installs the Wagtail Quick Create plugin which offers shortcut links on the admin
+home screen to create defined pages under parent pages.
+"""
+
+from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='wagtail-quick-create',
+      version='1.0.5',
+      description='Offer links to the admin user to create content under sections quickly.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/kevinhowbrook/wagtailquickcreate',
+      author='Kevin Howbrook - Torchbox',
+      author_email='kevin.howbrook@torchbox.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+          'wagtail>=2.0',
+      ])
