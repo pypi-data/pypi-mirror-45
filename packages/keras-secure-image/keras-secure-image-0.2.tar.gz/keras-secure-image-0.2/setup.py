093cd0604ca5b6f9eeb26dcb940dd07f4d23112d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:35 +0000

    Add package keras-secure-image/0.2/keras-secure-image-0.2.tar.gz

---
+++ packages/keras-secure-image/keras-secure-image-0.2.tar.gz/keras-secure-image-0.2/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+
+long_description = '''
+This package is an add on library on top of keras or tensorflow.
+The aim of this package is to provide security for encrypting images.
+The model is trained on the encrypted data
+'''
+
+setup(
+  name = 'keras-secure-image',         
+  packages = ['keras_secure_image'],   
+  version = '0.2',      
+  license='MIT',       
+  long_description = long_description,   
+  author = 'Akshay Bahadur',                   
+  author_email = 'akshaybahadur21@gmail.com',      
+  url = 'https://github.com/akshaybahadur21',   
+  download_url = 'https://github.com/akshaybahadur21/keras-secure-image/archive/v0.2.tar.gz',
+  keywords = ['KERAS', 'SECURITY', 'DEEP LEARNING'],   
+  install_requires=[            
+          'numpy',
+          'Pillow',
+		  'scikit-image'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      	
+
+    'Intended Audience :: Developers',      	
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: MIT License',   
+
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
