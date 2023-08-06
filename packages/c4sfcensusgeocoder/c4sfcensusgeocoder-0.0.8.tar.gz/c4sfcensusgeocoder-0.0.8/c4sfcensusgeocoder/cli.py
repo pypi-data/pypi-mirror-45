1495e880b241437631c512733aacd8b1bd90c503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:11 +0000

    Add package c4sfcensusgeocoder/0.0.8/c4sfcensusgeocoder-0.0.8.tar.gz

---
+++ packages/c4sfcensusgeocoder/c4sfcensusgeocoder-0.0.8.tar.gz/c4sfcensusgeocoder-0.0.8/c4sfcensusgeocoder/cli.py
@@ -0,0 +1,29 @@
+import click
+# import batch_geocoder as bg
+
+@click.group()
+def c4sfcensusgeocoder():
+    """Command-line access to C4sf Census Geocoder.
+    Run ``c4sfcensusgeocoder --help`` for a list of available commands.
+    To get help with a specific command, run ``c4sfcensusgeocoder [COMMAND] --help``."""
+    pass  # pragma: no cover
+
+
+@c4sfcensusgeocoder.command(help="Testing Hello World.")
+@click.option(
+    "--test_text",
+    required=True,
+    help='The Text you want to print',
+)
+def hello_world(test_text):
+    """Testing Hello World"""
+    print(test_text)
+
+
+@click.command()
+def main():
+    c4sfcensusgeocoder()
+
+
+if __name__ == '__main__':
+    main()
