c40c566238eabb020aa30888a9a943a39b098fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:33 +0000

    Add package arche/0.3.4/arche-0.3.4.tar.gz

---
+++ packages/arche/arche-0.3.4.tar.gz/arche-0.3.4/src/arche/tools/json_schema_validator.py
@@ -0,0 +1,91 @@
+from collections import defaultdict
+from typing import Any, Dict, Deque
+
+from arche.readers.schema import Schema
+import fastjsonschema
+from jsonschema import FormatChecker, validators
+import pandas as pd
+from tqdm import tqdm_notebook
+
+
+class JsonSchemaValidator:
+    def __init__(self, schema: Schema):
+        self.errors = defaultdict(set)
+        self.schema = schema
+
+    def run(self, df: pd.DataFrame, fast: bool) -> None:
+        # itertuples renames columns starting with `_`
+        df = df.copy()
+        df.columns = df.columns.str.replace("^_+", "ud")
+        if fast:
+            self.fast_validate(df)
+        else:
+            self.validate(df)
+
+    def fast_validate(self, df: pd.DataFrame) -> None:
+        """Verify items one by one. It stops after the first error in an item in most cases.
+        Faster than jsonschema validation"""
+        validate = fastjsonschema.compile(self.schema)
+        for row in tqdm_notebook(
+            df.itertuples(index=False), desc="JSON Schema Validation"
+        ):
+            item = row._asdict()
+            try:
+                validate(item)
+            except fastjsonschema.JsonSchemaException as error:
+                self.errors[str(error)].add(item["udkey"])
+
+    def validate(self, df: pd.DataFrame) -> None:
+        validator = validators.validator_for(self.schema)(self.schema)
+        validator.format_checker = FormatChecker()
+        for item in tqdm_notebook(
+            df.itertuples(index=False), desc="JSON Schema Validation"
+        ):
+            self.validate_item(item._asdict(), validator)
+
+    def validate_item(self, item: Dict[str, Any], validator) -> None:
+        """Check a single item against jsonschema
+
+        Args:
+            item: a dict with item data
+            validator: a validator instance
+        """
+        for e in validator.iter_errors(item):
+            error = self.format_validation_message(
+                e.message, e.path, e.schema_path, e.validator
+            )
+            self.errors[error].add(item["udkey"])
+
+    @staticmethod
+    def format_validation_message(
+        error_msg: str, path: Deque, schema_path: Deque, validator: str
+    ) -> str:
+        str_path = "/".join(p for p in path if type(p) is str)
+        schema_path = "/".join(p for p in schema_path)
+
+        if validator == "anyOf":
+            if str_path:
+                return f"'{str_path}' does not satisfy 'schema/{schema_path}'"
+            else:
+                return f"'schema/{schema_path}' failed"
+
+        if "Additional properties are not allowed" in error_msg:
+            return error_msg[: error_msg.index(" (")]
+
+        if not str_path:
+            return error_msg
+
+        for path_message in [
+            "is not of type",
+            "does not match",
+            "is not one of",
+            "is not a",
+            "is greater than the maximum of",
+            "is less than the minimum of",
+            "is too long",
+            "is too short",
+        ]:
+            if path_message in error_msg:
+                return f"{str_path} {error_msg[error_msg.index(path_message) :]}"
+
+        return f"{str_path} - {error_msg}"
---
+++ packages/arche/arche-0.3.4.tar.gz/arche-0.3.4/tests/readers/test_items.py
@@ -0,0 +1,105 @@
+from arche import SH_URL
+from arche.readers.items import Items, CollectionItems, JobItems
+from conftest import Collection, Job
+import pandas as pd
+import pytest
+
+
+@pytest.mark.parametrize(
+    "name, expected_name", [("address", "address"), ("address_0", "address")]
+)
+def test_get_origin_column_name(get_job_items, name, expected_name):
+    items = get_job_items
+    items._columns_map = {"address_0": "address"}
+    assert items.get_origin_column_name(name) == expected_name
+
+
+collection_items = pd.DataFrame(
+    [
+        {"_key": "0", "name": "Book"},
+        {"_key": "1", "name": "Movie"},
+        {"_key": "2", "name": "Guitar"},
+        {"_key": "3", "name": "Dog"},
+    ]
+)
+expected_col_items = pd.DataFrame(
+    [
+        {"_key": f"{SH_URL}/key/0", "name": "Book"},
+        {"_key": f"{SH_URL}/key/1", "name": "Movie"},
+        {"_key": f"{SH_URL}/key/2", "name": "Guitar"},
+        {"_key": f"{SH_URL}/key/3", "name": "Dog"},
+    ]
+)
+
+
+@pytest.mark.parametrize(
+    "df, expected_df",
+    [(pd.DataFrame({"0": [0]}), pd.DataFrame({"0": [0], "_key": ["0"]}))],
+)
+def test_items_from_df(df, expected_df):
+    items = Items.from_df(df)
+    pd.testing.assert_frame_equal(items.df, expected_df)
+
+
+@pytest.mark.parametrize(
+    "count, filters, expand, expected_count",
+    [(1, None, False, 1), (None, None, True, 4)],
+)
+def test_collection_items(mocker, count, filters, expand, expected_count):
+    mocker.patch(
+        "arche.tools.api.get_collection",
+        return_value=Collection(len(collection_items)),
+        autospec=True,
+    )
+    get_items_mock = mocker.patch(
+        "arche.tools.api.get_items",
+        return_value=collection_items.copy().iloc[:expected_count],
+        autospec=True,
+    )
+    items = CollectionItems("key", count, filters, expand)
+    assert items.key == "key"
+    assert items.filters == filters
+    assert items.expand == expand
+    pd.testing.assert_frame_equal(items.df, expected_col_items.iloc[:expected_count])
+    pd.testing.assert_frame_equal(items.flat_df, items.df)
+
+    assert len(items) == len(expected_col_items.iloc[:expected_count])
+
+    assert items.limit == len(collection_items)
+    assert items.count == expected_count
+    get_items_mock.assert_called_once_with("key", expected_count, 0, filters)
+
+
+job_items = pd.DataFrame(
+    [
+        {"_key": "112358/13/21/0", "name": "Elizabeth"},
+        {"_key": "112358/13/21/1", "name": "Margaret"},
+        {"_key": "112358/13/21/2", "name": "Yulia"},
+        {"_key": "112358/13/21/3", "name": "Vivien"},
+    ]
+)
+expected_job_items = pd.DataFrame(
+    [
+        {"_key": f"{SH_URL}/112358/13/21/item/0", "name": "Elizabeth"},
+        {"_key": f"{SH_URL}/112358/13/21/item/1", "name": "Margaret"},
+        {"_key": f"{SH_URL}/112358/13/21/item/2", "name": "Yulia"},
+        {"_key": f"{SH_URL}/112358/13/21/item/3", "name": "Vivien"},
+    ]
+)
+
+
+@pytest.mark.parametrize("start, count, expected_count", [(1, 2, 2)])
+def test_job_items(mocker, start, count, expected_count):
+    mocker.patch("arche.readers.items.JobItems.job", return_value=Job(), autospec=True)
+    mocker.patch(
+        "arche.tools.api.get_items",
+        return_value=job_items.iloc[start:expected_count].reset_index(drop=True),
+        autospec=True,
+    )
+    items = JobItems(
+        key="112358/13/21", start=start, count=count, filters=None, expand=False
+    )
+    pd.testing.assert_frame_equal(
+        items.df, expected_job_items.iloc[start:count].reset_index(drop=True)
+    )
+    assert items.count == count
