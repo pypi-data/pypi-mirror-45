513fb0e559aac7a6f8f7872b44684ad4f7a0cba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:40 +0000

    Add package dndbuddy/0.0.1/dndbuddy-0.0.1-py3-none-any.whl

---
+++ packages/dndbuddy/dndbuddy-0.0.1-py3-none-any.whl/dndbuddy/constants.py
@@ -0,0 +1,15 @@
+
+from dndbuddy_core import colors
+
+
+TITLE = colors.red("D&DBuddy")
+
+WELCOME = """\
+Welcome to {}!
+====================
+Run `help` for hints!
+""".format(TITLE)
+
+PROMPT = """[{}]> """.format(TITLE)
+
+GOODBYE = """\nGood game!"""
