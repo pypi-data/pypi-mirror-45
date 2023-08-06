5a445dd7bedeea077b93c85e4df361c21beb22c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:11 +0000

    Add package edc-locator/0.2.27/edc_locator-0.2.27-py3-none-any.whl

---
+++ packages/edc-locator/edc_locator-0.2.27-py3-none-any.whl/edc_locator/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig as DjangoAppConfig
+
+
+class AppConfig(DjangoAppConfig):
+    name = "edc_locator"
+    verbose_name = "Edc Locator"
+    has_exportable_data = True
