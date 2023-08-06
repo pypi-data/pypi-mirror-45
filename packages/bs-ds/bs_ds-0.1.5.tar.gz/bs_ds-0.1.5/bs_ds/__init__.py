35e5591e5bea26d5b9be2694fd725fd5babc0ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:19 +0000

    Add package bs-ds/0.1.5/bs_ds-0.1.5.tar.gz

---
+++ packages/bs-ds/bs_ds-0.1.5.tar.gz/bs_ds-0.1.5/bs_ds/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for bs_ds."""
+
+__author__ = """James Irving"""
+__email__ = 'james.irving.phd@outlook.com'
+__version__ = '0.1.5'
+
+from .bs_ds import *
