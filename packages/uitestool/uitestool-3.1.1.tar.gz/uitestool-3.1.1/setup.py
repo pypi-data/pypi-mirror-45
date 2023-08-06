58e37e673c96ab5d1ca5e42ce909a5114b4f9b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:45 +0000

    Add package uitestool/3.1.1/uitestool-3.1.1.tar.gz

---
+++ packages/uitestool/uitestool-3.1.1.tar.gz/uitestool-3.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+    name='uitestool',
+    version='3.1.1',
+    description='a automated test tools',
+    author='szmgithubcode',
+    author_email='13023297816@163.com',
+    url='https://github.com/szmgithubcode/',
+    #packages=find_packages(),
+    packages=['autotest'],
+    #install_requires=['requests'],
+)
