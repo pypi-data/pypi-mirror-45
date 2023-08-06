49f94387d80c4c3d5f92f412a27c2f1f4cabab54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:41 +0000

    Add package Quasar/0.3.1/Quasar-0.3.1.tar.gz

---
+++ packages/Quasar/Quasar-0.3.1.tar.gz/Quasar-0.3.1/installer/windows/specs/update-conda-spec.bat
@@ -0,0 +1,18 @@
+setlocal EnableDelayedExpansion
+
+if "%CONDA%" == "" (
+    set "%CONDA%"=conda
+)
+
+"%CONDA%" config --append channels conda-forge
+"%CONDA%" config --append channels https://quasar.codes/conda/
+
+rem %CONDA% remove -y -n orange --all
+
+rem added some spectroscopy dependencies by hand as it is packaged manually
+"%CONDA%" create -y -n env python=3.6.* orange3=3.20.1 ^
+ quasar=0.3.1 orange-spectroscopy=0.4.3 opusFC=1.2.* ^
+ keyring scipy=1.1.* numpy=1.15.* pyqt=5.6.* ^
+ scikit-learn=0.20.* spectral ^
+ colorcet h5py || exit /b !ERRORLEVEL!
+"%CONDA%" list -n env --export --explicit --md5 > env-spec.txt
