f9c937e4d8b529c3ce82dbc867f5015fa7254c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:02 +0000

    Add package nba-scraper/0.2/nba_scraper-0.2.tar.gz

---
+++ packages/nba-scraper/nba_scraper-0.2.tar.gz/nba_scraper-0.2/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'nba_scraper',
+  packages = ['nba_scraper'],
+  version = '0.2',
+  license='GNU General Public License v3.0',
+  description = 'A Python package to scraper the NBA api and return a play by play file',
+  author = 'Matthew Barlowe',
+  author_email = 'matt@barloweanalytics.com',
+  url = 'https://https://github.com/mcbarlowe/nba_scraper',
+  download_url = 'https://github.com/mcbarlowe/nba_scraper/archive/v_02.tar.gz',
+  keywords = ['basketball', 'NBA', 'scraper'],
+  install_requires=[
+          'requests',
+          'pandas',
+          'numpy'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
