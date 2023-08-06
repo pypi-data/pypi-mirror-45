e32115d243d1e0ff9a2c1c63d654fdcbb0e08703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:05 +0000

    Add package parenclitic/0.1/parenclitic-0.1.tar.gz

---
+++ packages/parenclitic/parenclitic-0.1.tar.gz/parenclitic-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='parenclitic',
+      version='0.1',
+      description='Parenclitic approach with kernels inside',
+      url='https://github.com/mike-live/parenclitic',
+      author='Mikhail Krivonosov',
+      author_email='mike_live@mail.ru',
+      license='MIT',
+      packages=['parenclitic'],
+      zip_safe=False)
