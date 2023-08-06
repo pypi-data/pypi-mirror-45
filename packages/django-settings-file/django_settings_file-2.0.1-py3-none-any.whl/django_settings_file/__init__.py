ebc2992a6412d7b4e4b02f5961f2507651b98366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:14 +0000

    Add package django-settings-file/2.0.1/django_settings_file-2.0.1-py3-none-any.whl

---
+++ packages/django-settings-file/django_settings_file-2.0.1-py3-none-any.whl/django_settings_file/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+__version__ = '2.0.1'
+
+
+class DjangoSettingsFileError(Exception):
+    pass
+
+
+def setup():
+    if 'DJANGO_SETTINGS_MODULE' in os.environ:
+        raise DjangoSettingsFileError(
+            'DJANGO_SETTINGS_MODULE should not be defined, only DJANGO_SETTINGS_FILE is obeyed',
+        )
+    if 'DJANGO_SETTINGS_FILE' not in os.environ:
+        raise DjangoSettingsFileError('DJANGO_SETTINGS_FILE is not defined')
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'django_settings_file.settings'
