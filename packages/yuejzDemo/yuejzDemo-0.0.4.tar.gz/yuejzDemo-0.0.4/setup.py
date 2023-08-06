586b042b9f448bf286b11cc88600ee5627438f0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:48 +0000

    Add package yuejzDemo/0.0.4/yuejzDemo-0.0.4.tar.gz

---
+++ packages/yuejzDemo/yuejzDemo-0.0.4.tar.gz/yuejzDemo-0.0.4/setup.py
@@ -0,0 +1,31 @@
+# import setuptools
+#
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+#
+# setuptools.setup(
+#     name="yuejzDemo",
+#     version="0.0.2",
+#     author="YueJZ",
+#     author_email="yuejianzhong@sensorsdata.cn",
+#     description="This is my demo",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="https://github.com/YueJZSensorsData/FirstDemo",
+#     packages=['SADemo'],
+# )
+
+from distutils.core import setup
+
+setup(
+    name='yuejzDemo',
+    version='0.0.4',
+    author='YueJZ',
+    author_email='yuejianzhong@sensorsdata.cn',
+    url='https://github.com/YueJZSensorsData/FirstDemo',
+    license='LICENSE.txt',
+    packages=['SADemo'],
+    description='This is my demo',
+    long_description=open('README.txt').read(),
+
+)
