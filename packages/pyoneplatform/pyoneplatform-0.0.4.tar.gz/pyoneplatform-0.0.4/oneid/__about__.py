8b78d4e2fff9cd02a31f8c6416bc58c89aaf4252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:53 +0000

    Add package pyoneplatform/0.0.4/pyoneplatform-0.0.4.tar.gz

---
+++ packages/pyoneplatform/pyoneplatform-0.0.4.tar.gz/pyoneplatform-0.0.4/oneid/__about__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+""" Meta Data of OneID """
+
+__version__ = '1.0.0'
+__author__ = 'OnePlatform Development'
+__copyright__ = 'Copyright 2019, INET Co.,Ltd.'
+__license__ = 'GNU GPL 3.0'
+
+__all__ = (
+    '__version__'
+)
