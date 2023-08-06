3a63a29b89e4fa046003665757880ac1608402e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:58 +0000

    Add package pspring-rest-client/0.0.15/pspring-rest-client-0.0.15.tar.gz

---
+++ packages/pspring-rest-client/pspring-rest-client-0.0.15.tar.gz/pspring-rest-client-0.0.15/pspringrestclient/restclient.py
@@ -0,0 +1,137 @@
+import requests
+
+from pspring import *
+
+import logging
+import json
+import inspect
+
+logger = logging.getLogger("pspring-rest-client")
+
+class PayloadException(Exception):
+    def __init__(self,*args):
+        super().__init__(*args)
+        self.response = args[2]
+        self.statusCode = args[1]
+
+class Mapping():
+    def __init__(self,*args,**kargs):
+        self.method = kargs.get("method")
+        self.url = kargs.get("url")
+
+    def __call__(self,funcObj):
+        def newFunc(*args,**kargs):
+            argspec = inspect.getfullargspec(funcObj)
+            argumentNames = argspec[0]
+            for i in range(len(argumentNames)):
+                self.url = self.url.replace("{"+argumentNames[i]+"}",str(args[i]))
+
+            for (kargKey,kargVal) in kargs.items():
+                self.url = self.url.replace("{"+kargKey+"}",str(kargVal))
+
+            selfObj = args[0]
+            kargs.update({
+                "url" : selfObj.getUrl()+self.url
+            })
+            kargs.update({
+                "method" : self.method
+            })
+
+            funcObj(*args,**kargs)
+            return selfObj.send(**kargs)
+        return newFunc
+
+
+class RestClient():
+    def __init__(self,*args,**kargs):
+        self.url = kargs.get("url")
+
+    def __call__(self,classObj):
+        def constructor(*args,**kargs):
+            selfOrig = args[0]
+            selfOrig.headers = {}
+            selfOrig.url = self.url
+
+        def addHeader(selfOrig,name,value):
+            selfOrig.headers.update({name:value})
+
+        def send(*args,**kargs):
+            selfOrig = args[0]
+            selfOrig.finalize()
+
+            logger.info({
+                "message" : "request details",
+                "method" : kargs.get("method"),
+                "url" : kargs.get("url"),
+                "data" : kargs.get("data"),
+                "proxies" : kargs.get("proxies"),
+                "headers" : selfOrig.headers
+            })
+
+            kargs["headers"] = selfOrig.headers
+
+            response = requests.request(**kargs)
+
+            try:
+                logger.info({
+                    "message" : "response details",
+                    "method" : kargs.get("method"),
+                    "url" : kargs.get("url"),
+                    "data" : kargs.get("data"),
+                    "proxies" : kargs.get("proxies"),
+                    "headers" : selfOrig.headers,
+                    "status_code" : response.status_code,
+                    "responseHeaders" : json.loads(str(response.headers).replace("'","\"")),
+                    "response" : response.json(),
+                    "elapsed" : response.elapsed.total_seconds()
+                })
+            except Exception as ex:
+                logger.error({
+                    "message" : str(ex)
+                })
+                logger.info({
+                    "message" : "response details",
+                    "method" : kargs.get("method"),
+                    "url" : kargs.get("url"),
+                    "data" : kargs.get("data"),
+                    "proxies" : kargs.get("proxies"),
+                    "headers" : selfOrig.headers,
+                    "status_code" : response.status_code,
+                    "responseHeaders" : response.headers,
+                    "response" : response.text,
+                    "elapsed" : response.elapsed.total_seconds()
+                })
+
+            if response.ok:
+                if "application/json" in response.headers.get("Content-Type"):
+                    return {
+                        "body":response.json(),
+                        "headers" : response.headers
+                    }
+                else:
+                    return {
+                        "body":response.text,
+                        "headers" : response.headers
+                    }
+            else:
+                if getattr(selfOrig,"handleError",None) != None and "application/json" in response.headers.get("Content-Type"):
+                    mappedResponse = selfOrig.handleError(response.json())
+                    statusCode = mappedResponse.get("statusCode")
+                    del mappedResponse["statusCode"]
+                    raise PayloadException("backend error",statusCode,mappedResponse)
+                raise PayloadException("backend error",response.status_code,response.text)
+
+        def getUrl(*args,**kargs):
+            selfOrig = args[0]
+            return selfOrig.url
+
+        def finalize(*args,**kargs):
+            pass
+
+        classObj.__init__ = constructor
+        classObj.addHeader = addHeader
+        classObj.send = send
+        classObj.getUrl = getUrl
+        classObj.finalize = finalize
+
+        return classObj
