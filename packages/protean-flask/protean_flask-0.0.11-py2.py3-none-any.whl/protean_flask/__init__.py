90b52bc5c6189458f4a1d375d6d394f24aef4895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:45 +0000

    Add package protean-flask/0.0.11/protean_flask-0.0.11-py2.py3-none-any.whl

---
+++ packages/protean-flask/protean_flask-0.0.11-py2.py3-none-any.whl/protean_flask/__init__.py
@@ -0,0 +1,12 @@
+from protean.conf import active_config
+
+from . import config
+from .core.base import Protean
+
+__version__ = '0.0.11'
+
+__all__ = ('Protean',)
+
+
+# Update the config here so that loading the repo will load the config
+active_config.update_defaults(config)
