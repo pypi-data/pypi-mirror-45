c99f3fde05b5945916d9cea0d4dcbb79df2a4c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:18 +0000

    Add package mechmat/0.1.1/mechmat-0.1.1-py2.py3-none-any.whl

---
+++ packages/mechmat/mechmat-0.1.1-py2.py3-none-any.whl/mechmat/material.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from yaml import dump, load
+
+from .linked import MetaLinked
+from .subject import Subject
+
+"""Main module."""
+
+__all__ = ['Material']
+
+
+class Material(metaclass=MetaLinked):
+    def __init__(self):
+        for key in self._state:
+            if not hasattr(self, '_Subject__{}'.format(key)):
+                setattr(self, '_Subject__{}'.format(key), Subject(key))
+        for key in self._state:
+            if hasattr(self._linked[key], '_depended_on'):
+                for dep in self._linked[key]._depended_on:
+                    getattr(self, '_Subject__{}'.format(dep)).register(getattr(self, '_Subject__{}'.format(key)))
+
+    _state = list()
+    _linked = dict()
+
+    def __repr__(self):
+        state = {}
+        for prop in self._state:
+            state[prop] = getattr(self, prop)
+        return '{} with state {}>'.format(str(type(self))[:-2], state)
+
+    def dump(self):
+        return dump(self)
+
+    def load(self, data):
+        self = load(data)
