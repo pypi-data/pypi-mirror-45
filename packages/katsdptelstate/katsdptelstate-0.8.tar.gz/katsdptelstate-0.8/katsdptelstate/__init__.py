559692588404b41e8e9cfa50c1553fe7dc2f653e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:24 +0000

    Add package katsdptelstate/0.8/katsdptelstate-0.8.tar.gz

---
+++ packages/katsdptelstate/katsdptelstate-0.8.tar.gz/katsdptelstate-0.8/katsdptelstate/__init__.py
@@ -0,0 +1,26 @@
+################################################################################
+# Copyright (c) 2015-2019, National Research Foundation (Square Kilometre Array)
+#
+# Licensed under the BSD 3-Clause License (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy
+# of the License at
+#
+#   https://opensource.org/licenses/BSD-3-Clause
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+from .telescope_state import (TelescopeState, ConnectionError, InvalidKeyError,
+                              ImmutableKeyError, TimeoutError, CancelledError,
+                              DecodeError, EncodeError, RdbParseError,
+                              PICKLE_PROTOCOL, encode_value, decode_value,
+                              ALLOWED_ENCODINGS, ENCODING_DEFAULT,
+                              ENCODING_PICKLE, ENCODING_MSGPACK)
+
+
+# Automatically added by katversion
+__version__ = '0.8'
