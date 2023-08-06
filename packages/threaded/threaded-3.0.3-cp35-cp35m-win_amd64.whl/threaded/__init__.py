7563f26b7f9045c2fd6618c62a806ad0a7f39c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:50 +0000

    Add package threaded/3.0.3/threaded-3.0.3-cp35-cp35m-win_amd64.whl

---
+++ packages/threaded/threaded-3.0.3-cp35-cp35m-win_amd64.whl/threaded/__init__.py
@@ -0,0 +1,61 @@
+#    Copyright 2017 - 2019 Alexey Stepanov aka penguinolog
+##
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""threaded module."""
+
+# Standard Library
+import typing
+
+# External Dependencies
+import pkg_resources
+
+# Local Implementation
+from ._asynciotask import AsyncIOTask
+from ._asynciotask import asynciotask
+from ._threaded import Threaded
+from ._threaded import threaded
+from ._threadpooled import ThreadPooled
+from ._threadpooled import threadpooled
+
+try:  # pragma: no cover
+    __version__ = pkg_resources.get_distribution(__name__).version
+except pkg_resources.DistributionNotFound:  # pragma: no cover
+    # package is not installed, try to get from SCM
+    try:
+        import setuptools_scm  # type: ignore
+
+        __version__ = setuptools_scm.get_version()
+    except ImportError:
+        pass
+
+
+__all__ = (
+    "ThreadPooled",
+    "Threaded",
+    "threadpooled",
+    "threaded",
+    "AsyncIOTask",
+    "asynciotask",
+)  # type: typing.Tuple[str, ...]
+
+__author__ = "Alexey Stepanov"
+__author_email__ = "penguinolog@gmail.com"
+__maintainers__ = {
+    "Alexey Stepanov": "penguinolog@gmail.com",
+    "Antonio Esposito": "esposito.cloud@gmail.com",
+    "Dennis Dmitriev": "dis-xcom@gmail.com",
+}
+__url__ = "https://github.com/python-useful-helpers/threaded"
+__description__ = "Decorators for running functions in Thread/ThreadPool/IOLoop"
+__license__ = "Apache License, Version 2.0"
