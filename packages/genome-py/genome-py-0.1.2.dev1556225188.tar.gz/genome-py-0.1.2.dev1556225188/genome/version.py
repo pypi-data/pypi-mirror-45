92d9e8c6c99f317c1c0e93a0dccb3b1b30b81361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:21 +0000

    Add package genome-py/0.1.2.dev1556225188/genome-py-0.1.2.dev1556225188.tar.gz

---
+++ packages/genome-py/genome-py-0.1.2.dev1556225188.tar.gz/genome-py-0.1.2.dev1556225188/genome/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '0.1.2'
+version = '0.1.2'
+full_version = '0.1.2.dev1556225188'
+dev_version = '1556225188'
+release = False
+if not release:
+    version = full_version
