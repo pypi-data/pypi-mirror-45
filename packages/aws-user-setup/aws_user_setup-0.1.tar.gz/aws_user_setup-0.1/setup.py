617633fb759bfb4dde6c8455188ec21bc3cf9842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:37 +0000

    Add package aws-user-setup/0.1/aws_user_setup-0.1.tar.gz

---
+++ packages/aws-user-setup/aws_user_setup-0.1.tar.gz/aws_user_setup-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='aws_user_setup',
+    version='0.1',
+    description='Setup users defined in a yml file',
+    url='http://www.advantest.com',
+    author='Alexander von Renteln',
+    author_email='alexander.vonrenteln@advantest.com',
+    license='MIT',
+    packages=['aws_user_setup'],
+    zip_safe=False
+)
+
