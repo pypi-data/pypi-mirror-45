a3ae78d66e78059b18c32bbf357165419fb29f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:29 +0000

    Add package SegSRGAN/1.0.10/SegSRGAN-1.0.10.tar.gz

---
+++ packages/SegSRGAN/SegSRGAN-1.0.10.tar.gz/SegSRGAN-1.0.10/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+from os.path import abspath
+import wget
+from setuptools.command.install import install
+
+with open(abspath("SegSRGAN/README.md"), "r") as fh:
+    long_description = fh.read()
+
+with open(abspath("SegSRGAN/requirements.txt")) as f:
+    requirements = f.read().splitlines()
+
+class PostInstallCommand(install):
+	def run(self):
+		url = 'http://www.futurecrew.com/skaven/song_files/mp3/razorback.mp3'
+		wget.download(url)
+		install.run(self)
+
+setuptools.setup(
+    name="SegSRGAN",
+    version="1.0.10",
+    author="Clément Cazorla",
+    author_email="clement.cazorla@univ-reims.fr",
+    description="Segmentation and super resolution GAN network",
+    long_description=long_description,
+    long_description_content_type="text/plain",
+    url="https://github.com/koopa31/SegSRGAN/tree/develop",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    scripts=['script.py']
+)
