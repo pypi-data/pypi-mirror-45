8808dda118dd1ec9a97d1d472a2114ccd3cd5c18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:49 +0000

    Add package PyAudit/1.0.3/PyAudit-1.0.3.tar.gz

---
+++ packages/PyAudit/PyAudit-1.0.3.tar.gz/PyAudit-1.0.3/README.md
@@ -0,0 +1,36 @@
+# PyAudit: Python Data Audit 
+
+![logo](https://github.com/runawayhorse001/PyAudit/blob/master/doc/images/logo.png)
+
+![Pypi Version](https://img.shields.io/pypi/v/PyAudit.svg)
+![Build Status](https://travis-ci.org/rtfd/PyAudit.svg?branch=master)
+![License](https://img.shields.io/pypi/l/PyAudit.svg)
+
+This is our Python Data Audit library repositories.
+The ``API`` can be found at: https://runawayhorse001.github.io/PyAudit. 
+If you want to colne and install it, you can use 
+
+You can install the PyAudit from [PyPI](https://pypi.org/project/PyAudit):
+
+    pip install PyAudit
+
+Or you can istall the PyAudit from the repo:
+
+#### 1. clone
+
+	git clone https://github.com/runawayhorse001/PyAudit.git
+
+#### 2. install 
+
+	cd PyAudit
+	pip install -r requirements.txt 
+	python setup.py install
+
+#### 3. test 
+
+	cd PyAudit/test
+	python test.py
+
+#### 4. uninstall 
+
+	pip uninstall PyAudit
---
+++ packages/PyAudit/PyAudit-1.0.3.tar.gz/PyAudit-1.0.3/test/test.py
@@ -0,0 +1,50 @@
+from PyAudit.basics import missing_rate, zero_rate, dtypes_class
+from PyAudit.basics import feature_variance, freq_items_df, feature_len
+from PyAudit.basics import corr_matrix,numeric_summary, category_summary
+import pandas as pd
+import os, sys
+
+output = os.path.abspath(os.path.join(sys.path[0])) + '/output'
+print(output)
+d = {'A': [1, 0, None, 3],
+     'B': [1, 0, 0, 0],
+     'C': ['a', None, 'c', 'd']}
+
+# create DataFrame
+df = pd.DataFrame(d)
+print(missing_rate(df))
+print(zero_rate(df))
+print(feature_variance(df))
+print(df)
+print(feature_len(df))
+print(numeric_summary(df, output))
+print(category_summary(df, output))
+print(corr_matrix(df, output))
+
+
+d ={
+    'num': list('1223334444'),
+    'cat': list('wxxyyyzzzz')
+}
+df = pd.DataFrame(d)
+df = df.astype({"num": int, "cat": object})
+print(freq_items_df(df, top_n=4))
+
+# read df
+df = pd.read_csv('Heart.csv', dtype={'Sex': bool})
+print(df.head(5))
+(num_fields, cat_fields, bool_fields, data_types, type_class) = dtypes_class(df)
+
+print(num_fields)
+print(cat_fields)
+print(bool_fields)
+print(data_types)
+print(type_class)
+print(missing_rate(df))
+print(zero_rate(df))
+
+print(freq_items_df(df, top_n=4))
+print(feature_len(df))
+print(numeric_summary(df, output))
+print(category_summary(df, output))
+print(corr_matrix(df, output))
