f4d3d843837b79c4046546e80ae50e46494d8789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:02 +0000

    Add package nav/5.3.1/nav-5.3.1.tar.gz

---
+++ packages/nav/nav-5.3.1.tar.gz/nav-5.3.1/README.md
@@ -0,0 +1,22 @@
+# nav
+
+[![Travis CI build status (Linux)](https://travis-ci.org/jmagnusson/nav.svg?branch=master)](https://travis-ci.org/jmagnusson/nav)
+[![PyPI version](https://img.shields.io/pypi/v/nav.svg)](https://pypi.python.org/pypi/nav/)
+[![License](https://img.shields.io/pypi/l/nav.svg)](https://pypi.python.org/pypi/nav/)
+[![Available as wheel](https://img.shields.io/pypi/wheel/nav.svg)](https://pypi.python.org/pypi/nav/)
+[![Supported Python versions](https://img.shields.io/pypi/pyversions/nav.svg)](https://pypi.python.org/pypi/nav/)
+[![PyPI status (alpha/beta/stable)](https://img.shields.io/pypi/status/nav.svg)](https://pypi.python.org/pypi/nav/)
+[![Coverage Status](https://coveralls.io/repos/github/jmagnusson/nav/badge.svg?branch=master)](https://coveralls.io/github/jmagnusson/nav?branch=master)
+[![Code Health](https://landscape.io/github/jmagnusson/nav/master/landscape.svg?style=flat)](https://landscape.io/github/jmagnusson/nav/master)
+
+Make requests to Microsoft Dynamics NAV Web Services
+
+## Installation
+
+Programmatic use only:
+
+    pip install nav
+
+With CLI support:
+
+    pip install nav[cli]
---
+++ packages/nav/nav-5.3.1.tar.gz/nav-5.3.1/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+
+setup_kwargs = dict(
+    name='nav',
+    version='5.3.1',
+    description='Conveniently make requests to Microsoft Dynamics NAV Web Services',
+    packages=['nav', 'nav.wrappers'],
+    include_package_data=True,
+    author='Jacob Magnusson',
+    author_email='m@jacobian.se',
+    url='https://github.com/jmagnusson/nav',
+    license='BSD',
+    platforms='any',
+    install_requires=[
+        'lxml',
+        'requests-ntlm',
+        'zeep>=3.0.0',
+    ],
+    extras_require={
+        'cli': [
+            'argh',
+            'ipython',
+        ],
+        'test': {
+            'coverage>=4.2',
+            'flake8>=3.0.4',
+            'pytest>=3.0.3',
+            'responses>=0.5.1',
+        },
+    },
+    entry_points={
+        'console_scripts': [
+            'nav = nav.__main__:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
+
+if __name__ == '__main__':
+    setup(**setup_kwargs)
