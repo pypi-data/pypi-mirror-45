cc2f6143b08f18fe3abf0ae0668fe17f99b2e474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:00 +0000

    Add package pytorchfi/0.1.6/pytorchfi-0.1.6.tar.gz

---
+++ packages/pytorchfi/pytorchfi-0.1.6.tar.gz/pytorchfi-0.1.6/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytorchfi",
+    version="0.1.6",
+    author="UIUC RSim",
+    description="A runtime fault injector for PyTorchFI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pytorchfi/pytorchfi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
