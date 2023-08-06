567d5964c618b7f9e85dec943433bce8d93eec29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:12 +0000

    Add package squidward/0.0.21/squidward-0.0.21-py3-none-any.whl

---
+++ packages/squidward/squidward-0.0.21-py3-none-any.whl/squidward/__init__.py
@@ -0,0 +1,9 @@
+"""
+Automatically import submodules for squidward package.
+"""
+
+import squidward.gpr
+import squidward.gpc
+import squidward.utils
+import squidward.validation
+import squidward.kernels
