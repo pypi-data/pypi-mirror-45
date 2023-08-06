aa9bcc98fa609b68ddb3ca02833fe883b13bcccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:08 +0000

    Add package mindconnectiot/0.1.1/mindconnectiot-0.1.1.tar.gz

---
+++ packages/mindconnectiot/mindconnectiot-0.1.1.tar.gz/mindconnectiot-0.1.1/setup.py
@@ -0,0 +1,21 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(name='mindconnectiot',
+      version='0.1.1',
+      description='Wrapper around the MindConnect IoT Extention.',
+	  long_description=README,
+	  long_description_content_type="text/markdown",
+      url='https://gitlab.com/Addono/mindconnect-iot-extention-python',
+      author='Adriaan Knapen',
+      author_email='hi@aknapen.nl',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=['paho-mqtt'])
