152f038338c557f961e442737fdb9575e7a1fc17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:43 +0000

    Add package JackkillianAutoPackager/1.0.1/JackkillianAutoPackager-1.0.1.tar.gz

---
+++ packages/JackkillianAutoPackager/JackkillianAutoPackager-1.0.1.tar.gz/JackkillianAutoPackager-1.0.1/setup.py
@@ -0,0 +1,15 @@
+"""Created with Jackkillian Auto Packager. To install: pip install JackkillianAutoPackager"""
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="JackkillianAutoPackager",
+    version="1.0.1",
+    author="jackkillian",
+    description="Upload projects to pip with ease!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Jackkillian/Jackkillian-Auto-Packager",
+    packages=['AutoPackager.code'],
+    classifiers=["Programming Language :: Python :: 3", "Development Status :: 5 - Production/Stable ", "License :: OSI Approved :: MIT License ", "Operating System :: OS Independent"])
