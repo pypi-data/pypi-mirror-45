823659cd532bee57539dc1216e5db0007eac8008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:52 +0000

    Add package pypi-name/0.0.1/pypi_name-0.0.1.tar.gz

---
+++ packages/pypi-name/pypi_name-0.0.1.tar.gz/pypi_name-0.0.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "pypi_name",
+    packages = ["pypi_name"],
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    version = "0.0.1",
+    description = "Check whether a package name is available on PyPI",
+    author = "Yoginth",
+    author_email = "me@yoginth.com",
+    url = "https://yoginth.com",
+    classifiers=(
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "Environment :: Plugins",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+    ),
+    project_urls={
+        'Source': 'https://gitlab.com/yoginth/pypi_name',
+    },
+)
