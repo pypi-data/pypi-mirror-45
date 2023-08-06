089bb2697c7d9f771e8dd3d4532c26d19d9f2646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:33 +0000

    Add package chempy/0.6.18/chempy-0.6.18.tar.gz

---
+++ packages/chempy/chempy-0.6.18.tar.gz/chempy-0.6.18/chempy/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+ChemPy is a Python package useful for solving problems in chemistry.
+"""
+
+from __future__ import absolute_import, division, print_function
+
+from ._url import __url__
+from ._release import __version__
+from .chemistry import Substance, Reaction, Equilibrium, Species, balance_stoichiometry, mass_fractions
+from .reactionsystem import ReactionSystem
+from .henry import Henry
+from .util.periodic import atomic_number
+from .util.pyutil import ChemPyDeprecationWarning
+from .kinetics import EyringParam, EyringHS, MassAction
