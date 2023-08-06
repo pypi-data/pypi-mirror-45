557df0e3bd223bd5d1620f164215a9186a03f68c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:43 +0000

    Add package hdx-data-freshness/1.3.6/hdx_data_freshness-1.3.6-py2.py3-none-any.whl

---
+++ packages/hdx-data-freshness/hdx_data_freshness-1.3.6-py2.py3-none-any.whl/hdx/freshness/database/base.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declared_attr
+
+
+class HDXBase(object):
+    @declared_attr
+    def __tablename__(cls):
+        return '%ss' % cls.__name__.lower()
+
+
+Base = declarative_base(cls=HDXBase)
