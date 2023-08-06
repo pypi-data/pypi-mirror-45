918548a32f1268928362d448264059cdd37c4177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:35 +0000

    Add package geometry-designer/0.3/geometry_designer-0.3.tar.gz

---
+++ packages/geometry-designer/geometry_designer-0.3.tar.gz/geometry_designer-0.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+__author__ = "Ricardo Ribeiro"
+__copyright__ = ""
+__credits__ = "Ricardo Ribeiro"
+__license__ = "MIT"
+__version__ = "0.3"
+__maintainer__ = ["Ricardo Ribeiro", "Carlos Mão de Ferro"]
+__email__ = ["ricardojvr at gmail.com", "cajomferro at gmail.com"]
+__status__ = "Development"
+__updated__ = "2016-03-14"
+
+from setuptools import setup, find_packages
+
+setup(
+    name='geometry_designer',
+    version=__version__,
+    description="""""",
+    author='Ricardo Ribeiro',
+    author_email='ricardo.ribeiro@research.fchampalimaud.org',
+    license='MIT',
+    url='https://github.com/UmSenhorQualquer/geometry-designer',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples', 'deploy', 'reports'])
+)
