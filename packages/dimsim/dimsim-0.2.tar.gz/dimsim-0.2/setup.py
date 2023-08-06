b61743c05f00da3099e9641cdbca623672b52c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:12 +0000

    Add package dimsim/0.2/dimsim-0.2.tar.gz

---
+++ packages/dimsim/dimsim-0.2.tar.gz/dimsim-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='dimsim',
+	version='0.2',
+	description='Python implementation of the Chinese soundex project DimSim',
+	author='IBM SystemT, IBM CODAIT',
+	author_email='qian.kun@ibm.com, karthik.muthuraman@ibm.com, ihjhuo@ibm.com, frreiss@us.ibm.com',
+	url='https://github.com/System-T/DimSim',
+	packages=['dimsim', 'dimsim.core', 'dimsim.utils', 'dimsim.data'],
+	package_data={'':['dimsim/data/pinyin_to_simplified.pickle','dimsim/data/pinyin_to_traditional.pickle']},
+	include_package_data=True,
+	classifiers=['License :: OSI Approved :: Apache Software License'],
+	install_requires=[
+        'pypinyin',
+        ],
+	test_suite='nose.collector',
+    tests_require=['nose']
+)
