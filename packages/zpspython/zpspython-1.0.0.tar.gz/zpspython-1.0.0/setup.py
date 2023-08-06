862955c44c03ca046d15c65e3ae394a151a85c2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:36 +0000

    Add package zpspython/1.0.0/zpspython-1.0.0.tar.gz

---
+++ packages/zpspython/zpspython-1.0.0.tar.gz/zpspython-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name            = "zpspython",
+        version         = "1.0.0",
+        py_modules      = ["zps_print_list"],
+        author          = "estiy",
+        author_email    = "zhangpengshuo@outlook.com",
+        url             = "http://www.headfirstlabs.com",
+        description     = "实验程序",
+    )
---
+++ packages/zpspython/zpspython-1.0.0.tar.gz/zpspython-1.0.0/zps_print_list.py
@@ -0,0 +1,6 @@
+﻿def print_list(the_list):
+	for each in the_list:
+		if isinstance(each,list):
+			print_list(each)
+		else:
+			print(each)
