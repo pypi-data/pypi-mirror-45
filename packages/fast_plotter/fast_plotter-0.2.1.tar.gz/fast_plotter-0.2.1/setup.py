f538c25b57783d3a2aad61ad2853f6213cfc4344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:30 +0000

    Add package fast_plotter/0.2.1/fast_plotter-0.2.1.tar.gz

---
+++ packages/fast_plotter/fast_plotter-0.2.1.tar.gz/fast_plotter-0.2.1/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+#with open('HISTORY.rst') as history_file:
+#    history = history_file.read()
+
+requirements = ['matplotlib<3', 'pandas', 'numpy']
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', 'flake8', 'pytest-cov']
+
+setup(
+    author="Ben Krikler",
+    author_email='fast-hep@cern.ch',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="F.A.S.T. plotter package",
+    entry_points={
+        'console_scripts': [
+            'fast_plotter=fast_plotter.__main__:main',
+        ],
+    },
+    install_requires=requirements,
+    license="Apache Software License 2.0",
+    long_description=readme,
+    include_package_data=True,
+    keywords='fast_plotter',
+    name='fast_plotter',
+    packages=find_packages(include=['fast_plotter']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://gitlab.cern.ch/fast-hep/public/fast_plotter',
+    version='0.2.1',
+    zip_safe=False,
+)
