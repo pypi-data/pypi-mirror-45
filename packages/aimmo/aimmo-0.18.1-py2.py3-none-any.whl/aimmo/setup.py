1c62a753a08b6ce27436cc5e621963c199b8f445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:08 +0000

    Add package aimmo/0.18.1/aimmo-0.18.1-py2.py3-none-any.whl

---
+++ packages/aimmo/aimmo-0.18.1-py2.py3-none-any.whl/aimmo/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+setup(
+    name="aimmo",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["eventlet", "flask", "requests", "six"],
+    tests_require=["httmock", "mock"],
+    test_suite="test_utils.test_suite.DjangoAutoTestSuite",
+    zip_safe=False,
+)
