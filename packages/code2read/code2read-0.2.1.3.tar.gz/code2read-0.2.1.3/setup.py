2564f8024797f2954486721fbbafced3d3d450dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:52 +0000

    Add package code2read/0.2.1.3/code2read-0.2.1.3.tar.gz

---
+++ packages/code2read/code2read-0.2.1.3.tar.gz/code2read-0.2.1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='code2read',
+      version='0.2.1.3',
+      description='Converts your code with comments into a workable README.md: Code more. Write less.',
+      url='https://github.com/SubhadityaMukherjee/code_to_readme',
+      author='Subhaditya Mukherjee',
+      author_email='mukherjeesubhaditya001@gmail.com',
+      license='MIT',
+      packages=['code2read'],
+      entry_points={
+          'console_scripts': [
+              'code2read = code2read.__main__:exec'
+          ]
+      },
+      zip_safe=False)
