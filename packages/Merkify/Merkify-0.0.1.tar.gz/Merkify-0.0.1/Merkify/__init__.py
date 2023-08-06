8eaca7af1c8b2977b3fe2f8bc851d6d7eb729397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:33 +0000

    Add package Merkify/0.0.1/Merkify-0.0.1.tar.gz

---
+++ packages/Merkify/Merkify-0.0.1.tar.gz/Merkify-0.0.1/Merkify/__init__.py
@@ -0,0 +1,1 @@
+from Merkify.Merkify import Merkify
---
+++ packages/Merkify/Merkify-0.0.1.tar.gz/Merkify-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'Merkify',         # How you named your package folder (MyLib)
+  packages = ['Merkify'],   # Chose the same as "name"
+  version = '0.0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
+  author = 'Bani Singh',                   # Type in your name
+  author_email = 'banihns@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/banisingh/Merkify/archive/0.0.1.tar.gz',    # I explain this later on
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'hashlib',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
