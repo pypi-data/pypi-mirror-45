1458eca082383247acc2548be63003acc2e950cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:36 +0000

    Add package skl2onnx/1.4.7/skl2onnx-1.4.7-py2.py3-none-any.whl

---
+++ packages/skl2onnx/skl2onnx-1.4.7-py2.py3-none-any.whl/scikitonx/__init__.py
@@ -0,0 +1,28 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+"""
+Main entry point to the converter from the scikit-learn to onnx.
+"""
+__version__ = "1.4.0"
+__author__ = "Microsoft"
+__producer__ = "scikitonx"
+__producer_version__ = __version__
+__domain__ = "onnxml"
+__model_version__ = 0
+
+
+from .convert import convert
+from .common import utils
+
+
+def convert_sklearn(model, name=None, initial_types=None, doc_string='',
+                    target_opset=None, custom_conversion_functions=None, custom_shape_calculators=None):
+    if not utils.sklearn_installed():
+        raise RuntimeError('scikit-learn is not installed. Please install scikit-learn to use this feature.')
+
+    return convert(model, name, initial_types, doc_string, target_opset,
+                   custom_conversion_functions, custom_shape_calculators)
