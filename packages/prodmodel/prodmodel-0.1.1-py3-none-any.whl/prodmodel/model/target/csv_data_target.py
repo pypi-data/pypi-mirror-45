4c0c008daf0babe2169e9ddba2cc42398f59e876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:32 +0000

    Add package prodmodel/0.1.1/prodmodel-0.1.1-py3-none-any.whl

---
+++ packages/prodmodel/prodmodel-0.1.1-py3-none-any.whl/prodmodel/model/target/csv_data_target.py
@@ -0,0 +1,23 @@
+import csv
+
+from model.files.data_file import DataFile
+from model.target.iterable_data_target import IterableDataTarget
+
+
+class CSVDataTarget(IterableDataTarget):
+  def __init__(self, source: DataFile, dtypes: dict):
+    super().__init__(sources=[source], deps=[], file_deps=[])
+    self.source = source
+    self.dtypes = dtypes
+
+
+  def __iter__(self):
+    def convert(record):
+      for feature_name, feature_type in self.dtypes.items():
+        record[feature_name] = feature_type(record[feature_name])
+      return record
+    return map(convert, self.source.__iter__())
+
+
+  def params(self) -> dict:
+    return {'dtypes': {k: str(v) for k, v in self.dtypes.items()}}
