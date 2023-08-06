d88a36f3a6d22a20e1054d75c5e424387364d624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:05 +0000

    Add package djangorestframework-reactive/4.2.1/djangorestframework-reactive-4.2.1.tar.gz

---
+++ packages/djangorestframework-reactive/djangorestframework-reactive-4.2.1.tar.gz/djangorestframework-reactive-4.2.1/setup.py
@@ -0,0 +1,70 @@
+import os.path
+import setuptools
+
+# Get long description from README.
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+# Get package metadata from '__about__.py' file.
+about = {}
+base_dir = os.path.abspath(os.path.dirname(__file__))
+with open(
+    os.path.join(base_dir, 'src', 'rest_framework_reactive', '__about__.py'), 'r'
+) as fh:
+    exec(fh.read(), about)
+
+setuptools.setup(
+    name=about['__title__'],
+    use_scm_version=True,
+    description=about['__summary__'],
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    author=about['__author__'],
+    author_email=about['__email__'],
+    url=about['__url__'],
+    license=about['__license__'],
+    # Exclude tests from built/installed package.
+    packages=setuptools.find_packages(
+        'src', exclude=['tests', 'tests.*', '*.tests', '*.tests.*']
+    ),
+    package_dir={'': 'src'},
+    python_requires='>=3.6, <3.7',
+    install_requires=['Django~=1.11.6', 'djangorestframework>=3.4.0', 'channels~=2.2'],
+    extras_require={
+        'docs': ['sphinx>=1.3.2', 'sphinx_rtd_theme'],
+        'package': ['twine', 'wheel'],
+        'test': [
+            'django-filter~=2.0.0',
+            'django-guardian>=1.4.2',
+            'django-priority-batch~=1.1.0',
+            'channels-redis~=2.4',
+            'pylint>=1.4.3',
+            'pytest~=3.5.1',
+            'pytest-django~=3.2.1',
+            'pytest-asyncio~=0.8.0',
+            'async_timeout>=2.0,<4.0',
+            'psycopg2>=2.5.0',
+            'check-manifest',
+            'twine',
+            'setuptools_scm',
+            'black',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='django-rest-framework reactive django',
+)
---
+++ packages/djangorestframework-reactive/djangorestframework-reactive-4.2.1.tar.gz/djangorestframework-reactive-4.2.1/tests/drfr_test_app/models.py
@@ -0,0 +1,30 @@
+"""
+The models defined here are only used during testing.
+"""
+from django.db import models
+
+
+class ExampleItem(models.Model):
+    enabled = models.BooleanField()
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        permissions = (("view_exampleitem", "Can view example item"),)
+        ordering = ['pk']
+
+
+class ExampleSubItem(models.Model):
+    parent = models.ForeignKey(ExampleItem)
+    enabled = models.BooleanField()
+
+    class Meta:
+        permissions = (("view_examplesubitem", "Can view example sub item"),)
+        ordering = ['pk']
+
+
+class ExampleM2MItem(models.Model):
+    value = models.IntegerField(default=1)
+    items = models.ManyToManyField(ExampleItem, related_name='items')
+
+    class Meta:
+        ordering = ['pk']
