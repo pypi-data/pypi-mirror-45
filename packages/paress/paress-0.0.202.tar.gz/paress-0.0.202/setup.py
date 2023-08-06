676245d7446d71dfae732000121904607941e8a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:34 +0000

    Add package paress/0.0.202/paress-0.0.202.tar.gz

---
+++ packages/paress/paress-0.0.202.tar.gz/paress-0.0.202/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="paress",
+    version="0.0.202",
+    author="Jairo Antonio Melo Flórez",
+    author_email="jairom@colmich.edu.mx",
+    description="Utilidad para Web Scrapping en el Portal de Archivos Españoles-PARES",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jairomelo/PARESS",
+    packages=setuptools.find_packages(),
+    install_requires=['selenium','beautifulsoup4','requests','instapy-chromedriver'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
