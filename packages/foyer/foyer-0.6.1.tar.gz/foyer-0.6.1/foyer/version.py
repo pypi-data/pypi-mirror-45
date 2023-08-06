a4d1ee277525c11dd7fe641321421199b0ce426f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:38 +0000

    Add package foyer/0.6.1/foyer-0.6.1.tar.gz

---
+++ packages/foyer/foyer-0.6.1.tar.gz/foyer-0.6.1/foyer/version.py
@@ -0,0 +1,7 @@
+
+# This file is generated in setup.py at build time.
+version = '0.6.1'
+short_version = '0.6.1'
+full_version = '0.6.1'
+git_revision = 'f91fc5dd116af4c537096282c34129f02a7f029f'
+release = True
