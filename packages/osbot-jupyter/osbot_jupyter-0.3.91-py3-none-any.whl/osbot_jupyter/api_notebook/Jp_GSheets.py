2826651b0cdaffc9f465d2afbb3f9738ef34b30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:45 +0000

    Add package osbot-jupyter/0.3.91/osbot_jupyter-0.3.91-py3-none-any.whl

---
+++ packages/osbot-jupyter/osbot_jupyter-0.3.91-py3-none-any.whl/osbot_jupyter/api_notebook/Jp_GSheets.py
@@ -0,0 +1,64 @@
+from IPython.core.display import display, HTML
+from IPython.lib.display import IFrame
+from IPython.display import display
+from ipywidgets import widgets
+from osbot_gsuite.apis.GSheets import GSheets
+from osbot_gsuite.apis.sheets.API_Jira_Sheets_Create import API_Jira_Sheets_Create
+from osbot_gsuite.apis.sheets.API_Jira_Sheets_Sync import API_Jira_Sheets_Sync
+from osbot_jira.api.jira_server.API_Jira_Rest import API_Jira_Rest
+
+class Jp_GSheets():
+
+    def __init__(self, file_id=None, sheet_name=None):
+        self.gsuite_secret_id = 'gsuite_gsbot_user'
+        self.file_id          = file_id
+        self.sheet_name       = sheet_name
+        self.gsheets          = GSheets               (self.gsuite_secret_id)
+        self.gsheets_sync     = API_Jira_Sheets_Sync  (self.file_id,self.gsuite_secret_id)
+        self.jira_api_rest    = API_Jira_Rest()
+        #self.gsheets_create   = API_Jira_Sheets_Create(self.file_id)
+
+
+
+    def metadata(self):
+        return self.gsheets.sheets_metadata(self.file_id)
+
+    def values(self):
+        return self.gsheets.get_values_as_objects(self.file_id, self.sheet_name)
+
+    def link(self):
+        url = self.gsheets.gdrive.file_weblink(self.file_id)
+        html = "Here is the <a href='{0}' target='_blank'>link</a> for the sheet with id <i>{1}</i>".format(url, self.file_id)
+
+        display(HTML(html))
+
+    def show(self, width='100%', height=400):
+        url     = self.gsheets.gdrive.file_weblink(self.file_id)
+        iframe  = IFrame(url,width=width,height=height)
+        display(HTML("<style>.container { width:98% !important; }</style>"))
+        display(iframe)
+
+    def load_data(self):
+        print('loading data for sheet: {0}'.format(self.file_id))
+        self.sheets.gsheets_sync.load_data_from_jira()
+
+    def sync_data(self):
+        print('syncing data for sheet: {0}'.format(self.file_id))
+        self.sheets.gsheets_sync.sync_sheet()
+
+    def buttons(self):
+
+        button_load_data = widgets.Button(description="Load Data")
+        button_load_data.on_click(lambda x : self.load_data())
+
+        button_sync_data = widgets.Button(description="Sync Data")
+        button_sync_data.on_click(lambda x: self.load_data())
+
+        display(button_load_data)
+        display(button_sync_data)
+
+
+
+#%autoreload
+#from osbot_jupyter.api_notebook.Jp_Helper import Jp_Helper
+##Jp_Helper.trace_method(sheets.gsheets_sync.load_data_from_jira)
