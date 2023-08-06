2925157bcfd726849f584cc38125bc499abea405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:27 +0000

    Add package pi7447/1.0.1/pi7447-1.0.1.tar.gz

---
+++ packages/pi7447/pi7447-1.0.1.tar.gz/pi7447-1.0.1/README.md
@@ -0,0 +1,28 @@
+# Pi7447
+7447 lib for raspberry pi
+
+## Installation
+`pip install pi7447`
+
+## Usage example
+```
+import pi7447
+import time
+import RPi.GPIO as gpio
+gpio.setmode(gpio.BCM)
+
+IC7447_INPUT_A = 2
+IC7447_INPUT_B = 3
+IC7447_INPUT_C = 4
+IC7447_INPUT_D = 17
+
+sevenSegmentSisplay = pi7447.IC7447(IC7447_INPUT_A,IC7447_INPUT_B,IC7447_INPUT_C,IC7447_INPUT_D)
+
+#show 1 to 9
+for i in range(10):
+    sevenSegmentSisplay.show(i)
+    time.sleep(0.5)
+
+#turn off display
+sevenSegmentSisplay.off()
+```
---
+++ packages/pi7447/pi7447-1.0.1.tar.gz/pi7447-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pi7447",
+    version="1.0.1",
+    author="Philip Huang",
+    author_email="p208p2002@gmail.com",
+    description="7447 lib for raspberry pi",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/p208p2002/pi7447",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
