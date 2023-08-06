fd1e00bbc910eed74877faed2c55d3cbd818ea33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:07 +0000

    Add package reem/0.0.11/reem-0.0.11.tar.gz

---
+++ packages/reem/reem-0.0.11.tar.gz/reem-0.0.11/reem/connection.py
@@ -0,0 +1,30 @@
+import rejson
+from .supports import  MetadataListener
+from .ships import *
+from threading import Lock
+
+
+class RedisInterface:
+    """
+
+    """
+    def __init__(self, host='localhost', ships=[NumpyShip()]):
+        self.hostname = host
+        self.ships = ships
+        self.client = rejson.Client(host=host, decode_responses=True)
+        self.client_no_decode = rejson.Client(host=host)
+        self.metadata_listener = MetadataListener(self)
+        self.INTERFACE_LOCK = Lock()
+
+        self.label_to_shipper = {}
+        for sh in self.ships:
+            self.label_to_shipper[sh.get_label()] = sh
+
+        self.shipper_labels = [sh.get_label() for sh in ships]
+
+    def initialize(self):
+        """
+        Call before doing anything with this connection
+        :return:
+        """
+        pass
