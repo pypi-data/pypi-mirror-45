65c6dc695580004560ea6892545d0d20a45a336c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:13 +0000

    Add package imgbase64/0.1.5/imgbase64-0.1.5.tar.gz

---
+++ packages/imgbase64/imgbase64-0.1.5.tar.gz/imgbase64-0.1.5/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='imgbase64',
+    version='0.1.5',
+    url='https://github.com/Ronnasayd/imgbase64',
+    license='MIT License',
+    author='Ronnasayd de Sousa Machado',
+    author_email='ronnasayd@hotmail.com',
+    keywords='image bas64 url',
+    description=u'transform images into base64',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['imgbase64'],
+    install_requires=['argparse', 'requests'],
+)
