bfcbbe3a0ac76e7c0fee81cb2e7b82037151f912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:02 +0000

    Add package apiql/0.12.6/apiql-0.12.6.tar.gz

---
+++ packages/apiql/apiql-0.12.6.tar.gz/apiql-0.12.6/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='apiql',
+    version='0.12.6',
+    test_suite='tests',
+    author="Artur Karazniewicz",
+    author_email="karaznie+pip@protonmail.com",
+    description="A simple API Query Language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/karaznie/apiql",
+    packages=setuptools.find_packages(),
+    package_data={'': ['*.txt', '*.md', '*.tokens', '*.interp']},
+    install_requires=[
+        'funcy',
+        'whatever',
+        'antlr4-python3-runtime',
+        'python-dateutil'
+    ],
+    python_requires='>=3.5',
+    keywords='api dynamic query flask bottle django sqlalchemy',
+    project_urls={
+        "Homepage": "https://github.com/akarazniewicz/apiql",
+        "Bug Tracker": "https://github.com/akarazniewicz/apiql/issues",
+        "Documentation": "https://github.com/akarazniewicz/apiql",
+        "Source Code": "https://github.com/akarazniewicz/apiql",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+        "Environment :: Web Environment"
+    ],
+)
