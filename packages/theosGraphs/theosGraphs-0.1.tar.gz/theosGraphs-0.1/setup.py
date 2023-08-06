2ac2ce1efd9ff53897808f10b799223d3662d7b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:26 +0000

    Add package theosGraphs/0.1/theosGraphs-0.1.tar.gz

---
+++ packages/theosGraphs/theosGraphs-0.1.tar.gz/theosGraphs-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='theosGraphs',
+        version='0.1',
+        description='A Graph package for discrete math',
+        url='http://github.com/tlincke125/Data-Structures',
+        author='Theo Lincke',
+        author_email='tlincke@protonmail.com',
+        license='MIT',
+        packages=['theosGraphs'],
+        zip_safe=False)
