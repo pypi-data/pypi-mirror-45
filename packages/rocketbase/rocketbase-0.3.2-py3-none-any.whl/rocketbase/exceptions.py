09bf8e8cbcde6c026b2824f16ce91a277f6eff69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:39 +0000

    Add package rocketbase/0.3.2/rocketbase-0.3.2-py3-none-any.whl

---
+++ packages/rocketbase/rocketbase-0.3.2-py3-none-any.whl/rocketbase/exceptions.py
@@ -0,0 +1,64 @@
+class RocketNotEnoughInfo(Exception):
+    """
+    Exception raised when not enough information is provided to fetch/find a Rocket.
+    """
+
+    def __init__(self, message, errors=None):
+
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+
+class RocketInfoFormat(Exception):
+    """
+    Exception raised when the format of a Rocket's information is not conform.
+    """
+
+    def __init__(self, message, errors=None):
+
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+
+class RocketAPIError(Exception):
+    """
+    Exception raised when the RocketAPI is returning an error code.
+    """
+
+    def __init__(self, message, errors=None):
+
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+
+class RocketNotFound(Exception):
+    """
+    Exception raised when no Rocket is found.
+    """
+
+    def __init__(self, message, errors=None):
+
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+
+class RocketHashNotValid(Exception):
+    """
+    Exception raised when the hash of a Rocket is not valid.
+    """
+
+    def __init__(self, message, errors=None):
+
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+
+class CloudStorageCredentials(Exception):
+    """
+    Exception raised when the retrieval of the credentials for the Cloud Storage failed
+    """
+
+    def __init__(self, message, errors=None):
+
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
