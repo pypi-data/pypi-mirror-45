483f995e0a52dc9ff92aa6522808edf8129b76fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:53 +0000

    Add package psmatching/0.1.dev9/psmatching-0.1.dev9-py3-none-any.whl

---
+++ packages/psmatching/psmatching-0.1.dev9-py3-none-any.whl/psmatching/__init__.py
@@ -0,0 +1,10 @@
+"""
+Here's a placeholder for a description of psmatching
+"""
+
+from match import match
+from utilities import utilities
+import os.path as _osp
+
+pkg_dir = _osp.abspath(_osp.dirname(__file__))
+data_dir = _osp.join(pkg_dir, 'data')
