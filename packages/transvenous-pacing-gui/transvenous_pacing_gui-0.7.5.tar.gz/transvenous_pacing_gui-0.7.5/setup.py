12ca54f4ccc9db6bca614b5dba8e9af0f2b2668d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:00 +0000

    Add package transvenous-pacing-gui/0.7.5/transvenous_pacing_gui-0.7.5.tar.gz

---
+++ packages/transvenous-pacing-gui/transvenous_pacing_gui-0.7.5.tar.gz/transvenous_pacing_gui-0.7.5/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='transvenous_pacing_gui',
+    version='0.7.5',
+    description='Transvenous pacing procedure simulation GUI written in Python 3',
+    long_description=readme,
+    author='Nam Tran, Cooper Pearson, Richie Beck, Marcel Isper, Brianna Cathey',
+    author_email='tranngocnam97@gmail.com',
+    url='https://github.com/omn0mn0m/transvenous_pacing_gui',
+    license="MIT license",
+    packages=find_packages(exclude=('tests', 'docs')),
+    tests_require=['pytest'],
+    extras_require={
+        'testing': ['pytest'],
+    },
+    scripts=['bin/transvenous_pacing_gui']
+)
