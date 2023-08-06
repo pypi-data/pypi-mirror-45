514c4dc598d3e16e9c9821bd68b34a447c463b71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:02 +0000

    Add package alibaba-suanfa/1.0/alibaba_suanfa-1.0.tar.gz

---
+++ packages/alibaba-suanfa/alibaba_suanfa-1.0.tar.gz/alibaba_suanfa-1.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='alibaba_suanfa',
+    version=1.0,
+    description=(
+        '应聘常考算法'
+    ),
+    long_description=open('README.rst').read(),
+    author='Hiang',
+    author_email='xiang_liu2013@163.com',
+    maintainer='hiang',
+    maintainer_email='xiang_liu2013@163.com',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    url='https://github.com/lxgithub24/leetcode.git',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        'nltk==3.4',
+        'scikit-learn==0.20.2',
+    ]
+)
