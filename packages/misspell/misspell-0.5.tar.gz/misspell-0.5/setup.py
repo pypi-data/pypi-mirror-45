8918ee44864807293a065e5459962cb2e39028ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:44 +0000

    Add package misspell/0.5/misspell-0.5.tar.gz

---
+++ packages/misspell/misspell-0.5.tar.gz/misspell-0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='misspell',
+    version='0.5',
+    scripts=['misspell/__init__.py'],
+    author='Vladyslav Burzakovskyy',
+    author_email='mutex-lock@protonmail.com',
+    description='Some functions that introduce random typographical errors in provided text.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/MrMebelMan/misspell',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
+
