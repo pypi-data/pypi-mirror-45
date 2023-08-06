cf74c6d6123766abbcbe3193540e4801e7126c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:28 +0000

    Add package lifx-photons-core/0.13.4/lifx-photons-core-0.13.4.tar.gz

---
+++ packages/lifx-photons-core/lifx-photons-core-0.13.4.tar.gz/lifx-photons-core-0.13.4/photons_transport/__init__.py
@@ -0,0 +1,16 @@
+__doc__ = """
+Core functionality for making photons transports. These are objects responsible
+for getting messages to a device.
+
+Tasks
+-----
+
+See :ref:`tasks`.
+
+.. photons_module_tasks::
+
+Target
+------
+
+.. automodule:: photons_transport.target
+"""
