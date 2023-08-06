383127ebd033e37a5208108c43a5b8acb61be7ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:37 +0000

    Add package life360/3.1.0/life360-3.1.0-py3-none-any.whl

---
+++ packages/life360/life360-3.1.0-py3-none-any.whl/life360/life360.py
@@ -0,0 +1,168 @@
+# This is what requests does.
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+import logging
+import os
+import stat
+
+import requests
+
+from .exceptions import *
+
+_BASE_URL = 'https://api.life360.com/v3/'
+_TOKEN_URL = _BASE_URL + 'oauth2/token.json'
+_CIRCLES_URL = _BASE_URL + 'circles.json'
+_CIRCLE_URL = _BASE_URL + 'circles/{}'
+_CIRCLE_MEMBERS_URL = _CIRCLE_URL + '/members'
+_CIRCLE_PLACES_URL = _CIRCLE_URL + '/places'
+_AUTH_ERRS = (401, 403)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class life360(object):
+    """Life360 API"""
+    def __init__(self, api_token, username, password, timeout=None,
+                 authorization_cache_file=None):
+        self._credentials = {
+            'api_token': api_token,
+            'username': username,
+            'password': password}
+        self._timeout = timeout
+        self._cache_file = authorization_cache_file
+        self._auth = None
+        self._session = requests.Session()
+        self._session.headers.update(
+            {'Accept': 'application/json', 'cache-control': 'no-cache'})
+
+    def _load_authorization(self):
+        if self._cache_file:
+            try:
+                with open(self._cache_file) as f:
+                    cache = json.load(f)
+            except FileNotFoundError:
+                _LOGGER.debug('Authorization cache file does not exist')
+                return
+            except json.JSONDecodeError:
+                credentials = authorization = None
+            except Exception as error:
+                _LOGGER.error(
+                    'Could not load authorization from cache file: %s', error)
+                self._discard_authorization()
+                return
+            else:
+                credentials = cache.get('credentials')
+                authorization = cache.get('authorization')
+            if credentials != self._credentials or authorization is None:
+                _LOGGER.warning(
+                    'Authorization cache file out of date. Reauthorizing')
+                self._discard_authorization()
+            else:
+                self._auth = authorization
+
+    def _save_authorization(self):
+        if self._cache_file:
+            flags = os.O_WRONLY | os.O_CREAT | os.O_EXCL
+            mode = stat.S_IRUSR | stat.S_IWUSR
+            umask = 0o777 ^ mode
+            umask_orig = os.umask(umask)
+            cache = {'credentials': self._credentials,
+                     'authorization': self._auth}
+            try:
+                with open(os.open(
+                        self._cache_file, flags, mode), 'w') as f:
+                    json.dump(cache, f)
+            except Exception as error:
+                _LOGGER.warning(
+                    'Could not save authorization to cache file: %s', error)
+            finally:
+                os.umask(umask_orig)
+
+    def _discard_authorization(self):
+        self._auth = None
+        if self._cache_file:
+            try:
+                os.remove(self._cache_file)
+            except:
+                pass
+
+    def _get_authorization(self):
+        data = {
+            'grant_type': 'password',
+            'username': self._credentials['username'],
+            'password': self._credentials['password'],
+        }
+        try:
+            resp = self._session.post(
+                _TOKEN_URL, data=data, timeout=self._timeout,
+                headers={'Authorization':
+                             'Basic ' + self._credentials['api_token']})
+            resp.raise_for_status()
+        except requests.RequestException as error:
+            _LOGGER.debug(
+                'Error while getting authorization token: %s', error)
+            # Try to return a useful error message.
+            try:
+                err_msg = resp.json()['errorMessage']
+            except (json.JSONDecodeError, ValueError, KeyError):
+                raise CommError(error)
+            if resp.status_code in _AUTH_ERRS and 'login' in err_msg.lower():
+                raise LoginError(err_msg)
+            raise CommError(err_msg)
+        try:
+            resp = resp.json()
+            self._auth = ' '.join([resp['token_type'], resp['access_token']])
+        except (json.JSONDecodeError, ValueError, KeyError):
+            raise Life360Error(
+                'Unexpected response while getting authorization token: '
+                '{}: {}'.format(resp.status_code, resp.text))
+
+        self._save_authorization()
+
+    @property
+    def _authorization(self):
+        if not self._auth:
+            self._load_authorization()
+            if not self._auth:
+                self._get_authorization()
+        return self._auth
+
+    def _get(self, url):
+        try:
+            resp = self._session.get(url, timeout=self._timeout,
+                headers={'Authorization': self._authorization})
+            # If authorization error try regenerating authorization
+            # and sending again.
+            if resp.status_code in (401, 403):
+                _LOGGER.warning('Error %i %s. Reauthorizing',
+                                resp.status_code, resp.reason)
+                self._discard_authorization()
+                resp.request.headers['Authorization'] = self._authorization
+                resp = self._session.send(resp.request)
+                if resp.status_code in (401, 403):
+                    _LOGGER.error('Error %i %s', resp.status_code, resp.reason)
+                    self._discard_authorization()
+            resp.raise_for_status()
+            return resp.json()
+        except (requests.RequestException, json.JSONDecodeError) as error:
+            _LOGGER.debug('Error while getting: %s: %s', url, error)
+            if isinstance(error, requests.RequestException):
+                raise CommError(error)
+            raise Life360Error(
+                'Unexpected response to query: {}: {}'.format(
+                    resp.status_code, resp.text))
+
+    def get_circles(self):
+        return self._get(_CIRCLES_URL)['circles']
+
+    def get_circle(self, circle_id):
+        return self._get(_CIRCLE_URL.format(circle_id))
+
+    def get_circle_members(self, circle_id):
+        return self._get(_CIRCLE_MEMBERS_URL.format(circle_id))['members']
+
+    def get_circle_places(self, circle_id):
+        return self._get(_CIRCLE_PLACES_URL.format(circle_id))['places']
