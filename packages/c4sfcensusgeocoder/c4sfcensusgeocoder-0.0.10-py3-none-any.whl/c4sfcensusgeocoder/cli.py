383a340b18d283cd7cc55e5599cd3552dc0506d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:13 +0000

    Add package c4sfcensusgeocoder/0.0.10/c4sfcensusgeocoder-0.0.10-py3-none-any.whl

---
+++ packages/c4sfcensusgeocoder/c4sfcensusgeocoder-0.0.10-py3-none-any.whl/c4sfcensusgeocoder/cli.py
@@ -0,0 +1,41 @@
+import click
+from . import batch_geocoder as bg
+
+
+@c4sfcensusgeocoder.command(help="Testing Hello World.")
+@click.option(
+    "--test-text",
+    required=True,
+    help='The Text you want to print'
+)
+def test_world(test_text):
+    """Testing Hello World"""
+    print(test_text)
+
+
+@click.group()
+def c4sfcensusgeocoder():
+    """Command-line access to C4sf Census Geocoder.
+    Run ``c4sfcensusgeocoder --help`` for a list of available commands.
+    To get help with a specific command, run ``c4sfcensusgeocoder [COMMAND] --help``."""
+    pass  # pragma: no cover
+
+
+@c4sfcensusgeocoder.command(help="Testing Hello World.")
+@click.option(
+    "--test-text",
+    required=True,
+    help='The Text you want to print'
+)
+def helloworld(test_text):
+    """Testing Hello World"""
+    print(test_text)
+
+
+@click.command()
+def main():
+    c4sfcensusgeocoder()
+
+
+if __name__ == '__main__':  # pragma: no cover
+    main()  # pragma: no cover
