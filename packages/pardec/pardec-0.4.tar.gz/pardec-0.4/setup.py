41139e393aa0cf63d3e00f2f3ec6c6837dea37b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:36 +0000

    Add package pardec/0.4/pardec-0.4.tar.gz

---
+++ packages/pardec/pardec-0.4.tar.gz/pardec-0.4/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='pardec',
+    version='0.4',
+    license='MIT',
+    author='Yunlu Wen',
+    author_email='looooooeee@gmail.com',
+    url='https://github.com/wylswz/ParallelDecoder',
+    keywords=['parallel', 'generator', 'stream'],
+    download_url='https://github.com/wylswz/ParallelDecoder/archive/0.2.tar.gz',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+)
