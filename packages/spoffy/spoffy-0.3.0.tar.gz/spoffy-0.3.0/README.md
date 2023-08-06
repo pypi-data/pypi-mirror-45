98252daaa99a7daa7e6fe4f401ac8ae46d272bd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:55 +0000

    Add package spoffy/0.3.0/spoffy-0.3.0.tar.gz

---
+++ packages/spoffy/spoffy-0.3.0.tar.gz/spoffy-0.3.0/README.md
@@ -0,0 +1,16 @@
+# Spoffy
+
+The IDE friendly sync and async [Spotify API](https://developer.spotify.com/documentation/web-api/) wrapper for python.
+
+Read the docs: https://spoffy.readthedocs.io
+
+
+# Install
+
+```
+pip install spoffy
+```
+Python3.6 and newer are supported
+
+
+
---
+++ packages/spoffy/spoffy-0.3.0.tar.gz/spoffy-0.3.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['spoffy', 'spoffy.client', 'spoffy.io', 'spoffy.models', 'spoffy.modules']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['attrs>=19,<20', 'cattrs>=0.9,<0.10', 'urlobject>=2,<3']
+
+setup_kwargs = {
+    'name': 'spoffy',
+    'version': '0.3.0',
+    'description': 'Spotify API client with async and sync support',
+    'long_description': '# Spoffy\n\nThe IDE friendly sync and async [Spotify API](https://developer.spotify.com/documentation/web-api/) wrapper for python.\n\nRead the docs: https://spoffy.readthedocs.io\n\n\n# Install\n\n```\npip install spoffy\n```\nPython3.6 and newer are supported\n\n\n\n',
+    'author': 'Steinthor Palsson',
+    'author_email': 'steini90@gmail.com',
+    'url': 'https://github.com/steinitzu/spoffy',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
