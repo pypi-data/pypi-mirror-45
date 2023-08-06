926844ded398daadb222b15a0a71fccbf24cf89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:10 +0000

    Add package jupyterlmod/1.7.4/jupyterlmod-1.7.4-py3-none-any.whl

---
+++ packages/jupyterlmod/jupyterlmod-1.7.4-py3-none-any.whl/jupyterlmod/handler.py
@@ -0,0 +1,48 @@
+import json
+
+import tornado.web
+import lmod
+
+from functools import partial
+from notebook.base.handlers import IPythonHandler
+
+ACTIONS = {
+    "avail"    : lmod.avail,
+    "list"     : partial(lmod.list, hide_hidden=True),
+    "freeze"   : lmod.freeze,
+    "show"     : lmod.show,
+    "load"     : lmod.load,
+    "unload"   : lmod.unload,
+    "purge"    : lmod.purge,
+    "savelist" : lmod.savelist,
+    "save"     : lmod.save,
+    "restore"  : lmod.restore,
+    "reset"    : lmod.reset
+}
+
+class LmodActionHandler(IPythonHandler):
+    @tornado.web.authenticated
+    def get(self, action):
+        func = ACTIONS.get(action, None)
+        if func:
+            args = self.get_arguments("args")
+            result = func(*args)
+            self.finish(json.dumps(result))
+
+    @tornado.web.authenticated
+    def post(self, action):
+        func = ACTIONS.get(action, None)
+        if func:
+            args = self.get_arguments('args')
+            if args:
+                func(*args)
+                self.finish(json.dumps('SUCCESS'))
+
+_action_regex = r"/lmod/(?P<action>{})".format("|".join(ACTIONS.keys()))
+default_handlers = [(_action_regex, LmodActionHandler)]
+
+if __name__ == "__main__":
+    import tornado.ioloop
+    app = tornado.web.Application([ (r"/", LmodActionHandler), ])
+    app.listen(12345)
+    tornado.ioloop.IOLoop.current().start()
