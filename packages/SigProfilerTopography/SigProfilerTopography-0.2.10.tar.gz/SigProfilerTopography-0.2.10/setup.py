37aa096f19feff6c4eecf292768292fe21aaadd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:13 +0000

    Add package SigProfilerTopography/0.2.10/SigProfilerTopography-0.2.10.tar.gz

---
+++ packages/SigProfilerTopography/SigProfilerTopography-0.2.10.tar.gz/SigProfilerTopography-0.2.10/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+
+setup(name="SigProfilerTopography",
+    version="0.2.10",
+    author="Burcak Otlu",
+    author_email="burcakotlu@eng.ucsd.edu",
+    description="SigProfilerTopography provides topography analyses for substitutions, dinucleotides and indels for each sample and all samples pooled.",
+    url="https://github.com/AlexandrovLab/SigProfilerTopography",
+    license='UCSD',
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib==2.2.2",
+        "scipy==1.1.0",
+        "pandas==0.23.4",
+        "numpy==1.14.3",
+        "statsmodels==0.9.0",
+        "twobitreader"],
+    include_package_data=True,
+	zip_safe=False)
