3c1fcbaabf8b78279e5e61ba18d576423c649368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:07 +0000

    Add package service/0.5.2/service-0.5.2.tar.gz

---
+++ packages/service/service-0.5.2.tar.gz/service-0.5.2/README.rst
@@ -0,0 +1,50 @@
+Python background services made easy
+####################################
+
+.. image:: https://travis-ci.org/torfsen/service.svg?branch=master
+   :target: https://travis-ci.org/torfsen/service
+
+The Python ``service`` package makes it easy to write Unix *services*, i.e.
+background processes ("daemons") that are controlled by a foreground
+application (e.g. a console script)::
+
+    from service import Service
+
+    class MyService(Service):
+        def run(self):
+            while True:
+                do_work()
+
+    if __name__ == '__main__':
+        import sys
+
+        if len(sys.argv) != 2:
+            sys.exit('Syntax: %s COMMAND' % sys.argv[0])
+
+        cmd = sys.argv[1].lower()
+        service = MyService('my_service', pid_dir='/tmp')
+
+        if cmd == 'start':
+            service.start()
+        elif cmd == 'stop':
+            service.stop()
+        elif cmd == 'status':
+            if service.is_running():
+                print "Service is running."
+            else:
+                print "Service is not running."
+        else:
+            sys.exit('Unknown command "%s".' % cmd)
+
+For details, please see the documentation_.
+
+.. _documentation: http://python-service.readthedocs.org/
+
+
+Installation
+============
+Installation is easy via pip_::
+
+    pip install service
+
+.. _pip: https://pip.pypa.io/en/latest/index.html
---
+++ packages/service/service-0.5.2.tar.gz/service-0.5.2/test/test_service.py
@@ -0,0 +1,437 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2014-2019 Florian Brucker (www.florianbrucker.de)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""
+Tests for the ``service`` module.
+"""
+
+import errno
+import logging
+import os
+import os.path
+import signal
+import subprocess
+import sys
+import tempfile
+import threading
+import time
+
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
+
+from nose.tools import eq_ as eq, ok_ as ok, raises
+import psutil
+
+import service
+
+from .helpers import get_current_case
+
+
+NAME = 'python-service-test-daemon'
+
+LOG_FILE = os.path.join(os.path.dirname(__file__),
+                        'test-%d.%d.log' % sys.version_info[:2])
+try:
+    os.unlink(LOG_FILE)
+except OSError as e:
+    if e.errno != errno.ENOENT:
+        raise
+
+PID_DIR = '/tmp'
+
+DELAY = 5
+
+# Timeout for waiting for a service to start/stop. Rather high because
+# on Travis services sometimes take ages to start.
+TIMEOUT = 20
+
+
+def get_processes():
+    """
+    Return a list of all processes of the test service.
+    """
+    return [p for p in psutil.process_iter() if p.name() == NAME]
+
+
+def kill_processes():
+    """
+    Kill all test service processes and remove the PID file.
+    """
+    for process in get_processes():
+        process.kill()
+    try:
+        os.remove(os.path.join(PID_DIR, NAME + '.pid'))
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
+
+
+def is_running():
+    """
+    Check if the test daemon is running.
+    """
+    return bool(get_processes())
+
+
+def pid_file_exists():
+    """
+    Check if the daemon's PID file exists.
+    """
+    return os.path.isfile(os.path.join(PID_DIR, NAME + '.pid'))
+
+
+def assert_running():
+    """
+    Assert that the test daemon is running.
+    """
+    ok(is_running(), 'Process is not running but should.')
+    ok(pid_file_exists(), "PID file doesn't exist but should.")
+
+
+def assert_not_running():
+    """
+    Assert that the test daemon is not running.
+    """
+    ok(not is_running(), "Process is running but shouldn't.")
+    ok(not pid_file_exists(), "PID file exists but shouldn't.")
+
+
+class BasicService(service.Service):
+    """
+    Service base class.
+
+    Sets the service name and uses a temporary directory for PID files
+    to avoid the necessity of root privileges.
+    """
+    def __init__(self):
+        super(BasicService, self).__init__(NAME, pid_dir=PID_DIR)
+        formatter = logging.Formatter('%(created)f: %(message)s')
+        handler = logging.FileHandler(LOG_FILE)
+        handler.setFormatter(formatter)
+        self.logger.handlers[:] = [handler]
+        self.logger.setLevel(service.SERVICE_DEBUG)
+
+    def start(self, block=False):
+        value = super(BasicService, self).start(block=block)
+        self._debug('start(block={}) returns {}'.format(block, value))
+        return value
+
+    def stop(self, block=False):
+        value = super(BasicService, self).stop(block=block)
+        self._debug('stop(block={}) returns {}'.format(block, value))
+        return value
+
+
+class WaitingService(BasicService):
+    """
+    Test service that waits until shutdown via SIGTERM.
+    """
+    def run(self):
+        self.logger.info('start')
+        self.wait_for_sigterm()
+        self.logger.info('end')
+
+
+class ForeverService(BasicService):
+    """
+    A service that runs forever.
+    """
+    def run(self):
+        self.logger.info('start')
+        while True:
+            time.sleep(1)
+
+
+class OneTimeLock(object):
+    """
+    Pseudo-lock that can only be acquired once.
+
+    See ``FailingService``.
+    """
+    def __init__(self, *args, **kwargs):
+        self._acquired = False
+
+    def acquire(self, *args, **kwargs):
+        if self._acquired:
+            raise RuntimeError('Oops')
+        self._acquired = True
+
+    def release(self, *args, **kwargs):
+        pass
+
+    def read_pid(self, *args, **kwargs):
+        return None
+
+
+class FailingService(BasicService):
+    """
+    A service that fails to start.
+
+    This is a hack that works by using a PID lock file object which can
+    only be acquired once. This is necessary to create a daemon that
+    fails reliably before the daemon process exists.
+    """
+    def __init__(self, *args, **kwargs):
+        super(FailingService, self).__init__(*args, **kwargs)
+        self.pid_file = OneTimeLock()
+
+
+class CallbackService(BasicService):
+    """
+    A service that calls a callback in ``run``.
+    """
+    def __init__(self, run=None, *args, **kwargs):
+        super(CallbackService, self).__init__(*args, **kwargs)
+        self._run_callback = run
+
+    def run(self):
+        self.logger.info('start')
+        if self._run_callback:
+            self._run_callback(self)
+        self.logger.info('end')
+
+
+def start(service):
+    """
+    Start a service and wait until it's running.
+    """
+    ok(service.start(block=TIMEOUT))
+    assert_running()
+    return service
+
+
+class TestService(object):
+    """
+    Tests for ``service.Service``.
+    """
+    def get_log(self):
+        with open(self.logfile.name) as f:
+            return f.read()
+
+    def assert_log_contains(self, text, msg=None):
+        if not msg:
+            msg = 'Log does not contain "%s":\n\n%s' % (text, self.get_log())
+        ok(text in self.get_log(), msg)
+
+    def setup(self):
+        with open(LOG_FILE, 'a') as f:
+            f.write('\n\n{}\n'.format(get_current_case()))
+        kill_processes()
+        self.logfile = tempfile.NamedTemporaryFile(delete=False)
+        self.logfile.close()
+        self.handler = logging.FileHandler(self.logfile.name)
+
+    def teardown(self):
+        kill_processes()
+        try:
+            os.unlink(self.logfile.name)
+        except OSError:
+            pass
+
+    def test_start(self):
+        """
+        Test ``Service.start``.
+        """
+        start(WaitingService())
+
+    def test_start_timeout_ok(self):
+        """
+        Test ``Service.start`` with a timeout.
+        """
+        ok(WaitingService().start(block=TIMEOUT))
+
+    def test_start_timeout_fail(self):
+        """
+        Test ``Service.start`` with a timeout and a failing daemon.
+        """
+        ok(not FailingService().start(block=0.1))
+
+    def test_stop(self):
+        """
+        Test ``Service.stop``.
+        """
+        start(WaitingService()).stop()
+        time.sleep(DELAY)
+        assert_not_running()
+
+    def test_stop_timeout_ok(self):
+        """
+        Test ``Service.stop`` with a timeout.
+        """
+        ok(start(WaitingService()).stop(block=TIMEOUT))
+
+    def test_stop_timeout_fail(self):
+        """
+        Test ``Service.stop`` with a timeout and stuck daemon.
+        """
+        ok(not start(ForeverService()).stop(block=0.1))
+
+    def test_kill(self):
+        """
+        Test ``Service.kill``.
+        """
+        start(ForeverService()).kill()
+        time.sleep(DELAY)
+        assert_not_running()
+
+    def test_kill_timeout_ok(self):
+        """
+        Test ``Service.kill`` with a timeout.
+        """
+        ok(start(ForeverService()).kill(block=TIMEOUT))
+        assert_not_running()
+
+    def test_kill_timeout_fail(self):
+        """
+        Test ``Service.kill`` with too short a timeout.
+        """
+        os_kill = os.kill
+
+        def kill_mock(pid, sig):
+            if sig != signal.SIGKILL:
+                return os_kill(pid, sig)
+
+        with mock.patch('os.kill', side_effect=kill_mock):
+            ok(not start(ForeverService()).kill(block=0.1))
+
+    @raises(ValueError)
+    def test_stop_not_running(self):
+        """
+        Test stopping a service that is not running.
+        """
+        WaitingService().stop()
+
+    @raises(ValueError)
+    def test_kill_not_running(self):
+        """
+        Test killing a service that is not running.
+        """
+        WaitingService().kill()
+
+    @raises(ValueError)
+    def test_start_already_running(self):
+        """
+        Test starting a service that is already running.
+        """
+        start(WaitingService()).start()
+
+    def test_is_running(self):
+        """
+        Test ``Service.is_running``.
+        """
+        service = WaitingService()
+        ok(not service.is_running())
+        start(service)
+        ok(service.is_running())
+
+    @raises(IOError)
+    def test_no_lock_permissions(self):
+        """
+        Test starting a service without necessary permissions.
+        """
+        service.Service(NAME).start()
+
+    def test_exception_in_run(self):
+        """
+        Test that exceptions in ``run`` are handled correctly.
+        """
+        def run(service):
+            service.logger.addHandler(logging.FileHandler(self.logfile.name))
+            raise Exception('FOOBAR')
+        CallbackService(run).start(block=TIMEOUT)
+        assert_not_running()
+        ok(not pid_file_exists())
+        self.assert_log_contains('FOOBAR')
+
+    def test_files_preserve(self):
+        """
+        Test file handle preservation.
+        """
+        class FileHandleService(BasicService):
+            def __init__(self):
+                super(FileHandleService, self).__init__()
+                self.f = tempfile.NamedTemporaryFile(mode='wt', delete=False)
+                self.files_preserve = [self.f]
+            def run(self):
+                self.f.write('foobar')
+                self.f.close()
+                self.wait_for_sigterm()
+
+        service = FileHandleService()
+        start(service)
+        try:
+            service.stop(block=TIMEOUT)
+            ok(os.path.isfile(service.f.name))
+            with open(service.f.name, 'r') as f:
+                eq(f.read(), 'foobar')
+        finally:
+            os.unlink(f.name)
+
+    def test_builtin_log_handlers_file_handles_are_preserved(self):
+        """
+        Test that file handles of built-in log handlers are preserved.
+        """
+        class LoggingService(BasicService):
+            def __init__(self):
+                super(LoggingService, self).__init__()
+                self.f = tempfile.NamedTemporaryFile(delete=False)
+                self.f.close()
+                self.logger.addHandler(logging.FileHandler(self.f.name))
+            def run(self):
+                self.logger.warn('foobar')
+                self.wait_for_sigterm()
+
+        service = LoggingService()
+        start(service)
+        try:
+            service.stop(block=TIMEOUT)
+            ok(os.path.isfile(service.f.name))
+            with open(service.f.name, 'r') as f:
+                ok('foobar' in f.read())
+        finally:
+            os.unlink(service.f.name)
+
+    def test_sys_exit(self):
+        """
+        Test that sys.exit is handled correctly.
+        """
+        def run(service):
+            import sys
+            sys.exit()
+        CallbackService(run).start(block=TIMEOUT)
+        assert_not_running()
+
+    def test_parent_does_not_remove_pid_file(self):
+        """
+        Test that the parent does not remote the PID file unintentionally.
+        """
+        # This could happen with older versions of the ``pid`` module,
+        # which installed an ``atexit`` handler that automatically removed
+        # the PID file. That mechanism could be triggered from within the
+        # parent process under certain circumstances. See
+        # https://github.com/torfsen/service/issues/10
+        subprocess.check_call(['python', '-m', 'test.start_waiting_service'])
+        ok(os.path.exists(WaitingService().pid_file._path),
+           'PID file does not exist')
+
