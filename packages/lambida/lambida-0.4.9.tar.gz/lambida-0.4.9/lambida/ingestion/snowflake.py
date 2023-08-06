60d0dbb5451b03d1e82cfdafb6508a393f71b33d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:54:43 +0000

    Add package lambida/0.4.9/lambida-0.4.9.tar.gz

---
+++ packages/lambida/lambida-0.4.9.tar.gz/lambida-0.4.9/lambida/ingestion/snowflake.py
@@ -0,0 +1,35 @@
+"""Upload Client for AWS Event Data to S3."""
+# -*- coding: utf-8 -*-
+import snowflake.connector
+
+class SnowflakeJsonInsert(object):
+    """A Snowflake Json Insert Operator."""
+
+    def __init__(self, config):
+        """A handler init."""
+        self.log = config["LOG"]
+        self.ctx = snowflake.connector.connect(
+            user=config["SNOWFLAKE"]["USER"],
+            password=config["SNOWFLAKE"]["PASSWORD"],
+            account=config["SNOWFLAKE"]["ACCOUNT"],
+            warehouse="STITCH",
+            database=config["SNOWFLAKE"]["DATABASE"],
+            schema=config["SNOWFLAKE"]["SCHEMA"])
+        self.table = config["SNOWFLAKE"]["TABLE"]
+        self.ctx.execute_string(
+            "create table if not exists %s (scr variant)" % self.table)
+
+
+    def close(self):
+        self.ctx.close()
+
+
+    def insert(self, message):
+        cursor_list = self.ctx.execute_string("insert into %s (select PARSE_JSON('%s'))" % (self.table, message))
+        for cursor in cursor_list:
+            for row in cursor:
+                self.log.info('Ingested Message into Snowflake: {}'.format(row))
+
+
+
+
