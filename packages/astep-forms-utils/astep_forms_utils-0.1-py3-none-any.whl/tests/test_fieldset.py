1d70f75fc7580dc11020bba11c3f7373d8f75f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:43 +0000

    Add package astep-forms-utils/0.1/astep_forms_utils-0.1-py3-none-any.whl

---
+++ packages/astep-forms-utils/astep_forms_utils-0.1-py3-none-any.whl/tests/test_fieldset.py
@@ -0,0 +1,15 @@
+from astep_form_utils import FieldSet, StringField
+
+
+def test_validate_positive():
+    field_set = FieldSet([
+        StringField("test")
+    ])
+    assert field_set.is_valid({"test": "foo"})
+
+
+def test_validate_negative():
+    field_set = FieldSet([
+        StringField("test")
+    ])
+    assert not field_set.is_valid({"test": 1})
