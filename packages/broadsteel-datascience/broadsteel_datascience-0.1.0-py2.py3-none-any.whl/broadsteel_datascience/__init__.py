577645a9f00785ca041275679b7dd22dfd3ca6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:05 +0000

    Add package broadsteel-datascience/0.1.0/broadsteel_datascience-0.1.0-py2.py3-none-any.whl

---
+++ packages/broadsteel-datascience/broadsteel_datascience-0.1.0-py2.py3-none-any.whl/broadsteel_datascience/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for BroadSteel DatasScience."""
+
+__author__ = """James Irving, Michael Moravetz"""
+__email__ = 'james.irving.phd@outlook.com'
+__version__ = '0.1.0'
+from .broadsteel_datascience import *
