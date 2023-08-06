b31d89c836981b2f69baf37de1f3b6ac04436830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:41 +0000

    Add package intouch-queryset-csv/0.3.0/intouch_queryset_csv-0.3.0-py3-none-any.whl

---
+++ packages/intouch-queryset-csv/intouch_queryset_csv-0.3.0-py3-none-any.whl/intouch/queryset_csv/tests.py
@@ -0,0 +1,48 @@
+import django
+from django.core.management.color import no_style
+from django.db import models, connection
+from django.db import models
+from django.test import TransactionTestCase
+from django.test.client import Client
+from django.conf import settings
+from intouch.queryset_csv.shortcuts import queryset_as_csv_response
+
+django.setup()
+
+class QuerysetCsvTests(TransactionTestCase):
+    
+    class TestModel(models.Model):
+        '''
+        Basic django model for use with this test case
+        '''
+        class Meta:
+            app_label = "queryset_csv.tests"
+        
+        number = models.IntegerField()
+    
+    def setUp(self):
+        self._style = no_style()
+        with connection.schema_editor() as schema_editor:
+            schema_editor.create_model(self.TestModel)
+
+    
+    
+    def tearDown(self):
+        with connection.schema_editor() as schema_editor:
+            schema_editor.delete_model(self.TestModel)
+            
+    
+    def test_queryset_csv_response(self):
+        for i in range(0, 10):
+            self.TestModel.objects.create(number=i)
+            
+        expected = b"""Id,Number\r\n1,0\r\n2,1\r\n3,2\r\n4,3\r\n5,4\r\n6,5\r\n7,6\r\n8,7\r\n9,8\r\n10,9\r\n"""
+        expected_filename = "some_file.csv"
+        
+        response = queryset_as_csv_response(self.TestModel.objects.all(), expected_filename)
+        self.assertEqual(expected, response.content, "Comparing response body (as bytes)")
+        self.assertEqual("text/csv", response["Content-Type"], "Ensure content type is csv")
+        self.assertEqual("attachment; filename={}".format(expected_filename), response["content-disposition"], "Ensuring filename is correctly propogated")
+
+
+        
---
+++ packages/intouch-queryset-csv/intouch_queryset_csv-0.3.0-py3-none-any.whl/intouch/queryset_csv/views.py
@@ -0,0 +1,19 @@
+from django.apps.registry import apps
+from django.views.generic.base import View
+from intouch.queryset_csv.shortcuts import queryset_as_csv_response
+from django.core.exceptions import PermissionDenied
+
+
+class ModelAsCsvView(View):
+    
+    def get(self, request, app, model):
+        
+        Model = apps.get_model(app, model)
+        
+        if self.has_export_permission(request.user, Model):
+            return queryset_as_csv_response(Model.objects.all(), is_stream=True)
+        else:
+            raise PermissionDenied
+        
+    def has_export_permission(self, user, Model):
+        return True  # TODO check for permission
