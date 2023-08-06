cbd564ace7e1747f294511e042e21b2c14fc5498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:38 +0000

    Add package lambida/0.3.6/lambida-0.3.6.tar.gz

---
+++ packages/lambida/lambida-0.3.6.tar.gz/lambida-0.3.6/lambida/ingestion/aws_s3.py
@@ -0,0 +1,42 @@
+"""Upload Client for AWS Event Data to S3."""
+# -*- coding: utf-8 -*-
+import json
+import boto3
+import datetime
+import lambida.datazone.utils as utils
+
+
+s3_resource = boto3.resource('s3')
+
+
+class S3ResourceClient(object):
+    """A client to operate on S3 resource actions."""
+
+    def __init__(self, context, config):
+        """A handler init."""
+        self.function_name = context.function_name
+        self.aws_request_id = context.aws_request_id
+        self.filename = self.aws_request_id + "_" + \
+            utils.get_timestamp() + ".json"
+        self.location = "lambda/"
+        self.log = config["LOG"]
+        self.bucket_name = config["BUCKET"]
+
+
+    def get_prefix(self, key):
+        """Return Prefix."""
+        return self.location +  \
+            "{}/".format(key) +  \
+            utils.get_table_partition_by_day()
+
+
+    def s3_put_request(self, data, key):
+        """Upload Data to S3."""
+        s3_object = \
+            s3_resource.Object(
+                bucket_name=self.bucket_name, 
+                key=self.function_name+self.get_prefix(key)+self.filename)
+        response = s3_object.put(Body=data)
+        self.log.info('S3 Put Requests: {}'.format(s3_object))
+        return s3_object, response
+
