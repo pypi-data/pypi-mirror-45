2c9cf974d2b59180acca4e898089aaec28a7547b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:44 +0000

    Add package printc/0.2/printc-0.2.tar.gz

---
+++ packages/printc/printc-0.2.tar.gz/printc-0.2/printc/print.py
@@ -0,0 +1,30 @@
+def print_red(str):
+	# 绿色字体
+	print('\033[1;31m')
+	print(str)
+	print('\033[0m')
+def print_green(str):
+	# 绿色字体
+	print('\033[1;32m')
+	print(str)
+	print('\033[0m')
+def print_blue(str):
+	# 蓝色字体
+	print('\033[1;34m')
+	print(str)
+	print('\033[0m')
+def print_yellow(str):
+	# 黄字下划线
+	print('\033[1;33m')
+	print(str)
+	print('\033[0m')
+def print_black(str):
+	# 红底黑字
+	print('\033[1;30;41m')
+	print(str)
+	print('\033[0m')
+def print_white(str):
+	# 白底黑字
+	print('\033[1;30;47m')
+	print(str)
+	print('\033[0m')
---
+++ packages/printc/printc-0.2.tar.gz/printc-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='printc',
+      version='0.2',
+      description='Perform color output',
+      author='xuyiwen',
+      author_email='daqvha@163.com',
+      packages=['printc'],
+      zip_safe=False)
