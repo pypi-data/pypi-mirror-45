373b2c3c16d27c9d96ce6af2b25dded4a24cf194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:26 +0000

    Add package GameBenchAPI-PyClient-BigFish/0.1.1/GameBenchAPI-PyClient-BigFish-0.1.1.tar.gz

---
+++ packages/GameBenchAPI-PyClient-BigFish/GameBenchAPI-PyClient-BigFish-0.1.1.tar.gz/GameBenchAPI-PyClient-BigFish-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name='GameBenchAPI-PyClient-BigFish',
+    version='0.1.1',
+    description='A GameBench API Library Client.',
+    url='https://github.com/bigfishgames/GameBenchAPI-PyClient',
+    author='Big Fish Games, Inc.',
+    author_email='',
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'requests-mock>=1.5.2',
+        'pandas>=0.24.2',
+        'coverage>=4.5.3',
+        'urllib3>=1.24.2,<1.25',
+    ]
+)
+
+
