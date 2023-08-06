b5922bf944538b28b4f8b3d20ecebc53517018d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:52 +0000

    Add package AgricolAI/0.0.2/AgricolAI-0.0.2-py3-none-any.whl

---
+++ packages/AgricolAI/AgricolAI-0.0.2-py3-none-any.whl/AgricolAI/__init__.py
@@ -0,0 +1,6 @@
+__content__=["Comp_Finder", "Field_Segmentation", "Handy"]
+__author__="Authored by WSU James Chen"
+
+from . import Comp_Finder
+from . import Field_Segmentation
+from . import Handy
