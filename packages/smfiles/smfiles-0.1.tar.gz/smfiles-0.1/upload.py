69b977f207220d4b7ce01053b6c40e35072b742d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:27 +0000

    Add package smfiles/0.1/smfiles-0.1.tar.gz

---
+++ packages/smfiles/smfiles-0.1.tar.gz/smfiles-0.1/upload.py
@@ -0,0 +1,11 @@
+﻿from setuptools import setup
+
+setup(
+name='smfiles', 
+version='0.1',
+author='张乐涛', 
+author_email='1668151593@qq.com', 
+url='https://github.com/zhangletao/smfiles', 
+description='读取.sm文件', 
+packages=['smfiles'], 
+install_requires=[])
