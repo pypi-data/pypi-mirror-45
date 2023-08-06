789b32432045278b42b05bd25fd3b0f2dd9927f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:11 +0000

    Add package mysqlutil/1.1.2/mysqlutil-1.1.2.tar.gz

---
+++ packages/mysqlutil/mysqlutil-1.1.2.tar.gz/mysqlutil-1.1.2/setup.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='mysqlutil',
+    description='util for mysql',
+    url='',
+    version='1.1.2',
+    license='MIT',
+    packages=find_packages()
+    # packages=('mysqlutil')
+)
