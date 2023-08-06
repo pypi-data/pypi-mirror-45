6e8b620f202c6e5c97b10a5af087f1f0289eee0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:03 +0000

    Add package buzzsprout/0.2/buzzsprout-0.2.tar.gz

---
+++ packages/buzzsprout/buzzsprout-0.2.tar.gz/buzzsprout-0.2/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.7
+from setuptools import setup
+
+setup(name='buzzsprout',
+      version='0.2',
+      description='Buzzsprout Podcasts Python class',
+      url='https://github.com/Harrtron/buzzsprout-python',
+      author='Harley Thorne',
+      author_email='harleyjthorne@gmail.com',
+      license='MIT',
+      packages=['buzzsprout'],
+      python_required='>3.7',
+      zip_safe=False)
