9298963d14d5072748570f6d0c3ed28ff823b3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:29 +0000

    Add package openpyscad/0.3.1/OpenPySCAD-0.3.1-py3-none-any.whl

---
+++ packages/openpyscad/OpenPySCAD-0.3.1-py3-none-any.whl/openpyscad/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+
+
+from .modifiers import *
+from .base import *
+from .shapes_3d import *
+from .shapes_2d import *
+from .boolean import *
+from .transformations import *
+from .custom2dshapes import Custom2dShapes
+
+__name__ = "OpenPySCAD"
+__version__ = "0.3.1"
+__author__ = "Takuro Wada"
+__email__ = "taxpon@gmail.com"
+__url__ = "https://github.com/taxpon/openpyscad"
+__license__ = "MIT"
