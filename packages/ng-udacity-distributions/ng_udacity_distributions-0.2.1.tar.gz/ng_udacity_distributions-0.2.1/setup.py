04a15936e93eafd32917848f75876a410531e500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:12 +0000

    Add package ng-udacity-distributions/0.2.1/ng_udacity_distributions-0.2.1.tar.gz

---
+++ packages/ng-udacity-distributions/ng_udacity_distributions-0.2.1.tar.gz/ng_udacity_distributions-0.2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="ng_udacity_distributions",
+      version="0.2.1",
+      description="A simple package for probability ng_udacity_distributions.",
+      package=['ng_udacity_distributions'],
+      author="Nathan Gilbert",
+      author_email="nathan.gilbert@gmail.com",
+      url="https://github.com/nathan-gilbert/udacity-mlnd",
+      zip_safe=False)
