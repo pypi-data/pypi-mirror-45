c0cdf87f19783ab1d8d651a4ba114a118ac5a047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:47 +0000

    Add package opentrons/3.8.3/opentrons-3.8.3-py2.py3-none-any.whl

---
+++ packages/opentrons/opentrons-3.8.3-py2.py3-none-any.whl/opentrons/legacy_api/api.py
@@ -0,0 +1,250 @@
+from . import (robot as _robot_module,
+               instruments as inst,
+               containers as cnt,
+               modules)
+from opentrons.config import pipette_config
+
+# Ignore the type here because well, this is exactly why this is the legacy_api
+robot = _robot_module.Robot()  # type: ignore
+modules.provide_singleton(robot)
+
+
+def reset():
+    robot.reset()
+    return robot
+
+
+class ContainersWrapper(object):
+    def __init__(self, robot):
+        self.robot = robot
+
+    def create(self, *args, **kwargs):
+        return cnt.create(*args, **kwargs)
+
+    def list(self, *args, **kwargs):
+        return cnt.list(*args, **kwargs)
+
+    def load(self, *args, **kwargs):
+        return cnt.load(self.robot, *args, **kwargs)
+
+
+class InstrumentsWrapper(object):
+    def __init__(self, robot):
+        self.robot = robot
+
+    def Pipette(self, *args, **kwargs):
+        """
+        Deprecated -- do not use this constructor directly. Use the model-
+        specific constructors available in this module.
+        """
+        return inst.Pipette(self.robot, *args, **kwargs)
+
+    def _pipette_details(self, mount, name_or_model):
+        pipette_model_version = self.retrieve_version_number(
+            mount, name_or_model)
+        attached = self.robot.get_attached_pipettes()
+        if attached[mount]['model'] == pipette_model_version\
+           and attached[mount]['id']\
+           and attached[mount]['id'] != 'uncommissioned':
+            pip_id = attached[mount]['id']
+        else:
+            pip_id = None
+        return (pipette_model_version, pip_id)
+
+    def P10_Single(
+            self,
+            mount,
+            trash_container='',
+            tip_racks=[],
+            aspirate_flow_rate=None,
+            dispense_flow_rate=None,
+            min_volume=None,
+            max_volume=None):
+        return self.pipette_by_name(mount, 'p10_single',
+                                    trash_container, tip_racks,
+                                    aspirate_flow_rate, dispense_flow_rate,
+                                    min_volume, max_volume)
+
+    def P10_Multi(
+            self,
+            mount,
+            trash_container='',
+            tip_racks=[],
+            aspirate_flow_rate=None,
+            dispense_flow_rate=None,
+            min_volume=None,
+            max_volume=None):
+        return self.pipette_by_name(mount, 'p10_multi',
+                                    trash_container, tip_racks,
+                                    aspirate_flow_rate, dispense_flow_rate,
+                                    min_volume, max_volume)
+
+    def P50_Single(
+            self,
+            mount,
+            trash_container='',
+            tip_racks=[],
+            aspirate_flow_rate=None,
+            dispense_flow_rate=None,
+            min_volume=None,
+            max_volume=None):
+        return self.pipette_by_name(mount, 'p50_single',
+                                    trash_container, tip_racks,
+                                    aspirate_flow_rate, dispense_flow_rate,
+                                    min_volume, max_volume)
+
+    def P50_Multi(
+            self,
+            mount,
+            trash_container='',
+            tip_racks=[],
+            aspirate_flow_rate=None,
+            dispense_flow_rate=None,
+            min_volume=None,
+            max_volume=None):
+        return self.pipette_by_name(mount, 'p50_multi',
+                                    trash_container, tip_racks,
+                                    aspirate_flow_rate, dispense_flow_rate,
+                                    min_volume, max_volume)
+
+    def P300_Single(
+            self,
+            mount,
+            trash_container='',
+            tip_racks=[],
+            aspirate_flow_rate=None,
+            dispense_flow_rate=None,
+            min_volume=None,
+            max_volume=None):
+        return self.pipette_by_name(mount, 'p300_single',
+                                    trash_container, tip_racks,
+                                    aspirate_flow_rate, dispense_flow_rate,
+                                    min_volume, max_volume)
+
+    def P300_Multi(
+            self,
+            mount,
+            trash_container='',
+            tip_racks=[],
+            aspirate_flow_rate=None,
+            dispense_flow_rate=None,
+            min_volume=None,
+            max_volume=None):
+        return self.pipette_by_name(mount, 'p300_multi',
+                                    trash_container, tip_racks,
+                                    aspirate_flow_rate, dispense_flow_rate,
+                                    min_volume, max_volume)
+
+    def P1000_Single(
+            self,
+            mount,
+            trash_container='',
+            tip_racks=[],
+            aspirate_flow_rate=None,
+            dispense_flow_rate=None,
+            min_volume=None,
+            max_volume=None):
+        return self.pipette_by_name(mount, 'p1000_single',
+                                    trash_container, tip_racks,
+                                    aspirate_flow_rate, dispense_flow_rate,
+                                    min_volume, max_volume)
+
+    def pipette_by_name(
+            self,
+            mount,
+            name_or_model,
+            trash_container='',
+            tip_racks=[],
+            aspirate_flow_rate=None,
+            dispense_flow_rate=None,
+            min_volume=None,
+            max_volume=None):
+        pipette_model_version, pip_id = self._pipette_details(
+            mount, name_or_model)
+        config = pipette_config.load(pipette_model_version, pip_id)
+
+        return self._create_pipette_from_config(
+            config=config,
+            mount=mount,
+            name=pipette_model_version,
+            trash_container=trash_container,
+            tip_racks=tip_racks,
+            aspirate_flow_rate=aspirate_flow_rate,
+            dispense_flow_rate=dispense_flow_rate,
+            min_volume=min_volume,
+            max_volume=max_volume)
+
+    def _create_pipette_from_config(
+            self,
+            config,
+            mount,
+            name,
+            trash_container='',
+            tip_racks=[],
+            aspirate_flow_rate=None,
+            dispense_flow_rate=None,
+            min_volume=None,
+            max_volume=None):
+
+        if aspirate_flow_rate is not None:
+            config = config._replace(aspirate_flow_rate=aspirate_flow_rate)
+        if dispense_flow_rate is not None:
+            config = config._replace(dispense_flow_rate=dispense_flow_rate)
+
+        if min_volume is not None:
+            config = config._replace(min_volume=min_volume)
+        if max_volume is not None:
+            config = config._replace(max_volume=max_volume)
+        plunger_positions = {
+            'top': config.top,
+            'bottom': config.bottom,
+            'blow_out': config.blow_out,
+            'drop_tip': config.drop_tip}
+        p = self.Pipette(
+            model_offset=config.model_offset,
+            mount=mount,
+            name=name,
+            trash_container=trash_container,
+            tip_racks=tip_racks,
+            channels=config.channels,
+            aspirate_flow_rate=config.aspirate_flow_rate,
+            dispense_flow_rate=config.dispense_flow_rate,
+            min_volume=config.min_volume,
+            max_volume=config.max_volume,
+            plunger_current=config.plunger_current,
+            drop_tip_current=config.drop_tip_current,
+            drop_tip_speed=config.drop_tip_speed,
+            plunger_positions=plunger_positions,
+            ul_per_mm=config.ul_per_mm,
+            pick_up_current=config.pick_up_current,
+            pick_up_distance=config.pick_up_distance,
+            pick_up_increment=config.pick_up_increment,
+            pick_up_presses=config.pick_up_presses,
+            pick_up_speed=config.pick_up_speed,
+            quirks=config.quirks,
+            fallback_tip_length=config.tip_length)  # TODO move to labware
+
+        return p
+
+    def retrieve_version_number(self, mount, expected_model_substring):
+        if pipette_config.HAS_MODEL_RE.match(expected_model_substring):
+            return expected_model_substring
+
+        attached_model = robot.get_attached_pipettes()[mount]['model']
+
+        if attached_model and expected_model_substring in attached_model:
+            return attached_model
+        else:
+            # pass a default pipette model-version for when robot is simulating
+            # this allows any pipette to be simulated, regardless of what is
+            # actually attached/cached on the robot's mounts
+            return expected_model_substring + '_v1'  # default to v1
+
+
+instruments = InstrumentsWrapper(robot)
+containers = ContainersWrapper(robot)
+labware = ContainersWrapper(robot)
+modules.provide_labware(labware)
+
+
+__all__ = ['containers', 'instruments', 'labware', 'robot', 'reset', 'modules']
---
+++ packages/opentrons/opentrons-3.8.3-py2.py3-none-any.whl/opentrons/main.py
@@ -0,0 +1,223 @@
+import os
+import logging
+import asyncio
+import re
+from opentrons import HERE
+from opentrons import server
+from opentrons.server.main import build_arg_parser
+from argparse import ArgumentParser
+from opentrons import hardware, __version__
+from opentrons.config import feature_flags as ff, CONFIG
+from logging.config import dictConfig
+from opentrons.system import udev, resin
+
+log = logging.getLogger(__name__)
+
+
+def log_init():
+    """
+    Function that sets log levels and format strings. Checks for the
+    OT_API_LOG_LEVEL environment variable otherwise defaults to DEBUG.
+    """
+    fallback_log_level = 'INFO'
+    ot_log_level = hardware.config.log_level
+    if ot_log_level not in logging._nameToLevel:
+        log.info("OT Log Level {} not found. Defaulting to {}".format(
+            ot_log_level, fallback_log_level))
+        ot_log_level = fallback_log_level
+
+    level_value = logging._nameToLevel[ot_log_level]
+
+    serial_log_filename = CONFIG['serial_log_file']
+    api_log_filename = CONFIG['api_log_file']
+
+    logging_config = dict(
+        version=1,
+        formatters={
+            'basic': {
+                'format':
+                '%(asctime)s %(name)s %(levelname)s [Line %(lineno)s] %(message)s'  # noqa: E501
+            }
+        },
+        handlers={
+            'debug': {
+                'class': 'logging.StreamHandler',
+                'formatter': 'basic',
+                'level': level_value
+            },
+            'serial': {
+                'class': 'logging.handlers.RotatingFileHandler',
+                'formatter': 'basic',
+                'filename': serial_log_filename,
+                'maxBytes': 5000000,
+                'level': logging.DEBUG,
+                'backupCount': 3
+            },
+            'api': {
+                'class': 'logging.handlers.RotatingFileHandler',
+                'formatter': 'basic',
+                'filename': api_log_filename,
+                'maxBytes': 1000000,
+                'level': logging.DEBUG,
+                'backupCount': 5
+            }
+
+        },
+        loggers={
+            '__main__': {
+                'handlers': ['debug', 'api'],
+                'level': logging.INFO
+            },
+            'opentrons.server': {
+                'handlers': ['debug', 'api'],
+                'level': level_value
+            },
+            'opentrons.api': {
+                'handlers': ['debug', 'api'],
+                'level': level_value
+            },
+            'opentrons.instruments': {
+                'handlers': ['debug', 'api'],
+                'level': level_value
+            },
+            'opentrons.config': {
+                'handlers': ['debug', 'api'],
+                'level': level_value
+            },
+            'opentrons.drivers.smoothie_drivers.driver_3_0': {
+                'handlers': ['debug', 'api'],
+                'level': level_value
+            },
+            'opentrons.drivers.serial_communication': {
+                'handlers': ['serial'],
+                'level': logging.DEBUG
+            },
+            'opentrons.drivers.thermocycler.driver': {
+                'handlers': ['serial'],
+                'level': logging.DEBUG
+            },
+            'opentrons.protocol_api': {
+                'handlers': ['api', 'debug'],
+                'level': level_value
+            },
+            'opentrons.hardware_control': {
+                'handlers': ['api', 'debug'],
+                'level': level_value
+            },
+            'opentrons.legacy_api.containers': {
+                'handlers': ['api'],
+                'level': level_value
+            }
+        }
+    )
+    dictConfig(logging_config)
+
+
+def _find_smoothie_file():
+    resources = os.listdir(os.path.join(HERE, 'resources'))
+    for fi in resources:
+        matches = re.search('smoothie-(.*).hex', fi)
+        if matches:
+            branch_plus_ref = matches.group(1)
+            return os.path.join(HERE, 'resources', fi), branch_plus_ref
+    raise OSError("Could not find smoothie firmware file in {}"
+                  .format(os.path.join(HERE, 'resources')))
+
+
+def _sync_do_smoothie_install(explicit_modeset, filename, loop):
+    loop.run_until_complete(hardware.update_firmware(filename,
+                                                     loop,
+                                                     explicit_modeset))
+
+
+def initialize_robot(loop):
+    packed_smoothie_fw_file, packed_smoothie_fw_ver = _find_smoothie_file()
+    try:
+        if ff.use_protocol_api_v2():
+            hardware.connect(force=True)
+        else:
+            hardware.connect()
+    except Exception as e:
+        # The most common reason for this exception (aside from hardware
+        # failures such as a disconnected smoothie) is that the smoothie
+        # is in programming mode. If it is, then we still want to update
+        # it (so it can boot again), but we don’t have to do the GPIO
+        # manipulations that _put_ it in programming mode
+        log.exception("Error while connecting to motor driver: {}".format(e))
+        explicit_modeset = False
+        fw_version = None
+    else:
+        explicit_modeset = True
+        fw_version = hardware.fw_version
+    log.info("Smoothie FW version: {}".format(fw_version))
+    if fw_version != packed_smoothie_fw_ver:
+        log.info("Executing smoothie update: current vers {}, packed vers {}"
+                 .format(fw_version, packed_smoothie_fw_ver))
+        loop.run_until_complete(
+            hardware.update_firmware(packed_smoothie_fw_file,
+                                     explicit_modeset=explicit_modeset))
+        if hardware.is_connected():
+            log.info("FW Update complete!")
+        else:
+            raise RuntimeError(
+                "Could not connect to motor driver after fw update")
+    else:
+        log.info("FW version OK: {}".format(packed_smoothie_fw_ver))
+
+
+def run(**kwargs):
+    """
+    This function was necessary to separate from main() to accommodate for
+    server startup path on system 3.0, which is server.main. In the case where
+    the api is on system 3.0, server.main will redirect to this function with
+    an additional argument of 'patch_old_init'. kwargs are hence used to allow
+    the use of different length args
+    """
+    log_init()
+    loop = asyncio.get_event_loop()
+    log.info("API server version:  {}".format(__version__))
+    if not os.environ.get("ENABLE_VIRTUAL_SMOOTHIE"):
+        initialize_robot(loop)
+        if ff.use_protocol_api_v2():
+            loop.run_until_complete(hardware.cache_instruments())
+        if not ff.disable_home_on_boot():
+            log.info("Homing Z axes")
+            if ff.use_protocol_api_v2():
+                loop.run_until_complete(hardware.home_z())
+            else:
+                hardware.home_z()
+        try:
+            udev.setup_rules_file()
+        except Exception:
+            log.exception(
+                "Could not setup udev rules, modules may not be detected")
+    # Explicitly unlock resin updates in case a prior server left them locked
+    resin.unlock_updates()
+
+    server.run(kwargs.get('hostname'), kwargs.get('port'), kwargs.get('path'),
+               loop)
+
+
+def main():
+    """ The main entrypoint for the Opentrons robot API server stack.
+
+    This function
+    - creates and starts the server for both the RPC routes
+      handled by :py:mod:`opentrons.server.rpc` and the HTTP routes handled
+      by :py:mod:`opentrons.server.http`
+    - initializes the hardware interaction handled by either
+      :py:mod:`opentrons.legacy_api` or :py:mod:`opentrons.hardware_control`
+
+    This function does not return until the server is brought down.
+    """
+
+    arg_parser = ArgumentParser(
+        description="Opentrons robot software",
+        parents=[build_arg_parser()])
+    args = arg_parser.parse_args()
+    run(**vars(args))
+    arg_parser.exit(message="Stopped\n")
+
+
+if __name__ == "__main__":
+    main()
