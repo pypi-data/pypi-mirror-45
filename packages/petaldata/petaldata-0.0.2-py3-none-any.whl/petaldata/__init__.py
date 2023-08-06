a8a0978a8ff6a86863c7f0bde0426f09f0bf6b46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:54 +0000

    Add package petaldata/0.0.2/petaldata-0.0.2-py3-none-any.whl

---
+++ packages/petaldata/petaldata-0.0.2-py3-none-any.whl/petaldata/__init__.py
@@ -0,0 +1,16 @@
+# import numpy
+# import pandas
+
+# Configuration variables
+
+api_key = None
+api_base = "https://petaldata.app/api"
+cache_dir = None
+
+# API Resources
+
+from petaldata.resource import *
+from petaldata.resource import abstract
+from petaldata.resource import hubspot
+from petaldata.resource import stripe
+
