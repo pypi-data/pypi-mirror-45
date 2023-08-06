cdb81f18272d60f017bb7554032360eee603af40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:36 +0000

    Add package luwiji/1.0.1/luwiji-1.0.1.tar.gz

---
+++ packages/luwiji/luwiji-1.0.1.tar.gz/luwiji-1.0.1/luwiji/gradient_boosting/illustration.py
@@ -0,0 +1,9 @@
+from luwiji.gradient_boosting.base import BaseIllustrationGradBoost
+
+
+class Illustration(BaseIllustrationGradBoost):
+    def __init__(self):
+        super().__init__()
+
+
+illustration = Illustration()
---
+++ packages/luwiji/luwiji-1.0.1.tar.gz/luwiji-1.0.1/luwiji/knn/illustration.py
@@ -0,0 +1,9 @@
+from luwiji.knn.base import BaseIllustrationKNN
+
+
+class Illustration(BaseIllustrationKNN):
+    def __init__(self):
+        super().__init__()
+
+
+illustration = Illustration()
