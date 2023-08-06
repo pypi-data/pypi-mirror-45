13f090b25c221100d86c1efe8d9614b662c87b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:54 +0000

    Add package aiomisc/6.0.6/aiomisc-6.0.6.tar.gz

---
+++ packages/aiomisc/aiomisc-6.0.6.tar.gz/aiomisc-6.0.6/aiomisc/entrypoint.py
@@ -0,0 +1,127 @@
+import asyncio
+import logging
+from contextlib import contextmanager
+from typing import Tuple, Optional, Union
+
+from .context import Context, get_context
+from .log import basic_config, LogFormat
+from .service import Service
+from .utils import new_event_loop, select
+
+
+def graceful_shutdown(services: Tuple[Service, ...],
+                      loop: asyncio.AbstractEventLoop,
+                      exception: Optional[Exception]):
+
+    if hasattr(loop, '_default_executor'):
+        try:
+            loop._default_executor.shutdown()
+        except Exception:
+            logging.exception("Failed to stop default executor")
+
+    tasks = [
+        asyncio.shield(loop.create_task(svc.stop(exception)), loop=loop)
+        for svc in services
+    ]
+
+    if not tasks:
+        return
+
+    loop.run_until_complete(
+        asyncio.wait(
+            tasks, loop=loop, return_when=asyncio.ALL_COMPLETED
+        )
+    )
+
+
+@contextmanager
+def entrypoint(*services: Service,
+               loop: asyncio.AbstractEventLoop = None,
+               pool_size: int = None,
+               log_level: Union[int, str] = logging.INFO,
+               log_format: Union[str, LogFormat] = 'color',
+               log_buffer_size: int = 1024,
+               log_flush_interval: float = 0.2,
+               log_config: bool = True,
+               debug: bool = False):
+    """
+
+    :param debug: set debug to event loop
+    :param loop: loop
+    :param services: Service instances which will be starting.
+    :param pool_size: thread pool size
+    :param log_level: Logging level which will be configured
+    :param log_format: Logging format which will be configures
+    :param log_buffer_size: Buffer size for logging
+    :param log_flush_interval: interval in seconds for flushing logs
+    :param log_config: if False do not configure logging
+    """
+
+    loop = loop or new_event_loop(pool_size)
+    loop.set_debug(debug)
+
+    ctx = Context(loop=loop)
+
+    if log_config:
+        basic_config(
+            level=log_level,
+            log_format=log_format,
+            loop=loop,
+            buffered=False,
+        )
+
+    async def start():
+        nonlocal loop
+        nonlocal services
+        nonlocal log_format
+        nonlocal log_level
+        nonlocal log_buffer_size
+        nonlocal log_flush_interval
+
+        if log_config:
+            basic_config(
+                level=log_level,
+                log_format=log_format,
+                buffered=True,
+                loop=loop,
+                buffer_size=log_buffer_size,
+                flush_interval=log_flush_interval,
+            )
+
+        starting = []
+
+        async def start_service(svc: Service):
+            await select(
+                svc.start(), svc.start_event.wait(),
+                cancel=False,
+                loop=loop,
+            )
+
+            if not svc.start_event.is_set():
+                svc.start_event.set()
+
+        for svc in services:
+            svc.set_loop(loop)
+            starting.append(loop.create_task(start_service(svc)))
+
+        await asyncio.gather(*starting, loop=loop)
+
+    loop.run_until_complete(start())
+
+    shutting_down = False
+
+    try:
+        yield loop
+    except Exception as e:
+        graceful_shutdown(services, loop, e)
+        shutting_down = True
+        raise
+    finally:
+        if not shutting_down:
+            graceful_shutdown(services, loop, None)
+
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        ctx.close()
+
+
+__all__ = ('entrypoint', 'get_context')
---
+++ packages/aiomisc/aiomisc-6.0.6.tar.gz/aiomisc-6.0.6/aiomisc/log.py
@@ -0,0 +1,84 @@
+import asyncio
+import logging
+import logging.handlers
+from functools import partial
+
+from typing import Union
+
+from aiomisc.thread_pool import threaded, ThreadPoolException
+from aiomisc.periodic import PeriodicCallback
+from prettylog import (
+    color_formatter,
+    create_logging_handler,
+    json_formatter,
+    JSONLogFormatter,
+    LogFormat,
+    wrap_logging_handler as _wrap_logging_handler,
+)
+
+
+class AsyncMemoryHandler(logging.handlers.MemoryHandler):
+    flush_async = threaded(logging.handlers.MemoryHandler.flush)
+
+
+def wrap_logging_handler(handler: logging.Handler,
+                         loop: asyncio.AbstractEventLoop = None,
+                         buffer_size: int = 1024,
+                         flush_interval: float = 0.1):
+    loop = loop or asyncio.get_event_loop()
+
+    buffered_handler = _wrap_logging_handler(
+        handler=handler, buffer_size=buffer_size,
+        logger_class=AsyncMemoryHandler,
+    )
+
+    periodic = PeriodicCallback(buffered_handler.flush_async)
+    loop.call_soon_threadsafe(
+        partial(
+            periodic.start, flush_interval, loop,
+            suppress_exceptions=(ThreadPoolException,)
+        )
+    )
+
+    return buffered_handler
+
+
+def basic_config(level: int = logging.INFO,
+                 log_format: Union[str, LogFormat] = LogFormat.color,
+                 buffered: bool = True, buffer_size: int = 1024,
+                 flush_interval: float = 0.2, loop=None):
+
+    if isinstance(level, str):
+        level = getattr(logging, level.upper())
+
+    logging.basicConfig()
+    logger = logging.getLogger()
+    logger.handlers.clear()
+
+    if isinstance(log_format, str):
+        log_format = LogFormat[log_format]
+
+    handler = create_logging_handler(log_format)
+
+    if buffered:
+        handler = wrap_logging_handler(
+            handler,
+            buffer_size=buffer_size,
+            flush_interval=flush_interval,
+            loop=loop,
+        )
+
+    logging.basicConfig(
+        level=level,
+        handlers=[handler]
+    )
+
+
+__all__ = (
+    'basic_config',
+    'color_formatter',
+    'json_formatter',
+    'JSONLogFormatter',
+    'LogFormat',
+    'wrap_logging_handler',
+)
