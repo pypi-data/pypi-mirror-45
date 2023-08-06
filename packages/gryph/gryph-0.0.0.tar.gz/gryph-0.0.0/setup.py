af58ea9c92715d856faf99baca4a5d0427d4391a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:32 +0000

    Add package gryph/0.0.0/gryph-0.0.0.tar.gz

---
+++ packages/gryph/gryph-0.0.0.tar.gz/gryph-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='gryph',
+        version='0.0.0',
+        packages=[''],
+        url='http://gryph.io',
+        license='agpl',
+        author='Joerg Baach',
+        author_email='pypi@baach.de',
+        description=''
+)
