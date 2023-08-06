174a770e6c5fd4d1ee85cc3331dbfc1c9bd21c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:06 +0000

    Add package django-react-templatetags/5.4.0/django_react_templatetags-5.4.0.tar.gz

---
+++ packages/django-react-templatetags/django_react_templatetags-5.4.0.tar.gz/django_react_templatetags-5.4.0/django_react_templatetags/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+django_react_templatetags
+----------
+This extension allows you to add React components into your django templates.
+"""
+
+__title__ = "django_react_templatetags"
+__version__ = "5.4.0"
+__build__ = 540
+__author__ = "Martin Sandström"
+__license__ = "MIT"
+__copyright__ = "Copyright 2015-2019 Fröjd Interactive"
