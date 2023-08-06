32f3207272f01e3be788c03d6758cfec84f8fa13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:35 +0000

    Add package dephell/0.6.0/dephell-0.6.0-py3-none-any.whl

---
+++ packages/dephell/dephell-0.6.0-py3-none-any.whl/dephell/converters/__init__.py
@@ -0,0 +1,39 @@
+# app
+from .conda import CondaConverter
+from .egginfo import EggInfoConverter
+from .installed import InstalledConverter
+from .pip import PIPConverter
+from .pipfile import PIPFileConverter
+from .pipfilelock import PIPFileLockConverter
+from .poetry import PoetryConverter
+from .poetrylock import PoetryLockConverter
+from .pyproject import PyProjectConverter
+from .sdist import SDistConverter
+from .setuppy import SetupPyConverter
+from .wheel import WheelConverter
+
+
+CONVERTERS = dict(
+    # archives
+    egginfo=EggInfoConverter(),
+    sdist=SDistConverter(),
+    wheel=WheelConverter(),
+
+    # pip
+    pip=PIPConverter(lock=False),
+    piplock=PIPConverter(lock=True),
+
+    # pipenv
+    pipfile=PIPFileConverter(),
+    pipfilelock=PIPFileLockConverter(),
+
+    # poetry
+    poetry=PoetryConverter(),
+    poetrylock=PoetryLockConverter(),
+
+    # other
+    conda=CondaConverter(),
+    pyproject=PyProjectConverter(),
+    setuppy=SetupPyConverter(),
+    installed=InstalledConverter(),
+)
