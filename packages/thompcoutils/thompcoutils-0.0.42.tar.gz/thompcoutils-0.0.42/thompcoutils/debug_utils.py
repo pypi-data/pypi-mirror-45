e914fb8be28b8124ce84dd08f9bb96be83f7e5ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:57 +0000

    Add package thompcoutils/0.0.42/thompcoutils-0.0.42.tar.gz

---
+++ packages/thompcoutils/thompcoutils-0.0.42.tar.gz/thompcoutils-0.0.42/thompcoutils/debug_utils.py
@@ -0,0 +1,48 @@
+from functools import partial
+
+
+def assert_test(value, message, error=None, passed=None):
+    if message is not None:
+        print(message, end="...")
+    if value:
+        if passed is None:
+            print("passed")
+        else:
+            print(passed)
+    else:
+        if error is None:
+            print("FAILED!!")
+        else:
+            print(error)
+        assert False
+
+
+def exception_test(message,  method_to_test):
+    print(message, end="...")
+    try:
+        method_to_test
+        raise Exception("this should fail")
+    except Exception as e:
+        pass
+    print("passed")
+
+
+def _test(throws, second):
+    if throws:
+        raise Exception("Throws")
+    else:
+        print(second)
+
+
+def main():
+    exception_test("testing - should throw", partial(_test, True, "hello"))
+    exception_test("testing - should throw", partial(_test, throws=True, second="hello"))
+    assert_test(True, "just some testing 0")
+    assert_test(True, "just some testing 1", "error occurred 1")
+    assert_test(True, "just some testing 2", "error occurred 2", "this passed 2")
+    # assert_test(False, "error occurred 3")
+    # assert_test(False, "just some testing 4", "error occurred 4")
+    # assert_test(False, "just some testing 5", "error occurred 5", "this passed 5")
+
+if __name__ == '__main__':
+    main()
