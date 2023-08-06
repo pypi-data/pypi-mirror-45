8717ff7e8360f3160a2039ceb26dc923feb4917a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:50 +0000

    Add package mudmaker/0.1.4/mudmaker-0.1.4.tar.gz

---
+++ packages/mudmaker/mudmaker-0.1.4.tar.gz/mudmaker-0.1.4/mudmaker/objects.py
@@ -0,0 +1,127 @@
+"""Provides the Object class."""
+
+from .attributes import Attribute
+from .base import BaseObject, LocationMixin
+from .exc import NoSuchObjectError
+
+
+class Object(BaseObject, LocationMixin):
+    """An object in the database."""
+
+    say_msg = Attribute(
+        '%1N say%1s: "{text}"', 'The social message used when this object '
+        'says something'
+    )
+
+    @classmethod
+    def on_init(cls, instance):
+        """Add this object to self.game.objects."""
+        instance.game.objects[instance.id] = instance
+        instance.connection = None
+        instance.parser = None
+
+    @property
+    def account(self):
+        """Get any account object associated with this object."""
+        try:
+            return self.game.account_store.account_for(self)
+        except NoSuchObjectError:
+            pass  # return None.
+
+    def message(self, text):
+        """Send some text to this object's connection."""
+        if self.connection is not None:
+            self.connection.message(text)
+            return True
+        return False
+
+    def look_here(self):
+        """Have this object look at its surroundings."""
+        here = self.location
+        self.message('[%s; %s]' % (here.zone.name, here.name))
+        self.message(here.get_description())
+
+    def no_location_match(self, string):
+        """No matches may be performed at this location. By default, sends a
+        message to this object explaining that."""
+        self.message('You cannot see anything here.')
+
+    def no_match(self, string):
+        """No match was found. By default, a message explaining that is sent to
+        this object."""
+        self.message('I don\'t see "%s" here.' % string)
+
+    def multiple_matches(self, string, objects):
+        """Multiple matches were found. By default, send a message explaining
+        that to this object."""
+        self.message('I don\'t know which "%s" you mean.' % string)
+
+    def match_object(self, obj, string):
+        """See if obj matches string."""
+        if string == "me" and obj is self:
+            return True
+        elif obj.name.lower().startswith(string.lower()):
+            return True
+        return False
+
+    def single_match(self, string, objects=None):
+        """Return a single match from the given list of objects. If objects is
+        None, self.location.contents will be used.
+
+        If objects is None, and this object has no location, the result of
+        calling self.no_location_match with string will be returned.
+
+        If the match string is "me", then this object will be considered in the
+        match if it is in the list of objects.
+
+        If no matches are found, then the result of calling self.no_match with
+        string will be returned.
+
+        If multiple matches are found, then the result of calling
+        self.multiple_matches with string and the list of objects that were
+        matched will be returned.
+
+        If this object is a member of staff, and the string starts with a hash
+        symbol (#), the remaining string will be considered an object ID, and
+        the object with that ID will be returned."""
+        if self.account.is_staff and string.startswith('#'):
+            try:
+                id = int(string[1:])
+                return self.game.objects[id]
+            except (ValueError, KeyError):
+                return self.no_match(string)
+        if objects is None:
+            if self.location is None:
+                return self.no_location_match(string)
+            else:
+                objects = self.location.contents
+        results = []
+        results = [x for x in objects if self.match_object(x, string)]
+        if len(results) == 1:
+            return results[0]
+        elif not results:
+            return self.no_match(string)
+        else:
+            return self.multiple_matches(string, results)
+
+    def do_social(self, string, _perspectives=None, **kwargs):
+        """Perform a social as this object. If _perspectives is not None, it
+        will be treated as a list, and this object will be prepended. All extra
+        keyword arguments will be passed to
+        self.game.socials_factory.get_strings."""
+        if _perspectives is None:
+            _perspectives = []
+        perspectives = [self] + _perspectives
+        strings = self.game.socials_factory.get_strings(
+            string, perspectives, **kwargs
+        )
+        for i, obj in enumerate(perspectives):
+            obj.message(strings[i])
+        objects = getattr(self.location, 'contents', [])
+        for obj in objects:
+            if obj not in perspectives:
+                obj.message(strings[-1])
+
+    def do_say(self, string):
+        """Say something as tis object."""
+        self.do_social(self.say_msg, text=string)
---
+++ packages/mudmaker/mudmaker-0.1.4.tar.gz/mudmaker-0.1.4/mudmaker/parsers.py
@@ -0,0 +1,170 @@
+"""Provides the two main command parsers: login_parser, and main_parser."""
+
+from datetime import datetime
+
+from commandlet import Parser, command
+from .exc import AuthenticationError
+from .objects import Object
+from .socials import Social
+from .util import get_login
+
+
+class MudMakerParser(Parser):
+    """A parser with extra socials."""
+
+    def social(
+        self, name, no_target, self_target, any_target, description=None
+    ):
+        """Add a social to this parser. The name argument will be the name of
+        the command. If None, the description argument will become '<name> at
+        someone.'. All other arguments must be social strings. They will be
+        used as follows:
+
+        no_target: When the command is used with no arguments.
+        self.target: When the object that performed the command is the
+        argument.
+        any_target: Any other object is passed.
+        """
+        if description is None:
+            description = '%s at something.' % name
+        s = Social(no_target, self_target, any_target)
+        s.__doc__ = description
+        self.command(name)(s)
+        self.command(name, '%s <object:target>' % name)(s)
+        return s
+
+
+login_parser = Parser()
+
+
+@login_parser.command('create', 'new', 'create <username> <password>')
+def do_create(con, accounts, game, username=None, password=None):
+    """Create a new character."""
+    if not username:
+        username, password = yield from get_login(con)
+    while True:
+        con.message('Enter a name for your new character (or quit to exit):')
+        name = yield
+        if name == 'quit':
+            return
+        elif not name:
+            con.message('Names must not be blank.')
+        elif list(
+            filter(
+                lambda obj: obj.name.lower().startswith(name),
+                game.objects.values()
+            )
+        ):
+            con.message(
+                'There is already a player with that name. Please choose '
+                'another.'
+            )
+        else:
+            break
+    if accounts.account_exists(username):
+        con.message(
+            'There is already an account with that username. Please pick '
+            'another.'
+        )
+        return
+    player = game.make_object('Object', (Object,), name=name)
+    if not game.players:
+        kwargs = dict(builder=True, admin=True)
+        con.message(
+            'You are the first connected player. You have been made an '
+            'administrator.'
+        )
+    else:
+        kwargs = {}
+    accounts.add_account(username, password, player, **kwargs)
+    con.message('You have successfully created a new character.')
+    game.finish_login(con, player)
+
+
+main_parser = MudMakerParser()
+
+
+@main_parser.filter('object')
+def object_filter(player, text):
+    """Attempt to return a match."""
+    return player.single_match(text)
+
+
+main_parser.social(
+    'smile', '%1N smile%1s.', '%1N smile%1s broadly.', '%1N smile%1s at %2n.'
+)
+
+main_parser.social(
+    'nod', '%1N nod%1s.', '%1N nod%1s emphatically.', '%1N nod%1s to %2n.'
+)
+
+
+@command([login_parser, main_parser], 'host', '@host', '@hostname')
+def do_hostname(con, host):
+    """Get your host name and port."""
+    con.message('You are connected from %s.' % host)
+
+
+@command([login_parser, main_parser], 'help', 'help', '@commands', r'\?')
+def do_help(con, parser):
+    """Get a list of possible commands."""
+    con.message('Commands available to you:')
+    commands = {}
+    for cmd in sorted(parser.commands, key=lambda cmd: cmd.name):
+        if cmd.name not in commands:
+            commands[cmd.name] = [
+                cmd.func.func.__doc__ or 'No description available.'
+            ]
+        commands[cmd.name].append(cmd.usage)
+    for name, values in commands.items():
+        con.message('%s:' % name)
+        formats = [x.replace('\\', '') for x in values[1:]]
+        con.message(', '.join(formats) + '.')
+        con.message(values[0])
+
+
+@command([login_parser, main_parser], 'uptime', '@uptime')
+def do_uptime(game, con):
+    """Show server uptime."""
+    now = datetime.utcnow()
+    con.message('Uptime: %s.' % (now - game.started))
+    con.message('Server started: %s.' % now.ctime())
+
+
+@command([login_parser, main_parser], 'quit', 'quit', '@quit')
+def do_quit(con):
+    """Quit the game."""
+    con.disconnect('Goodbye.')
+
+
+@login_parser.command('login', 'connect <username> <password>', '<username>')
+def do_login(game, con, username, password=None):
+    """Log in a character."""
+    if not password:
+        con.message('Password:')
+        password = yield
+    try:
+        player = game.account_store.authenticate(username, password)
+        game.finish_login(con, player)
+    except AuthenticationError:
+        con.logger.info('Attempted to login as %s.', username)
+        con.message('Invalid username or password.')
+
+
+@main_parser.command('look', 'look <object:thing>', 'l', 'l <object:thing>')
+def look(player, location, thing=False):
+    """Look around, or at something in this room."""
+    if location is None:
+        player.message('You cannot look here.')
+    elif thing is not False:
+        if thing is not None:
+            player.message(thing.name)
+            player.message(thing.get_description())
+    else:
+        player.look_here()
+
+
+@main_parser.command('say', 'say <string>', '"<string>', "'<string>")
+def do_say(player, string):
+    """Say something."""
+    player.do_say(string)
