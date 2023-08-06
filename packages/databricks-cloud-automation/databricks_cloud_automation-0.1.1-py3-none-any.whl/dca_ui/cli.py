bcdad0e29abfe8a9e32396290ebf9fb0d05be09e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:17 +0000

    Add package databricks-cloud-automation/0.1.1/databricks_cloud_automation-0.1.1-py3-none-any.whl

---
+++ packages/databricks-cloud-automation/databricks_cloud_automation-0.1.1-py3-none-any.whl/dca_ui/cli.py
@@ -0,0 +1,9 @@
+import subprocess
+import webbrowser
+
+HOST = '127.0.0.1:5000'
+
+def cli():
+
+	subprocess.call(['gunicorn', '-b', HOST, '-w', '1', '-t', '0', 'dca_ui.__init__:app'])
+	webbrowser.open(HOST, new = 2)
