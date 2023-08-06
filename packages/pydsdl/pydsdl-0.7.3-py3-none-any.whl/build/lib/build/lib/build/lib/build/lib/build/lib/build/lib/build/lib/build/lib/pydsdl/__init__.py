34265093db3350f937ea356e2d5a0995907c9d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:41 +0000

    Add package pydsdl/0.7.3/pydsdl-0.7.3-py3-none-any.whl

---
+++ packages/pydsdl/pydsdl-0.7.3-py3-none-any.whl/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/build/lib/pydsdl/__init__.py
@@ -0,0 +1,54 @@
+#
+# Copyright (C) 2018-2019  UAVCAN Development Team  <uavcan.org>
+# This software is distributed under the terms of the MIT License.
+#
+
+import os as _os
+import sys as _sys
+
+if _sys.version_info[:2] < (3, 5):   # pragma: no cover
+    print('A newer version of Python is required', file=_sys.stderr)
+    _sys.exit(1)
+
+__version__ = 0, 7, 3
+__license__ = 'MIT'
+
+# Our unorthodox approach to dependency management requires us to apply certain workarounds.
+# Here, the objective is to allow our library to import stuff from its third-party dependency directory,
+# but at the same time we don't want to interfere with the application that depends on this library.
+# So we modify the import lookup path temporarily while the package initialization is in progress;
+# when done, we restore the path back to its original value. One implication is that it won't be possible
+# to import stuff dynamically after the initialization is finished (e.g., function-local imports won't be
+# able to reach the third-party stuff), but we don't care.
+_original_sys_path = _sys.path
+_sys.path = [_os.path.join(_os.path.dirname(__file__), 'third_party')] + _sys.path
+
+# Never import anything that is not available here - API stability guarantees are only provided for the exposed items.
+from ._namespace import read_namespace
+from ._namespace import PrintOutputHandler
+
+# Error model.
+from ._error import FrontendError, InvalidDefinitionError, InternalError
+
+# Data type model - meta types.
+from ._serializable import SerializableType
+from ._serializable import PrimitiveType
+from ._serializable import BooleanType
+from ._serializable import ArithmeticType, IntegerType, SignedIntegerType, UnsignedIntegerType, FloatType
+from ._serializable import VoidType
+from ._serializable import ArrayType, FixedLengthArrayType, VariableLengthArrayType
+from ._serializable import CompositeType, UnionType, StructureType, ServiceType
+
+# Data type model - attributes.
+from ._serializable import Attribute, Field, PaddingField, Constant
+
+# Expression model.
+from ._expression import Any
+from ._expression import Primitive, Boolean, Rational, String
+from ._expression import Container, Set
+
+# Auxiliary.
+from ._serializable import ValueRange, Version
+from ._bit_length_set import BitLengthSet
+
+_sys.path = _original_sys_path
