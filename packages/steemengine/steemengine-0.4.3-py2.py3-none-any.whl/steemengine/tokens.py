2539a096d7d1107be18236a8447a6447d6d4aa50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:12 +0000

    Add package steemengine/0.4.3/steemengine-0.4.3-py2.py3-none-any.whl

---
+++ packages/steemengine/steemengine-0.4.3-py2.py3-none-any.whl/steemengine/tokens.py
@@ -0,0 +1,32 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from steemengine.api import Api
+from steemengine.tokenobject import Token
+
+
+class Tokens(list):
+    """ Access the steem-engine tokens
+    """
+    def __init__(self, url=None, **kwargs):
+        self.api = Api(url=url)
+        self.refresh()
+
+    def refresh(self):
+        super(Tokens, self).__init__(self.get_token_list())
+
+    def get_token_list(self):
+        """Returns all available token as list"""
+        tokens = self.api.find("tokens", "tokens", query={})
+        return tokens
+
+    def get_token(self, symbol):
+        """Returns Token from given token symbol. Is None
+            when token does not exists.
+        """
+        for t in self:
+            if t["symbol"].lower() == symbol.lower():
+                return Token(t)
+        return None
+
