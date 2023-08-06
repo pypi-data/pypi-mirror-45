724d20bf27cb7246ab69683ae8df6ed1ba20acff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:40 +0000

    Add package AgricolAI/0.0.21/AgricolAI-0.0.21.tar.gz

---
+++ packages/AgricolAI/AgricolAI-0.0.21.tar.gz/AgricolAI-0.0.21/AgricolAI/Field_Segmentation/__init__.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+from scipy.signal import find_peaks
+from PIL import Image
+from .__main__ import *
+
+import io
+import urllib.request
+from .mics import *
