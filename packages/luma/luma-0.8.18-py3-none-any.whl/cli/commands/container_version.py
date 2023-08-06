5c15d03a234c943cb37038be1083810031e4d85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:22 +0000

    Add package luma/0.8.18/luma-0.8.18-py3-none-any.whl

---
+++ packages/luma/luma-0.8.18-py3-none-any.whl/cli/commands/container_version.py
@@ -0,0 +1,26 @@
+from cli import pyke, behavior, commands
+import click
+
+@click.command('logs', help='Tail logs for an editable container.')
+@click.option('--profile', '-p', prompt=True, help='The name of the profile to use as the context for the command.', autocompletion=pyke.auth.get_profile_names)
+@click.option('--container', '-c', prompt=True, help='The ID or the url-ref of the container the editable version belongs to.')
+@click.option('--version', '-v', help='The version ID or the version-number')
+@pyke.auth.profile_context_required
+def tail(container, version, profile):
+  behavior.ContainerVersionBehavior(profile=profile).tail_logs(container, version)
+
+
+@click.command(help='This force updates your docker service. This can recover a process in a bad state.')
+@click.option('--profile', '-p', prompt=True, help='The name of the profile to use as the context for the command.', autocompletion=pyke.auth.get_profile_names)
+@click.option('--container', '-c', prompt=True, help='The ID or the url-ref of the container the editable version belongs to.')
+@click.option('--version', '-v', help='The version ID or the version-number')
+@pyke.auth.profile_context_required
+def force(container, version, profile):
+  behavior.ContainerVersionBehavior(profile=profile).force_update(container, version)
+
+
+commands.microservice_version.add_command(force)
+commands.widget_version.add_command(force)
+
+commands.microservice_version.add_command(tail)
+commands.widget_version.add_command(tail)
