7fa7fb75b16e9f2170ab245c72148668f40b9b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:44 +0000

    Add package maxauto/1.0.1/maxauto-1.0.1-py2.7.egg

---
+++ packages/maxauto/maxauto-1.0.1-py2.7.egg/max/__about__.py
@@ -0,0 +1,7 @@
+__title__ = 'maxauto'
+__description__ = 'maxauto'
+__version__ = '1.0.1'
+__author__ = 'xinxi'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 xinxi'
+__author_email__ = "xinxi1990@163.com"
