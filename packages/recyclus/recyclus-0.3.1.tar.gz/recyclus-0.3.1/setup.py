bd1eec92619634ba713d788caab1e21b97286ae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:12 +0000

    Add package recyclus/0.3.1/recyclus-0.3.1.tar.gz

---
+++ packages/recyclus/recyclus-0.3.1.tar.gz/recyclus-0.3.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='recyclus',
+    version='0.3.1',
+    license='BSD',
+    maintainer='Yarden Livnat',
+    description='Client for Recyclus remote services',
+    url='https://github.com/yarden-livnat/recyclus',
+
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'pp-ez',
+        'pprintpp',
+        'requests',
+        'requests_jwt',
+        'pyyaml'
+    ],
+    extras_require={
+        # 'test': [
+        #     'pytest',
+        #     'coverage',
+        # ],
+    },
+)
