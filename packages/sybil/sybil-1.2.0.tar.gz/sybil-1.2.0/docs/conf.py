1f86e6877247c0e7d013c8468f11c2f37ff2204f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:02 +0000

    Add package sybil/1.2.0/sybil-1.2.0.tar.gz

---
+++ packages/sybil/sybil-1.2.0.tar.gz/sybil-1.2.0/docs/conf.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import os, pkg_resources, datetime
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+intersphinx_mapping = {
+    'https://docs.python.org/3/': None,
+    'http://www.sphinx-doc.org/en/stable/': None,
+}
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx'
+    ]
+
+# General
+source_suffix = '.rst'
+master_doc = 'index'
+project = 'sybil'
+copyright = '2017 - %s Chris Withers' % datetime.datetime.now().year
+version = release = pkg_resources.get_distribution(project).version
+exclude_patterns = [
+    'description.rst',
+    '_build',
+    'example*',
+]
+pygments_style = 'sphinx'
+
+# Options for HTML output
+html_theme = 'default' if on_rtd else 'classic'
+htmlhelp_basename = project+'doc'
+
+# Options for LaTeX output
+latex_documents = [
+  ('index',project+'.tex', project+u' Documentation',
+   'Chris Withers', 'manual'),
+]
+
+autodoc_member_order = 'bysource'
