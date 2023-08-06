42a4df50392e6daf5733a5a02e53bc4939dd9d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:08 +0000

    Add package vmi/0.1.1/vmi-0.1.1.tar.gz

---
+++ packages/vmi/vmi-0.1.1.tar.gz/vmi-0.1.1/vmi/__init__.py
@@ -0,0 +1,12 @@
+name = 'vmi'
+version = '0.1.1'
+
+from .vapp import *
+from .vath import *
+from .vask import *
+from .view import *
+from .vicom import *
+from .vio import *
+from .vrop import *
+from .veom import *
+from .vroptool import *
