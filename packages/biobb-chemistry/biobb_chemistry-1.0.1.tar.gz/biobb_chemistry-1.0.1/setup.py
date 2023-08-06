2978018e71a6d784ac7bf121552effa63b1fab02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:06 +0000

    Add package biobb-chemistry/1.0.1/biobb_chemistry-1.0.1.tar.gz

---
+++ packages/biobb-chemistry/biobb_chemistry-1.0.1.tar.gz/biobb_chemistry-1.0.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="biobb_chemistry",
+    version="1.0.1",
+    author="Biobb developers",
+    author_email="genis.bayarri@irbbarcelona.org",
+    description="Biobb_md is the Biobb module collection to perform molecular dynamics simulations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="Bioinformatics Workflows BioExcel Compatibility",
+    url="https://github.com/bioexcel/biobb_chemistry",
+    project_urls={
+        "Documentation": "http://biobb_md.readthedocs.io/en/latest/",
+        "Bioexcel": "https://bioexcel.eu/"
+    },
+    packages=setuptools.find_packages(exclude=['docs', 'test',]),
+    install_requires=['biobb_common==0.1.4'],
+    python_requires='==3.6.*',
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+    ),
+)
