3b6d4ad9ab3aa71af4007a445513dd9e4282ca88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:01 +0000

    Add package npyi/0.1.1/npyi-0.1.1.tar.gz

---
+++ packages/npyi/npyi-0.1.1.tar.gz/npyi-0.1.1/setup.py
@@ -0,0 +1,40 @@
+import io
+
+from setuptools import setup, find_packages
+
+version = '0.1.1'
+requirements = [
+    'requests>=2.21.0',
+]
+
+with io.open('README.rst', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='npyi',
+    packages=find_packages(exclude=['test', 'test.*']),
+    version=version,
+    long_description=readme,
+    description='API wrapper around the NPPES API',
+    author='Andy Huynh',
+    license='BSD',
+    author_email='andy.huynh312@gmail.com',
+    url='https://github.com/andyh1203/npyi',
+    keywords=['npyi', 'npi', 'nppes'],
+    install_requests=requirements,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+    ],
+)
