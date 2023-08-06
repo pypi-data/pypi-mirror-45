14a200cffae8c64ef64306aaac70d8b7358bc324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:07 +0000

    Add package kfp/0.1.18/kfp.tar.gz

---
+++ packages/kfp/kfp.tar.gz/kfp-0.1.18/kfp_pipeline/api/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from kfp_pipeline.api.pipeline_service_api import PipelineServiceApi
---
+++ packages/kfp/kfp.tar.gz/kfp-0.1.18/kfp_uploadpipeline/api/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from kfp_uploadpipeline.api.pipeline_upload_service_api import PipelineUploadServiceApi
