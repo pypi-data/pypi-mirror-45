49dff6843dc4eeb6cfedebe41cdf98f123e7ba79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:06 +0000

    Add package make_colors_tc/3.19/make_colors_tc-3.19.tar.gz

---
+++ packages/make_colors_tc/make_colors_tc-3.19.tar.gz/make_colors_tc-3.19/make_colors_tc/__init__.py
@@ -0,0 +1,7 @@
+from .make_colors_tc import make_colors
+
+import imp
+import __version__ as version
+__version__ 	= version.version
+__email__		= "licface@yahoo.com"
+__author__		= "licface@yahoo.com"
