47574d126850394b724c990d8491e05d17132cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:06 +0000

    Add package mrldb/0.0.7/mrldb-0.0.7.tar.gz

---
+++ packages/mrldb/mrldb-0.0.7.tar.gz/mrldb-0.0.7/mrldb/__init__.py
@@ -0,0 +1,10 @@
+__all__=["MrlDBMsql", "MrlDBCassandra", "MrlDBSqlite", "MrlDBCluster", "mdbcl"]
+from .db_types import *
+from .mrl_cluster import *
+##version#start##
+__version__='0.0.7'
+##version#end##
+__doc__=f"""mrldb v{__version__} by Rémi "Mr e-RL" LANGDORPH
+Copyright (c) 2019 Rémi LANGDORPH - mrerl@warlegend.net
+under MIT License (https://github.com/merlleu/mrldb/blob/master/LICENSE)
+"""
