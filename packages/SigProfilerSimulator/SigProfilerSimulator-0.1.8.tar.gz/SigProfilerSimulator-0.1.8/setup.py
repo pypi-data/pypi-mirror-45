f64b0e27e66337d5257b8a7d2161544071c958db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:51 +0000

    Add package SigProfilerSimulator/0.1.8/SigProfilerSimulator-0.1.8.tar.gz

---
+++ packages/SigProfilerSimulator/SigProfilerSimulator-0.1.8.tar.gz/SigProfilerSimulator-0.1.8/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import os
+
+def readme():
+	with open('README.rst') as f:
+		return(f.read())
+
+
+setup(name='SigProfilerSimulator',
+		version='0.1.8',
+		description='SigProfiler simulator tool',
+		url='',
+		author='Erik Bergstrom',
+		author_email='ebergstr@eng.ucsd.edu',
+		license='UCSD',
+		packages=find_packages(),
+		install_requires =[
+			"SigProfilerMatrixGenerator>=0.1.22",
+			"sigProfilerPlotting>=0.1.19",
+			"fastrand>=1.1"],
+		include_package_data=True,
+		zip_safe=False)
