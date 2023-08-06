157b4bdfea19cd16f142e9c7243182e2c9fab732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:58 +0000

    Add package Cortecx/0.0.6/Cortecx-0.0.6.tar.gz

---
+++ packages/Cortecx/Cortecx-0.0.6.tar.gz/Cortecx-0.0.6/setup.py
@@ -0,0 +1,17 @@
+import sys, os
+from setuptools import setup, find_packages
+
+version = '0.0.6'
+
+setup(name='Cortecx',
+      version=version,
+      description="Cortecx is a NLP library",
+      long_description=open('README.md').read(),
+      classifiers=[],
+      keywords='artificial intelligence',
+      author='Lleyton Ariton',
+      author_email='lleyton.ariton@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      )
