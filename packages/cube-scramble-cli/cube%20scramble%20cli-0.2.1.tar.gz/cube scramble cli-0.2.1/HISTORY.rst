8be9d266d698a52f9ca496fc589c898c0a0c2bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:52 +0000

    Add package cube-scramble-cli/0.2.1/cube%20scramble%20cli-0.2.1.tar.gz

---
+++ packages/cube-scramble-cli/cube%20scramble%20cli-0.2.1.tar.gz/cube scramble cli-0.2.1/HISTORY.rst
@@ -0,0 +1,19 @@
+=======
+History
+=======
+
+0.1.0 (2019-02-16)
+------------------
+
+* Migrate from script to package.
+
+0.2.0 (2019-02-17)
+------------------
+
+* Add CMLL, ELL, CLL
+* Shorten certain scrambles to their acronyms
+
+0.2.1 (2019-05-02)
+------------------
+
+* Minor fix to setup.py, use entrypoint instead of console_script
---
+++ packages/cube-scramble-cli/cube%20scramble%20cli-0.2.1.tar.gz/cube scramble cli-0.2.1/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['colorama>=0.4.1', 'prompt_toolkit>=2.0.8', 'pyTwistyScrambler>=1.2']
+
+setup(
+    author="Sean Breckenridge",
+    author_email='seanbrecke@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="A simple CLI for pyTwistyScrambler, to generate random states for twisty puzzles.",
+    install_requires=requirements,
+    license="MIT",
+    long_description="Requires Python 3.6 | 3.7.\n\n" + \
+                    "Installation: python3 -m pip install cube-scramble-cli\n\n" + \
+                    "Run: $ scramble-cli\n\n" + \
+                    "More info here: https://github.com/seanbreckenridge/cube-scramble-cli",
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords='cube puzzle scramble',
+    name='cube scramble cli',
+    packages=find_packages(include=['cube_scramble_cli']),
+    entry_points = {
+        'console_scripts': [
+            "cube-scramble-cli = cube_scramble_cli.scramble_cli:main"
+        ]
+    },
+    url='https://github.com/seanbreckenridge/cube-scramble-cli',
+    version='0.2.1',
+    zip_safe=False,
+)
