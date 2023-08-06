e25f07aa7acfc398a849be3929439f88d68988a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:06 +0000

    Add package stahmctestt/0.5/stahmctestt-0.5.tar.gz

---
+++ packages/stahmctestt/stahmctestt-0.5.tar.gz/stahmctestt-0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='stahmctestt',
+      version='0.5',
+      description='testversion',
+      url='https://github.com/RihuiOu95/HMC.git',
+      author='Flying Circus',
+      author_email='thuizhou@outlook.com',
+      license='DUKE',
+      packages=['vec','oned'],
+      zip_safe=False)
