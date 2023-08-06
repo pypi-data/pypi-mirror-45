f48776ac853ea80d2603ab9f0c9151615cbc60e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:15 +0000

    Add package laziter/0.0.5/laziter-0.0.5.tar.gz

---
+++ packages/laziter/laziter-0.0.5.tar.gz/laziter-0.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='laziter',
+    url='https://github.com/jeffmomo/laziter',
+    author='Jeff Mo',
+    description='A lazy iterator wrapper with multiprocessing powers!',
+    version='0.0.5',
+    py_modules=['laziter'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
