07ccd1f9f0ba6fd9d08777cf79088ac7ab8208b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:07 +0000

    Add package lemkpg/1.2/lemkpg-1.2.tar.gz

---
+++ packages/lemkpg/lemkpg-1.2.tar.gz/lemkpg-1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='lemkpg',
+      version='1.2',
+      description='lemkpg package give simple API interface for quick access to PostgreSQL DB via async way',
+      url='https://github.com/Daniil-Kost/lemkpg',
+      author='Daniil Kostyshak',
+      author_email='lemk@ukr.net',
+      license='MIT',
+      packages=['lemkpg'],
+      install_requires=[
+          'aiopg',
+          'psycopg2',
+      ],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+      ],
+      zip_safe=False)
