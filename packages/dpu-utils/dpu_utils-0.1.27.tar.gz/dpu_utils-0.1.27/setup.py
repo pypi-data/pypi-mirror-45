b6ae5bce2856103ba1cf0a0a84083f9af57fd66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:35 +0000

    Add package dpu-utils/0.1.27/dpu_utils-0.1.27.tar.gz

---
+++ packages/dpu-utils/dpu_utils-0.1.27.tar.gz/dpu_utils-0.1.27/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+      name='dpu_utils',
+      version='0.1.27',
+      license='MIT',
+      description='Python utilities used by Deep Procedural Intelligence',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/microsoft/dpu-utils',
+      author='Deep Procedural Intelligence',
+      packages=setuptools.find_packages(),
+      include_package_data=True,
+      install_requires=[
+          'azure-storage', 'numpy', 'docopt', 'tqdm', 'SetSimilaritySearch'
+      ],
+      scripts=['dpu_utils/codeutils/deduplication/deduplicationcli'],
+      test_suite="tests",
+      zip_safe=False)
