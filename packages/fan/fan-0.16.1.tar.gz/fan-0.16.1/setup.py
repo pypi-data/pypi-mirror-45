d3f92e2d4eb630873ec46f73bd2eaf55374b81ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:04 +0000

    Add package fan/0.16.1/fan-0.16.1.tar.gz

---
+++ packages/fan/fan-0.16.1.tar.gz/fan-0.16.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open('fan/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+
+setup(
+    name='fan',
+    packages=find_packages(),
+    version=version,
+    description='microservices kit',
+    author='cybergrind',
+    author_email='cybergrind@gmail.com',
+    keywords=['rpc', 'microservices'],
+    url='https://github.com/tipsi/fan',
+    classifiers=[],
+    entry_points={
+        'console_scripts': [
+            'fan_register=fan.contrib.sync_helper.fan_register:main',
+            'fan=fan.scripts.fan:main',
+        ]
+    },
+    install_requires=[
+        'aiozk',
+        'opentracing',
+        'basictracer',
+        'requests',
+        'kazoo',
+        'tipsi_tools[logging]>=1.43.1',
+        'py-zipkin>=0.18.0,<1.0.0',
+        'aiohttp',
+        'sanic',
+    ],
+)
