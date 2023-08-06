8c6352e76b1cac83c540fb8ce3beddd9002e4c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:38 +0000

    Add package nbtest/0.2.8/nbtest-0.2.8.tar.gz

---
+++ packages/nbtest/nbtest-0.2.8.tar.gz/nbtest-0.2.8/nbtest/__init__.py
@@ -0,0 +1,1 @@
+__version__ = '0.2.8' # delete sys.setdefaultencoding('utf-8')
---
+++ packages/nbtest/nbtest-0.2.8.tar.gz/nbtest-0.2.8/nbtest/future2to3.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals, print_function
+
+# use "".format() insted ""%()
+# use six.text_type insted basestring
+import six
+
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
