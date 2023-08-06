fd741b87f3d1cc1563a0dd26357ee7cb49ffb510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:45 +0000

    Add package okta-cmd/0.0.5/okta-cmd-0.0.5.tar.gz

---
+++ packages/okta-cmd/okta-cmd-0.0.5.tar.gz/okta-cmd-0.0.5/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+
+path = os.path.abspath(os.path.dirname(__file__))
+
+try:
+  with open(os.path.join(path, 'README.md')) as f:
+    long_description = f.read()
+except Exception as e:
+  long_description = "customize okta cli"
+
+setup(
+    name = "okta-cmd",
+    version = "0.0.5",
+    keywords = ("pip", "okta", "cli", "cmd", "steven"),
+    description = "okta cli",
+    long_description = long_description,
+    license = "MIT Licence",
+
+    url = "https://github.com/stevenQiang/okta-cmd",
+    author = "steven",
+    author_email = "qianggao7@gmail.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = ["requests", "click"],
+    platforms = "any",
+
+    scripts = [],
+    entry_points = {
+        'console_scripts': [
+            'okta-cmd=oktacmd:main_cli'
+        ]
+    }
+)
