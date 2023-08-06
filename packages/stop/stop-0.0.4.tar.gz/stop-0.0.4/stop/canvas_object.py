c0470c1370c6a81709f45623bff86de481f68f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:58 +0000

    Add package stop/0.0.4/stop-0.0.4.tar.gz

---
+++ packages/stop/stop-0.0.4.tar.gz/stop-0.0.4/stop/canvas_object.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+class CanvasObject:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.width = 480
+        self.height = 360
+        self.origin = (int(self.width/2), int(self.height/2))
+        self.root.title("scratch.py library demo")
+        self.root.resizable(False, False)
+        # init
+        self.frame_rate = 2
+        self.root.iconbitmap("../favicon.ico")
+        self.canvas = tk.Canvas(self.root, width=self.width, height=self.height, bg="#fff", highlightthickness=0)
+        self.canvas.pack()
+
+    def run(self):
+        self.root.mainloop()
