2cab98e694e8602847f264cb6ca8473d31909c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:34 +0000

    Add package plone.tiles/2.2.1/plone.tiles-2.2.1.tar.gz

---
+++ packages/plone.tiles/plone.tiles-2.2.1.tar.gz/plone.tiles-2.2.1/setup.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+
+version = '2.2.1'
+
+setup(
+    name='plone.tiles',
+    version=version,
+    description='APIs for managing tiles',
+    long_description=open('README.rst').read() + '\n' +
+    open(os.path.join('plone', 'tiles', 'tiles.rst')).read() + '\n' +
+    open(os.path.join('plone', 'tiles', 'directives.rst')).read() + '\n' +
+    open(os.path.join('plone', 'tiles', 'esi.rst')).read() + '\n' +
+    open('CHANGES.rst').read(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.3',
+        'Framework :: Plone :: 5.0',
+        'Framework :: Plone :: 5.1',
+        'Framework :: Plone :: 5.2',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='plone tiles',
+    author='Martin Aspeli',
+    author_email='optilude@gmail.com',
+    url='https://github.com/plone/plone.tiles',
+    license='GPL version 2',
+    packages=find_packages(),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'plone.subrequest',
+        'setuptools',
+        'zope.annotation',
+        'zope.browserpage',
+        'zope.component',
+        'zope.configuration',
+        'zope.interface',
+        'zope.publisher',
+        'zope.schema',
+        'zope.security',
+        'zope.traversing',
+        'Zope2',
+    ],
+    extras_require={
+        'test': [
+            'plone.testing [zca, z2]',
+            'plone.rfc822',
+        ],
+    },
+)
