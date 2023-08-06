e93ccdf63302ab1cf657eb0b5ddf1513397c4365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:50 +0000

    Add package tarpit/0.0.19/tarpit-0.0.19.tar.gz

---
+++ packages/tarpit/tarpit-0.0.19.tar.gz/tarpit-0.0.19/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='tarpit',
+    version='0.0.19',
+    author='plank',
+    author_email='sieglive@gmail.com',
+    url='https://lazor.cn',
+    description='一个快速构建服务后台的库，可以快速创建简单后台。',
+    license="GPLv3+",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'alembic>=1.0.7',
+        'celery>=4.2.1',
+        'gevent>=1.4.0',
+        'motor>=2.0.0',
+        'PyJWT>=1.7.1',
+        'pymongo>=3.7.2',
+        'PyMySQL>=0.9.3',
+        'PyYAML>=5.1',
+        'redis>=3.2.0',
+        'requests>=2.21.0',
+        'SQLAlchemy>=1.2.18',
+        'tornado>=5.1.1',
+        'cryptography>=2.6.1',
+    ],
+    entry_points={'console_scripts': ['tarpit=tarpit:main']},
+)
---
+++ packages/tarpit/tarpit-0.0.19.tar.gz/tarpit-0.0.19/tarpit/checker.py
@@ -0,0 +1,72 @@
+class RequestParameter:
+
+    __param_name__ = None
+    __param_type__ = None
+    __param_subparam__ = None
+    __param_optional__ = True
+    __param_default__ = None
+
+    def __init__(self, name):
+        self.__param_name__ = name
+
+    def __rshift__(self, attr_type):
+        if self.__param_type__ is not None:
+            raise ValueError('Type of Parameter already set.')
+        if not isinstance(attr_type, (type, list, set)):
+            raise ValueError(
+                f'Type of Parameter must be a instance of "type" "list" or "set", not "{type(attr_type).__name__}"'
+            )
+        if isinstance(attr_type, type):
+            self.__param_type__ = attr_type
+        elif isinstance(attr_type, set):
+            self.__param_type__ = type(attr_type)
+            self.__param_subparam__ = attr_type
+            self.__param_default__ = type(attr_type)()
+        elif isinstance(attr_type, list):
+            if len(attr_type) == 1 and isinstance(attr_type[0], type):
+                self.__param_type__ = type(attr_type)
+                self.__param_subparam__ = attr_type[0]
+            else:
+                self.__param_type__ = type(attr_type)
+                self.__param_subparam__ = attr_type
+                self.__param_default__ = type(attr_type)()
+
+        return self
+
+    @property
+    def optional(self):
+        return self.__param_optional__
+
+    @property
+    def default(self):
+        return self.__param_default__
+
+    @property
+    def type(self):
+        return self.__param_type__
+
+    @property
+    def name(self):
+        return self.__param_name__
+
+    @property
+    def subparam(self):
+        return self.__param_subparam__
+
+
+class P(RequestParameter):
+    """optional parameter builder."""
+    __param_optional__ = True
+
+    def __ge__(self, value):
+        self.__param_default__ = value
+        return self
+
+
+class M(RequestParameter):
+    """required parameter builder."""
+    __param_optional__ = False
+
+    def __ge__(self, value):
+        raise ValueError(
+            'Only optional parameter <class "P"> can set default value.')
