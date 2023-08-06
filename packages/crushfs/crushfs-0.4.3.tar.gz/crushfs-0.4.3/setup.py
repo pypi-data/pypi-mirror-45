ee8175e3fb857c980617b920e9bdee1c1ba1a73c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:58 +0000

    Add package crushfs/0.4.3/crushfs-0.4.3.tar.gz

---
+++ packages/crushfs/crushfs-0.4.3.tar.gz/crushfs-0.4.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='crushfs',
+    version='0.4.3',
+    packages=find_packages(),
+    install_requires=['aiohttp','requests'],
+    author='David Poirier',
+    author_email='david@pxcrush.net',
+    description='Python 3 client for CrushFS (sync and async)',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='crushfs',
+    url='https://github.com/crushfs/crushfs-python-client',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+)
