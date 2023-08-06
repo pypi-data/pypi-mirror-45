2fea6138b7bcdae71166a91f8f716d390953ecb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:10 +0000

    Add package ytgrep/0.4.0/ytgrep-0.4.0.tar.gz

---
+++ packages/ytgrep/ytgrep-0.4.0.tar.gz/ytgrep-0.4.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('LICENSE') as f:
+    license = f.read()
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='ytgrep',
+    version='0.4.0',
+    description='CLI tool to search youtube captions',
+    long_description=readme,
+    author='Alex Kohler',
+    author_email='alexjohnkohler@gmail.com',
+    license=license,
+    packages=find_packages(exclude=('test')),
+    py_modules=['ytgrep'],
+    entry_points={
+        "console_scripts": ['ytgrep = ytgrep:main']
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+    ]
+)
