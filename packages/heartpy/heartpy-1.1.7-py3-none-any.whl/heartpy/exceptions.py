a891b3413b804a4ac720d5e109b711cf7b3db775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:52 +0000

    Add package heartpy/1.1.7/heartpy-1.1.7-py3-none-any.whl

---
+++ packages/heartpy/heartpy-1.1.7-py3-none-any.whl/heartpy/exceptions.py
@@ -0,0 +1,16 @@
+'''
+This module contains the custom exceptions and warnings 
+for the HeartPy module
+'''
+
+__all__ = ['BadSignalWarning']
+
+class BadSignalWarning(UserWarning):
+    '''
+    warning class to raise when no heart rate is detectable
+    in supplied signal.
+    
+    This warning notifies the user that the supplied signal is
+    of insufficient quality and/or does not contain enough information
+    to properly process.
+    '''
