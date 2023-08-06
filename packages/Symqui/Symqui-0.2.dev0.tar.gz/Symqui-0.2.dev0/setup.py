33a7cdba35f79876c256744de2915e18616b0090
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:44 +0000

    Add package Symqui/0.2.dev0/Symqui-0.2.dev0.tar.gz

---
+++ packages/Symqui/Symqui-0.2.dev0.tar.gz/Symqui-0.2.dev0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='Symqui',
+    url='https://github.com/suksmono/Symque',
+    author='Andriyan Suksmono',
+    author_email='suksmono@yahoo.com',
+    #install_requires=['sympy'],
+    version='0.2dev',
+    packages=['symqui'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
