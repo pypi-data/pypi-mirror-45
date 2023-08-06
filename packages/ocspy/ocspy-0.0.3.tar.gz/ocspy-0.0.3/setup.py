1751aa4dd7acd7a3498fcbc5e7a4b825d1c58c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:00 +0000

    Add package ocspy/0.0.3/ocspy-0.0.3.tar.gz

---
+++ packages/ocspy/ocspy-0.0.3.tar.gz/ocspy-0.0.3/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "ocspy",
+    version = "0.0.3",
+    keywords = ("pip", "optical communication","coherent optical", "ocspy", "ocspy"),
+    description = "simulate optical communication",
+    long_description = "simulate optical communication",
+    license = "MIT Licence",
+
+    url = "",
+    author = "nigulasikaochuan",
+    author_email = "nigulasikaochuan@sjtu.edu.cn",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = []
+)
