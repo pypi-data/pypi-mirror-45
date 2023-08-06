92216a08f84dccc0b4766668df7de3c9aa623965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:51 +0000

    Add package ppm-utils/0.6.6/ppm-utils-0.6.6.tar.gz

---
+++ packages/ppm-utils/ppm-utils-0.6.6.tar.gz/ppm-utils-0.6.6/setup.py
@@ -0,0 +1,63 @@
+import os
+import re
+import sys
+from io import open
+
+from setuptools import find_packages, setup
+
+
+def read(f):
+    return open(f, 'r', encoding='utf-8').read()
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.path.join(package, '__init__.py'))).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version('ppmutils')
+
+setup(
+    name='ppm-utils',
+    version=version,
+    url='https://github.com/hms-dbmi/ppm-utils',
+    author='HMS DBMI Tech-core',
+    author_email='dbmi-tech-core@hms.harvard.edu',
+    packages=find_packages(exclude=['tests*']),
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    install_requires=[
+        'django>=1.11,<3.0',
+        'python-dateutil',
+        'fhirclient',
+        'requests',
+        'furl',
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
+
+try:
+    from semantic_release import setup_hook
+    setup_hook(sys.argv)
+except ImportError:
+    pass
