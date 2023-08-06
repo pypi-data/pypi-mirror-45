ffae57c45b054a14f97c5f79e2d021be503d519b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:57 +0000

    Add package expressionable/1.2/expressionable-1.2-py3-none-any.whl

---
+++ packages/expressionable/expressionable-1.2-py3-none-any.whl/expressionable/utils/operatorenum.py
@@ -0,0 +1,11 @@
+from enum import Enum
+class OperatorEnum(Enum):
+	"""
+	Class representing an indicator for a logical operator
+	"""
+	Equals=0
+	GreaterThan = 1
+	GreaterThanOrEqualTo=2
+	LessThan = 3
+	LessThanOrEqualTo = 4
+	NotEquals=5 
