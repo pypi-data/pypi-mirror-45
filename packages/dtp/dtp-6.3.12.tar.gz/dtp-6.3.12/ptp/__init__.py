898fcb324629637c6a68a18ca67969c3b778d94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:44 +0000

    Add package dtp/6.3.12/dtp-6.3.12.tar.gz

---
+++ packages/dtp/dtp-6.3.12.tar.gz/dtp-6.3.12/ptp/__init__.py
@@ -0,0 +1,13 @@
+# encoding:utf-8 
+__version__ = "6.3.11.6"
+__date__ = "2018-12-09"
+
+from ptp.py_CtpMd     import py_CtpMd
+from ptp.py_CtpTrader import py_CtpTrader
+
+__all__ = (
+    "MdApiWrapper",
+    "TraderApiWrapper",
+    "py_CtpMd",
+    "py_CtpTrader"
+)
