2a813469a89c9f8fa4d8b3038d03997e299a9c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:18 +0000

    Add package ml-dwm/0.1/ml_dwm-0.1.tar.gz

---
+++ packages/ml-dwm/ml_dwm-0.1.tar.gz/ml_dwm-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ml_dwm',
+      version='0.1',
+      description='done',
+      url='',
+      author='Ankesh',
+      author_email='ankeshphapale1234@gmail.com',
+      license='MIT',
+      packages=['ml_dwm'],
+      zip_safe=False)
