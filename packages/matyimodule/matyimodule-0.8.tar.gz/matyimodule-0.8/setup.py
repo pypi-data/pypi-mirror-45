cac21b5133caab7fc9236ba687cf4a053ae19e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:12 +0000

    Add package matyimodule/0.8/matyimodule-0.8.tar.gz

---
+++ packages/matyimodule/matyimodule-0.8.tar.gz/matyimodule-0.8/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+import codecs
+ld = codecs.open('README.md','r','utf-8').read()
+
+setuptools.setup(
+    name="matyimodule",
+    version="0.8",
+    description="Egy minta, hogy hogyan kell Pip csomagot csinálni.",
+    long_description=ld,
+    long_description_content_type="text/markdown",
+    author="Matyi",
+    author_email="mmatyi1999@gmail.com",
+    url="https://github.com/matyifkbt/matyimodule",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'markdown',
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    entry_points={
+        'console_scripts': ['matyicommand=matyimodule.app:main'],
+    },
+    include_package_data=True,
+)
