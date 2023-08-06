3bf0208d97cafa4594b477ee13f89593df1f1b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:18 +0000

    Add package madagascar/0.1.1/madagascar-0.1.1.tar.gz

---
+++ packages/madagascar/madagascar-0.1.1.tar.gz/madagascar-0.1.1/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+# Package meta-data
+NAME = "madagascar"
+DESCRIPTION = "A package for geophysical data processing"
+URL = "https://github.com/pyahay/madagascar"
+AUTHOR = "Madagascar develop team"
+VERSION = "0.1.1"
+
+REQUIREMENTS = ["numpy"]
+
+# Open README.md and use it as the long-description.
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    url=URL,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=REQUIREMENTS,
+    license='GPLv3',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ]
+)
