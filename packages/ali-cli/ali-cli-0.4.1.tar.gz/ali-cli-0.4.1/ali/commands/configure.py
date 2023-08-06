28408c16259320e95abb1f7c5f0920ff9f8a7042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:51 +0000

    Add package ali-cli/0.4.1/ali-cli-0.4.1.tar.gz

---
+++ packages/ali-cli/ali-cli-0.4.1.tar.gz/ali-cli-0.4.1/ali/commands/configure.py
@@ -0,0 +1,12 @@
+import click
+import json
+from pathlib import Path
+
+
+@click.command()
+@click.pass_obj
+def configure(obj):
+    """Initialize CLI with an access key """
+    click.echo(
+        "Configuration through the `ali` command is currently unsupported. Please run `aliyun configure` instead."
+    )
