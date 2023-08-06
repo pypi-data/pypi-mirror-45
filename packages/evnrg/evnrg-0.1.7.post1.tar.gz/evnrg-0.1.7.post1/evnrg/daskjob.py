cbac6674bd424fcb06b76184c1a87efe59b346cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:46 +0000

    Add package evnrg/0.1.7.post1/evnrg-0.1.7.post1.tar.gz

---
+++ packages/evnrg/evnrg-0.1.7.post1.tar.gz/evnrg-0.1.7.post1/evnrg/daskjob.py
@@ -0,0 +1,56 @@
+from dask.distributed import progress
+from dask.distributed import Client
+
+import dask
+import os
+import uuid
+from typing import List
+import math
+import logging
+
+from .scenario import Scenario
+from .dataset import DatasetInfo
+from .datastorage import StorageInfo
+from .simulation import run_simulation, SimulationResult
+from .numbaloop import run_simulation as nb_run_simulation
+from .job_results import JobResults
+
+class DaskJobRunner(object):
+
+    __slots__ = (
+        'scheduler_address',
+        'client'
+    )
+
+    def __init__(self, scheduler_address = ''):
+
+        self.scheduler_address = scheduler_address
+        if not scheduler_address:
+            self.client = Client()
+            self.scheduler_address = 'localhost:8786'
+        else:
+            self.client = Client(self.scheduler_address)
+    
+    def run_simulations(self, scenarios: List[Scenario], datasets: List[DatasetInfo], 
+                        storage_info: StorageInfo, print_client_info = True, numba = True):
+
+        client = self.client
+
+        logging.info('Running Scenarios...')
+
+        results = []
+
+        for scenario in scenarios:
+            for dataset in datasets:
+                if numba:
+                    sim_result = dask.delayed(nb_run_simulation)(dataset, scenario, storage_info)
+                    results.append(sim_result)
+                else:
+                    sim_result = dask.delayed(run_simulation)(dataset, scenario, storage_info)
+                    results.append(sim_result)
+          
+        out = dask.compute(*results)
+      
+        return out
+    
+    
---
+++ packages/evnrg/evnrg-0.1.7.post1.tar.gz/evnrg-0.1.7.post1/evnrg/plug.py
@@ -0,0 +1,8 @@
+import enum
+
+class DCPlug(enum.IntEnum):
+    """DCFC connector enumeration"""
+    NONE = 0
+    CHADEMO = 6
+    COMBO = 7
+    TESLA = 8
