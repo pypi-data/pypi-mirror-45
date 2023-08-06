22772e5e953874b760b7f59d61e119d8623d62a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:27 +0000

    Add package refcliq/0.1.1/refcliq-0.1.1-py3-none-any.whl

---
+++ packages/refcliq/refcliq-0.1.1-py3-none-any.whl/refcliq/template/precache-manifest.4c066fbd44b7358b5154c28f3d74807d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "16abcf504e3f0b14e4e7",
+    "url": "/static/js/main.e93fc89a.chunk.js"
+  },
+  {
+    "revision": "faaab8148b18bf470d1a",
+    "url": "/static/js/2.1d3a63ae.chunk.js"
+  },
+  {
+    "revision": "16abcf504e3f0b14e4e7",
+    "url": "/static/css/main.13b54120.chunk.css"
+  },
+  {
+    "revision": "faaab8148b18bf470d1a",
+    "url": "/static/css/2.7f2d735d.chunk.css"
+  },
+  {
+    "revision": "a339fc72765ccc027093ee444eea4ab6",
+    "url": "/index.html"
+  }
+];
