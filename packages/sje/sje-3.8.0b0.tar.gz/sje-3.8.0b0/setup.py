7ff6069027d49ae08026cf78daa07f3488eb458c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:59 +0000

    Add package sje/3.8.0b0/sje-3.8.0b0.tar.gz

---
+++ packages/sje/sje-3.8.0b0.tar.gz/sje-3.8.0b0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='sje',
+      version='3.8.0-beta',
+      description='nthng',
+      author_email='oneview-pythonsdk@hpe.com',
+      license='MIT',
+      install_requires=['future>=0.15.2'])
