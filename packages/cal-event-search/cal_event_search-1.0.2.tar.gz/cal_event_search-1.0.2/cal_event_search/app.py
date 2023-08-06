b0662b4d52a14672802b2d2daf8489fbd4bfa404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:05 +0000

    Add package cal-event-search/1.0.2/cal_event_search-1.0.2.tar.gz

---
+++ packages/cal-event-search/cal_event_search-1.0.2.tar.gz/cal_event_search-1.0.2/cal_event_search/app.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5 import QtGui, QtCore, QtWidgets
+from Form import Ui_Form
+
+
+def run():
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    run()
+
