3ebca4287bd4e09e84696306117643c5219c5779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:47 +0000

    Add package django-etesync-journal/1.0.2/django-etesync-journal-1.0.2.tar.gz

---
+++ packages/django-etesync-journal/django-etesync-journal-1.0.2.tar.gz/django-etesync-journal-1.0.2/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-etesync-journal',
+    version='1.0.2',
+    packages=find_packages(),
+    include_package_data=True,
+    license='AGPLv3',
+    description='The server side implementation of the EteSync protocol.',
+    long_description=README,
+    url='https://www.etesync.com/',
+    author='EteSync',
+    author_email='development@etesync.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
---
+++ packages/django-etesync-journal/django-etesync-journal-1.0.2.tar.gz/django-etesync-journal-1.0.2/tests/test_settings.py
@@ -0,0 +1,39 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DEBUG = False
+
+SECRET_KEY = 'fake-key'
+
+ROOT_URLCONF = 'tests.test_urls'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.sites',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'journal.apps.JournalConfig',
+    'tests',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
