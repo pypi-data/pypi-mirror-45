872f2e3048b9e85425c3fd6bc7c3706a03695fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:46 +0000

    Add package wumbo/1.0.0/wumbo-1.0.0.tar.gz

---
+++ packages/wumbo/wumbo-1.0.0.tar.gz/wumbo-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+    name = 'wumbo',
+    version = '1.0.0',
+    packages= ['wumbo'],
+    description = "Wumbology, the study of Wumbo",
+    author = 'Aaron Graubert, but mostly Shankara Anand',
+    author_email = 'totallyfakeemail@gmail.com',
+    long_description = "Wumbology, the study of Wumbo",
+    classifiers = [
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+)
