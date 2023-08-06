c7e78b82f2cf94fb27bd5cfa73ae3510907afc29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:10 +0000

    Add package tblib/1.4.0/tblib-1.4.0.tar.gz

---
+++ packages/tblib/tblib-1.4.0.tar.gz/tblib-1.4.0/tests/test_issue30.py
@@ -0,0 +1,24 @@
+import pickle
+import sys
+
+import six
+from twisted.python.failure import Failure
+
+from tblib import pickling_support
+
+
+def test_30():
+    pickling_support.install()
+
+    try:
+        raise ValueError
+    except ValueError:
+        s = pickle.dumps(sys.exc_info())
+
+    f = None
+    try:
+        six.reraise(*pickle.loads(s))
+    except ValueError:
+        f = Failure()
+
+    assert f is not None
