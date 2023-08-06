62e8479ea5b56b042c2b2e4b9000b1efc3a4d6be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:55 +0000

    Add package hal9000/0.0.0/hal9000-0.0.0.tar.gz

---
+++ packages/hal9000/hal9000-0.0.0.tar.gz/hal9000-0.0.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    author="Carles Sala",
+    author_email='carles@pythiac.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    license="MIT license",
+    include_package_data=True,
+    name='hal9000',
+    url='https://github.com/csala/hal9000',
+    version='0.0.0',
+    zip_safe=False,
+)
