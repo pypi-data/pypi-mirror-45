ad2b71985659b4fea67b153c48a61af26fc42eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:24 +0000

    Add package antidote/0.5.1/antidote-0.5.1-cp37-cp37m-manylinux1_x86_64.whl

---
+++ packages/antidote/antidote-0.5.1-cp37-cp37m-manylinux1_x86_64.whl/antidote/helpers/__init__.py
@@ -0,0 +1,6 @@
+from .container import new_container
+from .factory import factory
+from .provider import provider
+from .register import register
+from .conf import LazyConfigurationMeta
+from .wire import wire
