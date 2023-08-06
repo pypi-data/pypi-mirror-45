fd376c97c5be911dc7ea98f1ff9f7f7c90fa994f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:11 +0000

    Add package doku/0.1.8/doku-0.1.8-py3-none-any.whl

---
+++ packages/doku/doku-0.1.8-py3-none-any.whl/doku/main.py
@@ -0,0 +1,150 @@
+import click
+import os
+import re
+import tempfile
+from datetime import datetime
+from subprocess import Popen
+from textwrap import indent
+
+from doku.context import Context
+
+pass_ctx = click.make_pass_decorator(Context)
+
+"""
+Potential:
+    - dokuwiki.appendPage -> Diary
+"""
+
+@click.group()
+@click.pass_context
+def cli(ctx):
+    ctx.obj = Context()
+
+@cli.command()
+@click.option('--namespace', default='')
+@pass_ctx
+def list(ctx, namespace):
+    pages = ctx.client.call('dokuwiki.getPagelist', namespace, [])
+    for p in pages:
+        t = datetime.utcfromtimestamp(p['rev']).strftime('%Y-%m-%d %H:%M:%S')
+        print('[{date}] {page}'.format(page=p['id'], date=t))
+
+@cli.command()
+@click.argument('name')
+@pass_ctx
+def cat(ctx, name):
+    print(ctx.client.call('wiki.getPage', name))
+
+@cli.command()
+@click.argument('query')
+@pass_ctx
+def search(ctx, query):
+    results = ctx.client.call('dokuwiki.search', query)
+    for hit in results:
+        print('> {}'.format(hit['id']))
+        if 'snippet' in hit:
+            content = re.sub(
+                r'</strong>|<strong class="search_hit">',
+                '',
+                hit['snippet']
+            )
+            print(indent(content, ' ' * 4, lambda line: True))
+        print()
+
+@cli.group()
+def diary():
+    pass
+
+@diary.command()
+@pass_ctx
+def show(ctx):
+    content = ctx.client.call('wiki.getPage', ctx.today())
+    if content == '':
+        print('No entries for today, yet!')
+    else:
+        print(content)
+
+@diary.command()
+@click.option('-p', '--pipe', is_flag=True, default=False, help='read content from stdin')
+@click.option('-c', '--code', is_flag=True, default=False, help='wraps content from stdin into <code></code> block; automatically applies --pipe')
+@pass_ctx
+def log(ctx, pipe, code):
+    ctx.fill_month_view()
+    ctx.fill_year_view()
+    ctx.update_diary_root()
+
+    if code:
+        pipe = True
+
+    if pipe:
+        content = click.get_text_stream('stdin').read().strip()
+        if content == '':
+            print('What? No more content!')
+            return
+        if code:
+            content = '\n'.join(['<code>', content, '</code>'])
+    else:
+        fp, path = tempfile.mkstemp(prefix='doku', suffix='.wiki', text=True)
+        os.close(fp)
+
+        with Popen([ctx.editor, path]) as proc:
+            proc.wait()
+
+        with open(path, 'r') as fp:
+            content = fp.read()
+
+        os.remove(path)
+
+    if not all(l == '' for l in content.split('\n')[-2:]):
+        content = '\n'.join([content, ''])
+
+    content = '\n'.join([
+        datetime.now().strftime('=== %r ===\n'),
+        content
+    ])
+
+    ret = ctx.client.call(
+        'dokuwiki.appendPage',
+        ctx.today(),
+        content,
+        {'sum': 'Log added with doku command line tool'}
+    )
+
+    if ret:
+        print('Document saved.')
+    else:
+        print('Something went wrong. :(')
+
+
+@cli.command()
+@click.argument('name')
+@pass_ctx
+def edit(ctx, name):
+    data = ctx.client.call('wiki.getPage', name)
+
+    fp, path = tempfile.mkstemp(prefix='doku', suffix='.wiki', text=True)
+    os.write(fp, data.encode())
+    os.close(fp)
+
+    with Popen([ctx.editor, path]) as proc:
+        proc.wait()
+
+    with open(path, 'r') as fp:
+        ret = ctx.client.call(
+            'wiki.putPage',
+            name,
+            fp.read(),
+            {'sum': 'Updated with doku command line tool'}
+        )
+
+        if ret:
+            print('Document saved.')
+        else:
+            print('Something went wrong. :(')
+
+    os.remove(path)
+
+
+if __name__ == '__main__':
+    cli()
+
